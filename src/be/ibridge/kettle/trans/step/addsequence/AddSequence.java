27d5a08fcf2b9b7ec7ba6310f699091801c215bb
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-01-11 20:01:46 +0000

    Added rowlevel logging
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@190 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/addsequence/AddSequence.java
+++ src/be/ibridge/kettle/trans/step/addsequence/AddSequence.java
@@ -112,11 +112,14 @@ public class AddSequence extends BaseStep implements StepInterface
 			return false;
 		}
 
+        log.logRowlevel(toString(), "Read row #"+linesRead+" : "+r);
+
 		try
 		{
 			addSequence(r); // add new values to the row in rowset[0].			
 			putRow(r);       // copy row to output rowset(s);
 			
+            log.logRowlevel(toString(), "Wrote row #"+linesWritten+" : "+r);
 			if ((linesRead>0) && (linesRead>0) && (linesRead%Const.ROWS_UPDATE)==0) logBasic("linenr "+linesRead);
 		}
 		catch(KettleException e)
