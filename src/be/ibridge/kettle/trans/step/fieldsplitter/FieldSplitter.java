4b3eac690748be74c6ca13a214cfeee15c311189
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-04-16 19:48:57 +0000

    Fix for bug - [# 2001] Field Splitter: not working on empty fields Check if v.getString()!=null ...
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@672 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/fieldsplitter/FieldSplitter.java
+++ src/be/ibridge/kettle/trans/step/fieldsplitter/FieldSplitter.java
@@ -46,7 +46,7 @@ import be.ibridge.kettle.trans.step.textfileinput.TextFileInputMeta;
 		super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
 	}
 	
-	private synchronized boolean splitField(Row r)
+	private boolean splitField(Row r)
 	{
 		if (first)
 		{
@@ -85,7 +85,7 @@ import be.ibridge.kettle.trans.step.textfileinput.TextFileInputMeta;
 			String pol[] = new String[meta.getField().length];
 			int prev=0;
 			int i=0;
-			while(prev<v.getString().length() && i<pol.length)
+			while(v.getString()!=null && prev<v.getString().length() && i<pol.length)
 			{
 				pol[i]=polNext(v.getString(), meta.getDelimiter(), prev);
 				logDebug("pol="+pol[i]+", prev="+prev);
@@ -193,7 +193,7 @@ import be.ibridge.kettle.trans.step.textfileinput.TextFileInputMeta;
 	{
 		String retval;
 		
-		if (start>=str.length()) return "";
+		if (str==null || start>=str.length()) return "";
 		
 		int next = str.indexOf(del, start);
 		if (next == start) // ;; or ,, : two consecutive delimiters
