d60f88491632ef2b92bb947130c4b6ec6f6c12df
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-05-17 07:19:58 +0000

    Fix for bug #2140
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@944 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/textfileinput/TextFileInput.java
+++ src/be/ibridge/kettle/trans/step/textfileinput/TextFileInput.java
@@ -879,12 +879,10 @@ public class TextFileInput extends BaseStep implements StepInterface
 			{
 				debug = "normal : data and footer";
 				/* IF we are done reading and we have a footer
-				   AND the number of lines in the buffer is smaller or equal to
-				   the number of footer lines
-				   THEN we can remove the remaining rows from the buffer: they
-				   are all footer rows.
+				   AND the number of lines in the buffer is smaller then the number of footer lines
+				   THEN we can remove the remaining rows from the buffer: they are all footer rows.
 				 */
-				if (data.doneReading && meta.hasFooter() && data.lineBuffer.size() <= meta.getNrFooterLines())
+				if (data.doneReading && meta.hasFooter() && data.lineBuffer.size() < meta.getNrFooterLines())
 				{
 					debug = "normal : footer";
 					data.lineBuffer.clear();
