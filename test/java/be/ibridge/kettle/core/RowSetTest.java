bc8ead9e7ed58854ba54a7d25b73ba275a99c942
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-05-27 14:02:03 +0000

    Changes in unit tests
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1031 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ test/java/be/ibridge/kettle/core/RowSetTest.java
@@ -0,0 +1,154 @@
+package be.ibridge.kettle.core;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+import junit.framework.TestCase;
+import be.ibridge.kettle.core.value.Value;
+
+/**
+ * Test class for the basic functionality of RowSet.
+ *
+ * @author Sven Boden
+ */
+public class RowSetTest extends TestCase
+{
+	/**
+	 * The basic stuff.
+	 */
+	public void testBasicCreation()
+	{
+	    RowSet set = new RowSet(10);
+	    
+	    assertTrue(!set.isDone());
+	    assertTrue(set.isEmpty());
+	    assertTrue(!set.isFull());
+	    assertEquals(0, set.size());
+	}
+
+	/**
+	 * Functionality test.
+	 */
+	public void testFuntionality1()
+	{
+		RowSet set = new RowSet(3);
+
+	    Row r1 = new Row();
+        r1.addValue(new Value("ROWNR", 1L));
+	    Row r2 = new Row();
+        r2.addValue(new Value("ROWNR", 2L));
+	    Row r3 = new Row();
+        r3.addValue(new Value("ROWNR", 3L));
+	    Row r4 = new Row();
+        r4.addValue(new Value("ROWNR", 4L));
+	    Row r5 = new Row();
+        r5.addValue(new Value("ROWNR", 5L));
+
+        assertTrue(set.isEmpty());
+        assertEquals(0, set.size());
+        
+        // Add first row. State 1
+        set.putRow(r1);
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(1, set.size());
+        
+        // Add another row. State: 1 2
+        set.putRow(r2);
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(2, set.size());
+        
+        // Pop off row. State: 2
+        Row r = set.getRow();
+        Value v = r.searchValue("ROWNR");
+        assertEquals(1L, v.getInteger()); 
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(1, set.size());
+
+        // Add another row. State: 2 3
+        set.putRow(r3);
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(2, set.size());        
+
+        // Add another row. State: 2 3 4
+        set.putRow(r4);
+        assertTrue(!set.isEmpty());
+        assertTrue(set.isFull());
+        assertEquals(3, set.size());        
+
+        // Add another row. State: 2 3 4 5
+        // Note that we can still add rows after the set is full.
+        set.putRow(r5);
+        assertTrue(!set.isEmpty());
+        assertTrue(set.isFull());
+        assertEquals(4, set.size());        
+
+        // Pop off row. State: 3 4 5 
+        r = set.getRow();
+        v = r.searchValue("ROWNR");
+        assertEquals(2L, v.getInteger()); 
+        assertTrue(!set.isEmpty());
+        assertTrue(set.isFull());
+        assertEquals(3, set.size());
+
+        // Pop off row. State: 4 5 
+        r = set.getRow();
+        v = r.searchValue("ROWNR");
+        assertEquals(3L, v.getInteger()); 
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(2, set.size());
+
+        // Pop off row. State: 5 
+        r = set.getRow();
+        v = r.searchValue("ROWNR");
+        assertEquals(4L, v.getInteger()); 
+        assertTrue(!set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(1, set.size());
+
+        // Pop off row. State:  
+        r = set.getRow();
+        v = r.searchValue("ROWNR");
+        assertEquals(5L, v.getInteger()); 
+        assertTrue(set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(0, set.size());
+
+        // Pop off row. State:
+        try  {
+            r = set.getRow();
+            fail("expected NoSuchElementException");
+        }
+        catch ( NoSuchElementException ex ) 
+        { }
+        assertTrue(set.isEmpty());
+        assertTrue(!set.isFull());
+        assertEquals(0, set.size());        
+	}
+
+	/**
+	 * Names test. Just for completeness.
+	 */
+	public void testNames()
+	{
+		RowSet set = new RowSet(3);
+
+	    Row r1 = new Row();
+        r1.addValue(new Value("ROWNR", 1L));
+        set.setThreadNameFromToCopy("from", 2, "to", 3);
+        
+        assertEquals("from", set.getOriginStepName());
+        assertEquals(2, set.getOriginStepCopy());
+        assertEquals("to", set.getDestinationStepName());
+        assertEquals(3, set.getDestinationStepCopy());
+        assertEquals(set.toString(), set.getName());
+        assertEquals("from.2 - to.3", set.getName());
+        
+	}
+
+}
--- test/java/be/ibridge/kettle/core/RowTest.java
+++ test/java/be/ibridge/kettle/core/RowTest.java
@@ -8,7 +8,7 @@ import be.ibridge.kettle.core.value.Value;
 
 /**
  * Test class for the basic functionality of Row.
- * 
+ *
  * Rows on the same stream in KETTLE are always assumed to be
  * of the same width.
  *
@@ -24,22 +24,22 @@ public class RowTest extends TestCase
 	    Row r = new Row();
 	    assertTrue(!r.isIgnored());
 	    assertNull(r.getLogdate());
-	    assertEquals(0L, r.getLogtime());	    
+	    assertEquals(0L, r.getLogtime());
 	}
-	
+
 	/**
 	 * Constructor test 2. Several cases.
 	 */
 	public void testConstructor2()
 	{
 		Row rEmpty = new Row();
-		
-	    Row r1 = new Row();	    
+
+	    Row r1 = new Row();
         r1.addValue(new Value("field1", "KETTLE"));
         r1.addValue(new Value("field2", 123L));
         r1.addValue(new Value("field3", true));
-        r1.setIgnore(); 
-        
+        r1.setIgnore();
+
         Row r2 = new Row(r1);
         Row r3 = new Row(rEmpty);
 
@@ -48,38 +48,38 @@ public class RowTest extends TestCase
         assertTrue(r1.equals(r2));
         assertTrue(r1.isIgnored());
         assertEquals(0, r3.size());
-	}	
+	}
 
 	/**
-	 * Test RemoveValue().	
+	 * Test RemoveValue().
 	 */
 	public void testRemoveValue()
 	{
 		Value values[] = {
 		    new Value("field1", "KETTLE"),                // String
-			new Value("field2", 123L),                    // integer			
+			new Value("field2", 123L),                    // integer
 			new Value("field3", 10.5D),                   // double
 			new Value("field4", new Date()),              // Date
 			new Value("field5", true),                    // Boolean
 			new Value("field6", new BigDecimal(123.45)),  // BigDecimal
 			new Value("field6", new BigDecimal(123.60))   // BigDecimal
 		};
-		
+
 	    Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
 		assertEquals(values.length, r1.size());
-		
-		
+
+
 		r1.removeValue(0);
 		assertEquals(values.length - 1, r1.size());
 		assertEquals(values[1], r1.getValue(0));
-		
+
 		boolean removed;
 		int idx;
-		
+
 		idx = r1.searchValueIndex("field5");
 		assertTrue(idx > 0);
 		removed = r1.removeValue("field5");
@@ -104,50 +104,50 @@ public class RowTest extends TestCase
 		assertTrue(removed);
 		assertTrue(idx < 0);
 		assertEquals(values.length - 4, r1.size());
-		
+
 		removed = r1.removeValue("field6");
-		assertTrue(!removed);			
-	}	
-	
+		assertTrue(!removed);
+	}
+
 	/**
-	 * Test clear().	
+	 * Test clear().
 	 */
 	public void testClear()
 	{
 		Value values[] = {
 		    new Value("field1", "KETTLE"),                // String
-			new Value("field2", 123L),                    // integer			
+			new Value("field2", 123L),                    // integer
 			new Value("field3", 10.5D),                   // double
 			new Value("field4", new Date()),              // Date
 			new Value("field5", true),                    // Boolean
 			new Value("field6", new BigDecimal(123.45))   // BigDecimal
 		};
-		
+
 	    Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
 		assertEquals(values.length, r1.size());
-		
+
 		r1.clear();
 		assertEquals(0, r1.size());
-		
+
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
 		assertEquals(values.length, r1.size());
 	}
-	
+
 	/**
-	 * Test addRow().	
+	 * Test addRow().
 	 */
 	public void testAddRow()
 	{
 		Value values[] = {
 		    new Value("field1", "KETTLE"),                // String
-			new Value("field2", 123L),                    // integer			
+			new Value("field2", 123L),                    // integer
 			new Value("field3", 10.5D),                   // double
 			new Value("field4", new Date()),              // Date
 			new Value("field5", true),                    // Boolean
@@ -156,27 +156,27 @@ public class RowTest extends TestCase
 
 		Value values1[] = {
 			new Value("field1", "dupl"),                  // String
-			new Value("field2", "string"),                // String			
+			new Value("field2", "string"),                // String
 			new Value("field7", true),                    // Boolean
      	};
-				
+
 	    Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
 		assertEquals(values.length, r1.size());
-		
+
 	    Row r2 = new Row();
 		for (int i=0; i < values1.length; i++ )
 		{
 			r2.addValue(values1[i]);
 		}
 		assertEquals(values1.length, r2.size());
-		
+
 		r1.addRow(r2);
 		assertEquals(values.length + values1.length, r1.size());
-		
+
 		Value v1 = r1.getValue(values.length);
 		assertEquals(values1[0], v1);
 
@@ -184,17 +184,17 @@ public class RowTest extends TestCase
 		assertEquals(values1[1], v2);
 
 		Value v3 = r1.getValue(values.length+2);
-		assertEquals(values1[2], v3);				
-	}	
+		assertEquals(values1[2], v3);
+	}
 
 	/**
-	 * Test mergeRow().	
+	 * Test mergeRow().
 	 */
 	public void testMergeRow()
 	{
 		Value values[] = {
 		    new Value("field1", "KETTLE"),                // String
-			new Value("field2", 123L),                    // integer			
+			new Value("field2", 123L),                    // integer
 			new Value("field3", 10.5D),                   // double
 			new Value("field4", new Date()),              // Date
 			new Value("field5", true),                    // Boolean
@@ -203,31 +203,31 @@ public class RowTest extends TestCase
 
 		Value values1[] = {
 			new Value("field1", "dupl"),                  // String
-			new Value("field2", "string"),                // String			
+			new Value("field2", "string"),                // String
 			new Value("field7", true),                    // Boolean
      	};
-				
+
 	    Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
 		assertEquals(values.length, r1.size());
-		
+
 	    Row r2 = new Row();
 		for (int i=0; i < values1.length; i++ )
 		{
 			r2.addValue(values1[i]);
 		}
 		assertEquals(values1.length, r2.size());
-		
+
 		r1.mergeRow(r2);
 		assertEquals(values.length + 1, r1.size());
-		
+
 		Value v1 = r1.getValue(values.length);
 		assertEquals(values1[2], v1);
-	}		
-	
+	}
+
 	/**
 	 * Test getValue.
 	 */
@@ -235,40 +235,53 @@ public class RowTest extends TestCase
 	{
 		Value values[] = {
 		    new Value("field1", "KETTLE"),                // String
-			new Value("field2", 123L),                    // integer			
+			new Value("field2", 123L),                    // integer
 			new Value("field3", 10.5D),                   // double
 			new Value("field4", new Date()),              // Date
 			new Value("field5", true),                    // Boolean
 			new Value("field6", new BigDecimal(123.45))   // BigDecimal
 		};
-		
+
 	    Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		for (int i=0; i < values.length; i++ )
 		{
 			Value v = r1.getValue(i);
-			assertTrue(v.equals(values[i]));			
+			assertTrue(v.equals(values[i]));
 		}
-		
+
+		//////////////////////////////////////////////////////////
+		// If you're here because the build fails and you want
+		// to change the following part, think again (Sven Boden).
+		//
+		// A Row returns an IndexOutOfBoundsException when the
+		// requested value is not in the Row. If this would be
+		// catched and a null returned, this check would slow
+		// down processing a lot.
+		//
+		// It's also unusual to get it in KETTLE, as one of the
+		// assumptions is that all Rows on a certain stream are
+		// of the same width (same number of columns).
+		//////////////////////////////////////////////////////////
 		try  {
 		    Value v = r1.getValue(values.length + 1);
 		    fail("expected out of bounds exception");
 		}
 		catch ( IndexOutOfBoundsException e ) {	}
-        
-	}	
+
+	}
 
 	/**
 	 * Test searchValueIndex.
 	 */
 	public void testSearchValueIndex()
-	{	
+	{
 		Date date = new Date();
-		
+
 		Value values[] = {
 			    new Value("field1", "KETTLE"),                // String
 				new Value("field2", 123L),                    // integer
@@ -277,23 +290,23 @@ public class RowTest extends TestCase
 				new Value("field5", true),                    // Boolean
 				new Value("field6", new BigDecimal(123.45)),  // BigDecimal
 		};
-			
+
 		Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		int idx;
 		idx = r1.searchValueIndex("field0");
 		assertEquals(-1L, idx);
-		
+
 		idx = r1.searchValueIndex("field1");
 		assertEquals(0, idx);
-		
+
 		idx = r1.searchValueIndex("field6");
 		assertEquals(5, idx);
-		
+
 		Row r2 = new Row(r1);
 
 		Value values1[] = {
@@ -305,25 +318,25 @@ public class RowTest extends TestCase
 		for (int i=0; i < values1.length; i++ )
 		{
 			r2.addValue(values1[i]);
-		}		
-		
+		}
+
 		idx = r2.searchValueIndex("field8");
 		assertEquals(6, idx);
 
 		idx = r2.searchValueIndex("field8");
 		assertEquals(6, idx);
-		
+
 		idx = r2.searchValueIndex("field9 ");
 		assertEquals(8, idx);
-	}			
-	
+	}
+
 	/**
 	 * Test searchValue.
 	 */
 	public void testSearchValue()
-	{	
+	{
 		Date date = new Date();
-		
+
 		Value values[] = {
 			    new Value("field1", "KETTLE"),                // String
 				new Value("field2", 123L),                    // integer
@@ -332,20 +345,20 @@ public class RowTest extends TestCase
 				new Value("field5", true),                    // Boolean
 				new Value("field6", new BigDecimal(123.45)),  // BigDecimal
 		};
-			
+
 		Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		Value v0 = r1.searchValue("field0");
 		assertNull(v0);
-		
+
 		Value v1 = r1.searchValue("field1");
 		assertEquals("field1", v1.getName());
 		assertEquals("KETTLE", v1.getString());
-		
+
 		Value v2 = r1.searchValue("field2");
 		assertEquals("field2", v2.getName());
 		assertEquals(new Long(123L), new Long(v2.getInteger()));
@@ -364,8 +377,8 @@ public class RowTest extends TestCase
 
 		Value v6 = r1.searchValue("field6");
 		assertEquals("field6", v6.getName());
-		assertEquals(new BigDecimal(123.45), v6.getBigNumber());			
-		
+		assertEquals(new BigDecimal(123.45), v6.getBigNumber());
+
 		Row r2 = new Row(r1);
 
 		Value values1[] = {
@@ -377,47 +390,47 @@ public class RowTest extends TestCase
 		for (int i=0; i < values1.length; i++ )
 		{
 			r2.addValue(values1[i]);
-		}		
-		
+		}
+
 		Value v21 = r2.searchValue("field8");
 		assertEquals("field8", v21.getName());
-		assertEquals("KETTLE ", v21.getString());		
-		
+		assertEquals("KETTLE ", v21.getString());
+
 		/* Second value is never found */
 		Value v22 = r2.searchValue("field8");
 		assertEquals("field8", v22.getName());
-		assertEquals("KETTLE ", v22.getString());		
+		assertEquals("KETTLE ", v22.getString());
 
 		Value v23 = r2.searchValue("field9 ");
 		assertEquals("field9 ", v23.getName());
-		assertEquals("KETTLE1", v23.getString());						
-	}			
-	
+		assertEquals("KETTLE1", v23.getString());
+	}
+
 	/**
 	 * Test isEmpty.
 	 */
 	public void testIsEmpty()
 	{
 		Row r0 = new Row();
-		
-	    Row r1 = new Row();	    
+
+	    Row r1 = new Row();
         r1.addValue(new Value("field1", "KETTLE"));
         r1.addValue(new Value("field2", 123L));
         r1.addValue(new Value("field3", true));
-        
+
         Row r2 = new Row();
         r2.addValue(new Value("field1", (String)null));
         r2.addValue(new Value("field2", (String)null));
-        
+
         Row r3 = new Row();
         r3.addValue(new Value("field1", "KETTLE"));
         r3.addValue(new Value("field2", (String)null));
-        
+
         assertTrue(r0.isEmpty());            /* row with no value is empty */
         assertFalse(r1.isEmpty());			 /* normal row */
         assertTrue(r2.isEmpty());			 /* all values are NULL => empty */
         assertFalse(r3.isEmpty());           /* some values are non null => not empty */
-	}	
+	}
 
 	/**
 	 * Test getFieldNames.
@@ -425,7 +438,7 @@ public class RowTest extends TestCase
 	public void testGetFieldNames()
 	{
 		Row r0 = new Row();
-		
+
 		Value values[] = {
 			    new Value("field1", "KETTLE"),                // String
 				new Value("field2", 123L),                    // integer
@@ -434,23 +447,23 @@ public class RowTest extends TestCase
 				new Value("field5", true),                    // Boolean
 				new Value("field6", new BigDecimal(123.45))   // BigDecimal
 		};
-			
+
 		Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		String fieldNames[] = r1.getFieldNames();
 		for (int i=0; i < fieldNames.length; i++ )
 		{
 			assertEquals(fieldNames[i], values[i].getName());
-		}		
+		}
 		assertEquals(fieldNames.length, values.length);
-		
+
 		String fieldNames1[] = r0.getFieldNames();
 		assertEquals(0L, fieldNames1.length);
-	}		
+	}
 
 	/**
 	 * Test toString().
@@ -458,23 +471,23 @@ public class RowTest extends TestCase
 	public void testToString()
 	{
 		Row r0 = new Row();
-		
+
 		Value values[] = {
 			    new Value("field1", "KETTLE"),                // String
 				new Value("field2", 123L),                    // integer
-				new Value("field3", 10.5D),                   // double			
+				new Value("field3", 10.5D),                   // double
 				new Value("field4", true),                    // Boolean
 				new Value("field5", new BigDecimal(123.0)),   // BigDecimal
 				new Value("field6", (String)null),            // NULL value
 				null                                          // null
 		};
-			
+
 		Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		assertEquals("[]", r0.toString());
 		assertEquals("[field1=KETTLE, field2= 123, field3=10.5, field4=true, field5=123, field6=, NULL]", r1.toString());
 	}
@@ -485,24 +498,24 @@ public class RowTest extends TestCase
 	public void testToStringMeta()
 	{
 		Row r0 = new Row();
-		
+
 		Value values[] = {
 			    new Value("field1", "KETTLE"),                // String
 				new Value("field2", 123L),                    // integer
-				new Value("field3", 10.5D),                   // double			
+				new Value("field3", 10.5D),                   // double
 				new Value("field4", true),                    // Boolean
 				new Value("field5", new BigDecimal(123.0)),   // BigDecimal
 				new Value("field6", (String)null),            // NULL value
 				null                                          // null
 		};
-			
+
 		Row r1 = new Row();
 		for (int i=0; i < values.length; i++ )
 		{
 			r1.addValue(values[i]);
 		}
-		
+
 		assertEquals("[]", r0.toStringMeta());
 		assertEquals("[field1(String), field2(Integer), field3(Number), field4(Boolean), field5(BigNumber), field6(String), NULL]", r1.toStringMeta());
-	}	
+	}
 }
