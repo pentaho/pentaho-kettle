80e49ca2fa814386c7a0bab036e667cf146b1b31
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-06-07 18:02:37 +0000

    Added variable support in field search progress dialog as well.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1164 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/spoon/dialog/SearchFieldsProgressDialog.java
+++ src/be/ibridge/kettle/spoon/dialog/SearchFieldsProgressDialog.java
@@ -10,6 +10,7 @@ import java.lang.reflect.InvocationTargetException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 
+import be.ibridge.kettle.core.LocalVariables;
 import be.ibridge.kettle.core.LogWriter;
 import be.ibridge.kettle.core.Row;
 import be.ibridge.kettle.core.exception.KettleStepException;
@@ -29,6 +30,7 @@ public class SearchFieldsProgressDialog implements IRunnableWithProgress
 	private boolean   before;
 	private TransMeta transMeta;
 	private Row       fields;
+    private Thread parentThread;
 	
 	public SearchFieldsProgressDialog(TransMeta transMeta, StepMeta stepMeta, boolean before)
 	{
@@ -36,10 +38,16 @@ public class SearchFieldsProgressDialog implements IRunnableWithProgress
 		this.stepInfo  = stepMeta;
 		this.before    = before;
 		this.fields    = null;
+        
+        this.parentThread = Thread.currentThread();
 	}
 	
 	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException
 	{
+	    // This is running in a new process: copy some KettleVariables info
+        LocalVariables.getInstance().createKettleVariables(Thread.currentThread().getName(), parentThread.getName(), true);
+
+        
 		int size = transMeta.findNrPrevSteps(stepInfo);
 
 		try
