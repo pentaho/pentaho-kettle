99bd55343249b1aa455e6cfcf80683bc40a3aafa
wconroy <wconroy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-06-07 14:33:15 +0000

    some more test cases.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1161 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/test/row/RowMapTest.java
+++ src/be/ibridge/kettle/test/row/RowMapTest.java
@@ -2,6 +2,7 @@ package be.ibridge.kettle.test.row;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -11,7 +12,7 @@ import be.ibridge.kettle.core.value.Value;
 public class RowMapTest
 {
     private static final int SIZE    =   50000; 
-    private static final int STEPS   =       5;
+    private static final int STEPS   =       20;
     private static final int RETRIES =      10;
     
     private ArrayList rows;
@@ -27,7 +28,7 @@ public class RowMapTest
         for (int i=0;i<SIZE;i++)
         {
             Row row = new Row();
-            Map map = new Hashtable();
+            Map map = new HashMap(fieldNames.length);
 
             for (int f=0;f<fieldNames.length;f++)
             {
@@ -82,8 +83,7 @@ public class RowMapTest
         for (int i=0;i<SIZE;i++)
         {
             Row row = new Row();
-            Map map = new Hashtable();
-
+            
             for (int f=0;f<fieldNames.length;f++)
             {
                 Value value = new Value(fieldNames[f], "dummy");
@@ -91,7 +91,7 @@ public class RowMapTest
             }
             
             rows.add(row);
-            maps.add(map);
+
         }
     }
 
@@ -134,6 +134,92 @@ public class RowMapTest
         // System.out.println("count = "+count);
     }
     
+    public void runTestsFastSearch()
+    {
+        int fieldIndexes[] = null;
+        
+        int count = 0;
+        boolean first=true;
+        
+        for (int s=0;s<STEPS;s++)
+        {
+            for (int i=0;i<SIZE;i++)
+            {
+                Row row = (Row)rows.get(i);
+                
+                if (first)
+                {
+                    first=false;
+                    fieldIndexes = new int[fieldNames.length];
+                    for (int f=0;f<fieldNames.length;f++)
+                    {
+                        fieldIndexes[f] = searchValueIndex(row, fieldNames[f]);
+                    }
+                }
+                
+                for (int f=0;f<fieldNames.length;f++)
+                {
+                    Value value = row.getValue( fieldIndexes[f] );
+                    String str = value.getString();
+                    if (str!=null) count++;
+                }
+            }
+        }
+        // System.out.println("count = "+count);
+    }
+    
+    public void runTestsFastMap()
+    {
+        int fieldIndexes[] = null;
+        
+        int count = 0;
+        boolean first=true;
+        
+        for (int s=0;s<STEPS;s++)
+        {
+            for (int i=0;i<SIZE;i++)
+            {
+                Row row = (Row)rows.get(i);
+                
+                if (first)
+                {
+                    Map map = (Map)maps.get(i); 
+                    first=false;
+                    fieldIndexes = new int[fieldNames.length];
+                    for (int f=0;f<fieldNames.length;f++)
+                    {
+                        int index = ((Integer)map.get(fieldNames[f])).intValue();
+                        fieldIndexes[f] = index;
+                    }
+                }
+                
+                for (int f=0;f<fieldNames.length;f++)
+                {
+                    Value value = row.getValue( fieldIndexes[f] );
+                    String str = value.getString();
+                    if (str!=null) count++;
+                }
+            }
+        }
+        // System.out.println("count = "+count);
+    }
+    
+    public int searchValueIndex(Row row, String name)
+    {
+        if (name==null) return -1;
+
+        for (int i = 0; i < row.size(); i++)
+        {
+            Value v = row.getValue(i);
+            if ( v.getName().equals(name) ) 
+            { 
+                return i; 
+            }
+        }
+
+        return -1;
+    }
+    
     public static void main(String[] args)
     {
         RowMapTest rowMapTest = new RowMapTest();
@@ -142,6 +228,9 @@ public class RowMapTest
 
         long totalDiffWith = 0;
         long totalDiffWithout = 0;
+        long totalDiffFast = 0;
+        long totalDiffFastSearch = 0;
+        long totalDiffWithButInit = 0;
         
         for (int i=1;i<=RETRIES;i++)
         {
@@ -168,13 +257,57 @@ public class RowMapTest
             long endMillis2 = new Date().getTime();
             long diff2 = endMillis2-startMillis2;
             totalDiffWithout+=diff2;
-
             System.out.println("RUN #"+i+" time lapsed without (ms): "+diff2);
+
+            
+            rowMapTest.clear(); // to even the odds
+            
+            System.gc();
+            
+            System.out.println("RUN #"+i+" Starting run fast map...");
+            long startMillis3 = new Date().getTime();
+            rowMapTest.initTestWith();
+            rowMapTest.runTestsFastMap();
+            long endMillis3 = new Date().getTime();
+            long diff3 = endMillis3-startMillis3;
+            totalDiffFast+=diff3;
+            System.out.println("RUN #"+i+" time lapsed fast (ms): "+diff3);
+            
+            rowMapTest.clear(); // to even the odds
+            
+            System.gc();
+            
+            System.out.println("RUN #"+i+" Starting run fast search...");
+            long startMillis4 = new Date().getTime();
+            rowMapTest.initTestWithout();
+            rowMapTest.runTestsFastSearch();
+            long endMillis4 = new Date().getTime();
+            long diff4 = endMillis4-startMillis4;
+            totalDiffFastSearch+=diff4;
+
+            System.out.println("RUN #"+i+" time lapsed fast (ms): "+diff4);            
+            
+            rowMapTest.clear(); // to even the odds
+            
+            System.gc();
+            
+            System.out.println("RUN #"+i+" Starting run without maps, but map init...");
+            long startMillis5 = new Date().getTime();
+            rowMapTest.initTestWith();
+            rowMapTest.runTestsWithout();
+            long endMillis5 = new Date().getTime();
+            long diff5 = endMillis5-startMillis5;
+            totalDiffWithButInit+=diff5;
+            System.out.println("RUN #"+i+" time lapsed without maps, but map init (ms): "+diff5);
+
             System.out.println();
+            
         }
 
         System.out.println("Average runtime using maps  : "+(totalDiffWith/RETRIES));
         System.out.println("Average runtime without maps: "+(totalDiffWithout/RETRIES));
-
+        System.out.println("Average runtime fast map: "+(totalDiffFast/RETRIES));
+        System.out.println("Average runtime fast search: "+(totalDiffFastSearch/RETRIES));
+        System.out.println("Average runtime without maps, with map init: "+(totalDiffWithButInit/RETRIES));
     }
 }
