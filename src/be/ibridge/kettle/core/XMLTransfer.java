78c16a4e056623dfa8cd87d8df64d4165edd96bf
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-06-20 18:11:21 +0000

    Another try for OSX - bug #1805
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1295 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/XMLTransfer.java
+++ src/be/ibridge/kettle/core/XMLTransfer.java
@@ -1,18 +1,16 @@
 package be.ibridge.kettle.core;
 
-import java.io.UnsupportedEncodingException;
-
 import org.eclipse.swt.dnd.ByteArrayTransfer;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.TransferData;
 
 public class XMLTransfer extends ByteArrayTransfer
 {
-    private static final String    MYTYPENAME = "KETTLE_XML_TRANSFER";
+    private static final String MYTYPENAME = "KETTLE_XML_TRANSFER";
 
-    private static final int       MYTYPEID   = registerType(MYTYPENAME);
+    private static final int    MYTYPEID   = registerType(MYTYPENAME);
 
-    private static XMLTransfer _instance  = new XMLTransfer();
+    private static XMLTransfer  _instance  = new XMLTransfer();
 
     public static XMLTransfer getInstance()
     {
@@ -21,20 +19,16 @@ public class XMLTransfer extends ByteArrayTransfer
 
     public void javaToNative(Object object, TransferData transferData)
     {
-        if (!checkMyType(object) || !isSupportedType(transferData)) {
+        if (!checkMyType(object) || !isSupportedType(transferData))
+        {
             DND.error(DND.ERROR_INVALID_DATA);
-          }
-        
+        }
+
         try
         {
-            byte[] buffer = ((DragAndDropContainer)object).getXML().getBytes(Const.XML_ENCODING);
+            byte[] buffer = Base64.encodeBytes(((DragAndDropContainer) object).getXML().getBytes()).getBytes();
 
             super.javaToNative(buffer, transferData);
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            LogWriter.getInstance().logError(toString(), "XML Encoding [" + Const.XML_ENCODING + "] is not supported on this system!");
-            return;
         }
         catch (Exception e)
         {
@@ -43,11 +37,11 @@ public class XMLTransfer extends ByteArrayTransfer
             return;
         }
     }
-    
+
     boolean checkMyType(Object object)
     {
-        if (object == null || !(object instanceof DragAndDropContainer) ) { return false; }
-        
+        if (object == null || !(object instanceof DragAndDropContainer)) { return false; }
+
         return true;
     }
 
@@ -58,16 +52,13 @@ public class XMLTransfer extends ByteArrayTransfer
             try
             {
                 byte[] buffer = (byte[]) super.nativeToJava(transferData);
-                return new DragAndDropContainer(new String(buffer, Const.XML_ENCODING));
-            }
-            catch (UnsupportedEncodingException e)
-            {
-                LogWriter.getInstance().logError(toString(), "XML Encoding [" + Const.XML_ENCODING + "] is not supported on this system!");
-                return null;
+                String xml = new String(Base64.decode(new String(buffer)));
+                return new DragAndDropContainer(xml);
             }
             catch (Exception e)
             {
-                LogWriter.getInstance().logError(toString(), "Unexpected error trying to read a drag and drop container from the XML Transfer type: " + e.toString());
+                LogWriter.getInstance().logError(toString(),
+                        "Unexpected error trying to read a drag and drop container from the XML Transfer type: " + e.toString());
                 LogWriter.getInstance().logError(toString(), Const.getStackTracker(e));
                 return null;
             }
