4ae1ab75aa64b0bbcc3adf0ef52692bb640d42c7
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-07-21 14:53:05 +0000

    new fix for #2818 : keeping 1.4 compatibility
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1451 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/util/EnvUtil.java
+++ src/be/ibridge/kettle/core/util/EnvUtil.java
@@ -3,6 +3,7 @@ package be.ibridge.kettle.core.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Properties;
@@ -15,6 +16,7 @@ import be.ibridge.kettle.core.LogWriter;
 public class EnvUtil
 {
 	private static LogWriter log = LogWriter.getInstance();
+	private static Properties env = null;
 
 	/**
 	 * Returns the properties from the users ketle home directory.
@@ -66,6 +68,46 @@ public class EnvUtil
         local.createKettleVariables(Thread.currentThread().getName(), null, false);
 	}
     
+	/**
+	 * Get System.getenv() in a reflection kind of way. The problem is
+	 * that System.getenv() was deprecated in Java 1.4 while reinstated in 1.5
+	 * This method will get at getenv() using reflection and will return
+	 * empty properties when used in 1.4
+	 * 
+	 * @return Properties containing the environment. You're not meant
+	 *         to change any value in the returned Properties!
+	 */
+	private static final Properties getEnv()
+	{		
+		 Class system = System.class;
+		 if ( env == null )
+		 {
+			 Map returnMap = null;
+			 try  {
+			     Method method = system.getMethod("getenv", (Class[])null);
+			     returnMap = (Map)method.invoke(system, (Object[])null);
+			 }
+   	         catch ( Exception ex )  {
+   	        	 returnMap = null;
+   	         }
+   	         
+   	         env = new Properties();
+   	         if ( returnMap != null )
+   	         {
+   	             // We're on a VM with getenv() defined.
+   	             ArrayList list = new ArrayList(returnMap.keySet());
+   	             for (int i=0;i<list.size();i++)
+   	             {
+   	                 String var = (String)list.get(i);
+   	                 String val = (String)returnMap.get(var);
+   	        	 
+   	        	     env.setProperty(var, val);   	          
+   	             }
+   	         }
+		 }
+		 return env;
+	}
+	
     /**
      * @return an array of strings, made up of all the environment variables available in the VM, format var=value.
      * To be used for Runtime.exec(cmd, envp)
@@ -75,10 +117,9 @@ public class EnvUtil
         KettleVariables vars = KettleVariables.getInstance();
         
         Properties sysprops = new Properties();
-        sysprops.putAll( System.getenv() );
+        sysprops.putAll( getEnv() );
         sysprops.putAll( System.getProperties() );
         sysprops.putAll( vars.getProperties() );
-        
         
         String[] envp = new String[sysprops.size()];
         ArrayList list = new ArrayList(sysprops.keySet());
