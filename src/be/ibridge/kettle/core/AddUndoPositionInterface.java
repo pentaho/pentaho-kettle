2838d7984fb088533d4dbb8ae16525a168fc8118
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-08-21 21:08:59 +0000

    After allignment etc. we need a redraw as well Perhaps we should put this in another interface, for now it will do just fine.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1573 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/AddUndoPositionInterface.java
+++ src/be/ibridge/kettle/core/AddUndoPositionInterface.java
@@ -3,4 +3,5 @@ package be.ibridge.kettle.core;
 public interface AddUndoPositionInterface
 {
     public void addUndoPosition(Object obj[], int pos[], Point prev[], Point curr[]);
+    public void redraw();
 }
