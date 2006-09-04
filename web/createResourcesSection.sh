a2fa0bb9c1f191e58e02f799485c8d77418b4d80
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-09-04 09:02:04 +0000

    Updates to Enterprise DT FTP library, removal of ehcache lib
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1643 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ web/createResourcesSection.sh
@@ -0,0 +1,22 @@
+
+cd ..
+
+echo '
+ <resources>
+  <j2se version="1.4+" java-vm-args="-Xmx256M"/>
+  <jar href="lib/kettle.jar"/>
+'
+
+
+for file in $( find libext -name "*.jar" )
+do
+   echo '  <jar href="'${file}'"/>'
+done
+
+echo '
+  <jar href="libswt/jface.jar"/>
+  <jar href="libswt/runtime.jar"/>
+ </resources>
+'
+
+cd -
