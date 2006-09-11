b6b3044c0e2d988c8de15f4ba90e297ed5cff5d5
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-09-11 14:02:14 +0000

    Put version at 2.3.2-dev
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1737 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/Const.java
+++ src/be/ibridge/kettle/core/Const.java
@@ -59,7 +59,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "2.3.1";
+	public static final String VERSION = "2.3.2-dev";
 
 	/**
 	 * Sleep time waiting when buffer is empty
