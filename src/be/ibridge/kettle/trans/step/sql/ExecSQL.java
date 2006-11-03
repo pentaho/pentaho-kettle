a0837b315d85f76ed7b862ee5b6abb22b3ec31d8
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-11-03 15:07:39 +0000

    Fix for Bug #3751 Spoon: SQL step - result fields will only be set when field "update stats" was defined
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1932 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/sql/ExecSQL.java
+++ src/be/ibridge/kettle/trans/step/sql/ExecSQL.java
@@ -61,21 +61,21 @@ public class ExecSQL extends BaseStep implements StepInterface
             row.addValue(rowsUpdated);
         }
 
-        if (upd!=null && upd.length()>0)
+        if (ins!=null && ins.length()>0)
         {
             Value rowsInserted = new Value(ins, result.getNrLinesOutput());
             rowsInserted.setLength(9);
             row.addValue(rowsInserted);
         }
 
-        if (upd!=null && upd.length()>0)
+        if (del!=null && del.length()>0)
         {
             Value rowsDeleted = new Value(del, result.getNrLinesDeleted());
             rowsDeleted.setLength(9);
             row.addValue(rowsDeleted);
         }
 
-        if (upd!=null && upd.length()>0)
+        if (read!=null && read.length()>0)
         {
             Value rowsRead = new Value(read, result.getNrLinesRead());
             rowsRead.setLength(9);
