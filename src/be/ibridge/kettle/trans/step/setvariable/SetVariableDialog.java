d626d52ba0e67780015b1bada70d61c3a013b761
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-11-07 20:25:44 +0000

    Fix for bug #3763
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1939 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/setvariable/SetVariableDialog.java
+++ src/be/ibridge/kettle/trans/step/setvariable/SetVariableDialog.java
@@ -132,7 +132,7 @@ public class SetVariableDialog extends BaseStepDialog implements StepDialogInter
             {
 		        new ColumnInfo(Messages.getString("SetVariableDialog.Fields.Column.FieldName"), ColumnInfo.COLUMN_TYPE_TEXT, false), //$NON-NLS-1$
 		        new ColumnInfo(Messages.getString("SetVariableDialog.Fields.Column.VariableName"), ColumnInfo.COLUMN_TYPE_TEXT, false), //$NON-NLS-1$
-                new ColumnInfo(Messages.getString("SetVariableDialog.Fields.Column.VariableType"), ColumnInfo.COLUMN_TYPE_CCOMBO, SetVariableMeta.getVariableTypeDescriptions(), true), //$NON-NLS-1$
+                new ColumnInfo(Messages.getString("SetVariableDialog.Fields.Column.VariableType"), ColumnInfo.COLUMN_TYPE_CCOMBO, SetVariableMeta.getVariableTypeDescriptions(), false), //$NON-NLS-1$
             };
 
 		wFields=new TableView(shell, 
