7ebad44d988b584e599a7b5ee9dbc414d473112b
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-11-15 14:40:04 +0000

    Introducing Carte, the a-la-carte Kettle webserver
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1976 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ bin/Carte.bat
@@ -0,0 +1,72 @@
+@echo off
+
+REM **************************************************
+REM ** Make sure we use the correct J2SE version!   **
+REM ** Uncomment the PATH line in case of trouble   **
+REM **************************************************
+
+REM set PATH=C:\j2sdk1.4.2_01\bin;.;%PATH%
+
+REM **************************************************
+REM ** Libraries used by Kettle:                    **
+REM **************************************************
+
+set CLASSPATH=.
+
+REM ******************
+REM   KETTLE Library
+REM ******************
+
+set CLASSPATH=%CLASSPATH%;lib\kettle.jar
+
+REM **********************
+REM   External Libraries
+REM **********************
+
+set CLASSPATH=%CLASSPATH%;libext\CacheDB.jar
+set CLASSPATH=%CLASSPATH%;libext\SQLBaseJDBC.jar
+set CLASSPATH=%CLASSPATH%;libext\activation.jar
+set CLASSPATH=%CLASSPATH%;libext\db2jcc.jar
+set CLASSPATH=%CLASSPATH%;libext\db2jcc_license_cu.jar
+set CLASSPATH=%CLASSPATH%;libext\edtftpj-1.5.3.jar
+set CLASSPATH=%CLASSPATH%;libext\firebirdsql-full.jar
+set CLASSPATH=%CLASSPATH%;libext\hsqldb.jar
+set CLASSPATH=%CLASSPATH%;libext\ifxjdbc.jar
+set CLASSPATH=%CLASSPATH%;libext\javadbf.jar
+set CLASSPATH=%CLASSPATH%;libext\js.jar
+set CLASSPATH=%CLASSPATH%;libext\jt400.jar
+set CLASSPATH=%CLASSPATH%;libext\jtds-1.2.jar
+set CLASSPATH=%CLASSPATH%;libext\jxl.jar
+set CLASSPATH=%CLASSPATH%;libext\log4j-1.2.8.jar
+set CLASSPATH=%CLASSPATH%;libext\mail.jar
+set CLASSPATH=%CLASSPATH%;libext\mysql-connector-java-3.1.13-bin.jar
+set CLASSPATH=%CLASSPATH%;libext\ojdbc14.jar
+set CLASSPATH=%CLASSPATH%;libext\orai18n.jar
+set CLASSPATH=%CLASSPATH%;libext\postgresql-8.1-407.jdbc3.jar
+set CLASSPATH=%CLASSPATH%;libext\edbc.jar
+set CLASSPATH=%CLASSPATH%;libext\jsch-0.1.24.jar
+set CLASSPATH=%CLASSPATH%;libext\interclient.jar
+set CLASSPATH=%CLASSPATH%;libext\sapdbc.jar
+set CLASSPATH=%CLASSPATH%;libext\xdbjdbc.jar
+set CLASSPATH=%CLASSPATH%;libext\rdbthin.jar
+set CLASSPATH=%CLASSPATH%;libext\jackcess-1.1.5.jar
+set CLASSPATH=%CLASSPATH%;libext\commons-collections-3.1.jar
+set CLASSPATH=%CLASSPATH%;libext\commons-logging.jar
+set CLASSPATH=%CLASSPATH%;libext\commons-lang-2.2.jar
+set CLASSPATH=%CLASSPATH%;libext\commons-dbcp-1.2.1.jar
+set CLASSPATH=%CLASSPATH%;libext\commons-pool-1.3.jar
+set CLASSPATH=%CLASSPATH%;libext\nzjdbc.jar
+
+REM ******************************************************************
+REM ** Set java runtime options                                     **
+REM ** Change 128m to higher values in case you run out of memory.  **
+REM ******************************************************************
+
+set OPT=-Xmx512M -cp %CLASSPATH% -Djava.library.path=libswt\win32\ -DKETTLE_HOME="%KETTLE_HOME%" -DKETTLE_REPOSITORY="%KETTLE_REPOSITORY%" -DKETTLE_USER="%KETTLE_USER%" -DKETTLE_PASSWORD="%KETTLE_PASSWORD%"
+
+REM ***************
+REM ** Run...    **
+REM ***************
+
+java %OPT% be.ibridge.kettle.www.Carte %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ bin/carte.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# **************************************************
+# ** Libraries used by Kettle:                    **
+# **************************************************
+
+BASEDIR=$(dirname $0)
+CLASSPATH=$BASEDIR
+CLASSPATH=$CLASSPATH:$BASEDIR/lib/kettle.jar
+CLASSPATH=$CLASSPATH:$BASEDIR/libswt/jface.jar
+CLASSPATH=$CLASSPATH:$BASEDIR/libswt/runtime.jar
+
+# **************************************************
+# ** JDBC & other libraries used by Kettle:       **
+# **************************************************
+
+for f in `find $BASEDIR/libext -type f -name "*.jar"`
+do
+  CLASSPATH=$CLASSPATH:$f
+done
+
+
+# ******************************************************************
+# ** Set java runtime options                                     **
+# ** Change 128m to higher values in case you run out of memory.  **
+# ******************************************************************
+
+OPT="-Xmx256m -cp $CLASSPATH -Djava.library.path=$LIBPATH -DKETTLE_HOME=$KETTLE_HOME -DKETTLE_REPOSITORY=$KETTLE_REPOSITORY -DKETTLE_USER=$KETTLE_USER -DKETTLE_PASSWORD=$KETTLE_PASSWORD"
+
+# ***************
+# ** Run...    **
+# ***************
+
+$JAVA_BIN $OPT be.ibridge.kettle.www.Carte "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
