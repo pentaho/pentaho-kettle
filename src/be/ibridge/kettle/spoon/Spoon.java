3ebe35bd6cc1a7e3a70e0fc880b98810e85f9dde
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-11-17 16:21:05 +0000

    Typo fix in the menu
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@1998 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/spoon/Spoon.java
+++ src/be/ibridge/kettle/spoon/Spoon.java
@@ -1162,7 +1162,7 @@ public class Spoon implements AddUndoPositionInterface
         tiPart   = new TreeItem(selectionTree, SWT.NONE); tiPart.setText(STRING_PARTITIONS);
         tiStep   = new TreeItem(selectionTree, SWT.NONE); tiStep.setText(STRING_STEPS);
         tiHops   = new TreeItem(selectionTree, SWT.NONE); tiHops.setText(STRING_HOPS);
-        tiClus   = new TreeItem(selectionTree, SWT.NONE); tiHops.setText(STRING_CLUSTERS);
+        tiClus   = new TreeItem(selectionTree, SWT.NONE); tiClus.setText(STRING_CLUSTERS);
         tiBase   = new TreeItem(selectionTree, SWT.NONE); tiBase.setText(STRING_BASE);
         tiPlug   = new TreeItem(selectionTree, SWT.NONE); tiPlug.setText(STRING_PLUGIN);
         
