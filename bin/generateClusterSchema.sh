1261ba38a3738e3f1acce7870b2de4ffa1f55fba
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-12-05 10:45:20 +0000

    This script generates a cluster schema from a properties file
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2130 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ bin/generateClusterSchema.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# **************************************************
+# ** Libraries used by Kettle:                    **
+# **************************************************
+
+BASEDIR=$(dirname $0)
+CLASSPATH=$BASEDIR
+CLASSPATH=$CLASSPATH:$BASEDIR/lib/kettle.jar
+
+# **************************************************
+# ** JDBC & other libraries used by Kettle:       **
+# **************************************************
+
+for f in `find $BASEDIR/libext -type f -name "*.jar"` `find $BASEDIR/libext -type f -name "*.zip"`
+do
+  CLASSPATH=$CLASSPATH:$f
+done
+
+
+# **************************************************
+# ** Platform specific libraries ...              **
+# **************************************************
+
+JAVA_BIN=java
+
+# ******************************************************************
+# ** Set java runtime options                                     **
+# ** Change 128m to higher values in case you run out of memory.  **
+# ******************************************************************
+
+if [ -z "$JAVAMAXMEM" ]; then
+  JAVAMAXMEM="256"
+fi
+
+OPT="-Xmx${JAVAMAXMEM}m -cp $CLASSPATH -Djava.library.path=$LIBPATH -DKETTLE_HOME=$KETTLE_HOME -DKETTLE_REPOSITORY=$KETTLE_REPOSITORY -DKETTLE_USER=$KETTLE_USER -DKETTLE_PASSWORD=$KETTLE_PASSWORD"
+
+if [ "$1" = "-x" ]; then
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BASEDIR/libext
+  OPT="-Xruntracer $OPT"
+  shift
+fi
+
+# ***************
+# ** Run...    **
+# ***************
+
+$JAVA_BIN $OPT be.ibridge.kettle.cluster.GenerateClusterSchema "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+
