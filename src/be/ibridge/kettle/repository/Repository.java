36dfb1b8404e3121978cd212cb49eb169ce881de
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2006-12-19 15:15:32 +0000

    Fix for bug #4134
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2213 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/repository/Repository.java
+++ src/be/ibridge/kettle/repository/Repository.java
@@ -1480,7 +1480,7 @@ public class Repository
     
     public synchronized long insertClusterSlave(ClusterSchema clusterSchema, SlaveServer slaveServer) throws KettleDatabaseException
     {
-        long id = getNextClusterID();
+        long id = getNextClusterSlaveID();
 
         Row table = new Row();
 
@@ -1488,7 +1488,7 @@ public class Repository
         table.addValue(new Value("ID_CLUSTER", id));
         table.addValue(new Value("ID_SLAVE", id));
 
-        database.prepareInsert(table, "R_CLUSTER");
+        database.prepareInsert(table, "R_CLUSTER_SLAVE");
         database.setValuesInsert(table);
         database.insertRow();
         database.closeInsert();
