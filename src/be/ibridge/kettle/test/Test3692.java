fa92419c6535acba85f1f9f8f5dc550356ef3693
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-01-02 13:00:48 +0000

    Test code to draw out #3692
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2244 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/be/ibridge/kettle/test/Test3692.java
@@ -0,0 +1,28 @@
+package be.ibridge.kettle.test;
+
+import be.ibridge.kettle.core.LogWriter;
+import be.ibridge.kettle.core.exception.KettleXMLException;
+import be.ibridge.kettle.core.util.EnvUtil;
+import be.ibridge.kettle.job.JobEntryLoader;
+import be.ibridge.kettle.trans.StepLoader;
+import be.ibridge.kettle.trans.Trans;
+import be.ibridge.kettle.trans.TransMeta;
+
+public class Test3692
+{
+    public static void main(String[] args) throws KettleXMLException
+    {
+        // init...
+        EnvUtil.environmentInit();
+        StepLoader.getInstance().read();
+        JobEntryLoader.getInstance().read();
+        LogWriter log = LogWriter.getInstance(LogWriter.LOG_LEVEL_BASIC);
+        
+        TransMeta transMeta = new TransMeta("C:\\Documents and Settings\\Matt\\Bureaublad\\test-stuff\\lostrows.ktr");
+        for (int i=0;i<1000;i++)
+        {
+            Trans trans = new Trans(log, transMeta);
+            trans.execute(null);
+        }
+    }
+}
