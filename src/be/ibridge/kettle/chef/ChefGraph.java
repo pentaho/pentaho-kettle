1d74ebf315b3481ba2e119ea29a97b442a9802ff
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-01-17 23:53:45 +0000

    Don't try to popup unless it's needed (button==3)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2373 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/chef/ChefGraph.java
+++ src/be/ibridge/kettle/chef/ChefGraph.java
@@ -222,7 +222,7 @@ public class ChefGraph extends Canvas implements Redrawable, TabItemInterface
 				setToolTipText(null);
 
 				// Set the pop-up menu
-				setMenu(real.x, real.y);
+				if (e.button==3) setMenu(real.x, real.y);
 				
 				JobEntryCopy je = jobMeta.getChefGraphEntry(real.x, real.y, iconsize);
 				if (je != null) 
