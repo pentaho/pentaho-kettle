63e9437c3c4b111ce29395c2dcf20178c74b0cc5
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-01-19 21:48:53 +0000

    Messages fix, found by Sven Boden
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2406 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/spoon/Spoon.java
+++ src/be/ibridge/kettle/spoon/Spoon.java
@@ -7998,8 +7998,8 @@ public class Spoon implements AddUndoPositionInterface
                     if (jobMeta.showReplaceWarning(rep))
                     {
                         MessageBox mb = new MessageBox(shell, SWT.YES | SWT.NO | SWT.ICON_QUESTION);
-                        mb.setMessage(Messages.getString("Chef.Dialog.JobExistsOverwrite.Message1")+jobMeta.getName()+Messages.getString("Chef.Dialog.JobExistsOverwrite.Message2")+Const.CR+Messages.getString("Chef.Dialog.JobExistsOverwrite.Message3")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-                        mb.setText(Messages.getString("Chef.Dialog.JobExistsOverwrite.Title")); //$NON-NLS-1$
+                        mb.setMessage("'"+jobMeta.getName()+"'"+Const.CR+Const.CR+Messages.getString("Chef.Dialog.FileExistsOverWrite.Message")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        mb.setText(Messages.getString("Chef.Dialog.FileExistsOverWrite.Title")); //$NON-NLS-1$
                         response = mb.open();
                     }
                     
