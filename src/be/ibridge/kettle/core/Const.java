1537a8ee0922aa403f698e9ca256ed8a8004f405
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-01-23 21:25:11 +0000

    Changed the version to 2.4.0 for the doc screenshot, preparing for release.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2440 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/Const.java
+++ src/be/ibridge/kettle/core/Const.java
@@ -59,7 +59,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "2.4.0-dev";
+	public static final String VERSION = "2.4.0";
 
 	/**
 	 * Sleep time waiting when buffer is empty
