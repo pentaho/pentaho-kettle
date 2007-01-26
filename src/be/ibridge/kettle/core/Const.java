58d200204a50c8a15dbd30aad04fc2b6e495dbaf
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-01-26 16:27:28 +0000

    bumping to version 2.4.1-dev
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2474 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/Const.java
+++ src/be/ibridge/kettle/core/Const.java
@@ -59,7 +59,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "2.4.0";
+	public static final String VERSION = "2.4.1-dev";
 
 	/**
 	 * Sleep time waiting when buffer is empty
