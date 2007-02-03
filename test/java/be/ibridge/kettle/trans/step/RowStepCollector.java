381ad5aa0997f69cff790bbe9eb5b57954fd747f
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-02-03 19:46:08 +0000

    Add testcase for injector step + showcase
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2580 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ test/java/be/ibridge/kettle/trans/step/RowStepCollector.java
@@ -0,0 +1,67 @@
+/**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+
+package be.ibridge.kettle.trans.step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.ibridge.kettle.core.Row;
+import be.ibridge.kettle.trans.step.RowListener;
+
+/**
+ * Helper class for testcases. You can add an instance of this class to
+ * a step to read all of the Rows the step read or wrote.
+ *
+ * @author Sven Boden
+ */
+public class RowStepCollector implements RowListener
+{
+	private List rowsRead;
+	private List rowsWritten;
+	
+	public RowStepCollector()
+	{
+		rowsRead = new ArrayList();
+		rowsWritten = new ArrayList();
+	}
+
+	public void rowReadEvent(Row row) {
+		rowsRead.add(row);
+	}
+
+	public void rowWrittenEvent(Row row) {
+		rowsWritten.add(row);
+	}
+	
+	/**
+	 * Clear the rows read and rows written.
+	 */
+	public void clear()
+	{
+		rowsRead.clear();
+		rowsWritten.clear();
+	}
+	
+	public List getRowsRead()
+	{
+		return rowsRead;
+	}
+	
+	public List getRowsWritten()
+	{
+		return rowsWritten;
+	}	
+}
