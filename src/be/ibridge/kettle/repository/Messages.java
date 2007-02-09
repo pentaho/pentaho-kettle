1ee05bdc947739df3179a42105eaeaf076109d66
sven.thiergen <sven.thiergen@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-02-09 12:06:58 +0000

    . removed unused casts (e.g. (int)stuff on key handlers) . removed unused exceptions . removed some unused method parameters . localized lots of dialogs . removed "Props" and "Log" from dialog CTs, made the old CTs "deprecated" . localized "Tips of days" . localized number and date formates . added images to "Options" dialog ("edit_option.png" and "reset_option.png"), see second tab (painted myself, no Copyright violation). Changed and hopefully improved look and feel; if not suiting / acceptable I am going to remove this. . FileInputList: added flag "includeSubdirs" to search in all subdirs for matching filenames
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2607 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/be/ibridge/kettle/repository/Messages.java
@@ -0,0 +1,35 @@
+package be.ibridge.kettle.repository;
+
+import java.util.MissingResourceException;
+
+import be.ibridge.kettle.i18n.GlobalMessages;
+
+public class Messages {
+	public static final String packageName = Messages.class.getPackage().getName();
+    
+    
+	public static String getString(String key) {
+		try {
+			return be.ibridge.kettle.i18n.GlobalMessages.getString(packageName, key);
+		} catch (MissingResourceException e) {
+            return GlobalMessages.getSystemString(key);
+		}
+	}
+    
+    public static String getString(String key, String param1) {
+        try {
+            return be.ibridge.kettle.i18n.GlobalMessages.getString(packageName, key, param1);
+        } catch (MissingResourceException e) {
+            return GlobalMessages.getSystemString(key, param1);
+        }
+    }
+
+    public static String getString(String key, String param1, String param2) {
+        try {
+            return be.ibridge.kettle.i18n.GlobalMessages.getString(packageName, key, param1, param2);
+        } catch (MissingResourceException e) {
+            return GlobalMessages.getSystemString(key, param1, param2);
+        }
+    }
+
+}
