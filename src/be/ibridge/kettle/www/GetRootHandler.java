14021539c782a9454ac2da9eca1b2f2d66a395e9
MattCasters <MattCasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-03-08 21:04:29 +0000

    Convert root handler to a servlet.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/mirror/Kettle/trunk@2921 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/www/GetRootHandler.java
+++ src/be/ibridge/kettle/www/GetRootHandler.java
@@ -3,17 +3,18 @@ package be.ibridge.kettle.www;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mortbay.jetty.HttpConnection;
 import org.mortbay.jetty.Request;
-import org.mortbay.jetty.handler.AbstractHandler;
 
 import be.ibridge.kettle.core.LogWriter;
 
-public class GetRootHandler extends AbstractHandler
+public class GetRootHandler extends HttpServlet
 {
     private static final long serialVersionUID = 3634806745372015720L;
     public static final String CONTEXT_PATH = "/";
@@ -23,13 +24,20 @@ public class GetRootHandler extends AbstractHandler
     public GetRootHandler()
     {
     }
-
-    public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException
+    
+    public void init(ServletConfig servletConfig) throws ServletException
     {
-        if (!request.getPathInfo().equals(CONTEXT_PATH)) return;
-        if (!isStarted()) return;
-        Request baseRequest = (request instanceof Request) ? (Request)request:HttpConnection.getCurrentConnection().getRequest();
-        baseRequest.setHandled(true);
+        super.init(servletConfig);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doPost(request, response);
+    }
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        if (!request.getServletPath().equals(CONTEXT_PATH)) return;
 
         if (log.isDebug()) log.logDebug(toString(), "Root requested");
 
@@ -51,6 +59,9 @@ public class GetRootHandler extends AbstractHandler
         out.println("</HTML>");        
         
         response.flushBuffer();
+        
+        Request baseRequest = (request instanceof Request) ? (Request)request:HttpConnection.getCurrentConnection().getRequest();
+        baseRequest.setHandled(true);
     }
 
     public String toString()
