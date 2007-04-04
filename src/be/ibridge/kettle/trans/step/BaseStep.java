bc2b2c656d7a93ff0107f743a4bf93e864f5486c
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-04-04 20:06:23 +0000

    i18n cleanup for safe mode checking: made the error messages easier to read, on one line, simpeler.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3096 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/BaseStep.java
+++ src/be/ibridge/kettle/trans/step/BaseStep.java
@@ -1214,7 +1214,7 @@ public class BaseStep extends Thread
         // First check the number of fields
         if (referenceRow.size() != row.size())
         {
-            throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.Varying", Const.CR, ""+referenceRow.size(), ""+row.size(), row.toString()));
+            throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.VaryingSize", ""+referenceRow.size(), ""+row.size(), row.toString()));
         }
         else
         {
@@ -1226,12 +1226,12 @@ public class BaseStep extends Thread
 
                 if (!referenceValue.getName().equalsIgnoreCase(compareValue.getName()))
                 {
-                    throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.MixingLayout", ""+(i+1), Const.CR, referenceValue.getName()+" "+referenceValue.toStringMeta(), compareValue.getName()+" " +compareValue.toStringMeta()));
+                    throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.MixingLayout", ""+(i+1), referenceValue.getName()+" "+referenceValue.toStringMeta(), compareValue.getName()+" "+compareValue.toStringMeta()));
                 }
 
                 if (referenceValue.getType()!=compareValue.getType())
                 {
-                    throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.MixingTypes", ""+(i+1), Const.CR, referenceValue.getName()+" "+referenceValue.toStringMeta(), compareValue.getName()+" " +compareValue.toStringMeta()));               
+                    throw new KettleRowException(Messages.getString("BaseStep.SafeMode.Exception.MixingTypes", ""+(i+1), referenceValue.getName()+" "+referenceValue.toStringMeta(), compareValue.getName()+" "+compareValue.toStringMeta()));               
                 }
             }
         }
