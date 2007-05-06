0c7761055b39393380d4475021a1748d78b0a061
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-06 13:18:29 +0000

    CRQ #5556 : Blocking step now optionally passes all rows.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3341 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/blockingstep/BlockingStepDialog.java
+++ src/be/ibridge/kettle/trans/step/blockingstep/BlockingStepDialog.java
@@ -11,6 +11,7 @@ import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
@@ -19,6 +20,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import be.ibridge.kettle.core.Const;
+import be.ibridge.kettle.core.util.StringUtil;
+import be.ibridge.kettle.core.widget.TextVar;
 import be.ibridge.kettle.i18n.BaseMessages;
 import be.ibridge.kettle.trans.TransMeta;
 import be.ibridge.kettle.trans.step.BaseStepDialog;
@@ -29,6 +32,26 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
 {
     private BlockingStepMeta input;
     
+	private Label        wlPassAllRows;
+	private Button       wPassAllRows;
+	
+	private Label        wlSpoolDir;
+	private Button       wbSpoolDir;
+	private TextVar      wSpoolDir;
+	private FormData     fdlSpoolDir, fdbSpoolDir, fdSpoolDir;
+
+	private Label        wlPrefix;
+	private Text         wPrefix;
+	private FormData     fdlPrefix, fdPrefix;
+
+    private Label        wlCacheSize;
+    private Text         wCacheSize;
+    private FormData     fdlCacheSize, fdCacheSize;
+
+    private Label        wlCompress;
+    private Button       wCompress;
+    private FormData     fdlCompress, fdCompress;
+   
     public BlockingStepDialog(Shell parent, Object in, TransMeta transMeta, String sname)
     {
         super(parent, (BaseStepMeta)in, transMeta, sname);
@@ -82,6 +105,146 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
         fdStepname.right= new FormAttachment(100, 0);
         wStepname.setLayoutData(fdStepname);
         
+        // Update the dimension?
+        wlPassAllRows=new Label(shell, SWT.RIGHT);
+        wlPassAllRows.setText(Messages.getString("BlockingStepDialog.PassAllRows.Label")); //$NON-NLS-1$
+        props.setLook(wlPassAllRows);
+        FormData fdlUpdate=new FormData();
+        fdlUpdate.left   = new FormAttachment(0, 0);
+        fdlUpdate.right  = new FormAttachment(middle, -margin);
+        fdlUpdate.top    = new FormAttachment(wStepname, margin);
+        wlPassAllRows.setLayoutData(fdlUpdate);
+        wPassAllRows=new Button(shell, SWT.CHECK);
+        props.setLook(wPassAllRows);
+        FormData fdUpdate=new FormData();
+        fdUpdate.left = new FormAttachment(middle, 0);
+        fdUpdate.top  = new FormAttachment(wStepname, margin);
+        fdUpdate.right= new FormAttachment(100, 0);
+        wPassAllRows.setLayoutData(fdUpdate);
+        
+        // Clicking on update changes the options in the update combo boxes!
+        wPassAllRows.addSelectionListener(new SelectionAdapter()
+            {
+                public void widgetSelected(SelectionEvent e)
+                {
+                    input.setPassAllRows(!input.isPassAllRows());
+                    input.setChanged();
+                    setEnableDialog();                
+                }
+            }
+        );                 
+        
+		// Temp directory for sorting
+		wlSpoolDir=new Label(shell, SWT.RIGHT);
+		wlSpoolDir.setText(Messages.getString("BlockingStepDialog.SpoolDir.Label"));
+ 		props.setLook(wlSpoolDir);
+		fdlSpoolDir=new FormData();
+		fdlSpoolDir.left = new FormAttachment(0, 0);
+		fdlSpoolDir.right= new FormAttachment(middle, -margin);
+		fdlSpoolDir.top  = new FormAttachment(wPassAllRows, margin);
+		wlSpoolDir.setLayoutData(fdlSpoolDir);
+
+		wbSpoolDir=new Button(shell, SWT.PUSH| SWT.CENTER);
+ 		props.setLook(wbSpoolDir);
+		wbSpoolDir.setText(Messages.getString("System.Button.Browse"));
+		fdbSpoolDir=new FormData();
+		fdbSpoolDir.right= new FormAttachment(100, 0);
+		fdbSpoolDir.top  = new FormAttachment(wPassAllRows, margin);
+		wbSpoolDir.setLayoutData(fdbSpoolDir);
+
+		wSpoolDir=new TextVar(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+ 		props.setLook(wSpoolDir);
+		wSpoolDir.addModifyListener(lsMod);
+		fdSpoolDir=new FormData();
+		fdSpoolDir.left = new FormAttachment(middle, 0);
+		fdSpoolDir.top  = new FormAttachment(wPassAllRows, margin);
+		fdSpoolDir.right= new FormAttachment(wbSpoolDir, -margin);
+		wSpoolDir.setLayoutData(fdSpoolDir);
+		
+		wbSpoolDir.addSelectionListener(new SelectionAdapter()
+		{
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				DirectoryDialog dd = new DirectoryDialog(shell, SWT.NONE);
+				dd.setFilterPath(wSpoolDir.getText());
+				String dir = dd.open();
+				if (dir!=null)
+				{
+					wSpoolDir.setText(dir);
+				}
+			}
+		});
+
+		// Whenever something changes, set the tooltip to the expanded version:
+		wSpoolDir.addModifyListener(new ModifyListener()
+			{
+				public void modifyText(ModifyEvent e)
+				{
+					wSpoolDir.setToolTipText(StringUtil.environmentSubstitute( wSpoolDir.getText() ) );
+				}
+			}
+		);
+
+        // Prefix of temporary file
+		wlPrefix=new Label(shell, SWT.RIGHT);
+		wlPrefix.setText(Messages.getString("BlockingStepDialog.Prefix.Label"));
+ 		props.setLook(wlPrefix);
+		fdlPrefix=new FormData();
+		fdlPrefix.left = new FormAttachment(0, 0);
+		fdlPrefix.right= new FormAttachment(middle, -margin);
+		fdlPrefix.top  = new FormAttachment(wbSpoolDir, margin*2);
+		wlPrefix.setLayoutData(fdlPrefix);
+		wPrefix=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER );
+ 		props.setLook(wPrefix);
+		wPrefix.addModifyListener(lsMod);
+		fdPrefix=new FormData();
+		fdPrefix.left  = new FormAttachment(middle, 0);
+		fdPrefix.top   = new FormAttachment(wbSpoolDir, margin*2);
+		fdPrefix.right = new FormAttachment(100, 0);
+		wPrefix.setLayoutData(fdPrefix);
+
+        // Maximum number of lines to keep in memory before using temporary files
+        wlCacheSize=new Label(shell, SWT.RIGHT);
+        wlCacheSize.setText(Messages.getString("BlockingStepDialog.CacheSize.Label"));
+        props.setLook(wlCacheSize);
+        fdlCacheSize=new FormData();
+        fdlCacheSize.left = new FormAttachment(0, 0);
+        fdlCacheSize.right= new FormAttachment(middle, -margin);
+        fdlCacheSize.top  = new FormAttachment(wPrefix, margin*2);
+        wlCacheSize.setLayoutData(fdlCacheSize);
+        wCacheSize=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER );
+        props.setLook(wCacheSize);
+        wCacheSize.addModifyListener(lsMod);
+        fdCacheSize=new FormData();
+        fdCacheSize.left  = new FormAttachment(middle, 0);
+        fdCacheSize.top   = new FormAttachment(wPrefix, margin*2);
+        fdCacheSize.right = new FormAttachment(100, 0);
+        wCacheSize.setLayoutData(fdCacheSize);
+
+        // Using compression for temporary files?
+        wlCompress=new Label(shell, SWT.RIGHT);
+        wlCompress.setText(Messages.getString("BlockingStepDialog.Compress.Label"));
+        props.setLook(wlCompress);
+        fdlCompress=new FormData();
+        fdlCompress.left = new FormAttachment(0, 0);
+        fdlCompress.right= new FormAttachment(middle, -margin);
+        fdlCompress.top  = new FormAttachment(wCacheSize, margin*2);
+        wlCompress.setLayoutData(fdlCompress);
+        wCompress=new Button(shell, SWT.CHECK);
+        props.setLook(wCompress);
+        fdCompress=new FormData();
+        fdCompress.left  = new FormAttachment(middle, 0);
+        fdCompress.top   = new FormAttachment(wCacheSize, margin*2);
+        fdCompress.right = new FormAttachment(100, 0);
+        wCompress.setLayoutData(fdCompress);
+        wCompress.addSelectionListener(new SelectionAdapter() 
+			{
+				public void widgetSelected(SelectionEvent e) 
+				{
+					input.setChanged();
+				}
+			}
+        );        
 
         // Some buttons
         wOK=new Button(shell, SWT.PUSH);
@@ -89,7 +252,7 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
         wCancel=new Button(shell, SWT.PUSH);
         wCancel.setText(BaseMessages.getString(getClass().getPackage().getName(), "System.Button.Cancel")); //$NON-NLS-1$
         
-        setButtonPositions(new Button[] { wOK, wCancel }, margin, wStepname);        
+        setButtonPositions(new Button[] { wOK, wCancel }, margin, wCompress);        
 
         // Add listeners
         lsCancel   = new Listener() { public void handleEvent(Event e) { cancel(); } };
@@ -101,7 +264,7 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
         lsDef=new SelectionAdapter() { public void widgetDefaultSelected(SelectionEvent e) { ok(); } };
         
         wStepname.addSelectionListener( lsDef );
-        
+                
         // Detect X or ALT-F4 or something that kills this window...
         shell.addShellListener( new ShellAdapter() { public void shellClosed(ShellEvent e) { cancel(); } } );
 
@@ -110,6 +273,9 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
         
         getData();
         input.setChanged(changed);
+        
+        // Set the enablement of the dialog widgets
+        setEnableDialog();
     
         shell.open();
         while (!shell.isDisposed())
@@ -124,6 +290,12 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
      */ 
     public void getData()
     {
+    	wPassAllRows.setSelection(input.isPassAllRows());
+		if (input.getPrefix() != null) wPrefix.setText(input.getPrefix());
+		if (input.getDirectory() != null) wSpoolDir.setText(input.getDirectory());
+		wCacheSize.setText(""+input.getCacheSize());
+		wCompress.setSelection(input.getCompress());
+		
         wStepname.selectAll();
     }
     
@@ -137,6 +309,32 @@ public class BlockingStepDialog extends BaseStepDialog implements StepDialogInte
     private void ok()
     {
         stepname = wStepname.getText(); // return value
+        
+		input.setPrefix( wPrefix.getText() );
+		input.setDirectory( wSpoolDir.getText() );
+        input.setCacheSize( Const.toInt( wCacheSize.getText(), BlockingStepMeta.CACHE_SIZE ) );
+        log.logDetailed("Sort rows", "Compression is set to " + wCompress.getSelection());
+        input.setCompress(wCompress.getSelection());
+        input.setPassAllRows(wPassAllRows.getSelection());
+
         dispose();
+    }
+    
+    /**
+     * Set the correct state "enabled or not" of the dialog widgets.
+     */
+    private void setEnableDialog()
+    {
+        boolean isPassRows = input.isPassAllRows();
+        
+    	wlSpoolDir.setEnabled(isPassRows);
+    	wbSpoolDir.setEnabled(isPassRows);
+    	wSpoolDir.setEnabled(isPassRows);
+  	    wlPrefix.setEnabled(isPassRows);
+    	wPrefix.setEnabled(isPassRows);
+        wlCacheSize.setEnabled(isPassRows);
+        wCacheSize.setEnabled(isPassRows);
+        wlCompress.setEnabled(isPassRows);
+        wCompress.setEnabled(isPassRows);
     }
 }
