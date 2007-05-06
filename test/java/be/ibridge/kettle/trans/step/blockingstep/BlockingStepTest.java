b74ff69f8ce4e369c1582446a624967d24dfece7
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-06 13:19:15 +0000

    CRQ #5556 : new unit testcase for Blocking Step
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3342 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- test/java/be/ibridge/kettle/trans/step/blockingstep/BlockingStepTest.java
+++ test/java/be/ibridge/kettle/trans/step/blockingstep/BlockingStepTest.java
@@ -247,4 +247,118 @@ public class BlockingStepTest extends TestCase
         lastList.add(inputList.get(inputList.size() - 1));
         checkRows(resultRows3, lastList);
     }
+    
+	/**
+	 * Test case for blocking step step passing all rows. Injector step to a 
+	 * blocking step to a dummy step. rows go in, all rows should be output.
+	 */
+    public void testBlockingStepPassAll() throws Exception
+    {
+        LogWriter log = LogWriter.getInstance();
+        EnvUtil.environmentInit();
+
+        //
+        // Create a new transformation...
+        //
+        TransMeta transMeta = new TransMeta();
+        transMeta.setName("blockingsteptest");
+    	
+        StepLoader steploader = StepLoader.getInstance();            
+
+        // 
+        // create an injector step...
+        //
+        String injectorStepname = "injector step";
+        InjectorMeta im = new InjectorMeta();
+        
+        // Set the information of the injector.                
+        String injectorPid = steploader.getStepPluginID(im);
+        StepMeta injectorStep = new StepMeta(injectorPid, injectorStepname, (StepMetaInterface)im);
+        transMeta.addStep(injectorStep);
+
+        // 
+        // Create a dummy step 1
+        //
+        String dummyStepname1 = "dummy step 1";            
+        DummyTransMeta dm1 = new DummyTransMeta();
+
+        String dummyPid1 = steploader.getStepPluginID(dm1);
+        StepMeta dummyStep1 = new StepMeta(dummyPid1, dummyStepname1, (StepMetaInterface)dm1);
+        transMeta.addStep(dummyStep1);                              
+
+        TransHopMeta hi = new TransHopMeta(injectorStep, dummyStep1);
+        transMeta.addTransHop(hi);
+
+        // 
+        // Create a blocking step
+        //
+        String blockingStepname = "blocking step";            
+        BlockingStepMeta bm = new BlockingStepMeta();
+        bm.setPassAllRows(true);
+
+        String blockingStepPid = steploader.getStepPluginID(bm);
+        StepMeta blockingStep = new StepMeta(blockingStepPid, blockingStepname, (StepMetaInterface)bm);
+        transMeta.addStep(blockingStep);            
+
+        TransHopMeta hi2 = new TransHopMeta(dummyStep1, blockingStep);
+        transMeta.addTransHop(hi2);        
+        
+        // 
+        // Create a dummy step 2
+        //
+        String dummyStepname2 = "dummy step 2";            
+        DummyTransMeta dm2 = new DummyTransMeta();
+
+        String dummyPid2 = steploader.getStepPluginID(dm2);
+        StepMeta dummyStep2 = new StepMeta(dummyPid2, dummyStepname2, (StepMetaInterface)dm2);
+        transMeta.addStep(dummyStep2);                              
+
+        TransHopMeta hi3 = new TransHopMeta(blockingStep, dummyStep2);
+        transMeta.addTransHop(hi3);        
+        
+        // Now execute the transformation...
+        Trans trans = new Trans(log, transMeta);
+
+        trans.prepareExecution(null);
+                
+        StepInterface si = trans.getStepInterface(dummyStepname1, 0);
+        RowStepCollector dummyRc1 = new RowStepCollector();
+        si.addRowListener(dummyRc1);
+
+        si = trans.getStepInterface(blockingStepname, 0);
+        RowStepCollector blockingRc = new RowStepCollector();
+        si.addRowListener(blockingRc);
+               
+        si = trans.getStepInterface(dummyStepname2, 0);
+        RowStepCollector dummyRc2 = new RowStepCollector();
+        si.addRowListener(dummyRc2);
+        
+        RowProducer rp = trans.addRowProducer(injectorStepname, 0);
+        trans.startThreads();
+        
+        // add rows
+        List inputList = createData();
+        Iterator it = inputList.iterator();
+        while ( it.hasNext() )
+        {
+        	Row r = (Row)it.next();
+        	rp.putRow(r);
+        }   
+        rp.finished();
+
+        trans.waitUntilFinished();   
+        
+        // The results should be that dummy1 gets all rows.
+        // blocking step should receive all rows (but only send the 
+        // last one through). dummy2 should only get the last row.
+        
+        List resultRows1 = dummyRc1.getRowsRead();
+        checkRows(resultRows1, inputList);
+        
+        List resultRows2 = blockingRc.getRowsRead();
+        checkRows(resultRows2, inputList);
+                
+        List resultRows3 = dummyRc2.getRowsRead();
+        checkRows(resultRows3, inputList);
+    }    
 }
