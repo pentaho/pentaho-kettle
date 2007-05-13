a3319c8075cc3d4a9da98a2b6e89dd2dbf68002f
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-13 16:13:17 +0000

    Purely cosmetics
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3403 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- experimental/org/pentaho/di/core/row/RowDataUtil.java
+++ experimental/org/pentaho/di/core/row/RowDataUtil.java
@@ -4,7 +4,7 @@ package org.pentaho.di.core.row;
  * 
  * We use this class to do row manipulations like add, delete, resize, etc.
  * That way, when we want to go for a metadata driven system with 
- * hiding deletes, oversized arrays etc, we can change these methods to find occurences.
+ * hiding deletes, oversized arrays etc, we can change these methods to find occurrences.
  * 
  * @author Matt
  *
@@ -12,7 +12,7 @@ package org.pentaho.di.core.row;
 public class RowDataUtil
 {
     /**
-     * Resize an object array
+     * Resize an object array making it bigger.
      * 
      * @param objects
      * @param newSize
@@ -54,6 +54,7 @@ public class RowDataUtil
     
     /**
      * Add two arrays and make one new one.
+     * 
      * @param one The first array
      * @param two The second array
      * @return a new Array containing all elements from one and two after one another
@@ -73,7 +74,8 @@ public class RowDataUtil
     }
 
     /**
-     * Remove a number of items in a row of data
+     * Remove a number of items in a row of data.
+     * 
      * @param rowData the row of data to remove from
      * @param index the index of all the items in the source table to remove.  
      *        We don't check if the same index gets deleted twice!
