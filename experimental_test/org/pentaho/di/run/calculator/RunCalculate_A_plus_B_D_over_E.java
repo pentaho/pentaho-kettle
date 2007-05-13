821ac93d2bb0c44e5431ead04809708f5d02ad15
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-13 13:21:46 +0000

    Calculator test cases updates
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3400 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- experimental_test/org/pentaho/di/run/calculator/RunCalculate_A_plus_B_D_over_E.java
+++ experimental_test/org/pentaho/di/run/calculator/RunCalculate_A_plus_B_D_over_E.java
@@ -19,7 +19,7 @@ public class RunCalculate_A_plus_B_D_over_E extends TestCase
     {
         EnvUtil.environmentInit();
         StepLoader.getInstance().read();
-        LogWriter.getInstance(LogWriter.LOG_LEVEL_BASIC);
+        LogWriter.getInstance(LogWriter.LOG_LEVEL_NOTHING);
         
         TransMeta transMeta = new TransMeta("experimental_test/org/pentaho/di/run/calculator/Calculate_A_plus_B_D_over_E.ktr");
         System.out.println("Name of transformation: "+transMeta.getName());
