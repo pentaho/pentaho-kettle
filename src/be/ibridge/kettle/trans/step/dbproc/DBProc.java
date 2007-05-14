fbbdbed9462691e234b0cac6ac375daaa00c9ad1
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-14 08:59:37 +0000

    Backporting fix for bug #5604
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/2.5.0@3409 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/dbproc/DBProc.java
+++ src/be/ibridge/kettle/trans/step/dbproc/DBProc.java
@@ -42,110 +42,132 @@ import be.ibridge.kettle.trans.step.StepMetaInterface;
 
 public class DBProc extends BaseStep implements StepInterface
 {
-	private DBProcMeta meta;
-	private DBProcData data;
-	
-	public DBProc(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
-	{
-		super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
-	}
-	
-	private void runProc(Row row) throws KettleException
-	{
-		int i;
-		Row add;
+    private DBProcMeta meta;
+    private DBProcData data;
+    
+    public DBProc(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
+    {
+        super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+    }
+    
+    private void runProc(Row row) throws KettleException
+    {
+        int i;
+        Row add;
 
         if (first)
-		{
-			first=false;
-			data.argnrs=new int[meta.getArgument().length];
-			data.addnrs=new ArrayList();
-			for (i=0;i<meta.getArgument().length;i++)
-			{
-				if (!meta.getArgumentDirection()[i].equalsIgnoreCase("OUT")) // IN or INOUT //$NON-NLS-1$
-				{
-					data.argnrs[i]=row.searchValueIndex(meta.getArgument()[i]);
-					if (data.argnrs[i]<0)
-					{
-						logError(Messages.getString("DBProc.Log.ErrorFindingField")+meta.getArgument()[i]+"]"); //$NON-NLS-1$ //$NON-NLS-2$
-						throw new KettleStepException(Messages.getString("DBProc.Exception.CouldnotFindField",meta.getArgument()[i])); //$NON-NLS-1$ //$NON-NLS-2$
-					}
-				}
-				else
-				{
-					data.argnrs[i]=-1;
-				}
+        {
+            first=false;
+            data.argnrs=new int[meta.getArgument().length];
+            data.addnrs=new ArrayList();
+            for (i=0;i<meta.getArgument().length;i++)
+            {
+                if (!meta.getArgumentDirection()[i].equalsIgnoreCase("OUT")) // IN or INOUT //$NON-NLS-1$
+                {
+                    data.argnrs[i]=row.searchValueIndex(meta.getArgument()[i]);
+                    if (data.argnrs[i]<0)
+                    {
+                        logError(Messages.getString("DBProc.Log.ErrorFindingField")+meta.getArgument()[i]+"]"); //$NON-NLS-1$ //$NON-NLS-2$
+                        throw new KettleStepException(Messages.getString("DBProc.Exception.CouldnotFindField",meta.getArgument()[i])); //$NON-NLS-1$ //$NON-NLS-2$
+                    }
+                }
+                else
+                {
+                    data.argnrs[i]=-1;
+                }
                 
                 if (meta.getArgumentDirection()[i].equalsIgnoreCase("OUT") || meta.getArgumentDirection()[i].equalsIgnoreCase("INOUT")) // OUT or INOUT //$NON-NLS-1$
                 {
                     data.addnrs.add(new Integer(data.argnrs[i])); // Given the logic above, ONLY the INOUT args have a valid index (meaning >=0)
                 }
-			}
-			data.db.setProcLookup(meta.getProcedure(), meta.getArgument(), meta.getArgumentDirection(), meta.getArgumentType(), 
-			                      meta.getResultName(), meta.getResultType());
-		}
+            }
+            data.db.setProcLookup(meta.getProcedure(), meta.getArgument(), meta.getArgumentDirection(), meta.getArgumentType(), 
+                                  meta.getResultName(), meta.getResultType());
+        }
 
-		data.db.setProcValues(row, data.argnrs, meta.getArgumentDirection(), !Const.isEmpty(meta.getResultName())); 
+        data.db.setProcValues(row, data.argnrs, meta.getArgumentDirection(), !Const.isEmpty(meta.getResultName())); 
 
-		add=data.db.callProcedure(meta.getArgument(), meta.getArgumentDirection(), meta.getArgumentType(), meta.getResultName(), meta.getResultType());
+        add=data.db.callProcedure(meta.getArgument(), meta.getArgumentDirection(), meta.getArgumentType(), meta.getResultName(), meta.getResultType());
         // We are only expecting the OUT and INOUT arguments here.
         // The INOUT values need to replace the value with the same name in the row.
         //
-		for (i=0;i<add.size();i++)
-		{
-            int idx = ((Integer)data.addnrs.get(i)).intValue();
-            if (idx<0)
+        for (i=0;i<add.size();i++)
+        {
+            if (Const.isEmpty(meta.getArgument()))
             {
-                row.addValue( add.getValue(i) ); // new for OUT
+                int idx = ((Integer)data.addnrs.get(i)).intValue();
+                if (idx<0)
+                {
+                    row.addValue( add.getValue(i) ); // new for OUT
+                }
+                else
+                {
+                    row.setValue(idx, add.getValue(i) ); // replace for INOUT
+                }
             }
             else
             {
-                row.setValue(idx, add.getValue(i) ); // replace for INOUT
+                if (i>0)
+                {
+                    int idx = ((Integer)data.addnrs.get(i)).intValue();
+                    if (idx<0)
+                    {
+                        row.addValue( add.getValue(i) ); // new for OUT
+                    }
+                    else
+                    {
+                        row.setValue(idx, add.getValue(i) ); // replace for INOUT
+                    }
+                }
+                else
+                {
+                    row.addValue( add.getValue(i) ); // the function return value
+                }
             }
-		}
-	}
-	
-	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
-	{
-		meta=(DBProcMeta)smi;
-		data=(DBProcData)sdi;
+        }
+    }
+    
+    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
+    {
+        meta=(DBProcMeta)smi;
+        data=(DBProcData)sdi;
 
-		Row r=getRow();       // Get row from input rowset & set row busy!
-		if (r==null)  // no more input to be expected...
-		{
-			setOutputDone();
-			return false;
-		}
-		    
-		try
-		{
-			runProc(r); // add new values to the row in rowset[0].
-			putRow(r);  // copy row to output rowset(s);
-				
+        Row r=getRow();       // Get row from input rowset & set row busy!
+        if (r==null)  // no more input to be expected...
+        {
+            setOutputDone();
+            return false;
+        }
+            
+        try
+        {
+            runProc(r); // add new values to the row in rowset[0].
+            putRow(r);  // copy row to output rowset(s);
+                
             if (checkFeedback(linesRead)) logBasic(Messages.getString("DBProc.LineNumber")+linesRead); //$NON-NLS-1$
-		}
-		catch(KettleException e)
-		{
-			logError(Messages.getString("DBProc.ErrorInStepRunning")+e.getMessage()); //$NON-NLS-1$
-			setErrors(1);
-			stopAll();
-			setOutputDone();  // signal end to receiver(s)
-			return false;
-		}
-			
-		return true;
-	}
-	
-	public boolean init(StepMetaInterface smi, StepDataInterface sdi)
-	{
-		meta=(DBProcMeta)smi;
-		data=(DBProcData)sdi;
+        }
+        catch(KettleException e)
+        {
+            logError(Messages.getString("DBProc.ErrorInStepRunning")+e.getMessage()); //$NON-NLS-1$
+            setErrors(1);
+            stopAll();
+            setOutputDone();  // signal end to receiver(s)
+            return false;
+        }
+            
+        return true;
+    }
+    
+    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta=(DBProcMeta)smi;
+        data=(DBProcData)sdi;
 
-		if (super.init(smi, sdi))
-		{
-			data.db=new Database(meta.getDatabase());
-			try
-			{
+        if (super.init(smi, sdi))
+        {
+            data.db=new Database(meta.getDatabase());
+            try
+            {
                 if (getTransMeta().isUsingUniqueConnections())
                 {
                     synchronized (getTrans()) { data.db.connect(getTrans().getThreadName(), getPartitionID()); }
@@ -160,24 +182,24 @@ public class DBProc extends BaseStep implements StepInterface
                     logBasic(Messages.getString("DBProc.Log.AutoCommit")); //$NON-NLS-1$
                     data.db.setCommit(9999);
                 }
-				logBasic(Messages.getString("DBProc.Log.ConnectedToDB")); //$NON-NLS-1$
-				
-				return true;
-			}
-			catch(KettleException e)
-			{
-				logError(Messages.getString("DBProc.Log.DBException")+e.getMessage()); //$NON-NLS-1$
-				data.db.disconnect();
-			}
-		}
-		return false;
-	}
-		
-	public void dispose(StepMetaInterface smi, StepDataInterface sdi)
-	{
-	    meta = (DBProcMeta)smi;
-	    data = (DBProcData)sdi;
-	    
+                logBasic(Messages.getString("DBProc.Log.ConnectedToDB")); //$NON-NLS-1$
+                
+                return true;
+            }
+            catch(KettleException e)
+            {
+                logError(Messages.getString("DBProc.Log.DBException")+e.getMessage()); //$NON-NLS-1$
+                data.db.disconnect();
+            }
+        }
+        return false;
+    }
+        
+    public void dispose(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta = (DBProcMeta)smi;
+        data = (DBProcData)sdi;
+        
         try
         {
             if (!meta.isAutoCommit())
@@ -189,38 +211,38 @@ public class DBProc extends BaseStep implements StepInterface
         {
             logError(Messages.getString("DBProc.Log.CommitError")+e.getMessage());
         }
-	    data.db.disconnect();
-	    
-	    super.dispose(smi, sdi);
-	}
+        data.db.disconnect();
+        
+        super.dispose(smi, sdi);
+    }
 
-	//
-	// Run is were the action happens!
-	public void run()
-	{
-		logBasic(Messages.getString("DBProc.Log.StartingToRun")); //$NON-NLS-1$
-		
-		try
-		{
-			while (processRow(meta, data) && !isStopped());
-		}
-		catch(Exception e)
-		{
-			logError(Messages.getString("DBProc.Log.UnexpectedError")+" : "+e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+    //
+    // Run is were the action happens!
+    public void run()
+    {
+        logBasic(Messages.getString("DBProc.Log.StartingToRun")); //$NON-NLS-1$
+        
+        try
+        {
+            while (processRow(meta, data) && !isStopped());
+        }
+        catch(Exception e)
+        {
+            logError(Messages.getString("DBProc.Log.UnexpectedError")+" : "+e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
             logError(Const.getStackTracker(e));
             setErrors(1);
-			stopAll();
-		}
-		finally
-		{
-			dispose(meta, data);
-			logSummary();
-			markStop();
-		}
-	}
-	
-	public String toString()
-	{
-		return this.getClass().getName();
-	}
+            stopAll();
+        }
+        finally
+        {
+            dispose(meta, data);
+            logSummary();
+            markStop();
+        }
+    }
+    
+    public String toString()
+    {
+        return this.getClass().getName();
+    }
 }
