a9d65fa8dd071d4ecf0cde418ab2185401fb7273
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-15 19:42:11 +0000

    Add rowgenetor test (conversion from 2.5)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3438 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ experimental_test/org/pentaho/di/trans/steps/rowgenerator/RowGeneratorTest.java
@@ -0,0 +1,206 @@
+/**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+
+package org.pentaho.di.trans.steps.rowgenerator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.pentaho.di.core.RowMetaAndData;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMeta;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.trans.RowStepCollector;
+import org.pentaho.di.trans.StepLoader;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransHopMeta;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.steps.dummytrans.DummyTransMeta;
+
+import be.ibridge.kettle.core.LogWriter;
+import be.ibridge.kettle.core.exception.KettleValueException;
+import be.ibridge.kettle.core.util.EnvUtil;
+
+
+/**
+ * Test class for the RowGenerator step.
+ * 
+ * For the moment only the basic stuff is verified. Formats, lengths, precision
+ * should best also be tested. TODO
+ *
+ * @author Sven Boden
+ */
+public class RowGeneratorTest extends TestCase
+{
+	public RowMetaInterface createRowMetaInterface()
+	{
+		RowMetaInterface rm = new RowMeta();
+		
+		ValueMetaInterface valuesMeta[] = {
+			    new ValueMeta("string",  ValueMeta.TYPE_STRING),
+			    new ValueMeta("boolean", ValueMeta.TYPE_BOOLEAN),
+			    new ValueMeta("integer", ValueMeta.TYPE_INTEGER),
+	    };
+
+		for (int i=0; i < valuesMeta.length; i++ )
+		{
+			rm.addValueMeta(valuesMeta[i]);
+		}
+		
+		return rm;
+	}
+	
+	public List createData()
+	{
+		List list = new ArrayList();	
+		
+		RowMetaInterface rm = createRowMetaInterface();
+		
+		Object[] r1 = new Object[] { "string_value", new Boolean(true), 
+				                     new Long(20L)};
+		
+		list.add(new RowMetaAndData(rm, r1));
+		list.add(new RowMetaAndData(rm, r1));
+		list.add(new RowMetaAndData(rm, r1));
+		
+		return list;					
+	}
+
+	/**
+	 *  Check the 2 lists comparing the rows in order.
+	 *  If they are not the same fail the test. 
+	 */
+    public void checkRows(List rows1, List rows2)
+    {
+    	int idx = 1;
+        if ( rows1.size() != rows2.size() )
+        {
+        	fail("Number of rows is not the same: " + 
+          		 rows1.size() + " and " + rows2.size());
+        }
+        Iterator it1 = rows1.iterator();
+        Iterator it2 = rows2.iterator();
+        
+        while ( it1.hasNext() && it2.hasNext() )
+        {
+        	RowMetaAndData rm1 = (RowMetaAndData)it1.next();
+        	RowMetaAndData rm2 = (RowMetaAndData)it2.next();
+        	
+        	Object[] r1 = rm1.getData();
+        	Object[] r2 = rm2.getData();
+        	
+        	if ( r1.length != r2.length )
+        	{
+        		fail("row nr " + idx + "is not equal");
+        	}
+        	int fields[] = new int[r1.length];
+        	for ( int ydx = 0; ydx < r1.length; ydx++ )
+        	{
+        		fields[ydx] = ydx;
+        	}
+            try {
+				if ( rm1.getRowMeta().compare(r1, r2, fields) != 0 )
+				{
+					fail("row nr " + idx + "is not equal");
+				}
+			} catch (KettleValueException e) {
+				fail("row nr " + idx + "is not equal");
+			}
+            	
+            idx++;
+        }
+    }
+	
+   	
+	/**
+	 * Test case for Row Generator step.
+	 */
+    public void testRowGenerator() throws Exception
+    {
+        LogWriter log = LogWriter.getInstance();
+        EnvUtil.environmentInit();
+
+        //
+        // Create a new transformation...
+        //
+        TransMeta transMeta = new TransMeta();
+        transMeta.setName("row generatortest");
+    	
+        StepLoader steploader = StepLoader.getInstance();            
+
+        // 
+        // create a row generator step...
+        //
+        String rowGeneratorStepname = "row generator step";
+        RowGeneratorMeta rm = new RowGeneratorMeta();
+        
+        // Set the information of the row generator.                
+        String rowGeneratorPid = steploader.getStepPluginID(rm);
+        StepMeta rowGeneratorStep = new StepMeta(rowGeneratorPid, rowGeneratorStepname, (StepMetaInterface)rm);
+        transMeta.addStep(rowGeneratorStep);
+        
+        //
+        // Do the following specs 3 times.
+        //
+        String fieldName[] = { "string", "boolean", "integer" };
+        String type[]      = { "String", "Boolean", "Integer" };
+        String value[]     = { "string_value", "true", "20"   };
+        int    intDummies[] =  { -1, -1, -1 };
+                
+        rm.setDefault();
+        rm.setFieldName(fieldName);
+        rm.setFieldType(type);
+        rm.setValue(value);
+        rm.setFieldLength(intDummies);
+        rm.setFieldPrecision(intDummies);        
+        rm.setRowLimit("3");
+
+        // 
+        // Create a dummy step
+        //
+        String dummyStepname = "dummy step";            
+        DummyTransMeta dm = new DummyTransMeta();
+
+        String dummyPid = steploader.getStepPluginID(dm);
+        StepMeta dummyStep = new StepMeta(dummyPid, dummyStepname, (StepMetaInterface)dm);
+        transMeta.addStep(dummyStep);                              
+
+        TransHopMeta hi = new TransHopMeta(rowGeneratorStep, dummyStep);
+        transMeta.addTransHop(hi);
+                
+        // Now execute the transformation...
+        Trans trans = new Trans(log, transMeta);
+
+        trans.prepareExecution(null);
+                
+        StepInterface si = trans.getStepInterface(dummyStepname, 0);
+        RowStepCollector rc = new RowStepCollector();
+        si.addRowListener(rc);
+        
+        trans.startThreads();        
+        trans.waitUntilFinished();   
+        
+        List checkList = createData();
+        List resultRows = rc.getRowsRead();
+        checkRows(resultRows, checkList);
+    }
+}
