ddf759ce702291413578a742444cc779e4d3426f
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-16 08:56:07 +0000

    More text file output tests
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3463 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- experimental_test/org/pentaho/di/run/textfileoutput/RunTextFileInputCSV.java
+++ experimental_test/org/pentaho/di/run/textfileoutput/RunTextFileInputCSV.java
@@ -10,10 +10,50 @@ import be.ibridge.kettle.core.exception.KettleXMLException;
 
 public class RunTextFileInputCSV extends TestCase
 {
-    public void testRunTextFileInputCSV() throws KettleXMLException
+    public void testTextFileInputOutputCSV() throws KettleXMLException
     {
         TimedTransRunner timedTransRunner = new TimedTransRunner(
                 "experimental_test/org/pentaho/di/run/textfileoutput/TextFileInputOutputCSV.ktr", 
+                LogWriter.LOG_LEVEL_ERROR, 
+                10000
+            );
+        
+        timedTransRunner.init();
+        timedTransRunner.runOldEngine();
+        timedTransRunner.runNewEngine();
+        timedTransRunner.compareResults();
+        
+        Result oldResult = timedTransRunner.getOldResult();
+        assertTrue(oldResult.getNrErrors()==0);
+        
+        Result newResult = timedTransRunner.getNewResult();
+        assertTrue(newResult.getNrErrors()==0);
+    }
+    
+    public void testRunTextFileOutputFromTable() throws KettleXMLException
+    {
+        TimedTransRunner timedTransRunner = new TimedTransRunner(
+                "experimental_test/org/pentaho/di/run/textfileoutput/TextFileOutputFromTable.ktr", 
+                LogWriter.LOG_LEVEL_ERROR, 
+                10000
+            );
+        
+        timedTransRunner.init();
+        timedTransRunner.runOldEngine();
+        timedTransRunner.runNewEngine();
+        timedTransRunner.compareResults();
+        
+        Result oldResult = timedTransRunner.getOldResult();
+        assertTrue(oldResult.getNrErrors()==0);
+        
+        Result newResult = timedTransRunner.getNewResult();
+        assertTrue(newResult.getNrErrors()==0);
+    }
+    
+    public void testRunTextFileOutputFromGenerator() throws KettleXMLException
+    {
+        TimedTransRunner timedTransRunner = new TimedTransRunner(
+                "experimental_test/org/pentaho/di/run/textfileoutput/TextFileOutputFromGenerator.ktr", 
                 LogWriter.LOG_LEVEL_ERROR, 
                 100000
             );
