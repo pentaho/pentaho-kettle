df4e33ab99088e04279a79e7f3f289461a6d89da
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-19 20:55:27 +0000

    Add unit testcases
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3496 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- experimental_test/org/pentaho/di/AllRegressionTests.java
+++ experimental_test/org/pentaho/di/AllRegressionTests.java
@@ -8,6 +8,7 @@ import org.pentaho.di.core.RowSetTest;
 import org.pentaho.di.core.database.DatabaseTest;
 import org.pentaho.di.core.row.RowDataUtilTest;
 import org.pentaho.di.core.row.RowTest;
+import org.pentaho.di.core.row.ValueDataUtilTest;
 import org.pentaho.di.trans.HopTest;
 import org.pentaho.di.trans.steps.blockingstep.BlockingStepTest;
 import org.pentaho.di.trans.steps.injector.InjectorTest;
@@ -22,14 +23,15 @@ public class AllRegressionTests
 {   
     public static Test suite() throws Exception
     {
-        TestSuite suite = new TestSuite("Run performance tests");
+        TestSuite suite = new TestSuite("Run regression tests");
 
         // Suggestion is to put the tests going from easy to hard functionality.
         // If the easy functionality doesn't work, chances are big the harder
         // functionality will also not work.
         suite.addTestSuite(RowSetTest.class);
         suite.addTestSuite(RowTest.class);
-        suite.addTestSuite(RowDataUtilTest.class);        
+        suite.addTestSuite(RowDataUtilTest.class);
+        suite.addTestSuite(ValueDataUtilTest.class);  
         suite.addTestSuite(DatabaseTest.class);
         suite.addTestSuite(HopTest.class);
         
---
+++ experimental_test/org/pentaho/di/core/row/ValueDataUtilTest.java
@@ -0,0 +1,87 @@
+package org.pentaho.di.core.row;
+
+
+import junit.framework.TestCase;
+
+import be.ibridge.kettle.core.exception.KettleValueException;
+
+/**
+ * Not yet completely finished.
+ * 
+ * @author sboden
+ */
+public class ValueDataUtilTest extends TestCase
+{
+    public void testLeftTrim() throws KettleValueException
+    {
+    	assertEquals("", ValueDataUtil.leftTrim(""));
+    	assertEquals("string", ValueDataUtil.leftTrim("string"));
+    	assertEquals("string", ValueDataUtil.leftTrim(" string"));
+    	assertEquals("string", ValueDataUtil.leftTrim("  string"));
+    	assertEquals("string", ValueDataUtil.leftTrim("   string"));
+    	assertEquals("string", ValueDataUtil.leftTrim("     string"));
+    	
+    	assertEquals("string ", ValueDataUtil.leftTrim(" string "));
+    	assertEquals("string  ", ValueDataUtil.leftTrim("  string  "));
+    	assertEquals("string   ", ValueDataUtil.leftTrim("   string   "));
+    	assertEquals("string    ", ValueDataUtil.leftTrim("    string    "));
+    	
+    	assertEquals("", ValueDataUtil.leftTrim(" "));
+    	assertEquals("", ValueDataUtil.leftTrim("  "));
+    	assertEquals("", ValueDataUtil.leftTrim("   "));    	
+    }
+
+    public void testRightTrim() throws KettleValueException
+    {
+    	assertEquals("", ValueDataUtil.rightTrim(""));
+    	assertEquals("string", ValueDataUtil.rightTrim("string"));
+    	assertEquals("string", ValueDataUtil.rightTrim("string "));
+    	assertEquals("string", ValueDataUtil.rightTrim("string  "));
+    	assertEquals("string", ValueDataUtil.rightTrim("string   "));
+    	assertEquals("string", ValueDataUtil.rightTrim("string    "));
+    	
+    	assertEquals(" string", ValueDataUtil.rightTrim(" string "));
+    	assertEquals("  string", ValueDataUtil.rightTrim("  string  "));
+    	assertEquals("   string", ValueDataUtil.rightTrim("   string   "));
+    	assertEquals("    string", ValueDataUtil.rightTrim("    string    "));
+
+    	assertEquals("", ValueDataUtil.rightTrim(" "));
+    	assertEquals("", ValueDataUtil.rightTrim("  "));
+    	assertEquals("", ValueDataUtil.rightTrim("   "));    	    	
+    }
+
+    public void testIsSpace() throws KettleValueException
+    {
+    	assertTrue(ValueDataUtil.isSpace(' '));
+    	assertTrue(ValueDataUtil.isSpace('\t'));
+    	assertTrue(ValueDataUtil.isSpace('\r'));
+    	assertTrue(ValueDataUtil.isSpace('\n'));
+    	
+    	assertFalse(ValueDataUtil.isSpace('S'));
+    	assertFalse(ValueDataUtil.isSpace('b'));
+    }
+    
+    public void testTrim() throws KettleValueException
+    {
+    	assertEquals("", ValueDataUtil.trim(""));
+    	assertEquals("string", ValueDataUtil.trim("string"));
+    	assertEquals("string", ValueDataUtil.trim("string "));
+    	assertEquals("string", ValueDataUtil.trim("string  "));
+    	assertEquals("string", ValueDataUtil.trim("string   "));
+    	assertEquals("string", ValueDataUtil.trim("string    "));
+    	
+    	assertEquals("string", ValueDataUtil.trim(" string "));
+    	assertEquals("string", ValueDataUtil.trim("  string  "));
+    	assertEquals("string", ValueDataUtil.trim("   string   "));
+    	assertEquals("string", ValueDataUtil.trim("    string    "));
+
+    	assertEquals("string", ValueDataUtil.trim(" string"));
+    	assertEquals("string", ValueDataUtil.trim("  string"));
+    	assertEquals("string", ValueDataUtil.trim("   string"));
+    	assertEquals("string", ValueDataUtil.trim("    string"));    	
+    	
+    	assertEquals("", ValueDataUtil.rightTrim(" "));
+    	assertEquals("", ValueDataUtil.rightTrim("  "));
+    	assertEquals("", ValueDataUtil.rightTrim("   "));    	    	
+    }    
+}
