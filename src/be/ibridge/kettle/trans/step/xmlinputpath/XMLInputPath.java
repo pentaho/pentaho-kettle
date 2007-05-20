21a40f364d66b72e48e172f1a540f4ca5330e70a
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-05-20 22:57:56 +0000

    Code clean
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3502 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/xmlinputpath/XMLInputPath.java
+++ src/be/ibridge/kettle/trans/step/xmlinputpath/XMLInputPath.java
@@ -54,18 +54,18 @@ public class XMLInputPath extends BaseStep implements StepInterface
 	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
 	{
 		data.rownr=0;
-	     for (int i=0;i<data.files.size();i++)
-	     {
+		for (int i=0;i<data.files.size();i++)
+		{
 	    	 	
 			
 			if ((meta.getRowLimit()>0 &&  data.rownr<meta.getRowLimit()) || meta.getRowLimit()==0) 
-	    	{
+			{
 		
-		    	data.file = (FileObject) data.files.get(i);
+				data.file = (FileObject) data.files.get(i);
 		    	
-		    	logBasic("-----------------------------------");
+				logBasic("-----------------------------------");
 		    	
-		    	logBasic(Messages.getString("XMLInputPath.Log.OpeningFile", data.file.toString()));
+				logBasic(Messages.getString("XMLInputPath.Log.OpeningFile", data.file.toString()));
 		    	
 				// Fetch files and process each one
 				Processfile(data.file);	
@@ -74,20 +74,20 @@ public class XMLInputPath extends BaseStep implements StepInterface
 				if (log.isDetailed()) logDetailed(Messages.getString("XMLInputPath.Log.FileOpened", data.file.toString()));
 		        		
 	
-	    	}
+			}
 	    	
 			//	Add this to the result file names...
 			ResultFile resultFile = new ResultFile(ResultFile.FILE_TYPE_GENERAL, data.file, getTransMeta().getName(), getStepname());
 			resultFile.setComment(Messages.getString("XMLInputPath.Log.FileAddedResult"));
 			addResultFile(resultFile);
 	    	
-	    	// 	 Move file pointer ahead!
+			// 	 Move file pointer ahead!
 			data.filenr++;	
 			
-	     }
+		}
 		
 		setOutputDone();  // signal end to receiver(s)
-	    return false;     // This is the end of this step.
+		return false;     // This is the end of this step.
    
 	}
 		
@@ -125,201 +125,201 @@ public class XMLInputPath extends BaseStep implements StepInterface
 			setErrors(1);		
 		}
 			
-        return valueNode;
+		return valueNode;
 	}
 	
 	private void Processfile(FileObject file)
 	{
 		
-        try 
-        {
-        	// get encoding. By default UTF-8
-        	 String encodage="UTF-8";
-        	 if (!Const.isEmpty(meta.getEncoding()))
-             {
-        		 encodage=meta.getEncoding();
-             }
-	        javax.xml.parsers.DocumentBuilder builder = javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	        org.w3c.dom.Document document = builder.parse(new org.xml.sax.InputSource(new java.io.InputStreamReader(new java.io.FileInputStream(KettleVFS.getFilename(file)), encodage)));        
+		try 
+		{
+			// get encoding. By default UTF-8
+			String encodage="UTF-8";
+			if (!Const.isEmpty(meta.getEncoding()))
+			{
+				encodage=meta.getEncoding();
+			}
+			javax.xml.parsers.DocumentBuilder builder = javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			org.w3c.dom.Document document = builder.parse(new org.xml.sax.InputSource(new java.io.InputStreamReader(new java.io.FileInputStream(KettleVFS.getFilename(file)), encodage)));        
 	    	
-	        javax.xml.xpath.XPath xpath = javax.xml.xpath.XPathFactory.newInstance().newXPath();
-	        NodeList widgetNodes = (NodeList) xpath.evaluate(meta.getRealLoopXPath(), document,javax.xml.xpath.XPathConstants.NODESET);
+			javax.xml.xpath.XPath xpath = javax.xml.xpath.XPathFactory.newInstance().newXPath();
+			NodeList widgetNodes = (NodeList) xpath.evaluate(meta.getRealLoopXPath(), document,javax.xml.xpath.XPathConstants.NODESET);
 	        
-	        if (log.isDetailed()) logDetailed(widgetNodes.getLength() + Messages.getString("XMLInputPath.Log.LoopOccurences1") + 
-	        		KettleVFS.getFilename(file) + " " + Messages.getString("XMLInputPath.Log.LoopOccurences2") );
+			if (log.isDetailed()) logDetailed(widgetNodes.getLength() + Messages.getString("XMLInputPath.Log.LoopOccurences1") + 
+									  KettleVFS.getFilename(file) + Messages.getString("XMLInputPath.Log.LoopOccurences2") );
 			
 	        
 	        
 			for (int itFileInputXML_1 = 0; itFileInputXML_1 < widgetNodes.getLength(); itFileInputXML_1++) 
 			{
 				if ((meta.getRowLimit()>0 && data.rownr<meta.getRowLimit()) || meta.getRowLimit()==0)  
-		    	{
+				{
 					// Create new row
 					Row row = buildEmptyRow();
 					
-					  // Read from the Node...
-			        for (int i=0;i<meta.getInputFields().length;i++)
-			        {
-			        	XMLInputPathField XMLInputPathField = meta.getInputFields()[i];
-			        	// Get the Path to look for
-			        	String XPathValue = XMLInputPathField.getRealXPath();
-			        	String Element_Type = XMLInputPathField.getElementTypeCode();
+					// Read from the Node...
+					for (int i=0;i<meta.getInputFields().length;i++)
+					{
+						XMLInputPathField xmlInputField = meta.getInputFields()[i];
+						// Get the Path to look for
+						String XPathValue = xmlInputField.getRealXPath();
+						String Element_Type = xmlInputField.getElementTypeCode();
 			        
 			        	
-			        	String valueNode = getValueXML(widgetNodes,itFileInputXML_1,xpath, XPathValue,Element_Type);        	 
+						String valueNode = getValueXML(widgetNodes,itFileInputXML_1,xpath, XPathValue,Element_Type);        	 
 			        	
 			        	
 						// OK, we have the string...
-			            Value v = row.getValue(i);
-			 			v.setValue(valueNode);
+						Value v = row.getValue(i);
+						v.setValue(valueNode);
 			 			
-			 			 // DO Trimming!
-			            switch(XMLInputPathField.getTrimType())
-			            {
-			            case XMLInputPathField.TYPE_TRIM_LEFT  : v.ltrim(); break;
-			            case XMLInputPathField.TYPE_TRIM_RIGHT : v.rtrim(); break;
-			            case XMLInputPathField.TYPE_TRIM_BOTH  : v.trim(); break;
-			            default: break;
-			            }
+						// DO Trimming!
+						switch(xmlInputField.getTrimType())
+						{
+							case XMLInputPathField.TYPE_TRIM_LEFT  : v.ltrim(); break;
+							case XMLInputPathField.TYPE_TRIM_RIGHT : v.rtrim(); break;
+							case XMLInputPathField.TYPE_TRIM_BOTH  : v.trim(); break;
+							default: break;
+						}
 			      	            
 			            
-			            // DO CONVERSIONS...
-			            switch(XMLInputPathField.getType())
-			            {
-			            case Value.VALUE_TYPE_STRING:
-			                break;
-			            case Value.VALUE_TYPE_NUMBER:
-			                // System.out.println("Convert value to Number :"+v);
-			                if (XMLInputPathField.getFormat()!=null && XMLInputPathField.getFormat().length()>0)
-			                {
-			                    if (XMLInputPathField.getDecimalSymbol()!=null && XMLInputPathField.getDecimalSymbol().length()>0)
-			                    {
-			                        if (XMLInputPathField.getGroupSymbol()!=null && XMLInputPathField.getGroupSymbol().length()>0)
-			                        {
-			                            if (XMLInputPathField.getCurrencySymbol()!=null && XMLInputPathField.getCurrencySymbol().length()>0)
-			                            {
-			                                v.str2num(XMLInputPathField.getFormat(), XMLInputPathField.getDecimalSymbol(), XMLInputPathField.getGroupSymbol(), XMLInputPathField.getCurrencySymbol());
-			                            }
-			                            else
-			                            {
-			                                v.str2num(XMLInputPathField.getFormat(), XMLInputPathField.getDecimalSymbol(), XMLInputPathField.getGroupSymbol());
-			                            }
-			                        }
-			                        else
-			                        {
-			                            v.str2num(XMLInputPathField.getFormat(), XMLInputPathField.getDecimalSymbol());
-			                        }
-			                    }
-			                    else
-			                    {
-			                        v.str2num(XMLInputPathField.getFormat()); // just a format mask
-			                   }
-			                }
-			                else
-			                {
-			                    v.str2num();
-			                }
-			                v.setLength(XMLInputPathField.getLength(), XMLInputPathField.getPrecision());
-			                break;
-			            case Value.VALUE_TYPE_INTEGER:
-			                // System.out.println("Convert value to integer :"+v);
-			                v.setValue(v.getInteger());
-			                v.setLength(XMLInputPathField.getLength(), XMLInputPathField.getPrecision());
-			                break;
-			            case Value.VALUE_TYPE_BIGNUMBER:
-			                // System.out.println("Convert value to BigNumber :"+v);
-			                v.setValue(v.getBigNumber());
-			                v.setLength(XMLInputPathField.getLength(), XMLInputPathField.getPrecision());
-			                break;
-			            case Value.VALUE_TYPE_DATE:
-			                // System.out.println("Convert value to Date :"+v);
+						// DO CONVERSIONS...
+						switch(xmlInputField.getType())
+						{
+							case Value.VALUE_TYPE_STRING:
+								break;
+							case Value.VALUE_TYPE_NUMBER:
+								// System.out.println("Convert value to Number :"+v);
+								if (xmlInputField.getFormat()!=null && xmlInputField.getFormat().length()>0)
+								{
+									if (xmlInputField.getDecimalSymbol()!=null && xmlInputField.getDecimalSymbol().length()>0)
+									{
+										if (xmlInputField.getGroupSymbol()!=null && xmlInputField.getGroupSymbol().length()>0)
+										{
+											if (xmlInputField.getCurrencySymbol()!=null && xmlInputField.getCurrencySymbol().length()>0)
+											{
+												v.str2num(xmlInputField.getFormat(), xmlInputField.getDecimalSymbol(), xmlInputField.getGroupSymbol(), xmlInputField.getCurrencySymbol());
+											}
+											else
+											{
+												v.str2num(xmlInputField.getFormat(), xmlInputField.getDecimalSymbol(), xmlInputField.getGroupSymbol());
+											}
+										}
+										else
+										{
+											v.str2num(xmlInputField.getFormat(), xmlInputField.getDecimalSymbol());
+										}
+									}
+									else
+									{
+										v.str2num(xmlInputField.getFormat()); // just a format mask
+									}
+								}
+								else
+								{
+									v.str2num();
+								}
+								v.setLength(xmlInputField.getLength(), xmlInputField.getPrecision());
+								break;
+							case Value.VALUE_TYPE_INTEGER:
+								// System.out.println("Convert value to integer :"+v);
+								v.setValue(v.getInteger());
+								v.setLength(xmlInputField.getLength(), xmlInputField.getPrecision());
+								break;
+							case Value.VALUE_TYPE_BIGNUMBER:
+								// System.out.println("Convert value to BigNumber :"+v);
+								v.setValue(v.getBigNumber());
+								v.setLength(xmlInputField.getLength(), xmlInputField.getPrecision());
+								break;
+							case Value.VALUE_TYPE_DATE:
+								// System.out.println("Convert value to Date :"+v);
 	
-			                if (XMLInputPathField.getFormat()!=null && XMLInputPathField.getFormat().length()>0)
-			                {
-			                    v.str2dat(XMLInputPathField.getFormat());
-			                }
-			                else
-			                {
-			                    v.setValue(v.getDate());
-			                }
-			                break;
-			            case Value.VALUE_TYPE_BOOLEAN:
-			                v.setValue(v.getBoolean());
-			                break;
-			            default: break;
-			            }
+								if (xmlInputField.getFormat()!=null && xmlInputField.getFormat().length()>0)
+								{
+									v.str2dat(xmlInputField.getFormat());
+								}
+								else
+								{
+									v.setValue(v.getDate());
+								}
+								break;
+							case Value.VALUE_TYPE_BOOLEAN:
+								v.setValue(v.getBoolean());
+								break;
+							default: break;
+						}
 			           	            
-			            // Do we need to repeat this field if it is null?
-			            if (XMLInputPathField.isRepeated())
-			            {
-			                if (v.isNull() && data.previousRow!=null)
-			                {
-			                    Value previous = data.previousRow.getValue(i);
-			                    v.setValue(previous);
-			                }
-			            }
+						// Do we need to repeat this field if it is null?
+						if (xmlInputField.isRepeated())
+						{
+							if (v.isNull() && data.previousRow!=null)
+							{
+								Value previous = data.previousRow.getValue(i);
+								v.setValue(previous);
+							}
+						}
 			      
-			        } // End of loop over fields...
+					} // End of loop over fields...
 					   	
 			        
-			        // See if we need to add the filename to the row...  
-			        if (meta.includeFilename() && meta.getFilenameField()!=null && meta.getFilenameField().length()>0)
-			        {
-			            Value fn = new Value( meta.getRealFilenameField(), KettleVFS.getFilename(file));
-			            row.addValue(fn);
+					// See if we need to add the filename to the row...  
+					if (meta.includeFilename() && meta.getFilenameField()!=null && meta.getFilenameField().length()>0)
+					{
+						Value fn = new Value( meta.getRealFilenameField(), KettleVFS.getFilename(file));
+						row.addValue(fn);
 			            
-			        }
+					}
 			        
-			        // See if we need to add the row number to the row...  
-			        if (meta.includeRowNumber() && meta.getRowNumberField()!=null && meta.getRowNumberField().length()>0)
-			        {
-			            Value fn = new Value( meta.getRealRowNumberField(), data.rownr );
-			            row.addValue(fn);
-			        }
+					// See if we need to add the row number to the row...  
+					if (meta.includeRowNumber() && meta.getRowNumberField()!=null && meta.getRowNumberField().length()>0)
+					{
+						Value fn = new Value( meta.getRealRowNumberField(), data.rownr );
+						row.addValue(fn);
+					}
 			        
-			        data.previousRow = new Row(row); // copy it to make sure the next step doesn't change it in between... 
-			        data.rownr++;
+					data.previousRow = new Row(row); // copy it to make sure the next step doesn't change it in between... 
+					data.rownr++;
 			        
 		    		
-		    		if (log.isRowLevel()) logRowlevel(Messages.getString("XMLInputPath.Log.ReadRow", row.toString()));        
+					if (log.isRowLevel()) logRowlevel(Messages.getString("XMLInputPath.Log.ReadRow", row.toString()));        
 		            
-		            putRow(row);
-		    	}
+					putRow(row);
+				}
 			}        
-	        } 
-	     	catch(Exception e)
-			{
-				logError(Messages.getString("XMLInputPath.Log.UnableToOpenFile", ""+data.filenr, data.file.toString(), e.toString()));
-				stopAll();
-				setErrors(1);
-			}    
+		} 
+		catch(Exception e)
+		{
+			logError(Messages.getString("XMLInputPath.Log.UnableToOpenFile", ""+data.filenr, data.file.toString(), e.toString()));
+			stopAll();
+			setErrors(1);
+		}    
     		
 		
 	}
 	
 
-    /**
-     * Build an empty row based on the meta-data...
-     * @return
-     */
-    private Row buildEmptyRow()
-    {
-        Row row = new Row();
+	/**
+	 * Build an empty row based on the meta-data...
+	 * @return
+	 */
+	private Row buildEmptyRow()
+	{
+		Row row = new Row();
         
-        XMLInputPathField fields[] = meta.getInputFields();
-        for (int i=0;i<fields.length;i++)
-        {
-            XMLInputPathField field = fields[i];
+		XMLInputPathField fields[] = meta.getInputFields();
+		for (int i=0;i<fields.length;i++)
+		{
+			XMLInputPathField field = fields[i];
             
-            Value value = new Value(StringUtil.environmentSubstitute(field.getName()), field.getType());
-            value.setLength(field.getLength(), field.getPrecision());
-            value.setNull();
+			Value value = new Value(StringUtil.environmentSubstitute(field.getName()), field.getType());
+			value.setLength(field.getLength(), field.getPrecision());
+			value.setNull();
             
-            row.addValue(value);
-        }
+			row.addValue(value);
+		}
         
-        return row;
-    }
+		return row;
+	}
 
 	
 	public boolean init(StepMetaInterface smi, StepDataInterface sdi)
@@ -336,9 +336,9 @@ public class XMLInputPath extends BaseStep implements StepInterface
 				return false;
 			}
             
-            data.rownr = 1L;
+			data.rownr = 1L;
 			
-		    return true;
+			return true;
 		}
 		return false;
 	}
@@ -359,15 +359,15 @@ public class XMLInputPath extends BaseStep implements StepInterface
 	{			    
 		try
 		{
-			logBasic(Messages.getString("XMLInputPath.Log.StartingRun"));		
+			logBasic(Messages.getString("XMLInput.Log.StartingRun"));		
 			
 			while (processRow(meta, data) && !isStopped());
 		}
 		catch(Exception e)
 		{
 			logError("Unexpected error : "+e.toString());
-            logError(Const.getStackTracker(e));
-            setErrors(1);
+			logError(Const.getStackTracker(e));
+			setErrors(1);
 			stopAll();
 		}
 		finally
