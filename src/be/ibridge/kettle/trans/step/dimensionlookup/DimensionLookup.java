705caff4f59219c297850030f3e2da82dda41b1b
Brassrat <Brassrat@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-14 17:02:06 +0000

    check for incorrect field name to prevent later NPE
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3623 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/dimensionlookup/DimensionLookup.java
+++ src/be/ibridge/kettle/trans/step/dimensionlookup/DimensionLookup.java
@@ -137,7 +137,11 @@ public class DimensionLookup extends BaseStep implements StepInterface
 				data.fieldnrs = new int[meta.getFieldStream().length];
 				for (int i=0;meta.getFieldStream()!=null && i<meta.getFieldStream().length;i++)
 				{
-					data.fieldnrs[i]=row.searchValueIndex(meta.getFieldStream()[i]);
+          data.fieldnrs[i]=row.searchValueIndex(meta.getFieldStream()[i]);
+				  if (data.fieldnrs[i]<0) // couldn't find field!
+          {
+            throw new KettleStepException(Messages.getString("DimensionLookup.Exception.KeyFieldNotFound", meta.getFieldStream()[i])); //$NON-NLS-1$ //$NON-NLS-2$
+          }
 				}
 			}
 
@@ -366,7 +370,7 @@ public class DimensionLookup extends BaseStep implements StepInterface
                             punch=true;
                       }
 					  
-					  logRowlevel(Messages.getString("DimensionLookup.Log.ComparingValues",""+v1,""+v2,String.valueOf(cmp),String.valueOf(identical),String.valueOf(insert),String.valueOf(punch))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+					  if (log.isRowLevel()) logRowlevel(Messages.getString("DimensionLookup.Log.ComparingValues",""+v1,""+v2,String.valueOf(cmp),String.valueOf(identical),String.valueOf(insert),String.valueOf(punch))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 				}
 				
 				if (!insert)  // Just an update of row at key = valueKey
