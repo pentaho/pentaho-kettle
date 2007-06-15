de4db5dcd5a66486d2b37593b9f774879b8d7d74
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-15 19:11:04 +0000

    Fix for #5771 : throw exception in getRowsFrom() when hop name is not found. More specific exception.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/2.5.0@3630 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/BaseStep.java
+++ src/be/ibridge/kettle/trans/step/BaseStep.java
@@ -1244,12 +1244,12 @@ public class BaseStep extends Thread
      * This version of getRow() only takes data from certain rowsets We select these rowsets that have name = step
      * Otherwise it's the same as the other one.
      */
-    public synchronized Row getRowFrom(String from)
+    public synchronized Row getRowFrom(String from) throws KettleRowException
     {
         output_rowset_nr = findInputRowSetNumber(from, 0, stepname, 0);
         if (output_rowset_nr < 0) // No rowset found: normally it can't happen: we deleted the rowset because it was
                                     // finished
-        { return null; }
+        { throw new KettleRowException("Couldn't find hop '" + from + "' to read from"); }
 
         return getRowFrom(output_rowset_nr);
     }
