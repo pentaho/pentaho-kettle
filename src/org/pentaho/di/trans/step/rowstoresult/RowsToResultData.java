5457c8504d24237f037b5cf4895eb05b10e0b96b
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-19 17:41:45 +0000

    Modified classes to comply with new 3.0 architecture
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3669 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/trans/step/rowstoresult/RowsToResultData.java
@@ -0,0 +1,45 @@
+ /**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+
+package org.pentaho.di.trans.step.rowstoresult;
+
+import java.util.ArrayList;
+
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+
+
+/**
+ * @author Matt
+ * @since 24-jan-2005
+ */
+public class RowsToResultData extends BaseStepData implements StepDataInterface 
+{
+	public ArrayList rows;
+	public RowMetaInterface outputRowMeta;
+	/**
+	 * 
+	 */
+	public RowsToResultData()
+	{
+		super();
+		
+		rows = new ArrayList();
+	}
+
+}
