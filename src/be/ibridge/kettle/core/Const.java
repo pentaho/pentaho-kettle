364fc329bf27c26a8cb719690924cb761828552b
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-20 23:54:12 +0000

    2.5.1 branch is now opened
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/2.5.1@3683 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/core/Const.java
+++ src/be/ibridge/kettle/core/Const.java
@@ -59,7 +59,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "2.5.0";
+	public static final String VERSION = "2.5.1";
 
 	/**
 	 * Sleep time waiting when buffer is empty
