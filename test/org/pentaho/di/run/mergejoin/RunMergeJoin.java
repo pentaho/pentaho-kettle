d4fdd9af86cd9ccc6c3cbfd038024596ce5ac9a2
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-22 21:23:21 +0000

    MergeJoin test
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3733 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ test/org/pentaho/di/run/mergejoin/RunMergeJoin.java
@@ -0,0 +1,36 @@
+package org.pentaho.di.run.mergejoin;
+
+import junit.framework.TestCase;
+
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.logging.LogWriter;
+import org.pentaho.di.run.AllRunTests;
+import org.pentaho.di.run.TimedTransRunner;
+
+public class RunMergeJoin extends TestCase
+{
+    public void test_MERGE_JOIN_00()
+    {
+        System.out.println();
+        System.out.println("MERGE JOIN");
+        System.out.println("==================");
+    }
+    
+    public void test_MERGE_JOIN_01_NoCondition() throws Exception
+    {
+        TimedTransRunner timedTransRunner = new TimedTransRunner(
+                "test/org/pentaho/di/run/mergejoin/MergeJoin.ktr", 
+                LogWriter.LOG_LEVEL_ERROR, 
+                AllRunTests.getOldTargetDatabase(),
+                AllRunTests.getNewTargetDatabase(),
+                1000
+            );
+        timedTransRunner.runOldAndNew();
+        
+        be.ibridge.kettle.core.Result oldResult = timedTransRunner.getOldResult();
+        assertTrue(oldResult.getNrErrors()==0);
+        
+        Result newResult = timedTransRunner.getNewResult();
+        assertTrue(newResult.getNrErrors()==0);
+    }
+}
