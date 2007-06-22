bd4a61be5df6b7ad8ed95116824ba98ce29c9552
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-22 21:21:29 +0000

    MergeJoin goes to 3.0
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3731 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/trans/steps/mergejoin/MergeJoinData.java
@@ -0,0 +1,61 @@
+ /**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+
+package org.pentaho.di.trans.steps.mergejoin;
+
+import java.util.ArrayList;
+
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+/**
+ * @author Biswapesh
+ * @since 24-nov-2005
+ *
+ */
+
+public class MergeJoinData extends BaseStepData implements StepDataInterface
+{
+	public Object[] one, two;
+	public RowMetaInterface oneMeta, twoMeta;
+	public RowMetaInterface oneTwoMeta; //just for speed: oneMeta+twoMeta
+	public Object[] one_dummy, two_dummy;
+    public ArrayList<Object[]> ones, twos;
+    public Object[] one_next, two_next;
+    public boolean one_optional, two_optional;
+    public int[] keyNrs1;
+    public int[] keyNrs2;
+    
+	/**
+	 * Default initializer
+	 */
+	public MergeJoinData()
+	{
+		super();
+		ones = null;
+		twos = null;
+		one_next = null;
+		two_next = null;
+		one_dummy = null;
+		two_dummy = null;
+		one_optional = false;
+		two_optional = false;
+		keyNrs1 = null;
+		keyNrs2 = null;
+	}
+
+}
