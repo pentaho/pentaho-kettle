02f96b79fae4e855ce2e00fafb253a725e6c6289
jdixon <jdixon@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-29 15:34:31 +0000

    Updates to the XUL stuff
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3865 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/xul/swt/menu/MenuBar.java
+++ src/org/pentaho/xul/swt/menu/MenuBar.java
@@ -2,20 +2,30 @@ package org.pentaho.xul.swt.menu;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
+import org.pentaho.xul.EventHandler;
+import org.pentaho.xul.XulObject;
+import org.pentaho.xul.menu.XulMenu;
+import org.pentaho.xul.menu.XulMenuBar;
+import org.pentaho.xul.menu.XulMenuChoice;
+import org.pentaho.xul.menu.XulMenuItem;
 
-public class MenuBar extends MenuObject {
+public class MenuBar extends XulObject implements XulMenuBar {
 
     private Shell shell;
-    private MenuHandler handler;
+    private EventHandler handler;
 	
     private org.eclipse.swt.widgets.Menu menuBar;
 
 	public MenuBar( Shell shell ) {
 		super( null, null );
 		this.shell = shell;
-		handler = new MenuHandler();
+		handler = new EventHandler();
 		menuBar = new org.eclipse.swt.widgets.Menu(shell, SWT.BAR);
         shell.setMenuBar(menuBar);
+	}
+
+	public Object getNativeObject() {
+		return menuBar;
 	}
 
 	public org.eclipse.swt.widgets.Menu getSwtMenu() {
@@ -38,7 +48,7 @@ public class MenuBar extends MenuObject {
 		handler.handleAccessKey( accessKey );
 	}
 
-	public void register( Object item, String id, String accessKey ) {
+	public void register( XulMenuItem item, String id, String accessKey ) {
 		handler.register( item, id, accessKey );
 	}
 	
@@ -46,19 +56,19 @@ public class MenuBar extends MenuObject {
 		return handler.getMenuItemIds();
 	}
 	
-	public MenuChoice getMenuItemById( String id ) {
+	public XulMenuChoice getMenuItemById( String id ) {
 		return handler.getMenuItemById( id );
 	}
 	
-	public Menu getMenuById( String id ) {
+	public XulMenu getMenuById( String id ) {
 		return handler.getMenuById( id );
 	}
 	
-	public MenuItemSeparator getSeparatorById( String id ) {
+	public XulMenuItem getSeparatorById( String id ) {
 		return handler.getSeparatorById( id );
 	}
 	
-	public MenuChoice getMenuItemByKey( String accessKey ) {
+	public XulMenuChoice getMenuItemByKey( String accessKey ) {
 		return handler.getMenuItemByKey( accessKey );
 	}
 
@@ -71,14 +81,14 @@ public class MenuBar extends MenuObject {
 	}
 
 	public void setEnableById( String id, boolean enabled ) {
-		MenuChoice item = getMenuItemById( id );
+		XulMenuChoice item = getMenuItemById( id );
 		if( item != null ) {
 			item.setEnabled( enabled );
 		}
 	}
 	
 	public void setTextById( String id, String text ) {
-		MenuChoice item = getMenuItemById( id );
+		XulMenuChoice item = getMenuItemById( id );
 		if( item != null ) {
 			item.setText( text );
 		}
