aa2abf9bf3302e0db5f637eb0ade0daaf57bd349
jdixon <jdixon@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-06-29 16:06:50 +0000

    Updates to the XUL stuff
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3878 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/xul/toolbar/XulToolbar.java
@@ -0,0 +1,17 @@
+package org.pentaho.xul.toolbar;
+
+import org.pentaho.xul.XulItem;
+
+public interface XulToolbar extends XulItem {
+
+	public boolean handleMenuEvent( String id );
+
+	public void setEnableById( String id, boolean enabled );
+	
+	public void setHintById( String id, String text );
+	
+	public void setTextById( String id, String text );
+	
+	public void addMenuListener(String id, Object listener, String methodName);
+
+}
