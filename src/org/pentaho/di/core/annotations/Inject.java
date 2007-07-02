2dfc3d6490a44c83a250084004b91e6afb103021
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-02 17:30:03 +0000

    Custom "marker" annotation
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3937 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/core/annotations/Inject.java
@@ -0,0 +1,31 @@
+package org.pentaho.di.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * Allows for properties to be dynamically injected into classes during runtime.  
+ * 
+ * Both methods and fields can be annotated.
+ * 
+ * @author Alex Silva
+ *
+ */
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject
+{
+	/**
+	 * The name of the property to be injected.
+	 * Default values:
+	 * <ol><li>
+	 * <u>Fields:</u> The name of the annotated field.  For instance, annotating a field named "callerId" with @Inject has the same effect as
+	 * annotating this field with @Inject("callerId")</li>
+	 * <li><u>Methods:</u>The name of the property being set by the method, as defined by <code>Introspector.decapitalize</code> and 
+	 * the Java Beans API.</li>
+	 * </ol>
+	 * @return
+	 */
+	String property() default "";
+}
