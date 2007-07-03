1ff37430e81809af45221f9efa694ab7647eda2d
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-03 17:56:52 +0000

    Annotating a class with Step causes it to register itself as a Kettle step automatically.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@3964 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/core/annotations/Step.java
@@ -0,0 +1,37 @@
+package org.pentaho.di.core.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.pentaho.di.trans.step.StepCategory;
+
+
+/**
+ * An alternate way of defining steps. Classes annotated with "Step" are
+ * automatically recognized and registered as a step.
+ * 
+ * Important: The XML definitions alienate annoated steps and the two methods of definition are therefore
+ * mutually exclusive.
+ * 
+ * @author Alex Silva
+ * 
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Step
+{
+	String[] name();
+
+	String description() default "";
+
+	String tooltip() default "";
+
+	String image();
+
+	StepCategory category();
+
+}
