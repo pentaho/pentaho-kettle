dbb442e3bd7299a29ac86ddd441100cd411e643c
mlowery <mlowery@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-09 20:26:11 +0000

    Formatting only.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4026 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/entries/delay/JobEntryDelay.java
+++ src/org/pentaho/di/job/entries/delay/JobEntryDelay.java
@@ -1,4 +1,4 @@
- /**********************************************************************
+/**********************************************************************
  **                                                                   **
  **               This code belongs to the KETTLE project.            **
  **                                                                   **
@@ -12,7 +12,7 @@
  ** info@kettle.be                                                    **
  **                                                                   **
  **********************************************************************/
- 
+
 package org.pentaho.di.job.entries.delay;
 
 import java.util.List;
@@ -34,216 +34,181 @@ import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.repository.Repository;
 import org.w3c.dom.Node;
 
-
 /**
  * Job entry type to sleep for a time. It uses a piece of javascript to do this.
- * 
+ *
  * @author Samatar
  * @since 21-02-2007
  */
-public class JobEntryDelay extends JobEntryBase implements Cloneable, JobEntryInterface
-{
-	static private String DEFAULT_MAXIMUM_TIMEOUT  = "0";        
-	private String  maximumTimeout;                        // maximum timeout in seconds
-	public int scaletime;
+public class JobEntryDelay extends JobEntryBase implements Cloneable, JobEntryInterface {
+  static private String DEFAULT_MAXIMUM_TIMEOUT = "0";
 
-	public JobEntryDelay(String n)
-	{
-		super(n, "");
-		setID(-1L);
-		setType(JobEntryInterface.TYPE_JOBENTRY_DELAY);
-	}
+  private String maximumTimeout; // maximum timeout in seconds
 
-	public JobEntryDelay()
-	{
-		this("");
-	}
-	
-	public JobEntryDelay(JobEntryBase jeb)
-	{
-		super(jeb);
-	}
-    
-    public Object clone()
-    {
-        JobEntryDelay je = (JobEntryDelay) super.clone();
-        return je;
+  public int scaletime;
+
+  public JobEntryDelay(String n) {
+    super(n, "");
+    setID(-1L);
+    setType(JobEntryInterface.TYPE_JOBENTRY_DELAY);
+  }
+
+  public JobEntryDelay() {
+    this("");
+  }
+
+  public JobEntryDelay(JobEntryBase jeb) {
+    super(jeb);
+  }
+
+  public Object clone() {
+    JobEntryDelay je = (JobEntryDelay) super.clone();
+    return je;
+  }
+
+  public String getXML() {
+    StringBuffer retval = new StringBuffer(200);
+
+    retval.append(super.getXML());
+    retval.append("      ").append(XMLHandler.addTagValue("maximumTimeout", maximumTimeout));
+    retval.append("      ").append(XMLHandler.addTagValue("scaletime", scaletime));
+
+    return retval.toString();
+  }
+
+  public void loadXML(Node entrynode, List<DatabaseMeta> databases, Repository rep) throws KettleXMLException {
+    try {
+      super.loadXML(entrynode, databases);
+      maximumTimeout = XMLHandler.getTagValue(entrynode, "maximumTimeout");
+      scaletime = Integer.parseInt(XMLHandler.getTagValue(entrynode, "scaletime"));
+    } catch (Exception e) {
+      throw new KettleXMLException("Unable to load job entry of type 'delay' from XML node", e);
     }
-	
-	public String getXML()
-	{
-        StringBuffer retval = new StringBuffer(200);
-	
-		retval.append(super.getXML());
-		retval.append("      ").append(XMLHandler.addTagValue("maximumTimeout",  maximumTimeout));
-		retval.append("      ").append(XMLHandler.addTagValue("scaletime",       scaletime));
+  }
 
-		return retval.toString();
-	}
-	
-	public void loadXML(Node entrynode, List<DatabaseMeta> databases, Repository rep)
-		throws KettleXMLException
-	{
-		try
-		{
-			super.loadXML(entrynode, databases);
-			maximumTimeout = XMLHandler.getTagValue(entrynode, "maximumTimeout");
-			scaletime      = Integer.parseInt(XMLHandler.getTagValue(entrynode, "scaletime"));
-		}
-		catch(Exception e)
-		{
-			throw new KettleXMLException("Unable to load job entry of type 'delay' from XML node", e);
-		}
-	}
+  public void loadRep(Repository rep, long id_jobentry, List<DatabaseMeta> databases) throws KettleException {
+    try {
+      super.loadRep(rep, id_jobentry, databases);
 
-	public void loadRep(Repository rep, long id_jobentry, List<DatabaseMeta> databases)
-		throws KettleException
-	{
-		try
-		{
-			super.loadRep(rep, id_jobentry, databases);
-
-			maximumTimeout =      rep.getJobEntryAttributeString(id_jobentry,  "maximumTimeout");
-			scaletime      = (int)rep.getJobEntryAttributeInteger(id_jobentry, "scaletime");
-		}
-		catch(KettleDatabaseException dbe)
-		{
-			throw new KettleException("Unable to load job entry of type 'delay' from the repository with id_jobentry="+id_jobentry, dbe);
-		}
-	}
-
-	//
-	// Save the attributes of this job entry
-	//
-	public void saveRep(Repository rep, long id_job)
-		throws KettleException
-	{
-		try
-		{
-			super.saveRep(rep, id_job);
-			
-			rep.saveJobEntryAttribute(id_job, getID(), "maximumTimeout", maximumTimeout);
-			rep.saveJobEntryAttribute(id_job, getID(), "scaletime",      scaletime);
-		}
-		catch(KettleDatabaseException dbe)
-		{
-			throw new KettleException("Unable to save job entry of type 'delay' to the repository for id_job="+id_job, dbe);
-		}
-	}
-		
-	/**
-	 * Execute this job entry and return the result.
-	 * In this case it means, just set the result boolean in the Result class.
-	 * @param previousResult The result of the previous execution
-	 * @return The Result of the execution.
-	 */
-	public Result execute(Result previousResult, int nr, Repository rep, Job parentJob)
-	{
-		LogWriter log = LogWriter.getInstance();
-		Result result = previousResult;
-		result.setResult( false );
-		int Multiple;
-		String Waitscale;
-
-		// Scale time
-		if (scaletime == 0)
-		{
-			// Second
-			Multiple = 1000;
-			Waitscale = Messages.getString("JobEval.SScaleTime.Label");
-
-		}
-		else if (scaletime == 1)
-		{
-			// Minute
-			Multiple = 60000;
-			Waitscale = Messages.getString("JobEval.MnScaleTime.Label");
-		}
-		else
-		{
-			// Hour
-			Multiple=3600000;
-			Waitscale = Messages.getString("JobEval.HrScaleTime.Label");
-		}
-		try
-		{
-			// starttime (in seconds ,Minutes or Hours)
-			long timeStart = System.currentTimeMillis() / Multiple;
-
-			long iMaximumTimeout = Const.toInt(getMaximumTimeout(),Const.toInt(DEFAULT_MAXIMUM_TIMEOUT, 0));
-
-			if ( log.isDetailed() )
-			{
-				log.logDetailed(toString(), Messages.getString("JobEval.LetsWaitFor.Label") + iMaximumTimeout + " " + Waitscale + "...");
-			}
-
-			boolean continueLoop = true;
-			//
-			// Sanity check on some values, and complain on insanity
-			//
-			if ( iMaximumTimeout < 0 )
-			{
-				iMaximumTimeout = Const.toInt(DEFAULT_MAXIMUM_TIMEOUT, 0);
-				log.logBasic(toString(), Messages.getString("JobEval.MaximunTimeReseted.Label") + iMaximumTimeout+ " " + Waitscale);
-			}
-
-			// TODO don't contineously loop, but sleeping would be better.
-			while ( continueLoop && !parentJob.isStopped() )
-			{
-				// Update Time value
-				long now = System.currentTimeMillis()/ Multiple;
-
-				// Let's check the limit time
-				if ( (iMaximumTimeout > 0) && (now >= (timeStart + iMaximumTimeout)))
-				{													
-					// We have reached the time limit
-					if ( log.isDetailed() )
-					{
-						log.logDetailed(toString(), Messages.getString("JobEval.WaitTimeIsElapsed.Label"));
-					}
-					continueLoop = false;
-					result.setResult( true );
-				}
-			}
-		}
-		catch(Exception e)
-		{
-			// We get an exception
-			result.setResult( false );
-			log.logError(toString(), "Error  : "+e.getMessage());
-		}
-	
-		return result;
-	}
-	
-	public boolean resetErrorsBeforeExecution()
-	{
-		// we should be able to evaluate the errors in
-		// the previous jobentry.
-	    return false;
-	}
-	
-	public boolean evaluates()
-	{
-		return true;
-	}
-	
-	public boolean isUnconditional()
-	{
-		return false;
-	}
-    
-    public JobEntryDialogInterface getDialog(Shell shell,JobEntryInterface jei,JobMeta jobMeta,String jobName,Repository rep) {
-        return new JobEntryDelayDialog(shell,this,jobMeta);
+      maximumTimeout = rep.getJobEntryAttributeString(id_jobentry, "maximumTimeout");
+      scaletime = (int) rep.getJobEntryAttributeInteger(id_jobentry, "scaletime");
+    } catch (KettleDatabaseException dbe) {
+      throw new KettleException("Unable to load job entry of type 'delay' from the repository with id_jobentry="
+          + id_jobentry, dbe);
     }
-	
-	public String getMaximumTimeout() 
-	{
-		return environmentSubstitute(maximumTimeout);
-	}
+  }
 
-	public void setMaximumTimeout(String s)
-	{
-		maximumTimeout = s;
-	}
+  //
+  // Save the attributes of this job entry
+  //
+  public void saveRep(Repository rep, long id_job) throws KettleException {
+    try {
+      super.saveRep(rep, id_job);
+
+      rep.saveJobEntryAttribute(id_job, getID(), "maximumTimeout", maximumTimeout);
+      rep.saveJobEntryAttribute(id_job, getID(), "scaletime", scaletime);
+    } catch (KettleDatabaseException dbe) {
+      throw new KettleException("Unable to save job entry of type 'delay' to the repository for id_job=" + id_job, dbe);
+    }
+  }
+
+  /**
+   * Execute this job entry and return the result.
+   * In this case it means, just set the result boolean in the Result class.
+   * @param previousResult The result of the previous execution
+   * @return The Result of the execution.
+   */
+  public Result execute(Result previousResult, int nr, Repository rep, Job parentJob) {
+    LogWriter log = LogWriter.getInstance();
+    Result result = previousResult;
+    result.setResult(false);
+    int Multiple;
+    String Waitscale;
+
+    // Scale time
+    if (scaletime == 0) {
+      // Second
+      Multiple = 1000;
+      Waitscale = Messages.getString("JobEval.SScaleTime.Label");
+
+    } else if (scaletime == 1) {
+      // Minute
+      Multiple = 60000;
+      Waitscale = Messages.getString("JobEval.MnScaleTime.Label");
+    } else {
+      // Hour
+      Multiple = 3600000;
+      Waitscale = Messages.getString("JobEval.HrScaleTime.Label");
+    }
+    try {
+      // starttime (in seconds ,Minutes or Hours)
+      long timeStart = System.currentTimeMillis() / Multiple;
+
+      long iMaximumTimeout = Const.toInt(getMaximumTimeout(), Const.toInt(DEFAULT_MAXIMUM_TIMEOUT, 0));
+
+      if (log.isDetailed()) {
+        log.logDetailed(toString(), Messages.getString("JobEval.LetsWaitFor.Label") + iMaximumTimeout + " " + Waitscale
+            + "...");
+      }
+
+      boolean continueLoop = true;
+      //
+      // Sanity check on some values, and complain on insanity
+      //
+      if (iMaximumTimeout < 0) {
+        iMaximumTimeout = Const.toInt(DEFAULT_MAXIMUM_TIMEOUT, 0);
+        log.logBasic(toString(), Messages.getString("JobEval.MaximunTimeReseted.Label") + iMaximumTimeout + " "
+            + Waitscale);
+      }
+
+      // TODO don't contineously loop, but sleeping would be better.
+      while (continueLoop && !parentJob.isStopped()) {
+        // Update Time value
+        long now = System.currentTimeMillis() / Multiple;
+
+        // Let's check the limit time
+        if ((iMaximumTimeout > 0) && (now >= (timeStart + iMaximumTimeout))) {
+          // We have reached the time limit
+          if (log.isDetailed()) {
+            log.logDetailed(toString(), Messages.getString("JobEval.WaitTimeIsElapsed.Label"));
+          }
+          continueLoop = false;
+          result.setResult(true);
+        }
+      }
+    } catch (Exception e) {
+      // We get an exception
+      result.setResult(false);
+      log.logError(toString(), "Error  : " + e.getMessage());
+    }
+
+    return result;
+  }
+
+  public boolean resetErrorsBeforeExecution() {
+    // we should be able to evaluate the errors in
+    // the previous jobentry.
+    return false;
+  }
+
+  public boolean evaluates() {
+    return true;
+  }
+
+  public boolean isUnconditional() {
+    return false;
+  }
+
+  public JobEntryDialogInterface getDialog(Shell shell, JobEntryInterface jei, JobMeta jobMeta, String jobName,
+      Repository rep) {
+    return new JobEntryDelayDialog(shell, this, jobMeta);
+  }
+
+  public String getMaximumTimeout() {
+    return environmentSubstitute(maximumTimeout);
+  }
+
+  public void setMaximumTimeout(String s) {
+    maximumTimeout = s;
+  }
 }
