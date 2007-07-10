88f11e10d15ab08487fda0d97cf97c97f1971c2d
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-10 20:58:51 +0000

    Job annotation - used to automatically register a class as a job
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4058 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/core/annotations/Job.java
@@ -0,0 +1,33 @@
+package org.pentaho.di.core.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.pentaho.di.job.JobEntryType;
+
+/**
+ * An alternative when defining Jobs. Classes annotated with "Job" are
+ * automatically recognized and registered as a job.
+ * 
+ * Important: The XML definitions alienate annoated steps and the two methods of definition are therefore
+ * mutually exclusive.
+ * 
+ * @author Alex Silva
+ * 
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Job
+{
+	String id();
+
+	String tooltip() default "";
+
+	String image();
+
+	JobEntryType type();
+}
