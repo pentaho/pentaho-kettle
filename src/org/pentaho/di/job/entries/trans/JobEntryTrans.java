39813de34e346893dcef20d121cd45f681a4c81b
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-13 13:17:57 +0000

    Modified id to 'TRANS' (TRANSFORMATION was not backwards compatible)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4140 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/entries/trans/JobEntryTrans.java
+++ src/org/pentaho/di/job/entries/trans/JobEntryTrans.java
@@ -68,7 +68,7 @@ import org.w3c.dom.Node;
  * @since 1-10-2003, rewritten on 18-06-2004
  * 
  */
-@org.pentaho.di.core.annotations.Job(image="TRN.png",id="TRANSFORMATION",type=JobEntryType.TRANSFORMATION,tooltip="JobEntry.Trans.Tooltip")
+@org.pentaho.di.core.annotations.Job(image="TRN.png",id="TRANS",type=JobEntryType.TRANSFORMATION,tooltip="JobEntry.Trans.Tooltip")
 public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryInterface
 {	
 	private String              transname;
