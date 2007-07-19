44194988b0f595e2117c88088b6474a3ac6a3143
jdixon <jdixon@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-19 08:35:58 +0000

    Refactor of the classes into two source trees: one for the engine code and one for the UI code. All of the UI and SWT dependent code is now in src-ui. All the UI classes are in the org.pentaho.du.ui.* packages in the src-ui tree. The build has not yet been modified to create two separate jars from these source tree.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4259 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/normaliser/NormaliserMeta.java
+++ src/org/pentaho/di/trans/steps/normaliser/NormaliserMeta.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.swt.widgets.Shell;
 import org.pentaho.di.core.CheckResult;
 import org.pentaho.di.core.CheckResultInterface;
 import org.pentaho.di.core.Const;
@@ -23,7 +22,6 @@ import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
 import org.pentaho.di.trans.step.StepDataInterface;
-import org.pentaho.di.trans.step.StepDialogInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
@@ -379,11 +377,6 @@ public class NormaliserMeta extends BaseStepMeta implements StepMetaInterface
 			cr = new CheckResult(CheckResult.TYPE_RESULT_ERROR, Messages.getString("NormaliserMeta.CheckResult.NoInputReceivedError"), stepMeta); //$NON-NLS-1$
 			remarks.add(cr);
 		}
-	}
-
-	public StepDialogInterface getDialog(Shell shell, StepMetaInterface info, TransMeta transMeta, String name)
-	{
-		return new NormaliserDialog(shell, info, transMeta, name);
 	}
 
 	public StepInterface getStep(StepMeta stepMeta, StepDataInterface stepDataInterface, int cnr, TransMeta transMeta, Trans trans)
