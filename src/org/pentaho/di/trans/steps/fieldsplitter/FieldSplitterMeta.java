1a918fabf7cb8dd1aaa41fcda89ebda7d9a1ee6d
jdixon <jdixon@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-19 08:34:55 +0000

    Refactor of the classes into two source trees: one for the engine code and one for the UI code. All of the UI and SWT dependent code is now in src-ui. All the UI classes are in the org.pentaho.du.ui.* packages in the src-ui tree. The build has not yet been modified to create two separate jars from these source tree.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4257 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/fieldsplitter/FieldSplitterMeta.java
+++ src/org/pentaho/di/trans/steps/fieldsplitter/FieldSplitterMeta.java
@@ -18,7 +18,6 @@ package org.pentaho.di.trans.steps.fieldsplitter;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.swt.widgets.Shell;
 import org.w3c.dom.Node;
 
 import org.pentaho.di.core.CheckResult;
@@ -40,7 +39,6 @@ import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
 import org.pentaho.di.trans.step.StepCategory;
 import org.pentaho.di.trans.step.StepDataInterface;
-import org.pentaho.di.trans.step.StepDialogInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
@@ -636,11 +634,6 @@ public class FieldSplitterMeta extends BaseStepMeta implements StepMetaInterface
 			cr = new CheckResult(CheckResult.TYPE_RESULT_ERROR, Messages.getString("FieldSplitterMeta.CheckResult.NoInputReceivedFromOtherStep"), stepMeta); //$NON-NLS-1$
 			remarks.add(cr);
 		}
-	}
-
-	public StepDialogInterface getDialog(Shell shell, StepMetaInterface info, TransMeta transMeta, String name)
-	{
-		return new FieldSplitterDialog(shell, info, transMeta, name);
 	}
 
 	public StepInterface getStep(StepMeta stepMeta, StepDataInterface stepDataInterface, int cnr, TransMeta transMeta, Trans trans)
