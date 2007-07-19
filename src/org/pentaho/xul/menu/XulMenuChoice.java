c28eff6d9a70589a4db82d372dd027b2fe9fcbd1
jdixon <jdixon@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-07-19 08:38:01 +0000

    Refactor of the classes into two source trees: one for the engine code and one for the UI code. All of the UI and SWT dependent code is now in src-ui. All the UI classes are in the org.pentaho.du.ui.* packages in the src-ui tree. The build has not yet been modified to create two separate jars from these source tree.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4263 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/xul/menu/XulMenuChoice.java
+++
@@ -1,7 +0,0 @@
-package org.pentaho.xul.menu;
-
-public interface XulMenuChoice extends XulMenuItem {
-
-	public void handleMenuEvent( );
-	
-}
--- src/org/pentaho/xul/menu/XulMenuItemSeparator.java
+++
@@ -1,5 +0,0 @@
-package org.pentaho.xul.menu;
-
-public interface XulMenuItemSeparator {
-
-}
