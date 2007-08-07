24452e5fa83ca03841386dd4995a2cbc98b0014c
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-07 23:11:12 +0000

    OI (Organized Imports)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4504 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/mappinginput/MappingInput.java
+++ src/org/pentaho/di/trans/steps/mappinginput/MappingInput.java
@@ -23,7 +23,6 @@ import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.row.RowDataUtil;
 import org.pentaho.di.core.row.RowMeta;
-import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
