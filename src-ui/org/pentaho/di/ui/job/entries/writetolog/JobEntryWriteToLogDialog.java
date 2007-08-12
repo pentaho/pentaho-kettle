7bcb77c59efd4ef698c8e9b6d9f1f50f24ae18cc
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-12 11:41:03 +0000

    Write to log job entry
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4544 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src-ui/org/pentaho/di/ui/job/entries/writetolog/JobEntryWriteToLogDialog.java
@@ -0,0 +1,340 @@
+/**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+
+/*
+ * Created on 19-jun-2003
+ *
+ */
+
+package org.pentaho.di.ui.job.entries.writetolog;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.Props;
+import org.pentaho.di.core.logging.LogWriter;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.ui.core.gui.WindowProperty;
+import org.pentaho.di.ui.core.widget.TextVar;
+import org.pentaho.di.ui.job.dialog.JobDialog;
+import org.pentaho.di.ui.job.entry.JobEntryDialog; 
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.entry.JobEntryDialogInterface;
+import org.pentaho.di.job.entry.JobEntryInterface;
+import org.pentaho.di.ui.trans.step.BaseStepDialog;
+import org.pentaho.di.ui.trans.steps.textfileinput.VariableButtonListenerFactory;
+import org.pentaho.di.job.entries.writetolog.JobEntryWriteToLog;
+import org.pentaho.di.job.entries.writetolog.Messages;
+
+/**
+ * This dialog allows you to edit a JobEntryWriteToLog object.
+ * 
+ * @author Samatar
+ * @since 08-08-2007
+ */
+
+public class JobEntryWriteToLogDialog extends JobEntryDialog implements JobEntryDialogInterface
+{
+    private Label wlName;
+
+    private Text wName;
+
+    private FormData fdlName, fdName;
+
+    private Label wlLogMessage;
+
+    private Text wLogMessage;
+
+    private FormData fdlLogMessage, fdLogMessage;
+
+    private Button wOK, wCancel;
+
+    private Listener lsOK, lsCancel;
+
+    private JobEntryWriteToLog jobEntry;
+
+    private Shell shell;
+
+    private SelectionAdapter lsDef;
+
+    private boolean changed;
+
+	//Log subject
+	private Label wlLogSubject;
+
+	private TextVar wLogSubject;
+
+	private FormData fdlLogSubject, fdLogSubject;
+	
+    private Label wlLoglevel;
+
+    private CCombo wLoglevel;
+
+    private FormData fdlLoglevel, fdLoglevel;
+    
+    
+    public JobEntryWriteToLogDialog(Shell parent, JobEntryInterface jobEntryInt, Repository rep, JobMeta jobMeta)
+    {
+        super(parent, jobEntryInt, rep, jobMeta);
+        jobEntry = (JobEntryWriteToLog) jobEntryInt;
+        if (this.jobEntry.getName() == null)
+            this.jobEntry.setName(Messages.getString("WriteToLog.Name.Default"));
+    }
+    
+    
+    public JobEntryInterface open()
+    {
+        Shell parent = getParent();
+        Display display = parent.getDisplay();
+
+        shell = new Shell(parent, props.getJobsDialogStyle());
+        props.setLook(shell);
+        JobDialog.setShellImage(shell, jobEntry);
+
+        ModifyListener lsMod = new ModifyListener()
+        {
+            public void modifyText(ModifyEvent e)
+            {
+                jobEntry.setChanged();
+            }
+        };
+        changed = jobEntry.hasChanged();
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.marginWidth = Const.FORM_MARGIN;
+        formLayout.marginHeight = Const.FORM_MARGIN;
+
+        shell.setLayout(formLayout);
+        shell.setText(Messages.getString("WriteToLog.Title"));
+
+        int middle = props.getMiddlePct();
+        int margin = Const.MARGIN;
+
+        wOK = new Button(shell, SWT.PUSH);
+        wOK.setText(Messages.getString("System.Button.OK"));
+        wCancel = new Button(shell, SWT.PUSH);
+        wCancel.setText(Messages.getString("System.Button.Cancel"));
+
+        // at the bottom
+        BaseStepDialog.positionBottomButtons(shell, new Button[] { wOK, wCancel }, margin, null);
+
+        // Filename line
+        wlName = new Label(shell, SWT.RIGHT);
+        wlName.setText(Messages.getString("WriteToLog.Label"));
+        props.setLook(wlName);
+        fdlName = new FormData();
+        fdlName.left = new FormAttachment(0, 0);
+		fdlName.right = new FormAttachment(middle, 0);
+        fdlName.top = new FormAttachment(0, margin);
+        wlName.setLayoutData(fdlName);
+        wName = new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+        props.setLook(wName);
+        wName.addModifyListener(lsMod);
+        fdName = new FormData();
+        fdName.left = new FormAttachment(middle, 0);
+        fdName.top = new FormAttachment(0, margin);
+        fdName.right = new FormAttachment(100, 0);
+        wName.setLayoutData(fdName);
+
+        // Log Level
+        wlLoglevel = new Label(shell, SWT.RIGHT);
+        wlLoglevel.setText(Messages.getString("WriteToLog.Loglevel.Label"));
+        props.setLook(wlLoglevel);
+        fdlLoglevel = new FormData();
+        fdlLoglevel.left = new FormAttachment(0, 0);
+        fdlLoglevel.right = new FormAttachment(middle, -margin);
+        fdlLoglevel.top = new FormAttachment(wName, margin);
+        wlLoglevel.setLayoutData(fdlLoglevel);
+        wLoglevel = new CCombo(shell, SWT.SINGLE | SWT.READ_ONLY | SWT.BORDER);
+        for (int i = 0; i < LogWriter.logLevelDescription.length; i++)
+            wLoglevel.add(LogWriter.logLevelDescription[i]);
+        wLoglevel.select(jobEntry.loglevel + 1); // +1: starts at -1
+
+        props.setLook(wLoglevel);
+        fdLoglevel = new FormData();
+        fdLoglevel.left = new FormAttachment(middle, 0);
+        fdLoglevel.top = new FormAttachment(wName, margin);
+        fdLoglevel.right = new FormAttachment(100, 0);
+        wLoglevel.setLayoutData(fdLoglevel);
+
+        // Subject
+		wlLogSubject = new Label(shell, SWT.RIGHT);
+		wlLogSubject.setText(Messages.getString("WriteToLog.LogSubject.Label"));
+        props.setLook(wlLogSubject);
+        fdlLogSubject = new FormData();
+        fdlLogSubject.left = new FormAttachment(0, 0);
+        fdlLogSubject.top = new FormAttachment(wLoglevel, margin);
+        fdlLogSubject.right = new FormAttachment(middle, -margin);
+        wlLogSubject.setLayoutData(fdlLogSubject);
+
+        wLogSubject = new TextVar(jobMeta, shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+        wLogSubject.setText(Messages.getString("WriteToLog.Name.Default"));
+        props.setLook(wLogSubject);
+        wLogSubject.addModifyListener(lsMod);
+        fdLogSubject = new FormData();
+        fdLogSubject.left = new FormAttachment(middle, 0);
+        fdLogSubject.top = new FormAttachment(wLoglevel, margin);
+        fdLogSubject.right = new FormAttachment(100, 0);
+		wLogSubject.setLayoutData(fdLogSubject);
+		
+			
+
+        // Log message to display
+		wlLogMessage = new Label(shell, SWT.RIGHT);
+        wlLogMessage.setText(Messages.getString("WriteToLog.LogMessage.Label"));
+        props.setLook(wlLogMessage);
+        fdlLogMessage = new FormData();
+        fdlLogMessage.left = new FormAttachment(0, 0);
+        fdlLogMessage.top = new FormAttachment(wLogSubject, margin);
+		fdlLogMessage.right = new FormAttachment(middle, -margin);
+        wlLogMessage.setLayoutData(fdlLogMessage);
+
+        wLogMessage = new Text(shell, SWT.MULTI | SWT.LEFT | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+        wLogMessage.setText(Messages.getString("WriteToLog.Name.Default"));
+        props.setLook(wLogMessage,Props.WIDGET_STYLE_FIXED);
+        wLogMessage.addModifyListener(lsMod);
+        fdLogMessage = new FormData();
+        fdLogMessage.left = new FormAttachment(middle, 0);
+        fdLogMessage.top = new FormAttachment(wLogSubject, margin);
+        fdLogMessage.right = new FormAttachment(100, 0);
+		fdLogMessage.bottom =new FormAttachment(wOK, -margin);
+        wLogMessage.setLayoutData(fdLogMessage);
+
+
+
+		SelectionAdapter lsVar = VariableButtonListenerFactory.getSelectionAdapter(shell, wLogMessage, jobMeta);
+		wLogMessage.addKeyListener(TextVar.getControlSpaceKeyListener(wLogMessage, lsVar));
+
+
+
+
+        // Add listeners
+        lsCancel = new Listener()
+        {
+            public void handleEvent(Event e)
+            {
+                cancel();
+            }
+        };
+
+
+
+        lsOK = new Listener()
+        {
+            public void handleEvent(Event e)
+            {
+                ok();
+            }
+        };
+
+        wCancel.addListener(SWT.Selection, lsCancel);
+        wOK.addListener(SWT.Selection, lsOK);
+
+        lsDef = new SelectionAdapter()
+        {
+            public void widgetDefaultSelected(SelectionEvent e)
+            {
+                ok();
+            }
+        };
+
+        wName.addSelectionListener(lsDef);
+
+        // Detect X or ALT-F4 or something that kills this window...
+        shell.addShellListener(new ShellAdapter()
+        {
+            public void shellClosed(ShellEvent e)
+            {
+                cancel();
+            }
+        });
+
+
+        getData();
+
+        BaseStepDialog.setSize(shell, 250, 250, false);
+
+        shell.open();
+        props.setDialogSize(shell, "JobEvalDialogSize");
+        while (!shell.isDisposed())
+        {
+            if (!display.readAndDispatch())
+                display.sleep();
+        }
+        return jobEntry;
+    }
+
+    public void dispose()
+    {
+        WindowProperty winprop = new WindowProperty(shell);
+        props.setScreen(winprop);
+        shell.dispose();
+    }
+
+    /**
+     * Copy information from the meta-data input to the dialog fields.
+     */
+    public void getData()
+    {
+        if (jobEntry.getName() != null)
+            wName.setText(jobEntry.getName());
+        wName.selectAll();
+        if (jobEntry.getLogMessage() != null)
+            wLogMessage.setText(jobEntry.getLogMessage());
+        
+        if (jobEntry.getLogSubject() != null)
+            wLogSubject.setText(jobEntry.getLogSubject());
+
+        wLoglevel.select(jobEntry.loglevel + 1);
+    }
+
+    private void cancel()
+    {
+        jobEntry.setChanged(changed);
+        jobEntry = null;
+        dispose();
+    }
+
+    private void ok()
+    {
+        jobEntry.setName(wName.getText());
+        jobEntry.setLogMessage(wLogMessage.getText());
+        jobEntry.setLogSubject(wLogSubject.getText());
+        jobEntry.loglevel = wLoglevel.getSelectionIndex() - 1;
+        dispose();
+    }
+
+    public String toString()
+    {
+        return this.getClass().getName();
+    }
+}
