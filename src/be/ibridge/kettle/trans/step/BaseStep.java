d2f2a760c730e41bb630af3774859b98794deaad
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-16 16:31:32 +0000

    Change Request - [# 6026] Regex evaluation
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/2.5.1@4593 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/be/ibridge/kettle/trans/step/BaseStep.java
+++ src/be/ibridge/kettle/trans/step/BaseStep.java
@@ -113,6 +113,7 @@ import be.ibridge.kettle.trans.step.xbaseinput.XBaseInputMeta;
 import be.ibridge.kettle.trans.step.xmlinput.XMLInputMeta;
 import be.ibridge.kettle.trans.step.xmloutput.XMLOutputMeta;
 import be.ibridge.kettle.trans.step.orabulkloader.OraBulkLoaderMeta;
+import be.ibridge.kettle.trans.step.regexeval.RegexEvalMeta;
 
 public class BaseStep extends Thread
 {
@@ -266,7 +267,10 @@ public class BaseStep extends Thread
             new StepPluginMeta(AbortMeta.class, "Abort", Messages.getString("BaseStep.TypeLongDesc.Abort"), Messages
                     .getString("BaseStep.TypeTooltipDesc.Abort"), "ABR.png", CATEGORY_TRANSFORM),
             new StepPluginMeta(OraBulkLoaderMeta.class, "OraBulkLoader", Messages.getString("BaseStep.TypeLongDesc.OraBulkLoader"), Messages
-                    .getString("BaseStep.TypeTooltipDesc.OraBulkLoader"), "OBL.png", CATEGORY_EXPERIMENTAL),                     
+                    .getString("BaseStep.TypeTooltipDesc.OraBulkLoader"), "OBL.png", CATEGORY_EXPERIMENTAL),    
+            new StepPluginMeta(RegexEvalMeta.class, "RegexEval", Messages.getString("BaseStep.TypeLongDesc.RegexEval"), Messages
+                      .getString("BaseStep.TypeTooltipDesc.RegexEval"), "RGE.png", CATEGORY_EXPERIMENTAL),  
+                    
         };
 
     public static final String category_order[] =
