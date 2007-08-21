cf303f98b407c716b89829af1f2492d1ecae7a83
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-21 14:06:51 +0000

    Added trimType field
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4655 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/textfileoutput/TextFileField.java
+++ src/org/pentaho/di/trans/steps/textfileoutput/TextFileField.java
@@ -1,4 +1,4 @@
- /**********************************************************************
+/**********************************************************************
  **                                                                   **
  **               This code belongs to the KETTLE project.            **
  **                                                                   **
@@ -12,91 +12,100 @@
  ** info@kettle.be                                                    **
  **                                                                   **
  **********************************************************************/
- 
 
 package org.pentaho.di.trans.steps.textfileoutput;
 
 import org.pentaho.di.core.row.ValueMeta;
+import org.pentaho.di.trans.steps.textfileinput.TextFileInputMeta;
 
 /**
  * Describes a single field in a text file
  * 
  * @author Matt
  * @since 11-05-2005
- *
+ * 
  */
 public class TextFileField implements Cloneable
 {
-	private String 	name;
-	private int 	type;
-	private String 	format;
-	private int 	length;
-	private int 	precision;
-	private String 	currencySymbol;
-	private String 	decimalSymbol;
-	private String 	groupingSymbol;
-	private String 	nullString;
-	
-	public TextFileField(String name, int type, String format, int length, int precision, String currencySymbol, String decimalSymbol, String groupSymbol, String nullString)
+	private String name;
+
+	private int type;
+
+	private String format;
+
+	private int length;
+
+	private int precision;
+
+	private String currencySymbol;
+
+	private String decimalSymbol;
+
+	private String groupingSymbol;
+
+	private String nullString;
+
+	private int trimType;
+
+	public TextFileField(String name, int type, String format, int length, int precision,
+			String currencySymbol, String decimalSymbol, String groupSymbol, String nullString)
 	{
-		this.name      		= name;
-		this.type			= type;
-		this.format			= format;
-		this.length			= length;
-		this.precision  	= precision;
-		this.currencySymbol	= currencySymbol;
-		this.decimalSymbol	= decimalSymbol;
-		this.groupingSymbol	= groupSymbol;
-		this.nullString		= nullString; 
+		this.name = name;
+		this.type = type;
+		this.format = format;
+		this.length = length;
+		this.precision = precision;
+		this.currencySymbol = currencySymbol;
+		this.decimalSymbol = decimalSymbol;
+		this.groupingSymbol = groupSymbol;
+		this.nullString = nullString;
 	}
-	
+
 	public TextFileField()
 	{
 	}
 
-	
 	public int compare(Object obj)
 	{
-		TextFileField field = (TextFileField)obj;
-		
+		TextFileField field = (TextFileField) obj;
+
 		return name.compareTo(field.getName());
 	}
 
 	public boolean equal(Object obj)
 	{
-		TextFileField field = (TextFileField)obj;
-		
+		TextFileField field = (TextFileField) obj;
+
 		return name.equals(field.getName());
 	}
-	
+
 	public Object clone()
 	{
 		try
 		{
 			Object retval = super.clone();
 			return retval;
-		}
-		catch(CloneNotSupportedException e)
+		} catch (CloneNotSupportedException e)
 		{
 			return null;
 		}
 	}
-	
+
 	public int getLength()
 	{
 		return length;
 	}
-	
+
 	public void setLength(int length)
 	{
 		this.length = length;
 	}
-	
+
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	public void setName(String fieldname)
 	{
 		this.name = fieldname;
@@ -111,32 +120,32 @@ public class TextFileField implements Cloneable
 	{
 		return ValueMeta.getTypeDesc(type);
 	}
-	
+
 	public void setType(int type)
 	{
 		this.type = type;
 	}
-	
+
 	public void setType(String typeDesc)
 	{
-	    this.type = ValueMeta.getType(typeDesc);
+		this.type = ValueMeta.getType(typeDesc);
 	}
-	
+
 	public String getFormat()
 	{
 		return format;
 	}
-	
+
 	public void setFormat(String format)
 	{
 		this.format = format;
 	}
-	
+
 	public String getGroupingSymbol()
 	{
 		return groupingSymbol;
 	}
-	
+
 	public void setGroupingSymbol(String group_symbol)
 	{
 		this.groupingSymbol = group_symbol;
@@ -146,7 +155,7 @@ public class TextFileField implements Cloneable
 	{
 		return decimalSymbol;
 	}
-	
+
 	public void setDecimalSymbol(String decimal_symbol)
 	{
 		this.decimalSymbol = decimal_symbol;
@@ -156,7 +165,7 @@ public class TextFileField implements Cloneable
 	{
 		return currencySymbol;
 	}
-	
+
 	public void setCurrencySymbol(String currency_symbol)
 	{
 		this.currencySymbol = currency_symbol;
@@ -166,24 +175,44 @@ public class TextFileField implements Cloneable
 	{
 		return precision;
 	}
-	
+
 	public void setPrecision(int precision)
 	{
 		this.precision = precision;
 	}
-	
+
 	public String getNullString()
 	{
-		return nullString;	
+		return nullString;
 	}
-	
+
 	public void setNullString(String null_string)
 	{
 		this.nullString = null_string;
 	}
-	
+
 	public String toString()
 	{
-		return name+":"+getTypeDesc();
-	}	
+		return name + ":" + getTypeDesc();
+	}
+
+	public int getTrimType()
+	{
+		return trimType;
+	}
+
+	public void setTrimType(int trimType)
+	{
+		this.trimType = trimType;
+	}
+
+	public String getTrimTypeCode()
+	{
+		return TextFileInputMeta.getTrimTypeCode(trimType);
+	}
+
+	public String getTrimTypeDesc()
+	{
+		return TextFileInputMeta.getTrimTypeDesc(trimType);
+	}
 }
