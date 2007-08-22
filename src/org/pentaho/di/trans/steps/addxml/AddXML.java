b1800f12d98dfab9ed3982f9dec7bc4222a770b0
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-22 10:33:08 +0000

    Adding the AddXML step, one of the last 2.x steps
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4680 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/trans/steps/addxml/AddXML.java
@@ -0,0 +1,360 @@
+/***********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+
+package org.pentaho.di.trans.steps.addxml;
+
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleValueException;
+import org.pentaho.di.core.row.RowDataUtil;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStep;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ * Converts input rows to one or more XML files.
+ * 
+ * @author Matt
+ * @since 14-jan-2006
+ */
+public class AddXML extends BaseStep implements StepInterface
+{
+    private AddXMLMeta meta;
+    private AddXMLData data;
+    
+    private DOMImplementation domImplentation; 
+    private Transformer serializer;
+     
+     
+    public AddXML(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
+    {
+        super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+    }
+    
+    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
+    {
+        meta=(AddXMLMeta)smi;
+        data=(AddXMLData)sdi;
+
+        Object[] r = getRow();       // This also waits for a row to be finished.
+        if (r==null)  // no more input to be expected...
+        {
+            setOutputDone();
+            return false;
+        }
+        
+        if (first) 
+        {
+        	first=false;
+        	
+        	data.outputRowMeta = (RowMetaInterface) getInputRowMeta().clone();
+        	meta.getFields(data.outputRowMeta, getStepname(), null, null, this);
+        	
+        	// Cache the fieldname indexes
+        	data.fieldIndexes = new int[meta.getOutputFields().length];
+        	for (int i=0;i<data.fieldIndexes.length;i++)
+        	{
+        		data.fieldIndexes[i] = getInputRowMeta().indexOfValue(meta.getOutputFields()[i].getFieldName());
+        		if (data.fieldIndexes[i]<0) 
+        		{
+        			throw new KettleException(Messages.getString("AddXML.Exception.FieldNotFound"));
+        		}
+        	}
+        }
+        
+        Document xmldoc = getDomImplentation().createDocument(null, meta.getRootNode(), null);
+        Element root = xmldoc.getDocumentElement();
+        for (int i=0;i<meta.getOutputFields().length;i++)
+        {
+            XMLField outputField = meta.getOutputFields()[i];
+            String fieldname = outputField.getFieldName();
+            
+            ValueMetaInterface v = getInputRowMeta().getValueMeta( data.fieldIndexes[i]);
+            Object valueData = r[ data.fieldIndexes[i] ];
+            
+            String value = formatField(v, valueData, outputField);
+
+            String element = outputField.getElementName();
+            if(element == null || element.length() == 0)
+                element = fieldname;
+            
+            if(element == null || element.length() == 0) {
+                throw new KettleException("XML does not allow empty strings for element names.");
+            }
+            if(outputField.isAttribute() ){
+                root.setAttribute(element, value);
+            }
+            else { /* encode as subnode */
+                Element e = xmldoc.createElement(element);
+                Node n = xmldoc.createTextNode(value);
+                e.appendChild(n);
+                root.appendChild(e);
+            }
+        }
+        
+        StringWriter sw = new StringWriter();
+        DOMSource domSource = new DOMSource(xmldoc);
+        try 
+        {
+            this.getSerializer().transform(domSource, new StreamResult(sw));
+        } catch (TransformerException e) {
+            throw new KettleException(e);
+        }
+
+        Object[] outputRowData = RowDataUtil.addValueData(r, getInputRowMeta().size(), sw.toString());
+        
+        putRow(data.outputRowMeta, outputRowData);
+        
+        return true;
+    }
+
+    private String formatField(ValueMetaInterface v, Object valueData, XMLField field) throws KettleValueException
+    {
+        String retval="";
+        if(field == null)
+            return "";
+
+        if(v == null || v.isNull(valueData)) 
+        {
+            String defaultNullValue = field.getNullString();
+            return Const.isEmpty(defaultNullValue) ? "" : defaultNullValue ;
+        }
+
+        if (v.isNumeric())
+        {
+            // Formatting
+            if ( !Const.isEmpty(field.getFormat()) )
+            {
+                data.df.applyPattern(field.getFormat());
+            }
+            else
+            {
+                data.df.applyPattern(data.defaultDecimalFormat.toPattern());
+            }
+            // Decimal 
+            if ( !Const.isEmpty( field.getDecimalSymbol()) )
+            {
+                data.dfs.setDecimalSeparator( field.getDecimalSymbol().charAt(0) );
+            }
+            else
+            {
+                data.dfs.setDecimalSeparator( data.defaultDecimalFormatSymbols.getDecimalSeparator() );
+            }
+            // Grouping
+            if ( !Const.isEmpty( field.getGroupingSymbol()) )
+            {
+                data.dfs.setGroupingSeparator( field.getGroupingSymbol().charAt(0) );
+            }
+            else
+            {
+                data.dfs.setGroupingSeparator( data.defaultDecimalFormatSymbols.getGroupingSeparator() );
+            }
+            // Currency symbol
+            if ( !Const.isEmpty( field.getCurrencySymbol()) ) 
+            {
+                data.dfs.setCurrencySymbol( field.getCurrencySymbol() );
+            }
+            else
+            {
+                data.dfs.setCurrencySymbol( data.defaultDecimalFormatSymbols.getCurrencySymbol() );
+            }
+
+            data.df.setDecimalFormatSymbols(data.dfs);
+
+            if (v.isBigNumber())
+            {
+                retval=data.df.format(v.getBigNumber(valueData));
+            }
+            else if (v.isNumber())
+            {
+                retval=data.df.format(v.getNumber(valueData));
+            }
+            else // Integer
+            {
+                retval=data.df.format(v.getInteger(valueData));
+            }
+        }
+        else
+        if (v.isDate())
+        {
+            if (field!=null && !Const.isEmpty(field.getFormat()) && v.getDate(valueData)!=null)
+            {
+                if (!Const.isEmpty(field.getFormat()))
+                {
+                    data.daf.applyPattern( field.getFormat() );
+                }
+                else
+                {
+                    data.daf.applyPattern( data.defaultDateFormat.toLocalizedPattern() );
+                }
+                data.daf.setDateFormatSymbols(data.dafs);
+                retval= data.daf.format(v.getDate(valueData));
+            }
+            else
+            {
+                if (v.isNull(valueData)) 
+                {
+                    if (field!=null && !Const.isEmpty(field.getNullString()))
+                    {
+                        retval=field.getNullString();
+                    }
+                }
+                else
+                {
+                    retval=v.toString();
+                }
+            }
+        }
+        else
+        if (v.isString())
+        {
+            retval=v.toString();
+        }
+        else if (v.isBinary())
+        {
+            if (v.isNull(valueData))
+            {
+                if (!Const.isEmpty(field.getNullString()))
+                {
+                    retval=field.getNullString();
+                }
+                else
+                {
+                    retval=Const.NULL_BINARY;
+                }
+            }
+            else
+            {                   
+                try 
+                {
+                    retval=new String(v.getBinary(valueData), "UTF-8");
+                } 
+                catch (UnsupportedEncodingException e) 
+                {
+                    // chances are small we'll get here. UTF-8 is
+                    // mandatory.
+                    retval=Const.NULL_BINARY;   
+                }                   
+            }
+        }        
+        else // Boolean
+        {
+            retval=v.toString();
+        }
+
+        return retval;
+    }
+    
+    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta=(AddXMLMeta)smi;
+        data=(AddXMLData)sdi;
+        if(!super.init(smi, sdi))
+            return false;
+
+        try {
+            setSerializer(TransformerFactory.newInstance().newTransformer());
+            setDomImplentation(DocumentBuilderFactory.newInstance().newDocumentBuilder().getDOMImplementation());
+
+            if(meta.getEncoding()!=null) {
+            	getSerializer().setOutputProperty(OutputKeys.ENCODING, meta.getEncoding());
+            }
+            
+            if(meta.isOmitXMLheader()) {
+                getSerializer().setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            }
+        } catch (TransformerConfigurationException e) {
+            return false;
+        } catch (ParserConfigurationException e) {
+            return false;
+        }
+
+        return true;
+    }
+    
+    public void dispose(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta=(AddXMLMeta)smi;
+        data=(AddXMLData)sdi;
+        
+        super.dispose(smi, sdi);
+        
+    }
+    
+    //
+    // Run is were the action happens!
+    public void run()
+    {
+        try
+        {
+            logBasic("Starting to run...");
+            while (processRow(meta, data) && !isStopped())
+                ;
+        }
+        catch(Exception e)
+        {
+            logError("Unexpected error : "+e.toString());
+            logError(Const.getStackTracker(e));
+            setErrors(1);
+            stopAll();
+        }
+        finally
+        {
+            dispose(meta, data);
+            logSummary();
+            markStop();
+        }
+    }
+
+    private void setDomImplentation(DOMImplementation domImplentation) {
+        this.domImplentation = domImplentation;
+    }
+
+    private DOMImplementation getDomImplentation() {
+        return domImplentation;
+    }
+
+    private void setSerializer(Transformer serializer) {
+        this.serializer = serializer;
+    }
+
+    private Transformer getSerializer() {
+        return serializer;
+    }   
+}
---
+++ src/org/pentaho/di/trans/steps/addxml/AddXMLData.java
@@ -0,0 +1,85 @@
+ /**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+
+package org.pentaho.di.trans.steps.addxml;
+
+import java.io.OutputStreamWriter;
+import java.text.DateFormatSymbols;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.zip.ZipOutputStream;
+
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+/**
+ * @author Matt
+ * @since 22-jan-2005
+ */
+public class AddXMLData extends BaseStepData implements StepDataInterface
+{
+	public RowMetaInterface outputRowMeta;
+	
+    public int splitnr;
+
+    public Object[] headerrow;
+    public int fieldnrs[];
+
+    public NumberFormat nf;
+    public DecimalFormat df;
+    public DecimalFormatSymbols dfs;
+    
+    public SimpleDateFormat daf;
+    public DateFormatSymbols dafs;
+
+    public ZipOutputStream zip;
+    
+    public OutputStreamWriter writer;
+
+    public DecimalFormat        defaultDecimalFormat;
+    public DecimalFormatSymbols defaultDecimalFormatSymbols;
+
+    public SimpleDateFormat  defaultDateFormat;
+    public DateFormatSymbols defaultDateFormatSymbols;
+
+	public int[] fieldIndexes;
+
+    /**
+     * 
+     */
+    public AddXMLData()
+    {
+        super();
+        
+        nf = NumberFormat.getInstance();
+        df = (DecimalFormat)nf;
+        dfs=new DecimalFormatSymbols();
+        
+        defaultDecimalFormat = (DecimalFormat)NumberFormat.getInstance();
+        defaultDecimalFormatSymbols =  new DecimalFormatSymbols();
+        
+        daf = new SimpleDateFormat();
+        dafs= new DateFormatSymbols();
+        
+        defaultDateFormat = new SimpleDateFormat();
+        defaultDateFormatSymbols = new DateFormatSymbols();
+        
+    }
+
+}
