ae06044df31ff0f3c37aff822476fdd6a009daf8
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-08-30 19:35:58 +0000

    Cosmetic abort job entry
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4756 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/job/entries/abort/JobEntryAbortDialog.java
+++ src-ui/org/pentaho/di/ui/job/entries/abort/JobEntryAbortDialog.java
@@ -120,14 +120,7 @@ public class JobEntryAbortDialog extends JobEntryDialog implements JobEntryDialo
         int middle = props.getMiddlePct();
         int margin = Const.MARGIN;
 
-        wOK = new Button(shell, SWT.PUSH);
-        wOK.setText(Messages.getString("System.Button.OK"));
-        wCancel = new Button(shell, SWT.PUSH);
-        wCancel.setText(Messages.getString("System.Button.Cancel"));
-
-        // at the bottom
-        BaseStepDialog.positionBottomButtons(shell, new Button[] { wOK, wCancel }, margin, null);
-
+        
         // Filename line
         wlName = new Label(shell, SWT.RIGHT);
         wlName.setText(Messages.getString("JobEntryAbortDialog.Label"));
@@ -165,6 +158,15 @@ public class JobEntryAbortDialog extends JobEntryDialog implements JobEntryDialo
 		fdMessageAbort.top = new FormAttachment(wName, margin);
 		fdMessageAbort.right = new FormAttachment(100, 0);
 		wMessageAbort.setLayoutData(fdMessageAbort);
+		
+		wOK = new Button(shell, SWT.PUSH);
+        wOK.setText(Messages.getString("System.Button.OK"));
+        wCancel = new Button(shell, SWT.PUSH);
+        wCancel.setText(Messages.getString("System.Button.Cancel"));
+
+        // at the bottom
+        BaseStepDialog.positionBottomButtons(shell, new Button[] { wOK, wCancel }, margin, wMessageAbort);
+
 	
 		// Add listeners
         lsCancel = new Listener()
@@ -209,6 +211,7 @@ public class JobEntryAbortDialog extends JobEntryDialog implements JobEntryDialo
         getData();
 
         BaseStepDialog.setSize(shell);
+        
 
         shell.open();
         props.setDialogSize(shell, "JobAbortDialogSize");
