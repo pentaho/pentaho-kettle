ba1e19c19f066b415d45ffe50961e9a4a79cbbb7
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-09-07 19:52:42 +0000

    Fixed bug in jar 'explosion' with the nested jars
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4852 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/plugins/PluginLoader.java
+++ src/org/pentaho/di/core/plugins/PluginLoader.java
@@ -116,8 +116,8 @@ public class PluginLoader
 								{
 									boolean isAlsoJar = childFolder.getName().getURI().endsWith(".jar");
 
-									// ignore anything that is not a folder
-									if( childFolder.getType() != FileType.FOLDER ) {
+									// ignore anything that is not a folder or a jar
+									if(!isAlsoJar && childFolder.getType() != FileType.FOLDER) {
 										continue;
 									}
 
@@ -206,12 +206,11 @@ public class PluginLoader
 				{
 					Node libnode = XMLHandler.getSubNodeByNr(libsnode, "library", j);
 					String jarfile = XMLHandler.getTagAttribute(libnode, "name");
-					jarfiles[j] = parent.getChild(jarfile).getURL().getFile();
-					// System.out.println("jar files=" + jarfiles[j]);
+					jarfiles[j] = parent.resolveFile(jarfile).getURL().getFile();
+				//	System.out.println("jar files=" + jarfiles[j]);
 				}
 
-				String iconFilename = parent.getChild(iconfile).getURL().getFile();
-				// System.out.println("iconfile=" + iconFilename);
+				String iconFilename = parent.resolveFile(iconfile).getURL().getFile();
 				JobPlugin sp = new JobPlugin(JobPlugin.TYPE_PLUGIN, id, description, tooltip, parent
 						.getName().getURI(), jarfiles, iconFilename, classname);
 
