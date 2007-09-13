d076a3197d9c61541dafc856a8dc3663dfa382f1
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-09-13 10:40:56 +0000

    Jira PDI-157
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@4949 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/scriptvalues_mod/ScriptValuesAddedFunctions.java
+++ src/org/pentaho/di/trans/steps/scriptvalues_mod/ScriptValuesAddedFunctions.java
@@ -88,7 +88,7 @@ public class ScriptValuesAddedFunctions extends ScriptableObject {
         "rpad", "week", "month", "year", "str2RegExp","fileExists", "touch", "isRegExp", "date2str",
         "str2date", "sendMail", "replace", "decode", "isNum","isDate", "lower", "upper", "str2num",
         "num2str", "Alert", "setEnvironmentVar", "getEnvironmentVar", "LoadScriptFile", "LoadScriptFromTab", 
-        "print", "println", "resolveIP", "trim", "substr", "getVariable", "setVariable" 
+        "print", "println", "resolveIP", "trim", "substr", "getVariable", "setVariable" ,"LuhnCheck",
         };
 	
 	// This is only used for reading, so no concurrency problems.
@@ -100,6 +100,71 @@ public class ScriptValuesAddedFunctions extends ScriptableObject {
 	// fisc_date, isNull
 	// 
 	
+	
+
+	public static boolean LuhnCheck(Context actualContext, Scriptable actualObject, Object[] ArgList, Function FunctionContext){
+
+	    boolean retour=false;
+	    
+		if(ArgList.length==1)
+		{
+			if (!isNull(ArgList) && !isUndefined(ArgList))
+			{
+			    int sum = 0;
+			    int digit = 0;
+			    int addend = 0;
+			    boolean timesTwo = false;
+				String digitsOnly = getDigitsOnly (Context.toString(ArgList[0]));
+	
+		
+			    for (int i = digitsOnly.length () - 1; i >= 0; i--) 
+			    {
+			      digit = Integer.parseInt (digitsOnly.substring (i, i + 1));
+			      if (timesTwo) {
+			        addend = digit * 2;
+			        if (addend > 9) 
+			        {
+			          addend -= 9;
+			        }
+			      }
+			      else 
+			      {
+			        addend = digit;
+			      }
+			      sum += addend;
+			      timesTwo = !timesTwo;
+		      
+			      int modulus = sum % 10;
+			      
+			     if(modulus==0) retour=true;
+			      
+			      }
+			    }
+			    
+		}
+		else
+		{
+			throw Context.reportRuntimeError("The function call LuhnCheck requires 1 argument.");
+
+		}
+		return retour;
+	}
+	
+	 //--------------------------------
+	  // Filter out non-digit characters
+	  //--------------------------------
+
+	  private static String getDigitsOnly (String s) {
+	    StringBuffer digitsOnly = new StringBuffer ();
+	    char c;
+	    for (int i = 0; i < s.length (); i++) {
+	      c = s.charAt (i);
+	      if (Character.isDigit (c)) {
+	        digitsOnly.append (c);
+	      }
+	    }
+	    return digitsOnly.toString ();
+	  }	
 	public static Object getTransformationName(Context actualContext, Scriptable actualObject, Object[] ArgList, Function FunctionContext){
 		try{
 			Object objTranName = Context.toString(actualObject.get("_TransformationName_", actualObject));
