910914e2360c271be36d01a96bdab66e3f4581c0
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-10-01 19:56:26 +0000

    First import
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5248 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/core/plugins/Plugin.java
@@ -0,0 +1,146 @@
+package org.pentaho.di.core.plugins;
+
+
+public class Plugin<E>
+{
+	public static final String NAME = "name"; //$NON-NLS-1$
+
+	public static final String LIBRARY = "library"; //$NON-NLS-1$
+
+	public static final String LIBRARIES = "libraries"; //$NON-NLS-1$
+
+	public static final String LOCALIZED_TOOLTIP = "localized_tooltip"; //$NON-NLS-1$
+
+	public static final String LOCALIZED_DESCRIPTION = "localized_description"; //$NON-NLS-1$
+
+	public static final String LOCALE = "locale"; //$NON-NLS-1$
+
+	public static final String ICONFILE = "iconfile"; //$NON-NLS-1$
+
+	public static final String ID = "id"; //$NON-NLS-1$
+
+	public static final String DESCRIPTION = "description"; //$NON-NLS-1$
+
+	public static final String CLASSNAME = "classname"; //$NON-NLS-1$
+
+	public static final String ERRORHELPFILE = "errorhelpfile"; //$NON-NLS-1$
+
+	public static final String LOCALIZED_CATEGORY = "localized_category"; //$NON-NLS-1$
+
+	public static final String TOOLTIP = "tooltip"; //$NON-NLS-1$
+
+	public static final String CATEGORY = "category"; //$NON-NLS-1$
+
+	public static final String PLUGIN = "plugin"; //$NON-NLS-1$
+
+	public static final String PLUGIN_XML_FILE = "plugin.xml"; //$NON-NLS-1$
+
+	public static final String PLUGIN_LOADER = "PluginLoader"; //$NON-NLS-1$
+	
+	public static final int TYPE_ALL = 0;
+
+	public static final int TYPE_NATIVE = 1;
+
+	public static final int TYPE_PLUGIN = 2;
+
+	private int type;
+
+	private E id;
+
+	protected String description;
+
+	private String tooltip;
+
+	private String directory;
+
+	private String jarfiles[];
+
+	private String icon_filename;
+
+	private String classname;
+	
+	private ClassLoader classLoader;
+	
+
+	public Plugin(int type, E id, String description, String tooltip, String directory,
+			String jarfiles[], String icon_filename, String classname)
+	{
+		this.type = type;
+		this.id = id;
+		this.description = description;
+		this.tooltip = tooltip;
+		this.directory = directory;
+		this.jarfiles = jarfiles;
+		this.icon_filename = icon_filename;
+		this.classname = classname;
+	}
+
+	public int getType()
+	{
+		return type;
+	}
+
+	public boolean isNative()
+	{
+		return type == TYPE_NATIVE;
+	}
+
+	public boolean isPlugin()
+	{
+		return type == TYPE_PLUGIN;
+	}
+
+	/**
+	 * @return The ID (code String) of the job or job-plugin.
+	 */
+	public E getID()
+	{
+		return id;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public String getTooltip()
+	{
+		return tooltip;
+	}
+
+	public String getDirectory()
+	{
+		return directory;
+	}
+
+	public String[] getJarfiles()
+	{
+		return jarfiles;
+	}
+
+	public String getIconFilename()
+	{
+		return icon_filename;
+	}
+
+	public void setIconFilename(String filename)
+	{
+		icon_filename = filename;
+	}
+
+	public String getClassname()
+	{
+		return classname;
+	}
+
+	public ClassLoader getClassLoader()
+	{
+		return classLoader;
+	}
+
+	public void setClassLoader(ClassLoader classLoader)
+	{
+		this.classLoader = classLoader;
+	}
+
+}
