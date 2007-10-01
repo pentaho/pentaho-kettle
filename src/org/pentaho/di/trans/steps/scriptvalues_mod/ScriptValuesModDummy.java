f02c393d98aba33f649fd2121eab2c93b96416d0
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-10-01 20:18:03 +0000

    Split of dummy class for test procedure in the dialog.  We need it here at runtime though because it's referenced in the functions.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5250 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/trans/steps/scriptvalues_mod/ScriptValuesModDummy.java
@@ -0,0 +1,204 @@
+package org.pentaho.di.trans.steps.scriptvalues_mod;
+
+import java.util.List;
+import java.util.Map;
+
+import org.pentaho.di.core.RowSet;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.trans.step.RowListener;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMetaInterface;
+	/**
+	 * Dummy class used for test().
+	 */
+	public class ScriptValuesModDummy implements StepInterface, ScriptValuesModInterface
+	{
+		private RowMetaInterface inputRowMeta;
+		private RowMetaInterface outputRowMeta;
+
+		public ScriptValuesModDummy(RowMetaInterface inputRowMeta, RowMetaInterface outputRowMeta) {
+			this.inputRowMeta = inputRowMeta;
+			this.outputRowMeta = outputRowMeta;
+		}
+		public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
+			return false;
+		}
+
+		public void addRowListener(RowListener rowListener) {
+		}
+
+		public void dispose(StepMetaInterface sii, StepDataInterface sdi) {
+		}
+
+		public long getErrors() {
+			return 0;
+		}
+
+		public List<RowSet> getInputRowSets() {
+			return null;
+		}
+
+		public long getLinesInput() {
+			return 0;
+		}
+
+		public long getLinesOutput() {
+			return 0;
+		}
+
+		public long getLinesRead() {
+			return 0;
+		}
+
+		public long getLinesUpdated() {
+			return 0;
+		}
+
+		public long getLinesWritten() {
+			return 0;
+		}
+
+		public long getLinesRejected() {
+			return 0;
+		}		
+		
+		public List<RowSet> getOutputRowSets() {
+			return null;
+		}
+
+		public String getPartitionID() {
+			return null;
+		}
+
+		public Object[] getRow() throws KettleException {
+			return null;
+		}
+
+		public List<RowListener> getRowListeners() {
+			return null;
+		}
+
+		public String getStepID() {
+			return null;
+		}
+
+		public String getStepname() {
+			return null;
+		}
+
+		public boolean init(StepMetaInterface stepMetaInterface, StepDataInterface stepDataInterface) {
+			return false;
+		}
+
+		public boolean isAlive() {
+			return false;
+		}
+
+		public boolean isPartitioned() {
+			return false;
+		}
+
+		public boolean isStopped() {
+			return false;
+		}
+
+		public void markStart() {
+		}
+
+		public void markStop() {
+		}
+
+		public void putRow(RowMetaInterface rowMeta, Object[] row) throws KettleException {
+		}
+
+		public void removeRowListener(RowListener rowListener) {
+		}
+
+		public void run() {
+		}
+
+		public void setErrors(long errors) {
+		}
+
+		public void setOutputDone() {
+		}
+
+		public void setPartitionID(String partitionID) {
+		}
+
+		public void start() {
+		}
+
+		public void stopAll() {
+		}
+
+		public void stopRunning(StepMetaInterface stepMetaInterface, StepDataInterface stepDataInterface) throws KettleException {
+		}
+
+		public void cleanup() {
+		}
+
+		public void pauseRunning() {
+		}
+
+		public void resumeRunning() {
+		}
+
+		public void copyVariablesFrom(VariableSpace space) {
+		}
+
+		public String environmentSubstitute(String aString) {
+			return null;
+		}
+
+		public String[] environmentSubstitute(String[] string) {
+			return null;
+		}
+
+		public boolean getBooleanValueOfVariable(String variableName, boolean defaultValue) {
+			return false;
+		}
+
+		public VariableSpace getParentVariableSpace() {
+			return null;
+		}
+		
+		public void setParentVariableSpace(VariableSpace parent) 
+		{
+		}
+
+		public String getVariable(String variableName, String defaultValue) {
+			return defaultValue;
+		}
+
+		public String getVariable(String variableName) {
+			return null;
+		}
+
+		public void initializeVariablesFrom(VariableSpace parent) {
+		}
+
+		public void injectVariables(Map<String, String> prop) {
+		}
+
+		public String[] listVariables() {
+			return null;
+		}
+
+		public void setVariable(String variableName, String variableValue) {
+		}
+
+		public void shareVariablesWith(VariableSpace space) {
+		}	
+		
+		public RowMetaInterface getInputRowMeta() {
+			return inputRowMeta;
+		}
+
+		public RowMetaInterface getOutputRowMeta() {
+			return outputRowMeta;
+		}
+	}
