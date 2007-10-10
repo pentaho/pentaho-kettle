624f22e1cef32689f5c9d25f5f7a5eef60b339a7
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-10-10 19:40:21 +0000

    PDI-287 : Merge Join stalls in certain situations
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5371 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/mergejoin/MergeJoin.java
+++ src/org/pentaho/di/trans/steps/mergejoin/MergeJoin.java
@@ -164,6 +164,12 @@ public class MergeJoin extends BaseStep implements StepInterface
         	(data.one == null && data.one_optional == false) ||
         	(data.two == null && data.two_optional == false))
         {
+        	// Before we stop processing, we have to make sure that all rows from both input streams are depleted!
+        	// If we don't do this, the transformation can stall.
+        	//
+        	while (data.one!=null && !isStopped()) data.one = getRowFrom(data.oneRowSet);
+        	while (data.two!=null && !isStopped()) data.two = getRowFrom(data.twoRowSet);
+        	
             setOutputDone();
             return false;
         }
@@ -281,6 +287,12 @@ public class MergeJoin extends BaseStep implements StepInterface
         			 * If we are doing right outer join then we are done since
         			 * there are no more rows in the second set
         			 */
+                	// Before we stop processing, we have to make sure that all rows from both input streams are depleted!
+                	// If we don't do this, the transformation can stall.
+                	//
+                	while (data.one!=null && !isStopped()) data.one = getRowFrom(data.oneRowSet);
+                	while (data.two!=null && !isStopped()) data.two = getRowFrom(data.twoRowSet);
+
         			setOutputDone();
         			return false;
         		}
@@ -338,6 +350,12 @@ public class MergeJoin extends BaseStep implements StepInterface
         			 * We are doing a left outer join and there are no more rows
         			 * in the first stream; so we are done
         			 */
+                	// Before we stop processing, we have to make sure that all rows from both input streams are depleted!
+                	// If we don't do this, the transformation can stall.
+                	//
+                	while (data.one!=null && !isStopped()) data.one = getRowFrom(data.oneRowSet);
+                	while (data.two!=null && !isStopped()) data.two = getRowFrom(data.twoRowSet);
+
         			setOutputDone();
         			return false;
         		}
