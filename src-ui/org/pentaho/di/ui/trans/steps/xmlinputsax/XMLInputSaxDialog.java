5825875ceb060f4693e105c98518d2beb82b8c5b
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-10-26 19:24:59 +0000

    SEMINOLE-196: Fixed positioning and width of top table, fields tab.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5606 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/trans/steps/xmlinputsax/XMLInputSaxDialog.java
+++ src-ui/org/pentaho/di/ui/trans/steps/xmlinputsax/XMLInputSaxDialog.java
@@ -568,13 +568,13 @@ public class XMLInputSaxDialog extends BaseStepDialog implements StepDialogInter
 		fdAttributes = new FormData();
 		fdAttributes.left = new FormAttachment(0, 0);
 		fdAttributes.top = new FormAttachment(0, 0);
-		fdAttributes.right = new FormAttachment(20, 0);
+		fdAttributes.right = new FormAttachment(100, 0);
 		fdAttributes.bottom = new FormAttachment(15, 0);
 		wAttributes.setLayoutData(fdAttributes);
 
 		fdFields = new FormData();
 		fdFields.left = new FormAttachment(0, 0);
-		fdFields.top = new FormAttachment(wAttributes);
+		fdFields.top = new FormAttachment(wAttributes,margin*3);
 		fdFields.right = new FormAttachment(100, 0);
 		fdFields.bottom = new FormAttachment(wGet, -margin);
 		wFields.setLayoutData(fdFields);
