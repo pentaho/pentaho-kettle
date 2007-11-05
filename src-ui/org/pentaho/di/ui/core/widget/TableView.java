5f7d2524436ac5128081532af967317c03949d1e
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-05 19:12:14 +0000

    Problem with setting the width of a table column on OSX
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5688 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/widget/TableView.java
+++ src-ui/org/pentaho/di/ui/core/widget/TableView.java
@@ -1971,7 +1971,9 @@ public class TableView extends Composite
         int colmax = tablecolumn[colnr].getWidth(); 
         if (strmax>colmax) 
         {
-            tablecolumn[colnr].setWidth(strmax+20);
+            if (Const.isOSX()) strmax*=1.4;
+            tablecolumn[colnr].setWidth(strmax+30);
+            
             // On linux, this causes the text to select everything...
             // This is because the focus is lost and re-gained.  Nothing we can do about it now.
             
@@ -2196,7 +2198,7 @@ public class TableView extends Composite
 			try
 			{
                 int extra = 30;
-                if (Const.isOSX()) max*=1.3;
+                if (Const.isOSX()) max*=1.4;
                 tc.setWidth(max+extra);
 			}
 			catch(Exception e) {}
