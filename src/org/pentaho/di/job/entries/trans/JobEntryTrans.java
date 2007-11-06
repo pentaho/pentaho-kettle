1405f735f4f70925abf24601204706b9b4b0148a
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-06 20:07:54 +0000

    Very likely fix for SEMINOLE-171 : Environment Variables not being picked up By Custom Steps.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5708 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/entries/trans/JobEntryTrans.java
+++ src/org/pentaho/di/job/entries/trans/JobEntryTrans.java
@@ -1,4 +1,4 @@
- /* Copyright © 2007 Pentaho Corporation.  All rights reserved. 
+ /* Copyright ï¿½ 2007 Pentaho Corporation.  All rights reserved. 
  * This software was developed by Pentaho Corporation and is provided under the terms 
  * of the GNU Lesser General Public License, Version 2.1. You may not use 
  * this file except in compliance with the license. If you need a copy of the license, 
@@ -542,6 +542,15 @@ public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryIn
                     executionConfiguration.setClusterShowingTransformation(false);
                     executionConfiguration.setSafeModeEnabled(false);
                     executionConfiguration.setRepository(rep);
+                    
+                    // Also pass the variables from the transformation into the execution configuration
+                    // That way it can go over the HTTP connection to the slave server.
+                    //
+                    executionConfiguration.setVariables(transMeta);
+                    
+                    // Also set the arguments...
+                    //
+                    executionConfiguration.setArgumentStrings(args);
                     
                     TransSplitter transSplitter = Trans.executeClustered(transMeta, executionConfiguration );
                     
