30289873b33a5c1c8cbcd85c3443766525e052e3
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-07 01:37:45 +0000

    Fix part of PDI-299 : read empty variables that are passed over XML.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5720 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/TransExecutionConfiguration.java
+++ src/org/pentaho/di/trans/TransExecutionConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2007 Pentaho Corporation.  All rights reserved. 
+ * Copyright ï¿½ 2007 Pentaho Corporation.  All rights reserved. 
  * This software was developed by Pentaho Corporation and is provided under the terms 
  * of the GNU Lesser General Public License, Version 2.1. You may not use 
  * this file except in compliance with the license. If you need a copy of the license, 
@@ -529,8 +529,8 @@ public class TransExecutionConfiguration implements Cloneable
         	Node argNode = XMLHandler.getSubNodeByNr(varsNode, "variable", i);
         	String name = XMLHandler.getTagValue(argNode, "name");
         	String value = XMLHandler.getTagValue(argNode, "value");
-        	if (!Const.isEmpty(name) && !Const.isEmpty(value)) {
-        		variables.put(name, value);
+        	if (!Const.isEmpty(name) ) {
+        		variables.put(name, Const.NVL(value, ""));
         	}
         }
         
