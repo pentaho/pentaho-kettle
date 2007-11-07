2716eec25e9450782ebaa078d32a6510d2fa6ef1
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-07 02:23:17 +0000

    Fix for PDI-371 : Environments variables from DB config dialog are not set after starting transformation
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5721 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/databasejoin/DatabaseJoin.java
+++ src/org/pentaho/di/trans/steps/databasejoin/DatabaseJoin.java
@@ -1,4 +1,4 @@
- /* Copyright © 2007 Pentaho Corporation.  All rights reserved. 
+ /* Copyright ï¿½ 2007 Pentaho Corporation.  All rights reserved. 
  * This software was developed by Pentaho Corporation and is provided under the terms 
  * of the GNU Lesser General Public License, Version 2.1. You may not use 
  * this file except in compliance with the license. If you need a copy of the license, 
@@ -202,6 +202,8 @@ public class DatabaseJoin extends BaseStep implements StepInterface
 		if (super.init(smi, sdi))
 		{
 			data.db=new Database(meta.getDatabaseMeta());
+			data.db.shareVariablesWith(this);
+			
 			try
 			{
                 if (getTransMeta().isUsingUniqueConnections())
