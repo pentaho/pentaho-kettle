75df19755eb61165b2741046f58b3be79d3b183f
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-11 19:13:39 +0000

    Fix for PDI-378 : Problem using the additional error handling columns (error description, error code)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5741 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/step/BaseStep.java
+++ src/org/pentaho/di/trans/step/BaseStep.java
@@ -1085,7 +1085,7 @@ public class BaseStep extends Thread implements VariableSpace
         }
         
         Object[] errorRowData = RowDataUtil.allocateRowData(errorRowMeta.size());
-        System.arraycopy(row, 0, errorRowData, 0, errorRowMeta.size());
+        System.arraycopy(row, 0, errorRowData, 0, rowMeta.size());
         
         // Also add the error fields...
         stepErrorMeta.addErrorRowData(errorRowData, rowMeta.size(), nrErrors, errorDescriptions, fieldNames, errorCodes);
