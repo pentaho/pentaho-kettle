e571a068af52fb70d56b84a3fa5ede3efeb4c492
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-15 14:02:31 +0000

    Fix for PDI-466: Oracle Bulk loader producing incorrect config file for DateTime in special circumstances
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5784 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/orabulkloader/OraBulkLoader.java
+++ src/org/pentaho/di/trans/steps/orabulkloader/OraBulkLoader.java
@@ -201,11 +201,11 @@ public class OraBulkLoader extends BaseStep implements StepInterface
 			    case ValueMetaInterface.TYPE_BIGNUMBER:
 			    	break;
 			    case ValueMetaInterface.TYPE_DATE:			    	
-			    	if ( OraBulkLoaderMeta.DATE_MASK_DATE.equals(dateMask[pos]) )
+			    	if ( OraBulkLoaderMeta.DATE_MASK_DATE.equals(dateMask[i]) )
 			    	{
 			    	    contents.append(" DATE 'yyyy-mm-dd'");	
 			    	}
-			    	else if ( OraBulkLoaderMeta.DATE_MASK_DATETIME.equals(dateMask[pos]) )
+			    	else if ( OraBulkLoaderMeta.DATE_MASK_DATETIME.equals(dateMask[i]) )
 			    	{
 			    		contents.append(" TIMESTAMP 'yyyy-mm-dd hh24:mi:ss.ff'");
 			    	}			    	
