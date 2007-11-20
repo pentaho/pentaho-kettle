0194aaa9692824c1b07df4ac5d94f864ad2f83fc
asilva <asilva@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-11-20 20:13:47 +0000

    New method to get sub node with a specific attr name and value
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@5852 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/xml/XMLHandler.java
+++ src/org/pentaho/di/core/xml/XMLHandler.java
@@ -307,6 +307,35 @@ public class XMLHandler
 		}
 		return null;
 	}
+	
+	/**
+	 * Get node child with a certain subtag set to a certain value
+	 * @param n The node to search in
+	 * @param tag The tag to look for
+	 * @param subtag The subtag to look for
+	 * @param subtagvalue The value the subtag should have
+	 * @param nr The nr of occurance of the value
+	 * @return The node found or null if we couldn't find anything.
+	 */
+	public static final Node getNodeWithAttributeValue(Node n, String tag, String attributeName,String attributeValue)
+	{
+		NodeList children;
+		Node childnode;
+		
+		children=n.getChildNodes();
+		for (int i=0;i<children.getLength();i++)
+		{
+			childnode=children.item(i);
+			if (childnode.getNodeName().equalsIgnoreCase(tag))  // <hop>
+			{
+				Node attribute = childnode.getAttributes().getNamedItem(attributeName);
+				
+				if (attribute!=null && attributeValue.equals(attribute.getTextContent()))
+					return childnode;
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Get the n'th node with a certain tag and where 2 tags are set as specified.
