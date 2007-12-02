581b1c8af7ab64dc4c388085c016f5b454c0773b
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-12-02 22:25:05 +0000

    Fix for PDI-473: Table Output retrieve key fails
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6025 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/tableoutput/TableOutput.java
+++ src/org/pentaho/di/trans/steps/tableoutput/TableOutput.java
@@ -66,6 +66,7 @@ public class TableOutput extends BaseStep implements StepInterface
         {
             first=false;
             data.outputRowMeta = getInputRowMeta().clone();
+            meta.getFields(data.outputRowMeta, getStepname(), null, null, this);
             data.insertRowMeta = getInputRowMeta().clone();
         }
         
