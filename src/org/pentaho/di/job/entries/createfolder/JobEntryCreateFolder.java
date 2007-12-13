997b46b4bef769428b6d9b5e91ec743538a0aa9f
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-12-13 17:09:33 +0000

    Create folder & folderisempty job entry
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6132 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/job/entries/createfolder/JobEntryCreateFolder.java
@@ -0,0 +1,256 @@
+ /**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+package org.pentaho.di.job.entries.createfolder;
+
+import static org.pentaho.di.job.entry.validator.AbstractFileValidator.putVariableSpace;
+import static org.pentaho.di.job.entry.validator.AndValidator.putValidators;
+import static org.pentaho.di.job.entry.validator.JobEntryValidatorUtils.andValidator;
+import static org.pentaho.di.job.entry.validator.JobEntryValidatorUtils.fileDoesNotExistValidator;
+import static org.pentaho.di.job.entry.validator.JobEntryValidatorUtils.notNullValidator;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pentaho.di.cluster.SlaveServer;
+import org.pentaho.di.core.CheckResultInterface;
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleDatabaseException;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.logging.LogWriter;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.job.JobEntryType;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.entries.createfile.JobEntryCreateFile;
+import org.pentaho.di.job.entry.JobEntryBase;
+import org.pentaho.di.job.entry.JobEntryInterface;
+import org.pentaho.di.job.entry.validator.ValidatorContext;
+import org.pentaho.di.repository.Repository;
+import org.w3c.dom.Node;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileType;
+
+
+/**
+ * This defines a 'create folder' job entry. Its main use would be to create empty folder
+ * that can be used to control the flow in ETL cycles.
+ * 
+ * @author Sven/Samatar
+ * @since 18-10-2007
+ *
+ */
+public class JobEntryCreateFolder extends JobEntryBase implements Cloneable, JobEntryInterface
+{
+	private String foldername;
+	private boolean failIfFolderExists;
+	
+	public JobEntryCreateFolder(String n)
+	{
+		super(n, "");
+		foldername=null;
+		failIfFolderExists=true;
+		setID(-1L);
+		setJobEntryType(JobEntryType.CREATE_FOLDER);
+	}
+
+	public JobEntryCreateFolder()
+	{
+		this("");
+	}
+
+	public JobEntryCreateFolder(JobEntryBase jeb)
+	{
+		super(jeb);
+	}
+
+    public Object clone()
+    {
+        JobEntryCreateFolder je = (JobEntryCreateFolder) super.clone();
+        return je;
+    }
+    
+	public String getXML()
+	{
+        StringBuffer retval = new StringBuffer(50);
+		
+		retval.append(super.getXML());		
+		retval.append("      ").append(XMLHandler.addTagValue("foldername",   foldername));
+		retval.append("      ").append(XMLHandler.addTagValue("fail_if_Folder_exists", failIfFolderExists));
+		
+		return retval.toString();
+	}
+	
+	  public void loadXML(Node entrynode, List<DatabaseMeta> databases, List<SlaveServer> slaveServers, Repository rep) throws KettleXMLException
+	  {
+		try
+		{
+			super.loadXML(entrynode, databases, slaveServers);
+			foldername = XMLHandler.getTagValue(entrynode, "foldername");
+			failIfFolderExists = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "fail_if_Folder_exists"));
+		}
+		catch(KettleXMLException xe)
+		{
+			throw new KettleXMLException("Unable to load job entry of type 'create folder' from XML node", xe);
+		}
+	}
+
+	  public void loadRep(Repository rep, long id_jobentry, List<DatabaseMeta> databases, List<SlaveServer> slaveServers) throws KettleException
+	  {
+		try
+		{
+			super.loadRep(rep, id_jobentry, databases, slaveServers);
+			foldername = rep.getJobEntryAttributeString(id_jobentry, "foldername");
+			failIfFolderExists = rep.getJobEntryAttributeBoolean(id_jobentry, "fail_if_Folder_exists");
+		}
+		catch(KettleException dbe)
+		{
+			throw new KettleException("Unable to load job entry of type 'create Folder' from the repository for id_jobentry="+id_jobentry, dbe);
+		}
+	}
+	
+	public void saveRep(Repository rep, long id_job)
+		throws KettleException
+	{
+		try
+		{
+			super.saveRep(rep, id_job);
+			
+			rep.saveJobEntryAttribute(id_job, getID(), "foldername", foldername);
+            rep.saveJobEntryAttribute(id_job, getID(), "fail_if_Folder_exists", failIfFolderExists);
+		}
+		catch(KettleDatabaseException dbe)
+		{
+			throw new KettleException("Unable to save job entry of type 'create Folder' to the repository for id_job="+id_job, dbe);
+		}
+	}
+
+	public void setFoldername(String foldername)
+	{
+		this.foldername = foldername;
+	}
+	
+	public String getFoldername()
+	{
+		return foldername;
+	}
+    
+    public String getRealFoldername()
+    {
+        return environmentSubstitute(getFoldername());
+    }
+	
+	public Result execute(Result previousResult, int nr, Repository rep, Job parentJob)
+	{
+		LogWriter log = LogWriter.getInstance();
+		Result result = previousResult;
+		result.setResult( false );
+		
+	
+		if (foldername!=null)
+		{
+            String realFoldername = getRealFoldername();
+            FileObject FolderObject = null;
+			try {
+				FolderObject = KettleVFS.getFileObject(realFoldername);
+
+				if ( FolderObject.exists() )
+				{
+					boolean isFolder=false;
+					
+					//Check if it's a folder
+					if(FolderObject.getType() == FileType.FOLDER)  isFolder=true;
+					
+						if ( isFailIfFolderExists() )
+						{
+							// Folder exists and fail flag is on.
+						    result.setResult( false );
+						    if(isFolder)
+						    	log.logError(toString(), "Folder ["+realFoldername+"] exists, failing.");
+						    else
+						    	log.logError(toString(), "File ["+realFoldername+"] exists, failing.");
+						}
+						else
+						{
+							// Folder already exists, no reason to try to create it
+						    result.setResult( true );
+						    log.logBasic(toString(), "Folder ["+realFoldername+"] already exists, not recreating.");
+						}
+				
+					
+				}
+				else
+				{
+					//  No Folder yet, create an empty Folder.
+					FolderObject.createFolder();					
+					log.logBasic(toString(), "Folder ["+realFoldername+"] created!");
+					result.setResult( true );
+				}
+			} catch (IOException e) {
+				log.logError(toString(), "Could not create Folder ["+realFoldername+"], exception: " + e.getMessage());
+				result.setResult( false );
+				result.setNrErrors(1);					
+			}
+            finally {
+            	if ( FolderObject != null )
+            	{
+            		try  {
+            		     FolderObject.close();
+            		}
+            		catch ( IOException ex ) {};
+            	}
+            }			
+		}
+		else
+		{			
+			log.logError(toString(), "No Foldername is defined.");
+		}
+		
+		return result;
+	}
+
+	public boolean evaluates()
+	{
+		return true;
+	}
+    
+
+
+	public boolean isFailIfFolderExists() {
+		return failIfFolderExists;
+	}
+
+	public void setFailIfFolderExists(boolean failIfFolderExists) {
+		this.failIfFolderExists = failIfFolderExists;
+	}
+	
+	public static void main(String[] args)
+	  {
+	    List<CheckResultInterface> remarks = new ArrayList<CheckResultInterface>();
+	    new JobEntryCreateFile().check(remarks, null);
+	    System.out.printf("Remarks: %s\n", remarks);
+	  }
+
+	  public void check(List<CheckResultInterface> remarks, JobMeta jobMeta)
+	  {
+	    ValidatorContext ctx = new ValidatorContext();
+	    putVariableSpace(ctx, getVariables());
+	    putValidators(ctx, notNullValidator(), fileDoesNotExistValidator());
+	    andValidator().validate(this, "filename", remarks, ctx); //$NON-NLS-1$
+	  }
+}
---
+++ src/org/pentaho/di/job/entries/folderisempty/JobEntryFolderIsEmpty.java
@@ -0,0 +1,399 @@
+ /**********************************************************************
+ **                                                                   **
+ **               This code belongs to the KETTLE project.            **
+ **                                                                   **
+ ** Kettle, from version 2.2 on, is released into the public domain   **
+ ** under the Lesser GNU Public License (LGPL).                       **
+ **                                                                   **
+ ** For more details, please read the document LICENSE.txt, included  **
+ ** in this project                                                   **
+ **                                                                   **
+ ** http://www.kettle.be                                              **
+ ** info@kettle.be                                                    **
+ **                                                                   **
+ **********************************************************************/
+ 
+package org.pentaho.di.job.entries.folderisempty;
+
+import static org.pentaho.di.job.entry.validator.AndValidator.putValidators;
+import static org.pentaho.di.job.entry.validator.JobEntryValidatorUtils.andValidator;
+import static org.pentaho.di.job.entry.validator.JobEntryValidatorUtils.notBlankValidator;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.w3c.dom.Node;
+
+import org.pentaho.di.cluster.SlaveServer;
+import org.pentaho.di.core.CheckResultInterface;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleDatabaseException;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.logging.LogWriter;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.job.JobEntryType;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.entry.JobEntryBase;
+import org.pentaho.di.job.entry.JobEntryInterface;
+import org.pentaho.di.repository.Repository;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.FileSelector;
+import org.apache.commons.vfs.FileSelectInfo;
+
+
+/**
+ * This defines a 'create folder' job entry. Its main use would be to create empty folder
+ * that can be used to control the flow in ETL cycles.
+ * 
+ * @author Sven/Samatar
+ * @since 18-10-2007
+ *
+ */
+public class JobEntryFolderIsEmpty extends JobEntryBase implements Cloneable, JobEntryInterface
+{
+	private String foldername;
+	private int filescount;
+	private int folderscount;
+	private boolean includeSubfolders;
+	private boolean specifywilcard;
+	private String wildcard;
+	
+	public JobEntryFolderIsEmpty(String n)
+	{
+		super(n, "");
+		foldername=null;
+		wildcard=null;
+		includeSubfolders=false;
+		specifywilcard=false;
+		setID(-1L);
+		setJobEntryType(JobEntryType.FOLDER_IS_EMPTY);
+	}
+
+	public JobEntryFolderIsEmpty()
+	{
+		this("");
+	}
+
+	public JobEntryFolderIsEmpty(JobEntryBase jeb)
+	{
+		super(jeb);
+	}
+
+    public Object clone()
+    {
+        JobEntryFolderIsEmpty je = (JobEntryFolderIsEmpty) super.clone();
+        return je;
+    }
+    
+	public String getXML()
+	{
+        StringBuffer retval = new StringBuffer(50);
+		
+		retval.append(super.getXML());		
+		retval.append("      ").append(XMLHandler.addTagValue("foldername",   foldername));
+		retval.append("      ").append(XMLHandler.addTagValue("include_subfolders", includeSubfolders));
+		retval.append("      ").append(XMLHandler.addTagValue("specify_wilcard", specifywilcard));
+		retval.append("      ").append(XMLHandler.addTagValue("wildcard",   wildcard));
+		
+		
+		return retval.toString();
+	}
+	
+	public void loadXML(Node entrynode, List<DatabaseMeta> databases, List<SlaveServer> slaveServers, Repository rep) throws KettleXMLException
+	{
+		try
+		{
+			super.loadXML(entrynode, databases, slaveServers);
+			foldername = XMLHandler.getTagValue(entrynode, "foldername");
+			includeSubfolders = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "include_subfolders"));
+			specifywilcard = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "specify_wilcard"));
+			wildcard = XMLHandler.getTagValue(entrynode, "wildcard");
+			
+			
+			
+		}
+		catch(KettleXMLException xe)
+		{
+			throw new KettleXMLException("Unable to load job entry of type 'create folder' from XML node", xe);
+		}
+	}
+
+	public void loadRep(Repository rep, long id_jobentry, List<DatabaseMeta> databases, List<SlaveServer> slaveServers) throws KettleException
+	{
+		try
+		{
+			super.loadRep(rep, id_jobentry, databases, slaveServers);
+			foldername = rep.getJobEntryAttributeString(id_jobentry, "foldername");
+			includeSubfolders = rep.getJobEntryAttributeBoolean(id_jobentry, "include_subfolders"); 
+			specifywilcard = rep.getJobEntryAttributeBoolean(id_jobentry, "specify_wilcard");  
+			wildcard = rep.getJobEntryAttributeString(id_jobentry, "wildcard");
+			
+			
+			 
+		}
+		catch(KettleException dbe)
+		{
+			throw new KettleException("Unable to load job entry of type 'create Folder' from the repository for id_jobentry="+id_jobentry, dbe);
+		}
+	}
+	
+	public void saveRep(Repository rep, long id_job)
+		throws KettleException
+	{
+		try
+		{
+			super.saveRep(rep, id_job);
+			
+			rep.saveJobEntryAttribute(id_job, getID(), "foldername", foldername);
+			rep.saveJobEntryAttribute(id_job, getID(), "include_subfolders", includeSubfolders);
+			rep.saveJobEntryAttribute(id_job, getID(), "specify_wilcard", specifywilcard);
+			rep.saveJobEntryAttribute(id_job, getID(), "wildcard", wildcard);
+			
+	
+		}
+		catch(KettleDatabaseException dbe)
+		{
+			throw new KettleException("Unable to save job entry of type 'create Folder' to the repository for id_job="+id_job, dbe);
+		}
+	}
+	
+	public void setSpecifyWilcard(boolean specifywilcard)
+	{
+		this.specifywilcard=specifywilcard;
+	}
+
+	public boolean isSpecifyWilcard()
+	{
+		return specifywilcard;
+	}
+	public void setFoldername(String foldername)
+	{
+		this.foldername = foldername;
+	}
+	
+	public String getFoldername()
+	{
+		return foldername;
+	}
+    
+    public String getRealFoldername()
+    {
+        return environmentSubstitute(getFoldername());
+    }
+    
+    public String getWildcard()
+    {
+    	return wildcard;
+    }
+    
+    public String getRealWildcard()
+    {
+    	return environmentSubstitute(getWildcard());
+    }
+    public void setWildcard(String wildcard)
+    {
+    	this.wildcard=wildcard;
+    }
+    public boolean isIncludeSubFolders()
+    {
+    	return includeSubfolders;
+    }
+	
+    public void setIncludeSubFolders(boolean includeSubfolders)
+    {
+    	this.includeSubfolders=includeSubfolders;
+    }
+	public Result execute(Result previousResult, int nr, Repository rep, Job parentJob)
+	{
+		LogWriter log = LogWriter.getInstance();
+		Result result = previousResult;
+		result.setResult( false );
+		
+		filescount=0;
+		folderscount=0;
+		
+		if (foldername!=null)
+		{
+            String realFoldername = getRealFoldername();
+            FileObject FolderObject = null;
+			try {
+				FolderObject = KettleVFS.getFileObject(realFoldername);
+
+				if ( FolderObject.exists() )
+				{
+					//Check if it's a folder
+					if(FolderObject.getType() == FileType.FOLDER) 
+					{
+						// File provided is a folder, so we can process ...
+						FolderObject.findFiles(new TextFileSelector(FolderObject.toString()));
+						
+						log.logBasic("Total files", "We found : "+filescount + " file(s)");
+						
+						if(filescount==0) result.setResult(true);
+						
+						
+					}
+					else
+					{
+						// Not a folder, fail
+						log.logError("Found files", "[" + realFoldername+"] is not a folder, failing.");
+					}
+						
+				}
+				else
+				{
+					//  No Folder found					
+					log.logBasic(toString(), "we can not find ["+realFoldername+"] !");
+				}
+			} catch (IOException e) {
+				log.logError(toString(), "Could not create Folder ["+realFoldername+"], exception: " + e.getMessage());
+				result.setResult( false );
+				result.setNrErrors(1);					
+			}
+            finally {
+            	if ( FolderObject != null )
+            	{
+            		try  {
+            		     FolderObject.close();
+            		}
+            		catch ( IOException ex ) {};
+            	}
+            }			
+		}
+		else
+		{			
+			log.logError(toString(), "No Foldername is defined.");
+		}
+		
+		return result;
+	}
+	
+	private class TextFileSelector implements FileSelector 
+	{
+		LogWriter log = LogWriter.getInstance();
+		String root_folder=null;
+		
+		public TextFileSelector(String rootfolder) 
+		 {
+			
+			 if (rootfolder!=null)
+			 {
+				 root_folder=rootfolder;
+			 }
+		 }
+		 
+		public boolean includeFile(FileSelectInfo info) 
+		{
+			boolean returncode=false;
+			FileObject file_name=null;
+			
+			try
+			{
+				if (!info.getFile().toString().equals(root_folder))
+				{
+					// Pass over the Base folder itself
+					if ((info.getFile().getType() == FileType.FILE))
+					{
+						
+						if (info.getFile().getParent().equals(info.getBaseFolder()))
+						 {
+							// We are in the Base folder
+							if((isSpecifyWilcard() && GetFileWildcard(info.getFile().toString()) || !isSpecifyWilcard()))
+							{
+								if(log.isDetailed()) log.logDetailed("Found files", "We found file : " + info.getFile().toString());
+								filescount++; 
+							}
+						 }
+						else
+						{
+							// We are not in the base Folder...ONLY if Use sub folders
+							// We are in the Base folder
+							if((isSpecifyWilcard() && GetFileWildcard(info.getFile().toString()) || !isSpecifyWilcard())
+									&& (isIncludeSubFolders()))
+							{
+								if(log.isDetailed()) log.logDetailed("Found files", "We found file : " + info.getFile().toString());
+								filescount++; 
+							}
+						}
+					}
+					else
+					{
+						folderscount++;
+					}
+				}
+				return true;
+								
+			}
+			catch (Exception e) 
+			{
+				log.logError(Messages.getString("JobFolderIsEmpty.Error") , Messages.getString("JobFolderIsEmpty.Error.Exception", 
+						info.getFile().toString(),e.getMessage()));
+				 returncode= false;
+			}
+			finally 
+			{
+				if ( file_name != null )
+				{
+					try  
+					{
+						file_name.close();
+						
+					}
+					catch ( IOException ex ) {};
+				}
+
+			}
+			
+			
+			return returncode;
+		}
+
+		public boolean traverseDescendents(FileSelectInfo info) 
+		{
+			return true;//isIncludeSubFolders();
+		}
+	}
+	/**********************************************************
+	 * 
+	 * @param selectedfile
+	 * @param wildcard
+	 * @return True if the selectedfile matches the wildcard
+	 **********************************************************/
+	private boolean GetFileWildcard(String selectedfile)
+	{
+		Pattern pattern = null;
+		boolean getIt=true;
+	
+        if (!Const.isEmpty(getWildcard()))
+        {
+        	 pattern = Pattern.compile(getRealWildcard());
+			// First see if the file matches the regular expression!
+			if (pattern!=null)
+			{
+				Matcher matcher = pattern.matcher(selectedfile);
+				getIt = matcher.matches();
+			}
+        }
+		
+		return getIt;
+	}
+
+	public boolean evaluates()
+	{
+		return true;
+	}
+    
+	
+	  @Override
+	  public void check(List<CheckResultInterface> remarks, JobMeta jobMeta) {
+	    andValidator().validate(this, "filename", remarks, putValidators(notBlankValidator())); //$NON-NLS-1$
+	  }
+}
