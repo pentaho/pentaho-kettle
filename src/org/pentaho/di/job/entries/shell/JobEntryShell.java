b3cfe919850307ffe34825b88adcf95a5fbbc183
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-12-17 14:59:26 +0000

    Fix for PDI-580 : apply environmentSubtitute on workdirectory
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6185 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/entries/shell/JobEntryShell.java
+++ src/org/pentaho/di/job/entries/shell/JobEntryShell.java
@@ -566,7 +566,7 @@ public class JobEntryShell extends JobEntryBase implements Cloneable, JobEntryIn
 
 			if (getWorkDirectory() != null && !Const.isEmpty(Const.rtrim(getWorkDirectory())))
 			{
-				File file = new File(getWorkDirectory());
+				File file = new File(environmentSubstitute(getWorkDirectory()));
 				procBuilder.directory(file);
 			}
 			Process proc = procBuilder.start();
