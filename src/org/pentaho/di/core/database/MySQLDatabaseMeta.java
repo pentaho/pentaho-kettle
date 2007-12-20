9398df8d557057f9178acce2d0f628d3c35debdc
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2007-12-20 10:44:45 +0000

    Fix for PDI-425: MySQL: default for Number (double) is FLOAT and looses precision
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6204 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/database/MySQLDatabaseMeta.java
+++ src/org/pentaho/di/core/database/MySQLDatabaseMeta.java
@@ -259,7 +259,7 @@ public class MySQLDatabaseMeta extends BaseDatabaseMeta implements DatabaseInter
 				// Floating point values...
 				else  
 				{
-					if (length>18)
+					if (length>15)
 					{
 						retval+="DECIMAL("+length;
 						if (precision>0) retval+=", "+precision;
@@ -267,7 +267,9 @@ public class MySQLDatabaseMeta extends BaseDatabaseMeta implements DatabaseInter
 					}
 					else
 					{
-						retval+="FLOAT";
+						// A double-precision floating-point number is accurate to approximately 15 decimal places.
+						// http://mysql.mirrors-r-us.net/doc/refman/5.1/en/numeric-type-overview.html 
+						retval+="DOUBLE";
 					}
 				}
 			}
