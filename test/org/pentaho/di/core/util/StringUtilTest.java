386b2a8d324a5b58f91970ef26772fc197515f7d
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-01-01 17:25:14 +0000

    Add Unittest cases for StringUtil
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6304 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- test/org/pentaho/di/core/util/StringUtilTest.java
+++ test/org/pentaho/di/core/util/StringUtilTest.java
@@ -15,6 +15,9 @@
 
 package org.pentaho.di.core.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 /**
@@ -43,4 +46,85 @@ public class StringUtilTest extends TestCase
 		assertEquals("AWard", StringUtil.initCap("AWard"));
 	}
 	
+	
+	/**
+	 * Create an example map to be used for variable resolution.
+	 * 
+	 * @return Map of variablenames/values.
+	 */
+	Map<String, String> createVariables1(String open, String close)
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("EMPTY", "");
+		map.put("checkcase", "case1");
+		map.put("CheckCase", "case2");
+		map.put("CHECKCASE", "case3");
+		map.put("VARIABLE_1", "VARIABLE1");
+		
+		map.put("recursive1", "A" + open + "recursive2" + close);
+		map.put("recursive2", "recurse");
+		
+		map.put("recursive3", open + "recursive4" + close + "B");
+		map.put("recursive4", "recurse");
+
+		map.put("recursive5", open + "recursive6" + close + "B");
+		map.put("recursive6", "Z" + open + "recursive7" + close);
+		map.put("recursive7", "final");		
+		
+		// endless recursive 
+		map.put("recursive_all", open + "recursive_all1" + close + " tail");
+		map.put("recursive_all1", open + "recursive_all" + close + " tail1");
+		
+		return map;
+	}
+	
+	/**
+	 * Test the basic substitute call.
+	 */
+	public void testSubstituteBasic()	
+	{
+		Map<String, String> map = createVariables1("${", "}");
+		assertEquals("||", StringUtil.substitute("|${EMPTY}|", map, "${", "}", 0));
+		assertEquals("|case1|", StringUtil.substitute("|${checkcase}|", map, "${", "}", 0));
+		assertEquals("|case2|", StringUtil.substitute("|${CheckCase}|", map, "${", "}", 0));
+		assertEquals("|case3|", StringUtil.substitute("|${CHECKCASE}|", map, "${", "}", 0));
+		assertEquals("|Arecurse|", StringUtil.substitute("|${recursive1}|", map, "${", "}", 0));
+		assertEquals("|recurseB|", StringUtil.substitute("|${recursive3}|", map, "${", "}", 0));
+		assertEquals("|ZfinalB|", StringUtil.substitute("|${recursive5}|", map, "${", "}", 0));
+		
+		try {
+		    StringUtil.substitute("|${recursive_all}|", map, "${", "}", 0);
+		    fail("recursive check is failing");
+		}
+		catch ( RuntimeException rex )
+		{ }
+		
+		map = createVariables1("%%", "%%");
+		assertEquals("||", StringUtil.substitute("|%%EMPTY%%|", map, "%%", "%%", 0));
+		assertEquals("|case1|", StringUtil.substitute("|%%checkcase%%|", map, "%%", "%%", 0));
+		assertEquals("|case2|", StringUtil.substitute("|%%CheckCase%%|", map, "%%", "%%", 0));
+		assertEquals("|case3|", StringUtil.substitute("|%%CHECKCASE%%|", map, "%%", "%%", 0));
+		assertEquals("|Arecurse|", StringUtil.substitute("|%%recursive1%%|", map, "%%", "%%", 0));
+		assertEquals("|recurseB|", StringUtil.substitute("|%%recursive3%%|", map, "%%", "%%", 0));
+		assertEquals("|ZfinalB|", StringUtil.substitute("|%%recursive5%%|", map, "%%", "%%", 0));
+		
+		try {
+		    StringUtil.substitute("|%%recursive_all%%|", map, "%%", "%%", 0);
+		    fail("recursive check is failing");
+		}
+		catch ( RuntimeException rex )
+		{ }		
+	}
+	
+	/**
+	 * Test isEmpty() call.
+	 */
+	public void testIsEmpty()	
+	{
+		assertTrue(StringUtil.isEmpty((String)null));
+		assertTrue(StringUtil.isEmpty(""));
+		assertFalse(StringUtil.isEmpty("A"));
+		assertFalse(StringUtil.isEmpty(" A "));
+	}
 }
