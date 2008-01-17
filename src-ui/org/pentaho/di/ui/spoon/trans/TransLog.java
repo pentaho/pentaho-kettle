7a099f906acc766b85fab91fc2d3f0c128d044bc
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-01-17 09:46:58 +0000

    PDI-560 : PDI cannot initialize sub transformation variables when used with repository.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.2@6451 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/trans/TransLog.java
+++ src-ui/org/pentaho/di/ui/spoon/trans/TransLog.java
@@ -536,6 +536,7 @@ public class TransLog extends Composite implements TabItemInterface
 						trans = new Trans(transMeta, spoon.rep, transMeta.getName(), transMeta.getDirectory().getPath(), transMeta.getFilename());
 						trans.setReplayDate(executionConfiguration.getReplayDate());
 						trans.setMonitored(true);
+						trans.setRepository(executionConfiguration.getRepository());
 						log.logBasic(toString(), Messages.getString("TransLog.Log.TransformationOpened")); //$NON-NLS-1$
 					}
 					catch (KettleException e)
