2769b67394cecbbf7eaf9039c1ef9d0e55e17428
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-02-14 10:49:45 +0000

    PDI-781 : Merging data with widely varying data volumes causes a performance bottleneck in any step (fix for stalling issue)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6667 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/step/BaseStep.java
+++ src/org/pentaho/di/trans/step/BaseStep.java
@@ -1204,9 +1204,9 @@ public class BaseStep extends Thread implements VariableSpace, StepInterface
 	    // The buffer to grow beyond "a few" entries.
 	    // We'll only do that if the previous step has not ended...
 	    //
-        while (isUsingThreadPriorityManagment() && !inputRowSet.isDone() && inputRowSet.size()<= ( transMeta.getSizeRowset()>>6 ) && !isStopped())
+        if (isUsingThreadPriorityManagment() && !inputRowSet.isDone() && inputRowSet.size()<= ( transMeta.getSizeRowset()>>6 ) && !isStopped())
         {
-        	try { Thread.sleep(0,10); } catch (InterruptedException e) { }
+        	try { Thread.sleep(0,1); } catch (InterruptedException e) { }
         }
     
 
