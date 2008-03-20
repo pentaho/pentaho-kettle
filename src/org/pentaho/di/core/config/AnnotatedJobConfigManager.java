37cebacb33a4ab3788705474a15f1a0ff414010b
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-03-20 10:53:56 +0000

    PDI-302 : Plugin architecture using annotations is not working
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@6930 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/config/AnnotatedJobConfigManager.java
+++ src/org/pentaho/di/core/config/AnnotatedJobConfigManager.java
@@ -15,12 +15,17 @@ package org.pentaho.di.core.config;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.annotations.Inject;
 import org.pentaho.di.core.annotations.Job;
 import org.pentaho.di.core.exception.KettleConfigException;
+import org.pentaho.di.core.logging.LogWriter;
 import org.pentaho.di.core.util.ResolverUtil;
+import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.job.JobEntryCategory;
 import org.pentaho.di.job.JobPluginMeta;
 import org.pentaho.di.job.entry.Messages;
+import org.pentaho.di.trans.step.StepCategory;
 
 
 /**
@@ -39,18 +44,71 @@ import org.pentaho.di.job.entry.Messages;
 	@SuppressWarnings("unchecked") //this is ok here because we defined T above.
 	public Collection<T> load() throws KettleConfigException
 	{
+		// saving old value for change the loglevel to BASIC to avoid i18n messages, see below
+		int oldLogLevel=LogWriter.getInstance().getLogLevel(); 
+		
 		ResolverUtil<JobPluginMeta> resolver = new ResolverUtil<JobPluginMeta>();
-		resolver.find(new ResolverUtil.AnnotatedWith(Job.class), packages != null ? packages.split(",")
-				: new String[] {});
+		
+		// If there is a system wide property set with name KETTLE_PLUGIN_PACKAGES we search those packages as well
+		//
+		String allPackages = packages;
+		String extraPackages = System.getProperty(Const.KETTLE_PLUGIN_PACKAGES);
+		if (!Const.isEmpty(extraPackages)) {
+			allPackages+=","+extraPackages;
+		}
+		
+		resolver.find(new ResolverUtil.AnnotatedWith(Job.class), allPackages != null ? allPackages.split(",") : new String[] {});
+		
 		Collection<JobPluginMeta> jobs = new LinkedHashSet<JobPluginMeta>(resolver.size());
 		for (Class<?> clazz : resolver.getClasses())
 		{
 			Job job = clazz.getAnnotation(Job.class);
+			
 			String jobId  = job.id();
 			if (jobId.equals("")) // default
 				jobId =  clazz.getName();
 
-			jobs.add(new JobPluginMeta(clazz, jobId, job.type(),Messages.getString(job.tooltip()),job.image()));
+			// The package name to get the descriptions or tool tip from...
+			//
+			String packageName = job.i18nPackageName();
+			if (Const.isEmpty(packageName)) packageName = org.pentaho.di.job.entry.Messages.class.getPackage().getName();
+
+			// An alternative package to get the description or tool tip from...
+			//
+			String altPackageName = clazz.getPackage().getName();
+
+			// Determine the i18n description of the step description (name)
+			//
+			LogWriter.getInstance().setLogLevel(LogWriter.LOG_LEVEL_BASIC); // avoid i18n messages for missing locale
+			String description = BaseMessages.getString(packageName, job.description());
+			if (description.startsWith("!") && description.endsWith("!")) description=Messages.getString(job.description());
+			LogWriter.getInstance().setLogLevel(oldLogLevel); // restore loglevel, when the last alternative fails, log it when loglevel is detailed
+			if (description.startsWith("!") && description.endsWith("!")) description=BaseMessages.getString(altPackageName, job.description());
+			
+			// Determine the i18n tool tip text for the step (the extended description)
+			//
+			LogWriter.getInstance().setLogLevel(LogWriter.LOG_LEVEL_BASIC); // avoid i18n messages for missing locale
+			String tooltip = BaseMessages.getString(packageName, job.tooltip());
+			if (tooltip.startsWith("!") && tooltip.endsWith("!")) tooltip=Messages.getString(job.tooltip());
+			LogWriter.getInstance().setLogLevel(oldLogLevel); // restore loglevel, when the last alternative fails, log it when loglevel is detailed
+			if (tooltip.startsWith("!") && tooltip.endsWith("!")) tooltip=BaseMessages.getString(altPackageName, job.tooltip());
+			
+			// If the step should have a separate category, this is the place to calculate that
+			// This calculation is only used if the category is USER_DEFINED
+			//
+			String category;
+			if (job.category()!=JobEntryCategory.CATEGORY_USER_DEFINED) {
+				category = JobEntryCategory.STANDARD_CATEGORIES[job.category()].getName();
+			}
+			else {
+				LogWriter.getInstance().setLogLevel(LogWriter.LOG_LEVEL_BASIC); // avoid i18n messsages for missing locale
+				category = BaseMessages.getString(packageName, job.categoryDescription());
+				if (category.startsWith("!") && category.endsWith("!")) category=Messages.getString(job.categoryDescription());
+				LogWriter.getInstance().setLogLevel(oldLogLevel); // restore loglevel, when the last alternative fails, log it when loglevel is detailed
+				if (category.startsWith("!") && category.endsWith("!")) category=BaseMessages.getString(altPackageName, job.categoryDescription());
+			}
+			
+			jobs.add(new JobPluginMeta(clazz, jobId, job.type(), description, tooltip, job.image(), category));
 		}
 		
 		
