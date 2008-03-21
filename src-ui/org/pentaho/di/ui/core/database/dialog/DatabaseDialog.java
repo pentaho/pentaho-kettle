1fb812497659c37d5900a856fa32f02708272792
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-03-21 18:34:07 +0000

    Fix for PDI-908 : put password in the right place when clustering
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.3@6952 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/database/dialog/DatabaseDialog.java
+++ src-ui/org/pentaho/di/ui/core/database/dialog/DatabaseDialog.java
@@ -1527,7 +1527,7 @@ public class DatabaseDialog extends Dialog
                 tableItem.setText(3, Const.NVL(meta.getPort(), "")); //$NON-NLS-1$
                 tableItem.setText(4, Const.NVL(meta.getDatabaseName(), "")); //$NON-NLS-1$
                 tableItem.setText(5, Const.NVL(meta.getUsername(), "")); //$NON-NLS-1$
-                tableItem.setText(5, Const.NVL(meta.getPassword(), "")); //$NON-NLS-1$
+                tableItem.setText(6, Const.NVL(meta.getPassword(), "")); //$NON-NLS-1$
             }
         }
         wCluster.removeEmptyRows();
