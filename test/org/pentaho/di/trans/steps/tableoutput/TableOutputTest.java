1e88174a11d1e30e3fe19ca48284fdefefddb2aa
sboden <sboden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-03-29 21:28:26 +0000

    Unit test case for PDI JIRA 897
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7027 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- test/org/pentaho/di/trans/steps/tableoutput/TableOutputTest.java
+++ test/org/pentaho/di/trans/steps/tableoutput/TableOutputTest.java
@@ -15,6 +15,7 @@
 
 package org.pentaho.di.trans.steps.tableoutput;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -69,6 +70,22 @@ public class TableOutputTest extends TestCase
     private static String target_table  = "table";
     private static String target_table1 = "table1";
     private static String target_table2 = "table2";
+
+	/**
+	 * Create table for the normal case.
+	 */
+	public void createTable(Database db, String tableName, RowMetaInterface rm) throws Exception
+	{		
+		String source = db.getCreateTableStatement(tableName, rm, null, false, null, true);
+		try  {
+		    db.execStatement(source);
+		}
+		catch ( KettleException ex ) 
+		{
+			fail("failure while creating table " + tableName + ": " + ex.getMessage());	
+		}						
+	}
+
     
 	public RowMetaInterface createSourceRowMetaInterface1()
 	{
@@ -87,38 +104,7 @@ public class TableOutputTest extends TestCase
 		return rm;
 	}       	
 	
-	/**
-	 * Create table for the normal case.
-	 */
-	public void createTableNormalCase(Database db) throws Exception
-	{		
-		String source = db.getCreateTableStatement(target_table, createSourceRowMetaInterface1(), null, false, null, true);
-		try  {
-		    db.execStatement(source);
-		}
-		catch ( KettleException ex ) 
-		{
-			fail("failure while creating table " + target_table + ": " + ex.getMessage());	
-		}						
-	}
-
 	
-	public RowMetaInterface createRowMetaInterface()
-	{
-		RowMetaInterface rm = new RowMeta();
-		
-		ValueMetaInterface valuesMeta[] = {
-			    new ValueMeta("int_field", ValueMeta.TYPE_INTEGER)
-	    };
-
-		for (int i=0; i < valuesMeta.length; i++ )
-		{
-			rm.addValueMeta(valuesMeta[i]);
-		}
-		
-		return rm;
-	}
-
 	/**
 	 * Create the input rows used for the normal unit test.
 	 */
@@ -147,10 +133,59 @@ public class TableOutputTest extends TestCase
 		return list;
 	}	
 
+	
+	public RowMetaInterface createJIRA897RowMetaInterface()
+	{
+		RowMetaInterface rm = new RowMeta();
+
+		ValueMetaInterface valuesMeta[] = {
+			    new ValueMeta("ID",    ValueMeta.TYPE_INTEGER,  8, 0),
+			    new ValueMeta("TABLE", ValueMeta.TYPE_STRING,  30, 0),
+			    new ValueMeta("CODE",  ValueMeta.TYPE_INTEGER,  8, 0),
+	    };
+
+		for (int i=0; i < valuesMeta.length; i++ )
+		{
+			rm.addValueMeta(valuesMeta[i]);
+		}
+		
+		return rm;
+	}       	
+	
+	
 	/**
-	 * Create the result rows for the normal case.
-	 *
-	public List<RowMetaAndData> createNormalResultDataRows()
+	 * Create the input rows used for the JIRA897 unit test.
+	 */
+	public List<RowMetaAndData> createJIRA897DataRows()
+	{
+		List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();	
+		
+		RowMetaInterface rm = createJIRA897RowMetaInterface();
+		
+		Object[] r1 = new Object[] { new Long(100L), target_table1, new Long(1000L) };
+		Object[] r2 = new Object[] { new Long(101L), target_table2, new Long(1001L) };
+		Object[] r3 = new Object[] { new Long(102L), target_table1, new Long(1002L) };
+		Object[] r4 = new Object[] { new Long(103L), target_table2, new Long(1003L) };
+		Object[] r5 = new Object[] { new Long(104L), target_table2, new Long(1004L) };
+		Object[] r6 = new Object[] { new Long(105L), target_table1, new Long(1005L) };
+		Object[] r7 = new Object[] { new Long(106L), target_table1, new Long(1006L) };					
+		
+		list.add(new RowMetaAndData(rm, r1));
+		list.add(new RowMetaAndData(rm, r2));
+		list.add(new RowMetaAndData(rm, r3));
+		list.add(new RowMetaAndData(rm, r4));
+		list.add(new RowMetaAndData(rm, r5));
+		list.add(new RowMetaAndData(rm, r6));
+		list.add(new RowMetaAndData(rm, r7));
+		
+		return list;
+	}	
+
+	
+	/**
+	 * Create the result rows for the JIRA897 case.
+	 */
+	public List<RowMetaAndData> createJIRA897ResultDataRows()
 	{
 		List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();	
 		
@@ -163,8 +198,7 @@ public class TableOutputTest extends TestCase
 		Object[] r5  = new Object[] { new Long(104L),   new Long(1004L) };
 		Object[] r6  = new Object[] { new Long(105L),   new Long(1005L) };
 		Object[] r7  = new Object[] { new Long(106L),   new Long(1006L) };
-	
-		
+			
 		list.add(new RowMetaAndData(rm, r1));
 		list.add(new RowMetaAndData(rm, r2));
 		list.add(new RowMetaAndData(rm, r3));
@@ -174,7 +208,7 @@ public class TableOutputTest extends TestCase
 		list.add(new RowMetaAndData(rm, r7));
 		
 		return list;
-	} */	
+	} 	
 	
 	/**
 	 *  Check the 2 lists comparing the rows in order.
@@ -222,9 +256,126 @@ public class TableOutputTest extends TestCase
     }
     
 	/**
+	 * Check the results in the target dimension table.
+	 * 
+	 * @param db database to use.
+	 */
+	public void checkResultsNormal(Database db) throws Exception
+	{
+		String query = "SELECT ID, CODE FROM " +
+		               target_table + " ORDER BY ID";
+
+		String correctResults[] =  {
+		    "100|1000",
+		    "101|1001",
+		    "102|1002",
+		    "103|1003",
+		    "104|1004",
+		    "105|1005",
+		    "106|1006",
+		};
+	
+		ResultSet rs = db.openQuery(query);
+		int idx = 0;
+		while (rs.next() )
+		{
+		   int id = rs.getInt("ID");
+		   int code = rs.getInt("CODE");
+		   
+		   String result = id + "|" + code;
+		   if ( idx > correctResults.length)
+		   {
+		       fail("more rows returned than expected");
+		   }
+		   if ( ! result.equals(correctResults[idx]))  {
+			   fail("row " + (idx + 1) + " is different than expected");
+		   }
+		   idx++;
+  	    }
+  	    if ( idx < correctResults.length)
+	    {
+	       fail("less rows returned than expected");
+	    }
+	}    
+
+	/**
+	 * Check the results in the target dimension table.
+	 * 
+	 * @param db database to use.
+	 */
+	public void checkResultsJIRA897(Database db) throws Exception
+	{		
+		// Table 1
+		String query = "SELECT ID, CODE FROM " +
+		               target_table1 + " ORDER BY ID";
+
+		String correctResults[] =  {
+		    "100|1000",
+		    "102|1002",
+		    "105|1005",
+		    "106|1006",
+		};
+	
+		ResultSet rs = db.openQuery(query);
+		int idx = 0;
+		while (rs.next() )
+		{
+		   int id = rs.getInt("ID");
+		   int code = rs.getInt("CODE");
+		   
+		   String result = id + "|" + code;
+		   if ( idx > correctResults.length)
+		   {
+		       fail("more rows returned than expected");
+		   }
+		   if ( ! result.equals(correctResults[idx]))  {
+			   fail("row " + (idx + 1) + " is different than expected");
+		   }
+		   idx++;
+  	    }
+  	    if ( idx < correctResults.length)
+	    {
+	       fail("less rows returned than expected");
+	    }
+  	    
+  	    
+		// Table 2
+		query = "SELECT ID, CODE FROM " +
+		               target_table2 + " ORDER BY ID";
+
+		String correctResults1[] =  {
+		    "101|1001",
+		    "103|1003",
+		    "104|1004",
+		};
+	
+		rs = db.openQuery(query);
+		idx = 0;
+		while (rs.next() )
+		{
+		   int id = rs.getInt("ID");
+		   int code = rs.getInt("CODE");
+		   
+		   String result = id + "|" + code;
+		   if ( idx > correctResults1.length)
+		   {
+		       fail("more rows returned than expected");
+		   }
+		   if ( ! result.equals(correctResults1[idx]))  {
+			   fail("row " + (idx + 1) + " is different than expected");
+		   }
+		   idx++;
+  	    }
+  	    if ( idx < correctResults1.length)
+	    {
+	       fail("less rows returned than expected");
+	    }  	    
+	}    
+	
+	/**
 	 * Test case for normal table output case. 
 	 */
-    public void testTableInputWithParam() throws Exception
+    public void testTableOutputNormal() throws Exception
     {
         EnvUtil.environmentInit();
         try
@@ -247,7 +398,7 @@ public class TableOutputTest extends TestCase
             // Execute our setup SQLs in the database.
             Database database = new Database(dbInfo);
             database.connect();
-            createTableNormalCase(database);
+            createTable(database, target_table, createSourceRowMetaInterface1());
 
             StepLoader steploader = StepLoader.getInstance();            
 
@@ -303,6 +454,101 @@ public class TableOutputTest extends TestCase
             List<RowMetaAndData> resultRows = rc.getRowsWritten();
             List<RowMetaAndData> goldRows = createNormalDataRows();
             checkRows(goldRows, resultRows);
+            checkResultsNormal(database);
+        }    	
+        finally {}    
+    }
+    
+    
+	/**
+	 * Test case for normal table output where the table is included in the instream, but the tablename
+	 * is not stored in the table. 
+	 */
+    public void testTableOutputJIRA897() throws Exception
+    {
+        EnvUtil.environmentInit();
+        try
+        {
+            //
+            // Create a new transformation...
+            //
+            TransMeta transMeta = new TransMeta();
+            transMeta.setName("table output JIRA897 test");
+
+            // Add the database connections
+            for (int i=0;i<databasesXML.length;i++)
+            {
+                DatabaseMeta databaseMeta = new DatabaseMeta(databasesXML[i]);
+                transMeta.addDatabase(databaseMeta);
+            }
+
+            DatabaseMeta dbInfo = transMeta.findDatabase("db");
+
+            // Execute our setup SQLs in the database.
+            Database database = new Database(dbInfo);
+            database.connect();
+            createTable(database, target_table1, createSourceRowMetaInterface1());
+            createTable(database, target_table2, createSourceRowMetaInterface1());
+
+            StepLoader steploader = StepLoader.getInstance();            
+
+            // 
+            // create an injector step...
+            //
+            String injectorStepname = "injector step";
+            InjectorMeta im = new InjectorMeta();
+            
+            // Set the information of the injector.                   
+            String injectorPid = steploader.getStepPluginID(im);
+            StepMeta injectorStep = new StepMeta(injectorPid, injectorStepname, (StepMetaInterface)im);
+            transMeta.addStep(injectorStep);            
+            
+            // 
+            // create the source step...
+            //
+            String outputname = "output to [" + target_table1 + "] and [" + target_table2 + "]";
+            TableOutputMeta tom = new TableOutputMeta();
+            tom.setDatabaseMeta(transMeta.findDatabase("db"));
+            tom.setTableNameInField(true);
+            tom.setTableNameField("TABLE");
+            tom.setTableNameInTable(false);
+
+            String fromid = steploader.getStepPluginID(tom);
+            StepMeta fromstep = new StepMeta(fromid, outputname, (StepMetaInterface)tom);
+            fromstep.setDescription("write data to tables on database [" + dbInfo + "]");
+            transMeta.addStep(fromstep);
+            
+            TransHopMeta hi = new TransHopMeta(injectorStep, fromstep);
+            transMeta.addTransHop(hi);
+
+            // Now execute the transformation...
+            Trans trans = new Trans(transMeta);
+
+            trans.prepareExecution(null);
+                    
+            StepInterface si = trans.getStepInterface(outputname, 0);
+            RowStepCollector rc = new RowStepCollector();
+            si.addRowListener(rc);
+            
+            RowProducer rp = trans.addRowProducer(injectorStepname, 0);
+            trans.startThreads();
+            
+            // add rows
+            List<RowMetaAndData> inputList = createJIRA897DataRows();
+            for (RowMetaAndData rm : inputList )
+            {
+            	rp.putRow(rm.getRowMeta(), rm.getData());
+            }   
+            rp.finished();
+
+            trans.waitUntilFinished();   
+
+            List<RowMetaAndData> resultRows = rc.getRowsWritten();
+
+            // The name of the table should still be in here.
+            List<RowMetaAndData> goldRows = createJIRA897DataRows();
+            checkRows(goldRows, resultRows);
+            checkResultsJIRA897(database);
         }    	
         finally {}    
     }    
