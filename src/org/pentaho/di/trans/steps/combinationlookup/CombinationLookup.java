5794929139745a86ab5e40aac413474223af30a6
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-04-15 18:36:22 +0000

    Addition to PDI-996 to use casting for parameter null check when using Vertica database.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7264 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/combinationlookup/CombinationLookup.java
+++ src/org/pentaho/di/trans/steps/combinationlookup/CombinationLookup.java
@@ -460,7 +460,7 @@ public class CombinationLookup extends BaseStep implements StepInterface
             data.lookupRowMeta.addValueMeta(inputRowMeta.getValueMeta(data.keynrs[i]));
             
             sql += " IS NULL AND ";
-            if (databaseMeta.getDatabaseType()==DatabaseMeta.TYPE_DATABASE_DB2)
+            if (databaseMeta.getDatabaseType()==DatabaseMeta.TYPE_DATABASE_DB2 || databaseMeta.getDatabaseType()==DatabaseMeta.TYPE_DATABASE_VERTICA)
             {
                 sql += "CAST(? AS VARCHAR(256)) IS NULL";
             }
