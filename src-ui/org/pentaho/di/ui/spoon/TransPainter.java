3f36b7d7a863b6568468216cd4f3e82c231a68f4
gmoran <gmoran@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-04-30 21:26:09 +0000

    PDI-886, PDI-988, small UI tweaks per PM
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7553 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/TransPainter.java
+++ src-ui/org/pentaho/di/ui/spoon/TransPainter.java
@@ -48,12 +48,15 @@ public class TransPainter
 	public static final String STRING_REMOTE_OUTPUT_STEPS       = "RemoteOutputSteps";       // $NON-NLS-1$
     
 	public static final String[] magnificationDescriptions = 
-		new String[] { "  25% ", "  50% ", "  75% ", "  100% " };
+		new String[] { "  100% ", "  75% ", "  50% ", "  25% "};
+
+/*	
 	public static final float[] magnifications = 
 		new float[] { 0.10f, 0.15f, 0.20f, 0.25f, 0.30f, 0.35f, 0.40f, 0.45f, 0.50f, 0.55f, 0.60f, 0.65f, 0.70f, 0.75f, 0.80f, 0.85f, 0.90f, 0.95f, 1.00f, 1.25f, 1.50f, 2.00f, 3.00f, 4.00f, 5.00f, 7.50f, 10.00f, };
 	
 	public static final int MAGNIFICATION_100_PERCENT_INDEX = 18;
-	
+
+	*/
 	private PropsUI      props;
     private int          shadowsize;
     private Point        area;
