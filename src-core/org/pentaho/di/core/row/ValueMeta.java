515810feb1307a342dabe67d999ae0762b9f7838
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-04-30 21:38:58 +0000

    Fix for bad XML generation in PDI-1104
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7555 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/row/ValueMeta.java
+++ src-core/org/pentaho/di/core/row/ValueMeta.java
@@ -2633,7 +2633,7 @@ public class ValueMeta implements ValueMetaInterface
 		    	throw new RuntimeException(toString()+" : There was a data type error: the data type of "+object.getClass().getName()+" object ["+object+"] does not correspond to value meta ["+toStringMeta()+"]");
 			}
         }
-    	xml.append(XMLHandler.closeTag(XML_META_TAG));
+    	xml.append(XMLHandler.closeTag(XML_DATA_TAG));
     	
     	return xml.toString();
     }
