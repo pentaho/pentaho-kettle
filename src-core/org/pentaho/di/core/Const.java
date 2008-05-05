2f6eddd05cc18deb1d9c2951a06cfa4cf6598a8a
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-05-05 15:53:11 +0000

    Upping version to 3.1.0-M2
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7622 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -51,7 +51,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "3.1.0-M1";
+	public static final String VERSION = "3.1.0-M2";
 
 	/**
 	 * Sleep time waiting when buffer is empty
