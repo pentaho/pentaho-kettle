1f83677e9c69a2d97d2cd101d26b4e57f9e4f0bd
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-05-05 15:50:24 +0000

    PDI-911 : Dimension Lookup/Update Surrogate Key Datatype
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.4@7621 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/dimensionlookup/DimensionLookup.java
+++ src/org/pentaho/di/trans/steps/dimensionlookup/DimensionLookup.java
@@ -330,6 +330,12 @@ public class DimensionLookup extends BaseStep implements StepInterface
                 returnIndex++;
                 
                 // See if we need to store this record in the cache as well...
+                /*
+                 * TODO: we can't really assume like this that the cache layout of the incoming rows (below) is the same as the stored data.
+                 * Storing this in the cache gives us data/metadata collision errors. (class cast problems etc)
+                 * Perhaps we need to convert this data to the target data types.  Alternatively, we can use a separate cache in the future.
+                 * Reference: PDI-911
+                 * 
                 if (meta.getCacheSize()>=0)
                 {
                     Object[] values = getCacheValues(rowMeta, row, technicalKey, valueVersion, valueDateFrom, valueDateTo);
@@ -340,6 +346,7 @@ public class DimensionLookup extends BaseStep implements StepInterface
                     	addToCache(lookupRow, values);
                     }
                 }
+                */
                 
 				if (log.isRowLevel()) logRowlevel(Messages.getString("DimensionLookup.Log.AddedDimensionEntry")+data.returnRowMeta.getString(returnRow)); //$NON-NLS-1$
 			}
