b65aa5fbe6ddac9e3088751e09604c1510e6ecdb
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-05-16 18:54:51 +0000

    PDI-1227 : Standard selection in StyledTextCompFind
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.4@7819 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/widget/StyledTextCompFind.java
+++ src-ui/org/pentaho/di/ui/core/widget/StyledTextCompFind.java
@@ -127,7 +127,7 @@ public class StyledTextCompFind extends org.eclipse.swt.widgets.Dialog {
 		optForward.setBounds(5, 15, 75, 15);
 		Button optBackward = new Button(grpDir, SWT.RADIO);
 		optBackward.setBounds(5, 33, 75, 15);
-		optBackward.setSelection(true);
+		optForward.setSelection(true);
 		optBackward.setText("&Backward");
 
 		frmData = new FormData();
