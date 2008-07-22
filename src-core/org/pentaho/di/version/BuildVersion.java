138a6156ea1e8570962a9c2efb1fcf1d3a6e5fe5
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-07-22 20:20:52 +0000

    Changed version report class to not show a stacktrace if the version file is not found.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@8564 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/version/BuildVersion.java
+++ src-core/org/pentaho/di/version/BuildVersion.java
@@ -125,8 +125,7 @@ public class BuildVersion
         }
         catch(Exception e)
         {
-            System.out.println("Unable to load revision number from file : ["+filename+"] : "+e.toString());
-            System.out.println(Const.getStackTracker(e));
+            System.out.println("Unable to load revision number from file : ["+filename+"]");
             
             version = 1;
             buildDate = new Date();
