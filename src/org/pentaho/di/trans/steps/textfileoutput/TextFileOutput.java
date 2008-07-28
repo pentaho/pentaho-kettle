7bce8f09200de5d9a2bf4ff326d256114f617ca1
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-07-28 15:26:44 +0000

    PDI-1541: TextFileOutput - ArrayIndexOutOfBoundsException if MORE THEN ONE enclosure character is represented in data output.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.5@8591 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/textfileoutput/TextFileOutput.java
+++ src/org/pentaho/di/trans/steps/textfileoutput/TextFileOutput.java
@@ -354,7 +354,7 @@ public class TextFileOutput extends BaseStep implements StepInterface
         			for (int i=0;i<enclosures.size();i++)
         			{
         				int position = enclosures.get(i);
-        				data.writer.write(str, from, position + data.binaryEnclosure.length);
+        				data.writer.write(str, from, position + data.binaryEnclosure.length - from);
         				data.writer.write(data.binaryEnclosure); // write enclosure a second time
         				from=position+data.binaryEnclosure.length;
         			}
@@ -380,7 +380,7 @@ public class TextFileOutput extends BaseStep implements StepInterface
 		List<Integer> positions = null;
 		if (data.binaryEnclosure!=null && data.binaryEnclosure.length>0)
 		{
-			for (int i=0;i<str.length - data.binaryEnclosure.length;i++)
+			for (int i=0;i<str.length - data.binaryEnclosure.length +1;i++)
 			{
 				// verify if on position i there is an enclosure
 				// 
