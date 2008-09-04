2cca18a1af7e6566c1cfb0f3f6b943f4d33db17a
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-09-04 12:40:21 +0000

    PDI-1204 : More secure management of kettle.pwd
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@8878 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- bin/Carte.bat
+++ bin/Carte.bat
@@ -65,9 +65,9 @@ REM ******************************************************************
 
 set OPT=-Xmx512M -cp %CLASSPATH% -Dorg.mortbay.util.URI.charset=UTF-8 -Djava.library.path=libswt\win32\ -DKETTLE_HOME="%KETTLE_HOME%" -DKETTLE_REPOSITORY="%KETTLE_REPOSITORY%" -DKETTLE_USER="%KETTLE_USER%" -DKETTLE_PASSWORD="%KETTLE_PASSWORD%" -DKETTLE_PLUGIN_PACKAGES="%KETTLE_PLUGIN_PACKAGES%"
 
-# ***********************************************************************
-# ** Optionally set up the options for JAAS (uncomment to make active) **
-# ***********************************************************************
+REM ***********************************************************************
+REM ** Optionally set up the options for JAAS (uncomment to make active) **
+REM ***********************************************************************
 
 REM set OPT=%OPT% -Djava.security.auth.login.config=%JAAS_LOGIN_MODULE_CONFIG%
 REM set	OPT=%OPT% -Dloginmodulename=%JAAS_LOGIN_MODULE_NAME%
