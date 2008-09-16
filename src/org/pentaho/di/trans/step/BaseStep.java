f02e929dd2c5e4f4541aaeb1a0e52feb41e35dba
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-09-16 15:47:19 +0000

    PDI-1694: Error handling of steps throws NPE when no row was there
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.0.5@8992 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/step/BaseStep.java
+++ src/org/pentaho/di/trans/step/BaseStep.java
@@ -1056,7 +1056,7 @@ public class BaseStep extends Thread implements VariableSpace
         }
         
         Object[] errorRowData = RowDataUtil.allocateRowData(errorRowMeta.size());
-        System.arraycopy(row, 0, errorRowData, 0, rowMeta.size());
+        if (row!=null) System.arraycopy(row, 0, errorRowData, 0, rowMeta.size());
         
         // Also add the error fields...
         stepErrorMeta.addErrorRowData(errorRowData, rowMeta.size(), nrErrors, errorDescriptions, fieldNames, errorCodes);
