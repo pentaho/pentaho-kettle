42bb09591d2571c1565ee003ca750dc36e2e7aba
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2008-09-19 10:29:37 +0000

    PDI-1705 : Execution results windows - messed up history table
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@9038 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/job/JobHistoryDelegate.java
+++ src-ui/org/pentaho/di/ui/spoon/job/JobHistoryDelegate.java
@@ -121,7 +121,7 @@ public class JobHistoryDelegate extends SpoonDelegate {
 		
 		final int FieldsRows=1;
 		colinf=new ColumnInfo[] {
-            new ColumnInfo(Messages.getString("JobHistory.Column.Name"),             ColumnInfo.COLUMN_TYPE_TEXT, true , true), //$NON-NLS-1$
+            new ColumnInfo(Messages.getString("JobHistory.Column.Name"),           ColumnInfo.COLUMN_TYPE_TEXT, true , true), //$NON-NLS-1$
             new ColumnInfo(Messages.getString("JobHistory.Column.BatchID"),        ColumnInfo.COLUMN_TYPE_TEXT, true , true), //$NON-NLS-1$
     		new ColumnInfo(Messages.getString("JobHistory.Column.Status"),         ColumnInfo.COLUMN_TYPE_TEXT, false, true), //$NON-NLS-1$
     		new ColumnInfo(Messages.getString("JobHistory.Column.Duration"),       ColumnInfo.COLUMN_TYPE_TEXT, true , true), //$NON-NLS-1$
@@ -377,9 +377,19 @@ public class JobHistoryDelegate extends SpoonDelegate {
                         database.shareVariablesWith(jobMeta);
                         database.connect();
                         
+                        RowMetaInterface jobLogMeta = Database.getJobLogrecordFields(false, jobMeta.isBatchIdUsed(), jobMeta.isLogfieldUsed());
+                        String sql = "SELECT ";
+                        for (int i=0;i<jobLogMeta.size();i++) {
+                        	if (i>0) sql+=", ";
+                        	sql+=jobLogMeta.getValueMeta(i).getName();
+                        }
+                        sql+=" FROM "+jobMeta.getLogTable();
+                        sql+=" WHERE jobName= ?";
+                        sql+=" ORDER BY ID_JOB DESC";
+                        
                         RowMetaAndData params = new RowMetaAndData();
                         params.addValue(new ValueMeta("transname", ValueMetaInterface.TYPE_STRING), jobMeta.getName()); //$NON-NLS-1$
-                        ResultSet resultSet = database.openQuery("SELECT * FROM "+jobMeta.getLogTable()+" WHERE JOBNAME = ? ORDER BY ID_JOB desc", params.getRowMeta(), params.getData()); //$NON-NLS-1$ //$NON-NLS-2$
+                        ResultSet resultSet = database.openQuery(sql, params.getRowMeta(), params.getData()); //$NON-NLS-1$ //$NON-NLS-2$
                         
                         rowList = new ArrayList<RowMetaAndData>();
                         Object[] rowData = database.getRow(resultSet);
@@ -398,6 +408,9 @@ public class JobHistoryDelegate extends SpoonDelegate {
                             wFields.table.clearAll();
                             
                             // OK, now that we have a series of rows, we can add them to the table view...
+                            // This needs to happen in a particular order and it also needs a few calculated fields.
+                            // As such, it's not that simple...
+                            //
                             for (int i=0;i<rowList.size();i++)
                             {
                                 RowMetaAndData row = rowList.get(i);
@@ -441,27 +454,37 @@ public class JobHistoryDelegate extends SpoonDelegate {
                                     colinf[13].setValueMeta(displayMeta.searchValueMeta("DEPDATE"));
                                     replayDateMeta = displayMeta.searchValueMeta("REPLAYDATE");
                                     colinf[14].setValueMeta(replayDateMeta);
-                                }
+                                }	
                                 
                                 TableItem item = new TableItem(wFields.table, SWT.NONE);
-                                String batchID = row.getString("ID_JOB", "");
-                                if(batchID != null)
-                                item.setText( 1, batchID);           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 2, Const.NVL( row.getString("STATUS", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 3, Const.NVL( row.getString("LINES_READ", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 4, Const.NVL( row.getString("LINES_WRITTEN", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 5, Const.NVL( row.getString("LINES_UPDATED", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 6, Const.NVL( row.getString("LINES_INPUT", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 7, Const.NVL( row.getString("LINES_OUTPUT", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 8, Const.NVL( row.getString("ERRORS", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText( 9, Const.NVL( row.getString("STARTDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText(10, Const.NVL( row.getString("ENDDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText(11, Const.NVL( row.getString("LOGDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
-                                item.setText(12, Const.NVL( row.getString("DEPDATE", ""), ""));   //$NON-NLS-1$ //$NON-NLS-2$
-                                String replayDate = row.getString("REPLAYDATE", ""); //$NON-NLS-1$ //$NON-NLS-2$
-                                if(replayDate == null)
-                                	replayDate = Const.NULL_STRING;
-								item.setText(13, replayDate);
+                                int idJobIndex = row.getRowMeta().indexOfValue("ID_JOB");
+                                String batchID = idJobIndex<0 ? "" : row.getString(idJobIndex, "");
+                                int colnr=1;
+
+                                // Duration : from DEPDATE to ENDDATE
+                                //
+                                Long duration = null;
+                                Date endDate = row.getDate("ENDDATE", null);
+                                Date depDate = row.getDate("DEPDATE", null);
+                                if (endDate!=null && depDate!=null) {
+                                	duration = (endDate.getTime() - depDate.getTime())/1000; // convert to seconds.
+                                }
+                                
+                                item.setText( colnr++, Const.NVL( row.getString("JOBNAME", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL(batchID, ""));           //$NON-NLS-1$
+                                item.setText( colnr++, Const.NVL( row.getString("STATUS", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, duration==null ? "" : duration.toString());           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LINES_READ", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LINES_WRITTEN", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LINES_UPDATED", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LINES_INPUT", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LINES_OUTPUT", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("ERRORS", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("STARTDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("ENDDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("LOGDATE", ""), ""));           //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("DEPDATE", ""), ""));   //$NON-NLS-1$ //$NON-NLS-2$
+                                item.setText( colnr++, Const.NVL( row.getString("REPLAYDATE", ""), ""));   //$NON-NLS-1$ //$NON-NLS-2$
                             }
                             
                             wFields.removeEmptyRows();
@@ -516,7 +539,8 @@ public class JobHistoryDelegate extends SpoonDelegate {
             String logging=null;
             try
             {
-                logging = row.getString("LOG_FIELD", ""); //$NON-NLS-1$ //$NON-NLS-2$
+            	int logFieldIndex = row.getRowMeta().indexOfValue("LOG_FIELD");
+                logging = logFieldIndex<0 ? "-" : row.getString(logFieldIndex, ""); //$NON-NLS-1$ $NON-NLS-2$ $NON-NLS-3$
             }
             catch (KettleValueException e)
             {
