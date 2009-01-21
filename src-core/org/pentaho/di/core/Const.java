9de5487fd1ba316452227f096b5081c72fab473c
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-01-21 15:15:09 +0000

    upping version to 3.1.3
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.1.3@10048 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -51,7 +51,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "3.1.2";
+	public static final String VERSION = "3.1.3";
 
 	/**
 	 * Sleep time waiting when buffer is empty
