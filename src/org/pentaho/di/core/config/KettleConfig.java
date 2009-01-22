db933d99d6712f1203044e8674ef3895bf3a919f
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-01-22 22:08:26 +0000

    Compile warning fix
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10060 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/config/KettleConfig.java
+++ src/org/pentaho/di/core/config/KettleConfig.java
@@ -105,7 +105,7 @@ public class KettleConfig
 		digester.addRule(KETTLE_CONFIG_PROPERTY, new SetPropertiesRule()
 		{
 			@Override
-			public void begin(Attributes attrs) throws Exception
+			public void begin(String name, String namespace, Attributes attrs) throws Exception
 			{
 				((TempConfig) digester.peek()).parms.put(attrs.getValue("name"), attrs.getValue("value"));
 			}
