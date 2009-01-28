bd27dc1105fd4a79487c2720a622dcbc589b0ad3
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-01-28 15:28:33 +0000

    PDI-2031HP Neoview: Database dialog - not possible to define a JNDI datasource
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.1.3@10084 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/database/NeoviewDatabaseMeta.java
+++ src-core/org/pentaho/di/core/database/NeoviewDatabaseMeta.java
@@ -65,7 +65,7 @@ public class NeoviewDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 		
 	public int[] getAccessTypeList()
 	{
-		return new int[] { DatabaseMeta.TYPE_ACCESS_NATIVE, DatabaseMeta.TYPE_ACCESS_ODBC};
+		return new int[] { DatabaseMeta.TYPE_ACCESS_NATIVE, DatabaseMeta.TYPE_ACCESS_ODBC, DatabaseMeta.TYPE_ACCESS_JNDI};
 	}
 	
 	public int getDefaultDatabasePort()
