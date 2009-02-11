4b905b1cc44277f1b04aab83b230a17278899800
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-02-11 18:09:33 +0000

    New step : exec process and return the result
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10159 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src/org/pentaho/di/trans/steps/execprocess/ExecProcess.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2007 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the GNU Lesser General Public License, Version 2.1. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.gnu.org/licenses/lgpl-2.1.txt. The Original Code is Pentaho 
+ * Data Integration.  The Initial Developer is Samatar Hassan.
+ *
+ * Software distributed under the GNU Lesser Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+*/
+package org.pentaho.di.trans.steps.execprocess;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowDataUtil;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStep;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.steps.fileexists.Messages;
+
+/**
+ * Execute a process
+ *  *  
+ * @author Samatar
+ * @since 03-11-2008
+ *
+ */
+
+public class ExecProcess extends BaseStep implements StepInterface
+{
+    private ExecProcessMeta meta;
+    private ExecProcessData data;
+    
+    public ExecProcess(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
+    {
+        super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+    }
+    
+   
+    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
+    {
+        meta=(ExecProcessMeta)smi;
+        data=(ExecProcessData)sdi;
+        
+        boolean sendToErrorRow=false;
+        String errorMessage = null;
+
+        Object[] r = getRow();      // Get row from input rowset & set row busy!
+        if (r==null)  // no more input to be expected...
+        {
+            setOutputDone();
+            return false;
+        }
+        
+        if(first)
+    	{
+    		first=false;
+			// get the RowMeta
+			data.previousRowMeta = getInputRowMeta().clone();
+			data.NrPrevFields=data.previousRowMeta.size();
+			data.outputRowMeta = data.previousRowMeta;
+			meta.getFields(data.outputRowMeta, getStepname(), null, null, this);
+			
+    		// Check is process field is provided
+			if (Const.isEmpty(meta.getProcessField()))
+			{
+				logError(Messages.getString("ExecProcess.Error.ProcessFieldMissing"));
+				throw new KettleException(Messages.getString("ExecProcess.Error.ProcessFieldMissing"));
+			}
+			
+			// cache the position of the field			
+			if (data.indexOfProcess<0)
+			{	
+				data.indexOfProcess =data.previousRowMeta .indexOfValue(meta.getProcessField());
+				if (data.indexOfProcess<0)
+				{
+					// The field is unreachable !
+					logError(Messages.getString("ExecProcess.Exception.CouldnotFindField")+ "[" + meta.getProcessField()+"]"); //$NON-NLS-1$ //$NON-NLS-2$
+					throw new KettleException(Messages.getString("ExecProcess.Exception.CouldnotFindField",meta.getProcessField())); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+			}
+    	}// End If first 
+
+    	Object[] outputRow = RowDataUtil.allocateRowData(data.outputRowMeta.size());
+		for (int i = 0; i < data.NrPrevFields; i++)
+		{
+			outputRow[i] = r[i];
+		}
+		// get process to execute
+    	String processString= data.previousRowMeta.getString(r,data.indexOfProcess);
+        
+        try
+        {
+         	if(Const.isEmpty(processString)) throw new KettleException(Messages.getString("ExecProcess.ProcessEmpty"));
+         	
+         	// execute and return result as String
+         	String resultString=execProcess(processString);
+         	
+        	// Add result field to input stream
+    		outputRow[data.NrPrevFields]= resultString;
+    		int rowIndex=data.NrPrevFields;
+    		rowIndex++;
+
+			 //	add new values to the row.
+	        putRow(data.outputRowMeta, outputRow);  // copy row to output rowset(s);
+         	
+	        if (log.isRowLevel()) log.logRowlevel(toString(), Messages.getString("ExecProcess.LineNumber",getLinesRead()+" : "+getInputRowMeta().getString(r)));
+        }
+        catch(KettleException e)
+        {
+        	if (getStepMeta().isDoingErrorHandling())
+        	{
+                 sendToErrorRow = true;
+                 errorMessage = e.toString();
+        	}
+        	else
+        	{
+	            logError(Messages.getString("ExecProcess.ErrorInStepRunning")+e.getMessage()); //$NON-NLS-1$
+	            setErrors(1);
+	            stopAll();
+	            setOutputDone();  // signal end to receiver(s)
+	            return false;
+        	}
+        	if (sendToErrorRow)
+        	{
+        	   // Simply add this row to the error row
+        	   putError(getInputRowMeta(), r, 1, errorMessage, meta.getResultFieldName(), "ExecProcess001");
+        	}
+        }
+            
+        return true;
+    }
+    private String execProcess(String process) throws KettleException
+    {
+   	String retval=null;
+   	
+   	Process p =null;
+   	try{
+   		
+   		p = Runtime.getRuntime().exec(process);
+   		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+   		String line;
+   		StringBuffer retvalBuff=new StringBuffer();
+   		
+   		while ((line = b.readLine()) != null) 
+   		{
+   			retvalBuff.append(line);
+   		}
+   		retval=retvalBuff.toString();
+   	}catch(Exception e){
+   		throw new KettleException(e); 
+   	}
+   	finally
+   	{
+   		if(p!=null) p.destroy();
+   	}
+   	return retval;	
+   }
+    
+    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta=(ExecProcessMeta)smi;
+        data=(ExecProcessData)sdi;
+
+        if (super.init(smi, sdi))
+        {
+        	if(Const.isEmpty(meta.getResultFieldName()))
+        	{
+        		log.logError(toString(), Messages.getString("ExecProcess.Error.ResultFieldMissing"));
+        		return false;
+        	}
+            return true;
+        }
+        return false;
+    }
+        
+    public void dispose(StepMetaInterface smi, StepDataInterface sdi)
+    {
+        meta = (ExecProcessMeta)smi;
+        data = (ExecProcessData)sdi;
+
+        super.dispose(smi, sdi);
+    }
+	  //
+    // Run is were the action happens!
+    public void run()
+    {
+    	if(log.isBasic()) logBasic(Messages.getString("ExecProcess.Log.StartingToRun")); //$NON-NLS-1$
+        
+        try
+        {
+            while (processRow(meta, data) && !isStopped());
+        }
+        catch(Exception e)
+        {
+            logError(Messages.getString("ExecProcess.Log.UnexpectedError")+" : "+e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+            logError(Const.getStackTracker(e));
+            setErrors(1);
+            stopAll();
+        }
+        finally
+        {
+            dispose(meta, data);
+            logSummary();
+            markStop();
+        }
+    }
+    
+}
---
+++ src/org/pentaho/di/trans/steps/execprocess/ExecProcessMeta.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2007 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the GNU Lesser General Public License, Version 2.1. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.gnu.org/licenses/lgpl-2.1.txt. The Original Code is Pentaho 
+ * Data Integration.  The Initial Developer is Samatar Hassan.
+ *
+ * Software distributed under the GNU Lesser Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+*/
+package org.pentaho.di.trans.steps.execprocess;
+
+import org.w3c.dom.Node;
+import java.util.List;
+import java.util.Map;
+
+import org.pentaho.di.core.CheckResult;
+import org.pentaho.di.core.CheckResultInterface;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.Counter;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMeta;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.shared.SharedObjectInterface;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStepMeta;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+
+
+
+/*
+ * Created on 03-11-2008
+ * 
+ */
+
+public class ExecProcessMeta extends BaseStepMeta implements StepMetaInterface
+{
+	
+    /** dynamic process field name */
+    private String       processfield;
+    
+    /** function result: new value name */
+    private String       resultfieldname;
+    
+    public ExecProcessMeta()
+    {
+        super(); // allocate BaseStepMeta
+    }
+
+    /**
+     * @return Returns the processfield.
+     */
+    public String getProcessField()
+    {
+        return processfield;
+    }
+
+    /**
+     * @param processfield The processfield to set.
+     */
+    public void setProcessField(String processfield)
+    {
+        this.processfield = processfield;
+    }
+
+    /**
+     * @return Returns the resultName.
+     */
+    public String getResultFieldName()
+    {
+        return resultfieldname;
+    }
+
+    /**
+     * @param resultfieldname The resultfieldname to set.
+     */
+    public void setResultFieldName(String resultfieldname)
+    {
+        this.resultfieldname = resultfieldname;
+    }
+    
+
+	public void loadXML(Node stepnode, List<DatabaseMeta> databases, Map<String, Counter> counters)
+	throws KettleXMLException
+	{
+		readData(stepnode, databases);
+	}
+ 
+
+    public Object clone()
+    {
+        ExecProcessMeta retval = (ExecProcessMeta) super.clone();
+       
+        return retval;
+    }
+
+    public void setDefault()
+    {
+        resultfieldname = "result"; //$NON-NLS-1$
+    }
+	public void getFields(RowMetaInterface inputRowMeta, String name, RowMetaInterface info[], StepMeta nextStep, VariableSpace space) throws KettleStepException
+	{    	
+        // Output fields (String)
+		 if (!Const.isEmpty(resultfieldname))
+	     {
+			 ValueMetaInterface v = new ValueMeta(space.environmentSubstitute(resultfieldname), ValueMeta.TYPE_STRING);
+			 v.setOrigin(name);
+			 inputRowMeta.addValueMeta(v);
+	     }
+    }
+	
+
+    public String getXML()
+    {
+        StringBuffer retval = new StringBuffer();
+
+        retval.append("    " + XMLHandler.addTagValue("processfield", processfield)); //$NON-NLS-1$ //$NON-NLS-2$
+        retval.append("    " + XMLHandler.addTagValue("resultfieldname", resultfieldname)); //$NON-NLS-1$ //$NON-NLS-2$
+        return retval.toString();
+    }
+
+    private void readData(Node stepnode, List<? extends SharedObjectInterface> databases)
+	throws KettleXMLException
+	{
+    	try{
+			processfield = XMLHandler.getTagValue(stepnode, "processfield"); //$NON-NLS-1$
+            resultfieldname = XMLHandler.getTagValue(stepnode, "resultfieldname"); 
+        }
+        catch (Exception e)
+        {
+            throw new KettleXMLException(Messages.getString("ExecProcessMeta.Exception.UnableToReadStepInfo"), e); //$NON-NLS-1$
+        }
+    }
+
+    public void readRep(Repository rep, long id_step, List<DatabaseMeta> databases, Map<String, Counter> counters)
+	throws KettleException
+	{
+        try
+        {
+        	processfield = rep.getStepAttributeString(id_step, "processfield"); //$NON-NLS-1$
+            resultfieldname = rep.getStepAttributeString(id_step, "resultfieldname"); //$NON-NLS-1$  
+        }
+        catch (Exception e)
+        {
+            throw new KettleException(Messages.getString("ExecProcessMeta.Exception.UnexpectedErrorReadingStepInfo"), e); //$NON-NLS-1$
+        }
+    }
+
+    public void saveRep(Repository rep, long id_transformation, long id_step) throws KettleException
+    {
+        try
+        {
+            rep.saveStepAttribute(id_transformation, id_step, "processfield", processfield); //$NON-NLS-1$
+            rep.saveStepAttribute(id_transformation, id_step, "resultfieldname", resultfieldname); //$NON-NLS-1$
+        }
+        catch (Exception e)
+        {
+            throw new KettleException(Messages.getString("ExecProcessMeta.Exception.UnableToSaveStepInfo") + id_step, e); //$NON-NLS-1$
+        }
+    }
+
+    public void check(List<CheckResultInterface> remarks, TransMeta transMeta, StepMeta stepMeta, RowMetaInterface prev, String input[], String output[], RowMetaInterface info)
+	{
+        CheckResult cr;
+        String error_message = ""; //$NON-NLS-1$
+
+      
+        if (Const.isEmpty(resultfieldname))
+        {
+            error_message = Messages.getString("ExecProcessMeta.CheckResult.ResultFieldMissing"); //$NON-NLS-1$
+            cr = new CheckResult(CheckResult.TYPE_RESULT_ERROR, error_message, stepMeta);
+        }
+        else
+        {
+            error_message = Messages.getString("ExecProcessMeta.CheckResult.ResultFieldOK"); //$NON-NLS-1$
+            cr = new CheckResult(CheckResult.TYPE_RESULT_OK, error_message, stepMeta);
+        }
+        remarks.add(cr);
+        
+        if (Const.isEmpty(processfield))
+        {
+            error_message = Messages.getString("ExecProcessMeta.CheckResult.ProcessFieldMissing"); //$NON-NLS-1$
+            cr = new CheckResult(CheckResult.TYPE_RESULT_ERROR, error_message, stepMeta);
+        }
+        else
+        {
+            error_message = Messages.getString("ExecProcessMeta.CheckResult.ProcessFieldOK"); //$NON-NLS-1$
+            cr = new CheckResult(CheckResult.TYPE_RESULT_OK, error_message, stepMeta);
+        }
+        remarks.add(cr);
+        
+        // See if we have input streams leading to this step!
+        if (input.length > 0)
+            cr = new CheckResult(CheckResult.TYPE_RESULT_OK, Messages.getString("ExecProcessMeta.CheckResult.ReceivingInfoFromOtherSteps"), stepMeta); //$NON-NLS-1$
+        else
+            cr = new CheckResult(CheckResult.TYPE_RESULT_ERROR, Messages.getString("ExecProcessMeta.CheckResult.NoInpuReceived"), stepMeta); //$NON-NLS-1$
+         remarks.add(cr);
+
+    }
+
+    public StepInterface getStep(StepMeta stepMeta, StepDataInterface stepDataInterface, int cnr, TransMeta transMeta, Trans trans)
+    {
+        return new ExecProcess(stepMeta, stepDataInterface, cnr, transMeta, trans);
+    }
+
+    public StepDataInterface getStepData()
+    {
+        return new ExecProcessData();
+    }
+
+    public boolean supportsErrorHandling()
+    {
+        return true;
+    }
+}
