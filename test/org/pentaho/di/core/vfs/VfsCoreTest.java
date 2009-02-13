d3180f6ff7865beeb2fc659eae2036a0fd40e590
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-02-13 16:43:49 +0000

    Extra VFS unit test
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10179 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ test/org/pentaho/di/core/vfs/VfsCoreTest.java
@@ -0,0 +1,53 @@
+package org.pentaho.di.core.vfs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.vfs.FileObject;
+import org.pentaho.di.core.Const;
+
+public class VfsCoreTest extends TestCase {
+
+	private static final String tmpDir = System.getProperty("java.io.tmpdir");
+	private static final String content = "Just a small line of text\n";
+
+	public void testWriteReadFile() throws IOException {
+		// Write a text
+		//
+		FileObject tempFile = KettleVFS.createTempFile("prefix", "suffix", tmpDir);
+		OutputStream outputStream = KettleVFS.getOutputStream(tempFile, false);
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+		writer.write(content);
+		writer.close();
+		outputStream.close();
+		
+		// Read it back...
+		//
+		InputStream inputStream = KettleVFS.getInputStream(tempFile);
+		StringBuffer buffer= new StringBuffer();
+		int c;
+		while ( (c=inputStream.read())>=0 ) {
+			buffer.append((char)c);
+		}
+		inputStream.close();
+		
+		assertEquals(content, buffer.toString());
+		
+		// Now open the data as a regular file...
+		//
+		String url = tempFile.getName().getURI();
+		String textFileContent = KettleVFS.getTextFileContent(url, Const.XML_ENCODING);
+		
+		// Now delete the file...
+		//
+		tempFile.delete();
+		
+		assertEquals(false, tempFile.exists());
+		
+		assertEquals(content, textFileContent);
+	}
+}
