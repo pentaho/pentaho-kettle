02fb4664ca457ee8dff01663628407c4af3e3fa3
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-03-02 21:14:08 +0000

    Fix runtime exception introduced in r10274 if you don't have any db or slave servers in a transformation
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10275 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/Spoon.java
+++ src-ui/org/pentaho/di/ui/spoon/Spoon.java
@@ -4026,7 +4026,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 					
 					String[] dbNames = new String[transMeta.nrDatabases()];
 					for (int i=0;i<dbNames.length;i++) dbNames[i]=transMeta.getDatabase(i).getName();
-					Arrays.sort(dbNames, 0, dbNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
+					Arrays.sort(dbNames, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
 
 					// Draw the connections themselves below it.
 					for (int i = 0; i < dbNames.length ; i++) {
@@ -4110,7 +4110,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 					// Put the slaves below it.
 					//
 					String[] slaveNames = transMeta.getSlaveServerNames();					
-					Arrays.sort(slaveNames, 0, slaveNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
+					Arrays.sort(slaveNames, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
 
 					for (int i = 0; i < slaveNames.length ; i++) {
 						SlaveServer slaveServer = transMeta.findSlaveServer(slaveNames[i]);

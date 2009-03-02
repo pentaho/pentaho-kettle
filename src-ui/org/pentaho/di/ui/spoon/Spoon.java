pentaho/pentaho-kettle

commit 82dac9270380cada67e8b6c1068526b8b0e95ef9

PDI-2134 : Update the "What's New in 3.2" wiki page
git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10274 5fb7f6ec-07c1-534a-b4ca-9155e429e800

src-ui/org/pentaho/di/ui/spoon/Spoon.java
@@ -4026,7 +4026,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 					
 					String[] dbNames = new String[transMeta.nrDatabases()];
 					for (int i=0;i<dbNames.length;i++) dbNames[i]=transMeta.getDatabase(i).getName();
-					Arrays.sort(dbNames);
+					Arrays.sort(dbNames, 0, dbNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
 
 					// Draw the connections themselves below it.
 					for (int i = 0; i < dbNames.length ; i++) {
@@ -4109,8 +4109,9 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 
 					// Put the slaves below it.
 					//
-					String[] slaveNames = transMeta.getSlaveServerNames();
-					Arrays.sort(slaveNames);
+					String[] slaveNames = transMeta.getSlaveServerNames();					
+					Arrays.sort(slaveNames, 0, slaveNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
+
 					for (int i = 0; i < slaveNames.length ; i++) {
 						SlaveServer slaveServer = transMeta.findSlaveServer(slaveNames[i]);
 						TreeItem tiSlave = new TreeItem(tiSlaveTitle, SWT.NONE);

docs/English/welcome/kettle_document_map.html
@@ -9,14 +9,14 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>Welcome to Pentaho Data Integration: Kettle Project</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration delivers powerful Extraction, Transformation, and Loading (ETL) capabilities using an innovative, metadata-driven approach.  You are currently using Spoon, the graphical designer for Pentaho Data Integration Jobs and Transformations. If you are new to Pentaho Data Integration, please use the links below to learn more and become active in the community.</p>
 
 <div align="center"><h1><b><em><u><a href="http://wiki.pentaho.com/display/EAI/Getting+started">Get started</a>!</u></em></b></h1></div>
 
 <h2>Documentation Links</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">What's new in Pentaho Data Integration 3.1?</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">What's new in Pentaho Data Integration 3.2?</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Spoon User Guide</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Steps documentation</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Job entries documentation</a></li>

docs/Francais/welcome/kettle_document_map.html
@@ -22,7 +22,7 @@ devenir un membre actif de la communauté.</p>
 
 <h2>Liens concernant la documentation</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">Quoi de neuf dans Pentaho Data Integration 3.1 (Anglais)</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">Quoi de neuf dans Pentaho Data Integration 3.2 (Anglais)</a></li>
 <li><a href="../Introduction_PDI.pdf">Introduction (PDF)</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Manuel de l'utilisateur de Spoon (Anglais)</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Documentation des étapes (Anglais)</a></li>

docs/Korean/welcome/kettle_document_map.html
@@ -9,14 +9,14 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>Pentaho Data Integration: Kettle 프로젝트에 오신 것을 환영합니다.</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration 은 혁신적이고 메타데이터 기반의 강력한 추출, 변환, 적재 (ETL) 기능을 제공합니다. 당신은 현재 Pentaho Data Integration 의 Job과 Transformation을 위한 그래픽 기반 디자이너인 Spoon을 사용하고 있습니다. Pentaho Data Integration을 처음 사용하신다면, 아래 링크를 방문하여 더 많은 정보를 얻고, 커뮤니티에 참여해 보십시오.</p>
 
 <div align="center"><h1><b><em><u><a href="http://wiki.pentaho.com/display/EAI/Getting+started">Get started!</a>!</u></em></b></h1></div>
 
 <h2>문서 링크</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">What's new in Pentaho Data Integration 3.1?</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">What's new in Pentaho Data Integration 3.2?</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Spoon User Guide</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Steps documentation</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Job entries documentation</a></li>

docs/Nederlands/welcome/kettle_document_map.html
@@ -9,14 +9,14 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>Welkom bij Pentaho Data Integration: het Kettle projekt</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration geeft U krachtige Extractie, Transformatie en Laden (ETL) mogelijkheden door middel van een innovatieve, metadata-gedreven aanpak.   U gebruikt hiervoor momenteel Spoon, de grafische ontwerp omgeving van Pentaho Data Integration. U kan met Spoon Jobs and Transformaties bewerken.  Als U nog niet bekend bent met Pentaho Data Integration, dan kan U de links hieronder gebruiken om meer te leren en om deel te nemen in de open source community.</p>
 
 <div align="center"><h1><b><em><u><a href="http://wiki.pentaho.com/display/EAI/Getting+started">Get started</a>!</u></em></b></h1></div>
 
 <h2>Documentation Links</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">Wat is er nieuw in Kettle 3.1?</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">Wat is er nieuw in Kettle 3.2?</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Spoon gebruikersdocumentatie</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Steps documentatie</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Job entries documentatie</a></li>

docs/cn_ZN/welcome/kettle_document_map.html
@@ -9,14 +9,14 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>欢迎使用Pentaho Data Integration: Kettle Project</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration 以元数据驱动的方式提供强大的抽取、转换和加载(ETL) 能力.  你当前正在使用Spoon, 这个图形化的设计工具是针对Pentaho数据集成任务和转换的. 如果你对于Pentaho Data Integration不熟悉的话, 请使用下面的链接来学习更多的内容，并且在团体里变得更加活跃.</p>
 
 <div align="center"><h1><b><em><u><a href="http://wiki.pentaho.com/display/EAI/Getting+started">Get started</a>!</u></em></b></h1></div>
 
 <h2>文档链接</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">What's new in Pentaho Data Integration 3.1?</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">What's new in Pentaho Data Integration 3.2?</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Spoon User Guide</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Steps documentation</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Job entries documentation</a></li>

docs/es_AR/welcome/kettle_document_map.html
@@ -9,14 +9,14 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>Welcome to Pentaho Data Integration: Kettle Project</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration delivers powerful Extraction, Transformation, and Loading (ETL) capabilities using an innovative, metadata-driven approach.  You are currently using Spoon, the graphical designer for Pentaho Data Integration Jobs and Transformations. If you are new to Pentaho Data Integration, please use the links below to learn more and become active in the community.</p>
 
 <div align="center"><h1><b><em><u><a href="http://wiki.pentaho.com/display/EAI/Getting+started">Get started</a>!</u></em></b></h1></div>
 
 <h2>Documentation Links</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">What's new in Pentaho Data Integration 3.1?</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">What's new in Pentaho Data Integration 3.2?</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Spoon User Guide</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Steps documentation</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Job entries documentation</a></li>

docs/it_IT/welcome/kettle_document_map.html
@@ -9,7 +9,7 @@
 <body class="body_doc">
 <div class="logo"><a href="http://www.pentaho.org" target="_blank"><img src="images/pentaho_logo.png" alt="Pentaho - Open Source Business Intelligence" width="220" height="65" border="0"></a></div>
 <h1>Benvenuto in Pentaho Data Integration</h1>
-Spoon v3.1
+Spoon v3.2
 <p>Pentaho Data Integration fornisce le funzionalità ETL (Estrazione,
 Transformazione, e caricamento 'Loading' dei database) utilizzando un approccio innovativo
 basato sul concetto di META-dati. In questo momento state utilizzando
@@ -21,7 +21,7 @@ fate riferimento ai link scritti sotto per saperne di più e per diventare membr
 
 <h2>Link sulla documentazione (in lingua inglese)</h2>
 <ul>
-<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.1">Novità in Pentaho Data Integration 3.0</a></li>
+<li><a href="http://wiki.pentaho.com/display/EAI/What%27s+new+in+PDI+version+3.2">Novità in Pentaho Data Integration 3.2</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Spoon+User+Guide">Manuale di Spoon</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Steps">Documentazione sui passi</a></li>
 <li><a href="http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+Job+Entries">Documentazione sulle job entry</a></li>

commit 02fb4664ca457ee8dff01663628407c4af3e3fa3

Fix runtime exception introduced in r10274 if you don't have any db or slave servers in a transformation
git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10275 5fb7f6ec-07c1-534a-b4ca-9155e429e800

src-ui/org/pentaho/di/ui/spoon/Spoon.java
@@ -4026,7 +4026,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 					
 					String[] dbNames = new String[transMeta.nrDatabases()];
 					for (int i=0;i<dbNames.length;i++) dbNames[i]=transMeta.getDatabase(i).getName();
-					Arrays.sort(dbNames, 0, dbNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
+					Arrays.sort(dbNames, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
 
 					// Draw the connections themselves below it.
 					for (int i = 0; i < dbNames.length ; i++) {
@@ -4110,7 +4110,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
 					// Put the slaves below it.
 					//
 					String[] slaveNames = transMeta.getSlaveServerNames();					
-					Arrays.sort(slaveNames, 0, slaveNames.length-1, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
+					Arrays.sort(slaveNames, new Comparator<String>() { public int compare(String o1, String o2) { return o1.compareToIgnoreCase(o2); } });
 
 					for (int i = 0; i < slaveNames.length ; i++) {
 						SlaveServer slaveServer = transMeta.findSlaveServer(slaveNames[i]);