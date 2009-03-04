0b204caf28cf5723ea2a217a9beb389387e04f7d
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-03-04 10:44:14 +0000

    PDI-2112 : Export of repository results in an invalid xml-File PDI-2105 : "Export to Repository" job entry stores same item multiple times (depending on directory depth. yes, bizarre stuff...)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10294 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/repository/Repository.java
+++ src/org/pentaho/di/repository/Repository.java
@@ -4283,7 +4283,7 @@ public class Repository
     	try
         {
             os = new BufferedOutputStream(KettleVFS.getOutputStream(xmlFilename, false));
-            writer = new OutputStreamWriter(os);
+            writer = new OutputStreamWriter(os, Const.XML_ENCODING);
             
 	        if (monitor!=null) monitor.beginTask("Exporting the repository to XML...", 3);
 	        
@@ -4365,9 +4365,6 @@ public class Repository
 	                    log.logError(toString(), "Job ["+jobs[i]+"] from directory ["+repdir+"] was not exported because of a loading error!");
 	                }
 	            }
-	            
-	            // OK, then export the jobs in the sub-directories as well!
-	            if (repdir.getID()!=dirTree.getID()) exportJobs(null, repdir, writer);
 	        }
     	} catch(Exception e) {
     		throw new KettleException("Error while exporting repository jobs", e);
@@ -4406,9 +4403,6 @@ public class Repository
 	                    log.logError(toString(), "Transformation ["+trans[i]+"] from directory ["+repdir+"] was not exported because of a loading error!");
 	                }
 	            }
-	            
-	            // OK, then export the transformations in the sub-directories as well!
-	            if (repdir.getID()!=dirTree.getID()) exportTransformations(null, repdir, writer);
 	        }
 	        if (monitor!=null) monitor.worked(1);
 	        
