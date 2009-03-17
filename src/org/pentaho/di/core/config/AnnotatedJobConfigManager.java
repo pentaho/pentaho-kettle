476597a81fdf606cc26eee6b8a96dac7344d6e7b
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-03-17 19:17:43 +0000

    Detailed level log fix
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10394 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/core/config/AnnotatedJobConfigManager.java
+++ src/org/pentaho/di/core/config/AnnotatedJobConfigManager.java
@@ -82,7 +82,7 @@ import org.pentaho.di.job.entry.Messages;
 			String name = BaseMessages.getString(packageName, job.name());
 			if (name.startsWith("!") && name.endsWith("!")) name=Messages.getString(job.name());
 			LogWriter.getInstance().setLogLevel(oldLogLevel); // restore loglevel, when the last alternative fails, log it when loglevel is detailed
-			if (name.startsWith("!") && name.endsWith("!")) name=BaseMessages.getString(altPackageName, job.name());
+			if (!Const.isEmpty(job.name()) && name.startsWith("!") && name.endsWith("!")) name=BaseMessages.getString(altPackageName, job.name());
 			if (name.startsWith("!") && name.endsWith("!")) name=job.name(); // Nothing to translate, keep it like it is. 
 			
 			// Determine the i18n tool tip text for the step (the extended description)
