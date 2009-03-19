f0860d7ca14857563faf2ce07199d37c5e8b975e
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-03-19 15:31:30 +0000

    Enhancements of Vertica DB meta: prevent full table scans when executing table exists disable features that are not supported fully enumerate reserved words return JDBC driver jar name in getUsedLibraries()
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10429 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-db/org/pentaho/di/core/database/VerticaDatabaseMeta.java
+++ src-db/org/pentaho/di/core/database/VerticaDatabaseMeta.java
@@ -17,10 +17,12 @@ import org.pentaho.di.core.Const;
 import org.pentaho.di.core.row.ValueMetaInterface;
 
 /**
- * Contains Generic Database Connection information through static final members 
+ * Contains Vertica Analytic Database information through static final members 
  * 
+ * @author DEinspanjer
+ * @since  2009-03-16
  * @author Matt
- * @since  11-mrt-2005
+ * @since  May-2008
  */
 public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInterface
 {
@@ -58,18 +60,6 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 	public int[] getAccessTypeList()
 	{
 		return new int[] { DatabaseMeta.TYPE_ACCESS_NATIVE, DatabaseMeta.TYPE_ACCESS_ODBC, DatabaseMeta.TYPE_ACCESS_JNDI };
-	}
-	
-	/**
-	 * @see DatabaseInterface#getNotFoundTK(boolean)
-	 */
-	public int getNotFoundTK(boolean use_autoinc)
-	{
-		if ( supportsAutoInc() && use_autoinc)
-		{
-			return 1;
-		}
-		return super.getNotFoundTK(use_autoinc);
 	}
 	
 	public String getDriverClass()
@@ -128,7 +118,7 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 	 */
 	public String getTruncateTableStatement(String tableName)
 	{
-	    return "DELETE FROM "+tableName;
+	    throw new UnsupportedOperationException("Truncate not supported.");
 	}
 
 
@@ -146,7 +136,7 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 	 */
 	public String getAddColumnStatement(String tablename, ValueMetaInterface v, String tk, boolean use_autoinc, String pk, boolean semicolon)
 	{
-		return "ALTER TABLE "+tablename+" ADD "+getFieldDefinition(v, tk, pk, use_autoinc, true, false);
+		return "--NOTE: Table cannot be altered unless all projections are dropped.\nALTER TABLE "+tablename+" ADD "+getFieldDefinition(v, tk, pk, use_autoinc, true, false);
 	}
 
 	/**
@@ -161,7 +151,7 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 	 */
 	public String getModifyColumnStatement(String tablename, ValueMetaInterface v, String tk, boolean use_autoinc, String pk, boolean semicolon)
 	{
-		return "ALTER TABLE "+tablename+" MODIFY "+getFieldDefinition(v, tk, pk, use_autoinc, true, false);
+		return "--NOTE: Table cannot be altered unless all projections are dropped.\nALTER TABLE "+tablename+" MODIFY "+getFieldDefinition(v, tk, pk, use_autoinc, true, false);
 	}
 
 	public String getFieldDefinition(ValueMetaInterface v, String tk, String pk, boolean use_autoinc, boolean add_fieldname, boolean add_cr)
@@ -179,10 +169,7 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 		switch(type)
 		{
 		case ValueMetaInterface.TYPE_DATE   : retval+="TIMESTAMP"; break;
-		
-		// XXX: Vertica supports BOOLEAN properly, but it seems like most 
-		// DBMeta classes just use CHAR(1) so I didn't change this.
-		case ValueMetaInterface.TYPE_BOOLEAN: retval+="CHAR(1)"; break;
+		case ValueMetaInterface.TYPE_BOOLEAN: retval+="BOOLEAN"; break;
 		case ValueMetaInterface.TYPE_NUMBER : 
         case ValueMetaInterface.TYPE_BIGNUMBER:
         	retval+="FLOAT";
@@ -205,6 +192,134 @@ public class VerticaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInt
 
     public String[] getUsedLibraries()
     {
+        return new String[] { "vertica_2.5_jdk_5.jar" };
+    }
+
+    @Override
+    public int getDefaultDatabasePort()
+    {
+        return 5433;
+    }
+
+    @Override
+    public String getLimitClause(int nrRows)
+    {
+        return " LIMIT "+nrRows;
+    }
+
+    @Override
+    public int getMaxTextFieldLength()
+    {
+        return 0;
+    }
+
+    @Override
+    public int getMaxVARCHARLength()
+    {
+        return 4000;
+    }
+
+    @Override
+    public String[] getReservedWords()
+    {
+        return new String[]
+                          {
+                              // From "SQL Reference Manual.pdf" found on support.vertica.com
+                              "ABORT", "ABSOLUTE", "ACCESS", "ACTION", "ADD", "AFTER", "AGGREGATE", 
+                              "ALL", "ALSO", "ALTER", "ANALYSE", "ANALYZE", "AND", "ANY", 
+                              "ARRAY", "AS", "ASC", "ASSERTION", "ASSIGNMENT", "AT", "AUTHORIZATION", 
+                              "BACKWARD", "BEFORE", "BEGIN", "BETWEEN", "BIGINT", "BINARY", "BIT", 
+                              "BLOCK_DICT", "BLOCKDICT_COMP", "BOOLEAN", "BOTH", "BY", 
+                              "CACHE", "CALLED", "CASCADE", "CASE", "CAST", "CATALOG_PATH", "CHAIN", "CHAR", "CHARACTER", 
+                              "CHARACTERISTICS", "CHECK", "CHECKPOINT", "CLASS", "CLOSE", "CLUSTER", 
+                              "COALESCE", "COLLATE", "COLUMN", "COMMENT", "COMMIT", "COMMITTED", "COMMONDELTA_COMP", 
+                              "CONSTRAINT", "CONSTRAINTS", "CONVERSION", "CONVERT", "COPY", "CORRELATION", 
+                              "CREATE", "CREATEDB", "CREATEUSER", "CROSS", "CSV", "CURRENT_DATABASE", 
+                              "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "CYCLE", 
+                              "DATA", "DATABASE", "DATAPATH", "DAY", "DEALLOCATE", "DEC", "DECIMAL", "DECLARE", "DEFAULT", "DEFAULTS",
+                              "DEFERRABLE", "DEFERRED", "DEFINER", "DELETE", "DELIMITER", "DELIMITERS", 
+                              "DELTARANGE_COMP", "DELTARANGE_COMP_SP", "DELTAVAL", 
+                              "DESC", "DETERMINES", "DIRECT", "DISTINCT", "DISTVALINDEX", "DO", "DOMAIN", "DOUBLE", "DROP", 
+                              "EACH", "ELSE", "ENCODING", "ENCRYPTED", "END", "EPOCH", "ERROR", "ESCAPE", 
+                              "EXCEPT", "EXCEPTIONS", "EXCLUDING", "EXCLUSIVE", "EXECUTE", "EXISTS", "EXPLAIN", "EXTERNAL", "EXTRACT", 
+                              "FALSE", "FETCH", "FIRST", "FLOAT", "FOR", "FORCE", "FOREIGN", "FORWARD", "FREEZE", "FROM", "FULL", "FUNCTION",  
+                              "GLOBAL", "GRANT", "GROUP", 
+                              "HANDLER", "HAVING", "HOLD", "HOUR", 
+                              "ILIKE", "IMMEDIATE", "IMMUTABLE", "IMPLICIT", "IN", "IN_P", "INCLUDING", "INCREMENT", "INDEX", "INHERITS",
+                              "INITIALLY", "INNER", "INOUT", "INPUT", "INSENSITIVE", "INSERT", "INSTEAD", 
+                              "INT", "INTEGER", "INTERSECT", "INTERVAL", "INTO", "INVOKER", "IS", "ISNULL", "ISOLATION", 
+                              "JOIN", 
+                              "KEY",  
+                              "LANCOMPILER", "LANGUAGE", "LARGE", "LAST", "LATEST", "LEADING", "LEFT", "LESS", "LEVEL", 
+                              "LIKE", "LIMIT", "LISTEN", "LOAD", "LOCAL", "LOCALTIME", "LOCALTIMESTAMP", "LOCATION", "LOCK", 
+                              "MATCH", "MAXVALUE", "MERGEOUT", "MINUTE", "MINVALUE", "MOBUF", "MODE", 
+                              "MONTH", "MOVE", "MOVEOUT", "MULTIALGORITHM_COMP", "MULTIALGORITHM_COMP_SP", 
+                              "NAMES", "NATIONAL", "NATURAL", "NCHAR", "NEW", "NEXT", "NO", "NOCREATEDB", 
+                              "NOCREATEUSER", "NODE", "NODES", "NONE",  
+                              "NOT", "NOTHING", "NOTIFY", "NOTNULL", "NOWAIT", "NULL", "NULLIF", "NUMERIC", 
+                              "OBJECT", "OF", "OFF", "OFFSET", "OIDS", "OLD", "ON", "ONLY", "OPERATOR", "OPTION", "OR", 
+                              "ORDER", "OUT", "OUTER", "OVERLAPS", "OVERLAY", "OWNER", 
+                              "PARTIAL", "PARTITION", "PASSWORD", "PLACING", "POSITION", "PRECISION", "PREPARE", "PRESERVE",
+                              "PRIMARY", "PRIOR", "PRIVILEGES", "PROCEDURAL", "PROCEDURE", "PROJECTION", 
+                              "QUOTE", 
+                              "READ", "REAL", "RECHECK", "RECORD", "RECOVER", "REFERENCES", "REFRESH", "REINDEX", "REJECTED", 
+                              "RELATIVE", "RELEASE", "RENAME", "REPEATABLE", "REPLACE", "RESET", "RESTART", "RESTRICT", "RETURNS", 
+                              "REVOKE", "RIGHT", "RLE", "ROLLBACK", "ROW", "ROWS", "RULE", 
+                              "SAVEPOINT", "SCHEMA", "SCROLL", "SECOND", "SECURITY", "SEGMENTED", "SELECT", "SEQUENCE", 
+                              "SERIALIZABLE", "SESSION", "SESSION_USER", "SET", "SETOF", "SHARE", "SHOW", 
+                              "SIMILAR", "SIMPLE", "SMALLINT", "SOME", "SPLIT", "STABLE", "START", "STATEMENT", "STATISTICS",
+                              "STDIN", "STDOUT", "STORAGE", "STRICT", "SUBSTRING", "SYSID",  
+                              "TABLE", "TABLESPACE", "TEMP", "TEMPLATE", "TEMPORARY", "TERMINATOR", 
+                              "THAN", "THEN", "TIME", "TIMESTAMP", "TIMESTAMPTZ", "TIMETZ", "TO", "TOAST", 
+                              "TRAILING", "TRANSACTION", "TREAT", "TRIGGER", "TRIM", "TRUE", "TRUE_P", "TRUNCATE", "TRUSTED", "TYPE", 
+                              "UNCOMMITTED", "UNENCRYPTED", "UNION", "UNIQUE", "UNKNOWN", "UNLISTEN", "UNSEGMENTED", 
+                              "UNTIL", "UPDATE", "USAGE", "USER", "USING", 
+                              "VACUUM", "VALID", "VALIDATOR", "VALINDEX", "VALUES", "VARCHAR", "VARYING", "VERBOSE", "VIEW", "VOLATILE", 
+                              "WHEN", "WHERE", "WITH", "WITHOUT", "WORK", "WRITE", 
+                              "YEAR", 
+                              "ZONE"
+                          };    }
+
+    @Override
+    public String getSQLColumnExists(String columnname, String tablename)
+    {
+        return super.getSQLColumnExists(columnname, tablename) + getLimitClause(1);
+    }
+
+    @Override
+    public String getSQLQueryFields(String tableName)
+    {
+        return super.getSQLQueryFields(tableName) + getLimitClause(1);
+    }
+
+    @Override
+    public String getSQLTableExists(String tablename)
+    {
+        return super.getSQLTableExists(tablename) + getLimitClause(1);
+    }
+
+    @Override
+    public String[] getViewTypes()
+    {
         return new String[] {};
+    }
+
+    @Override
+    public boolean supportsAutoInc()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean supportsBooleanDataType()
+    {
+        return true;
+    }
+    
+    @Override
+    public boolean supportsViews()
+    {
+    	//TODO: This will change in Vertica 3.0
+        return false;
     }
 }
