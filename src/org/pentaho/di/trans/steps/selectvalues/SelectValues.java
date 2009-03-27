a355015f37e9d7943400938fe5a17304eeedcd1d
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-03-27 16:35:59 +0000

    Small error level logging fix
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10476 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/selectvalues/SelectValues.java
+++ src/org/pentaho/di/trans/steps/selectvalues/SelectValues.java
@@ -374,7 +374,7 @@ public class SelectValues extends BaseStep implements StepInterface
         // Send the row on its way
 		putRow(data.metadataRowMeta, outputData);
         
-		if (log.isRowLevel()) logRowlevel(Messages.getString("SelectValues.Log.WroteRowToNextStep")+data.metadataRowMeta.getString(rowData)); //$NON-NLS-1$
+		if (log.isRowLevel()) logRowlevel(Messages.getString("SelectValues.Log.WroteRowToNextStep")+data.metadataRowMeta.getString(outputData)); //$NON-NLS-1$
 
         if (checkFeedback(getLinesRead())) logBasic(Messages.getString("SelectValues.Log.LineNumber")+getLinesRead()); //$NON-NLS-1$
 			
