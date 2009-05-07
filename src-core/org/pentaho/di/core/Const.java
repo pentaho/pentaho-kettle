3ab1abbed4b60794263a53aeda3be44ddf938e70
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-05-07 15:59:23 +0000

    PDI-2162 : Upping the version to 3.2.0
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10555 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -53,7 +53,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "3.2.0-RC1";
+	public static final String VERSION = "3.2.0";
 
 	/**
 	 * Sleep time waiting when buffer is empty
