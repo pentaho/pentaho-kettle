f16e06a1146a5f8489b3c776b1fb8b02c9e22bf4
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-05-18 19:00:11 +0000

    [PDI-2399] Updated the version in the new 3.1.5 branch
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.1.5@10598 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -51,7 +51,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "3.1.4";
+	public static final String VERSION = "3.1.5";
 
 	/**
 	 * Sleep time waiting when buffer is empty
