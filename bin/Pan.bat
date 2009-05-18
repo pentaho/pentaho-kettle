aa2ee59354d4e88f51f5c278066c5612a098cd2b
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-05-18 14:02:02 +0000

    PDI-2395: RSS Input on Windows systems throws: java.lang.ClassNotFoundException: it.sauronsoftware.feed4j.FeedParser
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10593 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- bin/Pan.bat
+++ bin/Pan.bat
@@ -36,8 +36,10 @@ FOR %%F IN (libext\commons\*.jar) DO call :addcp %%F
 FOR %%F IN (libext\web\*.jar) DO call :addcp %%F
 FOR %%F IN (libext\pentaho\*.jar) DO call :addcp %%F
 FOR %%F IN (libext\spring\*.jar) DO call :addcp %%F
+FOR %%F IN (libext\jfree\*.jar) DO call :addcp %%F
 FOR %%F IN (libext\mondrian\*.jar) DO call :addcp %%F
 FOR %%F IN (libext\salesforce\*.jar) DO call :addcp %%F
+FOR %%F IN (libext\feeds\*.jar) DO call :addcp %%F
 
 goto extlibe
 
