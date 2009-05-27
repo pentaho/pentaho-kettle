a15567b79c231154b36cfa64131cbafbf8ca0e66
pstoellberge <pstoellberge@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-05-27 16:05:29 +0000

    PDI-2423: Evaluate row number in a table - save repository bug
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@10615 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/entries/evaluatetablecontent/JobEntryEvalTableContent.java
+++ src/org/pentaho/di/job/entries/evaluatetablecontent/JobEntryEvalTableContent.java
@@ -226,7 +226,7 @@ public class JobEntryEvalTableContent extends JobEntryBase implements Cloneable,
 
 			schemaname = rep.getJobEntryAttributeString(id_jobentry, "schemaname");
 			tablename = rep.getJobEntryAttributeString(id_jobentry, "tablename");
-			successCondition = getSuccessConditionByCode(Const.NVL(rep.getStepAttributeString(id_jobentry,"success_condition"), ""));
+			successCondition = getSuccessConditionByCode(Const.NVL(rep.getJobEntryAttributeString(id_jobentry,"success_condition"), ""));
 			limit = rep.getJobEntryAttributeString(id_jobentry, "limit");
 			iscustomSQL = rep.getJobEntryAttributeBoolean(id_jobentry, "is_custom_sql");
 			isUseVars = rep.getJobEntryAttributeBoolean(id_jobentry, "is_usevars");
@@ -268,7 +268,7 @@ public class JobEntryEvalTableContent extends JobEntryBase implements Cloneable,
 
 			rep.saveJobEntryAttribute(id_job, getID(), "schemaname", schemaname);
 			rep.saveJobEntryAttribute(id_job, getID(), "tablename", tablename);
-			rep.saveStepAttribute(id_job, getID(),"success_condition", getSuccessConditionCode(successCondition));
+			rep.saveJobEntryAttribute(id_job, getID(),"success_condition", getSuccessConditionCode(successCondition));
 			rep.saveJobEntryAttribute(id_job, getID(), "limit", limit); 
 			rep.saveJobEntryAttribute(id_job, getID(), "custom_sql", customSQL);
 			rep.saveJobEntryAttribute(id_job, getID(), "is_custom_sql", iscustomSQL);
