df4ac783b05ecd62e448cd86e2bd8faa0fce7bbc
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-05-29 13:42:54 +0000

    PDI-2439: RSS Input can throw ArrayIndexOutOfBoundsException when URL is defined in a field
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.1@10620 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/rssinput/RssInput.java
+++ src/org/pentaho/di/trans/steps/rssinput/RssInput.java
@@ -171,7 +171,7 @@ public class RssInput extends BaseStep implements StepInterface
 		// Create new row
 		Object[] outputRowData = buildEmptyRow();
 			
-		if (data.readrow!=null) outputRowData = data.readrow.clone();
+		if (data.readrow!=null) System.arraycopy(data.readrow, 0, outputRowData, 0, data.readrow.length);
 				
 		try{
 			
