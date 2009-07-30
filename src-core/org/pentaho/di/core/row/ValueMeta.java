0aa2e53352f0491e4067678f6a7d28bfd2c219af
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-07-30 09:37:40 +0000

    PDI-2572 : Dimension lookup/update: problem with trailing spaces in keys
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.1@10753 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/row/ValueMeta.java
+++ src-core/org/pentaho/di/core/row/ValueMeta.java
@@ -2879,8 +2879,8 @@ public class ValueMeta implements ValueMetaInterface
         case TYPE_STRING:
             {
             	// if (isStorageBinaryString() && identicalFormat && storageMetadata.isSingleByteEncoding()) return compareBinaryStrings((byte[])data1, (byte[])data2); TODO
-            	String one = Const.rtrim(getString(data1));
-                String two = Const.rtrim(getString(data2));
+            	String one = getString(data1);
+                String two = getString(data2);
     
                 if (caseInsensitive)
                 {
@@ -2944,10 +2944,12 @@ public class ValueMeta implements ValueMetaInterface
                     cmp = b1[i] - b2[i];
                     if (cmp!=0)
                     {
-                        cmp = Math.abs(cmp);
+                        cmp = cmp < 0 ? -1 : 1;
                         break;
                     }
                 }
+                
+                cmp = b1.length - b2.length; 
             }
             break;
         default: 
