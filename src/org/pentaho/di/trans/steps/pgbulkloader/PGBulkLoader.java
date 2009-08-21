046ecbb56b130bc2404295d634a001ea5c1d5ca7
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2009-08-21 11:19:16 +0000

    PDI-1901:Patch for Postgres Dump
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.2@10884 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/pgbulkloader/PGBulkLoader.java
+++ src/org/pentaho/di/trans/steps/pgbulkloader/PGBulkLoader.java
@@ -152,7 +152,8 @@ public class PGBulkLoader extends BaseStep implements StepInterface
 	   }
 	   else
 	   {
-		   throw new KettleException("No psql application specified");
+		   if(log.isDetailed()) log.logDetailed(toString(), "psql defaults to system path");
+		   sb.append("psql");
 	   }
 
        DatabaseMeta dm = meta.getDatabaseMeta();
@@ -169,8 +170,8 @@ public class PGBulkLoader extends BaseStep implements StepInterface
 
            // Hostname and portname
            //
-           String hostname  = Const.NVL(dm.getHostname(), "");
-           String portnum  = Const.NVL(dm.getDatabasePortNumberString(), "");
+           String hostname = environmentSubstitute(Const.NVL(dm.getHostname(), ""));
+           String portnum = environmentSubstitute(Const.NVL(dm.getDatabasePortNumberString(), ""));
            sb.append(" -h ");
            sb.append(hostname);
            sb.append(" -p ");
