8b011b645878080909a5c46dafe3cd3d0766fdc2
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-01-22 18:33:12 +0000

    Enlarging window to preferred minimum size should happen before the check to make sure a window isn't too big.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@11665 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/gui/WindowProperty.java
+++ src-ui/org/pentaho/di/ui/core/gui/WindowProperty.java
@@ -104,13 +104,25 @@ public class WindowProperty
         Rectangle entireClientArea = shell.getDisplay().getClientArea();
         Rectangle resizedRect = Geometry.copy(shRect);
         constrainRectangleToContainer(resizedRect, entireClientArea);
+
+        // What is the preferred size of this dialog?
+        // This computed is a minimum.  If the minimum is smaller than the 
+        // size of the current shell, we make it larger.
+        //
+		Point computedSize = shell.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+		Rectangle shellSize = shell.getBounds();
+		if (shellSize.width<computedSize.x) shellSize.width=computedSize.x;
+		if (shellSize.height<computedSize.y) shellSize.height=computedSize.y;
+		shell.setBounds(shellSize);     
         
-        /* If the persisted size/location doesn't perfectly fit
-         * into the entire client area, the persisted settings
-         * likely were not meant for this configuration of monitors.
-         * Relocate the shell into either the parent monitor or if
-         * there is no parent, the primary monitor then center it.
-         */
+
+
+		// If the persisted size/location doesn't perfectly fit
+		// into the entire client area, the persisted settings
+		// likely were not meant for this configuration of monitors.
+		// Relocate the shell into either the parent monitor or if
+		// there is no parent, the primary monitor then center it.
+		//
         if (!resizedRect.equals(shRect) || isClippedByUnalignedMonitors(resizedRect, shell.getDisplay()))
         {
             Monitor monitor = shell.getDisplay().getPrimaryMonitor();
@@ -126,16 +138,6 @@ public class WindowProperty
 
             shell.setBounds(resizedRect);
         }
-
-        // Just to double check: what is the preferred size of this dialog?
-        // This computed is a minimum.  If the minimum is smaller than the 
-        // size of the current shell, we make it larger.
-        //
-		Point computedSize = shell.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-		Rectangle shellSize = shell.getBounds();
-		if (shellSize.width<computedSize.x) shellSize.width=computedSize.x;
-		if (shellSize.height<computedSize.y) shellSize.height=computedSize.y;
-		shell.setBounds(shellSize);     
 	}
 
     /**
