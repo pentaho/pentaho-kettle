4329be8f05ef3be71ae84618d0cbc21a65c36237
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-01-28 17:02:27 +0000

    [PDI-3040] Updated 3.2.5 version information
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.5@11714 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -53,7 +53,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "3.2.4"; //$NON-NLS-1$
+	public static final String VERSION = "3.2.5"; //$NON-NLS-1$
 
 	/**
 	 * Sleep time waiting when buffer is empty
