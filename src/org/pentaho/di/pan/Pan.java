46c2a639d71fa5b02c3fd458c4166d439365133f
deinspanjer <deinspanjer@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-01-29 17:49:14 +0000

    Remove unnecessary casts - code cleanup
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.5@11730 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/pan/Pan.java
+++ src/org/pentaho/di/pan/Pan.java
@@ -481,27 +481,27 @@ public class Pan
 			log.logMinimal("Pan", Messages.getString("Pan.Log.StartStop",begin,end));
 			
 			long millis=stop.getTime()-start.getTime();
-	        long seconds = millis / 1000;
+	        int seconds = (int)(millis / 1000);
 	        if (seconds <= 60) {
 	            log.logMinimal("Pan",  Messages.getString("Pan.Log.ProcessingEndAfter", String.valueOf(seconds)));
 	        }
 	        else if (seconds <= 60 * 60) {
-	            int min = (int)(seconds / 60);
-	            int rem = (int)(seconds % 60);
+	            int min = (seconds / 60);
+	            int rem = (seconds % 60);
 	            log.logMinimal("Pan",  Messages.getString("Pan.Log.ProcessingEndAfterLong", String.valueOf(min), String.valueOf(rem), String.valueOf(seconds)));
 	        }
 	        else if (seconds <= 60 * 60 * 24) {
 	            int rem;
-	            int hour = (int)(seconds / (60 * 60));
-	            rem = (int)(seconds % (60 * 60)); 
+	            int hour = (seconds / (60 * 60));
+	            rem = (seconds % (60 * 60)); 
 	            int min = rem / 60;
 	            rem = rem % 60;
 	            log.logMinimal("Pan",  Messages.getString("Pan.Log.ProcessingEndAfterLonger", String.valueOf(hour), String.valueOf(min), String.valueOf(rem), String.valueOf(seconds)));
 	        }
 	        else {
 	            int rem;
-	            int days = (int)(seconds / (60 * 60 * 24));
-	            rem = (int)(seconds % (60 * 60 * 24));
+	            int days = (seconds / (60 * 60 * 24));
+	            rem = (seconds % (60 * 60 * 24));
 	            int hour = rem / (60 * 60);
 	            rem = rem % (60 * 60); 
 	            int min = rem / 60;
@@ -511,7 +511,7 @@ public class Pan
 			
 			if (trans.getResult().getNrErrors()==0) 
 			{
-				trans.printStats((int)millis/1000);
+				trans.printStats(seconds);
 				exitJVM(0);
 			}
 			else
