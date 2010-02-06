d23321126d59034508e2ae2b6a76f72017d902b7
rmansoor <rmansoor@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-02-06 00:02:05 +0000

    PDI-2892 - fixing repository meta error
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@11826 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/repository/repositoryexplorer/model/UIRepositoryObjectAclModel.java
+++ src-ui/org/pentaho/di/ui/repository/repositoryexplorer/model/UIRepositoryObjectAclModel.java
@@ -53,7 +53,7 @@ public class UIRepositoryObjectAclModel extends XulEventSourceAdapter{
     previousVal.addAll(this.selectedAvailableRoles);
     this.selectedAvailableRoles.clear();
     for(Object role:selectedAvailableRoles) {
-      if(role instanceof UIRepositoryRole) {
+      if(role instanceof String) {
         this.selectedAvailableRoles.add((String) role);
       } else {
         UIRepositoryObjectAcl acl = (UIRepositoryObjectAcl) role;
@@ -74,7 +74,7 @@ public class UIRepositoryObjectAclModel extends XulEventSourceAdapter{
     previousVal.addAll(this.selectedAvailableUsers);
     this.selectedAvailableUsers.clear();
     for(Object user:selectedAvailableUsers) {
-      if(user instanceof UIRepositoryUser) {
+      if(user instanceof String) {
         this.selectedAvailableUsers.add((String) user);
       } else {
         UIRepositoryObjectAcl acl = (UIRepositoryObjectAcl) user;
@@ -95,7 +95,7 @@ public class UIRepositoryObjectAclModel extends XulEventSourceAdapter{
     previousVal.addAll(this.selectedAssignedRoles);
     this.selectedAssignedRoles.clear();
     for(Object role:selectedAssignedRoles) {
-      if(role instanceof UIRepositoryRole) {
+      if(role instanceof String) {
         this.selectedAssignedRoles.add(createAclFromRole((String)role));  
       } else {
         this.selectedAssignedRoles.add( (UIRepositoryObjectAcl) role);
