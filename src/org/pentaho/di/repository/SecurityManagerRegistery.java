88d7abcf2ccff27bf0d2966a66a7174426fa5672
rmansoor <rmansoor@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-02-19 16:50:41 +0000

    PDI-3049 - Fixing some exceptions handling
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@11988 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/repository/SecurityManagerRegistery.java
+++ src/org/pentaho/di/repository/SecurityManagerRegistery.java
@@ -5,38 +5,40 @@ import java.lang.reflect.Constructor;
 public class SecurityManagerRegistery {
 
   private Class<?> securityManagerClass;
+
   private static SecurityManagerRegistery instance;
-  RepositorySecurityManager securityManager;
+
   private SecurityManagerRegistery() {
-    
+
   }
+
   public static SecurityManagerRegistery getInstance() {
-    if(instance == null) {
+    if (instance == null) {
       instance = new SecurityManagerRegistery();
     }
     return instance;
   }
+
   public void registerSecurityManager(Class<?> securityManagerClass) {
     this.securityManagerClass = securityManagerClass;
   }
+
   public Class<?> getRegisteredSecurityManager() {
     return securityManagerClass;
   }
-  
-  public RepositorySecurityManager createSecurityManager(Repository repository, RepositoryMeta meta, UserInfo userInfo) throws SecurityManagerCreationException {
+
+  public RepositorySecurityManager createSecurityManager(Repository repository, RepositoryMeta meta, UserInfo userInfo)
+      throws SecurityManagerCreationException {
     try {
-        if(securityManager == null) {
-          Constructor<?> constructor = securityManagerClass
-          .getConstructor(Repository.class, RepositoryMeta.class, UserInfo.class);
-          if(constructor != null) {
-            securityManager = (RepositorySecurityManager) constructor.newInstance(repository, meta, userInfo);  
-          } else {
-            throw new SecurityManagerCreationException("Unable to get the constructor for " + securityManagerClass);
-          }
-        }
+      Constructor<?> constructor = securityManagerClass.getConstructor(Repository.class, RepositoryMeta.class,
+          UserInfo.class);
+      if (constructor != null) {
+        return (RepositorySecurityManager) constructor.newInstance(repository, meta, userInfo);
+      } else {
+        throw new SecurityManagerCreationException("Unable to get the constructor for " + securityManagerClass);
+      }
     } catch (Exception e) {
       throw new SecurityManagerCreationException(e);
     }
-    return securityManager;
   }
 }
