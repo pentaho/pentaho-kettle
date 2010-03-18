056d11916bb285bca8ca68d413255fc73f259100
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-03-18 15:08:28 +0000

    PDI-3389 : Job and Trans log tables are not properly stored and loaded from the repositories (compile fix)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12318 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/repository/RepositoryAttributeInterface.java
+++ src-core/org/pentaho/di/repository/RepositoryAttributeInterface.java
@@ -9,16 +9,11 @@ import org.pentaho.di.core.database.DatabaseMeta;
  *
  */
 public interface RepositoryAttributeInterface {
+
 	public void setAttribute(String code, String value);
 	public String getAttribute(String code);
-	
-	public void setAttribute(String code, long value);
-	public long getAttributeInteger(String code);
-
 	public void setAttribute(String code, boolean value);
-	public long getAttributeBoolean(String code);
-
-	public void setAttribute(String code, DatabaseMeta databaseMeta);
-	public DatabaseMeta getAttributeDatabaseMeta(String code);
-
+	public boolean getAttributeBoolean(String code, boolean defaultValue);
+	public void setAttribute(String code, long value);
+	public long getAttributeInteger(String code, long defaultValue);
 }
