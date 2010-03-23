40d6a8ee030bddd7437513a941fa394cd6dc6248
wseyler <wseyler@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-03-23 14:30:56 +0000

    PDI-3433: Added a check to keep the system from trying to set null test for a swt label.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12396 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/dialog/EnterOptionsDialog.java
+++ src-ui/org/pentaho/di/ui/core/dialog/EnterOptionsDialog.java
@@ -1482,7 +1482,7 @@ public class EnterOptionsDialog extends Dialog
 		for (final GUIOption<Object> e : PropsUI.getInstance().getRegisteredEditableComponents())
 		{
 			Label wlMaxNrLogLines1 = new Label(wGeneralComp, SWT.RIGHT);
-			wlMaxNrLogLines1.setText(e.getLabelText());
+			wlMaxNrLogLines1.setText(e.getLabelText() == null ? "" : e.getLabelText());
 			props.setLook(wlMaxNrLogLines1);
 			FormData fdlMaxNrLogLinesTemp = new FormData();
 			fdlMaxNrLogLinesTemp.left = new FormAttachment(0, 0);
