d04444b32acb2125e548ea467170e0feb60d10e9
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-03-23 12:26:23 +0000

    PDI-3337 : changed version name to 4.0.0-Preview
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12388 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -59,7 +59,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "4.0.0-M3";
+	public static final String VERSION = "4.0.0-Preview";
 
 	/**
 	 * Sleep time waiting when buffer is empty
