4914dfd84bc96c087ccbb871e7dddc41a1b2b6b2
rwintner <rwintner@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-03-24 00:01:28 +0000

    SAP Connecor from Aschauer EDV
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12417 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/sapinput/sap/SAPConnectionFactory.java
+++ src/org/pentaho/di/trans/steps/sapinput/sap/SAPConnectionFactory.java
@@ -1,3 +1,22 @@
+/* Copyright (c) 2010 Aschauer EDV GmbH.  All rights reserved. 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * This software was developed by Aschauer EDV GmbH and is provided under the terms 
+ * of the GNU Lesser General Public License, Version 2.1. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.gnu.org/licenses/lgpl-2.1.txt.
+ *
+ * Software distributed under the GNU Lesser Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ * 
+ * Please contact Aschauer EDV GmbH www.aschauer-edv.at if you need additional
+ * information or have any questions.
+ * 
+ * @author  Robert Wintner robert.wintner@aschauer-edv.at
+ * @since   PDI 4.0
+ */
+
 package org.pentaho.di.trans.steps.sapinput.sap;
 
 import org.pentaho.di.core.Const;
