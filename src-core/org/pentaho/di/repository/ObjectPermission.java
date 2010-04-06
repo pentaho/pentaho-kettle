8b0eddfe8edbe85ac0c52cf859fd98807413cd8e
mlowery <mlowery@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-04-06 19:33:11 +0000

    [PDI-3645] removed DELETE_CHILD permission
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12638 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/repository/ObjectPermission.java
+++ src-core/org/pentaho/di/repository/ObjectPermission.java
@@ -1,5 +1,5 @@
 package org.pentaho.di.repository;
 
 public enum ObjectPermission {
-	READ, WRITE, DELETE, DELETE_CHILD, READ_ACL, WRITE_ACL, ALL;
+	READ, WRITE, DELETE, READ_ACL, WRITE_ACL, ALL;
 }
