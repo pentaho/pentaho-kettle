0f05d4a8b4fc38bf8b66fc17578a461700945558
mdamour <mdamour@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-04-16 18:38:42 +0000

    PDI-3673 Carte Null Pointer within DI Server
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12787 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/www/CarteSingleton.java
+++ src/org/pentaho/di/www/CarteSingleton.java
@@ -65,37 +65,39 @@ public class CarteSingleton {
     }
 
     SlaveServer slaveServer = config.getSlaveServer();
-    String hostname = slaveServer.getHostname();
-    int port = WebServer.PORT;
-    if (!Const.isEmpty(slaveServer.getPort())) {
-      try {
-        port = Integer.parseInt(slaveServer.getPort());
-      } catch (Exception e) {
-        log.logError(BaseMessages.getString(PKG, "Carte.Error.CanNotPartPort", slaveServer.getHostname(), "" + port), e);
-      }
-    }
-
-    // TODO: see if we need to keep doing this on a periodic basis.
-    // The master might be dead or not alive yet at the time we send this
-    // message.
-    // Repeating the registration over and over every few minutes might
-    // harden this sort of problems.
-    //
-    if (config.isReportingToMasters()) {
-      final SlaveServer client = new SlaveServer("Dynamic slave [" + hostname + ":" + port + "]", hostname, "" + port, slaveServer.getUsername(), slaveServer
-          .getPassword());
-      for (final SlaveServer master : config.getMasters()) {
-        // Here we use the username/password specified in the slave
-        // server section of the configuration.
-        // This doesn't have to be the same pair as the one used on the
-        // master!
-        //
+    if (slaveServer != null) {
+      int port = WebServer.PORT;
+      if (!Const.isEmpty(slaveServer.getPort())) {
         try {
-          SlaveServerDetection slaveServerDetection = new SlaveServerDetection(client);
-          master.sendXML(slaveServerDetection.getXML(), RegisterSlaveServlet.CONTEXT_PATH + "/");
-          log.logBasic("Registered this slave server to master slave server [" + master.toString() + "] on address [" + master.getServerAndPort() + "]");
+          port = Integer.parseInt(slaveServer.getPort());
         } catch (Exception e) {
-          log.logError("Unable to register to master slave server [" + master.toString() + "] on address [" + master.getServerAndPort() + "]");
+          log.logError(BaseMessages.getString(PKG, "Carte.Error.CanNotPartPort", slaveServer.getHostname(), "" + port), e);
+        }
+      }
+
+      // TODO: see if we need to keep doing this on a periodic basis.
+      // The master might be dead or not alive yet at the time we send this
+      // message.
+      // Repeating the registration over and over every few minutes might
+      // harden this sort of problems.
+      //
+      if (config.isReportingToMasters()) {
+        String hostname = slaveServer.getHostname();
+        final SlaveServer client = new SlaveServer("Dynamic slave [" + hostname + ":" + port + "]", hostname, "" + port, slaveServer.getUsername(), slaveServer
+            .getPassword());
+        for (final SlaveServer master : config.getMasters()) {
+          // Here we use the username/password specified in the slave
+          // server section of the configuration.
+          // This doesn't have to be the same pair as the one used on the
+          // master!
+          //
+          try {
+            SlaveServerDetection slaveServerDetection = new SlaveServerDetection(client);
+            master.sendXML(slaveServerDetection.getXML(), RegisterSlaveServlet.CONTEXT_PATH + "/");
+            log.logBasic("Registered this slave server to master slave server [" + master.toString() + "] on address [" + master.getServerAndPort() + "]");
+          } catch (Exception e) {
+            log.logError("Unable to register to master slave server [" + master.toString() + "] on address [" + master.getServerAndPort() + "]");
+          }
         }
       }
     }
