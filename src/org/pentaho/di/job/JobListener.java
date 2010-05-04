413fc6ed0b742dcffe30b8ea89c3a7cd078ab734
wgorman <wgorman@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-05-04 03:06:04 +0000

    [PDI-3707] added license and copyright to java files
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@12991 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/job/JobListener.java
+++ src/org/pentaho/di/job/JobListener.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2010 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the GNU Lesser General Public License, Version 2.1. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.gnu.org/licenses/lgpl-2.1.txt. The Original Code is Pentaho 
+ * Data Integration.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the GNU Lesser Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ */
 package org.pentaho.di.job;
 
 import org.pentaho.di.core.exception.KettleException;
