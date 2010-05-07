8d604ef4980659887f518c3cf83944547d7581c3
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-05-07 13:13:09 +0000

    PDI-874 : failure setting character-set in XBase Input step
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@13020 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/trans/steps/xbaseinput/XBaseInputDialog.java
+++ src-ui/org/pentaho/di/ui/trans/steps/xbaseinput/XBaseInputDialog.java
@@ -488,17 +488,16 @@ public class XBaseInputDialog extends BaseStepDialog implements StepDialogInterf
 			wFilename.setText(input.getDbfFileName());
 			wFilename.setToolTipText(transMeta.environmentSubstitute(input.getDbfFileName()));
 		}
-		wLimit.setText(Integer.toString(input.getRowLimit())); //$NON-NLS-1$
-		wAddRownr.setSelection(input.isRowNrAdded());
-		if (input.getRowNrField()!=null) wFieldRownr.setText(input.getRowNrField());
+        wFieldRownr.setText(Const.NVL(input.getRowNrField(), ""));
 
         wInclFilename.setSelection(input.includeFilename());
-        if (input.getFilenameField()!=null) wInclFilenameField.setText(input.getFilenameField());
+        wInclFilenameField.setText(Const.NVL(input.getFilenameField(), ""));
 
         wAccFilenames.setSelection(input.isAcceptingFilenames());
-        if (input.getAcceptingField()!=null) wAccField.setText(input.getAcceptingField());
-        if (input.getAcceptingStep()!=null) wAccStep.setText(input.getAcceptingStep().getName());
-		
+        wAccField.setText(Const.NVL(input.getAcceptingField(), ""));
+        wAccStep.setText(Const.NVL(input.getAcceptingStep().getName(), ""));
+        wCharactersetName.setText(Const.NVL(input.getCharactersetName(), ""));
+
         setFlags();
 		
 		wStepname.selectAll();
@@ -526,7 +525,8 @@ public class XBaseInputDialog extends BaseStepDialog implements StepDialogInterf
         meta.setAcceptingFilenames( wAccFilenames.getSelection() );
         meta.setAcceptingField( wAccField.getText() );
         meta.setAcceptingStep( transMeta.findStep( wAccStep.getText() ) );
-
+        meta.setCharactersetName( wCharactersetName.getText() );
+        
 		if (Const.isEmpty(meta.getDbfFileName()) && !meta.isAcceptingFilenames())
 		{
 			throw new KettleStepException(BaseMessages.getString(PKG, "XBaseInputDialog.Exception.SpecifyAFileToUse")); //$NON-NLS-1$
