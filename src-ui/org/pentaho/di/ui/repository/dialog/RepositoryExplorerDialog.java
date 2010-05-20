5c4e7df4edb1f70e28be6309daf46c638d1e6abd
sflatley <sflatley@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-05-20 01:43:21 +0000

    PDI-3927:  Removed objectMap from method call to fix build.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@13155 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/repository/dialog/RepositoryExplorerDialog.java
+++ src-ui/org/pentaho/di/ui/repository/dialog/RepositoryExplorerDialog.java
@@ -1411,7 +1411,7 @@ public class RepositoryExplorerDialog extends Dialog
 		  TreeItem newCat = new TreeItem(tiTrans, SWT.NONE);
 		  newCat.setImage(GUIResource.getInstance().getImageLogoSmall());
 		  Color dircolor = GUIResource.getInstance().getColorDirectory();
-		  RepositoryDirectoryUI.getTreeWithNames(newCat, rep, objectMap, dircolor, sortColumn, includeDeleted, ascending, true, false, directoryTree, null, null);
+		  RepositoryDirectoryUI.getTreeWithNames(newCat, rep, dircolor, sortColumn, includeDeleted, ascending, true, false, directoryTree, null, null);
 
 			// The Jobs...				
 		  TreeItem tiJob = new TreeItem(tiTree, SWT.NONE); 
@@ -1420,7 +1420,7 @@ public class RepositoryExplorerDialog extends Dialog
 
 		  TreeItem newJob = new TreeItem(tiJob, SWT.NONE);
 		  newJob.setImage(GUIResource.getInstance().getImageLogoSmall());
-		  RepositoryDirectoryUI.getTreeWithNames(newJob, rep, objectMap, dircolor, sortColumn, includeDeleted, ascending, false, true, directoryTree, null, null);
+		  RepositoryDirectoryUI.getTreeWithNames(newJob, rep, dircolor, sortColumn, includeDeleted, ascending, false, true, directoryTree, null, null);
 	
 			//
 			// Add the users or only yourself
