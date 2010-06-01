719dd80dc8f89eccbcd7af9d35f8a3b37904ab33
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-06-01 08:37:14 +0000

    PDI-4051 :Get Data From XML - Nullpointer exception
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.5@13290 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/getxmldata/GetXMLData.java
+++ src/org/pentaho/di/trans/steps/getxmldata/GetXMLData.java
@@ -590,7 +590,7 @@ public class GetXMLData extends BaseStep implements StepInterface
 //	        return false; // end of data or error.
 //	     }
 		 
-		 if (log.isRowLevel()) logRowlevel(Messages.getString("GetXMLData.Log.ReadRow", getInputRowMeta().getString(r)));
+		 if (log.isRowLevel()) logRowlevel(Messages.getString("GetXMLData.Log.ReadRow", data.outputRowMeta.getString(r)));
 		 incrementLinesInput();
 		 data.rownr++;
 		 putRow(data.outputRowMeta, r);  // copy row to output rowset(s);
