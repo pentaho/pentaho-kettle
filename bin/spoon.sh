8f24711af22d253bad23def689faa2b5e6ba724a
rmansoor <rmansoor@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-06-03 00:47:50 +0000

    PDI-4069 - use JAVA_BIN variable for java
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.0.0-GA@13337 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- bin/spoon.sh
+++ bin/spoon.sh
@@ -51,7 +51,7 @@ case `uname -s` in
 	    ARCH=`uname -m`
 		case $ARCH in
 			x86_64)
-				if $(java -version 2>&1 | grep "64-Bit" > /dev/null )
+				if $(JAVA_BIN -version 2>&1 | grep "64-Bit" > /dev/null )
                                 then
 				  LIBPATH=$BASEDIR/../libswt/linux/x86_64/
                                 else
