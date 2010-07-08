eb85250498edba8704510525209103081ce64536
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-07-08 22:24:22 +0000

    PDI-4187 : Transformation directory does not reflect in a Job->Transformation job entry
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.0.1@13530 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/repository/kdr/delegates/KettleDatabaseRepositoryConnectionDelegate.java
+++ src/org/pentaho/di/repository/kdr/delegates/KettleDatabaseRepositoryConnectionDelegate.java
@@ -1379,9 +1379,7 @@ public class KettleDatabaseRepositoryConnectionDelegate extends KettleDatabaseRe
 	    		id_directory = new LongObjectId(0L); // root!
 	    	}
 	    	
-	    	RepositoryDirectoryInterface repositoryDirectory = new RepositoryDirectory();
-	    	repositoryDirectory.setObjectId(id_directory);
-	    	repository.directoryDelegate.loadRepositoryDirectory(new RepositoryDirectory(), id_directory);
+            RepositoryDirectoryInterface repositoryDirectory = repository.loadRepositoryDirectoryTree().findDirectory(id_directory);
    	
 	        String sql = "SELECT "+quote(KettleDatabaseRepository.FIELD_TRANSFORMATION_NAME)+", "+
 	        	quote(KettleDatabaseRepository.FIELD_TRANSFORMATION_MODIFIED_USER)+", "+
