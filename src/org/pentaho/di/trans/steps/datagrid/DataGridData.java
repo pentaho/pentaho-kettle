9e760f8f56b4b4928cb8f4d20948eb5043c1d4eb
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-08-11 19:48:05 +0000

    PDI-4432 : Data Grid step only passes half the rows while copying data to 2 target steps
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@13750 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/datagrid/DataGridData.java
+++ src/org/pentaho/di/trans/steps/datagrid/DataGridData.java
@@ -24,6 +24,8 @@ public class DataGridData extends BaseStepData implements StepDataInterface
     RowMetaInterface      outputRowMeta;
     
 	public RowMetaInterface	convertMeta;
+
+    public int linesWritten;
 	
 	/**
 	 * 
