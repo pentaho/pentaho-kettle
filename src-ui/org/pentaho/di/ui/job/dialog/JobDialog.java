891cac0dcb09c0668b3468cec3470c6a77209dbf
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-09-05 20:42:46 +0000

    PDI-4555.Job dialog - Filename textbox should be disabled
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.0.2@13861 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/job/dialog/JobDialog.java
+++ src-ui/org/pentaho/di/ui/job/dialog/JobDialog.java
@@ -356,6 +356,7 @@ public class JobDialog extends Dialog
 		fdJobFilename.top  = new FormAttachment(wJobname, margin);
 		fdJobFilename.right= new FormAttachment(100, 0);
 		wJobFilename.setLayoutData(fdJobFilename);
+		wJobFilename.setEnabled(false);
 
 
 		// Job description:
