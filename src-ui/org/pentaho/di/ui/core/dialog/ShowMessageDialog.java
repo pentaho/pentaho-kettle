4c84d9630059996e63a5ed8813a5f96ba4031d53
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-10-18 20:43:43 +0000

    [PDI-4756] Connection testing from the database dialog now shows in a standard Kettle dialog box
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@14201 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/core/dialog/ShowMessageDialog.java
+++ src-ui/org/pentaho/di/ui/core/dialog/ShowMessageDialog.java
@@ -29,6 +29,7 @@ import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.ui.core.PropsUI;
@@ -55,6 +56,8 @@ public class ShowMessageDialog extends Dialog
 
     private Shell parent;
     
+    private boolean scroll;
+    
     /** Timeout of dialog in seconds */
     private int timeOut;
 
@@ -73,11 +76,27 @@ public class ShowMessageDialog extends Dialog
      */
     public ShowMessageDialog(Shell parent, int flags, String title, String message)
     {
+      this(parent, flags, title, message, false);
+    }
+    
+    /**
+     * Dialog to allow someone to show a text with an icon in front
+     * 
+     * @param parent The parent shell to use
+     * @param flags the icon to show using SWT flags: SWT.ICON_WARNING, SWT.ICON_ERROR, ... Also SWT.OK, SWT.CANCEL is allowed.
+     * @param title The dialog title
+     * @param message The message to display
+     * @param text The text to display or edit
+     * @param scroll Set the dialog to a default size and enable scrolling
+     */
+    public ShowMessageDialog(Shell parent, int flags, String title, String message, boolean scroll)
+    {
         super(parent, SWT.NONE);
         this.parent = parent;
         this.flags = flags;
         this.title = title;
         this.message = message;
+        this.scroll = scroll;
         
         props = PropsUI.getInstance();
     }
@@ -86,7 +105,8 @@ public class ShowMessageDialog extends Dialog
     {
         Display display = parent.getDisplay();
 
-        shell = new Shell(parent, SWT.DIALOG_TRIM);
+        shell = new Shell(parent, SWT.DIALOG_TRIM | SWT.RESIZE);
+        
         props.setLook(shell);
         shell.setImage(GUIResource.getInstance().getImageSpoon());
 
@@ -128,10 +148,22 @@ public class ShowMessageDialog extends Dialog
         }
         
         // The message
-        Label wlDesc = new Label(shell, SWT.NONE);
+        Text wlDesc;
+        FormData fdlDesc = new FormData();
+        
+        if(scroll) {
+          wlDesc = new Text(shell, SWT.MULTI | SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL);
+          shell.setSize(550, 350);
+          fdlDesc.bottom = new FormAttachment(100, -50);
+          fdlDesc.right = new FormAttachment(100, 0);
+        } else {
+          wlDesc = new Text(shell, SWT.MULTI | SWT.READ_ONLY);
+          fdlDesc.right = new FormAttachment(100, 0);
+        }
+
         wlDesc.setText(message);
         props.setLook(wlDesc);
-        FormData fdlDesc = new FormData();
+        
         if (hasIcon)
         {
             fdlDesc.left = new FormAttachment(wIcon, margin*2);
@@ -142,9 +174,8 @@ public class ShowMessageDialog extends Dialog
             fdlDesc.left = new FormAttachment(0, 0);
             fdlDesc.top = new FormAttachment(0, margin);
         }
-        fdlDesc.right = new FormAttachment(100, 0);
-        wlDesc.setLayoutData(fdlDesc);
         
+        wlDesc.setLayoutData(fdlDesc);
 
         buttons = new ArrayList<Button>();
         adapters = new ArrayList<SelectionAdapter>();
@@ -193,7 +224,9 @@ public class ShowMessageDialog extends Dialog
         shell.addShellListener( new ShellAdapter() { public void shellClosed(ShellEvent e) { cancel(); } } );
 
         shell.layout();
-        shell.pack(true);
+        if(!scroll) {
+          shell.pack(true);
+        }
         
         final Button button = buttons.get(0);
         final SelectionAdapter selectionAdapter = adapters.get(0);
