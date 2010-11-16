036bf8c0856d5642f00b59493b34c3a3705720c3
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2010-11-16 20:49:00 +0000

    Updated version information in Kettle Trunk to reflect 4.2.0-M1
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@14408 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -61,7 +61,7 @@ public class Const
     /**
      *  Version number
      */
-    public static final String VERSION = "4.1.0";
+    public static final String VERSION = "4.2.0-M1";
     
     /**
      *  Release Type 
