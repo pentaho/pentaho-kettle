7a4e8e3d7503ddaf7a1d384738135455bcbff83e
jganoff <jganoff@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-01-15 20:22:00 +0000

    [PDI-5226] RowProducer now blocks for putRow() if the buffer is full.  Also exposed RowSet.putRow(..) and RowSet.putRowWait(..) through RowProducer.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@14692 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/RowProducer.java
+++ src/org/pentaho/di/trans/RowProducer.java
@@ -12,6 +12,8 @@
 */
 package org.pentaho.di.trans;
 
+import java.util.concurrent.TimeUnit;
+
 import org.pentaho.di.core.RowSet;
 import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.trans.step.StepInterface;
@@ -33,9 +35,36 @@ public class RowProducer
         this.rowSet = rowSet;
     }
     
+    /**
+     * Puts a row into the underlying row set.  This will block until the row is successfully added.
+     * 
+     * @see #putRow(RowMetaInterface, Object[], boolean) putRow(RowMetaInterface, Object[], true)
+     */
     public void putRow(RowMetaInterface rowMeta, Object[] row)
     {
-        rowSet.putRow(rowMeta, row);
+      putRow(rowMeta, row, true);
+    }
+    
+    /**
+     * Puts a row on to the underlying row set, optionally blocking until the row can be successfully put.
+     * 
+     * @return true if the row was successfully added to the rowset and false if this buffer was full.  If {@code block} is true this will always return true. 
+     * @see RowSet#putRow(RowMetaInterface, Object[])
+     */
+    public boolean putRow(RowMetaInterface rowMeta, Object[] row, boolean block) {
+      if (block) {
+        while (!rowSet.putRowWait(rowMeta, row, Long.MAX_VALUE, TimeUnit.DAYS));
+        return true;
+      }
+      return rowSet.putRow(rowMeta, row);
+    }
+    
+    /**
+     * @see RowSet#putRowWait(RowMetaInterface, Object[], long, TimeUnit)
+     */
+    public boolean putRowWait(RowMetaInterface rowMeta, Object[] rowData, long time, TimeUnit tu)
+    {
+      return rowSet.putRowWait(rowMeta, rowData, time, tu);
     }
     
     public void finished()
