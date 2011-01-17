05d4eb1031f96a740d450e55ce73a8dd528f5fd7
mlowery <mlowery@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-01-17 19:52:19 +0000

    [PDI-4010] removed use of java.ext.dirs; instead used launcher
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.2@14710 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- bin/Carte.bat
+++ bin/Carte.bat
@@ -3,9 +3,9 @@ setlocal
 
 cd /D %~dp0
 
-:: **************************************************
-:: ** Kettle home                                  **
-:: **************************************************
+REM **************************************************
+REM ** Kettle home                                  **
+REM **************************************************
 
 if "%KETTLE_DIR%"=="" set KETTLE_DIR=%~dp0
 if %KETTLE_DIR:~-1%==\ set KETTLE_DIR=%KETTLE_DIR:~0,-1%
@@ -13,51 +13,6 @@ if %KETTLE_DIR:~-1%==\ set KETTLE_DIR=%KETTLE_DIR:~0,-1%
 cd %KETTLE_DIR%
 
 call "%~dp0set-pentaho-env.bat"
-
-REM **************************************************
-REM ** Set up usage of JAVA_EXT_LIBS                **
-REM **************************************************
-
-if defined JAVA_EXT_DIRS goto :externalExtDirs
-:noExternalExtDirs
-set JAVA_EXT_DIRS=.
-goto endExtDirs
-
-:externalExtDirs
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%~dp0
-
-:endExtDirs
-
-if DEFINED _PENTAHO_JAVA_HOME goto withPentahoJavaHome
-goto endPentahoJavaHome
-:withPentahoJavaHome
-REM Every directory contains the null device. So check
-REM for directory existence:
-if exist %_PENTAHO_JAVA_HOME%\jre\lib\ext\nul set JAVA_EXT_DIRS=%_PENTAHO_JAVA_HOME%\jre\lib\ext;%JAVA_EXT_DIRS%
-if exist %_PENTAHO_JAVA_HOME%\lib\ext\nul set JAVA_EXT_DIRS=%_PENTAHO_JAVA_HOME%\lib\ext;%JAVA_EXT_DIRS%
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%
-goto endPentahoJavaHome
-
-:endPentahoJavaHome
-REM ******************
-REM   KETTLE Library
-REM *****************
-
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\lib
-
-REM **********************
-REM   External Libraries
-REM **********************
-
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\JDBC
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\webservices
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\spring
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\commons
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\web
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\pentaho
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\mondrian
-set JAVA_EXT_DIRS=%JAVA_EXT_DIRS%;%KETTLE_DIR%\libext\salesforce
 
 REM **************************************************
 REM   Platform Specific SWT       **
@@ -89,30 +44,15 @@ IF %IS64BITJAVA% == 1 GOTO :USE64
 REM ===========================================
 REM Using 32bit Java, so include 32bit SWT Jar
 REM ===========================================
-set LIBSPATH=..\libswt\win32
+set LIBSPATH=%KETTLE_DIR%\libswt\win32
 GOTO :CONTINUE
 :USE64
 REM ===========================================
 REM Using 64bit java, so include 64bit SWT Jar
 REM ===========================================
-set LIBSPATH=..\libswt\win64
+set LIBSPATH=%KETTLE_DIR%\libswt\win64
 :CONTINUE
 popd
-
-
-echo %LIBSPATH%
-
-REM *******************************************
-REM   Collect arguments from the command line
-REM *******************************************
-
-set _cmdline=
-:TopArg
-if %1!==! goto EndArg
-set _cmdline=%_cmdline% %1
-shift
-goto TopArg
-:EndArg
 
 REM ******************************************************************
 REM ** Set java runtime options                                     **
@@ -122,7 +62,7 @@ REM ******************************************************************
 
 if "%PENTAHO_DI_JAVA_OPTIONS%"=="" set PENTAHO_DI_JAVA_OPTIONS=-Xmx512m
 
-set OPT="%PENTAHO_DI_JAVA_OPTIONS%" "-Djava.ext.dirs=%JAVA_EXT_DIRS%" "-Dorg.mortbay.util.URI.charset=UTF-8" "-Djava.library.path=%LIBSPATH%" "-DKETTLE_HOME=%KETTLE_HOME%" "-DKETTLE_REPOSITORY=%KETTLE_REPOSITORY%" "-DKETTLE_USER=%KETTLE_USER%" "-DKETTLE_PASSWORD=%KETTLE_PASSWORD%" "-DKETTLE_PLUGIN_PACKAGES=%KETTLE_PLUGIN_PACKAGES%" "-DKETTLE_LOG_SIZE_LIMIT=%KETTLE_LOG_SIZE_LIMIT%"
+set OPT="%PENTAHO_DI_JAVA_OPTIONS%" "-Dorg.mortbay.util.URI.charset=UTF-8" "-Djava.library.path=%LIBSPATH%" "-DKETTLE_HOME=%KETTLE_HOME%" "-DKETTLE_REPOSITORY=%KETTLE_REPOSITORY%" "-DKETTLE_USER=%KETTLE_USER%" "-DKETTLE_PASSWORD=%KETTLE_PASSWORD%" "-DKETTLE_PLUGIN_PACKAGES=%KETTLE_PLUGIN_PACKAGES%" "-DKETTLE_LOG_SIZE_LIMIT=%KETTLE_LOG_SIZE_LIMIT%"
 
 REM ***********************************************************************
 REM ** Optionally set up the options for JAAS (uncomment to make active) **
@@ -135,5 +75,5 @@ REM ***************
 REM ** Run...    **
 REM ***************
 
-"%_PENTAHO_JAVA%" %OPT% org.pentaho.di.www.Carte %_cmdline%
+"%_PENTAHO_JAVA%" %OPT% -jar launcher\launcher.jar -main org.pentaho.di.www.Carte %*
 
