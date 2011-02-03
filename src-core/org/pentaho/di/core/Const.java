0be34a485a4b21f12c6f83d1dbfe70782368fe67
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-02-03 04:03:35 +0000

    [BUILD] Update version in 4.1.3 branch
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.3@14776 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -61,7 +61,7 @@ public class Const
     /**
      *  Version number
      */
-    public static final String VERSION = "4.1.2";
+    public static final String VERSION = "4.1.3";
     
     /**
      *  Release Type 
