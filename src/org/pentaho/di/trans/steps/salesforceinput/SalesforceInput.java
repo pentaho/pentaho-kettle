055c3b2528a527f9cd3261f6d6d3b6a7c31139b0
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-03-18 11:14:12 +0000

    PDI-5487.Salesforce Input Limit Off By One
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.3@14890 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/salesforceinput/SalesforceInput.java
+++ src/org/pentaho/di/trans/steps/salesforceinput/SalesforceInput.java
@@ -135,7 +135,7 @@ public class SalesforceInput extends BaseStep implements StepInterface
 		try{
 			
 			// check for limit rows
-            if (data.limit>0 && data.rownr>=data.limit-1) {
+            if (data.limit>0 && data.rownr>=data.limit) {
             	// User specified limit and we reached it 
             	// We end here 
             	data.limitReached = true;
@@ -428,5 +428,4 @@ public class SalesforceInput extends BaseStep implements StepInterface
 		}catch(Exception e){};
 		super.dispose(smi, sdi);
 	}
-
 }
