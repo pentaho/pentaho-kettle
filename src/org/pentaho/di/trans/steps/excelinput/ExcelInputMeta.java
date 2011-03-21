0f141849aefa0015a5687a0e8f538a6b6400b298
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-03-21 19:16:45 +0000

    PDI-5496 : StartRow and StartColumn of an ExcelInput-Sheet are lost when the transformation is exported to the repository
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/3.2.5@14899 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/excelinput/ExcelInputMeta.java
+++ src/org/pentaho/di/trans/steps/excelinput/ExcelInputMeta.java
@@ -459,7 +459,9 @@ public class ExcelInputMeta extends BaseStepMeta implements StepMetaInterface
 		for (int i=0;i<nrsheets;i++)
 		{
 			retval.sheetName[i] = sheetName[i];
-		}
+      retval.startColumn[i] = startColumn[i];
+      retval.startRow[i] = startRow[i];
+    }
 
 		return retval;
 	}
