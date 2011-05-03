2c6785937408c4acd13cc3094f06b3f40dcf846e
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-05-03 20:24:33 +0000

    PDI-6143.Replace in String Step - Whole Word not working
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@15096 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/replacestring/ReplaceString.java
+++ src/org/pentaho/di/trans/steps/replacestring/ReplaceString.java
@@ -70,9 +70,10 @@ public class ReplaceString extends BaseStep implements StepInterface {
     }
 
 
-    private Pattern buildPattern(boolean literalParsing, boolean caseSensitive, boolean wholeWord, String patternString) {
+    private Pattern buildPattern(boolean literalParsing, boolean caseSensitive, 
+    		boolean wholeWord, String patternString) {
         int flags = 0;
-        if (literalParsing) flags |= Pattern.LITERAL;
+        if (literalParsing && !wholeWord) flags |= Pattern.LITERAL;
         if (!caseSensitive) flags |= Pattern.CASE_INSENSITIVE;
 
         /* XXX: I don't like this parameter.
