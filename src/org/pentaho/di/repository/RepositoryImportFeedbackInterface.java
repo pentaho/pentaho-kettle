ce02863f674722c7b45b4b1de5da66d1235ffa73
cboyden <cboyden@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-05-23 15:01:45 +0000

    [BUILD] Removing code changes for PDI-5403 from the 4.1.x codeline [BUILD] Updated copyright version on the about screen
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.3-GA@15216 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/repository/RepositoryImportFeedbackInterface.java
+++ src/org/pentaho/di/repository/RepositoryImportFeedbackInterface.java
@@ -15,5 +15,4 @@ public interface RepositoryImportFeedbackInterface {
   public void showError(String title, String message, Exception e);
   public boolean askContinueOnErrorQuestion(String title, String message);
   
-  public boolean isAskingOverwriteConfirmation();
 }
