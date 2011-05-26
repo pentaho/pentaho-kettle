3945e39271e7ec9ab1f9bc319b4ddbf1c29dd4bb
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-05-26 12:42:52 +0000

    Make transformations less noisy about steps being finished. (reverted change)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@15240 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/step/BaseStep.java
+++ src/org/pentaho/di/trans/step/BaseStep.java
@@ -2483,8 +2483,12 @@ public class BaseStep implements VariableSpace, StepInterface, LoggingObjectInte
             long lo = getLinesOutput();
             long lr = getLinesRead();
             long lw = getLinesWritten();
+            long lu = getLinesUpdated();
             long lj = getLinesRejected();
-            logDetailed(BaseMessages.getString(PKG, "BaseStep.Log.SummaryInfo", String.valueOf(li), String.valueOf(lo), String.valueOf(lr), String.valueOf(lw), String.valueOf(lw), String.valueOf(errors+lj)));
+            if (li > 0 || lo > 0 || lr > 0 || lw > 0 || lu > 0 || lj > 0 || errors > 0)
+                logBasic(BaseMessages.getString(PKG, "BaseStep.Log.SummaryInfo", String.valueOf(li), String.valueOf(lo), String.valueOf(lr), String.valueOf(lw), String.valueOf(lw), String.valueOf(errors+lj)));
+            else
+                logDetailed(BaseMessages.getString(PKG, "BaseStep.Log.SummaryInfo", String.valueOf(li), String.valueOf(lo), String.valueOf(lr), String.valueOf(lw), String.valueOf(lw), String.valueOf(errors+lj)));
         }
     }
 
