5017ee69f5c7e2be1b78c80e45fca8ba7ff6d0d5
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-06-15 15:17:02 +0000

    PDI-6118 : Initial location of pasted step in Spoon is incorrect if the canvas is scrollable and not at the top.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@15380 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/Spoon.java
+++ src-ui/org/pentaho/di/ui/spoon/Spoon.java
@@ -2927,8 +2927,8 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
         hops[i] = new TransHopMeta(hopnode, alSteps);
       }
 
-      // What's the difference between loc and min?
       // This is the offset:
+      //
       Point offset = new Point(loc.x - min.x, loc.y - min.y);
 
       // Undo/redo object positions...
@@ -7469,7 +7469,7 @@ public class Spoon implements AddUndoPositionInterface, TabListener, SpoonInterf
         } else if (steps) {
           TransGraph transGraph = getActiveTransGraph();
           if (transGraph != null && transGraph.getLastMove() != null) {
-            pasteXML(transGraph.getManagedObject(), clipcontent, transGraph.getLastMove());
+            pasteXML(transGraph.getManagedObject(), clipcontent, transGraph.screen2real(transGraph.getLastMove().x, transGraph.getLastMove().y));
           }
         } else if (jobEntries) {
           JobGraph jobGraph = getActiveJobGraph();
