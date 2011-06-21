979614fdc41365ca970b764951eb89249d36cad9
mlowery <mlowery@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-06-21 16:08:26 +0000

    [PDI-4657] null out xform and job IDs when connecting to another repository
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.4@15410 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/spoon/delegates/SpoonDelegates.java
+++ src-ui/org/pentaho/di/ui/spoon/delegates/SpoonDelegates.java
@@ -12,45 +12,61 @@
 */
 package org.pentaho.di.ui.spoon.delegates;
 
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.ui.spoon.InstanceCreationException;
 import org.pentaho.di.ui.spoon.Spoon;
 
 public class SpoonDelegates
 {
-	public SpoonJobDelegate jobs;
+  public SpoonJobDelegate jobs;
 
-	public SpoonTabsDelegate tabs;
+  public SpoonTabsDelegate tabs;
 
-	public SpoonTransformationDelegate trans;
+  public SpoonTransformationDelegate trans;
 
-	public SpoonSlaveDelegate slaves;
+  public SpoonSlaveDelegate slaves;
 
-	public SpoonTreeDelegate tree;
+  public SpoonTreeDelegate tree;
 
-	public SpoonStepsDelegate steps;
+  public SpoonStepsDelegate steps;
 
-	public SpoonDBDelegate db;
+  public SpoonDBDelegate db;
 
-	public SpoonDelegates(Spoon spoon)
-	{
-		tabs = new SpoonTabsDelegate(spoon);
-		tree = new SpoonTreeDelegate(spoon);
-		slaves = new SpoonSlaveDelegate(spoon);
-		steps = new SpoonStepsDelegate(spoon);
-		db = new SpoonDBDelegate(spoon);
-		update(spoon);
-	}
+  public SpoonDelegates(Spoon spoon)
+  {
+    tabs = new SpoonTabsDelegate(spoon);
+    tree = new SpoonTreeDelegate(spoon);
+    slaves = new SpoonSlaveDelegate(spoon);
+    steps = new SpoonStepsDelegate(spoon);
+    db = new SpoonDBDelegate(spoon);
+    update(spoon);
+  }
 
-	public void update(Spoon spoon) {
+  public void update(Spoon spoon) {
+    SpoonJobDelegate origJobs = jobs;
     try {
       jobs = (SpoonJobDelegate)SpoonDelegateRegistry.getInstance().constructSpoonJobDelegate(spoon);
     } catch (InstanceCreationException e) {
       jobs = new SpoonJobDelegate(spoon);
     }
+    if (origJobs != null) {
+      // preserve open jobs
+      for (JobMeta jobMeta : origJobs.getLoadedJobs()) {
+        jobs.addJob(jobMeta);
+      }
+    }
+    SpoonTransformationDelegate origTrans = trans;
     try {
       trans = (SpoonTransformationDelegate)SpoonDelegateRegistry.getInstance().constructSpoonTransDelegate(spoon);
     } catch (InstanceCreationException e) {
       trans = new SpoonTransformationDelegate(spoon);
     }  
-	}
+    if (origTrans != null) {
+      // preseve open trans
+      for (TransMeta transMeta : origTrans.getLoadedTransformations()) {
+        trans.addTransformation(transMeta);
+      }
+    }
+  }
 }
