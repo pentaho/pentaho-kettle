7e6834ef97461617c8ad443be6efc7220c7ae3bf
buildguy <buildguy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-08-19 14:49:06 +0000

    [build] correcting splash screen release version
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.2.0@15676 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -61,7 +61,7 @@ public class Const
   /**
    * Version number
    */
-  public static final String VERSION = "4.2.0-GA";
+  public static final String VERSION = "4.2.0";
 
   /**
    * Release Type
