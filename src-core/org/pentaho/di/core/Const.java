2fefe66ce2199790a8d6bc28c27cf7a1f74b9438
buildguy <buildguy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-09-29 16:47:19 +0000

    [build] updating string to 4.1.0.1 for release build
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.1.0.1-customer@15792 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -61,7 +61,7 @@ public class Const
     /**
      *  Version number
      */
-    public static final String VERSION = "4.1.0";
+    public static final String VERSION = "4.1.0.1";
     
     /**
      *  Release Type 
