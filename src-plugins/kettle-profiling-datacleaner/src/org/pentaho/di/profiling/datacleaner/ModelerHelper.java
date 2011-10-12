3bc4036ae114b9b26610b8b6e2272e0d431ce674
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-10-12 19:15:57 +0000

    PDI-6706 : As an ETL developer I want to be able to use the DataCleaner profiling tool inside of Spoon PDI-6730 : Move the Kettle JDBC driver project to a separate source tree in trunk PDI-6802 : As a PDI and SAS user I want to be able to read sas7bdat files
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@15867 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-plugins/kettle-profiling-datacleaner/src/org/pentaho/di/profiling/datacleaner/ModelerHelper.java
+++ src-plugins/kettle-profiling-datacleaner/src/org/pentaho/di/profiling/datacleaner/ModelerHelper.java
@@ -1,14 +1,26 @@
 package org.pentaho.di.profiling.datacleaner;
 
+import java.awt.Image;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
+import org.eobjects.analyzer.beans.BooleanAnalyzer;
+import org.eobjects.analyzer.beans.DateAndTimeAnalyzer;
+import org.eobjects.analyzer.beans.NumberAnalyzer;
+import org.eobjects.analyzer.beans.StringAnalyzer;
 import org.eobjects.analyzer.cli.CliArguments;
 import org.eobjects.analyzer.connection.Datastore;
 import org.eobjects.analyzer.connection.DatastoreCatalog;
 import org.eobjects.analyzer.connection.JdbcDatastore;
+import org.eobjects.analyzer.data.DataTypeFamily;
+import org.eobjects.analyzer.data.InputColumn;
+import org.eobjects.analyzer.job.builder.AnalysisJobBuilder;
 import org.eobjects.datacleaner.bootstrap.Bootstrap;
 import org.eobjects.datacleaner.bootstrap.BootstrapOptions;
 import org.eobjects.datacleaner.bootstrap.ExitActionListener;
+import org.eobjects.metamodel.DataContext;
+import org.eobjects.metamodel.schema.Column;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.gui.SpoonFactory;
 import org.pentaho.di.core.vfs.KettleVFS;
@@ -72,35 +84,75 @@ public class ModelerHelper extends AbstractXulEventHandler implements ISpoonMenu
     return "profiler"; //$NON-NLS-1$
   }
 
-  public void profileStep() {
-    Spoon spoon = ((Spoon) SpoonFactory.getInstance());
-    final TransMeta transMeta = spoon.getActiveTransformation();
-    if (transMeta == null || spoon.getActiveTransGraph() == null) {
-      return;
-    }
-    if (transMeta.hasChanged()) {
-      MessageBox box = new MessageBox(spoon.getShell(), SWT.ICON_ERROR | SWT.CLOSE);
-      box.setText("Save first...");
-      box.setMessage("Please save your transformation first.  To run the profiling job, we need to run the transformation.");
-      box.open();
-      return;
-    }
-    
-    StepMeta stepMeta = spoon.getActiveTransGraph().getCurrentStep();
-    if (stepMeta==null) {
-      return;
-    }
-    
+  public void openProfiler() throws Exception {
+
+    BootstrapOptions bootstrapOptions = new BootstrapOptions() {
+      public Image getWelcomeImage() {
+        return null;
+      }
+
+      public Datastore getSingleDatastore(DatastoreCatalog catalog) {
+        return null;
+      };
+
+      public boolean isSingleDatastoreMode() {
+        return false;
+      }
+
+      public boolean isCommandLineMode() {
+        return false;
+      }
+
+      public ExitActionListener getExitActionListener() {
+        return null;
+      }
+
+      public CliArguments getCommandLineArguments() {
+        return null;
+      }
+
+      public void initializeSingleDatastoreJob(AnalysisJobBuilder arg0, DataContext arg1) {
+      }
+    };
+    Bootstrap bootstrap = new Bootstrap(bootstrapOptions);
+    bootstrap.run();
+
+  }
+
+  public void profileStep() throws Exception {
+
+    final Spoon spoon = ((Spoon) SpoonFactory.getInstance());
+
     try {
+
+      final TransMeta transMeta = spoon.getActiveTransformation();
+      if (transMeta == null || spoon.getActiveTransGraph() == null) {
+        return;
+      }
+      if (transMeta.hasChanged()) {
+        MessageBox box = new MessageBox(spoon.getShell(), SWT.ICON_ERROR | SWT.CLOSE);
+        box.setText("Save first...");
+        box.setMessage("Please save your transformation first.  To run the profiling job, we need to run the transformation.");
+        box.open();
+        return;
+      }
+
+      StepMeta stepMeta = spoon.getActiveTransGraph().getCurrentStep();
+      if (stepMeta == null) {
+        return;
+      }
+
+      final String schemaTable = stepMeta.getName();
+
       // Now that we have the transformation and everything we can profile it...
       //
       final String url;
       final String driverClass;
       if (!Const.isEmpty(transMeta.getFilename())) {
         String filename = KettleVFS.getFileObject(transMeta.getFilename()).toString();
-        url = "jdbc:kettle:"+filename;
+        url = "jdbc:kettle:" + filename;
         driverClass = "org.pentaho.di.jdbc.KettleDriver";
-        
+
       } else {
         MessageBox box = new MessageBox(spoon.getShell(), SWT.ICON_ERROR | SWT.CLOSE);
         box.setText("TODO");
@@ -108,16 +160,15 @@ public class ModelerHelper extends AbstractXulEventHandler implements ISpoonMenu
         box.open();
         return;
       }
-      
+
       BootstrapOptions bootstrapOptions = new BootstrapOptions() {
 
+        public Image getWelcomeImage() {
+          return null;
+        }
+
         public Datastore getSingleDatastore(DatastoreCatalog catalog) {
-            return new JdbcDatastore(transMeta.getName(), 
-                url, 
-                driverClass, 
-                "", 
-                ""
-              );
+          return new JdbcDatastore(transMeta.getName(), url, driverClass, "", "");
         };
 
         @Override
@@ -140,15 +191,43 @@ public class ModelerHelper extends AbstractXulEventHandler implements ISpoonMenu
           // TODO Auto-generated method stub
           return null;
         }
+
+        @Override
+        public void initializeSingleDatastoreJob(AnalysisJobBuilder analysisJobBuilder, DataContext dataContext) {
+          // add all columns of a table
+          Column[] customerColumns = dataContext.getTableByQualifiedLabel(schemaTable).getColumns();
+          analysisJobBuilder.addSourceColumns(customerColumns);
+
+          List<InputColumn<?>> numberColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.NUMBER);
+          if (!numberColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(NumberAnalyzer.class).addInputColumns(numberColumns);
+          }
+
+          List<InputColumn<?>> dateColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.DATE);
+          if (!dateColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(DateAndTimeAnalyzer.class).addInputColumns(dateColumns);
+          }
+
+          List<InputColumn<?>> booleanColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.BOOLEAN);
+          if (!booleanColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(BooleanAnalyzer.class).addInputColumns(booleanColumns);
+          }
+
+          List<InputColumn<?>> stringColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.STRING);
+          if (!stringColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(StringAnalyzer.class).addInputColumns(stringColumns);
+          }
+        }
       };
       Bootstrap bootstrap = new Bootstrap(bootstrapOptions);
       bootstrap.run();
 
-      
-    } catch(Exception e) {
-      new ErrorDialog(spoon.getShell(), "Error", "Error profiling:", e);
+    } catch (final Exception e) {
+      new ErrorDialog(spoon.getShell(), "Error", "unexpected error occurred", e);
+    } finally {
+      //
     }
-    
+
   }
 
   @Override
--- src-plugins/kettle-profiling-datacleaner/src/org/pentaho/di/profiling/datacleaner/ProfilerDatabaseExplorerController.java
+++ src-plugins/kettle-profiling-datacleaner/src/org/pentaho/di/profiling/datacleaner/ProfilerDatabaseExplorerController.java
@@ -18,102 +18,152 @@
  */
 package org.pentaho.di.profiling.datacleaner;
 
+import java.awt.Image;
+import java.util.List;
+
+import org.eobjects.analyzer.beans.BooleanAnalyzer;
+import org.eobjects.analyzer.beans.DateAndTimeAnalyzer;
+import org.eobjects.analyzer.beans.NumberAnalyzer;
+import org.eobjects.analyzer.beans.StringAnalyzer;
 import org.eobjects.analyzer.cli.CliArguments;
 import org.eobjects.analyzer.connection.Datastore;
 import org.eobjects.analyzer.connection.DatastoreCatalog;
 import org.eobjects.analyzer.connection.JdbcDatastore;
+import org.eobjects.analyzer.data.DataTypeFamily;
+import org.eobjects.analyzer.data.InputColumn;
+import org.eobjects.analyzer.job.builder.AnalysisJobBuilder;
 import org.eobjects.datacleaner.bootstrap.Bootstrap;
 import org.eobjects.datacleaner.bootstrap.BootstrapOptions;
 import org.eobjects.datacleaner.bootstrap.ExitActionListener;
+import org.eobjects.metamodel.DataContext;
+import org.eobjects.metamodel.schema.Column;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleDatabaseException;
+import org.pentaho.di.core.gui.SpoonFactory;
 import org.pentaho.di.ui.core.database.dialog.XulDatabaseExplorerController;
+import org.pentaho.di.ui.core.dialog.ErrorDialog;
+import org.pentaho.di.ui.spoon.Spoon;
 import org.pentaho.ui.xul.XulException;
 import org.pentaho.ui.xul.impl.AbstractXulEventHandler;
 
 public class ProfilerDatabaseExplorerController extends AbstractXulEventHandler {
 
-	private XulDatabaseExplorerController dbExplorerController;
+  private XulDatabaseExplorerController dbExplorerController;
 
-	public ProfilerDatabaseExplorerController() {
-	}
+  public ProfilerDatabaseExplorerController() {
+  }
 
-	public String getName() {
-		return "profiler_database"; //$NON-NLS-1$
-	}
-	
-  public void profileDbTable() {
-    getDbController();
-    
-    // Close the db explorer...
-    //
-    this.dbExplorerController.close();
-    
-    final DatabaseMeta dbMeta = this.dbExplorerController.getDatabaseMeta();
-    final String tableName= this.dbExplorerController.getSelectedTable();
-    final String schemaName = this.dbExplorerController.getSelectedSchema();
-    
-    // TODO: profile the table...
-    //
-    System.out.println("Database: "+dbMeta.getName());
-    System.out.println("Table: "+dbMeta.getQuotedSchemaTableCombination(schemaName, tableName));
-    System.out.println("Database: "+dbMeta.getName());
-    
-    BootstrapOptions bootstrapOptions = new BootstrapOptions() {
+  public String getName() {
+    return "profiler_database"; //$NON-NLS-1$
+  }
 
-      public Datastore getSingleDatastore(DatastoreCatalog catalog) {
-        try {
-          return new JdbcDatastore(dbMeta.getName(), 
-              dbMeta.getURL(), 
-              dbMeta.getDriverClass(), 
-              dbMeta.getUsername(), 
-              dbMeta.getPassword()
-            );
-        } catch (KettleDatabaseException e) {
-          throw new RuntimeException(e);
+  public void profileDbTable() throws Exception {
+
+    final Spoon spoon = ((Spoon) SpoonFactory.getInstance());
+
+    try {
+
+      getDbController();
+      // Close the db explorer...
+      //
+      dbExplorerController.close();
+
+      final DatabaseMeta dbMeta = dbExplorerController.getDatabaseMeta();
+      final String tableName = dbExplorerController.getSelectedTable();
+      final String schemaName = dbExplorerController.getSelectedSchema();
+
+      // TODO: profile the table...
+      //
+      final String schemaTable = dbMeta.getQuotedSchemaTableCombination(schemaName, tableName);
+
+      final BootstrapOptions bootstrapOptions = new BootstrapOptions() {
+
+        public Datastore getSingleDatastore(DatastoreCatalog catalog) {
+          try {
+            return new JdbcDatastore(dbMeta.getName(), dbMeta.getURL(), dbMeta.getDriverClass(), dbMeta.getUsername(), dbMeta.getPassword());
+          } catch (KettleDatabaseException e) {
+            throw new RuntimeException(e);
+          }
+        };
+
+        public Image getWelcomeImage() {
+          return null;
+        }
+
+        @Override
+        public boolean isSingleDatastoreMode() {
+          return true;
+        }
+
+        @Override
+        public boolean isCommandLineMode() {
+          return false;
+        }
+
+        @Override
+        public ExitActionListener getExitActionListener() {
+          return null;
+        }
+
+        @Override
+        public CliArguments getCommandLineArguments() {
+          // TODO Auto-generated method stub
+          return null;
+        }
+
+        @Override
+        public void initializeSingleDatastoreJob(AnalysisJobBuilder analysisJobBuilder, DataContext dataContext) {
+
+          // add all columns of a table
+          Column[] customerColumns = dataContext.getTableByQualifiedLabel(schemaTable).getColumns();
+          analysisJobBuilder.addSourceColumns(customerColumns);
+
+          List<InputColumn<?>> numberColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.NUMBER);
+          if (!numberColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(NumberAnalyzer.class).addInputColumns(numberColumns);
+          }
+
+          List<InputColumn<?>> dateColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.DATE);
+          if (!dateColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(DateAndTimeAnalyzer.class).addInputColumns(dateColumns);
+          }
+
+          List<InputColumn<?>> booleanColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.BOOLEAN);
+          if (!booleanColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(BooleanAnalyzer.class).addInputColumns(booleanColumns);
+          }
+
+          List<InputColumn<?>> stringColumns = analysisJobBuilder.getAvailableInputColumns(DataTypeFamily.STRING);
+          if (!stringColumns.isEmpty()) {
+            analysisJobBuilder.addAnalyzer(StringAnalyzer.class).addInputColumns(stringColumns);
+          }
         }
       };
 
-      @Override
-      public boolean isSingleDatastoreMode() {
-        return true;
-      }
+      Bootstrap bootstrap = new Bootstrap(bootstrapOptions);
+      bootstrap.run();
 
-      @Override
-      public boolean isCommandLineMode() {
-        return false;
-      }
-
-      @Override
-      public ExitActionListener getExitActionListener() {
-        return null;
-      }
-
-      @Override
-      public CliArguments getCommandLineArguments() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-    };
-    Bootstrap bootstrap = new Bootstrap(bootstrapOptions);
-    bootstrap.run();
+    } catch (final Exception ex) {
+      new ErrorDialog(spoon.getShell(), "Error", "unexpected error occurred", ex);
+    } finally {
+      //
+    }
 
   }
-  
 
-	private XulDatabaseExplorerController getDbController() {
-	  if (dbExplorerController == null) {
-	    try {
+  private XulDatabaseExplorerController getDbController() {
+    if (dbExplorerController == null) {
+      try {
         dbExplorerController = (XulDatabaseExplorerController) this.getXulDomContainer().getEventHandler("dbexplorer");
       } catch (XulException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
-	  }
-	  return dbExplorerController;
-	}
-	
-	public void setData(Object aDatabaseDialog) {
-		this.dbExplorerController = (XulDatabaseExplorerController) aDatabaseDialog;
-	}
+    }
+    return dbExplorerController;
+  }
+
+  public void setData(Object aDatabaseDialog) {
+    this.dbExplorerController = (XulDatabaseExplorerController) aDatabaseDialog;
+  }
 }
