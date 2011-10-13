63186d80380b78d4fb8661e65ea4727ad352614a
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2011-10-13 22:11:54 +0000

    PDI-6734 : Create unit tests that will test the functionality of the various repository types
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@15888 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ test/org/pentaho/di/repository/KettleDatabaseRepositoryTest.java
@@ -0,0 +1,216 @@
+package org.pentaho.di.repository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSelectInfo;
+import org.apache.commons.vfs.FileSelector;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepository;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepositoryCreationHelper;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepositoryMeta;
+import org.pentaho.di.trans.TransMeta;
+
+public class KettleDatabaseRepositoryTest extends TestCase {
+
+  protected KettleDatabaseRepositoryMeta repositoryMeta;
+  protected KettleDatabaseRepository repository;
+  protected RepositoryDirectoryInterface tree;
+  
+  public void testDatabaseRepository() throws Exception {
+
+    KettleEnvironment.init();
+    String filename = File.createTempFile("kdrtest", "").getAbsolutePath();
+    System.out.println("Using file '"+filename+"' as a H2 database repository");
+    
+    try {
+      DatabaseMeta databaseMeta = new DatabaseMeta("H2Repo", "H2", "JDBC", null, filename, null, null, null);
+      repositoryMeta = new KettleDatabaseRepositoryMeta("KettleDatabaseRepository", "H2Repo", "H2 Repository", databaseMeta);
+      repository = new KettleDatabaseRepository();
+      repository.init(repositoryMeta);
+      repository.connectionDelegate.connect(true, true);
+      KettleDatabaseRepositoryCreationHelper helper = new KettleDatabaseRepositoryCreationHelper(repository);
+      helper.createRepositorySchema(null, false, new ArrayList<String>(), false);
+      repository.disconnect();
+  
+      // Test connecting...
+      //
+      repository.connect("admin", "admin");
+      assertTrue(repository.isConnected());
+      
+      // Test loading the directory tree
+      tree = repository.loadRepositoryDirectoryTree();
+      assertNotNull(tree);
+  
+      // Test directory creation
+      //
+      RepositoryDirectoryInterface fooDirectory = repository.createRepositoryDirectory(tree, "foo");
+      RepositoryDirectoryInterface barDirectory = repository.createRepositoryDirectory(fooDirectory, "bar");
+      RepositoryDirectoryInterface samplesDirectory = repository.createRepositoryDirectory(fooDirectory, "samples");
+  
+      // Test directory path lookup
+      RepositoryDirectoryInterface checkBar = tree.findDirectory("/foo/bar");
+      assertNotNull(checkBar);
+      assertTrue(checkBar.equals(barDirectory));
+      
+      // Save all the transformations samples.
+      //
+      verifyTransformationSamples(samplesDirectory);
+      verifyJobSamples(samplesDirectory);
+      
+      // Finally test disconnecting
+      repository.disconnect();
+      assertFalse(repository.isConnected());
+      
+    } catch(Exception e) {
+      e.printStackTrace();
+      throw new KettleException("Error during database repository unit testing", e);
+    } finally {
+      // Remove the H2 database file
+      //
+      assertTrue(new File(filename+".h2.db").delete());
+      assertTrue(new File(filename+".trace.db").delete());
+    }    
+  }
+
+  private void verifyTransformationSamples(RepositoryDirectoryInterface samplesDirectory) throws Exception {
+    File transSamplesFolder = new File("samples/transformations/");
+    String[] files = transSamplesFolder.list(new FilenameFilter() { public boolean accept(File dir, String name) { return name.endsWith(".ktr"); }});
+    Arrays.sort(files);
+    
+    for (String file : files) {
+      String transFilename = transSamplesFolder.getAbsolutePath()+"/"+file;
+      System.out.println("Storing/Loading/validating transformation '"+transFilename+"'");
+      
+      // Load the TransMeta object...
+      //
+      TransMeta transMeta = new TransMeta(transFilename);
+      if (Const.isEmpty(transMeta.getName())) {
+        transMeta.setName(Const.createName(file));
+      }
+      
+      // Save it in the repository in the samples folder
+      //
+      transMeta.setRepositoryDirectory(samplesDirectory);
+      repository.save(transMeta, "unit testing");
+      assertNotNull(transMeta.getObjectId());
+      
+      // Load it back up again...
+      //
+      TransMeta repTransMeta = repository.loadTransformation(transMeta.getObjectId(), null);
+      String oneXml = repTransMeta.getXML();
+      
+      // Save & load it again
+      //
+      repository.save(transMeta, "unit testing");
+      repTransMeta = repository.loadTransformation(transMeta.getObjectId(), null);
+      String twoXml = repTransMeta.getXML();
+      
+      // The XML needs to be identical after loading
+      //
+      // storeFile(oneXml, "/tmp/one.ktr");
+      // storeFile(twoXml, "/tmp/two.ktr");
+      
+      assertEquals(oneXml, twoXml);
+    }
+    
+    // Verify the number of stored files, see if we can find them all again.
+    //
+    System.out.println("Stored "+files.length+" transformation samples in folder "+samplesDirectory.getPath());
+    String[] transformationNames = repository.getTransformationNames(samplesDirectory.getObjectId(), false);
+    assertEquals(files.length, transformationNames.length);
+  }
+  
+  private void verifyJobSamples(RepositoryDirectoryInterface samplesDirectory) throws Exception {
+    FileObject jobSamplesFolder = KettleVFS.getFileObject("samples/jobs/");
+    FileObject[] files = jobSamplesFolder.findFiles(new FileSelector() {
+      
+      @Override
+      public boolean traverseDescendents(FileSelectInfo arg0) throws Exception {
+        return true;
+      }
+      
+      @Override
+      public boolean includeFile(FileSelectInfo info) throws Exception {
+        return info.getFile().getName().getExtension().equalsIgnoreCase("kjb");
+      }
+    });
+    
+    List<FileObject> filesList = Arrays.asList(files);
+    Collections.sort(filesList, new Comparator<FileObject>() {
+      @Override
+      public int compare(FileObject o1, FileObject o2) {
+        return o1.getName().getPath().compareTo(o2.getName().getPath());
+      }
+    });
+    
+    for (FileObject file : filesList) {
+      String jobFilename = file.getName().getPath();
+      System.out.println("Storing/Loading/validating job '"+jobFilename+"'");
+      
+      // Load the JobMeta object...
+      //
+      JobMeta jobMeta = new JobMeta(jobFilename, repository);
+      if (Const.isEmpty(jobMeta.getName())) {
+        jobMeta.setName(Const.createName(file.getName().getBaseName()));
+      }
+      
+      // Save it in the repository in the samples folder
+      //
+      jobMeta.setRepositoryDirectory(samplesDirectory);
+      repository.save(jobMeta, "unit testing");
+      assertNotNull(jobMeta.getObjectId());
+      
+      // Load it back up again...
+      //
+      JobMeta repJobMeta = repository.loadJob(jobMeta.getObjectId(), null);
+      String oneXml = repJobMeta.getXML();
+      
+      // Save & load it again
+      //
+      repository.save(jobMeta, "unit testing");
+      repJobMeta = repository.loadJob(jobMeta.getObjectId(), null);
+      String twoXml = repJobMeta.getXML();
+      
+      // The XML needs to be identical after loading
+      //
+      // storeFile(oneXml, "/tmp/one.ktr");
+      // storeFile(twoXml, "/tmp/two.ktr");
+      //
+      assertEquals(oneXml, twoXml);
+    }
+    
+    // Verify the number of stored files, see if we can find them all again.
+    //
+    System.out.println("Stored "+files.length+" job samples in folder "+samplesDirectory.getPath());
+    String[] jobNames = repository.getJobNames(samplesDirectory.getObjectId(), false);
+    assertEquals(files.length, jobNames.length);
+  }
+
+
+  protected void storeFile(String xml, String filename) throws Exception {
+    File file = new File(filename);
+    FileOutputStream fos = new FileOutputStream(file);
+
+    fos.write(XMLHandler.getXMLHeader(Const.XML_ENCODING).getBytes(Const.XML_ENCODING));
+    fos.write(xml.getBytes(Const.XML_ENCODING));
+    fos.close();
+    
+  }
+  
+}
---
+++ test/org/pentaho/di/repository/KettleFileRepositoryTest.java
@@ -0,0 +1,230 @@
+package org.pentaho.di.repository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSelectInfo;
+import org.apache.commons.vfs.FileSelector;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.repository.filerep.KettleFileRepository;
+import org.pentaho.di.repository.filerep.KettleFileRepositoryMeta;
+import org.pentaho.di.trans.TransMeta;
+
+public class KettleFileRepositoryTest extends TestCase {
+
+  protected KettleFileRepositoryMeta repositoryMeta;
+  protected KettleFileRepository repository;
+  protected RepositoryDirectoryInterface tree;
+  
+  public void testDatabaseRepository() throws Exception {
+
+    KettleEnvironment.init();
+    String dirName = System.getProperty("java.io.tmpdir")+"/"+UUID.randomUUID();
+    File dirFile = new File(dirName);
+    if (!dirFile.mkdir()) {
+      throw new KettleException("bad luck, directory '"+dirName+"' already exists and can't be used to put a file repository in it.");
+    }
+    
+    System.out.println("Using folder '"+dirName+"' to store a file repository in.");
+    
+    try {
+      repositoryMeta = new KettleFileRepositoryMeta("KettleFileRepository", "FileRep", "File repository", dirName);
+      repository = new KettleFileRepository();
+      repository.init(repositoryMeta);
+      
+      // Test connecting... (no security needed)
+      //
+      repository.connect(null, null); 
+      assertTrue(repository.isConnected());
+      
+      // Test loading the directory tree
+      //
+      tree = repository.loadRepositoryDirectoryTree();
+      assertNotNull(tree);
+  
+      // Test directory creation
+      //
+      RepositoryDirectoryInterface fooDirectory = repository.createRepositoryDirectory(tree, "foo");
+      RepositoryDirectoryInterface barDirectory = repository.createRepositoryDirectory(fooDirectory, "bar");
+      RepositoryDirectoryInterface samplesDirectory = repository.createRepositoryDirectory(fooDirectory, "samples");
+  
+      // Test directory path lookup
+      RepositoryDirectoryInterface checkBar = tree.findDirectory("/foo/bar");
+      assertNotNull(checkBar);
+      assertTrue(checkBar.equals(barDirectory));
+      
+      // Save all the transformations samples.
+      //
+      verifyTransformationSamples(samplesDirectory);
+      verifyJobSamples(samplesDirectory);
+      
+      // Finally test disconnecting
+      repository.disconnect();
+      assertFalse(repository.isConnected());
+      
+    } catch(Exception e) {
+      e.printStackTrace();
+      throw new KettleException("Error during database repository unit testing", e);
+    } finally {
+      // Remove all the files and folders in the repository...
+      //
+      FileUtils.deleteDirectory( dirFile );
+    }    
+  }
+
+  private void verifyTransformationSamples(RepositoryDirectoryInterface samplesDirectory) throws Exception {
+    File transSamplesFolder = new File("samples/transformations/");
+    String[] files = transSamplesFolder.list(new FilenameFilter() { public boolean accept(File dir, String name) { return name.endsWith(".ktr"); }});
+    Arrays.sort(files);
+    
+    for (String file : files) {
+      String transFilename = transSamplesFolder.getAbsolutePath()+"/"+file;
+      System.out.println("Storing/Loading/validating transformation '"+transFilename+"'");
+      
+      // Load the TransMeta object...
+      //
+      TransMeta transMeta = new TransMeta(transFilename);
+      transMeta.setFilename(null);
+      
+      // The name is sometimes empty in the file, duplicates are present too...
+      // Replaces slashes and the like as well...
+      //
+      transMeta.setName(Const.createName(file));
+      transMeta.setName(transMeta.getName().replace('/', '-'));
+      
+      // Save it in the repository in the samples folder
+      //
+      transMeta.setRepositoryDirectory(samplesDirectory);
+      repository.save(transMeta, "unit testing");
+      assertNotNull(transMeta.getObjectId());
+      
+      // Load it back up again...
+      //
+      TransMeta repTransMeta = repository.loadTransformation(transMeta.getObjectId(), null);
+      String oneXml = repTransMeta.getXML();
+      
+      // Save & load it again
+      //
+      repository.save(transMeta, "unit testing");
+      repTransMeta = repository.loadTransformation(transMeta.getObjectId(), null);
+      String twoXml = repTransMeta.getXML();
+      
+      // The XML needs to be identical after loading
+      //
+      // storeFile(oneXml, "/tmp/one.ktr");
+      // storeFile(twoXml, "/tmp/two.ktr");
+      
+      assertEquals(oneXml, twoXml);
+    }
+    
+    // Verify the number of stored files, see if we can find them all again.
+    //
+    System.out.println("Stored "+files.length+" transformation samples in folder "+samplesDirectory.getPath());
+    String[] transformationNames = repository.getTransformationNames(samplesDirectory.getObjectId(), false);
+    assertEquals(files.length, transformationNames.length);
+  }
+  
+  private void verifyJobSamples(RepositoryDirectoryInterface samplesDirectory) throws Exception {
+    FileObject jobSamplesFolder = KettleVFS.getFileObject("samples/jobs/");
+    FileObject[] files = jobSamplesFolder.findFiles(new FileSelector() {
+      
+      @Override
+      public boolean traverseDescendents(FileSelectInfo arg0) throws Exception {
+        return true;
+      }
+      
+      @Override
+      public boolean includeFile(FileSelectInfo info) throws Exception {
+        return info.getFile().getName().getExtension().equalsIgnoreCase("kjb");
+      }
+    });
+    
+    List<FileObject> filesList = Arrays.asList(files);
+    Collections.sort(filesList, new Comparator<FileObject>() {
+      @Override
+      public int compare(FileObject o1, FileObject o2) {
+        return o1.getName().getPath().compareTo(o2.getName().getPath());
+      }
+    });
+    
+    for (FileObject file : filesList) {
+      String jobFilename = file.getName().getPath();
+      System.out.println("Storing/Loading/validating job '"+jobFilename+"'");
+      
+      // Load the JobMeta object...
+      //
+      JobMeta jobMeta = new JobMeta(jobFilename, repository);
+      jobMeta.setFilename(null);
+      
+      // The name is sometimes empty in the file, duplicates are present too...
+      // Replaces slashes and the like as well...
+      //
+      jobMeta.setName(Const.createName(file.getName().getBaseName()));
+      jobMeta.setName(jobMeta.getName().replace('/', '-'));
+
+      if (Const.isEmpty(jobMeta.getName())) {
+        jobMeta.setName(Const.createName(file.getName().getBaseName()));
+      }
+      if (jobMeta.getName().contains("/")) {
+        jobMeta.setName(jobMeta.getName().replace('/', '-'));
+      }
+
+      // Save it in the repository in the samples folder
+      //
+      jobMeta.setRepositoryDirectory(samplesDirectory);
+      repository.save(jobMeta, "unit testing");
+      assertNotNull(jobMeta.getObjectId());
+      
+      // Load it back up again...
+      //
+      JobMeta repJobMeta = repository.loadJob(jobMeta.getObjectId(), null);
+      String oneXml = repJobMeta.getXML();
+      
+      // Save & load it again
+      //
+      repository.save(jobMeta, "unit testing");
+      repJobMeta = repository.loadJob(jobMeta.getObjectId(), null);
+      String twoXml = repJobMeta.getXML();
+      
+      // The XML needs to be identical after loading
+      //
+      // storeFile(oneXml, "/tmp/one.ktr");
+      // storeFile(twoXml, "/tmp/two.ktr");
+      //
+      assertEquals(oneXml, twoXml);
+    }
+    
+    // Verify the number of stored files, see if we can find them all again.
+    //
+    System.out.println("Stored "+files.length+" job samples in folder "+samplesDirectory.getPath());
+    String[] jobNames = repository.getJobNames(samplesDirectory.getObjectId(), false);
+    assertEquals(files.length, jobNames.length);
+  }
+
+
+  protected void storeFile(String xml, String filename) throws Exception {
+    File file = new File(filename);
+    FileOutputStream fos = new FileOutputStream(file);
+
+    fos.write(XMLHandler.getXMLHeader(Const.XML_ENCODING).getBytes(Const.XML_ENCODING));
+    fos.write(xml.getBytes(Const.XML_ENCODING));
+    fos.close();
+    
+  }
+  
+}
