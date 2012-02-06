5a3f0c8d4e22b762da3e25b9829511abe778ae7f
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-02-06 14:23:28 +0000

    PDI-7147 : Change Kettle license to Apache
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.3.0@16339 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-dbdialog/org/pentaho/ui/util/Launch.java
+++ src-dbdialog/org/pentaho/ui/util/Launch.java
@@ -1,13 +1,25 @@
-///////////////////////////////////////////////////////// 
-// Bare Bones Browser Launch                           // 
-// Version 1.5 (December 10, 2005)                     // 
-// By Dem Pilafian                                     // 
-// Supports: Mac OS X, GNU/Linux, Unix, Windows XP     // 
-// Example Usage:                                      // 
-// String url = "http://www.centerkey.com/";           // 
-// BareBonesBrowserLaunch.openURL(url);                // 
-// Public Domain Software -- Free to Use as You Like   // 
-/////////////////////////////////////////////////////////
+/*******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.ui.util;
 
 import java.lang.reflect.Method;
