017affcebb3e6c6c35e4da4eb2ee2b4db3d3cfd7
sflatley <sflatley@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-04-17 13:23:28 +0000

    PDI-4654: Reverted changes.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.3.0@16752 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/valuemapper/ValueMapperMeta.java
+++ src/org/pentaho/di/trans/steps/valuemapper/ValueMapperMeta.java
@@ -179,21 +179,7 @@ public class ValueMapperMeta extends BaseStepMeta implements StepMetaInterface
 		if (!Const.isEmpty(getTargetField()))
         {
 		    extra = new ValueMeta(getTargetField(), ValueMetaInterface.TYPE_STRING);
-		    extra.setOrigin(name);
-            r.addValueMeta(extra);
-        }
-		else
-		{
-			if (!Const.isEmpty(getFieldToUse())) {
-				extra = r.searchValueMeta(getFieldToUse());
-			}
-		}
-		
-		if (extra!=null) {
-			// The output of a changed field or new field is always a normal storage type...
-			//
-			extra.setStorageType(ValueMetaInterface.STORAGE_TYPE_NORMAL);
-			
+		    
             // Lengths etc?
             // Take the max length of all the strings...
 		    //
@@ -210,7 +196,20 @@ public class ValueMapperMeta extends BaseStepMeta implements StepMetaInterface
                 maxlen = nonMatchDefault.length();
             }
             extra.setLength(maxlen);
-			
+            extra.setOrigin(name);
+            r.addValueMeta(extra);
+        }
+		else
+		{
+			if (!Const.isEmpty(getFieldToUse())) {
+				extra = r.searchValueMeta(getFieldToUse());
+			}
+		}
+		
+		if (extra!=null) {
+			// The output of a changed field or new field is always a normal storage type...
+			//
+			extra.setStorageType(ValueMetaInterface.STORAGE_TYPE_NORMAL);
 		}
 	}
 
