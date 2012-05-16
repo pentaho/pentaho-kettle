363cc6378778e9d11af6da70f7d0778e2f9e371b
sflatley <sflatley@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-05-16 14:54:54 +0000

    PDI-7782: About dialog is missing copyright year.  Updated version for the 4.3.1 check in.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.3.1@16846 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -72,7 +72,12 @@ public class Const
   /**
    * Version number
    */
-  public static final String VERSION = "4.3.0";
+  public static final String VERSION = "4.3.1";
+  
+  /**
+   * Copyright year 
+   */
+  public static final String COPYRIGHT_YEAR = "2012";
 
   /**
    * Release Type
