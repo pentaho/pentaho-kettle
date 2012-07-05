316ce1c06528117bdaac2d2228eca279c13a5c75
shassan2 <shassan2@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-07-05 08:36:47 +0000

    PDI-7289 : SFTP - Add Failure On No Files fix UI issue
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@16978 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/trans/steps/sftpput/SFTPPutDialog.java
+++ src-ui/org/pentaho/di/ui/trans/steps/sftpput/SFTPPutDialog.java
@@ -1037,7 +1037,7 @@ public class SFTPPutDialog extends BaseStepDialog implements StepDialogInterface
 		wUserName.setText(Const.NVL(input.getUserName(), ""));
 		wPassword.setText(Const.NVL(input.getPassword(), ""));
 		wRemoteDirectory.setText(Const.NVL(input.getRemoteDirectoryFieldName(), ""));
-		wSourceFileNameField.setText(Const.NVL(input.getSourceFileNameFieldName(), ""));
+		wSourceFileNameField.setText(Const.NVL(input.getSourceFileFieldName(), ""));
         wInputIsStream.setSelection(input.isInputStream());
         wAddFilenameToResult.setSelection(input.isAddFilenameResut());
         wusePublicKey.setSelection(input.isUseKeyFile());
@@ -1078,7 +1078,7 @@ public class SFTPPutDialog extends BaseStepDialog implements StepDialogInterface
 		input.setUserName(wUserName.getText());
 		input.setPassword(wPassword.getText());
 		input.setRemoteDirectoryFieldName(wRemoteDirectory.getText());
-		input.setSourceFileNameFieldName(wSourceFileNameField.getText());
+		input.setSourceFileFieldName(wSourceFileNameField.getText());
 		input.setAddFilenameResut(wAddFilenameToResult.getSelection());
 		input.setUseKeyFile(wusePublicKey.getSelection());
 		input.setKeyFilename(wKeyFilename.getText());
