2b47068a53fa1f5f491a61f332ef59a2a58bb8fb
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-08-07 17:14:07 +0000

    PDI-7936: Filter In List Against a Field only Uses First Field Value
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@17104 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Condition.java
+++ src-core/org/pentaho/di/core/Condition.java
@@ -489,7 +489,7 @@ public class Condition implements Cloneable, XMLInterface
           retval = (!fieldMeta.isNull(field));
           break;
         case FUNC_IN_LIST:
-          if (inList == null) {
+          if (inList==null || right_fieldnr>=0) { //performance reason: create the array first or again when it is against a field and not a constant
             inList = Const.splitString(fieldMeta2.getString(field2), ';');
             Arrays.sort(inList);
           }
