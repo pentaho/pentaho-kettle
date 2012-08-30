f2f4ce9de7748a29880729674059dd6ef5a937b5
jbleuel <jbleuel@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-08-30 19:58:51 +0000

    PDI-7699: Vertica v5.1 JDBC driver class name change
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.4.0@17232 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ src-db/org/pentaho/di/core/database/Vertica5DatabaseMeta.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.core.database;
+
+/**
+ * Vertica Analytic Database version 5 and later (changed driver class name) 
+ * 
+ * @author DEinspanjer
+ * @since  2009-03-16
+ * @author Matt
+ * @since  May-2008
+ * @author Jens
+ * @since  Aug-2012
+ */
+
+public class Vertica5DatabaseMeta extends VerticaDatabaseMeta
+{
+	@Override
+	public String getDriverClass()
+	{
+        if (getAccessType()==DatabaseMeta.TYPE_ACCESS_NATIVE)
+        {
+            return "com.vertica.jdbc.Driver";            
+        }
+        else
+        {
+            return "sun.jdbc.odbc.JdbcOdbcDriver"; // always ODBC!
+        }
+
+	}
+	
+}
