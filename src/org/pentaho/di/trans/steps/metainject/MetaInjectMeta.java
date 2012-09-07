98e56212f057fef3b391bae1f5240e1fb7b7fcfe
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-09-07 14:09:26 +0000

    PDI-7791 : "Mapping (sub-transformation)" step not passing Repository to sub-transformation or setting Internal.Transformation.Filename.Directory  (also making sure this issue doesn't present itself in the ETL metadata injection step)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@17273 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/metainject/MetaInjectMeta.java
+++ src/org/pentaho/di/trans/steps/metainject/MetaInjectMeta.java
@@ -369,6 +369,13 @@ public class MetaInjectMeta extends BaseStepMeta implements StepMetaInterface
       mappingTransMeta = rep.loadTransformation(mappingMeta.getTransObjectId(), null);
       break;
     }
+    
+    // Pass some important information to the mapping transformation metadata:
+    //
+    mappingTransMeta.copyVariablesFrom(space);
+    mappingTransMeta.setRepository(rep); 
+    mappingTransMeta.setFilename(mappingTransMeta.getFilename());
+    
     return mappingTransMeta;
   }
   
