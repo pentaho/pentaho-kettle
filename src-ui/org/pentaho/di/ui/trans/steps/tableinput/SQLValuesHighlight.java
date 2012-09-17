02150ce0da4f5d50dd6a8efd67497c9c56101098
mburgess <mburgess@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-09-17 16:29:44 +0000

    PDI-7822: Added EPL copyright to header
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/4.4.0@17345 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/trans/steps/tableinput/SQLValuesHighlight.java
+++ src-ui/org/pentaho/di/ui/trans/steps/tableinput/SQLValuesHighlight.java
@@ -19,6 +19,16 @@
  * limitations under the License.
  *
  ******************************************************************************/
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 package org.pentaho.di.ui.trans.steps.tableinput;
 
