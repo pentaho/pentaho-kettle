c390ed50ce460f2d30fe760615670eebf791d408
mburgess <mburgess@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-09-18 14:16:05 +0000

    PDI-6818: Fixed XSLT transformation step
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@17347 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-ui/org/pentaho/di/ui/trans/steps/xslt/XsltDialog.java
+++ src-ui/org/pentaho/di/ui/trans/steps/xslt/XsltDialog.java
@@ -483,7 +483,7 @@ public class XsltDialog extends BaseStepDialog implements StepDialogInterface
 	     fdlOutputProperties.top  = new FormAttachment(0, margin);
 	     wlOutputProperties.setLayoutData(fdlOutputProperties);
 
-	     final int OutputPropertiesRows=input.getParameterField().length;
+	     final int OutputPropertiesRows=input.getOutputPropertyName().length;
 		
 		 colinf=new ColumnInfo[] { 
 		  new ColumnInfo(BaseMessages.getString(PKG, "XsltDialog.ColumnInfo.OutputProperties.Name"),      ColumnInfo.COLUMN_TYPE_CCOMBO, new String[] { "" }, false),
