64b0514b97ba2308f83bc43d11efa42c4bc8d499
Mark Hall <mhall@pentaho.com>
2012-09-24 08:41:36 +1200

    Added javadoc header.

--- shims/common/src/org/pentaho/hbase/shim/fake/FakeHBaseConnection.java
+++ shims/common/src/org/pentaho/hbase/shim/fake/FakeHBaseConnection.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.hbase.shim.fake;
 
 import java.util.ArrayList;
@@ -14,14 +36,14 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.pentaho.di.core.variables.VariableSpace;
-import org.pentaho.hadoop.shim.ShimVersion;
-import org.pentaho.hbase.shim.api.ColumnFilter;
-import org.pentaho.hbase.shim.api.HBaseValueMeta;
 import org.pentaho.hbase.shim.common.CommonHBaseBytesUtil;
-import org.pentaho.hbase.shim.spi.HBaseBytesUtilShim;
-import org.pentaho.hbase.shim.spi.HBaseConnection;
 
+/**
+ * Implementation of HBaseConnection that partially "simulates" a real HBase
+ * instance. Used for unit testing the HBase steps and supporting classes.
+ * 
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
+ */
 public class FakeHBaseConnection extends HBaseConnection {
 
   public static class BytesComparator implements Comparator<byte[]> {
