81b0efe6b978663e002098b8a8a6bfc6f4e7340a
wgorman <wgorman@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-10-26 18:22:53 +0000

    PDI-8814 - clean up kettle plugin classloader so that plugins can uninstall and update.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@17519 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/plugins/KettleURLClassLoader.java
+++ src-core/org/pentaho/di/core/plugins/KettleURLClassLoader.java
@@ -22,11 +22,20 @@
 
 package org.pentaho.di.core.plugins;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Vector;
+import java.util.jar.JarFile;
 
+import org.hibernate.util.ReflectHelper;
 import org.pentaho.di.i18n.BaseMessages;
 
 public class KettleURLClassLoader extends URLClassLoader
@@ -154,4 +163,99 @@ public class KettleURLClassLoader extends URLClassLoader
             return null;
         }
     }
+    
+    private static Object getFieldObject( Class clazz, String name, Object obj) throws Exception {
+      Field field = clazz.getDeclaredField(name);
+      field.setAccessible(true);
+      return field.get(obj);
+    }
+
+    /**
+     * This method is designed to clear out classloader file locks in windows.
+     * 
+     * @param clazzLdr class loader to clean up
+     */
+    public void closeClassLoader() {
+      HashSet<String> closedFiles = new HashSet<String>();
+      try {
+        Object obj = getFieldObject(URLClassLoader.class, "ucp", this);
+        ArrayList loaders = (ArrayList) getFieldObject(obj.getClass(), "loaders", obj);
+        for (Object ldr : loaders) {
+          try {
+            JarFile file = (JarFile) getFieldObject(ldr.getClass(), "jar", ldr);
+            closedFiles.add(file.getName());
+            file.close();
+          } catch (Exception e) {
+            // skip
+          }
+        }
+      } catch (Exception e) {
+        // skip
+      }
+      
+      try {
+        Vector nativeLibArr = (Vector) getFieldObject(ClassLoader.class, "nativeLibraries", this);
+        for (Object lib : nativeLibArr) {
+          try {
+            Method fMethod = lib.getClass().getDeclaredMethod("finalize", new Class[0]);
+            fMethod.setAccessible(true);
+            fMethod.invoke(lib, new Object[0]);
+          } catch (Exception e) {
+            // skip
+          }
+        }
+      } catch (Exception e) {
+        // skip
+      }
+    
+      HashMap uCache = null;
+      HashMap fCache = null;
+      
+      try {
+        Class factory = getFieldObject(ReflectHelper.classForName("sun.net.www.protocol.jar.JarURLConnection"), "factory", null).getClass();
+        try {
+          fCache = (HashMap) getFieldObject(factory, "fileCache", null);
+        } catch (Exception e) {
+          // skip
+        }
+        try {
+          uCache = (HashMap) getFieldObject(factory, "urlCache", null);
+        } catch (Exception e) {
+          // skip
+        }
+        if (uCache != null) {
+          for (Object file : ((HashMap)uCache.clone()).keySet()) {
+            if (file instanceof JarFile) {
+              JarFile jar = (JarFile) file;
+              if (!closedFiles.contains(jar.getName())) continue;
+              try { 
+                jar.close();
+              } catch (IOException e) {
+                // skip
+              }
+              if (fCache != null) fCache.remove(uCache.get(jar));
+              uCache.remove(jar);
+            }
+          }
+        } else if (fCache != null) {
+          for (Object key : ((HashMap) fCache.clone()).keySet()) {
+            Object file = fCache.get(key);
+            if (file instanceof JarFile) {
+              JarFile jar = (JarFile) file;
+              if (!closedFiles.contains(jar.getName())) continue;
+              try {
+                jar.close();
+              } catch (IOException e) {
+                // ignore
+              }
+              fCache.remove(key);
+            }
+          }
+        }
+      } catch (Exception e) {
+        // skip
+        e.printStackTrace();
+      }
+    }
+
 }
