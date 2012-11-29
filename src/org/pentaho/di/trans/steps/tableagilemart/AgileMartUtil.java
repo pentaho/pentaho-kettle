d4228ef8c14eabe9d82df573992d7f6e467eef99
rfellows <rfellows@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-11-29 14:42:50 +0000

    PDI-8908 - NPE in MonetDB Bulk Loader step - AgileMartUtil.java
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@17613 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src/org/pentaho/di/trans/steps/tableagilemart/AgileMartUtil.java
+++ src/org/pentaho/di/trans/steps/tableagilemart/AgileMartUtil.java
@@ -1,5 +1,6 @@
 package org.pentaho.di.trans.steps.tableagilemart;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,43 +8,57 @@ import java.util.Date;
 import org.pentaho.di.core.ProvidesDatabaseConnectionInformation;
 import org.pentaho.metadata.registry.Entity;
 import org.pentaho.metadata.registry.IMetadataRegistry;
+import org.pentaho.metadata.registry.OrderedFileRegistry;
 import org.pentaho.metadata.registry.RegistryFactory;
 import org.pentaho.metadata.registry.util.RegistryUtil;
 
 public class AgileMartUtil {
 
 	public void updateMetadata( ProvidesDatabaseConnectionInformation dpci, long rowCount ) {
-		   // try to update the metadata registry
+		// try to update the metadata registry
 
 		RegistryFactory factory = RegistryFactory.getInstance();
-		    IMetadataRegistry registry = factory.getMetadataRegistry();
-		    RegistryUtil util = new RegistryUtil();
-		    String databaseName = dpci.getDatabaseMeta().getName();
-		    String schemaName = dpci.getSchemaName();
-		    String tableName = dpci.getTableName();
-		    Entity entity = util.getTableEntity(databaseName.toLowerCase(), (schemaName==null) ? null : schemaName.toLowerCase(), tableName.toLowerCase(), false);
-		    if( entity != null ) {
-				System.out.println("Util.updateMetadata writing "+util.generateTableId(dpci.getDatabaseMeta().getName(), dpci.getSchemaName(), dpci.getTableName())+" rowCount="+rowCount);
-		    	if( rowCount == -1 ) {
-		    		// the table has been emptied
-			    	util.setAttribute(entity, "rowcount", 0);
-		    	} else {
-		    		// add an offset
-			    	util.updateAttribute(entity, "rowcount", rowCount);
-		    	}
-		    	DateFormat fmt = new SimpleDateFormat();
-		    	Date now = new Date();
-		    	util.setAttribute(entity, "lastupdate", fmt.format(now));
-		    	util.setAttribute(entity, "lastupdatetick", now.getTime());
-		    } else {
-				System.out.println("Util.updateMetadata failed writing "+util.generateTableId(dpci.getDatabaseMeta().getName(), dpci.getSchemaName(), dpci.getTableName()));
-		    	
-		    }
-		    try {
-				registry.commit();
-			} catch (Exception e) {
-				// no biggie
-			}
-	}	
+    IMetadataRegistry registry = factory.getMetadataRegistry();
+
+    // PDI-8908 - NPE in MonetDB Bulk Loader step
+    if( registry == null ) {
+      try {
+        registry = new OrderedFileRegistry();
+        ((OrderedFileRegistry) registry).setFilePath(org.pentaho.di.core.Const.getKettleDirectory() + File.separator + "registry.xml"); //$NON-NLS-1$
+        factory.setMetadataRegistry(registry);
+        registry.init();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    RegistryUtil util = new RegistryUtil();
+    String databaseName = dpci.getDatabaseMeta().getName();
+    String schemaName = dpci.getSchemaName();
+    String tableName = dpci.getTableName();
+    Entity entity = util.getTableEntity(databaseName.toLowerCase(), (schemaName==null) ? null : schemaName.toLowerCase(), tableName.toLowerCase(), false);
+    if( entity != null ) {
+    System.out.println("Util.updateMetadata writing "+util.generateTableId(dpci.getDatabaseMeta().getName(), dpci.getSchemaName(), dpci.getTableName())+" rowCount="+rowCount);
+      if( rowCount == -1 ) {
+        // the table has been emptied
+        util.setAttribute(entity, "rowcount", 0);
+      } else {
+        // add an offset
+        util.updateAttribute(entity, "rowcount", rowCount);
+      }
+      DateFormat fmt = new SimpleDateFormat();
+      Date now = new Date();
+      util.setAttribute(entity, "lastupdate", fmt.format(now));
+      util.setAttribute(entity, "lastupdatetick", now.getTime());
+    } else {
+    System.out.println("Util.updateMetadata failed writing "+util.generateTableId(dpci.getDatabaseMeta().getName(), dpci.getSchemaName(), dpci.getTableName()));
+
+    }
+    try {
+    registry.commit();
+    } catch (Exception e) {
+      // no biggie
+    }
+  }
 	
 }
