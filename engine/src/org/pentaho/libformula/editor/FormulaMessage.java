96d1e935c05592bfd00e4cf3822d6439c4d1812d
mburgess <mburgess@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2012-12-12 20:47:07 +0000

    [PDI-3624]: Moved libformula-editor code to Kettle engine and ui modules
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/branches/trunk-restruct@17794 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ engine/src/org/pentaho/libformula/editor/FormulaMessage.java
@@ -0,0 +1,118 @@
+package org.pentaho.libformula.editor;
+
+import org.pentaho.reporting.libraries.formula.lvalues.ParsePosition;
+
+public class FormulaMessage {
+	public static final int TYPE_ERROR = 1;
+	public static final int TYPE_WARNING = 2;
+	public static final int TYPE_MESSAGE = 3;
+	public static final int TYPE_FUNCTION = 4;
+	public static final int TYPE_FIELD = 5;
+	public static final int TYPE_STATIC_NUMBER = 6;
+	public static final int TYPE_STATIC_STRING = 7;
+	public static final int TYPE_STATIC_DATE = 8;
+	public static final int TYPE_STATIC_LOGICAL = 9;
+	
+	private ParsePosition position;
+	private int type;
+	private String subject;
+	private String message;
+	
+	public FormulaMessage(int type, ParsePosition position, String subject, String message) {
+		this.type = type;
+		this.position = position;
+		this.subject = subject;
+		this.message = message;
+	}
+	
+	public FormulaMessage(int type, String subject, String message) {
+		this(type, null, subject, message);
+	}
+	
+	@Override
+	public String toString() {
+		String m = "";
+
+		switch(type) {
+		case TYPE_ERROR : m+="ERROR"; break;
+		case TYPE_WARNING : m+="WARNING"; break;
+		case TYPE_MESSAGE: m+="MESSAGE"; break;
+		case TYPE_FUNCTION: m+="FUNCTION"; break;
+		case TYPE_FIELD: m+="FIELD"; break;
+		case TYPE_STATIC_STRING: m+="STATIC STRING"; break;
+		case TYPE_STATIC_NUMBER: m+="STATIC NUMBER"; break;
+		case TYPE_STATIC_DATE: m+="STATIC DATE/TIME"; break;
+		case TYPE_STATIC_LOGICAL: m+="STATIC LOGICAL"; break;
+		default: break;
+		}
+		
+		if (position!=null) {
+			m+="@"+position.getStartLine()+"/"+position.getStartColumn()+" : "; 
+		} else {
+			m+=" : ";
+		}
+
+		m+=subject+" : "+message; 
+		
+		return m;
+	}
+	
+
+	/**
+	 * @return the position
+	 */
+	public ParsePosition getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(ParsePosition position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+}
---
+++ engine/src/org/pentaho/libformula/editor/function/FunctionLib.java
@@ -0,0 +1,98 @@
+package org.pentaho.libformula.editor.function;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class FunctionLib {
+	
+	private List<FunctionDescription> functions;
+	
+	public FunctionLib(String filename) throws KettleXMLException {    
+		functions = new ArrayList<FunctionDescription>();
+		
+		Document document = XMLHandler.loadXMLFile(getClass().getResourceAsStream(filename));
+		Node functionsNode = XMLHandler.getSubNode(document, "libformula-functions");
+		int nrFunctions = XMLHandler.countNodes(functionsNode, FunctionDescription.XML_TAG);
+		for (int i=0;i<nrFunctions;i++) {
+			Node functionNode = XMLHandler.getSubNodeByNr(functionsNode, FunctionDescription.XML_TAG, i);
+			this.functions.add(new FunctionDescription(functionNode));
+		}
+	}
+
+	/**
+	 * @return the functions
+	 */
+	public List<FunctionDescription> getFunctions() {
+		return functions;
+	}
+
+	/**
+	 * @param functions the functions to set
+	 */
+	public void setFunctions(List<FunctionDescription> functions) {
+		this.functions = functions;
+	}
+	
+	/**
+	 * @return A sorted array of function names, extracted from the function descriptions...
+	 */
+	public String[] getFunctionNames() {
+		String[] names = new String[functions.size()];
+		for (int i=0;i<functions.size();i++) {
+			names[i] = functions.get(i).getName();
+		}
+		Arrays.sort(names);
+		return names;
+	}
+	
+	/**
+	 * @return A sorted array of unique categories, extracted from the function descriptions...
+	 */
+	public String[] getFunctionCategories() {
+		List<String> categories = new ArrayList<String>();
+		for (FunctionDescription function : functions) {
+			String category = function.getCategory();
+			if (!categories.contains(category)) {
+				categories.add(category);
+			}
+		}
+		Collections.sort(categories);
+		return categories.toArray(new String[categories.size()]);
+	}
+	
+	/**
+	 * Get all the function names for a certain category
+	 * @param category the category name to look for
+	 * @return the sorted array of function names for the specified category
+	 */
+	public String[] getFunctionsForACategory(String category) {
+		List<String> names = new ArrayList<String>();
+		for (FunctionDescription function : functions) {
+			if (function.getCategory().equalsIgnoreCase(category)) {
+				names.add(function.getName());
+			}
+		}
+		Collections.sort(names);
+		return names.toArray(new String[names.size()]);
+	}
+
+	/**
+	 * @param functionName the name of the function to look for
+	 * @return the corresponding function description or null if nothing was found. 
+	 */
+	public FunctionDescription getFunctionDescription(String functionName) {
+		for (FunctionDescription function : functions) {
+			if (function.getName().equalsIgnoreCase(functionName)) {
+				return function;
+			}
+		}
+		return null;
+	}
+}
