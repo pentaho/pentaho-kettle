17148e831fda846e2de51608b3d9830f166fd628
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-02-27 20:31:36 +0000

    PDI-191 : As a developer/contributor, I want pluggable types PDI-7103 : Requirement for supporting Oracle Timestamp(6) datatype (Microseconds) PDI-9383 : As an ETL developer I want to have an overview of all the plugins that are loaded in Kettle + Additional cleanups
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@18121 5fb7f6ec-07c1-534a-b4ca-9155e429e800

---
+++ plugins/kettle5-log4j-plugin/src/org/pentaho/di/core/logging/log4j/Log4jLogging.java
@@ -0,0 +1,47 @@
+package org.pentaho.di.core.logging.log4j;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.pentaho.di.core.logging.CentralLogStore;
+import org.pentaho.di.core.logging.LoggingEvent;
+import org.pentaho.di.core.logging.LoggingPluginInterface;
+
+public class Log4jLogging implements LoggingPluginInterface {
+  
+  public static final String STRING_PENTAHO_DI_LOGGER_NAME = "org.pentaho.di";
+
+  public static final String STRING_PENTAHO_DI_CONSOLE_APPENDER = "ConsoleAppender:" + STRING_PENTAHO_DI_LOGGER_NAME;
+
+  private Logger pentahoLogger;
+  
+  public Log4jLogging() {
+    pentahoLogger = Logger.getLogger(STRING_PENTAHO_DI_LOGGER_NAME);
+    pentahoLogger.setAdditivity(false);
+  }
+
+  @Override
+  public void eventAdded(LoggingEvent event) {
+    switch(event.getLevel()) {
+      case ERROR: 
+        pentahoLogger.log(Level.ERROR, event.getMessage()); 
+        break; 
+      case DEBUG: 
+      case ROWLEVEL: 
+        pentahoLogger.log(Level.DEBUG, event.getMessage()); 
+        break; 
+      default: 
+        pentahoLogger.log(Level.INFO, event.getMessage()); 
+        break; 
+    }
+  }
+  
+  @Override
+  public void init() {
+    CentralLogStore.getAppender().addLoggingEventListener(this);
+  }
+  
+  public void dispose() {
+    CentralLogStore.getAppender().removeLoggingEventListener(this);
+  };
+
+}
