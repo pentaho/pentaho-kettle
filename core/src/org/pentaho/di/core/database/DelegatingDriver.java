6499d7d3cf7a4711ccbedb76887c0b6d02fc6aac
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-03-05 11:33:11 +0000

    javadoc improvement and Java7 compatibility fix.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@18151 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- core/src/org/pentaho/di/core/database/DelegatingDriver.java
+++ core/src/org/pentaho/di/core/database/DelegatingDriver.java
@@ -5,47 +5,56 @@ import java.sql.Driver;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-public class DelegatingDriver implements Driver
-{
-    private final Driver driver;
+/**
+ * This delegating driver allows the plugin system to be used in a separate 
+ * class loader while making DriverManager believe that the parent class loader is used.
+ * 
+ * It's an unfortunate hack but it works fine.  
+ *  
+ * @author matt
+ *
+ */
+public class DelegatingDriver implements Driver {
+  private final Driver driver;
 
-    public DelegatingDriver(Driver driver)
-    {
-        if (driver == null)
-        {
-            throw new IllegalArgumentException("Driver must not be null.");
-        }
-        this.driver = driver;
+  public DelegatingDriver(Driver driver) {
+    if (driver == null) {
+      throw new IllegalArgumentException("Driver must not be null.");
     }
+    this.driver = driver;
+  }
 
-    public Connection connect(String url, Properties info) throws SQLException
-    {
-       return driver.connect(url, info);
-    }
+  public Connection connect(String url, Properties info) throws SQLException {
+    return driver.connect(url, info);
+  }
 
-    public boolean acceptsURL(String url) throws SQLException
-    {
-       return driver.acceptsURL(url);
-    }
+  public boolean acceptsURL(String url) throws SQLException {
+    return driver.acceptsURL(url);
+  }
 
-    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException
-    {
-        return driver.getPropertyInfo(url, info);
-    }
+  public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+    return driver.getPropertyInfo(url, info);
+  }
 
-    public int getMajorVersion()
-    {
-        return driver.getMajorVersion();
-    }
+  public int getMajorVersion() {
+    return driver.getMajorVersion();
+  }
 
-    public int getMinorVersion()
-    {
-        return driver.getMinorVersion();
-    }
+  public int getMinorVersion() {
+    return driver.getMinorVersion();
+  }
 
-    public boolean jdbcCompliant()
-    { 
-        return driver.jdbcCompliant();
-    }
+  public boolean jdbcCompliant() {
+    return driver.jdbcCompliant();
+  }
+  
+  /**
+   * This method is added to make this driver compile on Java7
+   * @return always null until we finally switch over to Java7 with the codebase (TODO)
+   */
+  public Logger getParentLogger() {
+    return null;
+  }
 }
