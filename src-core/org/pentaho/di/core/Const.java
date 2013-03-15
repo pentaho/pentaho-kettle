7de977da9d218643e5213113e9f85a26c5c4d115
buildguy <buildguy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-03-15 15:33:57 +0000

    [SP-46] Update version and copyright constants.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/tags/4.4.0.2@18173 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -72,12 +72,12 @@ public class Const
   /**
    * Version number
    */
-  public static final String VERSION = "4.4.0";
+  public static final String VERSION = "4.4.0.2";
   
   /**
    * Copyright year 
    */
-  public static final String COPYRIGHT_YEAR = "2012";
+  public static final String COPYRIGHT_YEAR = "2013";
 
   /**
    * Release Type
