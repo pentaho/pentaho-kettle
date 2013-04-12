8be43640312c5a0064a3c150090db186fc7e02d0
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-04-12 21:47:42 +0000

    PDI-9754 : presence of "Identify last row in a stream" seems to cause an exception in Mapreduce reducer transformation
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@18301 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- engine/src/org/pentaho/di/trans/steps/detectlastrow/DetectLastRow.java
+++ engine/src/org/pentaho/di/trans/steps/detectlastrow/DetectLastRow.java
@@ -40,108 +40,105 @@ import org.pentaho.di.trans.step.StepMetaInterface;
  * @author Samatar
  * @since 03June2008
  */
-public class DetectLastRow extends BaseStep implements StepInterface
-{
-	private static Class<?> PKG = DetectLastRowMeta.class; // for i18n purposes, needed by Translator2!!   $NON-NLS-1$
+public class DetectLastRow extends BaseStep implements StepInterface {
+  private static Class<?> PKG = DetectLastRowMeta.class; // for i18n purposes, needed by Translator2!!   $NON-NLS-1$
 
-    private DetectLastRowMeta meta;
-    private DetectLastRowData data;
-    private Object[] previousRow;
-    
-    public DetectLastRow(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
-    {
-        super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
-    }
-       
-    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
-    {
-        meta=(DetectLastRowMeta)smi;
-        data=(DetectLastRowData)sdi;
-        
-        Object[] r = getRow();      // Get row from input rowset & set row busy!        
+  private DetectLastRowMeta meta;
 
-        if(first)
-        {
-        	if (getInputRowMeta() == null)  {
-        		setOutputDone();
-        		return false;
-        	}
-        	
-			// get the RowMeta
-			data.previousRowMeta = getInputRowMeta().clone();
-			data.NrPrevFields = data.previousRowMeta.size();
-			data.outputRowMeta = data.previousRowMeta;
-			meta.getFields(data.outputRowMeta, getStepname(), null, null, this, repository, metaStore);
-        }
-		Object[] outputRow=null;
+  private DetectLastRowData data;
 
-		if (r==null)  // no more input to be expected...
-		{
-			if(previousRow != null) 
-			{
-				//
-				// Output the last row with last row indicator set to true.
-				//
-		        outputRow = RowDataUtil.addRowData(previousRow, getInputRowMeta().size(), data.getTrueArray());
-				putRow(data.outputRowMeta, outputRow);  // copy row to output rowset(s);
+  private Object[] previousRow;
 
-				if (log.isRowLevel()) {
-					logRowlevel(BaseMessages.getString(PKG, "DetectLastRow.Log.WroteRowToNextStep")+data.outputRowMeta.getString(outputRow)); //$NON-NLS-1$
-				}
+  public DetectLastRow(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta,
+      Trans trans) {
+    super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+  }
 
-		        if (checkFeedback(getLinesRead())) {
-		        	logBasic(BaseMessages.getString(PKG, "DetectLastRow.Log.LineNumber")+getLinesRead()); //$NON-NLS-1$
-		        }				
-			}
+  public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
+    meta = (DetectLastRowMeta) smi;
+    data = (DetectLastRowData) sdi;
 
-			setOutputDone();
-			return false;
-		}
+    Object[] r = getRow(); // Get row from input rowset & set row busy!        
 
-		if(!first)
-		{
-	        outputRow = RowDataUtil.addRowData(previousRow, getInputRowMeta().size(), data.getFalseArray());
-			putRow(data.outputRowMeta, outputRow);  // copy row to output rowset(s);
-
-			if (log.isRowLevel()) {
-				logRowlevel(BaseMessages.getString(PKG, "DetectLastRow.Log.WroteRowToNextStep")+data.outputRowMeta.getString(outputRow)); //$NON-NLS-1$
-			}
-
-	        if (checkFeedback(getLinesRead())) {
-	        	logBasic(BaseMessages.getString(PKG, "DetectLastRow.Log.LineNumber")+getLinesRead()); //$NON-NLS-1$
-	        }		
-		}
-		// keep track of the current row
-		previousRow = r;
-		if (first) first = false;
-            
-        return true;
-    }
-    
-    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
-    {
-        meta=(DetectLastRowMeta)smi;
-        data=(DetectLastRowData)sdi;
-
-        if (super.init(smi, sdi))
-        {
-        	if(Const.isEmpty(meta.getResultFieldName()))
-        	{
-        		logError(BaseMessages.getString(PKG, "DetectLastRow.Error.ResultFieldMissing"));
-        		return false;
-        	}
-
-            return true;
-        }
+    if (first) {
+      if (getInputRowMeta() == null) {
+        setOutputDone();
         return false;
+      }
+
+      // get the RowMeta
+      data.previousRowMeta = getInputRowMeta().clone();
+      data.NrPrevFields = data.previousRowMeta.size();
+      data.outputRowMeta = data.previousRowMeta;
+      meta.getFields(data.outputRowMeta, getStepname(), null, null, this, repository, metaStore);
     }
-        
-    public void dispose(StepMetaInterface smi, StepDataInterface sdi)
+    Object[] outputRow = null;
+
+    if (r == null) // no more input to be expected...
     {
-        meta = (DetectLastRowMeta)smi;
-        data = (DetectLastRowData)sdi;
-     
-        super.dispose(smi, sdi);
+      if (previousRow != null) {
+        //
+        // Output the last row with last row indicator set to true.
+        //
+        if (!Const.isEmpty(meta.getResultFieldName())) {
+          outputRow = RowDataUtil.addRowData(previousRow, getInputRowMeta().size(), data.getTrueArray());
+        }
+        
+        putRow(data.outputRowMeta, outputRow); // copy row to output rowset(s);
+
+        if (log.isRowLevel()) {
+          logRowlevel(BaseMessages.getString(PKG, "DetectLastRow.Log.WroteRowToNextStep") + data.outputRowMeta.getString(outputRow)); //$NON-NLS-1$
+        }
+
+        if (checkFeedback(getLinesRead())) {
+          logBasic(BaseMessages.getString(PKG, "DetectLastRow.Log.LineNumber") + getLinesRead()); //$NON-NLS-1$
+        }
+      }
+
+      setOutputDone();
+      return false;
     }
-	    
+
+    if (!first) {
+      outputRow = RowDataUtil.addRowData(previousRow, getInputRowMeta().size(), data.getFalseArray());
+      putRow(data.outputRowMeta, outputRow); // copy row to output rowset(s);
+
+      if (log.isRowLevel()) {
+        logRowlevel(BaseMessages.getString(PKG, "DetectLastRow.Log.WroteRowToNextStep") + data.outputRowMeta.getString(outputRow)); //$NON-NLS-1$
+      }
+
+      if (checkFeedback(getLinesRead())) {
+        logBasic(BaseMessages.getString(PKG, "DetectLastRow.Log.LineNumber") + getLinesRead()); //$NON-NLS-1$
+      }
+    }
+    // keep track of the current row
+    previousRow = r;
+    if (first)
+      first = false;
+
+    return true;
+  }
+
+  public boolean init(StepMetaInterface smi, StepDataInterface sdi) {
+    meta = (DetectLastRowMeta) smi;
+    data = (DetectLastRowData) sdi;
+
+    if (super.init(smi, sdi)) {
+      if (Const.isEmpty(meta.getResultFieldName())) {
+        logError(BaseMessages.getString(PKG, "DetectLastRow.Error.ResultFieldMissing"));
+        return false;
+      }
+
+      return true;
+    }
+    return false;
+  }
+
+  public void dispose(StepMetaInterface smi, StepDataInterface sdi) {
+    meta = (DetectLastRowMeta) smi;
+    data = (DetectLastRowData) sdi;
+
+    super.dispose(smi, sdi);
+  }
+
 }
