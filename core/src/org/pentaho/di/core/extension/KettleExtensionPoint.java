5c652046fe9f54e78d81313bb8702d070996a150
mcasters <mcasters@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-04-17 15:33:56 +0000

    PDI-9778 : As a PDI plugin developer I need a flexible but specific extension point system in Kettle (id typo fix)
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@18327 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- core/src/org/pentaho/di/core/extension/KettleExtensionPoint.java
+++ core/src/org/pentaho/di/core/extension/KettleExtensionPoint.java
@@ -4,19 +4,19 @@ public enum KettleExtensionPoint {
 
   // Some transformation points
   //
-  TransformationPrepareExecution("TransformationStart", "A transformation begins to prepare execution"),
-  TransformationStartThreads("TransformationStart", "A transformation begins to start"),
+  TransformationPrepareExecution("TransformationPrepareExecution", "A transformation begins to prepare execution"),
+  TransformationStartThreads("TransformationStartThreads", "A transformation begins to start"),
   TransformationStarted("TransformationStart", "A transformation has started"),
   TransformationFinish("TransformationFinish", "A transformation finishes"),
-  TransformationMetaLoaded("TransMetaLoaded", "Transformation metadata was loaded"),
+  TransformationMetaLoaded("TransformationMetaLoaded", "Transformation metadata was loaded"),
   SpoonTransMetaExecutionStart("SpoonTransMetaExecutionStart", "Spoon initiates the execution of a trans (TransMeta)"),
+  SpoonTransExecutionConfiguration("SpoonTransExecutionConfiguration", "Right before Spoon configuration of transformation to be executed takes place"),
 
   JobStart("JobStart", "A job starts"),
   JobFinish("JobFinish", "A job finishes"),
-
   JobMetaLoaded("JobMetaLoaded", "Job metadata was loaded"),
-
   SpoonJobMetaExecutionStart("SpoonJobMetaExecutionStart", "Spoon initiates the execution of a job (JobMeta)"),
+  SpoonJobExecutionConfiguration("SpoonJobExecutionConfiguration", "Right before Spoon configuration of job to be executed takes place"),
   ;
 
   public String id;
