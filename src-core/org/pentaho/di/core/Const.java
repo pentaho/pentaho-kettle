6e560e8bb32b782dedaec6f48428046e172c6be8
buildguy <buildguy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-06-14 21:37:13 +0000

    [SP-151] Update static version string to 4.4.1.2
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/tags/4.4.1.2@18611 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -72,7 +72,7 @@ public class Const
   /**
    * Version number
    */
-  public static final String VERSION = "4.4.2";
+  public static final String VERSION = "4.4.1.2";
   
   /**
    * Copyright year 
