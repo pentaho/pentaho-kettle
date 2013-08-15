beeb9f61e06ab0082c768555b90697e2592c4aaf
brosander <brosander@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-08-15 17:20:48 +0000

    PDI-9422 - Refactoring CarteServlet to support overriding arbitrary servlets via web.xml
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@18861 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- engine/src/org/pentaho/di/www/CarteServlet.java
+++ engine/src/org/pentaho/di/www/CarteServlet.java
@@ -25,10 +25,12 @@ package org.pentaho.di.www;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -41,71 +43,102 @@ import org.pentaho.di.core.plugins.CartePluginType;
 import org.pentaho.di.core.plugins.PluginInterface;
 import org.pentaho.di.core.plugins.PluginRegistry;
 
-
 public class CarteServlet extends HttpServlet {
 
   private static final long serialVersionUID = 2434694833497859776L;
-  
+
   public static final String STRING_CARTE_SERVLET = "Carte Servlet";
-  
-  private final CartePluginRegistry cartePluginRegistry;
-  
+
+  private Map<String, CartePluginInterface> cartePluginRegistry;
+
   private final LogChannelInterface log;
-  
+
   public CarteServlet() {
     this.log = new LogChannel(STRING_CARTE_SERVLET);
-    
-    final TransformationMap transformationMap = CarteSingleton.getInstance().getTransformationMap();
-    final JobMap jobMap = CarteSingleton.getInstance().getJobMap();
-   
-    List<SlaveServerDetection> detections = Collections.synchronizedList(new ArrayList<SlaveServerDetection>());
-    SocketRepository socketRepository = CarteSingleton.getInstance().getSocketRepository();
-    
-    PluginRegistry pluginRegistry = PluginRegistry.getInstance();
-    List<PluginInterface> plugins = pluginRegistry.getPlugins(CartePluginType.class);
-    Map<String, CartePluginInterface> pluginMap = new HashMap<String, CartePluginInterface>();
-    for (PluginInterface plugin : plugins) {
-      CartePluginInterface servlet;
-      try {
-        servlet = (CartePluginInterface) pluginRegistry.loadClass(plugin);
-        servlet.setup(transformationMap, jobMap, socketRepository, detections);
-        servlet.setJettyMode(false);
-        String key = servlet.getContextPath();
-        if (key.startsWith("/kettle")) {
-          key = key.substring("/kettle".length());
-        }
-        pluginMap.put(key, servlet);
-      } catch (KettlePluginException e) {
-        log.logError("Unable to instantiate plugin for use with CarteServlet " + plugin.getName());
-      }
-    }
-
-    cartePluginRegistry = new CartePluginRegistry(pluginMap);
   }
 
-  public void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     doGet(req, resp);
   }
 
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String servletPath = req.getPathInfo();
     if (servletPath.endsWith("/")) {
       servletPath = servletPath.substring(0, servletPath.length() - 1);
     }
-    CartePluginInterface plugin = cartePluginRegistry.getCartePlugin(servletPath);
+    CartePluginInterface plugin = cartePluginRegistry.get(servletPath);
     if (plugin != null) {
       try {
         plugin.doGet(req, resp);
       } catch (Exception e) {
         throw new ServletException(e);
-      } 
+      }
     } else {
       if (log.isDebug()) {
         log.logDebug("Unable to find CartePlugin for key: /kettle" + req.getPathInfo());
       }
       resp.setStatus(404);
     }
+  }
+
+  private String getServletKey(CartePluginInterface servlet) {
+    String key = servlet.getContextPath();
+    if (key.startsWith("/kettle")) {
+      key = key.substring("/kettle".length());
+    }
+    return key;
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    final TransformationMap transformationMap = CarteSingleton.getInstance().getTransformationMap();
+    final JobMap jobMap = CarteSingleton.getInstance().getJobMap();
+
+    List<SlaveServerDetection> detections = Collections.synchronizedList(new ArrayList<SlaveServerDetection>());
+    SocketRepository socketRepository = CarteSingleton.getInstance().getSocketRepository();
+
+    PluginRegistry pluginRegistry = PluginRegistry.getInstance();
+    List<PluginInterface> plugins = pluginRegistry.getPlugins(CartePluginType.class);
+    Map<String, CartePluginInterface> pluginMap = new HashMap<String, CartePluginInterface>();
+
+    for (PluginInterface plugin : plugins) {
+      CartePluginInterface servlet;
+      try {
+        servlet = (CartePluginInterface) pluginRegistry.loadClass(plugin);
+        pluginMap.put(getServletKey(servlet), servlet);
+      } catch (KettlePluginException e) {
+        log.logError("Unable to instantiate plugin for use with CarteServlet " + plugin.getName());
+      }
+    }
+
+    // Servlets configured in web.xml take precedence to those discovered during plugin scan
+    @SuppressWarnings("unchecked")
+    Enumeration<String> initParameterNames = config.getInitParameterNames();
+    while (initParameterNames.hasMoreElements()) {
+      final String paramName = initParameterNames.nextElement();
+      final String className = config.getInitParameter(paramName);
+      final Class<?> clazz;
+      try {
+        clazz = Class.forName(className);
+        final CartePluginInterface servlet = (CartePluginInterface) clazz.newInstance();
+        pluginMap.put(getServletKey(servlet), servlet);
+      } catch (ClassNotFoundException e) {
+        log.logError("Unable to find configured " + paramName + " of " + className, e);
+      } catch (InstantiationException e) {
+        log.logError("Unable to instantiate configured " + paramName + " of " + className, e);
+      } catch (IllegalAccessException e) {
+        log.logError("Unable to access configured " + paramName + " of " + className, e);
+      } catch (ClassCastException e) {
+        log.logError("Unable to cast configured " + paramName + " of " + className + " to "
+            + CartePluginInterface.class, e);
+      }
+    }
+
+    for (CartePluginInterface servlet : pluginMap.values()) {
+      servlet.setup(transformationMap, jobMap, socketRepository, detections);
+      servlet.setJettyMode(false);
+    }
+
+    cartePluginRegistry = Collections.unmodifiableMap(pluginMap);
   }
 }
