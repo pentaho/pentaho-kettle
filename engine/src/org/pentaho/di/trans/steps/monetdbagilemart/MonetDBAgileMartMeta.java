297a21c7246899afd30bbbc0508763373d5d28f4
buildguy <buildguy@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-09-23 11:43:50 +0000

    [PPP-3081] license header updates on all .java, .js, and .jsp
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@19110 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- engine/src/org/pentaho/di/trans/steps/monetdbagilemart/MonetDBAgileMartMeta.java
+++ engine/src/org/pentaho/di/trans/steps/monetdbagilemart/MonetDBAgileMartMeta.java
@@ -1,8 +1,25 @@
-/**
- * The Pentaho proprietary code is licensed under the terms and conditions
- * of the software license agreement entered into between the entity licensing
- * such code and Pentaho Corporation. 
- */
+/*! ******************************************************************************
+*
+* Pentaho Data Integration
+*
+* Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+*
+*******************************************************************************
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+******************************************************************************/
+
 package org.pentaho.di.trans.steps.monetdbagilemart;
 
 import org.pentaho.di.core.Props;
