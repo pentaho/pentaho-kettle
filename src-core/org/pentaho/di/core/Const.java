36c7161c4f985a20af154336b9c8d38ae0b19a78
lgrill <lgrill@5fb7f6ec-07c1-534a-b4ca-9155e429e800>
2013-09-24 16:14:52 +0000

    Set const version to 4.4.2.1 for patch build.
    git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/tags/4.4.2.1@19120 5fb7f6ec-07c1-534a-b4ca-9155e429e800

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -72,7 +72,7 @@ public class Const
   /**
    * Version number
    */
-  public static final String VERSION = "4.4.2";
+  public static final String VERSION = "4.4.2.1";
   
   /**
    * Copyright year 
