3ca5753b12897dcd158135bcd1ccf1f606a8fe9c
Dzmitry_Prakapenka <dima.prokopenko@gmail.com>
2013-10-28 15:46:00 +0300

    [PDI-4325] Checkstyle passed

--- core/src/org/pentaho/di/cluster/HttpUtil.java
+++ core/src/org/pentaho/di/cluster/HttpUtil.java
@@ -1,5 +1,5 @@
 /*! ******************************************************************************
- *
+ * 
  * Pentaho Data Integration
  *
  * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
@@ -30,11 +30,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -54,7 +50,7 @@ import org.pentaho.di.core.variables.VariableSpace;
 public class HttpUtil {
 
   public static final int ZIP_BUFFER_SIZE = 8192;
-  
+
   public static String execService( VariableSpace space, String hostname, String port, String webAppName,
       String serviceAndArguments, String username, String password, String proxyHostname, String proxyPort,
       String nonProxyHosts ) throws Exception {
@@ -94,7 +90,7 @@ public class HttpUtil {
     }
 
   }
-  
+
   /**
    * Returns http GET request string using specified parameters.
    * 
@@ -170,7 +166,7 @@ public class HttpUtil {
    * @throws IOException
    */
   public static String decodeBase64ZippedString( String loggingString64 ) throws IOException {
-    if (loggingString64==null || loggingString64.isEmpty()){
+    if ( loggingString64 == null || loggingString64.isEmpty() ) {
       return "";
     }
     StringWriter writer = new StringWriter();
@@ -178,56 +174,56 @@ public class HttpUtil {
     byte[] bytes64 = Base64.decodeBase64( loggingString64.getBytes() );
     //unzip to string encoding-wise
     ByteArrayInputStream zip = new ByteArrayInputStream( bytes64 );
-    
+
     GZIPInputStream unzip = null;
     InputStreamReader reader = null;
     BufferedInputStream in = null;
     try {
       unzip = new GZIPInputStream( zip, HttpUtil.ZIP_BUFFER_SIZE );
-      in = new BufferedInputStream(unzip, HttpUtil.ZIP_BUFFER_SIZE );
+      in = new BufferedInputStream( unzip, HttpUtil.ZIP_BUFFER_SIZE );
       //PDI-4325 originally used xml encoding in servlet
-      reader = new InputStreamReader(in, Const.XML_ENCODING);
+      reader = new InputStreamReader( in, Const.XML_ENCODING );
       writer = new StringWriter();
-      
+
       //use same buffer size
       char[] buff = new char[HttpUtil.ZIP_BUFFER_SIZE];
-      for (int length = 0; (length = reader.read(buff)) > 0;) {
-          writer.write(buff, 0, length);
+      for ( int length = 0; ( length = reader.read( buff ) ) > 0; ) {
+        writer.write( buff, 0, length );
       }
     } finally {
       //close resources
-      if (reader!=null){
-        try{
+      if ( reader != null ) {
+        try {
           reader.close();
-        } catch (IOException e){
+        } catch ( IOException e ) {
           //Suppress
         }
       }
-      if (in!=null){
-        try{
+      if ( in != null ) {
+        try {
           in.close();
-        } catch (IOException e){
+        } catch ( IOException e ) {
           //Suppress
         }
       }
-      if (unzip!=null){
-        try{
+      if ( unzip != null ) {
+        try {
           unzip.close();
-        } catch (IOException e){
+        } catch ( IOException e ) {
           //Suppress
         }
       }
     }
     return writer.toString();
   }
-  
-  public static String encodeBase64ZippedString(String in) throws IOException {
-    Charset charset = Charset.forName(Const.XML_ENCODING);
+
+  public static String encodeBase64ZippedString( String in ) throws IOException {
+    Charset charset = Charset.forName( Const.XML_ENCODING );
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     GZIPOutputStream gzos = new GZIPOutputStream( baos );
     gzos.write( in.getBytes( charset ) );
     gzos.close();
-    
+
     return new String( Base64.encodeBase64( baos.toByteArray() ) );
-  }  
+  }
 }
--- core/test-src/org/pentaho/di/cluster/HttpUtilTest.java
+++ core/test-src/org/pentaho/di/cluster/HttpUtilTest.java
@@ -39,10 +39,9 @@ import org.junit.Test;
 public class HttpUtilTest {
 
   public static final String DEFAULT_ENCODING = "UTF-8";
-  public static final String STANDART = "(\u256e\u00b0-\u00b0)\u256e\u2533\u2501\u2501\u2533\u30c6\u30fc\u30d6"+
-      "\u30eb(\u256f\u00b0\u25a1\u00b0)\u256f\u253b\u2501\u2501\u253b\u30aa"+
-      "\u30d5";    
-  
+  public static final String STANDART = "(\u256e\u00b0-\u00b0)\u256e\u2533\u2501\u2501\u2533\u30c6\u30fc\u30d6"
+      + "\u30eb(\u256f\u00b0\u25a1\u00b0)\u256f\u253b\u2501\u2501\u253b\u30aa\u30d5";
+
   /**
    * [PDI-4325]
    * Test that we can decode/encode Strings without loss of data.
@@ -51,14 +50,14 @@ public class HttpUtilTest {
    * @throws NoSuchAlgorithmException 
    */
   @Test
-  public final void testDecodeBase64ZippedString() throws IOException, NoSuchAlgorithmException {  
+  public final void testDecodeBase64ZippedString() throws IOException, NoSuchAlgorithmException {
     String enc64 = this.canonicalBase64Encode( STANDART );
     //decode string
     String decoded = HttpUtil.decodeBase64ZippedString( enc64 );
 
     Assert.assertEquals( "Strings are the same after transformation", STANDART,  decoded );
   }
-  
+
   /**
    * Test that we can encode and decode String using 
    * only static class-under-test methods.
@@ -66,34 +65,35 @@ public class HttpUtilTest {
    * 
    * @throws IOException 
    */
-  public void testEncodeBase64ZippedString() throws IOException{
+  public void testEncodeBase64ZippedString() throws IOException {
     String enc64 = HttpUtil.encodeBase64ZippedString( STANDART );
     String decoded = HttpUtil.decodeBase64ZippedString( enc64 );
 
-    Assert.assertEquals( "Strings are the same after transformation", STANDART,  decoded );    
+    Assert.assertEquals( "Strings are the same after transformation", STANDART,  decoded );
   }
-  
+
   /**
-   * https://www.securecoding.cert.org/confluence/display/java/IDS12-J.+Perform+lossless+conversion+of+String+data+between+differing+character+encodings
+   * https://www.securecoding.cert.org/confluence/display/java/IDS12-J.+Perform+lossless+conversion+
+   * of+String+data+between+differing+character+encodings
    * 
    * @param in string to encode
    * @return
    * @throws IOException
    */
-  private String canonicalBase64Encode (String in) throws IOException{
-    Charset charset = Charset.forName(DEFAULT_ENCODING);
+  private String canonicalBase64Encode( String in ) throws IOException {
+    Charset charset = Charset.forName( DEFAULT_ENCODING );
     CharsetEncoder encoder = charset.newEncoder();
-    encoder.reset();   
-    ByteBuffer baosbf = encoder.encode(CharBuffer.wrap(in));
+    encoder.reset();
+    ByteBuffer baosbf = encoder.encode( CharBuffer.wrap( in ) );
     byte[] bytes = new byte[baosbf.limit()];
-    baosbf.get(bytes);
-    
+    baosbf.get( bytes );
+
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     GZIPOutputStream gzos = new GZIPOutputStream( baos );
     gzos.write( bytes );
     gzos.close();
     String encoded = new String( Base64.encodeBase64( baos.toByteArray() ) );
-    
-    return encoded; 
+
+    return encoded;
   }
 }
