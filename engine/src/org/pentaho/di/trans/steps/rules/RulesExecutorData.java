a281f57c6066b5a7c56bf65c10d3348b93f75bd3
Dzmitry_Bahdanovich <dzmitry_bahdanovich@epam.com>
2013-11-04 10:41:21 +0300

    [SP-502] Backport for PDI-10641
    * clean up cached results of execution when each row is passed to row executor
    * refactor the code to allow unit testing
    + add unit test

--- engine/src/org/pentaho/di/trans/steps/rules/RulesExecutorData.java
+++ engine/src/org/pentaho/di/trans/steps/rules/RulesExecutorData.java
@@ -1,8 +1,8 @@
-/*******************************************************************************
+/*! ******************************************************************************
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -47,11 +47,12 @@ import org.pentaho.di.trans.steps.rules.Rules.Column;
 
 /**
  * This Transformation Step allows a user to execute a rule set against an individual rule or a collection of rules.
- * <p/>
+ * 
  * Additional columns can be added to the output from the rules and these (of course) can be used for routing if
  * desired.
- *
+ * 
  * @author cboyden
+ * 
  */
 
 public class RulesExecutorData extends BaseStepData implements StepDataInterface {
@@ -127,7 +128,7 @@ public class RulesExecutorData extends BaseStepData implements StepDataInterface
     List<ValueMetaInterface> columns = inputRowMeta.getValueMetaList();
 
     // This array must 1-1 match the row[] feteched by getRow()
-    columnList = new Column[ columns.size() ];
+    columnList = new Column[columns.size()];
 
     for ( int i = 0; i < columns.size(); i++ ) {
       ValueMetaInterface column = columns.get( i );
@@ -137,24 +138,39 @@ public class RulesExecutorData extends BaseStepData implements StepDataInterface
       c.setType( column.getTypeDesc() );
       c.setPayload( null );
 
-      columnList[ i ] = c;
+      columnList[i] = c;
     }
   }
 
   public void loadRow( Object[] r ) {
     for ( int i = 0; i < columnList.length; i++ ) {
-      columnList[ i ].setPayload( r[ i ] );
+      columnList[i].setPayload( r[i] );
     }
+    resultMap.clear();
   }
 
   public void execute() {
+    StatefulKnowledgeSession session = initNewKnowledgeSession();
+
+    Collection<Object> oList = fetchColumns( session );
+    for ( Object o : oList ) {
+      resultMap.put( ( (Column) o ).getName(), (Column) o );
+    }
+
+    session.dispose();
+  }
+
+  protected StatefulKnowledgeSession initNewKnowledgeSession() {
     StatefulKnowledgeSession session = kbase.newStatefulKnowledgeSession();
     for ( int i = 0; i < columnList.length; i++ ) {
-      session.insert( columnList[ i ] );
+      session.insert( columnList[i] );
     }
 
     session.fireAllRules();
+    return session;
+  }
 
+  protected Collection<Object> fetchColumns( StatefulKnowledgeSession session ) {
     Collection<Object> oList = session.getObjects( new ObjectFilter() {
       @Override
       public boolean accept( Object o ) {
@@ -164,16 +180,13 @@ public class RulesExecutorData extends BaseStepData implements StepDataInterface
         return false;
       }
     } );
-
-    for ( Object o : oList ) {
-      resultMap.put( ( (Column) o ).getName(), (Column) o );
-    }
-
-    session.dispose();
+    return oList;
   }
 
   /**
-   * @param columnName Column.payload associated with the result, or null if not found
+   * 
+   * @param columnName
+   *          Column.payload associated with the result, or null if not found
    * @return
    */
   public Object fetchResult( String columnName ) {
