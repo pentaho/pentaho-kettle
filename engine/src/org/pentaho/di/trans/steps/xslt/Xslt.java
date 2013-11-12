80fd8bec74e78284375fea28a76fef6f1eb67e01
Bryan Rosander <brosander@pentaho.com>
2013-11-12 16:15:25 -0500

    PDI-6781 - Ensuring location is printed on Xslt error

--- engine/src/org/pentaho/di/trans/steps/xslt/Xslt.java
+++ engine/src/org/pentaho/di/trans/steps/xslt/Xslt.java
@@ -22,6 +22,7 @@
 
 package org.pentaho.di.trans.steps.xslt;
 
+import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Properties;
@@ -34,6 +35,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileType;
+import org.apache.xml.utils.DefaultErrorHandler;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleStepException;
@@ -260,20 +262,18 @@ public class Xslt extends BaseStep implements StepInterface {
       putRow( data.outputRowMeta, outputRowData ); // copy row to output rowset(s);
 
     } catch ( Exception e ) {
-
-      boolean sendToErrorRow = false;
-      String errorMessage = null;
+      String errorMessage = e.getMessage();
+      StringWriter sw = new StringWriter();
+      PrintWriter pw = new PrintWriter( sw );
+      DefaultErrorHandler.printLocation( pw, e );
+      pw.close();
+      errorMessage = sw.toString() + "\n" + errorMessage;
 
       if ( getStepMeta().isDoingErrorHandling() ) {
-        sendToErrorRow = true;
-        errorMessage = e.getMessage();
-      }
-
-      if ( sendToErrorRow ) {
         // Simply add this row to the error row
         putError( getInputRowMeta(), row, 1, errorMessage, meta.getResultfieldname(), "XSLT01" );
       } else {
-        logError( BaseMessages.getString( PKG, "Xslt.ErrorProcesing" + " : " + e.getMessage() ) );
+        logError( BaseMessages.getString( PKG, "Xslt.ErrorProcesing" + " : " + errorMessage ) );
         throw new KettleStepException( BaseMessages.getString( PKG, "Xslt.ErrorProcesing" ), e );
       }
     }
