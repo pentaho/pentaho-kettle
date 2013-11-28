d1228dc79a13bfc0aa48c90c99700cd4856fe785
Dzmitry Stsiapanau <stepanovdg@gmail.com>
2013-11-28 19:56:45 +0300

    [PDI-6923] The generated YAML file for Greenplum Loader is not correct if database schema is specified

--- test/org/pentaho/di/AllRegressionTests.java
+++ test/org/pentaho/di/AllRegressionTests.java
@@ -1,27 +1,28 @@
 /*! ******************************************************************************
-*
-* Pentaho Data Integration
-*
-* Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
-*
-*******************************************************************************
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-******************************************************************************/
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 package org.pentaho.di;
 
+import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.pentaho.di.compatibility.ValueBigNumberTest;
@@ -53,6 +54,7 @@ import org.pentaho.di.trans.steps.csvinput.CsvInput1Test;
 import org.pentaho.di.trans.steps.csvinput.CsvInput2Test;
 import org.pentaho.di.trans.steps.detectlastrow.DetectLastRowStepTest;
 import org.pentaho.di.trans.steps.getxmldata.GetXMLDataTest;
+import org.pentaho.di.trans.steps.gpload.GPLoadTest;
 import org.pentaho.di.trans.steps.injector.InjectorTest;
 import org.pentaho.di.trans.steps.nullif.NullIfTest;
 import org.pentaho.di.trans.steps.regexeval.RegexEvalTest;
@@ -65,74 +67,72 @@ import org.pentaho.di.trans.steps.tableoutput.TableOutputTest;
 import org.pentaho.di.trans.steps.valuemapper.ValueMapperTest;
 import org.pentaho.di.ui.dialog.TipsDialogTest;
 
-
 /**
  * Regression tests for the PDI framework.
  * 
  * @author sboden
  */
-public class AllRegressionTests
-{   
-    public static Test suite() throws Exception
-    {
-        TestSuite suite = new TestSuite("Run regression tests");
+public class AllRegressionTests {
+  public static Test suite() throws Exception {
+    TestSuite suite = new TestSuite( "Run regression tests" );
 
-        // The testcases should be executed from easy to hard. It
-        // actually defines the debugging sequence if ever required.
-        // If some of the suites fail you should start checking/debugging
-        // the suites from the first that failed onwards.
-        //
-        // So adding testcases in the right order is important.
-        //
-        
-        suite.addTestSuite(ValueStringTest.class);
-        suite.addTestSuite(ValueIntegerTest.class);
-        suite.addTestSuite(ValueNumberTest.class);
-        suite.addTestSuite(ValueBigNumberTest.class);
-        suite.addTestSuite(ValueDateTest.class);
-        suite.addTestSuite(ValueBooleanTest.class);
-        suite.addTestSuite(ValueTest.class);
-        suite.addTestSuite(ConstTest.class);
-        suite.addTestSuite(StringUtilTest.class);
-        suite.addTestSuite(EncrTest.class);
-        suite.addTestSuite(ParameterSimpleTransTest.class);
-        
-        suite.addTestSuite(ValueMetaTest.class);
-        suite.addTestSuite(RowDataUtilTest.class);
-        suite.addTestSuite(ValueDataUtilTest.class);
-        suite.addTestSuite(DatabaseTest.class);
-        suite.addTestSuite(RowTest.class);
-        suite.addTestSuite(RowSetTest.class);
-        suite.addTestSuite(HopTest.class);        
-        suite.addTestSuite(CounterTest.class);
-        suite.addTestSuite(CountersTest.class);
-        
-        suite.addTestSuite(InjectorTest.class);
-        suite.addTestSuite(RowGeneratorTest.class);
-        suite.addTestSuite(ConstantTest.class);
-        suite.addTestSuite(AppendTest.class);   
-        suite.addTestSuite(DetectLastRowStepTest.class);        
-        suite.addTestSuite(BlockingStepTest.class);
-        suite.addTestSuite(SortRowsTest.class);
-        suite.addTestSuite(ValueMapperTest.class);
-        suite.addTestSuite(NullIfTest.class);
-        suite.addTestSuite(RegexEvalTest.class);
-        suite.addTestSuite(AddSequenceTest.class);  
-        suite.addTestSuite(TableInputTest.class);        
-        suite.addTestSuite(TableOutputTest.class);
-//        suite.addTestSuite(DatabaseLookupTest.class);    Now a JUnit 4 testcase
-        suite.addTestSuite(CombinationLookupTest.class);
-        suite.addTestSuite(JavaScriptStringTest.class);
-        suite.addTestSuite(JavaScriptSpecialTest.class);
-        suite.addTestSuite(GetXMLDataTest.class);
-        suite.addTestSuite(CsvInput1Test.class);
-        suite.addTestSuite(CsvInput2Test.class);
-        suite.addTestSuite(TipsDialogTest.class);
+    // The testcases should be executed from easy to hard. It
+    // actually defines the debugging sequence if ever required.
+    // If some of the suites fail you should start checking/debugging
+    // the suites from the first that failed onwards.
+    //
+    // So adding testcases in the right order is important.
+    //
 
-        // Temporarily disable this test, it never worked on Windows or Unix so
-        // it doesn't make sense executing it for the moment.
-        // suite.addTestSuite( BlackBoxTests.class );
-        
-        return suite;
-    }
+    suite.addTestSuite( ValueStringTest.class );
+    suite.addTestSuite( ValueIntegerTest.class );
+    suite.addTestSuite( ValueNumberTest.class );
+    suite.addTestSuite( ValueBigNumberTest.class );
+    suite.addTestSuite( ValueDateTest.class );
+    suite.addTestSuite( ValueBooleanTest.class );
+    suite.addTestSuite( ValueTest.class );
+    suite.addTestSuite( ConstTest.class );
+    suite.addTestSuite( StringUtilTest.class );
+    suite.addTestSuite( EncrTest.class );
+    suite.addTestSuite( ParameterSimpleTransTest.class );
+
+    suite.addTestSuite( ValueMetaTest.class );
+    suite.addTestSuite( RowDataUtilTest.class );
+    suite.addTestSuite( ValueDataUtilTest.class );
+    suite.addTestSuite( DatabaseTest.class );
+    suite.addTestSuite( RowTest.class );
+    suite.addTestSuite( RowSetTest.class );
+    suite.addTestSuite( HopTest.class );
+    suite.addTestSuite( CounterTest.class );
+    suite.addTestSuite( CountersTest.class );
+
+    suite.addTestSuite( InjectorTest.class );
+    suite.addTestSuite( RowGeneratorTest.class );
+    suite.addTestSuite( ConstantTest.class );
+    suite.addTestSuite( AppendTest.class );
+    suite.addTestSuite( DetectLastRowStepTest.class );
+    suite.addTestSuite( BlockingStepTest.class );
+    suite.addTestSuite( SortRowsTest.class );
+    suite.addTestSuite( ValueMapperTest.class );
+    suite.addTestSuite( NullIfTest.class );
+    suite.addTestSuite( RegexEvalTest.class );
+    suite.addTestSuite( AddSequenceTest.class );
+    suite.addTestSuite( TableInputTest.class );
+    suite.addTestSuite( TableOutputTest.class );
+    // suite.addTestSuite(DatabaseLookupTest.class); Now a JUnit 4 testcase
+    suite.addTestSuite( CombinationLookupTest.class );
+    suite.addTestSuite( JavaScriptStringTest.class );
+    suite.addTestSuite( JavaScriptSpecialTest.class );
+    suite.addTestSuite( GetXMLDataTest.class );
+    suite.addTestSuite( CsvInput1Test.class );
+    suite.addTestSuite( CsvInput2Test.class );
+    suite.addTest( new JUnit4TestAdapter( GPLoadTest.class ) );
+    suite.addTest( new JUnit4TestAdapter( TipsDialogTest.class ) );
+
+    // Temporarily disable this test, it never worked on Windows or Unix so
+    // it doesn't make sense executing it for the moment.
+    // suite.addTestSuite( BlackBoxTests.class );
+
+    return suite;
+  }
 }
--- test/org/pentaho/di/ui/dialog/TipsDialogTest.java
+++ test/org/pentaho/di/ui/dialog/TipsDialogTest.java
@@ -1,6 +1,5 @@
 package org.pentaho.di.ui.dialog;
 
-import junit.framework.TestCase;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.junit.After;
@@ -10,61 +9,66 @@ import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.ui.core.PropsUI;
 import org.pentaho.di.ui.spoon.dialog.TipsDialog;
 
+import static org.junit.Assert.fail;
+
 /**
- * User: Dzmitry Stsiapanau
- * Date: 10/10/13
- * Time: 3:14 PM
+ * User: Dzmitry Stsiapanau Date: 10/10/13 Time: 3:14 PM
  */
-public class TipsDialogTest extends TestCase {
-    private TipsDialog tipsDialog;
-      private Boolean showTips;
+public class TipsDialogTest {
+  private TipsDialog tipsDialog;
+  private Boolean showTips;
+  private String prevUserDir;
 
-      @Before
-      public void setUp() throws Exception {
-        KettleEnvironment.init();
+  @Before
+  public void setUp() throws Exception {
+    prevUserDir = System.getProperty( "user.dir" );
+    String separator = System.getProperty( "file.separator" );
+    System.setProperty( "user.dir", prevUserDir + separator + "assembly" + separator + "package-res" );
+    KettleEnvironment.init();
 
-        Display display = Display.getDefault();
-        if ( !PropsUI.isInitialized() ) {
-          PropsUI.init( display, 1 );
-          showTips = PropsUI.getInstance().showTips();
+    Display display = Display.getDefault();
+    if ( !PropsUI.isInitialized() ) {
+      PropsUI.init( display, 1 );
+      showTips = PropsUI.getInstance().showTips();
+    }
+    PropsUI.getInstance().setShowTips( true );
+
+    display = PropsUI.getDisplay();
+    Shell rootShell = new Shell( display );
+
+    tipsDialog = new TipsDialog( rootShell );
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    PropsUI.getInstance().setShowTips( showTips );
+    System.setProperty( "user.dir", prevUserDir );
+  }
+
+  @Test
+  public void testOpen() throws Exception {
+    try {
+      Display.getDefault().asyncExec( new Runnable() {
+        @Override
+        public void run() {
+          tipsDialog.open();
         }
-        PropsUI.getInstance().setShowTips( true );
+      } );
 
-        display = PropsUI.getDisplay();
-        Shell rootShell = new Shell( display );
-
-        tipsDialog = new TipsDialog( rootShell );
-
-      }
-
-      @After
-      public void tearDown() throws Exception {
-        PropsUI.getInstance().setShowTips( showTips );
-      }
-
-      @Test
-      public void testOpen() throws Exception {
-        try {
-          Display.getDefault().asyncExec( new Runnable() {
-            @Override
-            public void run() {
-              tipsDialog.open();
-            }
-          } );
-
-        } catch ( Exception e ) {
-          fail( "Test failed due to exception: " + e.getLocalizedMessage() );
+    } catch ( Exception e ) {
+      fail( "Test failed due to exception: " + e.getLocalizedMessage() );
+    }
+    try {
+      Display.getDefault().asyncExec( new Runnable() {
+        @Override
+        public void run() {
+          tipsDialog.dispose();
         }
-        try {
-          Display.getDefault().asyncExec( new Runnable() {
-            @Override
-            public void run() {
-              tipsDialog.dispose();
-            }
-          } );
+      } );
 
-        } catch ( Exception e ) {
-          fail( "Test failed due to exception: " + e.getLocalizedMessage() );
-        }
-      }
+    } catch ( Exception e ) {
+      fail( "Test failed due to exception: " + e.getLocalizedMessage() );
+    }
+  }
 }
