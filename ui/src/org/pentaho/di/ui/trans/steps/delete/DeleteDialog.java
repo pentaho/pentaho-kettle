21081283d81faa1d931562ec8f2932485a9960a9
Kanstantsin Karneliuk <Kanstantsin_Karneliuk@epam.com>
2013-12-04 15:25:13 +0300

    PDI-10828 - Variables in Commit Size for Update, Insert/Update and Delete Steps are not accepted
    UI changes to mark fields that they support variables

--- ui/src/org/pentaho/di/ui/trans/steps/delete/DeleteDialog.java
+++ ui/src/org/pentaho/di/ui/trans/steps/delete/DeleteDialog.java
@@ -95,7 +95,7 @@ public class DeleteDialog extends BaseStepDialog implements StepDialogInterface 
   private FormData fdlTable, fdbTable, fdTable;
 
   private Label wlCommit;
-  private Text wCommit;
+  private TextVar wCommit;
   private FormData fdlCommit, fdCommit;
 
   private DeleteMeta input;
@@ -242,7 +242,7 @@ public class DeleteDialog extends BaseStepDialog implements StepDialogInterface 
     fdlCommit.top = new FormAttachment( wTable, margin );
     fdlCommit.right = new FormAttachment( middle, -margin );
     wlCommit.setLayoutData( fdlCommit );
-    wCommit = new Text( shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER );
+    wCommit = new TextVar(transMeta, shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER );
     props.setLook( wCommit );
     wCommit.addModifyListener( lsMod );
     fdCommit = new FormData();
