5f4e9786395ff36eee2dab4e64e21de5f92dfce0
Kanstantsin Karneliuk <Kanstantsin_Karneliuk@epam.com>
2013-12-05 12:50:58 +0300

    SP-326 - Backport of PDI-9908
    Adding copyright + javadoc

--- src-core/org/pentaho/di/core/util/FileUtil.java
+++ src-core/org/pentaho/di/core/util/FileUtil.java
@@ -1,3 +1,26 @@
+/*******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+
 package org.pentaho.di.core.util;
 
 import org.apache.commons.vfs.FileObject;
@@ -7,6 +30,16 @@ import org.pentaho.di.core.vfs.KettleVFS;
 import org.pentaho.di.i18n.BaseMessages;
 
 public class FileUtil {
+  
+  /**
+   * Utility method to create parent folder(s) for specified file location
+   * @param PKG -- class to be used in logging
+   * @param filename -- filename of the file, parent folder is required for
+   * @param createParentFolder -- flag indicating if we need to create parent folder
+   * @param log -- logging interface to be used
+   * @param vs -- variable space containing variables that might appear in filename 
+   * @return true -- if folder was created, false -- otherwise
+   */
   public static boolean createParentFolder( Class<?> PKG, String filename, boolean createParentFolder,
       LogChannelInterface log, VariableSpace vs ) {
     // Check for parent folder
