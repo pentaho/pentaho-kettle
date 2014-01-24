4575e9fc2616723b75cf85f8e8fbd6a92435d48c
Dzmitry <dima.prokopenko@gmail.com>
2014-01-24 15:40:19 +0300

    PDI-7255 - The table compare step reports incorrect record as missing
    junit integration tests added

---
+++ testfiles/blackbox/tests/trans/steps/tablecompare/reference_only.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS reference;
+CREATE TABLE reference (
+  key1 int,
+  key2 int,
+  value varchar(20),
+  PRIMARY KEY (key1)
+);
+INSERT INTO reference VALUES ('1', '2', 'a');
+INSERT INTO reference VALUES ('2', '2', 'b');
+INSERT INTO reference VALUES ('3', '3', 'c');
+INSERT INTO reference VALUES ('4', '4', 'd');
+
+DROP TABLE IF EXISTS compare;
+CREATE TABLE compare (
+  key1 int,
+  key2 int,
+  value varchar(20),
+  PRIMARY KEY (key1)
+);
