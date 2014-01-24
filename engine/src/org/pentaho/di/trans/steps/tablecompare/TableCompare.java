182064f23c7f6de05cdeedb94f3f2f5d0c60f6d0
Dzmitry <dima.prokopenko@gmail.com>
2014-01-24 15:41:32 +0300

    PDI-7255 - The table compare step reports incorrect record as missing
    fix provided

--- engine/src/org/pentaho/di/trans/steps/tablecompare/TableCompare.java
+++ engine/src/org/pentaho/di/trans/steps/tablecompare/TableCompare.java
@@ -442,7 +442,7 @@ public class TableCompare extends BaseStep implements StepInterface {
 
             one = data.referenceDb.getRow( refSet );
             if ( one != null ) {
-              nrRecordsCompare++;
+              nrRecordsReference++;
             }
           } else {
             if ( one != null && two != null ) {
@@ -496,11 +496,11 @@ public class TableCompare extends BaseStep implements StepInterface {
               } else {
                 if ( compare < 0 ) {
                   if ( getStepMeta().isDoingErrorHandling() ) {
-                    String keyDesc = getKeyDesc( keyRowMeta, keyNrs, two );
+                    String keyDesc = getKeyDesc( keyRowMeta, keyNrs, one );
                     Object[] errorRowData = constructErrorRow( rowMeta, r, keyDesc, null, null );
                     putError( data.errorRowMeta, errorRowData, 1, BaseMessages.getString(
                       PKG, "TableCompare.Error.RecordNotInReferenceFoundInCompareTable", cmpSchemaTable,
-                      keyRowMeta.getString( two ) ), null, "TAC004" );
+                      keyRowMeta.getString( one ) ), null, "TAC004" );
                   }
                   nrErrors++;
                   nrRightErrors++;
@@ -511,11 +511,11 @@ public class TableCompare extends BaseStep implements StepInterface {
                   }
                 } else {
                   if ( getStepMeta().isDoingErrorHandling() ) {
-                    String keyDesc = getKeyDesc( keyRowMeta, keyNrs, one );
+                    String keyDesc = getKeyDesc( keyRowMeta, keyNrs, two );
                     Object[] errorRowData = constructErrorRow( rowMeta, r, keyDesc, null, null );
                     putError( data.errorRowMeta, errorRowData, 1, BaseMessages.getString(
                       PKG, "TableCompare.Error.RecordInReferenceNotFoundInCompareTable", refSchemaTable,
-                      keyRowMeta.getString( one ) ), null, "TAC005" );
+                      keyRowMeta.getString( two ) ), null, "TAC005" );
                   }
                   nrErrors++;
                   nrLeftErrors++;
