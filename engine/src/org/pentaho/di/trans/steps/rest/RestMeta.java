718ba5f7d40ce9aa009d9c2c33cb2bbfd8492ea6
Bryan Rosander <brosander@pentaho.com>
2014-02-21 11:56:27 -0500

    [PDI-11467] - Obfuscating trust store password

--- engine/src/org/pentaho/di/trans/steps/rest/RestMeta.java
+++ engine/src/org/pentaho/di/trans/steps/rest/RestMeta.java
@@ -415,7 +415,10 @@ public class RestMeta extends BaseStepMeta implements StepMetaInterface {
     retval.append( "    " + XMLHandler.addTagValue( "preemptive", preemptive ) );
 
     retval.append( "    " + XMLHandler.addTagValue( "trustStoreFile", trustStoreFile ) );
-    retval.append( "    " + XMLHandler.addTagValue( "trustStorePassword", trustStorePassword ) );
+    retval
+        .append( "    "
+            + XMLHandler.addTagValue( "trustStorePassword", Encr
+                .encryptPasswordIfNotUsingVariables( trustStorePassword ) ) );
 
     retval.append( "    <headers>" + Const.CR );
     for ( int i = 0; i < headerName.length; i++ ) {
@@ -463,7 +466,8 @@ public class RestMeta extends BaseStepMeta implements StepMetaInterface {
       preemptive = "Y".equalsIgnoreCase( XMLHandler.getTagValue( stepnode, "preemptive" ) );
 
       trustStoreFile = XMLHandler.getTagValue( stepnode, "trustStoreFile" );
-      trustStorePassword = XMLHandler.getTagValue( stepnode, "trustStorePassword" );
+      trustStorePassword =
+          Encr.decryptPasswordOptionallyEncrypted( XMLHandler.getTagValue( stepnode, "trustStorePassword" ) );
 
       Node headernode = XMLHandler.getSubNode( stepnode, "headers" );
       int nrheaders = XMLHandler.countNodes( headernode, "header" );
@@ -509,7 +513,8 @@ public class RestMeta extends BaseStepMeta implements StepMetaInterface {
       proxyPort = rep.getStepAttributeString( id_step, "proxyPort" );
 
       trustStoreFile = rep.getStepAttributeString( id_step, "trustStoreFile" );
-      trustStorePassword = rep.getStepAttributeString( id_step, "trustStorePassword" );
+      trustStorePassword =
+          Encr.decryptPasswordOptionallyEncrypted( rep.getStepAttributeString( id_step, "trustStorePassword" ) );
 
       preemptive = rep.getStepAttributeBoolean( id_step, "preemptive" );
       int nrheaders = rep.countNrStepAttributes( id_step, "header_field" );
@@ -553,7 +558,8 @@ public class RestMeta extends BaseStepMeta implements StepMetaInterface {
       rep.saveStepAttribute( id_transformation, id_step, "proxyPort", proxyPort );
 
       rep.saveStepAttribute( id_transformation, id_step, "trustStoreFile", trustStoreFile );
-      rep.saveStepAttribute( id_transformation, id_step, "trustStorePassword", trustStorePassword );
+      rep.saveStepAttribute( id_transformation, id_step, "trustStorePassword", Encr
+          .encryptPasswordIfNotUsingVariables( trustStorePassword ) );
 
       rep.saveStepAttribute( id_transformation, id_step, "preemptive", preemptive );
       for ( int i = 0; i < headerName.length; i++ ) {
