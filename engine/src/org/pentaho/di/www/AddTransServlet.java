9816d3ad667714eb94af9b4e8de56ee0821b85a2
Pavel Sakun <pavel_sakun@epam.com>
2014-02-24 17:28:44 +0300

    SP-792 - Reformat

--- engine/src/org/pentaho/di/www/AddTransServlet.java
+++ engine/src/org/pentaho/di/www/AddTransServlet.java
@@ -1,8 +1,8 @@
-/*******************************************************************************
+/*! ******************************************************************************
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2014 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -51,121 +51,124 @@ public class AddTransServlet extends BaseHttpServlet implements CartePluginInter
   public AddTransServlet() {
   }
 
-  public AddTransServlet(TransformationMap transformationMap, SocketRepository socketRepository) {
-    super(transformationMap, socketRepository);
+  public AddTransServlet( TransformationMap transformationMap, SocketRepository socketRepository ) {
+    super( transformationMap, socketRepository );
   }
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    if (isJettyMode() && !request.getRequestURI().startsWith(CONTEXT_PATH))
+  public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+    if ( isJettyMode() && !request.getRequestURI().startsWith( CONTEXT_PATH ) ) {
       return;
+    }
 
-    if (log.isDebug())
-      logDebug("Addition of transformation requested");
+    if ( log.isDebug() ) {
+      logDebug( "Addition of transformation requested" );
+    }
 
-    boolean useXML = "Y".equalsIgnoreCase(request.getParameter("xml"));
+    boolean useXML = "Y".equalsIgnoreCase( request.getParameter( "xml" ) );
 
     PrintWriter out = response.getWriter();
     BufferedReader in = request.getReader();
-    if (log.isDetailed())
-      logDetailed("Encoding: " + request.getCharacterEncoding());
-
-    if (useXML) {
-      response.setContentType("text/xml");
-      out.print(XMLHandler.getXMLHeader());
-    } else {
-      response.setContentType("text/html");
-      out.println("<HTML>");
-      out.println("<HEAD><TITLE>Add transformation</TITLE></HEAD>");
-      out.println("<BODY>");
+    if ( log.isDetailed() ) {
+      logDetailed( "Encoding: " + request.getCharacterEncoding() );
     }
 
-    response.setStatus(HttpServletResponse.SC_OK);
+    if ( useXML ) {
+      response.setContentType( "text/xml" );
+      out.print( XMLHandler.getXMLHeader() );
+    } else {
+      response.setContentType( "text/html" );
+      out.println( "<HTML>" );
+      out.println( "<HEAD><TITLE>Add transformation</TITLE></HEAD>" );
+      out.println( "<BODY>" );
+    }
+
+    response.setStatus( HttpServletResponse.SC_OK );
 
     try {
       // First read the complete transformation in memory from the request
       //
-      StringBuilder xml = new StringBuilder(request.getContentLength());
+      StringBuilder xml = new StringBuilder( request.getContentLength() );
       int c;
-      while ((c = in.read()) != -1) {
-        xml.append((char) c);
+      while ( ( c = in.read() ) != -1 ) {
+        xml.append( (char) c );
       }
 
       // Parse the XML, create a transformation configuration
       //
-      TransConfiguration transConfiguration = TransConfiguration.fromXML(xml.toString());
+      TransConfiguration transConfiguration = TransConfiguration.fromXML( xml.toString() );
       TransMeta transMeta = transConfiguration.getTransMeta();
       TransExecutionConfiguration transExecutionConfiguration = transConfiguration.getTransExecutionConfiguration();
-      transMeta.setLogLevel(transExecutionConfiguration.getLogLevel());
-      if (log.isDetailed()) {
-        logDetailed("Logging level set to " + log.getLogLevel().getDescription());
+      transMeta.setLogLevel( transExecutionConfiguration.getLogLevel() );
+      if ( log.isDetailed() ) {
+        logDetailed( "Logging level set to " + log.getLogLevel().getDescription() );
       }
-      transMeta.injectVariables(transExecutionConfiguration.getVariables());
+      transMeta.injectVariables( transExecutionConfiguration.getVariables() );
 
       // Also copy the parameters over...
       //
       Map<String, String> params = transExecutionConfiguration.getParams();
-      for (String param : params.keySet()) {
-        String value = params.get(param);
-        transMeta.setParameterValue(param, value);
+      for ( String param : params.keySet() ) {
+        String value = params.get( param );
+        transMeta.setParameterValue( param, value );
       }
 
       // If there was a repository, we know about it at this point in time.
       //
       TransExecutionConfiguration executionConfiguration = transConfiguration.getTransExecutionConfiguration();
       final Repository repository = transConfiguration.getTransExecutionConfiguration().getRepository();
-      
+
       String carteObjectId = UUID.randomUUID().toString();
-      SimpleLoggingObject servletLoggingObject = new SimpleLoggingObject(CONTEXT_PATH, LoggingObjectType.CARTE, null);
-      servletLoggingObject.setContainerObjectId(carteObjectId);
-      servletLoggingObject.setLogLevel(executionConfiguration.getLogLevel());
+      SimpleLoggingObject servletLoggingObject = new SimpleLoggingObject( CONTEXT_PATH, LoggingObjectType.CARTE, null );
+      servletLoggingObject.setContainerObjectId( carteObjectId );
+      servletLoggingObject.setLogLevel( executionConfiguration.getLogLevel() );
 
       // Create the transformation and store in the list...
       //
-      final Trans trans = new Trans(transMeta, servletLoggingObject);
-      
-      trans.setRepository(repository);
-      trans.setSocketRepository(getSocketRepository());
+      final Trans trans = new Trans( transMeta, servletLoggingObject );
 
-      getTransformationMap().addTransformation(transMeta.getName(), carteObjectId, trans, transConfiguration);
-      trans.setContainerObjectId(carteObjectId);
+      trans.setRepository( repository );
+      trans.setSocketRepository( getSocketRepository() );
 
-      if (repository != null) {
+      getTransformationMap().addTransformation( transMeta.getName(), carteObjectId, trans, transConfiguration );
+      trans.setContainerObjectId( carteObjectId );
+
+      if ( repository != null ) {
         // The repository connection is open: make sure we disconnect from the repository once we
         // are done with this transformation.
         //
-        trans.addTransListener(new TransAdapter() {
-          public void transFinished(Trans trans) {
+        trans.addTransListener( new TransAdapter() {
+          public void transFinished( Trans trans ) {
             repository.disconnect();
           }
-        });
+        } );
       }
 
-      String message = "Transformation '" + trans.getName() + "' was added to Carte with id "+carteObjectId;
+      String message = "Transformation '" + trans.getName() + "' was added to Carte with id " + carteObjectId;
 
-      if (useXML) {
+      if ( useXML ) {
         // Return the log channel id as well
         //
-        out.println(new WebResult(WebResult.STRING_OK, message, carteObjectId));
+        out.println( new WebResult( WebResult.STRING_OK, message, carteObjectId ) );
       } else {
-        out.println("<H1>" + message + "</H1>");
-        out.println("<p><a href=\"" + convertContextPath(GetTransStatusServlet.CONTEXT_PATH) + "?name=" + trans.getName()
-            + "&id="+carteObjectId+"\">Go to the transformation status page</a><p>");
+        out.println( "<H1>" + message + "</H1>" );
+        out.println( "<p><a href=\"" + convertContextPath( GetTransStatusServlet.CONTEXT_PATH ) + "?name="
+            + trans.getName() + "&id=" + carteObjectId + "\">Go to the transformation status page</a><p>" );
       }
-    } catch (Exception ex) {
-      if (useXML) {
-        out.println(new WebResult(WebResult.STRING_ERROR, Const.getStackTracker(ex)));
+    } catch ( Exception ex ) {
+      if ( useXML ) {
+        out.println( new WebResult( WebResult.STRING_ERROR, Const.getStackTracker( ex ) ) );
       } else {
-        out.println("<p>");
-        out.println("<pre>");
-        ex.printStackTrace(out);
-        out.println("</pre>");
+        out.println( "<p>" );
+        out.println( "<pre>" );
+        ex.printStackTrace( out );
+        out.println( "</pre>" );
       }
     }
 
-    if (!useXML) {
-      out.println("<p>");
-      out.println("</BODY>");
-      out.println("</HTML>");
+    if ( !useXML ) {
+      out.println( "<p>" );
+      out.println( "</BODY>" );
+      out.println( "</HTML>" );
     }
   }
 
@@ -176,7 +179,7 @@ public class AddTransServlet extends BaseHttpServlet implements CartePluginInter
   public String getService() {
     return CONTEXT_PATH + " (" + toString() + ")";
   }
-  
+
   public String getContextPath() {
     return CONTEXT_PATH;
   }
