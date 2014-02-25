8fa44ed3fa8d380c315ad83fc8f97577ba9f445c
Dzmitry <dima.prokopenko@gmail.com>
2014-02-25 18:30:54 +0300

    SP-797 Backport of PDI-4325 - Spoon carte status is not showing in Japanese when Japanese is selected (4.8)
    fix backported

--- src/org/pentaho/di/www/GetJobStatusServlet.java
+++ src/org/pentaho/di/www/GetJobStatusServlet.java
@@ -22,19 +22,17 @@
 
 package org.pentaho.di.www;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
-import java.util.zip.GZIPOutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.binary.Base64;
 import org.owasp.esapi.ESAPI;
 import org.owasp.esapi.Encoder;
+import org.pentaho.di.cluster.HttpUtil;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.logging.CentralLogStore;
@@ -51,30 +49,33 @@ public class GetJobStatusServlet extends BaseHttpServlet implements CarteServlet
   public GetJobStatusServlet() {
   }
 
-  public GetJobStatusServlet(JobMap jobMap) {
-    super(jobMap);
+  public GetJobStatusServlet( JobMap jobMap ) {
+    super( jobMap );
   }
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    if (isJettyMode() && !request.getContextPath().startsWith(CONTEXT_PATH)) {
+  @Override
+  protected void doGet( HttpServletRequest request, HttpServletResponse response )
+    throws ServletException, IOException {
+    if ( isJettyMode() && !request.getContextPath().startsWith( CONTEXT_PATH ) ) {
       return;
     }
 
-    if (log.isDebug())
-      logDebug(BaseMessages.getString(PKG, "GetJobStatusServlet.Log.JobStatusRequested"));
+    if ( log.isDebug() ) {
+      logDebug( BaseMessages.getString( PKG, "GetJobStatusServlet.Log.JobStatusRequested" ) );
+    }
 
-    String jobName = request.getParameter("name");
-    String id = request.getParameter("id");
-    boolean useXML = "Y".equalsIgnoreCase(request.getParameter("xml"));
-    int startLineNr = Const.toInt(request.getParameter("from"), 0);
+    String jobName = request.getParameter( "name" );
+    String id = request.getParameter( "id" );
+    boolean useXML = "Y".equalsIgnoreCase( request.getParameter( "xml" ) );
+    int startLineNr = Const.toInt( request.getParameter( "from" ), 0 );
 
-    response.setStatus(HttpServletResponse.SC_OK);
+    response.setStatus( HttpServletResponse.SC_OK );
 
-    if (useXML) {
-      response.setContentType("text/xml");
-      response.setCharacterEncoding(Const.XML_ENCODING);
+    if ( useXML ) {
+      response.setContentType( "text/xml" );
+      response.setCharacterEncoding( Const.XML_ENCODING );
     } else {
-      response.setContentType("text/html;charset=UTF-8");
+      response.setContentType( "text/html;charset=UTF-8" );
     }
 
     PrintWriter out = response.getWriter();
@@ -83,131 +84,136 @@ public class GetJobStatusServlet extends BaseHttpServlet implements CarteServlet
     //
     Job job;
     CarteObjectEntry entry;
-    if (Const.isEmpty(id)) {
-    	// get the first job that matches...
-    	//
-    	entry = getJobMap().getFirstCarteObjectEntry(jobName);
-    	if (entry==null) {
-    		job = null;
-    	} else {
-    		id = entry.getId();
-    		job = getJobMap().getJob(entry);
-    	}
+    if ( Const.isEmpty( id ) ) {
+      // get the first job that matches...
+      //
+      entry = getJobMap().getFirstCarteObjectEntry( jobName );
+      if ( entry == null ) {
+        job = null;
+      } else {
+        id = entry.getId();
+        job = getJobMap().getJob( entry );
+      }
     } else {
-    	// Take the ID into account!
-    	//
-    	entry = new CarteObjectEntry(jobName, id);
-    	job = getJobMap().getJob(entry);
+      // Take the ID into account!
+      //
+      entry = new CarteObjectEntry( jobName, id );
+      job = getJobMap().getJob( entry );
     }
-    
+
     Encoder encoder = ESAPI.encoder();
-    
-    if (job != null) {
+
+    if ( job != null ) {
       String status = job.getStatus();
       int lastLineNr = CentralLogStore.getLastBufferLineNr();
-      String logText = CentralLogStore.getAppender().getBuffer(job.getLogChannel().getLogChannelId(), false, startLineNr, lastLineNr).toString();
+      String logText =
+          CentralLogStore.getAppender().getBuffer( job.getLogChannel().getLogChannelId(), false, startLineNr,
+              lastLineNr ).toString();
 
-      if (useXML) {
-        response.setContentType("text/xml");
-        response.setCharacterEncoding(Const.XML_ENCODING);
-        out.print(XMLHandler.getXMLHeader(Const.XML_ENCODING));
+      if ( useXML ) {
+        response.setContentType( "text/xml" );
+        response.setCharacterEncoding( Const.XML_ENCODING );
+        out.print( XMLHandler.getXMLHeader( Const.XML_ENCODING ) );
 
-        SlaveServerJobStatus jobStatus = new SlaveServerJobStatus(jobName, id, status);
-        jobStatus.setFirstLoggingLineNr(startLineNr);
-        jobStatus.setLastLoggingLineNr(lastLineNr);
+        SlaveServerJobStatus jobStatus = new SlaveServerJobStatus( jobName, id, status );
+        jobStatus.setFirstLoggingLineNr( startLineNr );
+        jobStatus.setLastLoggingLineNr( lastLineNr );
 
         // The log can be quite large at times, we are going to put a base64 encoding around a compressed stream
         // of bytes to handle this one.
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        GZIPOutputStream gzos = new GZIPOutputStream(baos);
-        gzos.write(logText.getBytes());
-        gzos.close();
-
-        String loggingString = new String(Base64.encodeBase64(baos.toByteArray()));
-        jobStatus.setLoggingString(loggingString);
+        String loggingString = HttpUtil.encodeBase64ZippedString( logText );
+        jobStatus.setLoggingString( loggingString );
 
         // Also set the result object...
         //
-        jobStatus.setResult(job.getResult()); // might be null
+        jobStatus.setResult( job.getResult() ); // might be null
 
         try {
-          out.println(jobStatus.getXML());
-        } catch (KettleException e) {
-          throw new ServletException("Unable to get the job status in XML format", e);
+          out.println( jobStatus.getXML() );
+        } catch ( KettleException e ) {
+          throw new ServletException( "Unable to get the job status in XML format", e );
         }
       } else {
-        response.setContentType("text/html");
+        response.setContentType( "text/html" );
 
-        out.println("<HTML>");
-        out.println("<HEAD>");
-        out.println("<TITLE>" + BaseMessages.getString(PKG, "GetJobStatusServlet.KettleJobStatus") + "</TITLE>");
-        out.println("<META http-equiv=\"Refresh\" content=\"10;url=" + convertContextPath(GetJobStatusServlet.CONTEXT_PATH) + "?name="
-            + URLEncoder.encode(jobName, "UTF-8") + "&id="+URLEncoder.encode(id, "UTF-8")+"\">");
-        out.println("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
-        out.println("</HEAD>");
-        out.println("<BODY>");
-        out.println("<H1>" + BaseMessages.getString(PKG, "GetJobStatusServlet.JobStatus") + "</H1>");
+        out.println( "<HTML>" );
+        out.println( "<HEAD>" );
+        out.println( "<TITLE>" + BaseMessages.getString( PKG, "GetJobStatusServlet.KettleJobStatus" ) + "</TITLE>" );
+        out.println( "<META http-equiv=\"Refresh\" content=\"10;url="
+            + convertContextPath( GetJobStatusServlet.CONTEXT_PATH ) + "?name=" + URLEncoder.encode( jobName, "UTF-8" )
+            + "&id=" + URLEncoder.encode( id, "UTF-8" ) + "\">" );
+        out.println( "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">" );
+        out.println( "</HEAD>" );
+        out.println( "<BODY>" );
+        out.println( "<H1>" + BaseMessages.getString( PKG, "GetJobStatusServlet.JobStatus" ) + "</H1>" );
 
         try {
-          out.println("<table border=\"1\">");
-          out.print("<tr> <th>" + BaseMessages.getString(PKG, "GetJobStatusServlet.Jobname") + "</th> <th>"
-              + BaseMessages.getString(PKG, "TransStatusServlet.TransStatus") + "</th> </tr>");
+          out.println( "<table border=\"1\">" );
+          out.print( "<tr> <th>" + BaseMessages.getString( PKG, "GetJobStatusServlet.Jobname" ) + "</th> <th>"
+              + BaseMessages.getString( PKG, "TransStatusServlet.TransStatus" ) + "</th> </tr>" );
 
-          out.print("<tr>");
-          out.print("<td>" + encoder.encodeForHTML(jobName) + "</td>");
-          out.print("<td>" + encoder.encodeForHTML(status) + "</td>");
-          out.print("</tr>");
-          out.print("</table>");
+          out.print( "<tr>" );
+          out.print( "<td>" + encoder.encodeForHTML( jobName ) + "</td>" );
+          out.print( "<td>" + encoder.encodeForHTML( status ) + "</td>" );
+          out.print( "</tr>" );
+          out.print( "</table>" );
 
-          out.print("<p>");
+          out.print( "<p>" );
 
-          if (job.isFinished()) {
-            out.print("<a href=\"" + convertContextPath(StartJobServlet.CONTEXT_PATH) + "?name=" + URLEncoder.encode(jobName, "UTF-8") + "&id="+URLEncoder.encode(id, "UTF-8")+"\">"
-                + BaseMessages.getString(PKG, "GetJobStatusServlet.StartJob") + "</a>");
-            out.print("<p>");
+          if ( job.isFinished() ) {
+            out.print( "<a href=\"" + convertContextPath( StartJobServlet.CONTEXT_PATH ) + "?name="
+                + URLEncoder.encode( jobName, "UTF-8" ) + "&id=" + URLEncoder.encode( id, "UTF-8" ) + "\">"
+                + BaseMessages.getString( PKG, "GetJobStatusServlet.StartJob" ) + "</a>" );
+            out.print( "<p>" );
           } else {
-            out.print("<a href=\"" + convertContextPath(StopJobServlet.CONTEXT_PATH) + "?name=" + URLEncoder.encode(jobName, "UTF-8") + "&id="+URLEncoder.encode(id, "UTF-8")+"\">"
-                + BaseMessages.getString(PKG, "GetJobStatusServlet.StopJob") + "</a>");
-            out.print("<p>");
+            out.print( "<a href=\"" + convertContextPath( StopJobServlet.CONTEXT_PATH ) + "?name="
+                + URLEncoder.encode( jobName, "UTF-8" ) + "&id=" + URLEncoder.encode( id, "UTF-8" ) + "\">"
+                + BaseMessages.getString( PKG, "GetJobStatusServlet.StopJob" ) + "</a>" );
+            out.print( "<p>" );
           }
 
-          out.println("<p>");
+          out.println( "<p>" );
 
-          out.print("<a href=\"" + convertContextPath(GetJobStatusServlet.CONTEXT_PATH) + "?name=" + URLEncoder.encode(jobName, "UTF-8") + "&xml=y&id="+URLEncoder.encode(id, "UTF-8")+"\">"
-              + BaseMessages.getString(PKG, "TransStatusServlet.ShowAsXml") + "</a><br>");
-          out.print("<a href=\"" + convertContextPath(GetStatusServlet.CONTEXT_PATH) + "\">"
-              + BaseMessages.getString(PKG, "TransStatusServlet.BackToStatusPage") + "</a><br>");
-          out.print("<p><a href=\"" + convertContextPath(GetJobStatusServlet.CONTEXT_PATH) + "?name=" + URLEncoder.encode(jobName, "UTF-8") + "&id="+URLEncoder.encode(id, "UTF-8")+"\">"
-              + BaseMessages.getString(PKG, "TransStatusServlet.Refresh") + "</a>");
+          out.print( "<a href=\"" + convertContextPath( GetJobStatusServlet.CONTEXT_PATH ) + "?name="
+              + URLEncoder.encode( jobName, "UTF-8" ) + "&xml=y&id=" + URLEncoder.encode( id, "UTF-8" ) + "\">"
+              + BaseMessages.getString( PKG, "TransStatusServlet.ShowAsXml" ) + "</a><br>" );
+          out.print( "<a href=\"" + convertContextPath( GetStatusServlet.CONTEXT_PATH ) + "\">"
+              + BaseMessages.getString( PKG, "TransStatusServlet.BackToStatusPage" ) + "</a><br>" );
+          out.print( "<p><a href=\"" + convertContextPath( GetJobStatusServlet.CONTEXT_PATH ) + "?name="
+              + URLEncoder.encode( jobName, "UTF-8" ) + "&id=" + URLEncoder.encode( id, "UTF-8" ) + "\">"
+              + BaseMessages.getString( PKG, "TransStatusServlet.Refresh" ) + "</a>" );
 
           // Put the logging below that.
 
-          out.println("<p>");
-          out.println("<textarea id=\"joblog\" cols=\"120\" rows=\"20\" wrap=\"off\" name=\"Job log\" readonly=\"readonly\">" + encoder.encodeForHTML(logText) + "</textarea>");
+          out.println( "<p>" );
+          out.println( "<textarea id=\"joblog\" cols=\"120\" rows=\"20\" wrap=\"off\""
+              + "name=\"Job log\" readonly=\"readonly\">"
+              + encoder.encodeForHTML( logText ) + "</textarea>" );
 
-          out.println("<script type=\"text/javascript\"> ");
-          out.println("  joblog.scrollTop=joblog.scrollHeight; ");
-          out.println("</script> ");
-          out.println("<p>");
-        } catch (Exception ex) {
-          out.println("<p>");
-          out.println("<pre>");
-          out.println(encoder.encodeForHTML(Const.getStackTracker(ex)));
-          out.println("</pre>");
+          out.println( "<script type=\"text/javascript\"> " );
+          out.println( "  joblog.scrollTop=joblog.scrollHeight; " );
+          out.println( "</script> " );
+          out.println( "<p>" );
+        } catch ( Exception ex ) {
+          out.println( "<p>" );
+          out.println( "<pre>" );
+          out.println( encoder.encodeForHTML( Const.getStackTracker( ex ) ) );
+          out.println( "</pre>" );
         }
 
-        out.println("<p>");
-        out.println("</BODY>");
-        out.println("</HTML>");
+        out.println( "<p>" );
+        out.println( "</BODY>" );
+        out.println( "</HTML>" );
       }
     } else {
-      if (useXML) {
-        out.println(new WebResult(WebResult.STRING_ERROR, BaseMessages.getString(PKG, "StartJobServlet.Log.SpecifiedJobNotFound", jobName, id)));
+      if ( useXML ) {
+        out.println( new WebResult( WebResult.STRING_ERROR, BaseMessages.getString( PKG,
+            "StartJobServlet.Log.SpecifiedJobNotFound", jobName, id ) ) );
       } else {
-        out.println("<H1>Job '" + encoder.encodeForHTML(jobName) + "' could not be found.</H1>");
-        out.println("<a href=\"" + convertContextPath(GetStatusServlet.CONTEXT_PATH) + "\">"
-            + BaseMessages.getString(PKG, "TransStatusServlet.BackToStatusPage") + "</a><p>");
+        out.println( "<H1>Job '" + encoder.encodeForHTML( jobName ) + "' could not be found.</H1>" );
+        out.println( "<a href=\"" + convertContextPath( GetStatusServlet.CONTEXT_PATH ) + "\">"
+            + BaseMessages.getString( PKG, "TransStatusServlet.BackToStatusPage" ) + "</a><p>" );
       }
     }
   }
