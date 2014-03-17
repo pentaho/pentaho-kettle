1ad249400cdd91e32ea80e07aa0cb304f24a8254
Pavel Sakun <pavel_sakun@epam.com>
2014-03-17 17:44:31 +0300

    SP-794 - Reformat

--- src/org/pentaho/di/trans/steps/csvinput/CsvInputData.java
+++ src/org/pentaho/di/trans/steps/csvinput/CsvInputData.java
@@ -22,185 +22,185 @@
 
 package org.pentaho.di.trans.steps.csvinput;
 
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.steps.textfileinput.EncodingType;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.List;
 
-import org.pentaho.di.core.row.RowMetaInterface;
-import org.pentaho.di.trans.step.BaseStepData;
-import org.pentaho.di.trans.step.StepDataInterface;
-import org.pentaho.di.trans.steps.textfileinput.EncodingType;
-
 
 /**
  * @author Matt
  * @since 24-jan-2005
  */
-public class CsvInputData extends BaseStepData implements StepDataInterface
-{
-	public FileChannel fc;
-	public ByteBuffer bb;
-	public RowMetaInterface convertRowMeta;
-	public RowMetaInterface outputRowMeta;
-	
-	public byte[] byteBuffer;
-	public int    startBuffer;
-	public int    endBuffer;
-	public int    bufferSize;
+public class CsvInputData extends BaseStepData implements StepDataInterface {
+  public FileChannel fc;
+  public ByteBuffer bb;
+  public RowMetaInterface convertRowMeta;
+  public RowMetaInterface outputRowMeta;
 
-	public byte[] delimiter;
-	public byte[] enclosure;
-	
-	public int preferredBufferSize;
-	public String[] filenames;
-	public int      filenr;
-	public int      startFilenr;
-	public byte[]   binaryFilename;
-	public long fileSize;
-	public FileInputStream fis;
-	
-	public boolean  isAddingRowNumber;
-	public long     rowNumber;
-	public boolean stopReading;
-	public int stepNumber;
-	public int totalNumberOfSteps;
-	public List<Long> fileSizes;
-	public long totalFileSize;
-	public long blockToRead;
-	public long startPosition;
-	public long endPosition;
-	public long bytesToSkipInFirstFile;
-			
-	public long totalBytesRead;
-	
-	public boolean parallel;
-	public int filenameFieldIndex;
-	public int rownumFieldIndex;
-    public EncodingType encodingType;
-    public PatternMatcherInterface delimiterMatcher;
-    public PatternMatcherInterface enclosureMatcher;
-    public CrLfMatcherInterface crLfMatcher;
-	
-	/**
-	 * 
-	 */
-	public CsvInputData()
-	{
-		super();
-		byteBuffer = new byte[] {};
-		startBuffer = 0;
-		endBuffer = 0;
-		totalBytesRead = 0;
-	}
+  public byte[] byteBuffer;
+  public int startBuffer;
+  public int endBuffer;
+  public int bufferSize;
 
-	// Resize
-	public void resizeByteBufferArray() {
-		// What's the new size?
-		// It's (endBuffer-startBuffer)+size !!
-		// That way we can at least read one full block of data using NIO
-		//
-		bufferSize = endBuffer-startBuffer;
-		int newSize = bufferSize+preferredBufferSize;
-		byte[] newByteBuffer = new byte[newSize+100];
-		
-		// copy over the old data...
-		System.arraycopy(byteBuffer, startBuffer, newByteBuffer, 0, bufferSize);
-		
-		// replace the old byte buffer...
-		byteBuffer = newByteBuffer;
-		
-		// Adjust start and end point of data in the byte buffer
-		//
-		startBuffer = 0;
-		endBuffer = bufferSize;
-	}
+  public byte[] delimiter;
+  public byte[] enclosure;
 
-	public int readBufferFromFile() throws IOException {
-		// See if the line is not longer than the buffer.
-		// In that case we need to increase the size of the byte buffer.
-		// Since this method doesn't get called every other character, I'm sure we can spend a bit of time here without major performance loss.
-		//
-		if (endBuffer>=bb.capacity()) {
-			resizeByteBuffer((int)(bb.capacity()*1.5));
-		}
-		
-		bb.position(endBuffer);
-		int n = fc.read( bb );
-		if( n>=0) {
+  public int preferredBufferSize;
+  public String[] filenames;
+  public int filenr;
+  public int startFilenr;
+  public byte[] binaryFilename;
+  public long fileSize;
+  public FileInputStream fis;
 
-			// adjust the highest used position...
-			//
-			bufferSize = endBuffer+n; 
-			
-			// Make sure we have room in the target byte buffer array
-			//
-			if (byteBuffer.length < bufferSize) {
-				byte[] newByteBuffer = new byte[bufferSize];
-				System.arraycopy(byteBuffer, 0, newByteBuffer, 0, byteBuffer.length);
-				byteBuffer=newByteBuffer;
-			}
-			
-			// Store the data in our byte array
-			//
-			bb.position(endBuffer);
-			bb.get( byteBuffer, endBuffer, n);
-		}
-		
-		return n;
-	}
+  public boolean isAddingRowNumber;
+  public long rowNumber;
+  public boolean stopReading;
+  public int stepNumber;
+  public int totalNumberOfSteps;
+  public List<Long> fileSizes;
+  public long totalFileSize;
+  public long blockToRead;
+  public long startPosition;
+  public long endPosition;
+  public long bytesToSkipInFirstFile;
 
-	private void resizeByteBuffer(int newSize) {
-		ByteBuffer newBuffer = ByteBuffer.allocateDirect( newSize ); // Increase by 50%
-		newBuffer.position(0);
-		newBuffer.put(bb);
-		bb=newBuffer;
-	}
+  public long totalBytesRead;
 
-	/**
-	 * Increase the endBuffer pointer by one.<br>
-	 * If there is not enough room in the buffer to go there, resize the byte buffer and read more data.<br>
-	 * if there is no more data to read and if the endBuffer pointer has reached the end of the byte buffer, we return true.<br>
-	 * @return true if we reached the end of the byte buffer.
-	 * @throws IOException In case we get an error reading from the input file.
-	 */
-	public boolean increaseEndBuffer() throws IOException {
-		endBuffer++;
-		
-		if (endBuffer>=bufferSize) {
-			// Oops, we need to read more data...
-			// Better resize this before we read other things in it...
-			//
-			resizeByteBufferArray();
-			
-			// Also read another chunk of data, now that we have the space for it...
-			//
-			int n =readBufferFromFile();
-			
-			// Return true we didn't manage to read anything and we reached the end of the buffer...
-			//
-			return n<0;
-		}
-		
-		return false;
-	}
+  public boolean parallel;
+  public int filenameFieldIndex;
+  public int rownumFieldIndex;
+  public EncodingType encodingType;
+  public PatternMatcherInterface delimiterMatcher;
+  public PatternMatcherInterface enclosureMatcher;
+  public CrLfMatcherInterface crLfMatcher;
 
-	/**
-      <pre>	 
-      [abcd "" defg] --> [abcd " defg]
-      [""""] --> [""]
-      [""] --> ["]
-      </pre>	 
+  /**
+   *
+   */
+  public CsvInputData() {
+    super();
+    byteBuffer = new byte[] { };
+    startBuffer = 0;
+    endBuffer = 0;
+    totalBytesRead = 0;
+  }
 
-     @return the byte array with escaped enclosures escaped.
-	*/
-	public byte[] removeEscapedEnclosures(byte[] field, int nrEnclosuresFound) {
-    byte[] result = new byte[field.length - nrEnclosuresFound];
+  // Resize
+  public void resizeByteBufferArray() {
+    // What's the new size?
+    // It's (endBuffer-startBuffer)+size !!
+    // That way we can at least read one full block of data using NIO
+    //
+    bufferSize = endBuffer - startBuffer;
+    int newSize = bufferSize + preferredBufferSize;
+    byte[] newByteBuffer = new byte[ newSize + 100 ];
+
+    // copy over the old data...
+    System.arraycopy( byteBuffer, startBuffer, newByteBuffer, 0, bufferSize );
+
+    // replace the old byte buffer...
+    byteBuffer = newByteBuffer;
+
+    // Adjust start and end point of data in the byte buffer
+    //
+    startBuffer = 0;
+    endBuffer = bufferSize;
+  }
+
+  public int readBufferFromFile() throws IOException {
+    // See if the line is not longer than the buffer.
+    // In that case we need to increase the size of the byte buffer.
+    // Since this method doesn't get called every other character, I'm sure we can spend a bit of time here without
+    // major performance loss.
+    //
+    if ( endBuffer >= bb.capacity() ) {
+      resizeByteBuffer( (int) ( bb.capacity() * 1.5 ) );
+    }
+
+    bb.position( endBuffer );
+    int n = fc.read( bb );
+    if ( n >= 0 ) {
+
+      // adjust the highest used position...
+      //
+      bufferSize = endBuffer + n;
+
+      // Make sure we have room in the target byte buffer array
+      //
+      if ( byteBuffer.length < bufferSize ) {
+        byte[] newByteBuffer = new byte[ bufferSize ];
+        System.arraycopy( byteBuffer, 0, newByteBuffer, 0, byteBuffer.length );
+        byteBuffer = newByteBuffer;
+      }
+
+      // Store the data in our byte array
+      //
+      bb.position( endBuffer );
+      bb.get( byteBuffer, endBuffer, n );
+    }
+
+    return n;
+  }
+
+  private void resizeByteBuffer( int newSize ) {
+    ByteBuffer newBuffer = ByteBuffer.allocateDirect( newSize ); // Increase by 50%
+    newBuffer.position( 0 );
+    newBuffer.put( bb );
+    bb = newBuffer;
+  }
+
+  /**
+   * Increase the endBuffer pointer by one.<br> If there is not enough room in the buffer to go there, resize the byte
+   * buffer and read more data.<br> if there is no more data to read and if the endBuffer pointer has reached the end of
+   * the byte buffer, we return true.<br>
+   *
+   * @return true if we reached the end of the byte buffer.
+   * @throws IOException In case we get an error reading from the input file.
+   */
+  public boolean increaseEndBuffer() throws IOException {
+    endBuffer++;
+
+    if ( endBuffer >= bufferSize ) {
+      // Oops, we need to read more data...
+      // Better resize this before we read other things in it...
+      //
+      resizeByteBufferArray();
+
+      // Also read another chunk of data, now that we have the space for it...
+      //
+      int n = readBufferFromFile();
+
+      // Return true we didn't manage to read anything and we reached the end of the buffer...
+      //
+      return n < 0;
+    }
+
+    return false;
+  }
+
+  /**
+   * <pre>
+   * [abcd "" defg] --> [abcd " defg]
+   * [""""] --> [""]
+   * [""] --> ["]
+   * </pre>
+   *
+   * @return the byte array with escaped enclosures escaped.
+   */
+  public byte[] removeEscapedEnclosures( byte[] field, int nrEnclosuresFound ) {
+    byte[] result = new byte[ field.length - nrEnclosuresFound ];
     int resultIndex = 0;
-    for (int i = 0; i < field.length; i++) {
-      result[resultIndex++] = field[i];
-      if (field[i] == enclosure[0] && i + 1 < field.length && field[i + 1] == enclosure[0]) {
+    for ( int i = 0; i < field.length; i++ ) {
+      result[ resultIndex++ ] = field[ i ];
+      if ( field[ i ] == enclosure[ 0 ] && i + 1 < field.length && field[ i + 1 ] == enclosure[ 0 ] ) {
         //Skip the escaped enclosure after adding the first one
         i++;
       }
