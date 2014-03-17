7192ba104062390d6e4f100f2b8fef9b48c875e4
Bryan Rosander <brosander@pentaho.com>
2014-03-17 17:28:51 -0400

    [PDI-11565] - Adding setId() to KerberosAuthenticationProvider

--- engine/src/org/pentaho/di/core/auth/KerberosAuthenticationProvider.java
+++ engine/src/org/pentaho/di/core/auth/KerberosAuthenticationProvider.java
@@ -107,4 +107,8 @@ public class KerberosAuthenticationProvider implements AuthenticationProvider {
   public String getId() {
     return id;
   }
+
+  public void setId( String id ) {
+    this.id = id;
+  }
 }
