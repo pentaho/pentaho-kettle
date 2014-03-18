b88f04905ba5aad817e8f149e682bd66eec0dc57
Pavel Sakun <pavel_sakun@epam.com>
2014-03-18 17:33:51 +0300

    SP-896 - Reformat

--- engine/src/org/pentaho/di/trans/steps/synchronizeaftermerge/SynchronizeAfterMergeData.java
+++ engine/src/org/pentaho/di/trans/steps/synchronizeaftermerge/SynchronizeAfterMergeData.java
@@ -42,91 +42,88 @@ import org.pentaho.di.trans.step.StepDataInterface;
  * @author Samatar
  * @since 13-10-2008
  */
-public class SynchronizeAfterMergeData extends BaseStepData implements StepDataInterface
-{
-	public Database db;
+public class SynchronizeAfterMergeData extends BaseStepData implements StepDataInterface {
+  public Database db;
 
-	public int    keynrs[];         // nr of keylookup -value in row...
-	public int    keynrs2[];        // nr of keylookup2-value in row...
-	public int    valuenrs[];       // Stream valuename nrs to prevent searches.
-	public int indexOfTableNameField;
-	
-	public int indexOfOperationOrderField;
+  public int keynrs[]; // nr of keylookup -value in row...
+  public int keynrs2[]; // nr of keylookup2-value in row...
+  public int valuenrs[]; // Stream valuename nrs to prevent searches.
+  public int indexOfTableNameField;
 
-	//List<String> updateColumns = new ArrayList<String>();
-	   /**
-     * Mapping between the SQL and the actual prepared statement.
-     * Normally this is only one, but in case we have more then one, it's convenient to have this.
-     */
-    public  Map<String, PreparedStatement>      preparedStatements;
-    public String realTableName;
-    public String realSchemaName;
-    public String realSchemaTable;
-    
-    /** Use batch mode or not? */
-    public boolean batchMode;
-    
-	PreparedStatement	insertStatement;
-	PreparedStatement	lookupStatement;
-	PreparedStatement	updateStatement;
-	PreparedStatement	deleteStatement;
-    
-    public String insertValue;
-    public String updateValue;
-    public String deleteValue;
-    
-    public String stringErrorKeyNotFound;
-    
-    public String stringFieldnames;
-    
-    public boolean lookupFailure;
-    
-    public RowMetaInterface outputRowMeta;
-    public RowMetaInterface inputRowMeta;
-    
-    public RowMetaInterface deleteParameterRowMeta;
-    public RowMetaInterface  updateParameterRowMeta;
-    public RowMetaInterface  lookupParameterRowMeta;
-    public RowMetaInterface  lookupReturnRowMeta;
-    public RowMetaInterface  insertRowMeta;
-    
-	public Map<String, Integer> commitCounterMap;
-	public int commitSize;
-	public DatabaseMeta databaseMeta;
-	public boolean specialErrorHandling;
-	public Savepoint savepoint;
-	public boolean releaseSavepoint;
-	
-    public List<Object[]> batchBuffer;
-	
-	
-	/**
-	 *  Default constructor.
-	 */
-	public SynchronizeAfterMergeData()
-	{
-		super();
-		insertStatement=null;
-		lookupStatement=null;
-		updateStatement=null;
-		deleteStatement=null;
-		
-		indexOfTableNameField=-1;
-		
-		db=null;
-        preparedStatements = new Hashtable<String, PreparedStatement>(); 
-        realTableName=null;
-		realSchemaName=null;
-		batchMode=false;
-		insertValue=null;
-		updateValue=null;
-		deleteValue=null;
-		indexOfOperationOrderField=-1;
-		lookupFailure=false;
-		realSchemaTable=null;
-        commitCounterMap = new HashMap<String, Integer>();
-        batchBuffer = new ArrayList<Object[]>();
-        releaseSavepoint = true;
+  public int indexOfOperationOrderField;
 
-	}
+  // List<String> updateColumns = new ArrayList<String>();
+  /**
+   * Mapping between the SQL and the actual prepared statement. Normally this is only one, but in case we have more then
+   * one, it's convenient to have this.
+   */
+  public Map<String, PreparedStatement> preparedStatements;
+  public String realTableName;
+  public String realSchemaName;
+  public String realSchemaTable;
+
+  /** Use batch mode or not? */
+  public boolean batchMode;
+
+  PreparedStatement insertStatement;
+  PreparedStatement lookupStatement;
+  PreparedStatement updateStatement;
+  PreparedStatement deleteStatement;
+
+  public String insertValue;
+  public String updateValue;
+  public String deleteValue;
+
+  public String stringErrorKeyNotFound;
+
+  public String stringFieldnames;
+
+  public boolean lookupFailure;
+
+  public RowMetaInterface outputRowMeta;
+  public RowMetaInterface inputRowMeta;
+
+  public RowMetaInterface deleteParameterRowMeta;
+  public RowMetaInterface updateParameterRowMeta;
+  public RowMetaInterface lookupParameterRowMeta;
+  public RowMetaInterface lookupReturnRowMeta;
+  public RowMetaInterface insertRowMeta;
+
+  public Map<String, Integer> commitCounterMap;
+  public int commitSize;
+  public DatabaseMeta databaseMeta;
+  public boolean specialErrorHandling;
+  public Savepoint savepoint;
+  public boolean releaseSavepoint;
+
+  public List<Object[]> batchBuffer;
+
+  /**
+   * Default constructor.
+   */
+  public SynchronizeAfterMergeData() {
+    super();
+    insertStatement = null;
+    lookupStatement = null;
+    updateStatement = null;
+    deleteStatement = null;
+
+    indexOfTableNameField = -1;
+
+    db = null;
+    preparedStatements = new Hashtable<String, PreparedStatement>();
+    realTableName = null;
+    realSchemaName = null;
+    batchMode = false;
+    insertValue = null;
+    updateValue = null;
+    deleteValue = null;
+    indexOfOperationOrderField = -1;
+    lookupFailure = false;
+    realSchemaTable = null;
+    commitCounterMap = new HashMap<String, Integer>();
+    batchBuffer = new ArrayList<Object[]>();
+    releaseSavepoint = true;
+
+  }
 }
