d7f1d1f7a71c3940c80c88e4d6fba4fd0e647e0e
Mikhail Tseu <mikhail_tseu@epam.com>
2014-03-19 16:14:54 +0100

    [SP-788] - Backport of PDI-10270: apply formatting

--- engine/src/org/pentaho/di/job/entries/filesexist/JobEntryFilesExist.java
+++ engine/src/org/pentaho/di/job/entries/filesexist/JobEntryFilesExist.java
@@ -51,193 +51,177 @@ import org.w3c.dom.Node;
  * 
  * @author Samatar
  * @since 10-12-2007
- *
+ * 
  */
 
-public class JobEntryFilesExist extends JobEntryBase implements Cloneable, JobEntryInterface
-{
-	private static Class<?> PKG = JobEntryFilesExist.class; // for i18n purposes, needed by Translator2!!   $NON-NLS-1$
+public class JobEntryFilesExist extends JobEntryBase implements Cloneable, JobEntryInterface {
+  private static Class<?> PKG = JobEntryFilesExist.class; // for i18n purposes, needed by Translator2!! $NON-NLS-1$
 
-	private String filename;
-	
-	public String arguments[];
-	
-	public JobEntryFilesExist(String n)
-	{
-		super(n, ""); 
-		filename=null;
-		setID(-1L);	
-	}
+  private String filename;
 
-	public JobEntryFilesExist()
-	{
-		this("");
-	}
+  public String[] arguments;
 
-    public Object clone()
-    {
-        JobEntryFilesExist je = (JobEntryFilesExist) super.clone();
-        return je;
+  public JobEntryFilesExist( String n ) {
+    super( n, "" );
+    filename = null;
+    setID( -1L );
+  }
+
+  public JobEntryFilesExist() {
+    this( "" );
+  }
+
+  public Object clone() {
+    JobEntryFilesExist je = (JobEntryFilesExist) super.clone();
+    return je;
+  }
+
+  public String getXML() {
+    StringBuffer retval = new StringBuffer();
+
+    retval.append( super.getXML() );
+    retval.append( "      " ).append( XMLHandler.addTagValue( "filename", filename ) );
+
+    retval.append( "      <fields>" ).append( Const.CR );
+    if ( arguments != null ) {
+      for ( int i = 0; i < arguments.length; i++ ) {
+        retval.append( "        <field>" ).append( Const.CR );
+        retval.append( "          " ).append( XMLHandler.addTagValue( "name", arguments[i] ) );
+        retval.append( "        </field>" ).append( Const.CR );
+      }
     }
-    
-	public String getXML()
-	{
-        StringBuffer retval = new StringBuffer();
-		
-		retval.append(super.getXML());		
-		retval.append("      ").append(XMLHandler.addTagValue("filename",   filename));
-		
-		 retval.append("      <fields>").append(Const.CR); 
-		    if (arguments != null) {
-		      for (int i = 0; i < arguments.length; i++) {
-		        retval.append("        <field>").append(Const.CR); 
-		        retval.append("          ").append(XMLHandler.addTagValue("name", arguments[i]));
-		        retval.append("        </field>").append(Const.CR); 
-		      }
-		    }
-		    retval.append("      </fields>").append(Const.CR); 
-		
-		return retval.toString();
-	}
-	
-	public void loadXML(Node entrynode, List<DatabaseMeta> databases, List<SlaveServer> slaveServers, Repository rep, IMetaStore metaStore) throws KettleXMLException
-	{
-		try
-		{
-			super.loadXML(entrynode, databases, slaveServers);
-			filename      = XMLHandler.getTagValue(entrynode, "filename");
-			
-		    Node fields = XMLHandler.getSubNode(entrynode, "fields"); 
+    retval.append( "      </fields>" ).append( Const.CR );
 
-	        // How many field arguments?
-	        int nrFields = XMLHandler.countNodes(fields, "field"); 
-	        arguments = new String[nrFields];
+    return retval.toString();
+  }
 
-	        // Read them all...
-	        for (int i = 0; i < nrFields; i++) {
-	        Node fnode = XMLHandler.getSubNodeByNr(fields, "field", i); 
+  public void loadXML( Node entrynode, List<DatabaseMeta> databases, List<SlaveServer> slaveServers, Repository rep,
+      IMetaStore metaStore ) throws KettleXMLException {
+    try {
+      super.loadXML( entrynode, databases, slaveServers );
+      filename = XMLHandler.getTagValue( entrynode, "filename" );
 
-	        arguments[i] = XMLHandler.getTagValue(fnode, "name"); 
+      Node fields = XMLHandler.getSubNode( entrynode, "fields" );
 
-	      }
-		}
-		catch(KettleXMLException xe)
-		{
-			throw new KettleXMLException(BaseMessages.getString(PKG, "JobEntryFilesExist.ERROR_0001_Cannot_Load_Job_Entry_From_Xml_Node", xe.getMessage()));
-		}
-	}
+      // How many field arguments?
+      int nrFields = XMLHandler.countNodes( fields, "field" );
+      arguments = new String[nrFields];
 
-	public void loadRep(Repository rep, IMetaStore metaStore, ObjectId id_jobentry, List<DatabaseMeta> databases, List<SlaveServer> slaveServers) throws KettleException
-	{
-		try
-		{
-			filename = rep.getJobEntryAttributeString(id_jobentry, "filename");
-			
-			 // How many arguments?
-	        int argnr = rep.countNrJobEntryAttributes(id_jobentry, "name"); 
-	        arguments = new String[argnr];
+      // Read them all...
+      for ( int i = 0; i < nrFields; i++ ) {
+        Node fnode = XMLHandler.getSubNodeByNr( fields, "field", i );
 
-	        // Read them all...
-	        for (int a = 0; a < argnr; a++) 
-	        {
-	          arguments[a] = rep.getJobEntryAttributeString(id_jobentry, a, "name"); 
-	        }
-		}
-		catch(KettleException dbe)
-		{
-			throw new KettleException(BaseMessages.getString(PKG, "JobEntryFilesExist.ERROR_0002_Cannot_Load_Job_From_Repository",""+id_jobentry, dbe.getMessage()));
-		}
-	}
-	
-	public void saveRep(Repository rep, IMetaStore metaStore, ObjectId id_job) throws KettleException
-	{
-		try
-		{
-			rep.saveJobEntryAttribute(id_job, getObjectId(), "filename", filename);
-			
-			   // save the arguments...
-		    if (arguments != null) {
-		       for (int i = 0; i < arguments.length; i++) {
-		          rep.saveJobEntryAttribute(id_job, getObjectId(), i, "name", arguments[i]);
-		       }
-		    }
-		}
-		catch(KettleDatabaseException dbe)
-		{
-			throw new KettleException(BaseMessages.getString(PKG, "JobEntryFilesExist.ERROR_0003_Cannot_Save_Job_Entry",""+id_job, dbe.getMessage()));
-		}
-	}
+        arguments[i] = XMLHandler.getTagValue( fnode, "name" );
 
-	public void setFilename(String filename)
-	{
-		this.filename = filename;
-	}
-	
-	public String getFilename()
-	{
-		return filename;
-	}
-    
+      }
+    } catch ( KettleXMLException xe ) {
+      throw new KettleXMLException( BaseMessages.getString( PKG,
+          "JobEntryFilesExist.ERROR_0001_Cannot_Load_Job_Entry_From_Xml_Node", xe.getMessage() ) );
+    }
+  }
 
-	
-	public Result execute(Result previousResult, int nr)
-	{
-		Result result = previousResult;
-		result.setResult( false );
-		int missingfiles=0;
-		
-		if (arguments != null) 
-		{
-		      for (int i = 0; i < arguments.length && !parentJob.isStopped(); i++) 
-		      {
-		    	  FileObject file =null;
-		      
-		    	  try
-		            {
-		    		   String realFilefoldername = environmentSubstitute(arguments[i]);
-		    		   file = KettleVFS.getFileObject(realFilefoldername, this);
-		    		  
-		    		    if (file.exists() && file.isReadable())
-		    		    {
-		    		    	if(log.isDetailed())
-		    		    		logDetailed(BaseMessages.getString(PKG, "JobEntryFilesExist.File_Exists", realFilefoldername)); 
-		    		    }
-		                else
-		                {
-		                	missingfiles ++;
-		                	result.setNrErrors(missingfiles);
-		                	if(log.isDetailed())
-		                		logDetailed(BaseMessages.getString(PKG, "JobEntryFilesExist.File_Does_Not_Exist", realFilefoldername)); 
-		                }
-		    		  
-		            }
-		    	  	catch (Exception e)
-		            {
-		    	  		missingfiles ++;
-		                result.setNrErrors(missingfiles);
-		                logError(BaseMessages.getString(PKG, "JobEntryFilesExist.ERROR_0004_IO_Exception", e.toString()), e); 
-		            }
-		    	  	finally
-		    	  	{
-		    	  		if (file != null) {try {file.close();file=null;} catch (IOException ex) { /* Ignore */ }}
-		    	  	}
-		      }
-		        
-		}
-		
-		if(missingfiles==0) 
-			result.setResult(true);
-		
-		return result;
-	}    
+  public void loadRep( Repository rep, IMetaStore metaStore, ObjectId id_jobentry, List<DatabaseMeta> databases,
+      List<SlaveServer> slaveServers ) throws KettleException {
+    try {
+      filename = rep.getJobEntryAttributeString( id_jobentry, "filename" );
 
-	public boolean evaluates()
-	{
-		return true;
-	}
-    
-   @Override
-   public void check(List<CheckResultInterface> remarks, JobMeta jobMeta, VariableSpace space, Repository repository, IMetaStore metaStore) {
-   }
+      // How many arguments?
+      int argnr = rep.countNrJobEntryAttributes( id_jobentry, "name" );
+      arguments = new String[argnr];
+
+      // Read them all...
+      for ( int a = 0; a < argnr; a++ ) {
+        arguments[a] = rep.getJobEntryAttributeString( id_jobentry, a, "name" );
+      }
+    } catch ( KettleException dbe ) {
+      throw new KettleException( BaseMessages.getString( PKG,
+          "JobEntryFilesExist.ERROR_0002_Cannot_Load_Job_From_Repository", "" + id_jobentry, dbe.getMessage() ) );
+    }
+  }
+
+  public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_job ) throws KettleException {
+    try {
+      rep.saveJobEntryAttribute( id_job, getObjectId(), "filename", filename );
+
+      // save the arguments...
+      if ( arguments != null ) {
+        for ( int i = 0; i < arguments.length; i++ ) {
+          rep.saveJobEntryAttribute( id_job, getObjectId(), i, "name", arguments[i] );
+        }
+      }
+    } catch ( KettleDatabaseException dbe ) {
+      throw new KettleException( BaseMessages.getString( PKG, "JobEntryFilesExist.ERROR_0003_Cannot_Save_Job_Entry", ""
+          + id_job, dbe.getMessage() ) );
+    }
+  }
+
+  public void setFilename( String filename ) {
+    this.filename = filename;
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public Result execute( Result previousResult, int nr ) {
+    Result result = previousResult;
+    result.setResult( false );
+    int missingfiles = 0;
+
+    if ( arguments != null ) {
+      for ( int i = 0; i < arguments.length && !parentJob.isStopped(); i++ ) {
+        FileObject file = null;
+
+        try {
+          String realFilefoldername = environmentSubstitute( arguments[i] );
+          file = KettleVFS.getFileObject( realFilefoldername, this );
+
+          if ( file.exists() && file.isReadable() ) {
+            if ( log.isDetailed() ) {
+              logDetailed( BaseMessages.getString( PKG, "JobEntryFilesExist.File_Exists", realFilefoldername ) );
+            }
+          } else {
+            missingfiles++;
+            result.setNrErrors( missingfiles );
+            if ( log.isDetailed() ) {
+              logDetailed( BaseMessages.getString(
+                  PKG,
+                  "JobEntryFilesExist.File_Does_Not_Exist",
+                  realFilefoldername ) );
+            }
+          }
+
+        } catch ( Exception e ) {
+          missingfiles++;
+          result.setNrErrors( missingfiles );
+          logError( BaseMessages.getString( PKG, "JobEntryFilesExist.ERROR_0004_IO_Exception", e.toString() ), e );
+        } finally {
+          if ( file != null ) {
+            try {
+              file.close();
+              file = null;
+            } catch ( IOException ex ) { /* Ignore */
+            }
+          }
+        }
+      }
+
+    }
+
+    if ( missingfiles == 0 ) {
+      result.setResult( true );
+    }
+
+    return result;
+  }
+
+  public boolean evaluates() {
+    return true;
+  }
+
+  @Override
+  public void check( List<CheckResultInterface> remarks, JobMeta jobMeta, VariableSpace space, Repository repository,
+      IMetaStore metaStore ) {
+  }
 
 }
