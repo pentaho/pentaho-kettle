db833b512ebb262ab807ed2f4c0f1648095637df
Dzmitry <dima.prokopenko@gmail.com>
2014-03-20 13:58:00 +0300

    SP-1117 - Backport of PDI-11420 - Cannot reopen Mapping (sub-transformation) step after saving with blank value for fieldname to target step. (5.0)
    PDI-11420 - Cannot reopen Mapping (sub-transformation) step after saving
    with blank value for fieldname to target step.
    
    fix added.
    Avoid to set null for rename target/source values.
    If somehow this value already set to null - show empty String on UI to
    avoid NPE.
    
    junit test added

--- engine/src/org/pentaho/di/trans/steps/mapping/MappingValueRename.java
+++ engine/src/org/pentaho/di/trans/steps/mapping/MappingValueRename.java
@@ -23,62 +23,67 @@
 package org.pentaho.di.trans.steps.mapping;
 
 public class MappingValueRename implements Cloneable {
-	private String sourceValueName;
-	private String targetValueName;
+  private String sourceValueName;
+  private String targetValueName;
 
-	/**
-	 * @param sourceValueName
-	 * @param targetValueName
-	 */
-	public MappingValueRename(String sourceValueName, String targetValueName) {
-		super();
-		this.sourceValueName = sourceValueName;
-		this.targetValueName = targetValueName;
-	}
-	
-	@Override
-	public Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
-		return super.clone();
-	}
-	
-	@Override
-	public String toString() {
-		return sourceValueName+"-->"+targetValueName;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return sourceValueName.equals(obj);
-	}
-	
-	@Override
-	public int hashCode() {
-		return sourceValueName.hashCode();
-	}
-	
-	/**
-	 * @return the sourceValueName
-	 */
-	public String getSourceValueName() {
-		return sourceValueName;
-	}
-	/**
-	 * @param sourceValueName the sourceValueName to set
-	 */
-	public void setSourceValueName(String sourceValueName) {
-		this.sourceValueName = sourceValueName;
-	}
-	/**
-	 * @return the targetValueName
-	 */
-	public String getTargetValueName() {
-		return targetValueName;
-	}
-	/**
-	 * @param targetValueName the targetValueName to set
-	 */
-	public void setTargetValueName(String targetValueName) {
-		this.targetValueName = targetValueName;
-	}
+  /**
+   * @param sourceValueName
+   * @param targetValueName
+   */
+  public MappingValueRename( String sourceValueName, String targetValueName ) {
+    super();
+    this.sourceValueName = sourceValueName;
+    this.targetValueName = targetValueName;
+  }
+
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    // TODO Auto-generated method stub
+    return super.clone();
+  }
+
+  @Override
+  public String toString() {
+    return sourceValueName + "-->" + targetValueName;
+  }
+
+  @Override
+  public boolean equals( Object obj ) {
+    return sourceValueName.equals( obj );
+  }
+
+  @Override
+  public int hashCode() {
+    return sourceValueName.hashCode();
+  }
+
+  /**
+   * @return the sourceValueName
+   */
+  public String getSourceValueName() {
+    return sourceValueName;
+  }
+
+  /**
+   * @param sourceValueName
+   *          the sourceValueName to set. If null set to empty String
+   */
+  public void setSourceValueName( String sourceValueName ) {
+    this.sourceValueName = sourceValueName == null ? "" : sourceValueName;
+  }
+
+  /**
+   * @return the targetValueName
+   */
+  public String getTargetValueName() {
+    return targetValueName;
+  }
+
+  /**
+   * @param targetValueName
+   *          the targetValueName to set. If null set to empty String.
+   */
+  public void setTargetValueName( String targetValueName ) {
+    this.targetValueName = targetValueName == null ? "" : targetValueName;
+  }
 }
