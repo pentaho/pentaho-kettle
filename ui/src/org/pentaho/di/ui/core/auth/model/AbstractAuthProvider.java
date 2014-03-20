1504fdb1f26884ee38d0293b8f8ea9ef5e9fe5e4
Gretchen Moran <gmoran@pentaho.com>
2014-03-20 07:43:00 -0400

    PDI-10342 i18n, exception handling, logging, fixes for warnings, copyright

--- ui/src/org/pentaho/di/ui/core/auth/model/AbstractAuthProvider.java
+++ ui/src/org/pentaho/di/ui/core/auth/model/AbstractAuthProvider.java
@@ -1,27 +1,31 @@
-/*
- * !
- *  * This program is free software; you can redistribute it and/or modify it under the
- *  * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- *  * Foundation.
- *  *
- *  * You should have received a copy of the GNU Lesser General Public License along with this
- *  * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- *  * or from the Free Software Foundation, Inc.,
- *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *  *
- *  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  * See the GNU Lesser General Public License for more details.
- *  *
- *  * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+/*! ******************************************************************************
  *
- */
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 package org.pentaho.di.ui.core.auth.model;
 
 import org.pentaho.ui.xul.XulEventSourceAdapter;
 import org.pentaho.ui.xul.XulException;
 import org.pentaho.ui.xul.binding.Binding;
+import org.pentaho.ui.xul.binding.BindingException;
 import org.pentaho.ui.xul.binding.BindingFactory;
 
 import java.lang.reflect.InvocationTargetException;
@@ -53,7 +57,7 @@ public abstract class AbstractAuthProvider extends XulEventSourceAdapter
   }
 
   @Override
-  public void bind() {
+  public void bind() throws BindingException, XulException, InvocationTargetException {
 
     unbind();
     elementBindings = new ArrayList<Binding>();
@@ -80,33 +84,16 @@ public abstract class AbstractAuthProvider extends XulEventSourceAdapter
   }
 
   @Override
-  public void fireBindingsChanged() {
+  public void fireBindingsChanged() throws XulException, InvocationTargetException {
 
     for ( Binding bind : elementBindings ) {
-      try {
-        bind.fireSourceChanged();
-      } catch ( XulException e ) {
-        e.printStackTrace();
-      } catch ( InvocationTargetException e ) {
-        e.printStackTrace();
-      }
+      bind.fireSourceChanged();
     }
-
   }
 
   @Override
-  public AuthProvider clone() {
-
-    AuthProvider provider = null;
-
-    try {
-      provider = (AuthProvider) super.clone();
-      provider.bind();
-    } catch ( CloneNotSupportedException e ) {
-      e.printStackTrace();
-    }
-
-    return provider;
+  public AuthProvider clone() throws CloneNotSupportedException {
+    return (AuthProvider) super.clone();
   }
 
   public String toString() {
--- ui/test-src/org/pentaho/di/ui/core/auth/AuthHarness.java
+++ ui/test-src/org/pentaho/di/ui/core/auth/AuthHarness.java
@@ -1,24 +1,28 @@
-/*
- * !
- *  * This program is free software; you can redistribute it and/or modify it under the
- *  * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- *  * Foundation.
- *  *
- *  * You should have received a copy of the GNU Lesser General Public License along with this
- *  * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- *  * or from the Free Software Foundation, Inc.,
- *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *  *
- *  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  * See the GNU Lesser General Public License for more details.
- *  *
- *  * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+/*! ******************************************************************************
  *
- */
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 package org.pentaho.di.ui.core.auth;
 
+import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.ui.core.auth.controller.AuthProviderController;
 import org.pentaho.di.ui.core.auth.model.BasicAuthProvider;
 import org.pentaho.di.ui.core.auth.model.KerberosAuthProvider;
@@ -38,11 +42,13 @@ public class AuthHarness
   {
     try
     {
-      BindingFactory bf = new SwtBindingFactory();
-      AuthProviderController controller = new AuthProviderController(new SwtXulLoader(), bf, new SwtXulRunner());
-      //controller.setNewOverlay(new NoAuthAuthProvider( bf ));
-      controller.addProviders( getProviders(bf) );
-      controller.open();
+
+      KettleEnvironment.init();
+
+      AuthProviderDialog dialog = new AuthProviderDialog( null );
+      dialog.addProviders( getProviders(dialog.getBindingFactory()) );
+      dialog.show();
+      
     }
     catch (Exception e) {
       System.out.println(e.getMessage());
