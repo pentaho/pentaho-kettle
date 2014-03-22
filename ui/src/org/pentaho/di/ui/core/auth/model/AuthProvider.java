18d5abaee5934d4986e86aac1b58a3ab210f842b
Gretchen Moran <gmoran@pentaho.com>
2014-03-21 20:45:20 -0400

    PDI-10342 checkstyle

--- ui/src/org/pentaho/di/ui/core/auth/model/AuthProvider.java
+++ ui/src/org/pentaho/di/ui/core/auth/model/AuthProvider.java
@@ -1,23 +1,31 @@
-/*
- * !
- *  * This program is free software; you can redistribute it and/or modify it under the
- *  * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- *  * Foundation.
- *  *
- *  * You should have received a copy of the GNU Lesser General Public License along with this
- *  * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- *  * or from the Free Software Foundation, Inc.,
- *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *  *
- *  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  * See the GNU Lesser General Public License for more details.
- *  *
- *  * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+/*! ******************************************************************************
  *
- */
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 package org.pentaho.di.ui.core.auth.model;
+
+import org.pentaho.ui.xul.XulException;
+import org.pentaho.ui.xul.binding.BindingException;
+
+import java.lang.reflect.InvocationTargetException;
 
 public abstract interface AuthProvider extends Cloneable {
   public abstract String getPrincipal();
@@ -28,11 +36,11 @@ public abstract interface AuthProvider extends Cloneable {
 
   public abstract String getOverlay();
 
-  public void bind();
+  public void bind() throws BindingException, XulException, InvocationTargetException;
 
   public void unbind();
 
-  public AuthProvider clone();
+  public AuthProvider clone() throws CloneNotSupportedException;
 
-  public void fireBindingsChanged();
+  public void fireBindingsChanged() throws XulException, InvocationTargetException;
 }
