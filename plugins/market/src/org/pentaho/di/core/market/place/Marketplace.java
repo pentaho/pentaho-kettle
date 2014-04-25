fe3375bb527dfc62135de82eb7b374e905c6e6a2
mattcasters <mcasters@pentaho.org>
2014-04-25 13:11:30 +0200

    PDI-11615 - Keep code in line with Pentaho CheckStyle rules

--- plugins/market/src/org/pentaho/di/core/market/place/Marketplace.java
+++ plugins/market/src/org/pentaho/di/core/market/place/Marketplace.java
@@ -28,7 +28,6 @@ import java.util.List;
 import org.apache.batik.xml.XMLException;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
-import org.pentaho.di.core.market.place.Marketplace;
 import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.core.xml.XMLInterface;
 import org.w3c.dom.Node;
@@ -39,7 +38,7 @@ import org.w3c.dom.Node;
  * @author matt
  */
 public class Marketplace implements XMLInterface {
-	
+
   public static String XML_TAG = "marketplace";
 
   private String name;
@@ -52,18 +51,22 @@ public class Marketplace implements XMLInterface {
    * @param name
    * @param entriesUrl
    */
-  public Marketplace(String name, String entriesUrl) {
+  public Marketplace( String name, String entriesUrl ) {
     this();
     this.name = name;
     this.entriesUrl = entriesUrl;
   }
 
   @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof Marketplace)) return false;
-    if (obj == this) return true;
+  public boolean equals( Object obj ) {
+    if ( !( obj instanceof Marketplace ) ) {
+      return false;
+    }
+    if ( obj == this ) {
+      return true;
+    }
 
-    return ((Marketplace)obj).getName().equalsIgnoreCase(name);
+    return ( (Marketplace) obj ).getName().equalsIgnoreCase( name );
   }
 
   @Override
@@ -74,23 +77,23 @@ public class Marketplace implements XMLInterface {
   @Override
   public String getXML() throws KettleException {
     StringBuilder xml = new StringBuilder();
-    xml.append(XMLHandler.openTag(XML_TAG)).append(Const.CR);
-    xml.append(XMLHandler.addTagValue("name", name));
-    xml.append(XMLHandler.addTagValue("entries_url", entriesUrl));
-    xml.append(XMLHandler.closeTag(XML_TAG)).append(Const.CR);
+    xml.append( XMLHandler.openTag( XML_TAG ) ).append( Const.CR );
+    xml.append( XMLHandler.addTagValue( "name", name ) );
+    xml.append( XMLHandler.addTagValue( "entries_url", entriesUrl ) );
+    xml.append( XMLHandler.closeTag( XML_TAG ) ).append( Const.CR );
     return xml.toString();
   }
 
-  public Marketplace(Node node) throws XMLException {
+  public Marketplace( Node node ) throws XMLException {
     this();
-    name = XMLHandler.getTagValue(node, "name");
-    entriesUrl = XMLHandler.getTagValue(node, "entries_url");
+    name = XMLHandler.getTagValue( node, "name" );
+    entriesUrl = XMLHandler.getTagValue( node, "entries_url" );
   }
 
   public static final List<Marketplace> getDefaultMarketplaces() {
     List<Marketplace> marketplaces = new ArrayList<Marketplace>();
-    marketplaces.add( new Marketplace("Pentaho Data Integration Marketplace",
-            "https://raw.github.com/pentaho/marketplace-metadata/master/marketplace.xml"));
+    marketplaces.add( new Marketplace( "Pentaho Data Integration Marketplace",
+      "https://raw.github.com/pentaho/marketplace-metadata/master/marketplace.xml" ) );
     return marketplaces;
   }
 
@@ -100,24 +103,26 @@ public class Marketplace implements XMLInterface {
   public String getName() {
     return name;
   }
+
   /**
    * @param name the name to set
    */
-  public void setName(String name) {
+  public void setName( String name ) {
     this.name = name;
   }
+
   /**
    * @return the entriesUrl
    */
   public String getEntriesUrl() {
     return entriesUrl;
   }
+
   /**
    * @param entriesUrl the entriesUrl to set
    */
-  public void setEntriesUrl(String entriesUrl) {
+  public void setEntriesUrl( String entriesUrl ) {
     this.entriesUrl = entriesUrl;
   }
-
 
 }
