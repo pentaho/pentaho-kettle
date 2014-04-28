7ca18cc5be288126b6dac6442985e19307939bd3
Tatsiana Kasiankova <tatsiana_kasiankova@epam.com>
2014-04-28 11:27:06 +0300

    [SP-1230]: Backport of PDI-11928 - Unable to import repository if the file path to the import file contains Japanese character (5.0)
    There ia a fix.

--- engine/src/org/pentaho/di/repository/RepositoryExportSaxParser.java
+++ engine/src/org/pentaho/di/repository/RepositoryExportSaxParser.java
@@ -22,6 +22,8 @@
 
 package org.pentaho.di.repository;
 
+import java.io.File;
+
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -65,7 +67,8 @@ public class RepositoryExportSaxParser extends DefaultHandler2 {
     
     this.factory = SAXParserFactory.newInstance();
     this.saxParser = this.factory.newSAXParser();
-    this.saxParser.parse(filename, this);
+    String normalizedFileName = new File( filename ).toURI().toASCIIString();
+    this.saxParser.parse(normalizedFileName, this);
   }
   
   public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
