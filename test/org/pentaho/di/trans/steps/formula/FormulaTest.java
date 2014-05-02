130d6b33ba68c345e74c0efdbd16a542a63c5bb4
Dzmitry Bahdanovich <dzmitry_bahdanovich@epam.com>
2014-05-02 19:44:18 +0300

    PDI-10663 - Formula step: IF() crashes step (NPE) in case a NULL numeric field is returned

--- test/org/pentaho/di/trans/steps/formula/FormulaTest.java
+++ test/org/pentaho/di/trans/steps/formula/FormulaTest.java
@@ -16,6 +16,7 @@ import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.core.row.value.ValueMetaBigNumber;
 import org.pentaho.di.core.row.value.ValueMetaInteger;
+import org.pentaho.di.core.row.value.ValueMetaNumber;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.TransTestFactory;
 
@@ -34,11 +35,11 @@ public class FormulaTest {
 
   List<RowMetaAndData> getTestRowMetaAndData( int[] value ) {
     List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();
-    Object[] row = new Object[value.length];
+    Object[] row = new Object[ value.length ];
     RowMetaInterface rm = new RowMeta();
     for ( int i = 0; i < value.length; i++ ) {
-      rm.addValueMeta( new ValueMetaInteger( keys[i] ) );
-      row[i] = new Long( value[i] );
+      rm.addValueMeta( new ValueMetaInteger( keys[ i ] ) );
+      row[ i ] = new Long( value[ i ] );
     }
     list.add( new RowMetaAndData( rm, row ) );
     return list;
@@ -46,25 +47,35 @@ public class FormulaTest {
 
   List<RowMetaAndData> getTestRowMetaAndData( BigDecimal[] value ) {
     List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();
-    Object[] row = new Object[value.length];
+    Object[] row = new Object[ value.length ];
     RowMetaInterface rm = new RowMeta();
     for ( int i = 0; i < value.length; i++ ) {
-      rm.addValueMeta( new ValueMetaBigNumber( keys[i] ) );
-      row[i] = value[i];
+      rm.addValueMeta( new ValueMetaBigNumber( keys[ i ] ) );
+      row[ i ] = value[ i ];
     }
     list.add( new RowMetaAndData( rm, row ) );
     return list;
   }
 
+  List<RowMetaAndData> getTestRowMetaAndData() {
+    List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();
+    Object[] row = new Object[] { null };
+    RowMetaInterface rm = new RowMeta();
+    rm.addValueMeta( new ValueMetaNumber( "n" ) );
+    list.add( new RowMetaAndData( rm, row ) );
+    return list;
+  }
+
+
   /**
    * PDI-7923 - Formula step requires Number value type when could be used Integer. see transf_formula_error.ktr
-   * 
+   *
    * @throws KettleException
    */
   @Test
   public void testValueMetaIntegerConversion() throws KettleException {
     FormulaMetaFunction function =
-        new FormulaMetaFunction( KEY2, "[int_value]", ValueMetaInterface.TYPE_NUMBER, -1, -1, null );
+      new FormulaMetaFunction( KEY2, "[int_value]", ValueMetaInterface.TYPE_NUMBER, -1, -1, null );
 
     FormulaMeta meta = new FormulaMeta();
     meta.setFormula( new FormulaMetaFunction[] { function } );
@@ -72,8 +83,8 @@ public class FormulaTest {
     TransMeta transMeta = TransTestFactory.generateTestTransformation( null, meta, stepName );
     List<RowMetaAndData> inputList = getTestRowMetaAndData( new int[] { 13, 14 } );
     List<RowMetaAndData> ret =
-        TransTestFactory.executeTestTransformation( transMeta, TransTestFactory.INJECTOR_STEPNAME, stepName,
-            TransTestFactory.DUMMY_STEPNAME, inputList );
+      TransTestFactory.executeTestTransformation( transMeta, TransTestFactory.INJECTOR_STEPNAME, stepName,
+        TransTestFactory.DUMMY_STEPNAME, inputList );
     Assert.assertNotNull( "So we have some results", ret );
     Assert.assertEquals( "We have one result row", 1, ret.size() );
     RowMetaAndData rmd = ret.get( 0 );
@@ -81,20 +92,20 @@ public class FormulaTest {
     Assert.assertNotNull( resValueMeta );
 
     Assert.assertEquals( "It was Integer value meta and now it is Number", ValueMetaInterface.TYPE_NUMBER, resValueMeta
-        .getType() );
+      .getType() );
   }
 
   /**
    * PDI-7923 - Formula step requires Number value type when could be used Integer. see
    * sample-datagrid-truncating-numbers.ktr
-   * 
+   *
    * @throws KettleException
    */
   @Test
   public void testValueMetaTypeNotErased() throws KettleException {
     FormulaMetaFunction function =
-        new FormulaMetaFunction( KEY2, "max([" + KEY1 + "];[" + KEY2 + "])", ValueMetaInterface.TYPE_BIGNUMBER, -1, -1,
-            null );
+      new FormulaMetaFunction( KEY2, "max([" + KEY1 + "];[" + KEY2 + "])", ValueMetaInterface.TYPE_BIGNUMBER, -1, -1,
+        null );
     // Hope kettle is also uses MathContext correctly everywhere
     BigDecimal great = new BigDecimal( 999.00002, new MathContext( 7 ) );
     BigDecimal less = new BigDecimal( 999.00001, new MathContext( 7 ) );
@@ -106,8 +117,8 @@ public class FormulaTest {
 
     List<RowMetaAndData> inputList = getTestRowMetaAndData( new BigDecimal[] { less, great } );
     List<RowMetaAndData> ret =
-        TransTestFactory.executeTestTransformation( transMeta, TransTestFactory.INJECTOR_STEPNAME, stepName,
-            TransTestFactory.DUMMY_STEPNAME, inputList );
+      TransTestFactory.executeTestTransformation( transMeta, TransTestFactory.INJECTOR_STEPNAME, stepName,
+        TransTestFactory.DUMMY_STEPNAME, inputList );
     Assert.assertNotNull( "So we have some results", ret );
     Assert.assertEquals( "We have one result row", 1, ret.size() );
     RowMetaAndData rmd = ret.get( 0 );
@@ -116,7 +127,26 @@ public class FormulaTest {
 
     Assert.assertEquals( "It is still BinDecimal", ValueMetaInterface.TYPE_BIGNUMBER, resValueMeta.getType() );
     Assert.assertTrue( "So we have an a row with at least 3 not null objects", rmd.getData().length >= 3 );
-    Assert.assertEquals( "Grater value is choosen correctly", great, rmd.getData()[2] );
+    Assert.assertEquals( "Grater value is choosen correctly", great, rmd.getData()[ 2 ] );
+  }
+
+  @Test
+  public void testNullReturnValueConversion() throws Exception {
+    FormulaMetaFunction function =
+      new FormulaMetaFunction( "if", "IF(ISNA([n]);[n];[n])", ValueMetaInterface.TYPE_NUMBER, -1, -1,
+        null );
+
+    FormulaMeta meta = new FormulaMeta();
+    meta.setFormula( new FormulaMetaFunction[] { function } );
+
+    TransMeta transMeta = TransTestFactory.generateTestTransformation( null, meta, stepName );
+    List<RowMetaAndData> inputList = getTestRowMetaAndData();
+    try {
+      TransTestFactory.executeTestTransformation( transMeta, TransTestFactory.INJECTOR_STEPNAME, stepName,
+        TransTestFactory.DUMMY_STEPNAME, inputList );
+    } catch ( KettleException e ) {
+      Assert.fail( "Null is not handled correctly" );
+    }
   }
 
 }
