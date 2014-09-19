eef165adb79055ae52befe000a254297c1e6bc47
Dzmitry Prakapenka <dima.prokopenko@gmail.com>
2014-09-19 14:57:18 +0300

    SP-1428 - Backport of PDI-12318 - NullPointerException running job with massive parallel Job/Transformation loads (4.8)Backport of PDI-12318 - NullPointerException running job with massive parallel Job/Transformation loads (4.8)
    backport originall issue fix to 4.+ branch
    Fix 2 collections to respect concurrency.
    See PDI-12318 comments for details.

--- src/org/pentaho/di/job/Job.java
+++ src/org/pentaho/di/job/Job.java
@@ -29,8 +29,10 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -643,8 +645,8 @@ public class Job extends Thread implements VariableSpace, NamedParams, HasLogCha
     // Keep track of the results of these executions too.
     //
     final List<Thread> threads = new ArrayList<Thread>();
-    final List<Result> threadResults = new ArrayList<Result>();
-    final List<KettleException> threadExceptions = new ArrayList<KettleException>();
+    final Queue<Result> threadResults = new ConcurrentLinkedQueue<Result>();
+    final Queue<KettleException> threadExceptions = new ConcurrentLinkedQueue<KettleException>();
     final List<JobEntryCopy> threadEntries = new ArrayList<JobEntryCopy>();
 
     // Launch only those where the hop indicates true or false
@@ -784,7 +786,7 @@ public class Job extends Thread implements VariableSpace, NamedParams, HasLogCha
 			
 			// Now throw the first Exception for good measure...
 			//
-			throw threadExceptions.get(0);
+			throw threadExceptions.poll();
 		}
 		
 		// In parallel execution, we aggregate all the results, simply add them to the previous result...
