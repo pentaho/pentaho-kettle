69014f467b521e51f15182d5f3de29a2f8019925
Mikhail Tseu <mikhail_tseu@epam.com>
2014-09-24 14:18:40 +0100

    [BACKLOG-186] - Need to provide user with a decent message when the repository is not availabe
    changed error message according to UX feedback
    slightly changed logic for process launch of Repository Explorer failure
    added failure processing for JobDialog: extraTabs
    
    [BACKLOG-186] - Need to provide user with a decent message when the repository is not availabe
    
    change ctor of KettleRepositoryLostException to rely on super getMessage() and getLocalizedMessage()
    checkstyle
    
    [BACKLOG-186] - Need to provide user with a decent message when the repository is not availabe
    
    fix typo in the message
    
    [BACKLOG-186] - Need to provide user with a decent message when the repository is not availabe
    
    move the second message to the same package with repositoryLostException
    in order to avoid strings as package names while loading string resources

--- engine/src/org/pentaho/di/repository/KettleRepositoryLostException.java
+++ engine/src/org/pentaho/di/repository/KettleRepositoryLostException.java
@@ -24,12 +24,15 @@ package org.pentaho.di.repository;
 
 import org.pentaho.di.i18n.BaseMessages;
 
+@SuppressWarnings( "serial" )
 public class KettleRepositoryLostException extends RuntimeException {
-  
+
   private static Class<?> PKG = KettleRepositoryLostException.class;
+  private static final String MSG = BaseMessages.getString( PKG, "Repository.Lost.Error.Message" );
+  private static final String PREFACE = BaseMessages.getString( PKG, "Repository.Lost.Error.Preface" );
 
   public KettleRepositoryLostException() {
-    super();
+    super( MSG );
   }
 
   public KettleRepositoryLostException( String message ) {
@@ -37,27 +40,32 @@ public class KettleRepositoryLostException extends RuntimeException {
   }
 
   public KettleRepositoryLostException( Throwable cause ) {
-    super( cause );
+    super( MSG, cause );
   }
 
   public KettleRepositoryLostException( String message, Throwable cause ) {
     super( message, cause );
   }
-  
+
   public static KettleRepositoryLostException lookupStackStrace( Throwable root ) {
-    while( root != null ) {
-      if( root instanceof KettleRepositoryLostException ) {
-        return (KettleRepositoryLostException)root;
+    while ( root != null ) {
+      if ( root instanceof KettleRepositoryLostException ) {
+        return (KettleRepositoryLostException) root;
       } else {
         root = root.getCause();
       }
     }
-    
+
     return null;
   }
-  
-  @Override
-  public String getLocalizedMessage() {
-    return BaseMessages.getString( PKG, "Repository.Lost.Error.Text" );
+
+  /*
+   * According to UX the verbiage to be displayed to user
+   * should consist of 2 parts: 
+   * the first one is in Exception message
+   * the second one is in the Preface.
+   */
+  public String getPrefaceMessage() {
+    return PREFACE;
   }
 }
