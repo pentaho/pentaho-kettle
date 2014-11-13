52b1d8c31f7366e06b16c666f7c18986dc4c70cf
Dimitar Nedev <dnedev@users.noreply.github.com>
2014-11-13 11:51:03 +0100

    Fix [PDI-12278] MonetDB Bulk Loader error with MonetDB
    Fix MonetDB Bulk Loader error with MonetDB Jan 2014-SP2 bugfix release.
    Originally proposed by Juan Carlos.

--- engine/src/org/pentaho/di/trans/steps/monetdbbulkloader/MonetDBBulkLoader.java
+++ engine/src/org/pentaho/di/trans/steps/monetdbbulkloader/MonetDBBulkLoader.java
@@ -502,13 +502,6 @@ public class MonetDBBulkLoader extends BaseStep implements StepInterface {
       if ( error != null ) {
         throw new KettleException( "Error loading data: " + error );
       }
-      data.out.writeLine( "" );
-
-      // and again, making sure we commit all the records
-      error = data.in.waitForPrompt();
-      if ( error != null ) {
-        throw new KettleException( "Error loading data: " + error );
-      }
 
       if ( log.isRowLevel() ) {
         logRowlevel( Const.CR );
