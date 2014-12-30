8235619f4c74a9a84cf9768f3054e88e25615fa4
David Kincade <dkincade@pentaho.com>
2014-12-30 15:54:07 -0500

    Revert "BACKLOG-1621 Send kettle.properties across Carte cluster"

--- engine/src/org/pentaho/di/cluster/SlaveServer.java
+++ engine/src/org/pentaho/di/cluster/SlaveServer.java
@@ -34,7 +34,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
@@ -46,7 +45,6 @@ import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.lang.StringUtils;
-import org.apache.tools.ant.filters.StringInputStream;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.changed.ChangedFlag;
 import org.pentaho.di.core.encryption.Encr;
@@ -73,7 +71,6 @@ import org.pentaho.di.www.AddExportServlet;
 import org.pentaho.di.www.AllocateServerSocketServlet;
 import org.pentaho.di.www.CleanupTransServlet;
 import org.pentaho.di.www.GetJobStatusServlet;
-import org.pentaho.di.www.GetPropertiesServlet;
 import org.pentaho.di.www.GetSlavesServlet;
 import org.pentaho.di.www.GetStatusServlet;
 import org.pentaho.di.www.GetTransStatusServlet;
@@ -125,10 +122,6 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
   private String proxyPort;
 
   private String nonProxyHosts;
-  
-  private String propertiesMasterName;
-
-  private boolean overrideExistingProperties;
 
   private boolean master;
 
@@ -182,9 +175,6 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
     this.proxyHostname = XMLHandler.getTagValue( slaveNode, "proxy_hostname" );
     this.proxyPort = XMLHandler.getTagValue( slaveNode, "proxy_port" );
     this.nonProxyHosts = XMLHandler.getTagValue( slaveNode, "non_proxy_hosts" );
-    this.propertiesMasterName = XMLHandler.getTagValue( slaveNode, "get_properties_from_master" );
-    this.overrideExistingProperties =
-        "Y".equalsIgnoreCase( XMLHandler.getTagValue( slaveNode, "override_existing_properties" ) );
     this.master = "Y".equalsIgnoreCase( XMLHandler.getTagValue( slaveNode, "master" ) );
     initializeVariablesFrom( null );
     this.log = new LogChannel( this );
@@ -354,20 +344,6 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
   public void setProxyPort( String proxyPort ) {
     this.proxyPort = proxyPort;
   }
-  
-  /**
-   * @return the Master name for read properties
-   */
-  public String getPropertiesMasterName() {
-    return propertiesMasterName;
-  }
-
-  /**
-   * @return flag for read properties from Master
-   */
-  public boolean isOverrideExistingProperties() {
-    return overrideExistingProperties;
-  }
 
   public String getPortSpecification() {
     String realPort = environmentSubstitute( port );
@@ -380,7 +356,7 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
 
   public String constructUrl( String serviceAndArguments ) throws UnsupportedEncodingException {
     String realHostname = environmentSubstitute( hostname );
-    if ( !StringUtils.isBlank( webAppName ) ) {
+    if ( !StringUtils.isEmpty( webAppName ) ) {
       serviceAndArguments = "/" + environmentSubstitute( getWebAppName() ) + serviceAndArguments;
     }
     String retval = "http://" + realHostname + getPortSpecification() + serviceAndArguments;
@@ -767,14 +743,6 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
       execService( CleanupTransServlet.CONTEXT_PATH + "/?name=" + URLEncoder.encode( transName, "UTF-8" ) + "&id="
         + Const.NVL( clusteredRunId, "" ) + "&xml=Y&sockets=Y" );
     return WebResult.fromXMLString( xml );
-  }
-  
-  public Properties getKettleProperties() throws Exception {
-    String xml = execService( GetPropertiesServlet.CONTEXT_PATH + "/?xml=Y" );
-    StringInputStream in = new StringInputStream( xml );
-    Properties properties = new Properties();
-    properties.loadFromXML( in );
-    return properties;
   }
 
   public static SlaveServer findSlaveServer( List<SlaveServer> slaveServers, String name ) {
