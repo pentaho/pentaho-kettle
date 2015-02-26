11c95f7214f4637019016d46a6a532ea007131a3
mbatchelor <mbatchelor@pentaho.com>
2015-02-26 11:37:01 -0500

    [SP-1732]/[PDI-13344] - Fix copyright year

--- ui/src/org/pentaho/di/ui/spoon/Spoon.java
+++ ui/src/org/pentaho/di/ui/spoon/Spoon.java
@@ -5444,10 +5444,11 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
     }
     return saved;
   }
-
-  public void helpAbout() {
-    MessageBox mb = new MessageBox(shell, SWT.OK | SWT.ICON_INFORMATION | SWT.CENTER | SWT.SHEET);
-
+  
+  // Put here to support testing, but you cannot instance Spoon from a
+  // JUnit test case without a bunch of work. I abandoned the attempt to
+  // create a Spoon test case to test this. mb
+  public StringBuilder getHelpAboutText() {
     //  resolve the release text
     String releaseText = "";
     if (Const.RELEASE.equals(Const.ReleaseType.PREVIEW)) {
@@ -5459,7 +5460,7 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
     } else if (Const.RELEASE.equals(Const.ReleaseType.GA)) {
       releaseText = BaseMessages.getString(PKG, "Spoon.GA.HelpAboutText");
     }
-
+    
     //  build a message
     StringBuilder messageBuilder = new StringBuilder();
 
@@ -5470,7 +5471,8 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
     messageBuilder.append(Const.CR);
     messageBuilder.append(Const.CR);
     messageBuilder.append(Const.CR);
-    messageBuilder.append(BaseMessages.getString(PKG, "System.CompanyInfo", Const.COPYRIGHT_YEAR));
+    messageBuilder.append(BaseMessages.getString(PKG, "System.CompanyInfo",
+        ""+((new Date()).getYear()+1900) ) );
     messageBuilder.append(Const.CR);
     messageBuilder.append("         ");
     messageBuilder.append(BaseMessages.getString(PKG, "System.ProductWebsiteUrl"));
@@ -5485,7 +5487,13 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
     messageBuilder.append(BuildVersion.getInstance().getBuildDate()); //  this should be the longest line of text
     messageBuilder.append("     "); //  so this is the right margin 
     messageBuilder.append(Const.CR);
+    return messageBuilder;
+  }
 
+  public void helpAbout() {
+    MessageBox mb = new MessageBox(shell, SWT.OK | SWT.ICON_INFORMATION | SWT.CENTER | SWT.SHEET);
+
+    StringBuilder messageBuilder = getHelpAboutText();
     //  set the text in the message box
     mb.setMessage(messageBuilder.toString());
     mb.setText(APP_NAME);
