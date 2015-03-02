233d34e3382b6c47ec9a0a7458438716bcedd0c5
Alexander Buloichik <alexander_buloichik@epam.com>
2015-03-02 15:09:20 +0300

    [BACKLOG-2208] Base SVG support classes

---
+++ ui/src/org/pentaho/di/core/SwtUniversalImage.java
@@ -0,0 +1,155 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.core;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.pentaho.di.ui.svg.SvgImage;
+
+/**
+ * Universal image storage. It contains SVG or bitmap image depends on file and settings.
+ */
+public class SwtUniversalImage {
+  private final SvgImage svg;
+  private final Image bitmap;
+
+  private Map<String, Image> cache = new TreeMap<String, Image>();
+
+  public SwtUniversalImage( SvgImage svg ) {
+    this.svg = svg;
+    this.bitmap = null;
+  }
+
+  public SwtUniversalImage( Image bitmap ) {
+    this.svg = null;
+    this.bitmap = bitmap;
+  }
+
+  public synchronized void dispose() {
+    if ( cache == null ) {
+      return;
+    }
+    for ( Image img : cache.values() ) {
+      if ( !img.isDisposed() ) {
+        img.dispose();
+      }
+    }
+    if ( bitmap != null && !bitmap.isDisposed() ) {
+      bitmap.dispose();
+    }
+    cache = null;
+  }
+
+  private void checkDisposed() {
+    if ( cache == null ) {
+      throw new RuntimeException( "Already disposed" );
+    }
+  }
+
+  public synchronized Image getAsBitmap( Device device ) {
+    checkDisposed();
+
+    Image result = cache.get( "" );
+
+    if ( result != null && !result.isDisposed() ) {
+      return result;
+    }
+
+    if ( svg != null ) {
+      result = renderToBitmap( device, svg );
+    } else {
+      result = bitmap;
+    }
+    cache.put( "", result );
+    return result;
+  }
+
+  public synchronized Image getAsBitmapForSize( Device device, int width, int height ) {
+    checkDisposed();
+
+    String key = width + "x" + height;
+    Image result = cache.get( key );
+    if ( result != null && !result.isDisposed() ) {
+      return result;
+    }
+
+    if ( svg != null ) {
+      result = renderToBitmap( device, svg, width, height );
+    } else {
+      int xsize = bitmap.getBounds().width;
+      int ysize = bitmap.getBounds().height;
+      result = new Image( device, width, height );
+      GC gc = new GC( result );
+      gc.drawImage( bitmap, 0, 0, xsize, ysize, 0, 0, width, height );
+      gc.dispose();
+    }
+    cache.put( key, result );
+    return result;
+  }
+
+  /**
+   * Convert SVG image to swt Image.
+   */
+  private static Image renderToBitmap( Device device, SvgImage svg ) {
+    PNGTranscoder tr = new PNGTranscoder();
+    TranscoderInput input = new TranscoderInput( svg.getDocument() );
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    TranscoderOutput output = new TranscoderOutput( out );
+    try {
+      tr.transcode( input, output );
+    } catch ( Exception ex ) {
+      throw new RuntimeException( ex );
+    }
+
+    return new Image( device, new ByteArrayInputStream( out.toByteArray() ) );
+  }
+
+  /**
+   * Convert SVG image to swt Image with specified size.
+   */
+  private static Image renderToBitmap( Device device, SvgImage svg, int width, int height ) {
+    PNGTranscoder tr = new PNGTranscoder();
+    tr.addTranscodingHint( PNGTranscoder.KEY_WIDTH, (float) width );
+    tr.addTranscodingHint( PNGTranscoder.KEY_HEIGHT, (float) height );
+
+    TranscoderInput input = new TranscoderInput( svg.getDocument() );
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    TranscoderOutput output = new TranscoderOutput( out );
+    try {
+      tr.transcode( input, output );
+    } catch ( Exception ex ) {
+      throw new RuntimeException( ex );
+    }
+
+    return new Image( device, new ByteArrayInputStream( out.toByteArray() ) );
+  }
+}
---
+++ ui/src/org/pentaho/di/ui/svg/SvgImage.java
@@ -0,0 +1,35 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2015 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.di.ui.svg;
+
+import org.w3c.dom.Document;
+
+/**
+ * Container for SVG image.
+ */
+public class SvgImage {
+  private final Document document;
+
+  protected SvgImage( Document doc ) {
+    this.document = doc;
+  }
+
+  public Document getDocument() {
+    return document;
+  }
+}
