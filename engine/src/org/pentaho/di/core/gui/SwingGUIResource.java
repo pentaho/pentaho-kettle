bb39f814ab03c6262262fe721ce345fc3b8397ff
Alexander Buloichik <alexander_buloichik@epam.com>
2015-03-09 11:11:07 +0300

    [BACKLOG-2269] SVG export to pdf as bitmaps

--- engine/src/org/pentaho/di/core/gui/SwingGUIResource.java
+++ engine/src/org/pentaho/di/core/gui/SwingGUIResource.java
@@ -31,6 +31,8 @@ import java.util.Map;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.io.IOUtils;
+import org.pentaho.di.core.SwingUniversalImage;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.logging.LogChannel;
 import org.pentaho.di.core.logging.LogChannelInterface;
@@ -38,6 +40,8 @@ import org.pentaho.di.core.plugins.JobEntryPluginType;
 import org.pentaho.di.core.plugins.PluginInterface;
 import org.pentaho.di.core.plugins.PluginRegistry;
 import org.pentaho.di.core.plugins.StepPluginType;
+import org.pentaho.di.core.svg.SvgImage;
+import org.pentaho.di.core.svg.SvgSupport;
 import org.pentaho.reporting.libraries.base.util.WaitingImageObserver;
 
 public class SwingGUIResource {
@@ -45,8 +49,8 @@ public class SwingGUIResource {
 
   private static SwingGUIResource instance;
 
-  private Map<String, BufferedImage> stepImages;
-  private Map<String, BufferedImage> entryImages;
+  private Map<String, SwingUniversalImage> stepImages;
+  private Map<String, SwingUniversalImage> entryImages;
 
   private SwingGUIResource() throws KettleException {
     this.stepImages = loadStepImages();
@@ -60,31 +64,26 @@ public class SwingGUIResource {
     return instance;
   }
 
-  private Map<String, BufferedImage> loadStepImages() throws KettleException {
-    Map<String, BufferedImage> map = new HashMap<String, BufferedImage>();
+  private Map<String, SwingUniversalImage> loadStepImages() throws KettleException {
+    Map<String, SwingUniversalImage> map = new HashMap<String, SwingUniversalImage>();
 
     for ( PluginInterface plugin : PluginRegistry.getInstance().getPlugins( StepPluginType.class ) ) {
       try {
-        BufferedImage image = getImageIcon( plugin );
+        SwingUniversalImage image = getUniversalImageIcon( plugin );
         for ( String id : plugin.getIds() ) {
           map.put( id, image );
         }
       } catch ( Exception e ) {
         log.logError( "Unable to load step icon image for plugin: "
           + plugin.getName() + " (id=" + plugin.getIds()[0], e );
-        try {
-          getImageIcon( plugin );
-        } catch ( Exception ex ) {
-          //
-        }
       }
     }
 
     return map;
   }
 
-  private Map<String, BufferedImage> loadEntryImages() throws KettleException {
-    Map<String, BufferedImage> map = new HashMap<String, BufferedImage>();
+  private Map<String, SwingUniversalImage> loadEntryImages() throws KettleException {
+    Map<String, SwingUniversalImage> map = new HashMap<String, SwingUniversalImage>();
 
     for ( PluginInterface plugin : PluginRegistry.getInstance().getPlugins( JobEntryPluginType.class ) ) {
       try {
@@ -92,22 +91,10 @@ public class SwingGUIResource {
           continue;
         }
 
-        String imageFile = plugin.getImageFile();
-        if ( imageFile == null ) {
-          throw new KettleException( "No image file (icon) specified for plugin: " + plugin );
-        }
-
-        BufferedImage image = getImageIcon( plugin );
+        SwingUniversalImage image = getUniversalImageIcon( plugin );
         if ( image == null ) {
           throw new KettleException( "Unable to find image file: "
             + plugin.getImageFile() + " for plugin: " + plugin );
-        }
-        if ( image.getHeight( null ) < 0 ) {
-          image = getImageIcon( plugin );
-          if ( image == null || image.getHeight( null ) < 0 ) {
-            throw new KettleException( "Unable to load image file: "
-              + plugin.getImageFile() + " for plugin: " + plugin );
-          }
         }
 
         map.put( plugin.getIds()[0], image );
@@ -118,6 +105,99 @@ public class SwingGUIResource {
     }
 
     return map;
+  }
+
+  private SwingUniversalImage getUniversalImageIcon( PluginInterface plugin ) throws KettleException {
+    try {
+      PluginRegistry registry = PluginRegistry.getInstance();
+      String filename = plugin.getImageFile();
+
+      ClassLoader classLoader = registry.getClassLoader( plugin );
+
+      SwingUniversalImage image = null;
+
+      if ( SvgSupport.isSvgEnabled() && SvgSupport.isSvgName( filename ) ) {
+        // Try to use the plugin class loader to get access to the icon
+        //
+        InputStream inputStream = classLoader.getResourceAsStream( filename );
+        if ( inputStream == null ) {
+          inputStream = classLoader.getResourceAsStream( "/" + filename );
+        }
+        // Try to use the PDI class loader to get access to the icon
+        //
+        if ( inputStream == null ) {
+          inputStream = registry.getClass().getResourceAsStream( filename );
+        }
+        if ( inputStream == null ) {
+          inputStream = registry.getClass().getResourceAsStream( "/" + filename );
+        }
+        // As a last resort, try to use the standard file-system
+        //
+        if ( inputStream == null ) {
+          try {
+            inputStream = new FileInputStream( filename );
+          } catch ( FileNotFoundException e ) {
+            // Ignore, throws error below
+          }
+        }
+        if ( inputStream != null ) {
+          try {
+            SvgImage svg = SvgSupport.loadSvgImage( inputStream );
+            image = new SwingUniversalImage( svg );
+          } finally {
+            IOUtils.closeQuietly( inputStream );
+          }
+        }
+      }
+
+      if ( image == null ) {
+        filename = SvgSupport.toPngName( filename );
+        // Try to use the plugin class loader to get access to the icon
+        //
+        InputStream inputStream = classLoader.getResourceAsStream( filename );
+        if ( inputStream == null ) {
+          inputStream = classLoader.getResourceAsStream( "/" + filename );
+        }
+        // Try to use the PDI class loader to get access to the icon
+        //
+        if ( inputStream == null ) {
+          inputStream = registry.getClass().getResourceAsStream( filename );
+        }
+        if ( inputStream == null ) {
+          inputStream = registry.getClass().getResourceAsStream( "/" + filename );
+        }
+        // As a last resort, try to use the standard file-system
+        //
+        if ( inputStream == null ) {
+          try {
+            inputStream = new FileInputStream( filename );
+          } catch ( FileNotFoundException e ) {
+            // Ignore, throws error below
+          }
+        }
+        if ( inputStream != null ) {
+          try {
+            BufferedImage bitmap = ImageIO.read( inputStream );
+
+            WaitingImageObserver wia = new WaitingImageObserver( bitmap );
+            wia.waitImageLoaded();
+
+            image = new SwingUniversalImage( bitmap );
+          } finally {
+            IOUtils.closeQuietly( inputStream );
+          }
+        }
+      }
+
+      if ( image == null ) {
+        throw new KettleException( "Unable to find file: " + plugin.getImageFile() + " for plugin: " + plugin );
+      }
+
+      return image;
+    } catch ( Throwable e ) {
+      throw new KettleException( "Unable to load image from file : '"
+        + plugin.getImageFile() + "' for plugin: " + plugin, e );
+    }
   }
 
   private BufferedImage getImageIcon( PluginInterface plugin ) throws KettleException {
@@ -168,11 +248,11 @@ public class SwingGUIResource {
     }
   }
 
-  public Map<String, BufferedImage> getEntryImages() {
+  public Map<String, SwingUniversalImage> getEntryImages() {
     return entryImages;
   }
 
-  public Map<String, BufferedImage> getStepImages() {
+  public Map<String, SwingUniversalImage> getStepImages() {
     return stepImages;
   }
 }
