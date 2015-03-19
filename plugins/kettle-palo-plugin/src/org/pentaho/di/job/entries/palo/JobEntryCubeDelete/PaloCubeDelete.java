13e9bd403b1eb3a667a6d2ec69889805fb9dc3aa
mdamour1976 <mdamour1976@gmail.com>
2015-03-19 15:11:17 -0400

    Updates for incorrect/missing icons/new, linux/windows/osx tweaks

--- plugins/kettle-palo-plugin/src/org/pentaho/di/job/entries/palo/JobEntryCubeDelete/PaloCubeDelete.java
+++ plugins/kettle-palo-plugin/src/org/pentaho/di/job/entries/palo/JobEntryCubeDelete/PaloCubeDelete.java
@@ -46,7 +46,7 @@ import org.w3c.dom.Node;
  */
 
 @org.pentaho.di.core.annotations.JobEntry( id = "PALO_CUBE_DELETE",
-    i18nPackageName = "org.pentaho.di.job.entries.palo.JobEntryCubeDelete", image = "PaloCubeDelete.png",
+    i18nPackageName = "org.pentaho.di.job.entries.palo.JobEntryCubeDelete", image = "PaloCubeDelete.svg",
     name = "PaloCubeDelete.JobName", description = "PaloCubeDelete.JobDescription",
     categoryDescription = "i18n:org.pentaho.di.job:JobCategory.Category.Palo" )
 public class PaloCubeDelete extends JobEntryBase implements Cloneable, JobEntryInterface {
