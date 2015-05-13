46cc35dac23699ea113b083420ef119a0814791b
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-05-13 11:36:00 +0400

    [SP-1830] - Backport of PDI-12749 - PMR: Bad Results in Group-By if an entire key is filtered earlier (5.3 Suite) - fix test (cherry picked from commit ca42d40)

--- test/org/pentaho/di/trans/WordCountSampleTest.java
+++ test/org/pentaho/di/trans/WordCountSampleTest.java
@@ -25,8 +25,8 @@ package org.pentaho.di.trans;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.TestCase;
-
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.RowMetaAndData;
@@ -40,12 +40,19 @@ import org.pentaho.di.trans.TransMeta.TransformationType;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMetaDataCombi;
 
-public class WordCountSingleThreadTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-  public void testWordCountMapper() throws Exception {
+public class WordCountSampleTest {
 
+  @BeforeClass
+  public static void setUp() throws Exception {
     KettleEnvironment.init();
+  }
 
+
+  @Test
+  public void testWordCountMapper() throws Exception {
     //
     // Create a new transformation...
     //
@@ -147,118 +154,35 @@ public class WordCountSingleThreadTest extends TestCase {
     System.out.println( "Average speed: " + rowsPerSecond + " rows/second" );
   }
 
+  @Test
   public void testWordCountReducer() throws Exception {
-
-    KettleEnvironment.init();
-
-    //
-    // Create a new transformation...
-    //
     TransMeta transMeta = new TransMeta( "testfiles/wordcount-reducer.ktr" );
-    transMeta.setTransformationType( TransformationType.SingleThreaded );
 
-    long transStart = System.currentTimeMillis();
-
-    // Now execute the transformation...
     Trans trans = new Trans( transMeta );
-    trans.setLogLevel( LogLevel.MINIMAL );
-
     trans.prepareExecution( null );
 
-    StepInterface si = trans.getStepInterface( "Output", 0 );
-    RowStepCollector rc = new RowStepCollector();
-    si.addRowListener( rc );
-
     RowProducer rp = trans.addRowProducer( "Injector", 0 );
-    trans.startThreads();
-
-    String metricsStep = "Injector";
-
-    // The single threaded transformation type expects us to run the steps
-    // ourselves.
-    //
-    SingleThreadedTransExecutor executor = new SingleThreadedTransExecutor( trans );
-
-    // Initialize all steps
-    //
-    executor.init();
-
-    int iterations = 1000000;
-    long totalWait = 0;
     List<RowMetaAndData> inputList = createReducerData();
-
-    for ( int i = 0; i < iterations; i++ ) {
-      // add rows
-      for ( RowMetaAndData rm : inputList ) {
-        Object[] copy = rm.getRowMeta().cloneRow( rm.getData() );
-        rp.putRow( rm.getRowMeta(), copy );
-      }
-
-      long start = System.currentTimeMillis();
-
-      boolean cont = executor.oneIteration();
-      if ( !cont ) {
-        fail( "We don't expect any step or the transformation to be done before the end of all iterations." );
-      }
-
-      long end = System.currentTimeMillis();
-      long delay = end - start;
-      totalWait += delay;
-      if ( i > 0 && ( i % 100000 ) == 0 ) {
-        long rowsProcessed = trans.findRunThread( metricsStep ).getLinesRead();
-        double speed = Const.round( ( rowsProcessed ) / ( (double) ( end - transStart ) / 1000 ), 1 );
-        int totalRows = 0;
-        for ( StepMetaDataCombi combi : trans.getSteps() ) {
-          for ( RowSet rowSet : combi.step.getInputRowSets() ) {
-            totalRows += rowSet.size();
-          }
-          for ( RowSet rowSet : combi.step.getOutputRowSets() ) {
-            totalRows += rowSet.size();
-          }
-        }
-        System.out.println( "#"
-          + i + " : Finished processing one iteration in " + delay + "ms, average is: "
-          + Const.round( ( (double) totalWait / ( i + 1 ) ), 1 ) + ", speed=" + speed
-          + " row/s, total rows buffered: " + totalRows );
-      }
-
-      List<RowMetaAndData> resultRows = rc.getRowsWritten();
-
-      // The "group by" step reduces the amount of rows from 6 to 4.
-      //
-      assertEquals( "Error found in iteration " + i + " : not the expected amount of output rows.", 4, resultRows
-        .size() );
-      rc.clear();
+    for ( RowMetaAndData rm : inputList ) {
+      Object[] copy = rm.getRowMeta().cloneRow( rm.getData() );
+      rp.putRow( rm.getRowMeta(), copy );
     }
-
     rp.finished();
 
-    // Dispose all steps.
-    //
-    executor.dispose();
+    trans.startThreads();
+    trans.waitUntilFinished();
 
-    long rowsProcessed = trans.findRunThread( metricsStep ).getLinesRead();
-
-    long transEnd = System.currentTimeMillis();
-    long transTime = transEnd - transStart;
-    System.out.println( "Average delay before idle : " + Const.round( ( (double) totalWait / iterations ), 1 ) );
-    double transTimeSeconds = Const.round( ( (double) transTime / 1000 ), 1 );
-    System.out.println( "Total transformation runtime for "
-      + iterations + " iterations :" + transTimeSeconds + " seconds" );
-    double transTimePerIteration = Const.round( ( (double) transTime / iterations ), 2 );
-    System.out.println( "Runtime per iteration: " + transTimePerIteration + " miliseconds" );
-    double rowsPerSecond = Const.round( ( rowsProcessed ) / ( (double) transTime / 1000 ), 1 );
-    System.out.println( "Average speed: " + rowsPerSecond + " rows/second" );
+    assertEquals( "Reducer should execute without errors", 0, trans.getErrors() );
   }
 
   public RowMetaInterface createMapperRowMetaInterface() {
     RowMetaInterface rm = new RowMeta();
 
     ValueMetaInterface[] valuesMeta =
-    { new ValueMeta( "key", ValueMeta.TYPE_STRING ), new ValueMeta( "value", ValueMeta.TYPE_STRING ), };
+      { new ValueMeta( "key", ValueMeta.TYPE_STRING ), new ValueMeta( "value", ValueMeta.TYPE_STRING ), };
 
     for ( int i = 0; i < valuesMeta.length; i++ ) {
-      rm.addValueMeta( valuesMeta[i] );
+      rm.addValueMeta( valuesMeta[ i ] );
     }
 
     return rm;
@@ -275,20 +199,20 @@ public class WordCountSingleThreadTest extends TestCase {
     return list;
   }
 
-  public RowMetaInterface createReducerRowMetaInterface() {
+  private RowMetaInterface createReducerRowMetaInterface() {
     RowMetaInterface rm = new RowMeta();
 
     ValueMetaInterface[] valuesMeta =
-    { new ValueMeta( "key", ValueMeta.TYPE_STRING ), new ValueMeta( "value", ValueMeta.TYPE_INTEGER ), };
+      { new ValueMeta( "key", ValueMeta.TYPE_STRING ), new ValueMeta( "value", ValueMeta.TYPE_INTEGER ), };
 
-    for ( int i = 0; i < valuesMeta.length; i++ ) {
-      rm.addValueMeta( valuesMeta[i] );
+    for ( ValueMetaInterface aValuesMeta : valuesMeta ) {
+      rm.addValueMeta( aValuesMeta );
     }
 
     return rm;
   }
 
-  public List<RowMetaAndData> createReducerData() {
+  private List<RowMetaAndData> createReducerData() {
     List<RowMetaAndData> list = new ArrayList<RowMetaAndData>();
 
     RowMetaInterface rm = createReducerRowMetaInterface();
