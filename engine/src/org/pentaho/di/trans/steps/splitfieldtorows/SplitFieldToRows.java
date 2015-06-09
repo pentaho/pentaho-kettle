31f1bcd624b87a92e43122a3fc6f2c90ba1be30e
Sergey_Travin <Sergey_Travin@epam.com>
2015-06-09 15:03:46 +0400

    [PDI-11477] Split field to rows step omit trailing empty values

--- engine/src/org/pentaho/di/trans/steps/splitfieldtorows/SplitFieldToRows.java
+++ engine/src/org/pentaho/di/trans/steps/splitfieldtorows/SplitFieldToRows.java
@@ -101,8 +101,8 @@ public class SplitFieldToRows extends BaseStep implements StepInterface {
     if ( meta.includeRowNumber() && meta.resetRowNumber() ) {
       data.rownr = 1L;
     }
-
-    String[] splitStrings = data.delimiterPattern.split( originalString );
+    // use -1 for include all strings. see http://jira.pentaho.com/browse/PDI-11477
+    String[] splitStrings = data.delimiterPattern.split( originalString, -1 );
     for ( String string : splitStrings ) {
       Object[] outputRow = RowDataUtil.createResizedCopy( rowData, data.outputRowMeta.size() );
       outputRow[rowMeta.size()] = string;
