0bcdd417ed5249645471a7dc63ab88d641e3d3ca
Michael D'Amour <mdamour1976@gmail.com>
2015-07-10 14:11:14 -0400

    [BACKLOG-3906] Spoon Error Messages in Ubuntu 14.04

--- assembly/package-res/spoon.sh
+++ assembly/package-res/spoon.sh
@@ -206,7 +206,12 @@ OPT="$OPT $PENTAHO_DI_JAVA_OPTIONS -Djava.library.path=$LIBPATH -DKETTLE_HOME=$K
 # ***************
 # ** Run...    **
 # ***************
-"$_PENTAHO_JAVA" $OPT -jar "$STARTUP" -lib $LIBPATH "${1+$@}"
+OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+if [ $OS = "linux" ]; then
+    "$_PENTAHO_JAVA" $OPT -jar "$STARTUP" -lib $LIBPATH "${1+$@}" 2>&1 | grep -viE "Gtk-WARNING|GLib-GObject|GLib-CRITICAL|^$"
+else
+    "$_PENTAHO_JAVA" $OPT -jar "$STARTUP" -lib $LIBPATH "${1+$@}"
+fi
 
 # return to the catalog from which spoon.sh has been started
 cd $INITIALDIR
