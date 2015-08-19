9c43023277ca54d8022fd15fbd941312de9ab87a
Jesse Adametz <jesse@graphiq.com>
2015-08-19 13:56:10 -0700

    [PDI-14253] Text file input: adding 'Minimal width' button to Fields tab

--- ui/src/org/pentaho/di/ui/trans/steps/textfileinput/TextFileInputDialog.java
+++ ui/src/org/pentaho/di/ui/trans/steps/textfileinput/TextFileInputDialog.java
@@ -68,6 +68,7 @@ import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.fileinput.FileInputList;
 import org.pentaho.di.core.gui.TextFileInputFieldInterface;
 import org.pentaho.di.core.row.ValueMeta;
+import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.core.util.EnvUtil;
 import org.pentaho.di.core.vfs.KettleVFS;
 import org.pentaho.di.i18n.BaseMessages;
@@ -360,6 +361,9 @@ public class TextFileInputDialog extends BaseStepDialog implements StepDialogInt
 
   private TextFileInputMeta input;
 
+  private Button wMinWidth;
+  private Listener lsMinWidth;
+
   // Wizard info...
   private Vector<TextFileInputFieldInterface> fields;
 
@@ -533,6 +537,11 @@ public class TextFileInputDialog extends BaseStepDialog implements StepDialogInt
         preview();
       }
     };
+    lsMinWidth = new Listener() {
+      public void handleEvent( Event e ) {
+        setMinimalWidth();
+      }
+    };
     lsCancel = new Listener() {
       public void handleEvent( Event e ) {
         cancel();
@@ -543,6 +552,7 @@ public class TextFileInputDialog extends BaseStepDialog implements StepDialogInt
     wFirst.addListener( SWT.Selection, lsFirst );
     wFirstHeader.addListener( SWT.Selection, lsFirstHeader );
     wGet.addListener( SWT.Selection, lsGet );
+    wMinWidth.addListener( SWT.Selection, lsMinWidth );
     wPreview.addListener( SWT.Selection, lsPreview );
     wCancel.addListener( SWT.Selection, lsCancel );
 
@@ -2040,6 +2050,16 @@ public class TextFileInputDialog extends BaseStepDialog implements StepDialogInt
     fdGet.bottom = new FormAttachment( 100, 0 );
     wGet.setLayoutData( fdGet );
 
+    wMinWidth = new Button( wFieldsComp, SWT.PUSH );
+    wMinWidth.setText( BaseMessages.getString( PKG, "TextFileInputDialog.MinWidth.Button" ) );
+    wMinWidth.setToolTipText( BaseMessages.getString( PKG, "TextFileInputDialog.MinWidth.Tooltip" ) );
+    wMinWidth.addSelectionListener( new SelectionAdapter() {
+      public void widgetSelected ( SelectionEvent e ) {
+        input.setChanged();
+      }
+    } );
+    setButtonPositions( new Button[] { wGet, wMinWidth }, margin, null );
+
     final int FieldsRows = input.getInputFields().length;
 
     ColumnInfo[] colinf =
@@ -3075,6 +3095,44 @@ public class TextFileInputDialog extends BaseStepDialog implements StepDialogInt
     Collections.sort( fields );
 
     return fields;
+  }
+
+  /**
+   * Sets the input width to minimal width...
+   *
+   */
+  public void setMinimalWidth() {
+    int nrNonEmptyFields = wFields.nrNonEmpty();
+    for ( int i = 0; i < nrNonEmptyFields; i++ ) {
+      TableItem item = wFields.getNonEmpty( i );
+
+      item.setText( 5, "" );
+      item.setText( 6, "" );
+      item.setText( 12, ValueMeta.getTrimTypeDesc( ValueMetaInterface.TRIM_TYPE_BOTH ) );
+
+      int type = ValueMeta.getType( item.getText( 2 ) );
+      switch ( type ) {
+        case ValueMetaInterface.TYPE_STRING:
+          item.setText( 3, "" );
+          break;
+        case ValueMetaInterface.TYPE_INTEGER:
+          item.setText( 3, "0" );
+          break;
+        case ValueMetaInterface.TYPE_NUMBER:
+          item.setText( 3, "0.#####" );
+          break;
+        case ValueMetaInterface.TYPE_DATE:
+          break;
+        default:
+          break;
+      }
+    }
+
+    for ( int i = 0; i < input.getInputFields().length; i++ ) {
+      input.getInputFields()[i].setTrimType( ValueMetaInterface.TRIM_TYPE_BOTH );
+    }
+
+    wFields.optWidth( true );
   }
 
   private void addAdditionalFieldsTab() {
