da5de12e098e3f6d1f8de9d8ea9d8e19c0092db4
Andrew FTB <aoverton@findthebest.com>
2015-09-04 23:36:36 -0400

    Fixing failing test for PDI-11943 - attempts to compare absolute and canonical path result in the test failing. Changed references to getCanonicalPath to getAbsolutePath

--- engine/test-src/org/pentaho/di/job/entries/getpop/JobEntryGetPOPTest.java
+++ engine/test-src/org/pentaho/di/job/entries/getpop/JobEntryGetPOPTest.java
@@ -237,7 +237,7 @@ public class JobEntryGetPOPTest {
       if ( e instanceof KettleException ) {
         assertTrue( "Output Folder should not be created",
           BaseMessages.getString( JobEntryGetPOP.class,
-            "JobGetMailsFromPOP.Error.OutputFolderNotExist", outputDir.getCanonicalPath() ).equals(
+            "JobGetMailsFromPOP.Error.OutputFolderNotExist", outputDir.getAbsolutePath() ).equals(
               Const.trim( e.getMessage() ) ) );
       } else {
         fail( "Output Folder should not have been created: " + e.getLocalizedMessage() );
@@ -250,7 +250,7 @@ public class JobEntryGetPOPTest {
       if ( e instanceof KettleException ) {
         assertTrue( "Output Folder should not be created",
           BaseMessages.getString( JobEntryGetPOP.class,
-            "JobGetMailsFromPOP.Error.AttachmentFolderNotExist", attachmentsDir.getCanonicalPath() ).equals(
+            "JobGetMailsFromPOP.Error.AttachmentFolderNotExist", attachmentsDir.getAbsolutePath() ).equals(
               Const.trim( e.getMessage() ) ) );
       } else {
         fail( "Attachments Folder should not have been created: " + e.getLocalizedMessage() );
