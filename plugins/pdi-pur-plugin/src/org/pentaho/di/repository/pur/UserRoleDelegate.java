576b79168ab4a8c3f95186d6321a5253e8e9961f
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-09-24 20:46:19 +0400

    [PDI-12814] - Need to prohibit empty names when adding new users or roles - refactor UserRoleDelegate: replace manual duplication by introducing a common method

--- plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/UserRoleDelegate.java
+++ plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/UserRoleDelegate.java
@@ -107,69 +107,66 @@ public class UserRoleDelegate implements java.io.Serializable {
     return managed;
   }
 
-  public void createUser( IUser newUser ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        ProxyPentahoUser user = UserRoleHelper.convertToPentahoProxyUser( newUser );
-        userRoleWebService.createUser( user );
-        if ( newUser instanceof IEEUser ) {
-          userRoleWebService
-            .setRoles( user, UserRoleHelper.convertToPentahoProxyRoles( ( (IEEUser) newUser ).getRoles() ) );
-        }
-        lookupCache.insertUserToLookupSet( newUser );
-        fireUserRoleListChange();
-      } catch ( Exception e ) { // it is the only way to determine AlreadyExistsException
-        if ( e.getCause().toString()
-            .contains( "org.pentaho.platform.api.engine.security.userroledao.AlreadyExistsException" ) ) {
-          throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-            "UserRoleDelegate.ERROR_0015_USER_NAME_ALREADY_EXISTS" ) );
-        }
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0002_UNABLE_TO_CREATE_USER", newUser.getLogin() ), e ); //$NON-NLS-1$
-      }
-    } else {
+  private void ensureHasPermissions() throws KettleException {
+    if ( !hasNecessaryPermissions ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
         "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
     }
+  }
 
+  public void createUser( IUser newUser ) throws KettleException {
+    ensureHasPermissions();
+
+    try {
+      ProxyPentahoUser user = UserRoleHelper.convertToPentahoProxyUser( newUser );
+      userRoleWebService.createUser( user );
+      if ( newUser instanceof IEEUser ) {
+        userRoleWebService
+          .setRoles( user, UserRoleHelper.convertToPentahoProxyRoles( ( (IEEUser) newUser ).getRoles() ) );
+      }
+      lookupCache.insertUserToLookupSet( newUser );
+      fireUserRoleListChange();
+    } catch ( Exception e ) { // it is the only way to determine AlreadyExistsException
+      if ( e.getCause().toString()
+        .contains( "org.pentaho.platform.api.engine.security.userroledao.AlreadyExistsException" ) ) {
+        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
+          "UserRoleDelegate.ERROR_0015_USER_NAME_ALREADY_EXISTS" ) );
+      }
+      throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
+        "UserRoleDelegate.ERROR_0002_UNABLE_TO_CREATE_USER", newUser.getLogin() ), e ); //$NON-NLS-1$
+    }
   }
 
   public void deleteUsers( List<IUser> users ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        userRoleWebService.deleteUsers( UserRoleHelper.convertToPentahoProxyUsers( users ) );
-        lookupCache.removeUsersFromLookupSet( users );
-        fireUserRoleListChange();
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0003_UNABLE_TO_DELETE_USERS", e.getLocalizedMessage() ), e ); //$NON-NLS-1$
-      }
-    } else {
+    ensureHasPermissions();
+
+    try {
+      userRoleWebService.deleteUsers( UserRoleHelper.convertToPentahoProxyUsers( users ) );
+      lookupCache.removeUsersFromLookupSet( users );
+      fireUserRoleListChange();
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0003_UNABLE_TO_DELETE_USERS", e.getLocalizedMessage() ), e ); //$NON-NLS-1$
     }
   }
 
   public void deleteUser( String name ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        ProxyPentahoUser user = userRoleWebService.getUser( name );
-        if ( user != null ) {
-          ProxyPentahoUser[] users = new ProxyPentahoUser[ 1 ];
-          users[ 0 ] = user;
-          userRoleWebService.deleteUsers( users );
-          fireUserRoleListChange();
-        } else {
-          throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-            "UserRoleDelegate.ERROR_0004_UNABLE_TO_DELETE_USER", name ) ); //$NON-NLS-1$       
-        }
-      } catch ( Exception e ) {
+    ensureHasPermissions();
+
+    try {
+      ProxyPentahoUser user = userRoleWebService.getUser( name );
+      if ( user != null ) {
+        ProxyPentahoUser[] users = new ProxyPentahoUser[ 1 ];
+        users[ 0 ] = user;
+        userRoleWebService.deleteUsers( users );
+        fireUserRoleListChange();
+      } else {
         throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0004_UNABLE_TO_DELETE_USER", name ), e ); //$NON-NLS-1$
+          "UserRoleDelegate.ERROR_0004_UNABLE_TO_DELETE_USER", name ) ); //$NON-NLS-1$
       }
-    } else {
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0004_UNABLE_TO_DELETE_USER", name ), e ); //$NON-NLS-1$
     }
   }
 
@@ -178,41 +175,35 @@ public class UserRoleDelegate implements java.io.Serializable {
   }
 
   public IUser getUser( String name, String password ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      IUser userInfo = null;
-      try {
-        ProxyPentahoUser user = userRoleWebService.getUser( name );
-        if ( user != null && user.getName().equals( name ) && user.getPassword().equals( password ) ) {
-          userInfo = UserRoleHelper.convertToUserInfo( user, userRoleWebService.getRolesForUser( user ), rsm );
-        }
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0005_UNABLE_TO_GET_USER", name ), e ); //$NON-NLS-1$
+    ensureHasPermissions();
+
+    IUser userInfo = null;
+    try {
+      ProxyPentahoUser user = userRoleWebService.getUser( name );
+      if ( user != null && user.getName().equals( name ) && user.getPassword().equals( password ) ) {
+        userInfo = UserRoleHelper.convertToUserInfo( user, userRoleWebService.getRolesForUser( user ), rsm );
       }
-      return userInfo;
-    } else {
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0005_UNABLE_TO_GET_USER", name ), e ); //$NON-NLS-1$
     }
+    return userInfo;
   }
 
   public IUser getUser( String name ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      IUser userInfo = null;
-      try {
-        ProxyPentahoUser user = userRoleWebService.getUser( name );
-        if ( user != null && user.getName().equals( name ) ) {
-          userInfo = UserRoleHelper.convertToUserInfo( user, userRoleWebService.getRolesForUser( user ), rsm );
-        }
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0005_UNABLE_TO_GET_USER", name ), e ); //$NON-NLS-1$
+    ensureHasPermissions();
+
+    IUser userInfo = null;
+    try {
+      ProxyPentahoUser user = userRoleWebService.getUser( name );
+      if ( user != null && user.getName().equals( name ) ) {
+        userInfo = UserRoleHelper.convertToUserInfo( user, userRoleWebService.getRolesForUser( user ), rsm );
       }
-      return userInfo;
-    } else {
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0005_UNABLE_TO_GET_USER", name ), e ); //$NON-NLS-1$
     }
+    return userInfo;
   }
 
   public List<IUser> getUsers() throws KettleException {
@@ -229,78 +220,66 @@ public class UserRoleDelegate implements java.io.Serializable {
   }
 
   public void updateUser( IUser user ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        ProxyPentahoUser proxyUser = UserRoleHelper.convertToPentahoProxyUser( user );
-        userRoleWebService.updateUser( proxyUser );
-        if ( user instanceof IEEUser ) {
-          userRoleWebService
-            .setRoles( proxyUser, UserRoleHelper.convertToPentahoProxyRoles( ( (IEEUser) user ).getRoles() ) );
-        }
-        lookupCache.updateUserInLookupSet( user );
-        fireUserRoleListChange();
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0007_UNABLE_TO_UPDATE_USER", user.getLogin() ), e ); //$NON-NLS-1$
+    ensureHasPermissions();
+
+    try {
+      ProxyPentahoUser proxyUser = UserRoleHelper.convertToPentahoProxyUser( user );
+      userRoleWebService.updateUser( proxyUser );
+      if ( user instanceof IEEUser ) {
+        userRoleWebService
+          .setRoles( proxyUser, UserRoleHelper.convertToPentahoProxyRoles( ( (IEEUser) user ).getRoles() ) );
       }
-    } else {
+      lookupCache.updateUserInLookupSet( user );
+      fireUserRoleListChange();
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0007_UNABLE_TO_UPDATE_USER", user.getLogin() ), e ); //$NON-NLS-1$
     }
   }
 
   public void createRole( IRole newRole ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        ProxyPentahoRole role = UserRoleHelper.convertToPentahoProxyRole( newRole );
-        userRoleWebService.createRole( role );
-        userRoleWebService.setUsers( role, UserRoleHelper.convertToPentahoProxyUsers( newRole.getUsers() ) );
-        lookupCache.insertRoleToLookupSet( newRole );
-        fireUserRoleListChange();
-      } catch ( UserRoleException e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0008_UNABLE_TO_CREATE_ROLE", newRole.getName() ), e ); //$NON-NLS-1$
-      } catch ( Exception e ) { // it is the only way to determine AlreadyExistsException
-        if ( e.getCause().toString()
-            .contains( "org.pentaho.platform.api.engine.security.userroledao.AlreadyExistsException" ) ) {
-          throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-            "UserRoleDelegate.ERROR_0016_ROLE_NAME_ALREADY_EXISTS" ) );
-        }
-      }
-    } else {
+    ensureHasPermissions();
+
+    try {
+      ProxyPentahoRole role = UserRoleHelper.convertToPentahoProxyRole( newRole );
+      userRoleWebService.createRole( role );
+      userRoleWebService.setUsers( role, UserRoleHelper.convertToPentahoProxyUsers( newRole.getUsers() ) );
+      lookupCache.insertRoleToLookupSet( newRole );
+      fireUserRoleListChange();
+    } catch ( UserRoleException e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0008_UNABLE_TO_CREATE_ROLE", newRole.getName() ), e ); //$NON-NLS-1$
+    } catch ( Exception e ) { // it is the only way to determine AlreadyExistsException
+      if ( e.getCause().toString()
+        .contains( "org.pentaho.platform.api.engine.security.userroledao.AlreadyExistsException" ) ) {
+        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
+          "UserRoleDelegate.ERROR_0016_ROLE_NAME_ALREADY_EXISTS" ) );
+      }
     }
   }
 
   public void deleteRoles( List<IRole> roles ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        userRoleWebService.deleteRoles( UserRoleHelper.convertToPentahoProxyRoles( roles ) );
-        lookupCache.removeRolesFromLookupSet( roles );
-        fireUserRoleListChange();
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0009_UNABLE_TO_DELETE_ROLES" ), e ); //$NON-NLS-1$
-      }
-    } else {
+    ensureHasPermissions();
+
+    try {
+      userRoleWebService.deleteRoles( UserRoleHelper.convertToPentahoProxyRoles( roles ) );
+      lookupCache.removeRolesFromLookupSet( roles );
+      fireUserRoleListChange();
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0009_UNABLE_TO_DELETE_ROLES" ), e ); //$NON-NLS-1$
     }
   }
 
   public IRole getRole( String name ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        return UserRoleHelper.convertFromProxyPentahoRole( userRoleWebService, UserRoleHelper.getProxyPentahoRole(
-          userRoleWebService, name ), lookupCache, rsm );
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0010_UNABLE_TO_GET_ROLE", name ), e ); //$NON-NLS-1$
-      }
-    } else {
+    ensureHasPermissions();
+
+    try {
+      return UserRoleHelper.convertFromProxyPentahoRole( userRoleWebService, UserRoleHelper.getProxyPentahoRole(
+        userRoleWebService, name ), lookupCache, rsm );
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0010_UNABLE_TO_GET_ROLE", name ), e ); //$NON-NLS-1$
     }
   }
 
@@ -318,61 +297,51 @@ public class UserRoleDelegate implements java.io.Serializable {
   }
 
   public List<IRole> getDefaultRoles() throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        return UserRoleHelper.convertToListFromProxyPentahoDefaultRoles( userRoleSecurityInfo, rsm );
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0011_UNABLE_TO_GET_ROLES" ), e ); //$NON-NLS-1$
-      }
-    } else {
+    ensureHasPermissions();
+
+    try {
+      return UserRoleHelper.convertToListFromProxyPentahoDefaultRoles( userRoleSecurityInfo, rsm );
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0011_UNABLE_TO_GET_ROLES" ), e ); //$NON-NLS-1$
     }
   }
 
   public void updateRole( IRole role ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        List<String> users = new ArrayList<String>();
-        for ( IUser user : role.getUsers() ) {
-          users.add( user.getLogin() );
-        }
-        userRoleWebService.updateRole( role.getName(), role.getDescription(), users );
-        lookupCache.updateRoleInLookupSet( role );
-        fireUserRoleListChange();
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0012_UNABLE_TO_UPDATE_ROLE", role.getName() ), e ); //$NON-NLS-1$
+    ensureHasPermissions();
+
+    try {
+      List<String> users = new ArrayList<String>();
+      for ( IUser user : role.getUsers() ) {
+        users.add( user.getLogin() );
       }
-    } else {
+      userRoleWebService.updateRole( role.getName(), role.getDescription(), users );
+      lookupCache.updateRoleInLookupSet( role );
+      fireUserRoleListChange();
+    } catch ( Exception e ) {
       throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
+        "UserRoleDelegate.ERROR_0012_UNABLE_TO_UPDATE_ROLE", role.getName() ), e ); //$NON-NLS-1$
     }
   }
 
   public void deleteRole( String name ) throws KettleException {
-    if ( hasNecessaryPermissions ) {
-      try {
-        ProxyPentahoRole roleToDelete = UserRoleHelper.getProxyPentahoRole( userRoleWebService, name );
-        if ( roleToDelete != null ) {
-          ProxyPentahoRole[] roleArray = new ProxyPentahoRole[ 1 ];
-          roleArray[ 0 ] = roleToDelete;
-          userRoleWebService.deleteRoles( roleArray );
-          fireUserRoleListChange();
-        } else {
-          throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-            "UserRoleDelegate.ERROR_0013_UNABLE_TO_DELETE_ROLE", name ) ); //$NON-NLS-1$
-        }
-      } catch ( Exception e ) {
-        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-          "UserRoleDelegate.ERROR_0013_UNABLE_TO_DELETE_ROLE", name ), e ); //$NON-NLS-1$
-      }
-    } else {
-      throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
-        "UserRoleDelegate.ERROR_0014_INSUFFICIENT_PRIVILEGES" ) ); //$NON-NLS-1$
-    }
+    ensureHasPermissions();
 
+    try {
+      ProxyPentahoRole roleToDelete = UserRoleHelper.getProxyPentahoRole( userRoleWebService, name );
+      if ( roleToDelete != null ) {
+        ProxyPentahoRole[] roleArray = new ProxyPentahoRole[ 1 ];
+        roleArray[ 0 ] = roleToDelete;
+        userRoleWebService.deleteRoles( roleArray );
+        fireUserRoleListChange();
+      } else {
+        throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
+          "UserRoleDelegate.ERROR_0013_UNABLE_TO_DELETE_ROLE", name ) ); //$NON-NLS-1$
+      }
+    } catch ( Exception e ) {
+      throw new KettleException( BaseMessages.getString( UserRoleDelegate.class,
+        "UserRoleDelegate.ERROR_0013_UNABLE_TO_DELETE_ROLE", name ), e ); //$NON-NLS-1$
+    }
   }
 
   public void setRoles( List<IRole> roles ) throws KettleException {
