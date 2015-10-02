d2d4a82ff3d0c0cdd3ef05499eda041e3370e31d
PogI <ivan_pogodin@epam.com>
2015-10-02 18:19:21 +0400

    [PDI-14124] (test) While running a transformation in a job on a slave server, the getSystemInfo parent batch id always returns 0/null for first level

--- engine/test-src/org/pentaho/di/trans/TransExecutionConfigurationTest.java
+++ engine/test-src/org/pentaho/di/trans/TransExecutionConfigurationTest.java
@@ -34,12 +34,15 @@ import org.pentaho.di.core.plugins.ClassLoadingPluginInterface;
 import org.pentaho.di.core.plugins.PluginInterface;
 import org.pentaho.di.core.plugins.PluginRegistry;
 import org.pentaho.di.core.plugins.RepositoryPluginType;
+import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.repository.RepositoriesMeta;
 import org.pentaho.di.repository.Repository;
 import org.pentaho.di.repository.RepositoryMeta;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 public class TransExecutionConfigurationTest {
 
@@ -50,16 +53,16 @@ public class TransExecutionConfigurationTest {
     final RepositoryMeta repositoryMeta = mock( RepositoryMeta.class );
     final Repository repository = mock( Repository.class );
     final String mockRepo = "mockRepo";
-    final boolean[] connectionSuccess = {false};
+    final boolean[] connectionSuccess = { false };
 
     Repository initialRepo = mock( Repository.class );
     transExecConf.setRepository( initialRepo );
 
     KettleLogStore.init();
 
-    //Create mock repository plugin
+    // Create mock repository plugin
     MockRepositoryPlugin mockRepositoryPlugin = mock( MockRepositoryPlugin.class );
-    when( mockRepositoryPlugin.getIds() ).thenReturn( new String[]{"mockRepo"} );
+    when( mockRepositoryPlugin.getIds() ).thenReturn( new String[] { "mockRepo" } );
     when( mockRepositoryPlugin.matches( "mockRepo" ) ).thenReturn( true );
     when( mockRepositoryPlugin.getName() ).thenReturn( "mock-repository" );
     when( mockRepositoryPlugin.getClassMap() ).thenAnswer( new Answer<Map<Class<?>, String>>() {
@@ -83,7 +86,7 @@ public class TransExecutionConfigurationTest {
     doAnswer( new Answer() {
       @Override
       public Object answer( InvocationOnMock invocation ) throws Throwable {
-        if ( "username".equals( invocation.getArguments()[0] ) &&  "password".equals( invocation.getArguments()[1] ) ) {
+        if ( "username".equals( invocation.getArguments()[0] ) && "password".equals( invocation.getArguments()[1] ) ) {
           connectionSuccess[0] = true;
         } else {
           connectionSuccess[0] = false;
@@ -93,18 +96,39 @@ public class TransExecutionConfigurationTest {
       }
     } ).when( repository ).connect( anyString(), anyString() );
 
-    //Ignore repository not found in RepositoriesMeta
+    // Ignore repository not found in RepositoriesMeta
     transExecConf.connectRepository( repositoriesMeta, "notFound", "username", "password" );
     assertEquals( "Repository Changed", initialRepo, transExecConf.getRepository() );
 
-    //Ignore failed attempt to connect
+    // Ignore failed attempt to connect
     transExecConf.connectRepository( repositoriesMeta, mockRepo, "username", "" );
     assertEquals( "Repository Changed", initialRepo, transExecConf.getRepository() );
 
-    //Save repository if connection passes
+    // Save repository if connection passes
     transExecConf.connectRepository( repositoriesMeta, mockRepo, "username", "password" );
     assertEquals( "Repository didn't change", repository, transExecConf.getRepository() );
     assertTrue( "Repository not connected", connectionSuccess[0] );
   }
-  private interface MockRepositoryPlugin extends PluginInterface, ClassLoadingPluginInterface { }
+
+  private interface MockRepositoryPlugin extends PluginInterface, ClassLoadingPluginInterface {
+  }
+
+  @Test
+  public void testCopy() {
+    TransExecutionConfiguration tec = new TransExecutionConfiguration();
+    tec.setPassedBatchId( 5L );
+    TransExecutionConfiguration tecCopy = (TransExecutionConfiguration) tec.clone();
+    assertEquals( "clone-copy", tecCopy.getPassedBatchId(), tec.getPassedBatchId() );
+  }
+
+  @Test
+  public void testCopyXml() throws Exception {
+    TransExecutionConfiguration tec = new TransExecutionConfiguration();
+    tec.setPassedBatchId( 5L );
+    String xml = tec.getXML();
+    Document doc = XMLHandler.loadXMLString( xml );
+    Node node = XMLHandler.getSubNode( doc, TransExecutionConfiguration.XML_TAG );
+    TransExecutionConfiguration tecCopy = new TransExecutionConfiguration( node );
+    assertEquals( "xml-copy", tecCopy.getPassedBatchId(), tec.getPassedBatchId() );
+  }
 }
