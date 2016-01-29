0ac7fef5ad187356f517500ae6f6a240a9f8cc9e
Matt Tucker <matt.tucker@pentaho.com>
2016-01-29 12:17:33 -0500

    [PDI-14901] Clustering Release Sockets
    Properly release server sockets after transformation finishes

--- engine/src/org/pentaho/di/www/SocketRepository.java
+++ engine/src/org/pentaho/di/www/SocketRepository.java
@@ -52,54 +52,59 @@ public class SocketRepository {
     socketMap = new HashMap<Integer, SocketRepositoryEntry>();
   }
 
+  private ServerSocket createServerSocket( int port ) throws IOException {
+    ServerSocket serverSocket = new ServerSocket();
+    serverSocket.setPerformancePreferences( 1, 2, 3 ); // order of importance: bandwidth, latency, connection time
+    serverSocket.setReuseAddress( true );
+
+    // It happens in high-paced environments where lots of sockets are opened and closed that the operating
+    // system keeps a lock on a socket. Because of this we have to wait at least for one minute and on some platforms
+    // up to 2 minutes.
+    // Let's take 5 to make sure we can get a socket connection and we still get into trouble.
+    //
+    // mc: It sucks and blows at the same time that we have to do this but I couldn't find another solution.
+    //
+    try {
+      serverSocket.bind( new InetSocketAddress( port ) );
+    } catch ( BindException e ) {
+      long totalWait = 0L;
+      long startTime = System.currentTimeMillis();
+
+      IOException ioException = null;
+      log.logMinimal( "Carte socket repository : Starting a retry loop to bind the server socket on port "
+        + port + ".  We retry for 5 minutes until the socket clears in your operating system." );
+      while ( !serverSocket.isBound() && totalWait < 300000 ) {
+        try {
+          totalWait = System.currentTimeMillis() - startTime;
+          log.logMinimal( "Carte socket repository : Retry binding the server socket on port "
+            + port + " after a " + ( totalWait / 1000 ) + " seconds wait..." );
+          Thread.sleep( 10000 ); // wait 10 seconds, try again...
+          serverSocket.bind( new InetSocketAddress( port ), 100 );
+        } catch ( IOException ioe ) {
+          ioException = ioe;
+        } catch ( Exception ex ) {
+          serverSocket.close();
+          throw new IOException( ex.getMessage() );
+        }
+
+        totalWait = System.currentTimeMillis() - startTime;
+      }
+      if ( !serverSocket.isBound() ) {
+        serverSocket.close();
+        throw ioException;
+      }
+      log.logDetailed( "Carte socket repository : Succesfully bound the server socket on port "
+        + port + " after " + ( totalWait / 1000 ) + " seconds." );
+    }
+    return serverSocket;
+  }
+
   public synchronized ServerSocket openServerSocket( int port, String user ) throws IOException {
 
     SocketRepositoryEntry entry = socketMap.get( port );
     if ( entry == null ) {
 
-      ServerSocket serverSocket = new ServerSocket();
-      serverSocket.setPerformancePreferences( 1, 2, 3 ); // order of importance: bandwidth, latency, connection time
-      serverSocket.setReuseAddress( true );
-
-      // It happens in high-paced environments where lots of sockets are opened and closed that the operating
-      // system keeps a lock on a socket. Because of this we have to wait at least for one minute and on some platforms
-      // up to 2 minutes.
-      // Let's take 5 to make sure we can get a socket connection and we still get into trouble.
-      //
-      // mc: It sucks and blows at the same time that we have to do this but I couldn't find another solution.
-      //
-      try {
-        serverSocket.bind( new InetSocketAddress( port ) );
-      } catch ( BindException e ) {
-        long totalWait = 0L;
-        long startTime = System.currentTimeMillis();
-
-        IOException ioException = null;
-        log.logMinimal( "Carte socket repository : Starting a retry loop to bind the server socket on port "
-          + port + ".  We retry for 5 minutes until the socket clears in your operating system." );
-        while ( !serverSocket.isBound() && totalWait < 300000 ) {
-          try {
-            totalWait = System.currentTimeMillis() - startTime;
-            log.logMinimal( "Carte socket repository : Retry binding the server socket on port "
-              + port + " after a " + ( totalWait / 1000 ) + " seconds wait..." );
-            Thread.sleep( 10000 ); // wait 10 seconds, try again...
-            serverSocket.bind( new InetSocketAddress( port ), 100 );
-          } catch ( IOException ioe ) {
-            ioException = ioe;
-          } catch ( Exception ex ) {
-            serverSocket.close();
-            throw new IOException( ex.getMessage() );
-          }
-
-          totalWait = System.currentTimeMillis() - startTime;
-        }
-        if ( !serverSocket.isBound() ) {
-          serverSocket.close();
-          throw ioException;
-        }
-        log.logDetailed( "Carte socket repository : Succesfully bound the server socket on port "
-          + port + " after " + ( totalWait / 1000 ) + " seconds." );
-      }
+      ServerSocket serverSocket = createServerSocket( port );
       entry = new SocketRepositoryEntry( port, serverSocket, true, user );
 
       // Store the entry in the map too!
@@ -111,6 +116,9 @@ public class SocketRepository {
       //
       if ( entry.isInUse() ) {
         throw new IOException( "Server socket on port " + port + " is already in use by [" + entry.getUser() + "]" );
+      }
+      if ( entry.getServerSocket().isClosed() ) {
+        entry.setServerSocket( createServerSocket( port ) );
       }
       entry.setInUse( true );
     }
