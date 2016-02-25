03c26edf6e750e37404cc498b23a47ec67f4af79
Alexander Buloichik <alexander_buloichik@epam.com>
2016-02-25 13:47:40 +0300

    [BACKLOG-6753] Fix unit tests for MDI

--- engine/test-src/org/pentaho/di/trans/steps/metainject/MetaInjectTest.java
+++ engine/test-src/org/pentaho/di/trans/steps/metainject/MetaInjectTest.java
@@ -188,18 +188,18 @@ public class MetaInjectTest {
   }
 
   @Test
-  public void initReturnsFalseOnInaccessibleSourceStep() {
+  public void initReturnsTrueOnInaccessibleSourceStep() {
     Map<TargetStepAttribute, SourceStepField> targetMap =
         Collections.singletonMap( new TargetStepAttribute( INJECTOR_STEP_NAME, TEST_ATTR_VALUE, false ),
             new SourceStepField( TEST_SOURCE_STEP_NAME, TEST_FIELD ) );
     MetaInjectMeta spyMeta = spy( meta );
     doReturn( targetMap ).when( spyMeta ).getTargetSourceMapping();
 
-    assertFalse( metaInject.init( spyMeta, data ) );
+    assertTrue( metaInject.init( spyMeta, data ) );
   }
 
   @Test
-  public void initReturnsFalseOnInaccessibleTargetStep() {
+  public void initReturnsTrueOnInaccessibleTargetStep() {
     doReturn( new String[] { TEST_SOURCE_STEP_NAME } ).when( transMeta ).getPrevStepNames( any( StepMeta.class ) );
 
     Map<TargetStepAttribute, SourceStepField> targetMap =
@@ -208,7 +208,7 @@ public class MetaInjectTest {
     MetaInjectMeta spyMeta = spy( meta );
     doReturn( targetMap ).when( spyMeta ).getTargetSourceMapping();
 
-    assertFalse( metaInject.init( spyMeta, data ) );
+    assertTrue( metaInject.init( spyMeta, data ) );
   }
 
   @Test
