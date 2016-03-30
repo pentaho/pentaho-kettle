c244a6cc409029a5f185605a3587d3e7236831b9
Ivan Nikolaichuk <Ivan_Nikolaichuk@epam.com>
2016-03-30 09:24:07 +0400

    [CLEANUP] Checkstyle applied, licence headers updated

--- plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/metastore/PurRepositoryMetaStore.java
+++ plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/metastore/PurRepositoryMetaStore.java
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2015 Pentaho Corporation.  All rights reserved.
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
  */
 package org.pentaho.di.repository.pur.metastore;
 
-import org.apache.commons.lang.StringUtils;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.repository.pur.PurRepository;
