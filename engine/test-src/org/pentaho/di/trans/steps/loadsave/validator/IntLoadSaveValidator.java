58a255b9229d7f49a64a144aa55387f3dbdf2ca6
Matt Tucker <matt.tucker@pentaho.com>
2016-05-05 17:13:02 -0400

    [SP-2688][TEST] LoadSaveTest for Analytic Query step

--- engine/test-src/org/pentaho/di/trans/steps/loadsave/validator/IntLoadSaveValidator.java
+++ engine/test-src/org/pentaho/di/trans/steps/loadsave/validator/IntLoadSaveValidator.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -28,11 +28,30 @@ import java.util.Random;
  * @author Andrey Khayrutdinov
  */
 public class IntLoadSaveValidator implements FieldLoadSaveValidator<Integer> {
-  private final Integer sample = new Random().nextInt();
+  private final Integer maxValue;
+
+  /**
+   * An IntLoadSaveValidator that returns test values between Integer.MIN_VALUE and Integer.MAX_VALUE.
+   */
+  public IntLoadSaveValidator() {
+    maxValue = null;
+  }
+
+  /**
+   * An IntLoadSaveValidator that only returns test values between 0 and maxValue, inclusive.
+   * 
+   * @param maxValue The maximum Int value that should be returned
+   */
+  public IntLoadSaveValidator( Integer maxValue ) {
+    this.maxValue = maxValue;
+  }
 
   @Override
   public Integer getTestObject() {
-    return sample;
+    if ( maxValue == null ) {
+      return new Random().nextInt();
+    }
+    return new Random().nextInt( maxValue );
   }
 
   @Override
