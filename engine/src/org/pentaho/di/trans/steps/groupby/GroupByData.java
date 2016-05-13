97e02f69897636ffbcdd51586ed1984f57087d78
Ivan Nikolaichuk <Ivan_Nikolaichuk@epam.com>
2016-05-13 08:30:41 +0400

    [CLEANUP] Checkstyle applied, licence headers updated

--- engine/src/org/pentaho/di/trans/steps/groupby/GroupByData.java
+++ engine/src/org/pentaho/di/trans/steps/groupby/GroupByData.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -103,9 +103,6 @@ public class GroupByData extends BaseStepData implements StepDataInterface {
 
   public boolean newBatch;
 
-  /**
-   *
-   */
   public GroupByData() {
     super();
 
