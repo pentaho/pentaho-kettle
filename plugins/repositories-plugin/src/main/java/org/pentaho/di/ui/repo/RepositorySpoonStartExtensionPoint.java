3ee4522d6f4b268091c3e3f8de833eb88696293a
Benjamin Morrise <bmorrise@pentaho.com>
2016-06-13 16:14:23 -0400

    [BACKLOG-8416] Updated connection error messages

--- plugins/repositories-plugin/src/main/java/org/pentaho/di/ui/repo/RepositorySpoonStartExtensionPoint.java
+++ plugins/repositories-plugin/src/main/java/org/pentaho/di/ui/repo/RepositorySpoonStartExtensionPoint.java
@@ -41,7 +41,7 @@ import org.pentaho.di.ui.spoon.Spoon;
 )
 public class RepositorySpoonStartExtensionPoint implements ExtensionPointInterface {
 
-  private static Class<?> PKG = Spoon.class;
+  private static Class<?> PKG = RepositorySpoonStartExtensionPoint.class;
 
   private RepositoryConnectController repositoryConnectController;
 
@@ -79,12 +79,12 @@ public class RepositorySpoonStartExtensionPoint implements ExtensionPointInterfa
           .connectToRepository( repositoryMeta, null, null );
       }
       if ( !repositoryConnectController.isConnected() ) {
-        String msg = BaseMessages.getString( PKG, "Spoon.Log.NoRepositoriesDefined" );
+        String msg = BaseMessages.getString( PKG, "Repository.NoConnected.Message" );
         log.logError( msg ); // "No repositories defined on this system."
         MessageBox mb = new MessageBox( getSpoon().getShell(), SWT.OK | SWT.ICON_ERROR );
-        mb.setMessage( BaseMessages.getString( PKG, "Spoon.Error.Repository.NotFound", optionRepname
+        mb.setMessage( BaseMessages.getString( PKG, "Repository.NoConnected.Message", optionRepname
           .toString() ) );
-        mb.setText( BaseMessages.getString( PKG, "Spoon.Error.Repository.NotFound.Title" ) );
+        mb.setText( BaseMessages.getString( PKG, "Repository.NoConnected.Message.Title" ) );
         mb.open();
       }
     } else if ( Const.isEmpty( optionFilename ) ) {
