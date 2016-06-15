e2bf5d5b18e1ab47ebdfe11f49fedc69f82ce355
mbatchelor <mbatchelor@pentaho.com>
2016-06-15 16:42:08 +0400

    [SP-2750] Backport of PDI-15437 - JobEntryWaitForSQL doesn't succeed without errors (5.4 Suite)

--- engine/src/org/pentaho/di/job/entries/waitforsql/JobEntryWaitForSQL.java
+++ engine/src/org/pentaho/di/job/entries/waitforsql/JobEntryWaitForSQL.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -132,6 +132,7 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     this( "" );
   }
 
+  @Override
   public Object clone() {
     JobEntryWaitForSQL je = (JobEntryWaitForSQL) super.clone();
     return je;
@@ -156,6 +157,7 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     return getSuccessConditionByCode( tt );
   }
 
+  @Override
   public String getXML() {
     StringBuffer retval = new StringBuffer( 200 );
 
@@ -229,6 +231,7 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     this.maximumTimeout = maximumTimeout;
   }
 
+  @Override
   public void loadXML( Node entrynode, List<DatabaseMeta> databases, List<SlaveServer> slaveServers,
     Repository rep, IMetaStore metaStore ) throws KettleXMLException {
     try {
@@ -254,6 +257,7 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     }
   }
 
+  @Override
   public void loadRep( Repository rep, IMetaStore metaStore, ObjectId id_jobentry, List<DatabaseMeta> databases,
     List<SlaveServer> slaveServers ) throws KettleException {
     try {
@@ -292,6 +296,7 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     return 0;
   }
 
+  @Override
   public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_job ) throws KettleException {
     try {
       rep.saveDatabaseMetaJobEntryAttribute( id_job, getObjectId(), "connection", "id_database", connection );
@@ -324,14 +329,17 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
     return connection;
   }
 
+  @Override
   public boolean evaluates() {
     return true;
   }
 
+  @Override
   public boolean isUnconditional() {
     return false;
   }
 
+  @Override
   public Result execute( Result previousResult, int nr ) {
     Result result = previousResult;
     result.setResult( false );
@@ -471,6 +479,12 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
       logBasic( "Exception while waiting for SQL data: " + e.getMessage() );
     }
 
+    if ( result.getResult() ) {
+      // Remove error count set at the beginning of the method
+      // PDI-15437
+      result.setNrErrors( 0 );
+    }
+
     return result;
   }
 
@@ -572,10 +586,12 @@ public class JobEntryWaitForSQL extends JobEntryBase implements Cloneable, JobEn
 
   }
 
+  @Override
   public DatabaseMeta[] getUsedDatabaseConnections() {
     return new DatabaseMeta[] { connection, };
   }
 
+  @Override
   public List<ResourceReference> getResourceDependencies( JobMeta jobMeta ) {
     List<ResourceReference> references = super.getResourceDependencies( jobMeta );
     if ( connection != null ) {
