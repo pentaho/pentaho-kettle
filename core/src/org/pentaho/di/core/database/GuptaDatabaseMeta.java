fcce3bc310392fb5b5a841f3ae6657dd49dd3c66
mbatchelor <mbatchelor@pentaho.com>
2016-06-16 12:50:40 -0400

    [PDI-14893] - UnsupportedMethodException on ps metadata fetch

--- core/src/org/pentaho/di/core/database/GuptaDatabaseMeta.java
+++ core/src/org/pentaho/di/core/database/GuptaDatabaseMeta.java
@@ -228,4 +228,16 @@ public class GuptaDatabaseMeta extends BaseDatabaseMeta implements DatabaseInter
     return false;
   }
 
+  /**
+   * Most databases allow you to retrieve result metadata by preparing a SELECT statement. Gupta though doesn't.
+   * See PDI-14893
+   *
+   * @return true if the database supports retrieval of query metadata from a prepared statement. False if the query
+   *         needs to be executed first.
+   */
+  @Override
+  public boolean supportsPreparedStatementMetadataRetrieval() {
+    return false;
+  }
+
 }
