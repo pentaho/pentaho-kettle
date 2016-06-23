5e97a7e2f5caaf79f1073995084e5f931716a0e7
Matt Tucker <matt.tucker@pentaho.com>
2016-06-23 15:49:54 -0400

    [PDI-13787] Get Variables Timestamp Conversion

--- engine/test-src/org/pentaho/di/trans/steps/getvariable/GetVariableMetaTest.java
+++ engine/test-src/org/pentaho/di/trans/steps/getvariable/GetVariableMetaTest.java
@@ -21,6 +21,10 @@
 ******************************************************************************/
 package org.pentaho.di.trans.steps.getvariable;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -29,38 +33,42 @@ import java.util.Map;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.plugins.PluginRegistry;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMetaInterface;
-import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaTimestamp;
+import org.pentaho.di.core.variables.Variables;
 import org.pentaho.di.trans.steps.getvariable.GetVariableMeta.FieldDefinition;
 import org.pentaho.di.trans.steps.loadsave.LoadSaveTester;
 import org.pentaho.di.trans.steps.loadsave.initializer.InitializerInterface;
 import org.pentaho.di.trans.steps.loadsave.validator.ArrayLoadSaveValidator;
 import org.pentaho.di.trans.steps.loadsave.validator.FieldLoadSaveValidator;
 
-public class GetVariableMetaTest implements InitializerInterface<StepMetaInterface> {
-  LoadSaveTester loadSaveTester;
+public class GetVariableMetaTest implements InitializerInterface<GetVariableMeta> {
+  LoadSaveTester<GetVariableMeta> loadSaveTester;
   Class<GetVariableMeta> testMetaClass = GetVariableMeta.class;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws KettleException {
+    KettleEnvironment.init();
+    PluginRegistry.init( true );
+  }
 
   @Before
   public void setUpLoadSave() throws Exception {
-    KettleEnvironment.init();
-    PluginRegistry.init( true );
     List<String> attributes = Arrays.asList( "fieldDefinitions" );
 
-    Map<String, String> getterMap = new HashMap<String, String>() {
-      {
-        put( "fieldDefinitions", "getFieldDefinitions" );
-      }
-    };
-    Map<String, String> setterMap = new HashMap<String, String>() {
-      {
-        put( "fieldDefinitions", "setFieldDefinitions" );
-      }
-    };
+    Map<String, String> getterMap = new HashMap<String, String>();
+    getterMap.put( "fieldDefinitions", "getFieldDefinitions" );
+
+    Map<String, String> setterMap = new HashMap<String, String>();
+    setterMap.put( "fieldDefinitions", "setFieldDefinitions" );
 
     FieldDefinition fieldDefinition = new FieldDefinition();
     fieldDefinition.setFieldName( "fieldName" );
@@ -72,7 +80,7 @@ public class GetVariableMetaTest implements InitializerInterface<StepMetaInterfa
     fieldDefinition.setVariableString( "variableString" );
 
     FieldLoadSaveValidator<FieldDefinition[]> fieldDefinitionLoadSaveValidator =
-        new ArrayLoadSaveValidator<FieldDefinition>( new FieldDefinitionLoadSaveValidator( fieldDefinition ), 5 );
+      new ArrayLoadSaveValidator<FieldDefinition>( new FieldDefinitionLoadSaveValidator( fieldDefinition ), 5 );
 
     Map<String, FieldLoadSaveValidator<?>> attrValidatorMap = new HashMap<String, FieldLoadSaveValidator<?>>();
     attrValidatorMap.put( "fieldName", fieldDefinitionLoadSaveValidator );
@@ -81,20 +89,40 @@ public class GetVariableMetaTest implements InitializerInterface<StepMetaInterfa
     typeValidatorMap.put( FieldDefinition[].class.getCanonicalName(), fieldDefinitionLoadSaveValidator );
 
     loadSaveTester =
-        new LoadSaveTester( testMetaClass, attributes, Collections.emptyList(), Collections.emptyList(), getterMap,
-            setterMap, attrValidatorMap, typeValidatorMap, this );
+      new LoadSaveTester<>( testMetaClass, attributes, Collections.emptyList(), Collections.emptyList(), getterMap,
+        setterMap, attrValidatorMap, typeValidatorMap, this );
   }
 
   // Call the allocate method on the LoadSaveTester meta class
-  public void modify( StepMetaInterface someMeta ) {
-    if ( someMeta instanceof GetVariableMeta ) {
-      ( (GetVariableMeta) someMeta ).allocate( 5 );
-    }
+  @Override
+  public void modify( GetVariableMeta someMeta ) {
+    someMeta.allocate( 5 );
   }
 
   @Test
   public void testSerialization() throws KettleException {
     loadSaveTester.testSerialization();
+  }
+
+  @Test
+  public void testGetValueMetaPlugin() throws KettleStepException {
+    GetVariableMeta meta = new GetVariableMeta();
+    meta.setDefault();
+
+    FieldDefinition field = new FieldDefinition();
+    field.setFieldName( "outputField" );
+    field.setVariableString( String.valueOf( 2000000L ) );
+    field.setFieldType( ValueMetaInterface.TYPE_TIMESTAMP );
+    meta.setFieldDefinitions( new FieldDefinition[]{ field } );
+
+    RowMetaInterface rowMeta = new RowMeta();
+    meta.getFields( rowMeta, "stepName", null, null, new Variables(), null, null );
+
+    assertNotNull( rowMeta );
+    assertEquals( 1, rowMeta.size() );
+    assertEquals( "outputField", rowMeta.getFieldNames()[0] );
+    assertEquals( ValueMetaInterface.TYPE_TIMESTAMP, rowMeta.getValueMeta( 0 ).getType() );
+    assertTrue( rowMeta.getValueMeta( 0 ) instanceof ValueMetaTimestamp );
   }
 
   public static class FieldDefinitionLoadSaveValidator implements FieldLoadSaveValidator<FieldDefinition> {
