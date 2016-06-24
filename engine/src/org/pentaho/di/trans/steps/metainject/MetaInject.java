dad3e692880b29ef8e021e8c777b60ecd1b19b01
Michael D'Amour <mdamour1976@gmail.com>
2016-06-24 14:37:51 -0400

    [BACKLOG-8272] Fixing injection issue for searchInfoAndTargetSteps

--- engine/src/org/pentaho/di/trans/steps/metainject/MetaInject.java
+++ engine/src/org/pentaho/di/trans/steps/metainject/MetaInject.java
@@ -108,18 +108,19 @@ public class MetaInject extends BaseStep implements StepInterface {
       }
     }
 
+    List<StepMeta> steps = data.transMeta.getSteps();
     for ( Map.Entry<String, StepMetaInterface> en : data.stepInjectionMetasMap.entrySet() ) {
       newInjection( en.getKey(), en.getValue() );
-      List<StepMeta> steps = en.getValue().getParentStepMeta().getParentTransMeta().getSteps();
       en.getValue().searchInfoAndTargetSteps( steps );
     }
 
     for ( String targetStepName : data.stepInjectionMap.keySet() ) {
       if ( !data.stepInjectionMetasMap.containsKey( targetStepName ) ) {
         oldInjection( targetStepName );
-        StepMeta targetStep = StepMeta.findStep( this.getTransMeta().getSteps(), targetStepName );
-        List<StepMeta> steps = targetStep.getParentTransMeta().getSteps();
-        targetStep.getStepMetaInterface().searchInfoAndTargetSteps( steps );
+        StepMeta targetStep = StepMeta.findStep( steps, targetStepName );
+        if ( targetStep != null ) {
+          targetStep.getStepMetaInterface().searchInfoAndTargetSteps( steps );
+        }
       }
     }
 
