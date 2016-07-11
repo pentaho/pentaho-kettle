6261c115027635797c9ca8036daa40ba9a7d78dd
Matt Tucker <matt.tucker@pentaho.com>
2016-07-11 17:00:38 -0400

    [SP-2775][PDI-15501] VFS SFTP Identity Fix
    Change to IdentityInfo as needed by VFS 2.1

--- core/src/org/pentaho/di/core/vfs/configuration/KettleSftpFileSystemConfigBuilder.java
+++ core/src/org/pentaho/di/core/vfs/configuration/KettleSftpFileSystemConfigBuilder.java
@@ -25,12 +25,14 @@ package org.pentaho.di.core.vfs.configuration;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.vfs2.FileSystemOptions;
 import org.apache.commons.vfs2.FileSystem;
+import org.apache.commons.vfs2.FileSystemOptions;
 import org.apache.commons.vfs2.provider.FileNameParser;
 import org.apache.commons.vfs2.provider.URLFileName;
+import org.apache.commons.vfs2.provider.sftp.IdentityInfo;
 import org.apache.commons.vfs2.provider.sftp.SftpFileNameParser;
 import org.apache.commons.vfs2.provider.sftp.SftpFileSystem;
+import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
 import org.pentaho.di.core.logging.LogChannel;
 import org.pentaho.di.core.logging.LogChannelInterface;
 
@@ -64,6 +66,7 @@ public class KettleSftpFileSystemConfigBuilder extends KettleGenericFileSystemCo
 
   private static final KettleSftpFileSystemConfigBuilder builder = new KettleSftpFileSystemConfigBuilder();
   private static final LogChannelInterface log = new LogChannel( "cfgbuilder" );
+  private static final String IDENTITY_KEY = SftpFileSystemConfigBuilder.class.getName() + ".IDENTITIES";
 
   public static KettleSftpFileSystemConfigBuilder getInstance() {
     return builder;
@@ -100,19 +103,20 @@ public class KettleSftpFileSystemConfigBuilder extends KettleGenericFileSystemCo
           if ( name.equalsIgnoreCase( "AuthKeyPassphrase" ) ) {
             setParam( opts, UserInfo.class.getName(), new PentahoUserInfo( value ) );
           } else if ( name.equals( "identity" ) ) {
-            File[] identities = (File[]) this.getParam( opts, "identities" );
+
+            IdentityInfo[] identities = (IdentityInfo[]) this.getParam( opts, IDENTITY_KEY );
 
             if ( identities == null ) {
-              identities = new File[] { new File( value ) };
+              identities = new IdentityInfo[] { new IdentityInfo( new File( value ) ) };
             } else {
               // Copy, in a Java 5 friendly manner, identities into a larger array
-              File[] temp = new File[identities.length + 1];
+              IdentityInfo[] temp = new IdentityInfo[identities.length + 1];
               System.arraycopy( identities, 0, temp, 0, identities.length );
               identities = temp;
 
-              identities[identities.length - 1] = new File( value );
+              identities[identities.length - 1] = new IdentityInfo( new File( value ) );
             }
-            setParam( opts, "identities", identities );
+            setParam( opts, IDENTITY_KEY, identities );
           } else {
             super.setParameter( opts, name, value, fullParameterName, vfsUrl );
           }
