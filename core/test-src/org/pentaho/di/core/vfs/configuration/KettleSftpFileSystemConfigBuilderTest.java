72c1f5f199abaf791d9f697277e223d0aa85dd57
Matt Tucker <matt.tucker@pentaho.com>
2016-07-11 17:00:38 -0400

    [PDI-15501] VFS SFTP Identity Fix
    Change to IdentityInfo as needed by VFS 2.1

--- core/test-src/org/pentaho/di/core/vfs/configuration/KettleSftpFileSystemConfigBuilderTest.java
+++ core/test-src/org/pentaho/di/core/vfs/configuration/KettleSftpFileSystemConfigBuilderTest.java
@@ -22,15 +22,17 @@
 
 package org.pentaho.di.core.vfs.configuration;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
 import org.apache.commons.vfs2.FileSystemOptions;
+import org.apache.commons.vfs2.provider.sftp.IdentityInfo;
 import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
 import org.junit.Test;
 import org.pentaho.di.core.Const;
 import org.springframework.util.ReflectionUtils;
-
-import java.lang.reflect.Method;
-
-import static org.junit.Assert.*;
 
 /**
  * @author Andrey Khayrutdinov
@@ -53,4 +55,23 @@ public class KettleSftpFileSystemConfigBuilderTest {
     assertEquals( true, value );
   }
 
+  @Test
+  public void recognizesAndSetsIdentityKeyFile() throws Exception {
+    File tempFile = File.createTempFile( "KettleSftpFileSystemConfigBuilderTest", ".tmp" );
+    tempFile.deleteOnExit();
+
+    final String fullName = "vfs.sftp.identity";
+    final String name = fullName.substring( "vfs.sftp.".length() );
+    final String vfsInternalName = SftpFileSystemConfigBuilder.class.getName() + ".IDENTITIES";
+
+    final FileSystemOptions opts = new FileSystemOptions();
+    KettleSftpFileSystemConfigBuilder builder = KettleSftpFileSystemConfigBuilder.getInstance();
+    builder.setParameter( opts, name, tempFile.getAbsolutePath(), fullName, "sftp://fake-url:22" );
+
+    Method getOption = ReflectionUtils.findMethod( opts.getClass(), "getOption", Class.class, String.class );
+    getOption.setAccessible( true );
+    Object value = ReflectionUtils.invokeMethod( getOption, opts, builder.getConfigClass(), vfsInternalName );
+    assertEquals( IdentityInfo[].class, value.getClass() );
+    assertEquals( tempFile.getAbsolutePath(), ( (IdentityInfo[]) value )[0].getPrivateKey().getAbsolutePath() );
+  }
 }
