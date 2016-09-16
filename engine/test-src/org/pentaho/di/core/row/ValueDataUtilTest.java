18ed34c28f3a6b58e14477224a35859fa489613c
Denis Protopopov <denis_protopopov@epam.com>
2016-09-16 13:10:39 +0400

    [TEST] fixed failing tests - fixed expected values

--- engine/test-src/org/pentaho/di/core/row/ValueDataUtilTest.java
+++ engine/test-src/org/pentaho/di/core/row/ValueDataUtilTest.java
@@ -172,7 +172,7 @@ public class ValueDataUtilTest {
     Object daysDiff =
         calculate( "2010-05-12", "2010-01-01", ValueMetaInterface.TYPE_DATE,
             CalculatorMetaFunction.CALC_DATE_WORKING_DIFF );
-    assertEquals( new Long( 93 ), daysDiff );
+    assertEquals( new Long( 94 ), daysDiff );
   }
 
   @Test
@@ -180,7 +180,7 @@ public class ValueDataUtilTest {
     Object daysDiff =
         calculate( "2010-12-31", "2011-02-10", ValueMetaInterface.TYPE_DATE,
             CalculatorMetaFunction.CALC_DATE_WORKING_DIFF );
-    assertEquals( new Long( -29 ), daysDiff );
+    assertEquals( new Long( -30 ), daysDiff );
   }
 
   @Test
