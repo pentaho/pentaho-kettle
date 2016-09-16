4981ffb5e8439be5c05cc6ce364dc63c1e3922db
Kanstantsin Karneliuk <kanstantsin_karneliuk@epam.com>
2016-09-16 19:02:51 +0300

    Revert "Merge pull request #2853 from AliaksandrShuhayeu/SP-2848-PDI-15507"
    This reverts commit 9af12a86a375da9ddec429c6cd7c35c48f558c41, reversing
    changes made to 9f72ab5f51616bd3aef8b9308023a7b53e5a69ce.

--- core/src/org/pentaho/di/core/database/Database.java
+++ core/src/org/pentaho/di/core/database/Database.java
@@ -667,64 +667,28 @@ public class Database implements VariableSpace, LoggingObjectInterface {
       if ( connection == null ) {
         return; // Nothing to do...
       }
-      try {
-        if ( connection.isClosed() ) {
-          return; // Nothing to do...
-        }
-      } catch ( SQLException ex ) {
-        // cannot do anything about this but log it
-        log.logError( "Error checking closing connection:" + Const.CR + ex.getMessage() );
-        log.logError( Const.getStackTracker( ex ) );
+      if ( connection.isClosed() ) {
+        return; // Nothing to do...
       }
 
       if ( pstmt != null ) {
-        try {
-          pstmt.close();
-        } catch ( SQLException ex ) {
-          // cannot do anything about this but log it
-          log.logError( "Error closing statement:" + Const.CR + ex.getMessage() );
-          log.logError( Const.getStackTracker( ex ) );
-        }
+        pstmt.close();
         pstmt = null;
       }
       if ( prepStatementLookup != null ) {
-        try {
-          prepStatementLookup.close();
-        } catch ( SQLException ex ) {
-          // cannot do anything about this but log it
-          log.logError( "Error closing lookup statement:" + Const.CR + ex.getMessage() );
-          log.logError( Const.getStackTracker( ex ) );
-        }
+        prepStatementLookup.close();
         prepStatementLookup = null;
       }
       if ( prepStatementInsert != null ) {
-        try {
-          prepStatementInsert.close();
-        } catch ( SQLException ex ) {
-          // cannot do anything about this but log it
-          log.logError( "Error closing insert statement:" + Const.CR + ex.getMessage() );
-          log.logError( Const.getStackTracker( ex ) );
-        }
+        prepStatementInsert.close();
         prepStatementInsert = null;
       }
       if ( prepStatementUpdate != null ) {
-        try {
-          prepStatementUpdate.close();
-        } catch ( SQLException ex ) {
-          // cannot do anything about this but log it
-          log.logError( "Error closing update statement:" + Const.CR + ex.getMessage() );
-          log.logError( Const.getStackTracker( ex ) );
-        }
+        prepStatementUpdate.close();
         prepStatementUpdate = null;
       }
       if ( pstmt_seq != null ) {
-        try {
-          pstmt_seq.close();
-        } catch ( SQLException ex ) {
-          // cannot do anything about this but log it
-          log.logError( "Error closing seq statement:" + Const.CR + ex.getMessage() );
-          log.logError( Const.getStackTracker( ex ) );
-        }
+        pstmt_seq.close();
         pstmt_seq = null;
       }
 
@@ -737,34 +701,23 @@ public class Database implements VariableSpace, LoggingObjectInterface {
       } else {
         if ( !isAutoCommit() ) {
           // Do we really still need this commit??
-          try {
-            commit();
-          } catch ( KettleDatabaseException ex ) {
-            // cannot do anything about this but log it
-            log.logError( "Error committing:" + Const.CR + ex.getMessage() );
-            log.logError( Const.getStackTracker( ex ) );
-          }
+          commit();
         }
       }
+
+      closeConnectionOnly();
 
       try {
         ExtensionPointHandler.callExtensionPoint( log, KettleExtensionPoint.DatabaseDisconnected.id, this );
       } catch ( KettleException e ) {
         throw new KettleDatabaseException( e );
       }
+    } catch ( SQLException ex ) {
+      log.logError( "Error disconnecting from database:" + Const.CR + ex.getMessage() );
+      log.logError( Const.getStackTracker( ex ) );
     } catch ( KettleDatabaseException dbe ) {
       log.logError( "Error disconnecting from database:" + Const.CR + dbe.getMessage() );
       log.logError( Const.getStackTracker( dbe ) );
-    } finally {
-      // Always close the connection, irrespective of what happens above...
-      try {
-        closeConnectionOnly();
-      } catch ( KettleDatabaseException ignoredKde ) { // The only exception thrown from closeConnectionOnly()
-        // cannot do anything about this but log it
-        log.logError(
-          "Error disconnecting from database - closeConnectionOnly failed:" + Const.CR + ignoredKde.getMessage() );
-        log.logError( Const.getStackTracker( ignoredKde ) );
-      }
     }
   }
 
@@ -2751,7 +2704,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         cstmt = connection.prepareCall( sql );
         pos = 1;
         if ( !Const.isEmpty( returnvalue ) ) {
-          switch ( returntype ) {
+          switch( returntype ) {
             case ValueMetaInterface.TYPE_NUMBER:
               cstmt.registerOutParameter( pos, java.sql.Types.DOUBLE );
               break;
@@ -2777,7 +2730,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         }
         for ( int i = 0; i < arg.length; i++ ) {
           if ( argdir[ i ].equalsIgnoreCase( "OUT" ) || argdir[ i ].equalsIgnoreCase( "INOUT" ) ) {
-            switch ( argtype[ i ] ) {
+            switch( argtype[ i ] ) {
               case ValueMetaInterface.TYPE_NUMBER:
                 cstmt.registerOutParameter( i + pos, java.sql.Types.DOUBLE );
                 break;
@@ -3169,7 +3122,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         int precision = pmd.getScale( i );
         ValueMeta val;
 
-        switch ( sqltype ) {
+        switch( sqltype ) {
           case java.sql.Types.CHAR:
           case java.sql.Types.VARCHAR:
             val = new ValueMeta( name, ValueMetaInterface.TYPE_STRING );
@@ -3227,7 +3180,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
     for ( int x = 0; x < sql.length(); x++ ) {
       char c = sql.charAt( x );
 
-      switch ( c ) {
+      switch( c ) {
         case '\'':
           quote_opened = !quote_opened;
           break;
@@ -3293,7 +3246,8 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         StringBuffer sqlBuff = new StringBuffer( 250 );
         sqlBuff.append( "UPDATE " ).append( schemaTable ).append( " SET " );
 
-        for ( int i = 1; i < rowMeta.size(); i++ ) { // Without ID_JOB or ID_BATCH
+        for ( int i = 1; i < rowMeta.size(); i++ ) // Without ID_JOB or ID_BATCH
+        {
           ValueMetaInterface valueMeta = rowMeta.getValueMeta( i );
           if ( i > 1 ) {
             sqlBuff.append( ", " );
@@ -4246,7 +4200,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
       if ( resultname != null && resultname.length() != 0 ) {
         ValueMeta vMeta = new ValueMeta( resultname, resulttype );
         Object v = null;
-        switch ( resulttype ) {
+        switch( resulttype ) {
           case ValueMetaInterface.TYPE_BOOLEAN:
             v = Boolean.valueOf( cstmt.getBoolean( pos ) );
             break;
@@ -4291,7 +4245,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         if ( argdir[ i ].equalsIgnoreCase( "OUT" ) || argdir[ i ].equalsIgnoreCase( "INOUT" ) ) {
           ValueMetaInterface vMeta = ValueMetaFactory.createValueMeta( arg[ i ], argtype[ i ] );
           Object v = null;
-          switch ( argtype[ i ] ) {
+          switch( argtype[ i ] ) {
             case ValueMetaInterface.TYPE_BOOLEAN:
               v = Boolean.valueOf( cstmt.getBoolean( pos + i ) );
               break;
@@ -4471,7 +4425,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
         } else {
           // Normal cases...
           //
-          switch ( valueMeta.getType() ) {
+          switch( valueMeta.getType() ) {
             case ValueMetaInterface.TYPE_BOOLEAN:
             case ValueMetaInterface.TYPE_STRING:
               String string = valueMeta.getString( valueData );
