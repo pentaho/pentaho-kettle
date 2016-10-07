bf28d742914d8d18e157c2f3dc610f10dcc18c8b
Ezequiel Cuellar <ecuellar@pentaho.com>
2016-10-07 14:53:28 -0400

    [BACKLOG-11051] Hover state locking area (#3004)

--- ui/src/org/pentaho/di/ui/spoon/trans/TransGraphExtension.java
+++ ui/src/org/pentaho/di/ui/spoon/trans/TransGraphExtension.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -30,6 +30,7 @@ public class TransGraphExtension {
   private TransGraph transGraph;
   private MouseEvent event;
   private Point point;
+  private boolean preventDefault;
 
   public TransGraphExtension( TransGraph transGraph, MouseEvent event, Point point ) {
     this.transGraph = transGraph;
@@ -61,4 +62,11 @@ public class TransGraphExtension {
     this.point = point;
   }
 
+  public boolean isPreventDefault() {
+    return preventDefault;
+  }
+
+  public void setPreventDefault( boolean preventDefault ) {
+    this.preventDefault = preventDefault;
+  }
 }
