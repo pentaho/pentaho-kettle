41ece7168310a14471c582e4d52d7e8d8c654fd2
stanislau-strelchanka <stanislau_strelchanka@epam.com>
2016-11-30 16:39:56 +0300

    [PPP-3581] CVE-2015-0250 - Batik 1.7 is vulnerable to XXE in SVG to PNG and SVG to JPG conversion classes

--- core/src/org/pentaho/di/core/svg/SvgSupport.java
+++ core/src/org/pentaho/di/core/svg/SvgSupport.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -24,7 +24,7 @@ package org.pentaho.di.core.svg;
 
 import java.io.InputStream;
 
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.w3c.dom.Document;
 
