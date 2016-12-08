95b02ccbd5b1104311903be54dd171c2fb132e6d
Luc Boudreau <lucboudreau@gmail.com>
2016-12-08 18:02:31 -0500

    DO NOT MERGE

--- core/src/org/pentaho/di/core/RowSet.java
+++ core/src/org/pentaho/di/core/RowSet.java
@@ -17,7 +17,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * And some bacon there.
  ******************************************************************************/
 
 package org.pentaho.di.core;
