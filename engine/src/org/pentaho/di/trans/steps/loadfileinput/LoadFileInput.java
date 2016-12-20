a150f16330d12e97448eec5a4730d6c641efa505
PogI <ivan_pogodin@epam.com>
2016-12-20 17:28:11 +0400

    [PDI-14338] "Load file content in memory" component produces duplicate rows when "ignore empty file" is checked

--- engine/src/org/pentaho/di/trans/steps/loadfileinput/LoadFileInput.java
+++ engine/src/org/pentaho/di/trans/steps/loadfileinput/LoadFileInput.java
@@ -75,7 +75,7 @@ public class LoadFileInput extends BaseStep implements StepInterface {
     }
   }
 
-  private boolean openNextFile() {
+  boolean openNextFile() {
     try {
       if ( meta.getIsInFields() ) {
         data.readrow = getRow(); // Grab another row ...
@@ -174,7 +174,7 @@ public class LoadFileInput extends BaseStep implements StepInterface {
 
       if ( meta.isIgnoreEmptyFile() && data.fileSize == 0 ) {
         logError( BaseMessages.getString( PKG, "LoadFileInput.Error.FileSizeZero", "" + data.file.getName() ) );
-        openNextFile();
+        return openNextFile();
 
       } else {
         if ( isDetailed() ) {
