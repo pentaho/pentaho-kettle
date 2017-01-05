10c57940dc7c1a53fb84936324a93479d570deb1
mbatchelor <mbatchelor@pentaho.com>
2017-01-05 13:26:27 -0500

    [CHECKSTYLE] - Fixed formatting in new class

--- ui/test-src/org/pentaho/di/ui/spoon/trans/TransGraphTest.java
+++ ui/test-src/org/pentaho/di/ui/spoon/trans/TransGraphTest.java
@@ -24,7 +24,6 @@ package org.pentaho.di.ui.spoon.trans;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import junit.framework.Assert;
 
 import org.eclipse.swt.events.MouseEvent;
 import org.junit.Test;
@@ -36,61 +35,63 @@ import org.pentaho.di.trans.step.StepErrorMeta;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.ui.spoon.Spoon;
 
+import junit.framework.Assert;
+
 public class TransGraphTest {
-    private static final boolean TRUE_RESULT = true;
+  private static final boolean TRUE_RESULT = true;
 
-    @Test
-    public void testMouseUpHopGetsSelected() {
-        try {
-            MouseEvent event = mock( MouseEvent.class );
-            int x = 0, y = 0;
+  @Test
+  public void testMouseUpHopGetsSelected() {
+    try {
+      MouseEvent event = mock( MouseEvent.class );
+      int x = 0, y = 0;
 
-            TransGraph transGraph = mock( TransGraph.class );
-            StepMeta stepMeta = mock( StepMeta.class );
-            StepErrorMeta errorMeta = new StepErrorMeta( null, null );
-            TransHopMeta selectedHop = new TransHopMeta();
-            selectedHop.setErrorHop( true );
-            selectedHop.setEnabled( TRUE_RESULT );
-            selectedHop.setFromStep( stepMeta );
+      TransGraph transGraph = mock( TransGraph.class );
+      StepMeta stepMeta = mock( StepMeta.class );
+      StepErrorMeta errorMeta = new StepErrorMeta( null, null );
+      TransHopMeta selectedHop = new TransHopMeta();
+      selectedHop.setErrorHop( true );
+      selectedHop.setEnabled( TRUE_RESULT );
+      selectedHop.setFromStep( stepMeta );
 
-            when( stepMeta.getStepErrorMeta() ).thenReturn( errorMeta );
-            when( transGraph.findHop( x, y ) ).thenReturn( selectedHop );
-            when( transGraph.screen2real( any( Integer.class ), any( Integer.class ) ) ).thenReturn( new Point( x, y ) );
+      when( stepMeta.getStepErrorMeta() ).thenReturn( errorMeta );
+      when( transGraph.findHop( x, y ) ).thenReturn( selectedHop );
+      when( transGraph.screen2real( any( Integer.class ), any( Integer.class ) ) ).thenReturn( new Point( x, y ) );
 
-            Mockito.doCallRealMethod().when( transGraph ).mouseUp( event );
-            transGraph.mouseUp( event );
+      Mockito.doCallRealMethod().when( transGraph ).mouseUp( event );
+      transGraph.mouseUp( event );
 
-            Assert.assertTrue( errorMeta.isEnabled() );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+      Assert.assertTrue( errorMeta.isEnabled() );
+    } catch ( Exception e ) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    public void testEnableHopGetsSelected() {
-        try {
-            TransGraph transGraph = mock( TransGraph.class );
-            Mockito.doCallRealMethod().when( transGraph ).setTransMeta( any( TransMeta.class ) );
-            Mockito.doCallRealMethod().when( transGraph ).setSpoon( any( Spoon.class ) );
-            transGraph.setTransMeta( new TransMeta() );
-            transGraph.setSpoon( mock( Spoon.class ) );
-            StepMeta stepMeta = mock( StepMeta.class );
-            StepErrorMeta errorMeta = new StepErrorMeta( null, null );
-            TransHopMeta selectedHop = new TransHopMeta();
-            selectedHop.setErrorHop( true );
-            selectedHop.setEnabled( false );
-            selectedHop.setFromStep( stepMeta );
+  @Test
+  public void testEnableHopGetsSelected() {
+    try {
+      TransGraph transGraph = mock( TransGraph.class );
+      Mockito.doCallRealMethod().when( transGraph ).setTransMeta( any( TransMeta.class ) );
+      Mockito.doCallRealMethod().when( transGraph ).setSpoon( any( Spoon.class ) );
+      transGraph.setTransMeta( new TransMeta() );
+      transGraph.setSpoon( mock( Spoon.class ) );
+      StepMeta stepMeta = mock( StepMeta.class );
+      StepErrorMeta errorMeta = new StepErrorMeta( null, null );
+      TransHopMeta selectedHop = new TransHopMeta();
+      selectedHop.setErrorHop( true );
+      selectedHop.setEnabled( false );
+      selectedHop.setFromStep( stepMeta );
 
-            when( stepMeta.getStepErrorMeta() ).thenReturn( errorMeta );
-            selectedHop.setToStep( new StepMeta() );
-            when( transGraph.getCurrentHop() ).thenReturn( selectedHop );
+      when( stepMeta.getStepErrorMeta() ).thenReturn( errorMeta );
+      selectedHop.setToStep( new StepMeta() );
+      when( transGraph.getCurrentHop() ).thenReturn( selectedHop );
 
-            Mockito.doCallRealMethod().when( transGraph ).enableHop();
-            transGraph.enableHop();
+      Mockito.doCallRealMethod().when( transGraph ).enableHop();
+      transGraph.enableHop();
 
-            Assert.assertTrue( errorMeta.isEnabled() );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+      Assert.assertTrue( errorMeta.isEnabled() );
+    } catch ( Exception e ) {
+      e.printStackTrace();
     }
+  }
 }
