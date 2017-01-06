287d75e0d9fd2e72c82430b935c5efdaa685f957
Chris Caspanello <ccaspanello@pentaho.com>
2017-01-06 13:58:19 -0500

    Renamed execution-engine-plugin to 'classic-engine-plugin' and created a new bundle for the spark engine where I will merge some POC code in later

--- plugins/spark-engine-plugin/src/main/java/org/pentaho/engine/spark/SparkEngine.java
+++ plugins/spark-engine-plugin/src/main/java/org/pentaho/engine/spark/SparkEngine.java
@@ -1,14 +1,13 @@
-package org.pentaho.engine.foobar;
+package org.pentaho.engine.spark;
 
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.engine.ExecutionEngine;
 import org.pentaho.di.engine.IExecutionEngine;
 import org.pentaho.di.trans.Trans;
 
-@ExecutionEngine( id = "FooBarOSGiEngine", name= "Foo Bar OSGi Engine" )
-public class FooBarOSGiEngine implements IExecutionEngine {
+@ExecutionEngine( id = "SparkEngine", name= "Spark Engine" )
+public class SparkEngine implements IExecutionEngine {
 
   @Override public void execute( Trans trans ) throws KettleException {
-
   }
 }
