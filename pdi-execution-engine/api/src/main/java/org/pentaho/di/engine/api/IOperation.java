b168922a610aff7f1e0722d1d55f27fd5359a600
Nick Hudak <hudak@users.noreply.github.com>
2017-01-16 12:08:35 -0500

    Update IOperation.java
    Removed overridden method getId()

--- pdi-execution-engine/api/src/main/java/org/pentaho/di/engine/api/IOperation.java
+++ pdi-execution-engine/api/src/main/java/org/pentaho/di/engine/api/IOperation.java
@@ -14,8 +14,6 @@ import java.util.List;
  */
 public interface IOperation extends IReportingEventSource {
 
-  @Override String getId();
-
   List<IOperation> getFrom();
   List<IOperation> getTo();
 
