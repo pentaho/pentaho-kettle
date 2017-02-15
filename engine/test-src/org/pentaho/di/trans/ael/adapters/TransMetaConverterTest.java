2ff84435c141eba32a0e326bf297a63a9752f26d
mkambol <mkambol@gmail.com>
2017-02-15 14:29:39 -0500

    [BACKLOG-14636] Enforcing serializable config items in ael HasConfig
    Related: updated TransMetaConverter to pass the transmeta xml in config,
    instead of TransMeta.

--- engine/test-src/org/pentaho/di/trans/ael/adapters/TransMetaConverterTest.java
+++ engine/test-src/org/pentaho/di/trans/ael/adapters/TransMetaConverterTest.java
@@ -25,8 +25,12 @@
 package org.pentaho.di.trans.ael.adapters;
 
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.engine.api.model.Transformation;
 import org.pentaho.di.trans.TransHopMeta;
 import org.pentaho.di.trans.TransMeta;
@@ -35,17 +39,26 @@ import org.pentaho.di.trans.step.StepMetaInterface;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
 
-
+@RunWith ( MockitoJUnitRunner.class )
 public class TransMetaConverterTest {
 
   @Mock StepMetaInterface stepMetaInterface;
+
+  final String XML = "<xml></xml>";
+
+  @Before
+  public void before() throws KettleException {
+
+    when( stepMetaInterface.getXML() ).thenReturn( XML );
+  }
 
   @Test
   public void simpleConvert() {
     TransMeta meta = new TransMeta();
     meta.setName( "transName" );
-    meta.addStep( new StepMeta( "stepName", null ) );
+    meta.addStep( new StepMeta( "stepName", stepMetaInterface ) );
     Transformation trans = TransMetaConverter.convert( meta );
     assertThat( trans.getId(), is( meta.getName() ) );
     assertThat( trans.getOperations().size(), is( 1 ) );
@@ -56,9 +69,9 @@ public class TransMetaConverterTest {
   public void transWithHops() {
     TransMeta meta = new TransMeta();
     meta.setName( "transName" );
-    StepMeta from = new StepMeta( "step1", null );
+    StepMeta from = new StepMeta( "step1",  stepMetaInterface );
     meta.addStep( from );
-    StepMeta to = new StepMeta( "step2", null );
+    StepMeta to = new StepMeta( "step2",  stepMetaInterface );
     meta.addStep( to );
     meta.addTransHop( new TransHopMeta( from, to ) );
     Transformation trans = TransMetaConverter.convert( meta );
