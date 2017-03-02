3eff6c89a4dbbfabab3eac130f4fa4d5e5c05cae
Alexander Buloichik <alexander_buloichik@epam.com>
2017-03-02 17:45:32 +0300

    [BACKLOG-14795] Add a flag to Abort step to prevent abort with error of transformation - engine changes (#3581)

--- engine/src/org/pentaho/di/trans/steps/abort/Abort.java
+++ engine/src/org/pentaho/di/trans/steps/abort/Abort.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -92,7 +92,9 @@ public class Abort extends BaseStep implements StepInterface {
         } else {
           logError( message );
         }
-        setErrors( 1 );
+        if ( meta.isAbortWithError() ) {
+          setErrors( 1 );
+        }
         stopAll();
       } else {
         // seen a row but not yet reached the threshold
