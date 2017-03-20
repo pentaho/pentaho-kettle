771d0fcda487d3379e12d73978445e33b8f702bd
Nick Baker <nbaker@pentaho.com>
2017-03-19 20:50:29 -0400

    [BACKLOG-15105] Extending PluginRegistry to allow additional Plugin mappings after plugin construction.

--- core/test-src/org/pentaho/di/core/plugins/PluginRegistryUnitTest.java
+++ core/test-src/org/pentaho/di/core/plugins/PluginRegistryUnitTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -24,11 +24,17 @@ package org.pentaho.di.core.plugins;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.pentaho.di.core.exception.KettlePluginException;
+import org.pentaho.di.core.logging.LoggingPluginType;
 import org.pentaho.di.core.row.RowBuffer;
 import org.pentaho.di.core.row.ValueMetaInterface;
+
+import java.util.UUID;
 
 public class PluginRegistryUnitTest {
 
@@ -41,5 +47,35 @@ public class PluginRegistryUnitTest {
     for ( ValueMetaInterface vmi : result.getRowMeta().getValueMetaList() ) {
       assertEquals( ValueMetaInterface.TYPE_STRING, vmi.getType() );
     }
+  }
+
+  /**
+   * Test that additional plugin mappings can be added via the PluginRegistry.
+   */
+  @Test
+  public void testSupplementalPluginMappings() throws Exception {
+    PluginRegistry registry = PluginRegistry.getInstance();
+    PluginInterface mockPlugin = mock( PluginInterface.class );
+    when( mockPlugin.getIds() ).thenReturn( new String[] { "mockPlugin"} );
+    when( mockPlugin.matches( "mockPlugin" ) ).thenReturn( true );
+    when( mockPlugin.getName() ).thenReturn( "mockPlugin" );
+    doReturn( LoggingPluginType.class ).when( mockPlugin ).getPluginType();
+    registry.registerPlugin( LoggingPluginType.class, mockPlugin );
+
+
+    registry.addClassFactory( LoggingPluginType.class, String.class, "mockPlugin", () -> { return "Foo"; } );
+    String result = registry.loadClass( LoggingPluginType.class, "mockPlugin", String.class );
+    assertEquals( "Foo", result );
+    assertEquals( 2, registry.getPlugins( LoggingPluginType.class ).size() );
+
+
+    // Now add another mapping and verify that it works and the existing supplementalPlugin was reused.
+    UUID uuid = UUID.randomUUID();
+    registry.addClassFactory( LoggingPluginType.class, UUID.class, "mockPlugin", () -> uuid );
+    UUID out = registry.loadClass( LoggingPluginType.class, "mockPlugin", UUID.class );
+    assertEquals( uuid, out );
+    assertEquals( 2, registry.getPlugins( LoggingPluginType.class ).size() );
+
+
   }
 }
