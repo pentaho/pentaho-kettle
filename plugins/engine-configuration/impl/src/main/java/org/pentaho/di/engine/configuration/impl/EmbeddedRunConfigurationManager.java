4092aafe441a60735d5e0182a2aed5fcfeae4d8a
Tim Kafalas tkafalas <tkafalas@pentaho.com>
2017-06-23 15:00:00 -0400

    [BACKLOG-17014]  Provide key driven Mulit-provider MetastoreLocator

--- plugins/engine-configuration/impl/src/main/java/org/pentaho/di/engine/configuration/impl/EmbeddedRunConfigurationManager.java
+++ plugins/engine-configuration/impl/src/main/java/org/pentaho/di/engine/configuration/impl/EmbeddedRunConfigurationManager.java
@@ -24,13 +24,15 @@
 
 package org.pentaho.di.engine.configuration.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.pentaho.di.core.attributes.metastore.EmbeddedMetaStore;
 import org.pentaho.di.engine.configuration.api.RunConfigurationProvider;
 import org.pentaho.di.engine.configuration.impl.pentaho.DefaultRunConfigurationProvider;
 import org.pentaho.di.engine.configuration.impl.spark.SparkRunConfigurationProvider;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.pentaho.metastore.api.IMetaStore;
+import org.pentaho.osgi.metastore.locator.api.MetastoreLocator;
 
 /**
  * Created by bmorrise on 5/4/17.
@@ -38,9 +40,9 @@ import java.util.List;
 public class EmbeddedRunConfigurationManager {
   public static RunConfigurationManager build( EmbeddedMetaStore embeddedMetaStore ) {
     DefaultRunConfigurationProvider defaultRunConfigurationProvider =
-      new DefaultRunConfigurationProvider( () -> embeddedMetaStore, null );
+      new DefaultRunConfigurationProvider( createMetastoreLocator( embeddedMetaStore ), null );
     SparkRunConfigurationProvider sparkRunConfigurationProvider =
-      new SparkRunConfigurationProvider( () -> embeddedMetaStore, null );
+      new SparkRunConfigurationProvider( createMetastoreLocator( embeddedMetaStore ), null );
 
     List<RunConfigurationProvider> runConfigurationProviders = new ArrayList<>();
     runConfigurationProviders.add( defaultRunConfigurationProvider );
@@ -48,4 +50,20 @@ public class EmbeddedRunConfigurationManager {
 
     return new RunConfigurationManager( runConfigurationProviders );
   }
+
+  private static MetastoreLocator createMetastoreLocator( IMetaStore embeddedMetaStore ) {
+    return new MetastoreLocator() {
+
+      @Override
+      public IMetaStore getMetastore( String providerKey ) {
+        return embeddedMetaStore;
+      }
+
+      @Override
+      public IMetaStore getMetastore() {
+        return embeddedMetaStore;
+      }
+    };
+  }
+
 }
