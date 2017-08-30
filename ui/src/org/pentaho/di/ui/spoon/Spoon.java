a7fa77f6e38e58ba7261b0ed667eb2531c451b8a
Artsiom Yurhevich <Artsiom_Yurhevich@epam.com>
2017-08-30 16:02:11 +0300

    Revert "[SP-3723][PDI-16416]-Disconnect from a repository does not prompt the user to save the files before disconnect"
    This reverts commit 23ac1b7861e9a32183cce362a971a1e7aa785876.

--- ui/src/org/pentaho/di/ui/spoon/Spoon.java
+++ ui/src/org/pentaho/di/ui/spoon/Spoon.java
@@ -4191,11 +4191,7 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
 
   public void closeRepository() {
     if ( rep != null ) {
-      try {
-        promptForSave();
-      } catch ( KettleException e ) {
-        log.logError( "Error saving Job or Transformation", e );
-      }
+
       // Prompt and close all tabs as user disconnected from the repo
       boolean shouldDisconnect = Spoon.getInstance().closeAllJobsAndTransformations( true );
       if ( shouldDisconnect ) {
