d3fc4108269e2c4c4fd1c76f18acb1a4575b3e18
Benjamin Morrise <bmorrise@pentaho.com>
2017-10-16 09:32:47 -0400

    [BACKLOG-18993] Made text in run configuration uneditable

--- plugins/engine-configuration/impl/src/main/java/org/pentaho/di/engine/configuration/impl/pentaho/DefaultRunConfigurationUI.java
+++ plugins/engine-configuration/impl/src/main/java/org/pentaho/di/engine/configuration/impl/pentaho/DefaultRunConfigurationUI.java
@@ -126,6 +126,7 @@ public class DefaultRunConfigurationUI implements RunConfigurationUI {
     wcLocal.setLayout( new GridLayout() );
 
     Text wlLocal = new Text( wcLocal, SWT.MULTI | SWT.WRAP );
+    wlLocal.setEditable( false );
     props.setLook( wlLocal );
     wlLocal.setText( BaseMessages.getString( PKG, "DefaultRunConfigurationDialog.Text.Local" ) );
     GridData gdlLocal = new GridData( GridData.FILL_HORIZONTAL );
@@ -144,6 +145,7 @@ public class DefaultRunConfigurationUI implements RunConfigurationUI {
     wcPentaho.setLayout( new GridLayout() );
 
     Text wlPentaho = new Text( wcPentaho, SWT.MULTI | SWT.WRAP | SWT.LEFT );
+    wlPentaho.setEditable( false );
     props.setLook( wlPentaho );
     wlPentaho.setText( BaseMessages.getString( PKG, "DefaultRunConfigurationDialog.Text.Pentaho" ) );
     GridData gdlPentaho = new GridData( GridData.FILL_HORIZONTAL );
