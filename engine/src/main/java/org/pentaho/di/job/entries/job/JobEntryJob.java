11789be1319dfdd765242abc742064b836a04898
ArtsiomYurhevich <Artsiom_Yurhevich@epam.com>
2017-12-12 03:56:57 +0300

    [PDI-16436]- Issues with several steps during exporting - importing in an EE repository : directories are not updated properly. Affected steps : JobEntryJob, JobEntryTrans, Mapping, SimpleMapping, SingleThreader, TransExecutor, JobExecutor, Metainject, Pentaho MapReduce (#4381)

--- engine/src/main/java/org/pentaho/di/job/entries/job/JobEntryJob.java
+++ engine/src/main/java/org/pentaho/di/job/entries/job/JobEntryJob.java
@@ -59,6 +59,7 @@ import org.pentaho.di.job.entry.JobEntryBase;
 import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.job.entry.validator.AndValidator;
 import org.pentaho.di.job.entry.validator.JobEntryValidatorUtils;
+import org.pentaho.di.repository.HasRepositoryDirectories;
 import org.pentaho.di.repository.ObjectId;
 import org.pentaho.di.repository.Repository;
 import org.pentaho.di.repository.RepositoryDirectory;
@@ -92,7 +93,7 @@ import java.util.UUID;
  * @since 01-10-2003, Rewritten on 18-06-2004
  *
  */
-public class JobEntryJob extends JobEntryBase implements Cloneable, JobEntryInterface {
+public class JobEntryJob extends JobEntryBase implements Cloneable, JobEntryInterface, HasRepositoryDirectories {
   private static Class<?> PKG = JobEntryJob.class; // for i18n purposes, needed by Translator2!!
 
   private String filename;
@@ -215,6 +216,16 @@ public class JobEntryJob extends JobEntryBase implements Cloneable, JobEntryInte
 
   public void setDirectory( String directory ) {
     this.directory = directory;
+  }
+
+  @Override
+  public String[] getDirectories() {
+    return new String[]{ directory };
+  }
+
+  @Override
+  public void setDirectories( String[] directories ) {
+    this.directory = directories[0];
   }
 
   public boolean isPassingExport() {
@@ -1609,6 +1620,11 @@ public class JobEntryJob extends JobEntryBase implements Cloneable, JobEntryInte
    */
   public ObjectLocationSpecificationMethod getSpecificationMethod() {
     return specificationMethod;
+  }
+
+  @Override
+  public ObjectLocationSpecificationMethod[] getSpecificationMethods() {
+    return new ObjectLocationSpecificationMethod[]{ specificationMethod };
   }
 
   /**
--- engine/src/main/java/org/pentaho/di/job/entries/trans/JobEntryTrans.java
+++ engine/src/main/java/org/pentaho/di/job/entries/trans/JobEntryTrans.java
@@ -62,6 +62,7 @@ import org.pentaho.di.job.entry.JobEntryBase;
 import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.job.entry.validator.AndValidator;
 import org.pentaho.di.job.entry.validator.JobEntryValidatorUtils;
+import org.pentaho.di.repository.HasRepositoryDirectories;
 import org.pentaho.di.repository.ObjectId;
 import org.pentaho.di.repository.Repository;
 import org.pentaho.di.repository.RepositoryDirectory;
@@ -91,7 +92,7 @@ import org.w3c.dom.Node;
  * @author Matt Casters
  * @since 1-Oct-2003, rewritten on 18-June-2004
  */
-public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryInterface {
+public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryInterface, HasRepositoryDirectories {
   private static Class<?> PKG = JobEntryTrans.class; // for i18n purposes, needed by Translator2!!
 
   private String transname;
@@ -231,6 +232,16 @@ public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryIn
 
   public void setDirectory( String directory ) {
     this.directory = directory;
+  }
+
+  @Override
+  public String[] getDirectories() {
+    return new String[]{ directory };
+  }
+
+  @Override
+  public void setDirectories( String[] directories ) {
+    this.directory = directories[0];
   }
 
   public String getLogFilename() {
@@ -1596,6 +1607,11 @@ public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryIn
    */
   public ObjectLocationSpecificationMethod getSpecificationMethod() {
     return specificationMethod;
+  }
+
+  @Override
+  public ObjectLocationSpecificationMethod[] getSpecificationMethods() {
+    return new ObjectLocationSpecificationMethod[] { specificationMethod };
   }
 
   /**
