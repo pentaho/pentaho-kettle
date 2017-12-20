405c587fb2fba0497a95611366a822bd20fb1b0f
Luis Martins <luis.martins@hitachivantara.com>
2017-12-20 09:59:12 +0000

    [PDI-16707] Date format is not retained after performing Memory Group by.
    [PDI-16707] Date format is not retained after performing Memory Group by.

--- engine/src/main/java/org/pentaho/di/trans/steps/groupby/GroupByMeta.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/groupby/GroupByMeta.java
@@ -530,7 +530,11 @@ public class GroupByMeta extends BaseStepMeta implements StepMetaInterface {
           }
           v.setOrigin( origin );
           v.setLength( length, precision );
-          v.setConversionMask( subj.getConversionMask() );
+
+          if ( subj != null ) {
+            v.setConversionMask( subj.getConversionMask() );
+          }
+
           fields.addValueMeta( v );
         }
       }
