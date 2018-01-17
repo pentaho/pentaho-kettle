5e4eb8a06031b2faae0a8ca30b8c54b4c0ff5e03
Artsiom Yurhevich <Artsiom_Yurhevich@epam.com>
2018-01-17 16:05:07 +0300

    [SP-4096][PDI-16798]-Get Files Rows Count - changing checkboxes does not prompt for save

--- ui/src/org/pentaho/di/ui/trans/steps/getfilesrowscount/GetFilesRowsCountDialog.java
+++ ui/src/org/pentaho/di/ui/trans/steps/getfilesrowscount/GetFilesRowsCountDialog.java
@@ -1,8 +1,9 @@
+
 /*! ******************************************************************************
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -584,6 +585,12 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
     fdSmartCount.left = new FormAttachment( middle, 0 );
     fdSmartCount.top = new FormAttachment( wRowSeparator, margin );
     wSmartCount.setLayoutData( fdSmartCount );
+    wSmartCount.addSelectionListener( new SelectionAdapter() {
+      @Override
+      public void widgetSelected( SelectionEvent selectionEvent ) {
+        input.setChanged();
+      }
+    } );
 
     fdRowSeparatorGroup = new FormData();
     fdRowSeparatorGroup.left = new FormAttachment( 0, margin );
@@ -681,6 +688,12 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
     fdAddResult.left = new FormAttachment( middle, 0 );
     fdAddResult.top = new FormAttachment( wAdditionalGroup, margin );
     wAddResult.setLayoutData( fdAddResult );
+    wAddResult.addSelectionListener( new SelectionAdapter() {
+      @Override
+      public void widgetSelected( SelectionEvent selectionEvent ) {
+        input.setChanged();
+      }
+    } );
 
     fdAddFileResult = new FormData();
     fdAddFileResult.left = new FormAttachment( 0, margin );
@@ -778,6 +791,7 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
         wFilenameList.remove( idx );
         wFilenameList.removeEmptyRows();
         wFilenameList.setRowNums();
+        input.setChanged();
       }
     } );
 
@@ -794,6 +808,7 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
         }
         wFilenameList.removeEmptyRows();
         wFilenameList.setRowNums();
+        input.setChanged();
       }
     } );
 
@@ -829,6 +844,7 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
     wInclFilesCount.addSelectionListener( new SelectionAdapter() {
       public void widgetSelected( SelectionEvent e ) {
         setIncludeRownum();
+        input.setChanged();
       }
     } );
 
@@ -968,6 +984,7 @@ public class GetFilesRowsCountDialog extends BaseStepDialog implements StepDialo
       wFilenameList.optWidth( true );
     }
     wInclFilesCount.setSelection( in.includeCountFiles() );
+    wSmartCount.setSelection( in.isSmartCount() );
 
     if ( in.getFilesCountFieldName() != null ) {
       wInclFilesCountField.setText( in.getFilesCountFieldName() );
