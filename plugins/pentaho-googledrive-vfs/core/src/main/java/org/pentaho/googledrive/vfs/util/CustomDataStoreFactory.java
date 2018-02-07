b0ed249238579306ed88b01c843fa5745d4b9729
Ezequiel Cuellar <713962+e-cuellar@users.noreply.github.com>
2018-02-07 11:08:07 -0500

    [BACKLOG-20717] Google Drive VFS pressing Close button throws an exception (#4898)

--- plugins/pentaho-googledrive-vfs/core/src/main/java/org/pentaho/googledrive/vfs/util/CustomDataStoreFactory.java
+++ plugins/pentaho-googledrive-vfs/core/src/main/java/org/pentaho/googledrive/vfs/util/CustomDataStoreFactory.java
@@ -1,5 +1,5 @@
 /*!
-* Copyright (C) 2017 by Hitachi Vantara : http://www.pentaho.com
+* Copyright (C) 2018 by Hitachi Vantara : http://www.pentaho.com
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -21,22 +21,40 @@ import com.google.api.client.util.Lists;
 import com.google.api.client.util.Maps;
 import com.google.api.client.util.Preconditions;
 import com.google.api.client.util.store.AbstractDataStore;
+import com.google.api.client.util.store.AbstractDataStoreFactory;
 import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
 
-import java.io.*;
-import java.util.*;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Iterator;
 
-public class CustomDataStoreFactory extends FileDataStoreFactory {
+public class CustomDataStoreFactory extends AbstractDataStoreFactory {
+
+  private final File dataDirectory;
 
   public CustomDataStoreFactory( File dataDirectory ) throws IOException {
-    super( dataDirectory );
+    dataDirectory = dataDirectory.getCanonicalFile();
+    this.dataDirectory = dataDirectory;
+    if ( IOUtils.isSymbolicLink( dataDirectory ) ) {
+      throw new IOException( "unable to use a symbolic link: " + dataDirectory );
+    } else if ( !dataDirectory.exists() && !dataDirectory.mkdirs() ) {
+      throw new IOException( "unable to create directory: " + dataDirectory );
+    }
   }
 
   protected <V extends Serializable> DataStore<V> createDataStore( String id ) throws IOException {
-    return new CustomDataStore( this, getDataDirectory(), id );
+    return new CustomDataStore( this, this.dataDirectory, id );
   }
 
   static class CustomDataStore<V extends Serializable> extends AbstractDataStore<V> {
@@ -45,7 +63,7 @@ public class CustomDataStoreFactory extends FileDataStoreFactory {
     private HashMap<String, byte[]> keyValueMap = Maps.newHashMap();
     private final Lock lock = new ReentrantLock();
 
-    CustomDataStore( FileDataStoreFactory dataStore, File dataDirectory, String id ) throws IOException {
+    CustomDataStore( CustomDataStoreFactory dataStore, File dataDirectory, String id ) throws IOException {
       super( dataStore, id );
       this.dataDirectory = dataDirectory;
       this.dataFile = new File( this.dataDirectory, getId() );
