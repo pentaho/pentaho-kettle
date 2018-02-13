7c809ffa70a5782f02e2cda584d0d1f35770f34f
Matt Campbell <mkambol@gmail.com>
2018-02-13 12:14:49 -0500

    [CLEANUP]  missing imports (#4922)

--- ui/src/main/java/org/pentaho/di/ui/spoon/trans/TransGraph.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/trans/TransGraph.java
@@ -193,6 +193,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
