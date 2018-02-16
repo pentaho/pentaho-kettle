1dfcaf7ba67938a2f780449a38bea49519fda536
Kurtis Walker <kwalker@pentaho.com>
2018-02-16 16:05:28 -0500

    [BACKLOG-20951] - row generator should not send row on next timer when stopped

--- engine/src/main/java/org/pentaho/di/trans/steps/rowgenerator/RowGenerator.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/rowgenerator/RowGenerator.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -208,7 +208,7 @@ public class RowGenerator extends BaseStep implements StepInterface {
       }
     }
 
-    if ( meta.isNeverEnding() || data.rowsWritten < data.rowLimit ) {
+    if ( ( meta.isNeverEnding() || data.rowsWritten < data.rowLimit ) && !isStopped() ) {
       r = data.outputRowMeta.cloneRow( data.outputRowData );
     } else {
       setOutputDone(); // signal end to receiver(s)
