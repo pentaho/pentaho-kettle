0ec13af279dee8f9d3ec426d3d3c9aae40d70c86
Valeran86 <valeran86@gmail.com>
2018-02-22 08:50:20 +0300

    Add correct crawling multiline message
    Replace zero or more spaces between `BaseMessages.getString(` 
    and `PKG` on exactly one space when `Translator2` is crawling translation
    keys from sources.
    
    Key like this don't parsed and stayed invisible in Translator2:
    miRowInsBef.setText( OsHelper.customizeMenuitemText( BaseMessages.getString(
      PKG, "TableView.menu.InsertBeforeRow" ) ) );

--- ui/src/main/java/org/pentaho/di/ui/i18n/MessagesSourceCrawler.java
+++ ui/src/main/java/org/pentaho/di/ui/i18n/MessagesSourceCrawler.java
@@ -349,6 +349,7 @@ public class MessagesSourceCrawler {
         if ( extraLine ) {
           line2 = reader.readLine();
           line += line2;
+          line = line.replaceAll( "BaseMessages.getString\\(\\s*PKG, ", "BaseMessages.getString( PKG," );
         }
       } while ( extraLine );
 
