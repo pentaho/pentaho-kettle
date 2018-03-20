57d7778bdd576ee6a30301e224a7c75422e6ce1b
Luis Martins <luis.martins@hitachivantara.com>
2018-03-20 15:38:53 +0000

    [SP-4211] Backport of PDI-15818 - sftp Job Entry relies on jzlib jar for compression feature which is missing since 5.0 (7.1 Suite)

--- engine/src/org/pentaho/di/job/entries/sftp/SFTPClient.java
+++ engine/src/org/pentaho/di/job/entries/sftp/SFTPClient.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -437,6 +437,11 @@ public class SFTPClient {
   public String getCompression() {
     if ( this.compression == null ) {
       return null;
+    }
+    if ( this.compression.equals( "zlib" ) ) {
+      // compatibility with OpenSSH implementation of delayed compression
+      // https://www.openssh.com/txt/draft-miller-secsh-compression-delayed-00.txt
+      return "zlib@openssh.com,zlib";
     }
     if ( this.compression.equals( "none" ) ) {
       return null;
