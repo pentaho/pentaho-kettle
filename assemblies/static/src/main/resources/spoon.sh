e90b6419c90236a16c4843bdcec7b9f22b3e74ae
Elio Freitas <elio.freitas@webdetails.pt>
2018-04-13 14:35:59 +0100

    [BACKLOG-22728] - DET - Segmentation Fault while using Select filter on Ubuntu

--- assemblies/static/src/main/resources/spoon.sh
+++ assemblies/static/src/main/resources/spoon.sh
@@ -139,6 +139,7 @@ case `uname -s` in
 
             HASWEBKITGTK=`ldconfig -p | grep webkitgtk-1.0`
             export LIBWEBKITGTK="$HASWEBKITGTK"
+            export JavaScriptCoreUseJIT=0
             if [ -z "$HASWEBKITGTK" ]; then
               echo "#######################################################################"
               echo "WARNING:  no libwebkitgtk-1.0 detected, some features will be unavailable"
