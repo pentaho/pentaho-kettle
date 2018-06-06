5035ce731007f03a2da33ae7c4812f30c6b7efb6
Ricardo Silva <ricardo.da.silva.ist@gmail.com>
2018-06-06 09:48:09 +0100

    [PDI-13276] Data Validator Array Index Out of Bounds error after removing validation

--- ui/src/main/java/org/pentaho/di/ui/trans/steps/validator/ValidatorDialog.java
+++ ui/src/main/java/org/pentaho/di/ui/trans/steps/validator/ValidatorDialog.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -960,6 +960,10 @@ public class ValidatorDialog extends BaseStepDialog implements StepDialogInterfa
           selectedField = null;
           wValidationsList.remove( index );
           enableFields();
+
+          if ( selectionList.size() > 0 ) {
+            wValidationsList.select( selectionList.size() - 1 );
+          }
         }
       }
     } );
