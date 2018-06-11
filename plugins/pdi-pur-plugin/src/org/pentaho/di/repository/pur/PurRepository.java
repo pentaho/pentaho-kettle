45db9e50f69322fe02b4cbdac5878ae50dfddcf8
Luis Martins <luis.martins@hitachivantara.com>
2018-06-11 15:43:19 +0100

    [SP-4374] Backport of PDI-17300 - 8.0 Execute Job fails when job name has .kjb Extension - 6.1 Does Not (7.1 Suite)

--- plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/PurRepository.java
+++ plugins/pdi-pur-plugin/src/org/pentaho/di/repository/pur/PurRepository.java
@@ -1,6 +1,6 @@
 //CHECKSTYLE:FileLength:OFF
 /*!
-* Copyright 2010 - 2017 Pentaho Corporation.  All rights reserved.
+* Copyright 2010 - 2018 Hitachi Vantara.  All rights reserved.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -800,7 +800,7 @@ public class PurRepository extends AbstractRepository implements Repository, Rec
     }
   }
 
-  private String getPath( final String name, final RepositoryDirectoryInterface repositoryDirectory,
+  protected String getPath( final String name, final RepositoryDirectoryInterface repositoryDirectory,
                           final RepositoryObjectType objectType ) {
 
     String path = null;
@@ -823,15 +823,6 @@ public class PurRepository extends AbstractRepository implements Repository, Rec
         return getDatabaseMetaParentFolderPath() + RepositoryFile.SEPARATOR + sanitizedName
             + RepositoryObjectType.DATABASE.getExtension();
       }
-      case TRANSFORMATION: {
-        // Check for null path
-        if ( path == null ) {
-          return null;
-        } else {
-          return path + ( path.endsWith( RepositoryFile.SEPARATOR ) ? "" : RepositoryFile.SEPARATOR ) + sanitizedName
-              + RepositoryObjectType.TRANSFORMATION.getExtension();
-        }
-      }
       case PARTITION_SCHEMA: {
         return getPartitionSchemaParentFolderPath() + RepositoryFile.SEPARATOR + sanitizedName
             + RepositoryObjectType.PARTITION_SCHEMA.getExtension();
@@ -844,13 +835,14 @@ public class PurRepository extends AbstractRepository implements Repository, Rec
         return getClusterSchemaParentFolderPath() + RepositoryFile.SEPARATOR + sanitizedName
             + RepositoryObjectType.CLUSTER_SCHEMA.getExtension();
       }
+      case TRANSFORMATION:
       case JOB: {
         // Check for null path
         if ( path == null ) {
           return null;
         } else {
           return path + ( path.endsWith( RepositoryFile.SEPARATOR ) ? "" : RepositoryFile.SEPARATOR ) + sanitizedName
-              + RepositoryObjectType.JOB.getExtension();
+              + ( sanitizedName.endsWith( objectType.getExtension() ) ? "" : objectType.getExtension() );
         }
       }
       default: {
