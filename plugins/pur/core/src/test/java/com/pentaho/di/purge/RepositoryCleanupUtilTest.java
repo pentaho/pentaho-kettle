e116ef88079955e47d83099bb9501f7372dd17b9
Luis Martins <luis.martins@hitachivantara.com>
2018-07-26 23:11:08 +0100

    [SP-4633] Backport of PDI-17467 - Kitchen, Pan, Carte, Import, PurgeUtility require plain-text password (8.1 Suite)

---
+++ plugins/pur/core/src/test/java/com/pentaho/di/purge/RepositoryCleanupUtilTest.java
@@ -0,0 +1,71 @@
+/*!
+ * Copyright 2010 - 2018 Hitachi Vantara.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.pentaho.di.purge;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Base64;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.reflect.Whitebox.getInternalState;
+import static org.powermock.reflect.Whitebox.setInternalState;
+
+@RunWith( PowerMockRunner.class )
+@PrepareForTest( Client.class )
+public class RepositoryCleanupUtilTest {
+
+  @Test
+  public void authenticateLoginCredentials() throws Exception {
+    RepositoryCleanupUtil util = mock( RepositoryCleanupUtil.class );
+    doCallRealMethod().when( util ).authenticateLoginCredentials();
+
+    setInternalState( util, "url", "http://localhost:8080/pentaho" );
+    setInternalState( util, "username", "admin" );
+    setInternalState( util, "password", "Encrypted 2be98afc86aa7f2e4bb18bd63c99dbdde" );
+
+    WebResource resource = mock( WebResource.class );
+    doReturn( "true" ).when( resource ).get( String.class );
+
+    Client client = mock( Client.class );
+    doCallRealMethod().when( client ).addFilter( any( HTTPBasicAuthFilter.class ) );
+    doCallRealMethod().when( client ).getHeadHandler();
+    doReturn( resource ).when( client ).resource( anyString() );
+
+    mockStatic( Client.class );
+    when( Client.create( any( ClientConfig.class ) ) ).thenReturn( client );
+    util.authenticateLoginCredentials();
+
+    // the expected value is: "Basic <base64 encoded username:password>"
+    assertEquals( "Basic " + new String( Base64.getEncoder().encode( "admin:password".getBytes( "utf-8" ) ) ),
+      getInternalState( client.getHeadHandler(), "authentication" ) );
+  }
+}
