5c5f889d89a6719536bd65f1e5eae4aa5f0d0bf6
Luis Martins <luis.martins@hitachivantara.com>
2018-09-26 09:37:40 +0100

    [SP-4653] Backport of PPP-4160 - Import-Export tool requires plain-text password (7.1 Suite)

--- core/src/org/pentaho/di/core/KettleClientEnvironment.java
+++ core/src/org/pentaho/di/core/KettleClientEnvironment.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -220,5 +220,13 @@ public class KettleClientEnvironment {
     }
 
     return KettleClientEnvironment.instance;
+  }
+
+  public static void reset() {
+    if ( KettleLogStore.isInitialized() ) {
+      KettleLogStore.getInstance().reset();
+    }
+    PluginRegistry.getInstance().reset();
+    initialized = null;
   }
 }
