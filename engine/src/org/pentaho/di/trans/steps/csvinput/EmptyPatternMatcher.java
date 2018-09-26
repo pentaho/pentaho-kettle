11730c02b4612c93d0b9160d630912724a57eb63
xiaoaowanghu@sina.com <xiaoaowanghu@sina.com>
2018-09-26 22:43:50 +0800

    fix the bug when the csvinput buffer splits the multiple chars of delimiter into 2 buffers the data is read incorrectly.

--- engine/src/org/pentaho/di/trans/steps/csvinput/EmptyPatternMatcher.java
+++ engine/src/org/pentaho/di/trans/steps/csvinput/EmptyPatternMatcher.java
@@ -24,8 +24,8 @@ package org.pentaho.di.trans.steps.csvinput;
 
 public class EmptyPatternMatcher implements PatternMatcherInterface {
 
-  public boolean matchesPattern( byte[] source, int location, byte[] pattern ) {
-    return false;
-  }
+	public boolean matchesPattern(byte[] source, int sourceSize, int location, byte[] patter) {		
+		return false;
+	}
 
 }
--- engine/src/org/pentaho/di/trans/steps/csvinput/SingleBytePatternMatcher.java
+++ engine/src/org/pentaho/di/trans/steps/csvinput/SingleBytePatternMatcher.java
@@ -24,8 +24,8 @@ package org.pentaho.di.trans.steps.csvinput;
 
 public class SingleBytePatternMatcher implements PatternMatcherInterface {
 
-  public boolean matchesPattern( byte[] source, int location, byte[] pattern ) {
-    return source[location] == pattern[0];
-  }
+	public boolean matchesPattern(byte[] source, int sourceSize, int location, byte[] pattern) {
+		return source[location] == pattern[0];
+	}
 
 }
