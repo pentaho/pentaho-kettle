91cb309c3ecdd8370a530f8f6cf6f490b5d5b530
Aleksandra Messier <pentaho-amessier@users.noreply.github.com>
2018-10-29 16:20:14 -0400

    Revert "[SP-4638] Backport of BACKLOG-21741 - Creating a data-lineage analyze…"

--- engine/src/main/java/org/pentaho/di/trans/steps/mapping/MappingMeta.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/mapping/MappingMeta.java
@@ -48,7 +48,6 @@ import org.pentaho.di.repository.StringObjectId;
 import org.pentaho.di.resource.ResourceEntry;
 import org.pentaho.di.resource.ResourceEntry.ResourceType;
 import org.pentaho.di.resource.ResourceReference;
-import org.pentaho.di.trans.ISubTransAwareMeta;
 import org.pentaho.di.trans.StepWithMappingMeta;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
@@ -79,8 +78,7 @@ import java.util.List;
  *
  */
 
-public class MappingMeta extends StepWithMappingMeta implements StepMetaInterface, HasRepositoryInterface,
-  ISubTransAwareMeta {
+public class MappingMeta extends StepWithMappingMeta implements StepMetaInterface, HasRepositoryInterface {
 
   private static Class<?> PKG = MappingMeta.class;
   private List<MappingIODefinition> inputMappings;
