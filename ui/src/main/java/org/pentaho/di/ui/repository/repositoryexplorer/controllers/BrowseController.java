0bfc83f525a7770dae1ed9e883d3a82b1da9fd84
Leonardo Coelho <leonardo.coelho@hitachivantara.com>
2018-11-19 15:12:36 +0000

    [SP-4821] Backport of PDI-17519 - The Public directory at root in the EE repo can be moved to into the Home directory and the change cannot be reverted (8.2 Suite)

--- ui/src/main/java/org/pentaho/di/ui/repository/repositoryexplorer/controllers/BrowseController.java
+++ ui/src/main/java/org/pentaho/di/ui/repository/repositoryexplorer/controllers/BrowseController.java
@@ -117,6 +117,8 @@ public class BrowseController extends AbstractXulEventHandler implements IUISupp
 
   private static final int DIALOG_WIDTH = 357, DIALOG_HEIGHT = 165, DIALOG_COLOR = SWT.COLOR_WHITE;
 
+  protected static final String HOME_PATH = "/home", PUBLIC_PATH = "/public";
+
   /**
    * Allows for lookup of a UIRepositoryDirectory by ObjectId. This allows the reuse of instances that are inside a UI
    * tree.
@@ -663,6 +665,15 @@ public class BrowseController extends AbstractXulEventHandler implements IUISupp
 
   // Object being dragged from the hierarchical folder tree
   public void onDragFromGlobalTree( DropEvent event ) {
+    for ( UIRepositoryDirectory uiRepositoryDirectory : this.getSelectedFolderItems() ) {
+      String path = uiRepositoryDirectory.getDirectory().getPath();
+
+      if ( path.equals( HOME_PATH ) || path.equals( PUBLIC_PATH ) ) {
+        event.setAccepted( false );
+        return;
+      }
+    }
+
     event.setAccepted( true );
   }
 
