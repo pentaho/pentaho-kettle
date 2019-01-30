47754d224908bff7c118d98b15325d595955553e
Luis Martins <luis.martins@hitachivantara.com>
2019-01-30 12:52:31 +0000

    [PDI-17824] Table Output step is ignoring the Target Schema field for MySQL and MariaDB

--- ui/src/main/java/org/pentaho/di/ui/spoon/job/JobHistoryDelegate.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/job/JobHistoryDelegate.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -241,18 +241,17 @@ public class JobHistoryDelegate extends SpoonDelegate implements XulEventHandler
     LogTableInterface logTable = model.logTable;
 
     if ( logTable.isDefined() ) {
-      String schemaTable = logTable.getQuotedSchemaTableCombination();
       DatabaseMeta databaseMeta = logTable.getDatabaseMeta();
 
       MessageBox mb = new MessageBox( jobGraph.getShell(), SWT.YES | SWT.NO | SWT.ICON_QUESTION );
-      //CHECKSTYLE:LineLength:OFF
-      mb.setMessage( BaseMessages.getString( PKG, "JobGraph.Dialog.AreYouSureYouWantToRemoveAllLogEntries.Message", schemaTable ) );
+      mb.setMessage( BaseMessages.getString( PKG, "JobGraph.Dialog.AreYouSureYouWantToRemoveAllLogEntries.Message",
+        logTable.getQuotedSchemaTableCombination() ) );
       mb.setText( BaseMessages.getString( PKG, "JobGraph.Dialog.AreYouSureYouWantToRemoveAllLogEntries.Title" ) );
       if ( mb.open() == SWT.YES ) {
         Database database = new Database( loggingObject, databaseMeta );
         try {
           database.connect();
-          database.truncateTable( schemaTable );
+          database.truncateTable( logTable.getSchemaName(), logTable.getTableName() );
         } catch ( Exception e ) {
           new ErrorDialog( jobGraph.getShell(),
             BaseMessages.getString( PKG, "JobGraph.Dialog.ErrorClearningLoggingTable.Title" ),
