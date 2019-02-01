6e804d0f48807e16546947aad5026f5d29d33ebf
Luis Martins <luis.martins@hitachivantara.com>
2019-02-01 10:51:25 +0000

    [SP-4891] Backport of PPP-4336 - Update copyright versions to 2019 in both 7.1.0.20 and 8.2.0.2 SP scripts (8.2 Suite)

--- assemblies/static/src/main/resources/spoon.sh
+++ assemblies/static/src/main/resources/spoon.sh
@@ -4,7 +4,7 @@
 #
 # Pentaho Data Integration
 #
-# Copyright (C) 2005-2018 by Hitachi Vantara : http://www.hitachivantara.com
+# Copyright (C) 2005-2019 by Hitachi Vantara : http://www.hitachivantara.com
 #
 # *****************************************************************************
 #
