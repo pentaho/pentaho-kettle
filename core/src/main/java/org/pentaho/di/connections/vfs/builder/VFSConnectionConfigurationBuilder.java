20d7c38037578a5f6d75110513bac20a9deeb32c
Benjamin Morrise <benjamin.morrise@hitachivantara.com>
2019-03-11 12:29:15 -0400

    [BACKLOG-27714] Add named connections

---
+++ core/src/main/java/org/pentaho/di/connections/vfs/builder/VFSConnectionConfigurationBuilder.java
@@ -0,0 +1,46 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.connections.vfs.builder;
+
+import org.apache.commons.vfs2.FileSystemConfigBuilder;
+import org.apache.commons.vfs2.FileSystemOptions;
+
+/**
+ * Created by bmorrise on 11/7/18.
+ */
+public abstract class VFSConnectionConfigurationBuilder extends FileSystemConfigBuilder {
+
+  public VFSConnectionConfigurationBuilder( FileSystemOptions fileSystemOptions ) {
+    this.fileSystemOptions = fileSystemOptions;
+  }
+
+  private FileSystemOptions fileSystemOptions;
+
+  public FileSystemOptions getFileSystemOptions() {
+    return fileSystemOptions;
+  }
+
+  public void setFileSystemOptions( FileSystemOptions fileSystemOptions ) {
+    this.fileSystemOptions = fileSystemOptions;
+  }
+}
