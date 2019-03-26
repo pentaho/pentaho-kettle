c16f8e046d89b04a7ac61aca36d4570c7f1b596e
Peter Minutillo <peter.minutillo@hitachivantara.com>
2019-03-26 13:37:48 -0400

    PDI-18003 - Fixing invalid JSON (#6349)

--- assemblies/samples/src/main/resources/transformations/files/jsonfile.js
+++ assemblies/samples/src/main/resources/transformations/files/jsonfile.js
@@ -21,7 +21,7 @@
         "title": "The Lord of the Rings",
         "isbn": "0-395-19395-8",
         "price": 22.99
-      }
+      },
       { "category": "fiction",
         "author": "Samatar",
         "title": "La belle vie",
