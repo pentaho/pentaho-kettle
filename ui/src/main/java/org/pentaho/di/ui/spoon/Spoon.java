9fce78f94f2891d87d11cbb9741118e988f52427
bantonio82 <bruno.antonio@hitachivantara.com>
2019-04-10 12:09:08 +0100

    [SP-4997] - Backport of PDI-17630 - PDI - Hadoop Job Executor step doesn't have the ability to scroll through fields in advanced mode (8.2 Suite)

--- ui/src/main/java/org/pentaho/di/ui/spoon/Spoon.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/Spoon.java
@@ -1436,6 +1436,8 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
         display.dispose();
       } catch ( SWTException e ) {
         // dispose errors
+      } catch ( NullPointerException  e ) {
+        // fixes NPE on Mac OS
       }
     }
   }
