50d6e5886d3862c1c1e4f2802b360db61e9bcaf3
Michael Bodkin <michael.bodkin@hitachivantara.com>
2019-04-11 16:37:29 -0400

    [BACKLOG-28338] Add metadata injection support for CONNECTION NAME field to Table Output (#6422)
    step

--- engine/src/main/java/org/pentaho/di/trans/steps/tableoutput/TableOutputMetaInjection.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/tableoutput/TableOutputMetaInjection.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -37,6 +37,7 @@ import org.pentaho.di.trans.step.StepMetaInjectionInterface;
  *
  * @author Chris
  */
+@Deprecated
 public class TableOutputMetaInjection implements StepMetaInjectionInterface {
 
   public enum Entry implements StepMetaInjectionEntryInterface {
