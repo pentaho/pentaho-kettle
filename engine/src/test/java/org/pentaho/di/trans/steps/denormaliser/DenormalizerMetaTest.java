e7c48f77f2c134780992b846daf80d4b9922d658
Jonathan Jarvis <jonathan.jarvis@hitachivantara.com>
2019-06-05 09:14:05 -0500

    [SP-5363] Backport of PDI-17738 - Row Denormaliser step ignores number format (8.3 Suite)

--- engine/src/test/java/org/pentaho/di/trans/steps/denormaliser/DenormalizerMetaTest.java
+++ engine/src/test/java/org/pentaho/di/trans/steps/denormaliser/DenormalizerMetaTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -35,7 +35,18 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.exception.KettleValueException;
 import org.pentaho.di.core.plugins.PluginRegistry;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaBigNumber;
+import org.pentaho.di.core.row.value.ValueMetaInteger;
+import org.pentaho.di.core.row.value.ValueMetaNumber;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.core.variables.Variables;
 import org.pentaho.di.junit.rules.RestorePDIEngineEnvironment;
 import org.pentaho.di.trans.step.StepMetaInterface;
 import org.pentaho.di.trans.steps.loadsave.LoadSaveTester;
@@ -43,6 +54,8 @@ import org.pentaho.di.trans.steps.loadsave.initializer.InitializerInterface;
 import org.pentaho.di.trans.steps.loadsave.validator.ArrayLoadSaveValidator;
 import org.pentaho.di.trans.steps.loadsave.validator.FieldLoadSaveValidator;
 import org.pentaho.di.trans.steps.loadsave.validator.StringLoadSaveValidator;
+
+import org.junit.Assert;
 
 public class DenormalizerMetaTest implements InitializerInterface<StepMetaInterface> {
   LoadSaveTester loadSaveTester;
@@ -136,6 +149,73 @@ public class DenormalizerMetaTest implements InitializerInterface<StepMetaInterf
           .append( testObject.getTargetAggregationType(), another.getTargetAggregationType() )
           .isEquals();
     }
+  }
+
+  @Test
+  public void testGetFields() throws KettleStepException {
+
+    final String PERSON = "Person";
+    final String YEAR = "Year";
+    final String SALARY = "Salary";
+    final String Y2018 = "2018";
+    final String Y2019 = "2019";
+    final int TARGET_LENGTH = 10;
+    final int TARGET_PRECISION = 2;
+    final String TARGET_FORMAT = "0.00";
+    final String TARGET_GROUP = ",";
+    final String TARGET_DECIMAL = ".";
+    final String TARGET_CURRENCY = "$";
+
+    RowMetaInterface inputRow = new RowMeta();
+    inputRow.addValueMeta( new ValueMetaString( PERSON ) );
+    inputRow.addValueMeta( new ValueMetaInteger( YEAR ) );
+    inputRow.addValueMeta( new ValueMetaNumber( SALARY ) );
+
+    DenormaliserTargetField targetField2018 = new DenormaliserTargetField();
+    targetField2018.setTargetName( Y2018 );
+    targetField2018.setFieldName( SALARY );
+    targetField2018.setTargetType( ValueMetaInterface.TYPE_BIGNUMBER );
+    targetField2018.setTargetLength( TARGET_LENGTH );
+    targetField2018.setTargetPrecision( TARGET_PRECISION );
+    targetField2018.setTargetFormat( TARGET_FORMAT );
+    targetField2018.setTargetDecimalSymbol( TARGET_DECIMAL );
+    targetField2018.setTargetGroupingSymbol( TARGET_GROUP );
+    targetField2018.setTargetCurrencySymbol( TARGET_CURRENCY );
+
+    DenormaliserTargetField targetField2019 = new DenormaliserTargetField();
+    targetField2019.setFieldName( SALARY );
+    targetField2019.setTargetName( Y2019 );
+    targetField2019.setTargetType( ValueMetaInterface.TYPE_BIGNUMBER );
+    targetField2019.setTargetLength( TARGET_LENGTH );
+    targetField2019.setTargetPrecision( TARGET_PRECISION );
+    targetField2019.setTargetFormat( TARGET_FORMAT );
+    targetField2019.setTargetDecimalSymbol( TARGET_DECIMAL );
+    targetField2019.setTargetGroupingSymbol( TARGET_GROUP );
+    targetField2019.setTargetCurrencySymbol( TARGET_CURRENCY );
+
+    DenormaliserMeta meta = new DenormaliserMeta();
+    meta.setDefault();
+    meta.setKeyField( YEAR );
+    meta.setGroupField( new String[] { PERSON } );
+    meta.setDenormaliserTargetField( new DenormaliserTargetField[] { targetField2018, targetField2019 } );
+
+    meta.getFields( inputRow, "Denormaliser", null, null, new Variables(), null, null );
+
+    Assert.assertEquals( 3, inputRow.size() );
+
+    Assert.assertEquals( PERSON, inputRow.getValueMeta( 0 ).getName() );
+    Assert.assertEquals( Y2018, inputRow.getValueMeta( 1 ).getName() );
+    Assert.assertEquals( Y2019, inputRow.getValueMeta( 2 ).getName() );
+
+    for ( int i = 1; i <= 2; i++ ) {
+      Assert.assertEquals( TARGET_LENGTH, inputRow.getValueMeta( i ).getLength() );
+      Assert.assertEquals( TARGET_PRECISION, inputRow.getValueMeta( i ).getPrecision() );
+      Assert.assertEquals( TARGET_FORMAT, inputRow.getValueMeta( i ).getFormatMask() );
+      Assert.assertEquals( TARGET_DECIMAL, inputRow.getValueMeta( i ).getDecimalSymbol() );
+      Assert.assertEquals( TARGET_GROUP, inputRow.getValueMeta( i ).getGroupingSymbol() );
+      Assert.assertEquals( TARGET_CURRENCY, inputRow.getValueMeta( i ).getCurrencySymbol() );
+    }
+
   }
 
 }
