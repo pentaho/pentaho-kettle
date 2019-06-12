4454ffcf8366e3b983dc3109579c4f0cb405bbe2
Garlog7712 <dana.maxfield@gmail.com>
2019-06-11 22:15:19 -0400

    Update travisSetup.sh

--- travisSetup.sh
+++ travisSetup.sh
@@ -5,4 +5,4 @@ sudo mv /usr/local/maven-3.5.2/conf/settings.xml /usr/local/maven-3.5.2/conf/ORI
 
 sudo cp ./.travis.maven.settings.xml /usr/local/maven-3.5.2/conf/settings.xml
 
-mvn clean install -DskipTests -B -V
+mvn clean install -DskipTests -B -V -q
