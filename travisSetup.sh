4310fd8cadfc5d4c4f5c221267ee608e749050ad
Garlog7712 <dana.maxfield@gmail.com>
2019-06-11 22:52:55 -0400

    Update travisSetup.sh

--- travisSetup.sh
+++ travisSetup.sh
@@ -5,6 +5,6 @@ set -x
 ##sudo cp ./.travis.maven.settings.xml /usr/local/maven-3.5.2/conf/settings.xml
 
 mkdir ~/.m2
-cp .travis.maven.settings.xml ~/.m2/.
+cp .travis.maven.settings.xml ~/.m2/settings.xml
 
 mvn clean install -DskipTests -B -V | grep -v "[INFO] Downloaded from "
