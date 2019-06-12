145d8ad9a3795453ef4968ecb228642ee94708b7
Garlog7712 <dana.maxfield@gmail.com>
2019-06-11 21:51:14 -0400

    Update travisSetup.sh

--- travisSetup.sh
+++ travisSetup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 set -x
 
-mv /usr/local/maven-3.5.2/conf/settings.xml /usr/local/maven-3.5.2/conf/ORIG.settings.xml
+sudo mv /usr/local/maven-3.5.2/conf/settings.xml /usr/local/maven-3.5.2/conf/ORIG.settings.xml
 
-cp ./.travis.maven.settings.xml /usr/local/maven-3.5.2/conf/settings.xml
+sudo cp ./.travis.maven.settings.xml /usr/local/maven-3.5.2/conf/settings.xml
 
 find /usr/local/maven-3.5.2 -type d -name conf -exec ls -ltr {} \;
 
