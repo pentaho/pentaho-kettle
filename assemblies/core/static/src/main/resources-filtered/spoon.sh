2d59ecd16b9c79cfeef81b688b02f615c4f829bf
Brandon Groves <brandon.groves@gmail.com>
2019-10-31 09:03:17 -0400

    [PDI-17271] Give the user the ability to filter GTK Warnings

--- assemblies/core/static/src/main/resources-filtered/spoon.sh
+++ assemblies/core/static/src/main/resources-filtered/spoon.sh
@@ -253,8 +253,7 @@ inputtoexitstatus() {
   return $exitstatus
 }
 
-OS=`uname -s | tr '[:upper:]' '[:lower:]'`
-if [ $OS = "linux" ]; then
+if [ -n "${FILTER_GTK_WARNINGS}" ] ; then
     (((("$_PENTAHO_JAVA" $OPT -jar "$STARTUP" -lib $LIBPATH "${1+$@}"  2>&1; echo $? >&3 ) | grep -viE "Gtk-WARNING|GLib-GObject|GLib-CRITICAL|^$" >&4 ) 3>&1)| inputtoexitstatus ) 4>&1
 else
     "$_PENTAHO_JAVA" $OPT -jar "$STARTUP" -lib $LIBPATH "${1+$@}"
