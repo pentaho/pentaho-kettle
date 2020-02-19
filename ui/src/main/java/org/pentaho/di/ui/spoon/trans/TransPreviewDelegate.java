fc4ad106757ee74d20008db038f21676cf82ff8c
ssamora <sergiosamora@gmail.com>
2020-02-19 11:18:04 +0000

    [SP-5407] Backport of PDI-17471 - Preview Dialog has sporadic behavior and no error logging (9.0 Suite)

--- ui/src/main/java/org/pentaho/di/ui/spoon/trans/TransPreviewDelegate.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/trans/TransPreviewDelegate.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2020 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -463,11 +463,11 @@ public class TransPreviewDelegate extends SpoonDelegate implements XulEventHandl
     previewMetaMap.clear();
     previewDataMap.clear();
 
-    try {
-      final TransMeta transMeta = trans.getTransMeta();
 
-      for ( final StepMeta stepMeta : stepMetas ) {
+    final TransMeta transMeta = trans.getTransMeta();
 
+    for ( final StepMeta stepMeta : stepMetas ) {
+      try {
         final RowMetaInterface rowMeta = transMeta.getStepFields( stepMeta ).clone();
         previewMetaMap.put( stepMeta, rowMeta );
         final List<RowMetaAndData> rowsData;
@@ -517,10 +517,9 @@ public class TransPreviewDelegate extends SpoonDelegate implements XulEventHandl
               break;
           }
         }
-
+      } catch ( Exception e ) {
+        loggingText.append( Const.getStackTracker( e ) );
       }
-    } catch ( Exception e ) {
-      loggingText.append( Const.getStackTracker( e ) );
     }
 
     // In case there were errors during preview...
