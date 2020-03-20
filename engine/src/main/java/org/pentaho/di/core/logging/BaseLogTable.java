b92dbc46fcee5bb28238ac51626ce2fe3c68e529
Angel Ramos <angel.ramos@hitachivantara.com>
2020-03-20 15:13:11 -0400

    [PDI-18555] Added logic to omit disabled log fields from ktrs and kjbs

--- engine/src/main/java/org/pentaho/di/core/logging/BaseLogTable.java
+++ engine/src/main/java/org/pentaho/di/core/logging/BaseLogTable.java
@@ -419,17 +419,19 @@ public abstract class BaseLogTable {
     StringBuilder retval = new StringBuilder();
 
     for ( LogTableField field : fields ) {
-      retval.append( "        " ).append( XMLHandler.openTag( XML_TAG ) ).append( Const.CR );
+      if ( field.isEnabled() ) {
+        retval.append( "        " ).append( XMLHandler.openTag( XML_TAG ) ).append( Const.CR );
 
-      retval.append( "          " ).append( XMLHandler.addTagValue( "id", field.getId() ) );
-      retval.append( "          " ).append( XMLHandler.addTagValue( "enabled", field.isEnabled() ) );
-      retval.append( "          " ).append( XMLHandler.addTagValue( "name", field.getFieldName() ) );
-      if ( field.isSubjectAllowed() ) {
-        retval.append( "          " ).append( XMLHandler.addTagValue( "subject",
-          field.getSubject() == null ? null : field.getSubject().toString() ) );
+        retval.append( "          " ).append( XMLHandler.addTagValue( "id", field.getId() ) );
+        retval.append( "          " ).append( XMLHandler.addTagValue( "enabled", field.isEnabled() ) );
+        retval.append( "          " ).append( XMLHandler.addTagValue( "name", field.getFieldName() ) );
+        if ( field.isSubjectAllowed() ) {
+          retval.append( "          " ).append( XMLHandler.addTagValue( "subject",
+                  field.getSubject() == null ? null : field.getSubject().toString() ) );
+        }
+
+        retval.append( "        " ).append( XMLHandler.closeTag( XML_TAG ) ).append( Const.CR );
       }
-
-      retval.append( "        " ).append( XMLHandler.closeTag( XML_TAG ) ).append( Const.CR );
     }
 
     return retval.toString();
