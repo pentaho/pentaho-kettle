be290c8b672abcc9d91ad63088f43de3709f7879
Pedro Patricio <pedro.patricio@hitachivantara.com>
2020-06-23 17:10:38 +0100

    [SP-5682] Backport of PDI-18304 - Saving an imported job (exported to XML), the ${Internal.Entry.Current.Directory} is replaced with a hard coded path. (8.3 Suite)

--- ui/src/main/java/org/pentaho/di/ui/spoon/JobFileListener.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/JobFileListener.java
@@ -22,6 +22,7 @@
 
 package org.pentaho.di.ui.spoon;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.pentaho.di.core.EngineMetaInterface;
 import org.pentaho.di.core.LastUsedFile;
 import org.pentaho.di.core.ObjectLocationSpecificationMethod;
@@ -62,9 +63,9 @@ public class JobFileListener implements FileListener, ConnectionListener {
           return true;
         }
       }
-      if ( jobMeta.getRepositoryDirectory() == null || !importfile ) {
-        jobMeta.setRepositoryDirectory( spoon.getDefaultSaveLocation( jobMeta ) );
-      }
+      //same fix as in PDI-18786 where the clearCurrentDirectoryChangedListeners method was added
+      clearCurrentDirectoryChangedListenersWhenImporting( importfile, jobMeta );
+      jobMeta.setRepositoryDirectory( spoon.getDefaultSaveLocation( jobMeta ) );
       jobMeta.setRepository( spoon.getRepository() );
       jobMeta.setMetaStore( spoon.getMetaStore() );
       if ( connection != null ) {
@@ -102,6 +103,13 @@ public class JobFileListener implements FileListener, ConnectionListener {
         + fname, e );
     }
     return false;
+  }
+
+  @VisibleForTesting
+  void clearCurrentDirectoryChangedListenersWhenImporting( boolean importfile, JobMeta jobMeta ) {
+    if ( importfile ) {
+      jobMeta.clearCurrentDirectoryChangedListeners();
+    }
   }
 
   private JobMeta fixLinks( JobMeta jobMeta ) {
