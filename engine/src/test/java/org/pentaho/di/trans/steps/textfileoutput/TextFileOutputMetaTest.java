b8a19035b5bf64a05ffedd97e2fd7c9549490ebc
Daniel Moore <moore@knights.ucf.edu>
2020-09-17 15:54:46 -0400

    [BACKLOG-34530] converts s3 to s3a before sending to AEL
    [BACKLOG-34530] converts s3 to s3a for AEL pvfs files
    
    [FIX] fixes broken test

--- engine/src/test/java/org/pentaho/di/trans/steps/textfileoutput/TextFileOutputMetaTest.java
+++ engine/src/test/java/org/pentaho/di/trans/steps/textfileoutput/TextFileOutputMetaTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2020 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -22,15 +22,6 @@
 
 package org.pentaho.di.trans.steps.textfileoutput;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
-
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -43,6 +34,15 @@ import org.pentaho.di.junit.rules.RestorePDIEngineEnvironment;
 import org.pentaho.di.trans.steps.loadsave.LoadSaveTester;
 import org.pentaho.di.trans.steps.loadsave.validator.ArrayLoadSaveValidator;
 import org.pentaho.di.trans.steps.loadsave.validator.FieldLoadSaveValidator;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class TextFileOutputMetaTest {
   @ClassRule public static RestorePDIEngineEnvironment env = new RestorePDIEngineEnvironment();
