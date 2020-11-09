50b504a51428f5885befd37119060fa6358186f8
Bruno Costa <bruno.costa@hitachivantara.com>
2020-11-09 11:14:27 +0000

    [SP-5799] - Backport of PDI-18856 - PDI Kettle Status Page - Sapphire Theme - Transformation/Job Table Records & References to mantleRuby.css (9.1 Suite)

--- engine/src/main/java/org/pentaho/di/www/StatusServletUtils.java
+++ engine/src/main/java/org/pentaho/di/www/StatusServletUtils.java
@@ -22,6 +22,7 @@
 
 package org.pentaho.di.www;
 
+import org.apache.commons.lang.StringUtils;
 import org.pentaho.di.core.logging.LogChannel;
 import org.pentaho.ui.xul.util.XmlParserFactoryProducer;
 import org.w3c.dom.Document;
@@ -84,7 +85,7 @@ class StatusServletUtils {
       }
 
       // webapps folder will always be one directory closer to default directory, need to update relative path string
-      relativePathSeparator = relativePathSeparator.replaceFirst( ".." + File.separator, "" );
+      relativePathSeparator = StringUtils.replaceOnce( relativePathSeparator, ".." + File.separator, "" );
 
       // Get mantle theme CSS file
       String mantleThemeDirStr = relativePathSeparator + "webapps" + root + File.separator + "mantle" + File.separator
