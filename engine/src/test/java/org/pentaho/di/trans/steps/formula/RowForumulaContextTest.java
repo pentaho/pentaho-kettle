b930679a08dca74200cf871071f7bc40b3e3e3e4
Bruno Costa <50630270+bcostahitachivantara@users.noreply.github.com>
2021-02-24 18:04:20 +0000

    [PDI-18402] - Formula step not considering the metadata of the column when performing the logical operations. (#7858)
    * [PDI-18402] - Formula step not considering the metadata of the column when performing the logical operations.
    
    * [PDI-18402] - Formula step not considering the metadata of the column when performing the logical operations.
    
    * [PDI-18402] - Formula step not considering the metadata of the column when performing the logical operations.

--- engine/src/test/java/org/pentaho/di/trans/steps/formula/RowForumulaContextTest.java
+++ engine/src/test/java/org/pentaho/di/trans/steps/formula/RowForumulaContextTest.java
@@ -27,6 +27,7 @@ import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.reporting.libraries.formula.typing.Type;
 import org.pentaho.reporting.libraries.formula.typing.coretypes.AnyType;
+import org.pentaho.reporting.libraries.formula.typing.coretypes.NumberType;
 import org.pentaho.reporting.libraries.formula.typing.coretypes.TextType;
 
 import static org.junit.Assert.*;
@@ -50,7 +51,49 @@ public class RowForumulaContextTest {
   }
 
   @Test
-  public void testResolveReferenceTypeWithMetaTypeNotString() {
+  public void testResolveReferenceTypeWithMetaTypeInteger() {
+    RowMetaInterface row = mock( RowMetaInterface.class );
+    ValueMetaInterface valueMeta = mock( ValueMetaInterface.class );
+    RowForumulaContext context = new RowForumulaContext( row );
+
+    String name = "name";
+    when( row.searchValueMeta( name ) ).thenReturn( valueMeta );
+    when( valueMeta.getType() ).thenReturn( ValueMetaInterface.TYPE_INTEGER );
+
+    Type type = context.resolveReferenceType( name );
+    assertTrue( type instanceof NumberType );
+  }
+
+  @Test
+  public void testResolveReferenceTypeWithMetaTypeNumber() {
+    RowMetaInterface row = mock( RowMetaInterface.class );
+    ValueMetaInterface valueMeta = mock( ValueMetaInterface.class );
+    RowForumulaContext context = new RowForumulaContext( row );
+
+    String name = "name";
+    when( row.searchValueMeta( name ) ).thenReturn( valueMeta );
+    when( valueMeta.getType() ).thenReturn( ValueMetaInterface.TYPE_NUMBER );
+
+    Type type = context.resolveReferenceType( name );
+    assertTrue( type instanceof NumberType );
+  }
+
+  @Test
+  public void testResolveReferenceTypeWithMetaTypeBigDecimal() {
+    RowMetaInterface row = mock( RowMetaInterface.class );
+    ValueMetaInterface valueMeta = mock( ValueMetaInterface.class );
+    RowForumulaContext context = new RowForumulaContext( row );
+
+    String name = "name";
+    when( row.searchValueMeta( name ) ).thenReturn( valueMeta );
+    when( valueMeta.getType() ).thenReturn( ValueMetaInterface.TYPE_BIGNUMBER );
+
+    Type type = context.resolveReferenceType( name );
+    assertTrue( type instanceof NumberType );
+  }
+
+  @Test
+  public void testResolveReferenceTypeWithMetaTypeNotStringAndNotNumeric() {
     RowMetaInterface row = mock( RowMetaInterface.class );
     ValueMetaInterface valueMeta = mock( ValueMetaInterface.class );
     RowForumulaContext context = new RowForumulaContext( row );
