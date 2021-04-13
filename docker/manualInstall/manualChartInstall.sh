8679d618fda80e28e3b1f41cf7c3b3a30d0f886f
Angel Ramos <angel.ramos@hitachivantara.com>
2021-04-13 14:27:15 -0400

    [webspoon-9.0] Updated helm charts

--- docker/manualInstall/manualChartInstall.sh
+++ docker/manualInstall/manualChartInstall.sh
@@ -4,7 +4,7 @@ export HELM_EXPERIMENTAL_OCI=1
 
 rm webspoon-*.tgz
 
-helm package ../k8s -d ../manualInstall
+helm package ../k8s/helm/src/main/resources/webspoon -d ../manualInstall
 helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:latest
 helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:latest
 kubectl apply -f solutionPackage.yaml -n hitachi-solutions
