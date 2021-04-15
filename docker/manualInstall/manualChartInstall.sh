8ba47dd956b5de33f74ebeacc2b69cf73c15e349
William E. Seyler <william.seyler@hitachivantara.com>
2021-04-15 14:58:26 -0400

    Webspoon 9.0 (#7932)
    * [TAP-4108] sso changes to facilitate webspoon url
    
    * [TAP-4108] sso changes to facilitate webspoon url

--- docker/manualInstall/manualChartInstall.sh
+++ docker/manualInstall/manualChartInstall.sh
@@ -6,7 +6,7 @@ rm webspoon-*.tgz
 
 helm package ../k8s/helm/src/main/resources/webspoon -d ../manualInstall
 # must rev version to match solutionPackage.yaml and Chart.yaml
-helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.8
+helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.9
 # must rev version to match solutionPackage.yaml and Chart.yaml
-helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.8
+helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.9
 kubectl apply -f solutionPackage.yaml -n hitachi-solutions
