95acf65dba07a7f7df79bfd163eb783da165b899
William Seyler <william.seyler@hitachivantara.com>
2021-04-17 22:18:06 -0400

    a version of webspoon where the url is not protected by sso

--- docker/manualInstall/manualChartInstall.sh
+++ docker/manualInstall/manualChartInstall.sh
@@ -6,7 +6,7 @@ rm webspoon-*.tgz
 
 helm package ../k8s/helm/src/main/resources/webspoon -d ../manualInstall
 # must rev version to match solutionPackage.yaml and Chart.yaml
-helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.9
+helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.4.1
 # must rev version to match solutionPackage.yaml and Chart.yaml
-helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.1.9
+helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:1.4.1
 kubectl apply -f solutionPackage.yaml -n hitachi-solutions
