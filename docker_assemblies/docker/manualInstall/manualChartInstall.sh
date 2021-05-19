2e856d4bf9a1bd728efd897293fb8d6451953fca
Angel Ramos <angel.ramos@hitachivantara.com>
2021-05-19 14:31:40 -0400

    [TAP-4397] Switched docker image build to maven assembly

--- docker_assemblies/docker/manualInstall/manualChartInstall.sh
+++ docker_assemblies/docker/manualInstall/manualChartInstall.sh
@@ -4,15 +4,16 @@ export HELM_EXPERIMENTAL_OCI=1
 
 rm webspoon-*.tgz
 
-cd ..
-docker build -t hiromuhota/webspoon:latest .
-docker tag hiromuhota/webspoon:latest ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:CHANGE_ME
+cd ../..
+mvn clean install -DskipTests
+docker tag org.pentaho.di/pentaho-kettle-docker:9.3.0.0-SNAPSHOT ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:CHANGE_ME
 docker push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:CHANGE_ME
-cd manualInstall
+cd docker/manualInstall
 
 
-helm package ../k8s/helm/src/main/resources/webspoon -d ../manualInstall
+helm package ../../k8s/helm/src/main/resources/webspoon -d ../manualInstall
 # must rev version to match solutionPackage.yaml and Chart.yaml
+export HELM_EXPERIMENTAL_OCI=1
 helm chart save ./webspoon* ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:CHANGE_ME
 # must rev version to match solutionPackage.yaml and Chart.yaml
 helm chart push ldl-dev-r2d2-c7-registry.dogfood.trylumada.com/webspoon:CHANGE_ME
