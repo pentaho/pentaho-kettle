b5174630d80d8874117464703b34554b5ae4e30b
ddiroma <dean.diroma@hitachivantara.com>
2021-06-21 15:40:55 -0400

    [SP-5992] JSON input: Step does not return record when trying to retrieve a field with NULL value. Fix using kettle.properties KETTLE_JSON_INPUT_INCLUDE_NULLS value to accomplish this This commit is a cherry-pick from master 7e8f41a66

--- core/src/main/java/org/pentaho/di/core/Const.java
+++ core/src/main/java/org/pentaho/di/core/Const.java
@@ -1184,6 +1184,9 @@ public class Const {
   // See PDI-18810 for details
   public static final String KETTLE_COMPATIBILITY_MDI_INJECTED_FILE_ALWAYS_IN_FILESYSTEM = "KETTLE_COMPATIBILITY_MDI_INJECTED_FILE_ALWAYS_IN_FILESYSTEM";
 
+  // See PDI-19138 for details
+  public static final String KETTLE_JSON_INPUT_INCLUDE_NULLS = "KETTLE_JSON_INPUT_INCLUDE_NULLS";
+
   /**
    * This property when set to Y force the same output file even when splits is required.
    * See PDI-19064 for details
