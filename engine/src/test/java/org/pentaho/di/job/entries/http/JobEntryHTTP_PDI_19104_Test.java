feec860e73c8ac53559c9462c55cbe9a0e6156d4
Bruno Costa <bruno.costa@hitachivantara.com>
2021-06-22 16:14:09 +0100

    [PDI-19104] - Getting "Illegal character in opaque part at index" error when using the "file:\" protocol in the HTTP Job Entry.

---
+++ engine/src/test/java/org/pentaho/di/job/entries/http/JobEntryHTTP_PDI_19104_Test.java
@@ -0,0 +1,68 @@
+package org.pentaho.di.job.entries.http;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.pentaho.di.core.KettleClientEnvironment;
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.RowMetaAndData;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.junit.rules.RestorePDIEngineEnvironment;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
+public class JobEntryHTTP_PDI_19104_Test {
+
+  @ClassRule
+  public static RestorePDIEngineEnvironment env = new RestorePDIEngineEnvironment();
+
+  @BeforeClass
+  public static void setupBeforeClass() throws KettleException, IOException {
+    KettleClientEnvironment.init();
+  }
+
+  @Test
+  public void testHTTPResultDefaultRows() throws IOException {
+    File localSourceFile = getInputFile( "sourceFile", ".tmp" );
+    File localTargetFile = File.createTempFile( "targetFile", ".tmp" );
+    localSourceFile.deleteOnExit();
+    localTargetFile.deleteOnExit();
+
+    Object[] r = new Object[] {
+      "file://" + localSourceFile.getCanonicalPath(),
+      null,
+      localTargetFile.getCanonicalPath() };
+
+    RowMeta rowMetaDefault = new RowMeta();
+    rowMetaDefault.addValueMeta( new ValueMetaString( "URL" ) );
+    rowMetaDefault.addValueMeta( new ValueMetaString( "UPLOAD" ) );
+    rowMetaDefault.addValueMeta( new ValueMetaString( "DESTINATION" ) );
+    List<RowMetaAndData> rows = new ArrayList<RowMetaAndData>();
+    rows.add( new RowMetaAndData( rowMetaDefault, r ) );
+    Result previousResult = new Result();
+    previousResult.setRows( rows );
+
+    JobEntryHTTP http = new JobEntryHTTP();
+    http.setParentJob( new Job() );
+    http.setRunForEveryRow( true );
+    http.setAddFilenameToResult( false );
+    http.execute( previousResult, 0 );
+    assertTrue( FileUtils.contentEquals( localSourceFile, localTargetFile ) );
+  }
+
+  private File getInputFile( String prefix, String suffix ) throws IOException {
+    File inputFile = File.createTempFile( prefix, suffix );
+    FileUtils.writeStringToFile( inputFile, UUID.randomUUID().toString(), "UTF-8" );
+    return inputFile;
+  }
+}
