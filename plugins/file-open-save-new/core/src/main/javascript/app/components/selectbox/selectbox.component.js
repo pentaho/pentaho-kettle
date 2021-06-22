41c64667f7ba6b7ab160ecd7dfcbfa4a7895cf4c
Angel L. Ramos Cardona <40777305+angel-ramoscardona@users.noreply.github.com>
2021-06-22 08:38:53 -0400

    [TAP-4636] Fixed pull down menus (#8024)
    Co-authored-by: Angel Ramos <angel.ramos@hitachivantara.com>

--- plugins/file-open-save-new/core/src/main/javascript/app/components/selectbox/selectbox.component.js
+++ plugins/file-open-save-new/core/src/main/javascript/app/components/selectbox/selectbox.component.js
@@ -60,7 +60,7 @@ define([
 
     function onBodyClick() {
       $scope.$apply(function() {
-        vm.isShowOptions = false;
+        vm.isShowOptions = !vm.isShowOptions;
       });
 
     }
@@ -76,8 +76,10 @@ define([
 
     function selectOption(option) {
       vm.selectedValue = option;
-      vm.isShowOptions = false;
       vm.onSelect({value: option});
+      $scope.$apply(function () {
+        vm.isShowOptions = !vm.isShowOptions;
+      });
     }
   }
 
