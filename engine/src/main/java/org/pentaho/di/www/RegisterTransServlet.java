34e1e80206ead4401540f74d5380148265fbb15b
ssamora <sergiosamora@gmail.com>
2021-07-13 19:42:21 +0100

    [PDI-17029] Issues with Carte Transformations API registerTrans endpoint in Pentaho REST API

--- engine/src/main/java/org/pentaho/di/www/RegisterTransServlet.java
+++ engine/src/main/java/org/pentaho/di/www/RegisterTransServlet.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2021 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -22,14 +22,26 @@
 package org.pentaho.di.www;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.xerces.dom.DeferredTextImpl;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleXMLException;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 public class RegisterTransServlet extends BaseJobServlet {
 
@@ -46,13 +58,39 @@ public class RegisterTransServlet extends BaseJobServlet {
 
     final String xml = IOUtils.toString( request.getInputStream() );
 
-    // Parse the XML, create a transformation configuration
-    TransConfiguration transConfiguration = TransConfiguration.fromXML( xml );
+    try {
+      // Parse the XML, create a transformation configuration
+      validateTransformation( new ByteArrayInputStream( xml.getBytes() ) );
+      TransConfiguration transConfiguration = TransConfiguration.fromXML( xml );
 
-    Trans trans = createTrans( transConfiguration );
+      Trans trans = createTrans( transConfiguration );
 
-    String message = "Transformation '" + trans.getName() + "' was added to Carte with id " + trans.getContainerObjectId();
-    return new WebResult( WebResult.STRING_OK, message, trans.getContainerObjectId() );
+      String message =
+        "Transformation '" + trans.getName() + "' was added to Carte with id " + trans.getContainerObjectId();
+      return new WebResult( WebResult.STRING_OK, message, trans.getContainerObjectId() );
+    } catch ( KettleXMLException | SAXException ex ) {
+      response.setStatus( HttpServletResponse.SC_BAD_REQUEST );
+      return new WebResult( WebResult.STRING_ERROR, ex.getMessage(), "" );
+    } catch ( Exception ex ) {
+      response.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+      return new WebResult( WebResult.STRING_ERROR, ex.getMessage(), "" );
+    }
+  }
+
+  public void validateTransformation( InputStream is ) throws Exception {
+    DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
+    df.setFeature( "http://xml.org/sax/features/external-general-entities", false );
+    df.setFeature( "http://xml.org/sax/features/external-parameter-entities", false );
+    DocumentBuilder builder = df.newDocumentBuilder();
+    Document doc = builder.parse( is );
+    if ( !doc.getDocumentElement().getNodeName().equals( "transformation_configuration" ) ) {
+      throw new SAXException( "Invalid Transformation - Missing transformation_configuration tag" );
+    }
+    XPath xPath = XPathFactory.newInstance().newXPath();
+    Node node = (Node) xPath.evaluate( "/transformation_configuration/transformation/info/name", doc, XPathConstants.NODE );
+    if ( node == null  || node.getChildNodes().getLength() > 1 || !( node.getFirstChild() instanceof DeferredTextImpl ) ) {
+      throw new SAXException( "Invalid Transformation Name" );
+    }
   }
 
 }
