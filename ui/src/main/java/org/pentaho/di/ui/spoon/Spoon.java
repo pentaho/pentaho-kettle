4249bc54a779f6141b2cd94586c41d385e7986c7
Bruno Costa <50630270+bcostahitachivantara@users.noreply.github.com>
2021-07-15 15:18:16 +0100

    [PDI-18997] - Browsing a file in CSV input step in local system, when connected to the repository, returns an incorrect path in Filename field [Ubuntu 18.04] (#8058)

--- ui/src/main/java/org/pentaho/di/ui/spoon/Spoon.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/Spoon.java
@@ -1181,6 +1181,21 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
     }
   }
 
+
+  /**
+   * PDI-18997 - clear specific repository directory when connect and or disconnect from server
+   */
+  public void clearRepositoryDirectory() {
+    for ( TabMapEntry entry : delegates.tabs.getTabs() ) {
+      Object managedObject = entry.getObject().getManagedObject();
+      if ( managedObject instanceof AbstractMeta ) {
+        AbstractMeta abstractMeta = (AbstractMeta) managedObject;
+        abstractMeta.setRepositoryDirectory( new RepositoryDirectory() );
+        abstractMeta.setFilename( null );
+      }
+    }
+  }
+
   /**
    * Search the transformation meta-data.
    *
@@ -4277,6 +4292,7 @@ public class Spoon extends ApplicationWindow implements AddUndoPositionInterface
         SpoonPluginManager.getInstance().notifyLifecycleListeners( SpoonLifeCycleEvent.REPOSITORY_DISCONNECTED );
         enableMenus();
         updateTreeForActiveAbstractMetas();
+        clearRepositoryDirectory();
       }
     }
   }
