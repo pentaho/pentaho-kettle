066855470dbdb276b16f922d636adbdb610b96bf
Bruno Costa <bruno.costa@hitachivantara.com>
2022-04-08 10:22:16 +0100

    [PDI-19396] - Error in Null Handling: Filter Rows, Anything using di.core.Condition

--- core/src/test/java/org/pentaho/di/core/ConditionTest.java
+++ core/src/test/java/org/pentaho/di/core/ConditionTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2022 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -22,6 +22,7 @@
 
 package org.pentaho.di.core;
 
+import com.google.common.collect.Lists;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.pentaho.di.core.row.RowMeta;
@@ -31,6 +32,11 @@ import org.pentaho.di.core.row.value.ValueMetaInteger;
 import org.pentaho.di.core.row.value.ValueMetaNumber;
 import org.pentaho.di.junit.rules.RestorePDIEnvironment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -77,9 +83,206 @@ public class ConditionTest {
     ValueMetaAndData right_exact = new ValueMetaAndData( new ValueMetaInteger( "name1" ), new Long( -10 ) );
 
     Condition condition = new Condition( left, Condition.FUNC_SMALLER, null, right_exact );
-    assertFalse( condition.evaluate( rowMeta1, new Object[] { null, "test" } ) );
+    assertTrue( condition.evaluate( rowMeta1, new Object[] { null, "test" } ) );
 
     condition = new Condition( left, Condition.FUNC_SMALLER_EQUAL, null, right_exact );
-    assertFalse( condition.evaluate( rowMeta1, new Object[] { null, "test" } ) );
+    assertTrue( condition.evaluate( rowMeta1, new Object[] { null, "test" } ) );
   }
+
+  @Test
+  public void testZeroLargerOrEqualsThanNull() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = 0L;
+    Long rightValue = null;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_LARGER_EQUAL, right, null );
+    assertTrue( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testZeroLargerThanNull() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = 0L;
+    Long rightValue = null;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_LARGER, right, null );
+    assertTrue( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testZeroSmallerOrEqualsThanNull() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = 0L;
+    Long rightValue = null;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_SMALLER_EQUAL, right, null );
+    assertFalse( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testZeroSmallerThanNull() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = 0L;
+    Long rightValue = null;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_SMALLER, right, null );
+    assertFalse( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testNullLargerOrEqualsThanZero() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = null;
+    Long rightValue = 0L;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_LARGER_EQUAL, right, null );
+    assertFalse( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testNullLargerThanZero() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = null;
+    Long rightValue = 0L;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_LARGER, right, null );
+    assertFalse( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testNullSmallerOrEqualsThanZero() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = null;
+    Long rightValue = 0L;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_SMALLER_EQUAL, right, null );
+    assertTrue( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+  @Test
+  public void testNullSmallerThanZero() {
+    String left = "left";
+    String right = "right";
+
+    Long leftValue = null;
+    Long rightValue = 0L;
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    Condition condition = new Condition( left, Condition.FUNC_SMALLER, right, null );
+    assertTrue( condition.evaluate( rowMeta, new Object[] { leftValue, rightValue  } ) );
+  }
+
+
+  /**
+   * This test should evaluate the following table result
+   *
+   * +------+------+-------+-------+-------+-------+
+   * |  A   |  B   |  A<B  | A<=B  |  B>A  | B>=A  |
+   * +------+------+-------+-------+-------+-------+
+   * | null | 1    | true  | true  | true  | true  |
+   * | 0    | 1    | true  | true  | true  | true  |
+   * | 1    | null | false | false | false | false |
+   * | 1    | 0    | false | false | false | false |
+   * | null | 0    | true  | true  | true  | true  |
+   * | 0    | null | false | false | false | false |
+   * | null | -1   | true  | true  | true  | true  |
+   * | 0    | -1   | false | false | false | false |
+   * | -1   | null | false | false | false | false |
+   * | -1   | 0    | true  | true  | true  | true  |
+   * +------+------+-------+-------+-------+-------+
+   *
+   */
+  @Test
+  public void testFormula() {
+    String left = "left";
+    String right = "right";
+
+    RowMetaInterface rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaInteger( left ) );
+    rowMeta.addValueMeta( new ValueMetaInteger( right ) );
+
+    List<Long> valuesA = Arrays.asList( null, 0L, 1L, 1L, null, 0L, null, 0L, -1L, -1L );
+    List<Long> valuesB = Arrays.asList( 1L, 1L, null, 0L, 0L, null, -1L, -1L, null, 0L );
+
+    List<Boolean> resultsSmallerFunction = Arrays.asList( true, true, false, false, true, false, true, false, false, true );
+    List<Boolean> resultsSmallerOrEqualFunction = Arrays.asList( true, true, false, false, true, false, true, false, false, true );
+    List<Boolean> resultsLargerFunction = Arrays.asList( true, true, false, false, true, false, true, false, false, true );
+    List<Boolean> resultsLargerOrEqualFunction = Arrays.asList( true, true, false, false, true, false, true, false, false, true );
+
+    for ( int i = 0; i < resultsSmallerFunction.size(); i++ ) {
+      System.out.print( String.format( "FUNC: A<B; A: %d; B: %d; Expected: %b;", valuesA.get( i ), valuesB.get( i ), resultsSmallerFunction.get( i ) ) );
+      Condition condition = new Condition( left, Condition.FUNC_SMALLER, right, null );
+      assertEquals( resultsSmallerFunction.get( i ), condition.evaluate( rowMeta, new Object[] { valuesA.get( i ), valuesB.get( i )  } ) );
+      System.out.println(" Result: " + condition.evaluate( rowMeta, new Object[] { valuesA.get( i ), valuesB.get( i )  } ) );
+    }
+
+    for ( int i = 0; i < resultsSmallerOrEqualFunction.size(); i++ ) {
+      System.out.print( String.format( "FUNC: A<=B; A: %d; B: %d; Expected: %b;", valuesA.get( i ), valuesB.get( i ), resultsSmallerOrEqualFunction.get( i ) ) );
+      Condition condition = new Condition( left, Condition.FUNC_SMALLER_EQUAL, right, null );
+      assertEquals( resultsSmallerOrEqualFunction.get( i ), condition.evaluate( rowMeta, new Object[] { valuesA.get( i ), valuesB.get( i )  } ) );
+      System.out.println(" Result: " + condition.evaluate( rowMeta, new Object[] { valuesA.get( i ), valuesB.get( i )  } ) );
+    }
+
+    for ( int i = 0; i < resultsLargerFunction.size(); i++ ) {
+      System.out.print( String.format( "FUNC: B>A; A: %d; B: %d; Expected: %b;", valuesA.get( i ), valuesB.get( i ), resultsLargerFunction.get( i ) ) );
+      Condition condition = new Condition( left, Condition.FUNC_LARGER, right, null );
+      assertEquals( resultsLargerFunction.get( i ), condition.evaluate( rowMeta, new Object[] { valuesB.get( i ), valuesA.get( i )  } ) );
+      System.out.println(" Result: " + condition.evaluate( rowMeta, new Object[] { valuesB.get( i ), valuesA.get( i )  } ) );
+    }
+
+    for ( int i = 0; i < resultsLargerOrEqualFunction.size(); i++ ) {
+      System.out.print( String.format( "FUNC: B>=A; A: %d; B: %d; Expected: %b;", valuesA.get( i ), valuesB.get( i ), resultsLargerOrEqualFunction.get( i ) ) );
+      Condition condition = new Condition( left, Condition.FUNC_LARGER_EQUAL, right, null );
+      assertEquals( resultsLargerOrEqualFunction.get( i ), condition.evaluate( rowMeta, new Object[] { valuesB.get( i ), valuesA.get( i )  } ) );
+      System.out.println(" Result: " + condition.evaluate( rowMeta, new Object[] { valuesB.get( i ), valuesA.get( i )  } ) );
+    }
+
+
+  }
+
 }
