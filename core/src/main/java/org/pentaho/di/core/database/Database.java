13333e752b04500ce8c962816d42fabb0d973304
Peter Rinehart <peter.rinehart@hitachivantara.com>
2022-04-29 14:10:38 -0400

    [SP-6138] [PDI-18826] Pentaho platform needs to be able to provide pooled DB connections when the connection is defined using variables.

--- core/src/main/java/org/pentaho/di/core/database/Database.java
+++ core/src/main/java/org/pentaho/di/core/database/Database.java
@@ -3,7 +3,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2022 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -446,7 +446,7 @@ public class Database implements VariableSpace, LoggingObjectInterface {
           }
           try {
             try {
-              this.connection = dsp.getNamedDataSource( name, DatasourceType.POOLED ).getConnection();
+              this.connection = dsp.getPooledDataSourceFromMeta( databaseMeta, DatasourceType.POOLED ).getConnection();
             } catch ( UnsupportedOperationException | NullPointerException e ) {
               // UnsupportedOperationException is happen at DatabaseUtil doesn't support pooled DS, use legacy routine
               // NullPointerException is happen when we will try to run the transformation on the remote server but
