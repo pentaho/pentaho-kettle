08304e288a32dd1fad0f41607bf82dbcae4335e0
urena-kenny <kenny.urena@hitachivantara.com>
2022-06-20 16:14:33 -0400

    [BACKLOG-35944] Added 'createDirectory' to FileProvider Interface and added 'Add Folder' implementations

--- plugins/file-open-save-new/core/src/main/java/org/pentaho/di/plugins/fileopensave/providers/repository/RepositoryFileProvider.java
+++ plugins/file-open-save-new/core/src/main/java/org/pentaho/di/plugins/fileopensave/providers/repository/RepositoryFileProvider.java
@@ -629,6 +629,14 @@ public class RepositoryFileProvider extends BaseFileProvider<RepositoryFile> {
     fileDialogOperation.setFilename( fileDetails.getName() );
   }
 
+  @Override public RepositoryFile createDirectory( String parentPath, RepositoryFile repositoryFile, String newDirectoryName)
+    throws FileException {
+      RepositoryDirectory newRepositoryDirectory = RepositoryDirectory.build( parentPath, findDirectory( repositoryFile.getPath() ) );
+      newRepositoryDirectory.setName( newDirectoryName );
+      RepositoryFile addRepositoryDirectoryResult = add( newRepositoryDirectory );
+    return addRepositoryDirectoryResult;
+  }
+
   private Repository getRepository() {
     return RepositoryBrowserController.repository != null ? RepositoryBrowserController.repository
       : spoonSupplier.get().getRepository();
