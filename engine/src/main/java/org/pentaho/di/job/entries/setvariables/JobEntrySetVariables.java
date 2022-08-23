fb44bcbfe5d68a248173e45e820353a8f4d04e8c
kallu <krishna.allu@hitachivantara.com>
2022-08-23 12:00:37 +0530

    [PDI-19160] - Set variables(job entry): When "Variable substitution" is N, the value of the externally set variable is replaced.

--- engine/src/main/java/org/pentaho/di/job/entries/setvariables/JobEntrySetVariables.java
+++ engine/src/main/java/org/pentaho/di/job/entries/setvariables/JobEntrySetVariables.java
@@ -255,9 +255,12 @@ public class JobEntrySetVariables extends JobEntryBase implements Cloneable, Job
 
       if ( variableName != null ) {
         for ( int i = 0; i < variableName.length; i++ ) {
-          variables.add( variableName[ i ] );
-          variableValues.add( variableValue[ i ] );
-          variableTypes.add( variableType[ i ] );
+          if ( ( variables.contains( variableName[ i ] ) && replaceVars ) || !variables.contains(
+            variableName[ i ] ) ) {
+            variables.add( variableName[ i ] );
+            variableValues.add( variableValue[ i ] );
+            variableTypes.add( variableType[ i ] );
+          }
         }
       }
 
