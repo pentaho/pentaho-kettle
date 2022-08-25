f61538d4dba7d1a1a1841dd4284dea2ee51119a7
amitkumar3 <amit.kumar3@hitachivantara.com>
2022-08-25 20:46:42 +0530

    [BACKLOG-36168] file repo ui new comps

--- plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/dialogdynamic/KettleFileRepoFormComposite.java
+++ plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/dialogdynamic/KettleFileRepoFormComposite.java
@@ -1,6 +1,8 @@
 package org.pentaho.di.ui.repo.dialogdynamic;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -9,25 +11,29 @@ import org.eclipse.swt.widgets.Text;
 import org.json.simple.JSONObject;
 import org.pentaho.di.core.util.Utils;
 import org.pentaho.di.repository.BaseRepositoryMeta;
+import org.pentaho.di.repository.filerep.KettleFileRepository;
+import org.pentaho.di.repository.filerep.KettleFileRepositoryMeta;
 import org.pentaho.di.ui.core.FormDataBuilder;
 import org.pentaho.di.ui.core.PropsUI;
+import org.pentaho.di.ui.repo.controller.RepositoryConnectController;
 
 import java.util.Map;
 
 public class KettleFileRepoFormComposite extends BaseRepoFormComposite {
 
   protected Text txtLocation;
-  protected PropsUI props;
+  protected  Button showHidden;
+  protected  Button doNotModify;
+
 
   public KettleFileRepoFormComposite( Composite parent, int style )
   {
     super( parent, style );
-    this.props=PropsUI.getInstance();
   }
   
   @Override
   protected Control uiAfterDisplayName() {
-    this.props = PropsUI.getInstance();
+    PropsUI props = PropsUI.getInstance();
 
     Label lLoc = new Label( this, SWT.NONE );
     lLoc.setText( "Location" );
@@ -39,7 +45,47 @@ public class KettleFileRepoFormComposite extends BaseRepoFormComposite {
     txtLocation.addModifyListener( lsMod );
     props.setLook( txtLocation );
 
-    return txtLocation;
+    Button browseBtn = new Button( this,SWT.PUSH );
+    // TODO: BaseMessages
+    browseBtn.setText( "Browse" );
+    browseBtn.setLayoutData( new FormDataBuilder().left( txtLocation, LABEL_CONTROL_MARGIN ).top( lLoc, LABEL_CONTROL_MARGIN ).result() );
+    props.setLook( browseBtn );
+
+    browseBtn.addSelectionListener( new SelectionAdapter() {
+      @Override public void widgetSelected( SelectionEvent selectionEvent ) {
+        String location =RepositoryConnectController.getInstance().browse();
+        if(!Utils.isEmpty( location )){
+          txtLocation.setText( location );
+        }
+      }
+    } );
+
+    doNotModify = new Button( this, SWT.CHECK );
+    // TODO: BaseMessages
+    doNotModify.setText( "Do not modify items in this location" );
+    doNotModify.setLayoutData(
+      new FormDataBuilder().left( 0, 0 ).top( txtLocation, CONTROL_MARGIN ).result() );
+    props.setLook( doNotModify );
+    doNotModify.addSelectionListener( new SelectionAdapter() {
+      public void widgetSelected( org.eclipse.swt.events.SelectionEvent e ) {
+        lsMod.modifyText( null );
+      };
+    } );
+
+
+    showHidden = new Button( this, SWT.CHECK );
+    // TODO: BaseMessages
+    showHidden.setText( "Show hidden files" );
+    showHidden.setLayoutData(
+      new FormDataBuilder().left( 0, 0 ).top( doNotModify, CONTROL_MARGIN ).result() );
+    props.setLook( showHidden );
+    showHidden.addSelectionListener( new SelectionAdapter() {
+      public void widgetSelected( org.eclipse.swt.events.SelectionEvent e ) {
+        lsMod.modifyText( null );
+      };
+    } );
+
+    return showHidden;
   }
 
   @Override
@@ -49,8 +95,10 @@ public class KettleFileRepoFormComposite extends BaseRepoFormComposite {
     //TODO: Change to PurRepositoryMeta.REPOSITORY_TYPE_ID
     ret.put( BaseRepositoryMeta.ID, "KettleFileRepository" );
     //TODO: Change to PurRepositoryMeta.URL
-    ret.put( "location", txtLocation.getText() );
-    
+    ret.put( KettleFileRepositoryMeta.LOCATION, txtLocation.getText() );
+    ret.put( KettleFileRepositoryMeta.SHOW_HIDDEN_FOLDERS,showHidden.getSelection() );
+    ret.put( KettleFileRepositoryMeta.DO_NOT_MODIFY,doNotModify.getSelection());
+
     return ret;
   }
   
@@ -58,8 +106,9 @@ public class KettleFileRepoFormComposite extends BaseRepoFormComposite {
   @Override
   public void populate( JSONObject source ) {
     super.populate( source );
-    txtLocation.setText( (String) source.getOrDefault( "location", " " ) );
-    props.setLook( txtLocation );
+    txtLocation.setText( (String) source.getOrDefault( KettleFileRepositoryMeta.LOCATION, "" ) );
+    showHidden.setSelection( (Boolean) source.getOrDefault( KettleFileRepositoryMeta.SHOW_HIDDEN_FOLDERS, false ) );
+    doNotModify.setSelection( (Boolean) source.getOrDefault( KettleFileRepositoryMeta.DO_NOT_MODIFY, false ) );
   }
   
   @Override
