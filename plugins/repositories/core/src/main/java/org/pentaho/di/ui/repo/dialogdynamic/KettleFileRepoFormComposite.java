2fe751474a2284a0dcd2296269b12e314dbba810
amitkumar3 <amit.kumar3@hitachivantara.com>
2022-08-25 18:48:15 +0530

    [BACKLOG-36168] file repo ui

---
+++ plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/dialogdynamic/KettleFileRepoFormComposite.java
@@ -0,0 +1,70 @@
+package org.pentaho.di.ui.repo.dialogdynamic;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.json.simple.JSONObject;
+import org.pentaho.di.core.util.Utils;
+import org.pentaho.di.repository.BaseRepositoryMeta;
+import org.pentaho.di.ui.core.FormDataBuilder;
+import org.pentaho.di.ui.core.PropsUI;
+
+import java.util.Map;
+
+public class KettleFileRepoFormComposite extends BaseRepoFormComposite {
+
+  protected Text txtLocation;
+  protected PropsUI props;
+
+  public KettleFileRepoFormComposite( Composite parent, int style )
+  {
+    super( parent, style );
+    this.props=PropsUI.getInstance();
+  }
+  
+  @Override
+  protected Control uiAfterDisplayName() {
+    this.props = PropsUI.getInstance();
+
+    Label lLoc = new Label( this, SWT.NONE );
+    lLoc.setText( "Location" );
+    lLoc.setLayoutData( new FormDataBuilder().left( 0, 0 ).right( 100, 0 ).top( txtDisplayName, CONTROL_MARGIN ).result() );
+    props.setLook( lLoc );
+
+    txtLocation = new Text( this, SWT.BORDER );
+    txtLocation.setLayoutData( new FormDataBuilder().left( 0, 0 ).top( lLoc, LABEL_CONTROL_MARGIN ).width( MEDIUM_WIDTH ).result() );
+    txtLocation.addModifyListener( lsMod );
+    props.setLook( txtLocation );
+
+    return txtLocation;
+  }
+
+  @Override
+  public Map<String,Object> toMap() {
+    Map<String,Object> ret = super.toMap();
+    
+    //TODO: Change to PurRepositoryMeta.REPOSITORY_TYPE_ID
+    ret.put( BaseRepositoryMeta.ID, "KettleFileRepository" );
+    //TODO: Change to PurRepositoryMeta.URL
+    ret.put( "location", txtLocation.getText() );
+    
+    return ret;
+  }
+  
+  @SuppressWarnings( "unchecked" )
+  @Override
+  public void populate( JSONObject source ) {
+    super.populate( source );
+    txtLocation.setText( (String) source.getOrDefault( "location", " " ) );
+    props.setLook( txtLocation );
+  }
+  
+  @Override
+  protected boolean validateSaveAllowed() {
+    return super.validateSaveAllowed() && !Utils.isEmpty( txtLocation.getText() );
+  }
+
+}
