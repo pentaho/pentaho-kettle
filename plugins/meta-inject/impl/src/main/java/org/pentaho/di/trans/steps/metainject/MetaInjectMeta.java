3d68f7b7a312648e64d20c2ded4d097d372e020e
Phalguni Amidala <phalguni.amidala@hitachivantara.com>
2022-08-30 17:46:45 +0530

    [BACKLOG-36263]-Unable to load image GenericTransform.svg for metainject

--- plugins/meta-inject/impl/src/main/java/org/pentaho/di/trans/steps/metainject/MetaInjectMeta.java
+++ plugins/meta-inject/impl/src/main/java/org/pentaho/di/trans/steps/metainject/MetaInjectMeta.java
@@ -71,7 +71,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 @Step( id = "MetaInject",
-    image = "org/pentaho/di/ui/trans/steps/metainject/img/GenericTransform.svg",
+    image = "org/pentaho/di/trans/steps/metainject/img/GenericTransform.svg",
     name = "i18n:org.pentaho.di.trans.step:BaseStep.TypeLongDesc.MetaInject",
     categoryDescription = "i18n:org.pentaho.di.trans.step:BaseStep.Category.Flow" )
 @InjectionSupported( localizationPrefix = "MetaInject.Injection.", groups = { "SOURCE_OUTPUT_FIELDS",
