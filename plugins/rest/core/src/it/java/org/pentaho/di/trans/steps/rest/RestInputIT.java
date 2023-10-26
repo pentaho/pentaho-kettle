6a442598c4d9d93de26879a307ab854224d2e686
Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>
2023-10-26 14:58:29 +0100

    [BACKLOG-38674] Integration Tests are outdated and some do not compile.
    Moving integration tests to the proper plugin tree.

--- plugins/rest/core/src/it/java/org/pentaho/di/trans/steps/rest/RestInputIT.java
+++ plugins/rest/core/src/it/java/org/pentaho/di/trans/steps/rest/RestInputIT.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.testing.ServletTester;
+import org.eclipse.jetty.servlet.ServletTester;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -80,8 +80,47 @@ public class RestInputIT {
     }
   }
 
+  @Test
+  public void testRESTInput() throws Exception {
+    //
+    // Create a new transformation...
+    //
+    TransMeta transMeta = new TransMeta();
+    transMeta.setName( "restinput" );
+
+    PluginRegistry registry = PluginRegistry.getInstance();
+
+    StepMeta inputStep = createRestInputStep( transMeta, registry );
+
+    RowStepCollector rowStepCollector = new RowStepCollector();
+    final Trans trans = createAndTestTrans( registry, transMeta, inputStep, rowStepCollector, "limit", 5 );
+    trans.startThreads();
+
+    trans.waitUntilFinished();
+
+    // Compare the results
+    List<RowMetaAndData> resultRows = rowStepCollector.getRowsWritten();
+
+    assertTrue( rowStepCollector.getRowsError().isEmpty() );
+    assertEquals( 1, rowStepCollector.getRowsWritten().size() );
+
+    final RowMetaAndData rowMetaAndData = resultRows.get( 0 );
+    final RowMetaInterface rowMeta = rowMetaAndData.getRowMeta();
+
+    final String[] fieldNames = rowMeta.getFieldNames();
+    final Object[] data = rowMetaAndData.getData();
+
+    assertEquals( "pageSize", fieldNames[0] );
+    assertEquals( "name", fieldNames[1] );
+    assertEquals( "result", fieldNames[2] );
+
+    assertEquals( Integer.valueOf( 5 ), data[0] );
+    assertEquals( "limit", data[1] );
+    assertEquals( "limit:5", data[2] );
+  }
+
   protected Trans createAndTestTrans( PluginRegistry registry, TransMeta transMeta, StepMeta inputStep,
-      RowStepCollector rowStepCollector, String name, int limit ) throws KettleException {
+                                      RowStepCollector rowStepCollector, String name, int limit ) throws KettleException {
     //
     // Create a dummy step
     //
@@ -123,7 +162,7 @@ public class RestInputIT {
     transMeta.addStep( inputStep );
 
     meta.setDefault();
-    meta.setUrl( tester.createSocketConnector( true ) + "/context/simple/join" );
+    meta.setUrl( tester.createConnector( true ) + "/context/simple/join" );
     meta.setMethod( "POST" );
     meta.setMatrixParameterField( new String[] { "pageSize" } );
     meta.setMatrixParameterName( new String[] { "limit" } );
@@ -136,44 +175,4 @@ public class RestInputIT {
 
     return inputStep;
   }
-
-  @Test
-  public void testRESTInput() throws Exception {
-    //
-    // Create a new transformation...
-    //
-    TransMeta transMeta = new TransMeta();
-    transMeta.setName( "restinput" );
-
-    PluginRegistry registry = PluginRegistry.getInstance();
-
-    StepMeta inputStep = createRestInputStep( transMeta, registry );
-
-    RowStepCollector rowStepCollector = new RowStepCollector();
-    final Trans trans = createAndTestTrans( registry, transMeta, inputStep, rowStepCollector, "limit", 5 );
-    trans.startThreads();
-
-    trans.waitUntilFinished();
-
-    // Compare the results
-    List<RowMetaAndData> resultRows = rowStepCollector.getRowsWritten();
-
-    assertTrue( rowStepCollector.getRowsError().isEmpty() );
-    assertEquals( 1, rowStepCollector.getRowsWritten().size() );
-
-    final RowMetaAndData rowMetaAndData = resultRows.get( 0 );
-    final RowMetaInterface rowMeta = rowMetaAndData.getRowMeta();
-
-    final String[] fieldNames = rowMeta.getFieldNames();
-    final Object[] data = rowMetaAndData.getData();
-
-    assertEquals( "pageSize", fieldNames[0] );
-    assertEquals( "name", fieldNames[1] );
-    assertEquals( "result", fieldNames[2] );
-
-    assertEquals( Integer.valueOf( 5 ), data[0] );
-    assertEquals( "limit", data[1] );
-    assertEquals( "limit:5", data[2] );
-  }
-
 }
