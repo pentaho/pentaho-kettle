15e6ea8082ae09644e50d8052b81a21bd01fd2d8
Eddie Martinez <eddie.martinez@hitachivantara.com>
2024-02-27 08:32:48 -0500

    [BACKLOG-39875] Change date format in logging console output

--- core/src/main/java/org/pentaho/di/core/logging/KettleLogLayout.java
+++ core/src/main/java/org/pentaho/di/core/logging/KettleLogLayout.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2024 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -21,22 +21,81 @@
  ******************************************************************************/
 
 package org.pentaho.di.core.logging;
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.util.Utils;
 import org.pentaho.di.version.BuildVersion;
 
 public class KettleLogLayout {
+
+  protected static Logger logger = LogManager.getLogger( KettleLogLayout.class );
+  static LoggerContext context = (LoggerContext) LogManager.getContext( false );
+  static Configuration configuration = context.getConfiguration();
   private static final ThreadLocal<SimpleDateFormat> LOCAL_SIMPLE_DATE_PARSER =
-    new ThreadLocal<SimpleDateFormat>() {
-      @Override
-      protected SimpleDateFormat initialValue() {
-        return new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss" );
+    new ThreadLocal<>();
+  @VisibleForTesting
+  protected static String log4J2Appender = "pdi-execution-appender";
+
+  /**
+   * This method will attempt to read and return the date Pattern within the {} from RollingFIle name
+   * "pdi-execution-appender" off the log4j2.xml file.
+   * If unsuccessful, then it will return the default date Pattern "yyyy/MM/dd HH:mm:ss".
+   *
+   * @return SimpleDateFormat
+   */
+  protected static SimpleDateFormat getLog4j2Appender() {
+    Optional<Appender> log4j2xmlAppender =
+      configuration.getAppenders().values().stream()
+        .filter( a -> a.getName().equalsIgnoreCase( log4J2Appender ) ).findFirst();
+    if ( log4j2xmlAppender.isPresent() ) {
+      ArrayList<String> matchesArray = new ArrayList<>();
+      String dateFormatFromLog4j2xml = log4j2xmlAppender.get().getLayout().getContentFormat().get( "format" );
+      Pattern pattern = Pattern.compile( "(\\{(.*?)})" );
+      Matcher matcher = pattern.matcher( dateFormatFromLog4j2xml );
+      while ( matcher.find() ) {
+        matchesArray.add( matcher.group( 2 ) );
       }
-    };
+      if ( !matchesArray.isEmpty() ) {
+        return processMatches( matchesArray );
+      }
+    }
+    return new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss" );
+  }
+
+  private static SimpleDateFormat processMatches( ArrayList<String> matchesArray ) {
+    if ( matchesArray.get( 0 ) != null && !matchesArray.get( 0 ).isEmpty() ) {
+      String strippedDateFormatFromLog4j2xml = matchesArray.get( 0 ).trim();
+      if ( matchesArray.size() > 1 ) {
+        try {
+          SimpleDateFormat timezoneDatePattern = new SimpleDateFormat( strippedDateFormatFromLog4j2xml );
+          timezoneDatePattern.setTimeZone( TimeZone.getTimeZone( matchesArray.get( 1 ).trim() ) );
+          return timezoneDatePattern;
+        } catch ( IllegalArgumentException e ) {
+          logger.error( e.getMessage() );
+        }
+      }
+      try {
+        return new SimpleDateFormat( strippedDateFormatFromLog4j2xml );
+      } catch ( IllegalArgumentException e ) {
+        logger.error( e.getMessage() );
+
+      }
+    }
+    return new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss" );
+  }
 
   public static final String ERROR_STRING = "ERROR";
 
@@ -59,6 +118,9 @@ public class KettleLogLayout {
 
     String dateTimeString = "";
     if ( timeAdded ) {
+      if ( LOCAL_SIMPLE_DATE_PARSER.get() == null ) {
+        LOCAL_SIMPLE_DATE_PARSER.set( getLog4j2Appender() );
+      }
       dateTimeString = LOCAL_SIMPLE_DATE_PARSER.get().format( new Date( event.timeStamp ) ) + " - ";
     }
 
--- core/src/test/java/org/pentaho/di/core/logging/KettleLogLayoutTest.java
+++ core/src/test/java/org/pentaho/di/core/logging/KettleLogLayoutTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2024 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -24,10 +24,12 @@ package org.pentaho.di.core.logging;
 import junit.framework.Assert;
 import org.junit.Test;
 
+
 /**
  * Test for {@link KettleLogLayout}.
  */
 public class KettleLogLayoutTest {
+
 
   @Test
   public void testFormat() throws Exception {
@@ -46,4 +48,135 @@ public class KettleLogLayoutTest {
         "Subject - Log message for Test",
         formattedMsg.substring( formattedMsg.indexOf( '-' ) + 2 ) );
   }
+
+  /**
+   * Log4j2 Date Patterns
+   * %d{HH:mm:ss,SSS} -> Parseable to Java SimpleDateFormat
+   * %d{HH:mm:ss,nnnn} to %d{HH:mm:ss,nnnnnnnnn} -> Not Parseable to Java SimpleDateFormat
+   * %d{dd MMM yyyy HH:mm:ss,SSS} -> Parseable to Java SimpleDateFormat
+   * %d{dd MMM yyyy HH:mm:ss,nnnn} to %d{dd MMM yyyy HH:mm:ss,nnnnnnnnn} -> Not Parseable to Java SimpleDateFormat
+   * %d{MMM dd,yyyy HH:mm:ss} -> Parseable to Java SimpleDateFormat
+   * %d{HH:mm:ss}{GMT+0} -> Parseable to Java SimpleDateFormat
+   *
+   * Test log4j2.xml core/src/test/resources/log4j2.xml with multiple "pdi-execution-appender" for testing.
+   */
+  @Test
+  public void testGetLog4j2AppenderSetDefaultDate() {
+    //"pdi-execution-appender" Date Pattern within the {} are empty, therefore unable to parse to SimpleDateFormat().
+    // Returns default date pattern "yyyy/MM/dd HH:mm:ss"
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test";
+    Assert.assertEquals( "yyyy/MM/dd HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender1() {
+    // Testing HH:mm:ss,SSS pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-1";
+    Assert.assertEquals( "HH:mm:ss,SSS",
+      KettleLogLayout.getLog4j2Appender(  ).toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender2() {
+    // This will throw an Illegal pattern character 'n' Exception parsing to Java SimpleDateFormat() and set Default
+    // Pattern value "yyyy/MM/dd HH:mm:ss"
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-2";
+    Assert.assertNotSame( "HH:mm:ss,nnnn",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+    Assert.assertEquals( "yyyy/MM/dd HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender3() {
+    // Testing dd MMM yyyy HH:mm:ss,SSS pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-3";
+    Assert.assertEquals( "dd MMM yyyy HH:mm:ss,SSS",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender4() {
+    // This will throw an Illegal pattern character 'n' Exception parsing to Java SimpleDateFormat() and set Default
+    // Pattern value "yyyy/MM/dd HH:mm:ss"
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-4";
+    Assert.assertNotSame( "dd MMM yyyy HH:mm:ss,nnnn",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+    Assert.assertEquals( "yyyy/MM/dd HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender5() {
+    // Testing dd-MMM-yyyy HH:mm:ss,SSS pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-5";
+    Assert.assertEquals( "dd-MMM-yyyy HH:mm:ss,SSS",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender6() {
+    // Testing dd/MMM/yyyy HH:mm:ss,SSS pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-6";
+    Assert.assertEquals( "dd/MMM/yyyy HH:mm:ss,SSS",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender7() {
+    // Testing MMM dd,yyyy HH:mm:ss pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-7";
+    Assert.assertEquals( "MMM dd,yyyy HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender8() {
+    // Testing MMM-dd,yyyy pattern
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-8";
+    Assert.assertEquals( "MMM-dd,yyyy",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender9() {
+    // Testing adding empty {} for TimeZone
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-9";
+    Assert.assertEquals( "MMM/dd,yyyy HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender10() {
+    // Testing adding TimeZone GMT+0
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-10";
+    Assert.assertEquals( "MMM dd,yyyy HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender11() {
+    // Testing adding TimeZone GMT-5
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-11";
+    Assert.assertEquals( "HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender12() {
+    // Test with no matching appender name and set Default Pattern value "yyyy/MM/dd HH:mm:ss"
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-twelve";
+    Assert.assertEquals( "yyyy/MM/dd HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
+  @Test
+  public void testGetLog4j2UsingAppender13() {
+    // Test with un-parseable TimeZone
+    KettleLogLayout.log4J2Appender = "pdi-execution-appender-test-13";
+    Assert.assertEquals( "HH:mm:ss",
+      KettleLogLayout.getLog4j2Appender().toPattern() );
+  }
+
 }
