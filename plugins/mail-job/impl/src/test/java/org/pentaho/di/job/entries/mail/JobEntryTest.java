4143401b0fe55ff81409ca9b437498c70d57d7f4
devuser <mandakini.alimineti@hitachivantara.com>
2024-05-22 13:50:03 +0100

    [PDI-19580]-Enable Use of OAUTH for PDI Mail Steps and Entries that use IMAP/SMTP
    [PDI-19580]-Enable Use of OAUTH for PDI Mail Steps and Entries that use IMAP/SMTP

--- plugins/mail-job/impl/src/test/java/org/pentaho/di/job/entries/mail/JobEntryTest.java
+++ plugins/mail-job/impl/src/test/java/org/pentaho/di/job/entries/mail/JobEntryTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2023 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2024 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -23,15 +23,49 @@ package org.pentaho.di.job.entries.mail;
 
 import junit.framework.Assert;
 
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicStatusLine;
+import org.pentaho.di.core.Result;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.pentaho.di.core.KettleClientEnvironment;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.util.HttpClientManager;
 import org.pentaho.di.junit.rules.RestorePDIEngineEnvironment;
+import java.io.IOException;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertTrue;
 
 public class JobEntryTest {
   @ClassRule public static RestorePDIEngineEnvironment env = new RestorePDIEngineEnvironment();
+
+  private JobEntryMail jobEntryMail;
+  @Mock
+  private HttpClientManager httpClientManager;
+  @Mock
+  private CloseableHttpClient httpClient;
+  @Mock
+  private CloseableHttpResponse httpResponse;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks( this );
+    jobEntryMail = new JobEntryMail();
+    httpClientManager = Mockito.mock( HttpClientManager.class );
+    httpClient = Mockito.mock( CloseableHttpClient.class );
+    httpResponse = Mockito.mock( CloseableHttpResponse.class );
+  }
 
   @BeforeClass
   public static void setupBeforeClass() throws KettleException {
@@ -62,5 +96,45 @@ public class JobEntryTest {
     JobEntryMail jem = new JobEntryMail();
     jem.setVariable( "my_pass", "Encrypted 2be98afc86aa7f2e4cb79ce10df81abdc" );
     Assert.assertEquals( jem.getPassword( "${my_pass}" ), "asdf" );
+  }
+
+  @Test
+  public void testOauthAuthenticationFailsWithInvalidTokenUrl()  {
+    JobEntryMail jobEntryMail = new JobEntryMail();
+    jobEntryMail.setUsingAuthentication( JobEntryMail.AUTENTICATION_OAUTH );
+    jobEntryMail.setTokenUrl( "http://invalidurl.com/token" );
+    Result result = new Result();
+    jobEntryMail.execute( result, 0 );
+    assertTrue( result.getNrErrors() > 0 );
+  }
+
+  @Test
+  public void testgrantTypeIsClientCredentials() {
+    jobEntryMail.setGrant_type( JobEntryMail.GRANTTYPE_CLIENTCREDENTIALS );
+    assertEquals( JobEntryMail.GRANTTYPE_CLIENTCREDENTIALS, jobEntryMail.getGrant_type() );
+  }
+
+  @Test
+  public void testgrantTypeIsAuthorizationCode() {
+    jobEntryMail.setGrant_type( JobEntryMail.GRANTTYPE_AUTHORIZATION_CODE );
+    assertEquals( JobEntryMail.GRANTTYPE_AUTHORIZATION_CODE, jobEntryMail.getGrant_type() );
+  }
+
+  @Test( expected = RuntimeException.class )
+  public void testgetOauthTokenThrowsExceptionOnUnsuccessfulResponse() throws IOException {
+    String tokenUrl = "http://example.com/token";
+    StatusLine statusLine = new BasicStatusLine( new ProtocolVersion( "HTTP", 1, 1 ), 400, "Bad Request" );
+    when( httpClientManager.createDefaultClient() ).thenReturn( httpClient );
+    when( httpClient.execute( any( HttpPost.class ) ) ).thenReturn( httpResponse );
+    when( httpResponse.getStatusLine() ).thenReturn( statusLine );
+    jobEntryMail.getOauthToken( tokenUrl );
+  }
+
+  @Test( expected = RuntimeException.class )
+  public void testgetOauthTokenThrowsExceptionOnHttpClientExecuteFailure() throws IOException {
+    String tokenUrl = "http://example.com/token";
+    when( httpClientManager.createDefaultClient() ).thenReturn( httpClient );
+    when( httpClient.execute( any( HttpPost.class ) ) ).thenThrow( new IOException() );
+    jobEntryMail.getOauthToken( tokenUrl );
   }
 }
