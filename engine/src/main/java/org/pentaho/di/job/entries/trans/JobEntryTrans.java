c0ebc5057971918e515b3b3ef4f2d37f0ed305f4
HDS\rammansoor <ramaiz.mansoor@hitachivantara.com>
2024-05-28 16:57:09 -0400

    [PDI-20109] - Extra object of LoggingObject created on execution of Transformation

--- engine/src/main/java/org/pentaho/di/job/entries/trans/JobEntryTrans.java
+++ engine/src/main/java/org/pentaho/di/job/entries/trans/JobEntryTrans.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2021 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2024 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -1135,8 +1135,7 @@ public class JobEntryTrans extends JobEntryBase implements Cloneable, JobEntryIn
           //
           //trans = new Trans( transMeta, this );
           final TransMeta meta = transMeta;
-          trans = new Trans( meta );
-          trans.setParent( this );
+          trans = new Trans( meta, this );
 
           // Pass the socket repository as early as possible...
           //
