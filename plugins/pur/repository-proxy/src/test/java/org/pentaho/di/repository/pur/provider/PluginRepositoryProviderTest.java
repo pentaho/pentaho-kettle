61b46510cc379e6680f4a308bdc7985df77d34d2
Tim Kafalas <tim.kafalas@hitachivantara.com>
2024-05-30 14:12:43 -0400

    [BACKLOG-40842] JDK17 Unit test fixes for Pur Repository (#9387)
    Co-authored-by: Tim Kafalas tkafalas <tkafalas@hitachivantara.com>

--- plugins/pur/repository-proxy/src/test/java/org/pentaho/di/repository/pur/provider/PluginRepositoryProviderTest.java
+++ plugins/pur/repository-proxy/src/test/java/org/pentaho/di/repository/pur/provider/PluginRepositoryProviderTest.java
@@ -23,7 +23,7 @@ import org.pentaho.di.repository.Repository;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
