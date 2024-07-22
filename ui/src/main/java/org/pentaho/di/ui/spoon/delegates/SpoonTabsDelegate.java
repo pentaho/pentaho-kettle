407c2cf0683ea09212b9d8c6fd3a0bb3d76a13d0
Alan Bryant <alan.bryant@hitachivantara.com>
2024-07-22 14:59:44 -0400

    [BACKLOG-41567] - refresh for active tab

--- ui/src/main/java/org/pentaho/di/ui/spoon/delegates/SpoonTabsDelegate.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/delegates/SpoonTabsDelegate.java
@@ -504,7 +504,7 @@ public class SpoonTabsDelegate extends SpoonDelegate {
     }
 
     // Also refresh the tree
-    spoon.refreshTree();
+    spoon.forceRefreshTree();
     spoon.setShellText(); // calls also enableMenus() and markTabsChanged()
   }
 
