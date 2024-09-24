c64f196acfc702c4364b9b67951271447a2a53e9
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:29:19 -0400

    HNC-766: updated license and copyright headers

--- plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpack/HL7MLLPAcknowledgeDialog.java
+++ plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpack/HL7MLLPAcknowledgeDialog.java
@@ -1,6 +1,14 @@
-/*
- * Copyright (c) 2009 kJube.  All rights reserved.
- */
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 
 package org.pentaho.di.ui.job.entries.hl7mllpack;
 
