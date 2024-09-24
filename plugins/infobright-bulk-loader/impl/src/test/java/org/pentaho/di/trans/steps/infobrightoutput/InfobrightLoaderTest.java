b2a8c33b4ed82ccc477a58690fce9633d7cbb726
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:22:27 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit c64f196acfc702c4364b9b67951271447a2a53e9.

--- plugins/infobright-bulk-loader/impl/src/test/java/org/pentaho/di/trans/steps/infobrightoutput/InfobrightLoaderTest.java
+++ plugins/infobright-bulk-loader/impl/src/test/java/org/pentaho/di/trans/steps/infobrightoutput/InfobrightLoaderTest.java
@@ -1,13 +1,23 @@
 /*! ******************************************************************************
  *
- * Pentaho
+ * Pentaho Data Integration
  *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ * Copyright (C) 2018-2024 by Hitachi Vantara : http://www.pentaho.com
  *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
+ *******************************************************************************
  *
- * Change Date: 2028-08-13
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  ******************************************************************************/
 
 package org.pentaho.di.trans.steps.infobrightoutput;
--- plugins/pur/core/src/test/java/org/pentaho/di/repository/pur/PurRepositoryUnitTest.java
+++ plugins/pur/core/src/test/java/org/pentaho/di/repository/pur/PurRepositoryUnitTest.java
@@ -1,14 +1,19 @@
-/*! ******************************************************************************
+/*!
+ * Copyright 2010 - 2024 Hitachi Vantara.  All rights reserved.
  *
- * Pentaho
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Change Date: 2028-08-13
- ******************************************************************************/
+ */
 package org.pentaho.di.repository.pur;
 
 import java.io.Serializable;
