14169c3833fbd8bbcabdcdebff39fae4de13be65
srallapa <srinivas.rallapalli@hitachivantara.com>
2024-09-24 18:00:56 -0500

    [SP-6630] backporting [BISERVER-15051] hide internal variables PUC side

--- plugins/pur/core/src/main/java/com/pentaho/repository/importexport/PdiContentProvider.java
+++ plugins/pur/core/src/main/java/com/pentaho/repository/importexport/PdiContentProvider.java
@@ -21,6 +21,7 @@ import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.parameters.DuplicateParamException;
 import org.pentaho.di.core.parameters.NamedParams;
 import org.pentaho.di.core.parameters.NamedParamsDefault;
+import org.pentaho.di.core.row.value.ValueMetaString;
 import org.pentaho.di.job.JobExecutionConfiguration;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.repository.Repository;
@@ -112,6 +113,12 @@ public class PdiContentProvider implements IPdiContentProvider {
         } catch ( KettleException e ) {
           log.error( e );
         }
+      }
+      /*Updating userVariables to remove based on the HideInternalVariable setting. If no other variables other than
+      Internal variables, then it won't show the Internal variable screen.*/
+      if ( ValueMetaString.convertStringToBoolean( System.getProperty( Const.HIDE_INTERNAL_VARIABLES,
+              Const.HIDE_INTERNAL_VARIABLES_DEFAULT ) ) ) {
+        userVariables.keySet().removeIf(key -> key.contains("Internal."));
       }
     }
     return userVariables;
