95d887587f82e797abd80759b3f9d2354e1a3ed0
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:55:05 -0400

    HNC-766: updated license and copyright headers

--- plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpin/HL7MLLPInputDialog.java
+++ plugins/hl7/core/src/main/java/org/pentaho/di/ui/job/entries/hl7mllpin/HL7MLLPInputDialog.java
@@ -1,19 +1,14 @@
-/*!
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- * Foundation.
+/*! ******************************************************************************
  *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- * or from the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * Pentaho
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
  *
- * Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
- */
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 
 package org.pentaho.di.ui.job.entries.hl7mllpin;
 
