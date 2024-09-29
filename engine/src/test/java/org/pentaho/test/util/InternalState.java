12d25b5f1ccb8422cb4998771a7758e2d55ab038
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-29 12:34:31 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 83fd6257a32a98384196196a8554999819d0edd5.

--- engine/src/test/java/org/pentaho/test/util/InternalState.java
+++ engine/src/test/java/org/pentaho/test/util/InternalState.java
@@ -1,14 +1,3 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 package org.pentaho.test.util;
 
 import java.lang.reflect.Field;
--- utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.sh
+++ utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-# ******************************************************************************
-#
-# Pentaho
-#
-# Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
-#
-# Use of this software is governed by the Business Source License included
-# in the LICENSE.TXT file.
-#
-# Change Date: 2028-08-13
-# ******************************************************************************
 CP="lib/*"
 java -Xms1024m -Xmx2048m -cp "$CP" org.hitachivantara.importer.utility.ImporterUtility
