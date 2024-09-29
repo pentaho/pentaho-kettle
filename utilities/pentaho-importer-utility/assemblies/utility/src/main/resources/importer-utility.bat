6c1a9a19ed023c722ff839a4c1073820cfd63848
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-29 15:00:12 -0400

    HNC-766: updated license and copyright headers

--- utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.bat
+++ utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.bat
@@ -1,3 +1,14 @@
+REM ******************************************************************************
+REM
+REM Pentaho
+REM
+REM Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+REM
+REM Use of this software is governed by the Business Source License included
+REM in the LICENSE.TXT file.
+REM
+REM Change Date: 2028-08-13
+REM ******************************************************************************
 @ECHO OFF
 set CP=lib/*
 java -Xms1024m -Xmx2048m -cp "%CP%" org.hitachivantara.importer.utility.ImporterUtility
