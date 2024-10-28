627cdadc49d3a5541de4f882ae14ecce9b375b6a
ssindhu23 <138762455+ssindhu23@users.noreply.github.com>
2024-10-28 20:23:16 +0000

    [BACKLOG-41283]-Upgrading Pentaho kettle from Java EE 9 to Jakarta EE 10 (#9656)

--- engine/src/test/java/org/pentaho/di/www/GetJobStatusServletTest.java
+++ engine/src/test/java/org/pentaho/di/www/GetJobStatusServletTest.java
@@ -25,10 +25,10 @@ import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.www.cache.CarteStatusCache;
 import org.pentaho.di.www.exception.DuplicateKeyException;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
