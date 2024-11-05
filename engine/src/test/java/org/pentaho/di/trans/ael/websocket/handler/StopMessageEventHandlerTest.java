ce0d5127d1268c25b60662f719e4cc242d369ce9
HDS\rammansoor <ramaiz.mansoor@hitachivantara.com>
2024-11-04 19:23:03 -0500

    [BACKLOG-42674] - Fix unit test for java 17

--- engine/src/test/java/org/pentaho/di/trans/ael/websocket/handler/StopMessageEventHandlerTest.java
+++ engine/src/test/java/org/pentaho/di/trans/ael/websocket/handler/StopMessageEventHandlerTest.java
@@ -25,9 +25,9 @@ package org.pentaho.di.trans.ael.websocket.handler;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
 import org.pentaho.di.core.logging.DefaultLogLevel;
 import org.pentaho.di.core.logging.KettleLogStore;
 import org.pentaho.di.core.logging.LogChannel;
@@ -35,16 +35,11 @@ import org.pentaho.di.core.logging.LogChannelFileWriterBuffer;
 import org.pentaho.di.core.logging.LogLevel;
 import org.pentaho.di.core.logging.LogMessageInterface;
 import org.pentaho.di.core.logging.LoggingRegistry;
-import org.pentaho.di.core.util.Utils;
+
 import org.pentaho.di.engine.api.remote.StopMessage;
 import org.pentaho.di.trans.ael.websocket.DaemonMessagesClientEndpoint;
 import org.pentaho.di.trans.ael.websocket.TransWebSocketEngineAdapter;
 import org.pentaho.di.trans.ael.websocket.exception.MessageEventHandlerExecutionException;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.reflect.Whitebox;
 
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
@@ -54,11 +49,11 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
-@RunWith( PowerMockRunner.class )
-@PowerMockIgnore( "jdk.internal.reflect.*" )
-@PrepareForTest( {DefaultLogLevel.class, LoggingRegistry.class, LogLevel.class, KettleLogStore.class, Utils.class} )
+
 public class StopMessageEventHandlerTest {
   @Mock
   TransWebSocketEngineAdapter transWebSocketEngineAdapter;
@@ -85,12 +80,12 @@ public class StopMessageEventHandlerTest {
   public void setUp() throws Exception {
     KettleLogStore.init();
     this.id = UUID.randomUUID().toString();
-    LogLevel logLevelStatic = PowerMockito.mock( LogLevel.class );
+    LogLevel logLevelStatic = mock( LogLevel.class );
     Whitebox.setInternalState( logLevelStatic, "name", "Basic" );
     Whitebox.setInternalState( logLevelStatic, "ordinal", 3 );
 
-    PowerMockito.mockStatic( DefaultLogLevel.class );
-    PowerMockito.when( DefaultLogLevel.getLogLevel() ).thenReturn( LogLevel.BASIC );
+    mockStatic( DefaultLogLevel.class );
+    when( DefaultLogLevel.getLogLevel() ).thenReturn( LogLevel.BASIC );
 
     logChFileWriterBuffer = Mockito.mock( LogChannelFileWriterBuffer.class );
 
@@ -98,10 +93,10 @@ public class StopMessageEventHandlerTest {
     Mockito.when( regInstance.registerLoggingSource( logChannelSubject ) ).thenReturn( channelId );
     Mockito.when( regInstance.getLogChannelFileWriterBuffer( channelId ) ).thenReturn( logChFileWriterBuffer );
 
-    PowerMockito.mockStatic( LoggingRegistry.class );
-    PowerMockito.when( LoggingRegistry.getInstance() ).thenReturn( regInstance );
+    mockStatic( LoggingRegistry.class );
+    when( LoggingRegistry.getInstance() ).thenReturn( regInstance );
 
-    logLevel = PowerMockito.mock( LogLevel.class );
+    logLevel = mock( LogLevel.class );
     Whitebox.setInternalState( logLevel, "name", "Basic" );
     Whitebox.setInternalState( logLevel, "ordinal", 3 );
 
