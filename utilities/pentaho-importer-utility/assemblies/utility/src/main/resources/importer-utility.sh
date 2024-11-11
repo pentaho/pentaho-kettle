63c7b45d6f933c317ed20ab16dce04181914c46f
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:38:23 -0500

    DEVO-11194: updated license and copyright headers

--- utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.sh
+++ utilities/pentaho-importer-utility/assemblies/utility/src/main/resources/importer-utility.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+# ******************************************************************************
+#
+# Pentaho
+#
+# Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+#
+# Use of this software is governed by the Business Source License included
+# in the LICENSE.TXT file.
+#
+# Change Date: 2029-07-20
+# ******************************************************************************
 CP="lib/*"
 java -Xms1024m -Xmx2048m -cp "$CP" org.hitachivantara.importer.utility.ImporterUtility
