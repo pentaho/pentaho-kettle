6df3a968b957e7f7c07b4413f3a7e3c78bfe4142
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:13:15 -0500

    DEVO-11194: updated license and copyright headers

--- engine/src/test/java/org/pentaho/di/trans/steps/loadsave/validator/NonZeroIntLoadSaveValidator.java
+++ engine/src/test/java/org/pentaho/di/trans/steps/loadsave/validator/NonZeroIntLoadSaveValidator.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 package org.pentaho.di.trans.steps.loadsave.validator;
 
 public class NonZeroIntLoadSaveValidator extends IntLoadSaveValidator {
