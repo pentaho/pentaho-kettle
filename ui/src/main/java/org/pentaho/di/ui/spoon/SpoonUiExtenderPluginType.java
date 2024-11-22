84a0bc862fe1c2b075b90f603cdbd1a046e985b7
joana-fb <132680682+joana-fb@users.noreply.github.com>
2024-11-22 17:55:54 +0000

    [SP-6662] Backport of PDI-20138 - SpoonPluginType does not honor Const.PLUGIN_BASE_FOLDERS_PROP (KETTLE_PLUGIN_BASE_FOLDERS) property and is hardcoded to plugins (10.2 Suite)
    Co-authored-by: Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>

--- ui/src/main/java/org/pentaho/di/ui/spoon/SpoonUiExtenderPluginType.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/SpoonUiExtenderPluginType.java
@@ -22,7 +22,6 @@ import java.util.Set;
 import org.pentaho.di.core.exception.KettlePluginException;
 import org.pentaho.di.core.plugins.BasePluginType;
 import org.pentaho.di.core.plugins.PluginAnnotationType;
-import org.pentaho.di.core.plugins.PluginFolder;
 import org.pentaho.di.core.plugins.PluginInterface;
 import org.pentaho.di.core.plugins.PluginMainClassType;
 import org.pentaho.di.core.plugins.PluginRegistry;
@@ -34,8 +33,7 @@ public class SpoonUiExtenderPluginType extends BasePluginType implements PluginT
 
   private SpoonUiExtenderPluginType() {
     super( SpoonUiExtenderPlugin.class, "SPOONUIEXTENDERPLUGIN", "Spoon UI Extender Plugin" );
-
-    pluginFolders.add( new PluginFolder( "plugins", false, true ) );
+    populateFolders( null );
   }
 
   private static SpoonUiExtenderPluginType pluginType;
