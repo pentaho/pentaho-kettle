f92ba85e4534e16bb6d54a24f4029feee4b0763f
amitkumar3 <amit.kumar3@hitachivantara.com>
2024-12-17 11:44:59 +0530

    [BACKLOG-43059]- Kettle changes for DB Connection

--- engine/src/main/java/org/pentaho/di/trans/steps/databaselookup/DatabaseLookup.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/databaselookup/DatabaseLookup.java
@@ -13,11 +13,19 @@
 
 package org.pentaho.di.trans.steps.databaselookup;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.pentaho.di.core.Const;
+import org.pentaho.di.core.logging.LoggingObjectInterface;
+import org.pentaho.di.core.logging.LoggingObjectType;
+import org.pentaho.di.core.logging.SimpleLoggingObject;
 import org.pentaho.di.core.row.value.ValueMetaBase;
 import org.pentaho.di.core.util.Utils;
 import org.pentaho.di.core.database.Database;
@@ -42,6 +50,7 @@ import org.pentaho.di.trans.step.StepMetaInterface;
 import org.pentaho.di.trans.steps.databaselookup.readallcache.ReadAllCache;
 
 import com.google.common.annotations.VisibleForTesting;
+
 
 /**
  * Looks up values in a database using keys from input streams.
@@ -679,4 +688,103 @@ public class DatabaseLookup extends BaseStep implements StepInterface {
       logDetailed( BaseMessages.getString( PKG, "DatabaseLookup.Log.ConnectedToDatabase" ) );
     }
   }
+
+  @Override
+  public JSONObject doAction( String fieldName, StepMetaInterface stepMetaInterface, TransMeta transMeta,
+                              Trans trans, Map<String, String> queryParamToValues ) {
+    JSONObject response = new JSONObject();
+    try {
+      Method actionMethod = DatabaseLookup.class.getDeclaredMethod( fieldName + "Action", Map.class );
+      this.setStepMetaInterface( stepMetaInterface );
+      response = (JSONObject) actionMethod.invoke( this, queryParamToValues );
+
+    } catch ( NoSuchMethodException | InvocationTargetException | IllegalAccessException e ) {
+      log.logError( e.getMessage() );
+      response.put( StepInterface.ACTION_STATUS, StepInterface.FAILURE_METHOD_NOT_RESPONSE );
+    }
+    return response;
+  }
+
+  @SuppressWarnings( "java:S1144" ) // Using reflection this method is being invoked
+  private JSONObject getSchemaAction( Map<String, String> queryParams ) {
+    JSONObject response = new JSONObject();
+    response.put( StepInterface.ACTION_STATUS, StepInterface.FAILURE_RESPONSE );
+    try {
+      String[] schemas = getSchemaNames( queryParams.get( "connection" ) );
+      JSONArray schemaNames = new JSONArray();
+      for ( String schema : schemas ) {
+        schemaNames.add( schema );
+      }
+      response.put( "schemaNames", schemaNames );
+      response.put( StepInterface.ACTION_STATUS, StepInterface.SUCCESS_RESPONSE );
+    } catch ( KettleDatabaseException e ) {
+      log.logError( e.getMessage() );
+    }
+    return response;
+  }
+
+  public String[] getSchemaNames( String dbname ) throws KettleDatabaseException {
+    DatabaseMeta databaseMeta = getTransMeta().findDatabase( dbname );
+    if ( databaseMeta != null ) {
+      LoggingObjectInterface loggingObject = new SimpleLoggingObject(
+        "DB Lookup Step", LoggingObjectType.STEP, null );
+      Database database = new Database( loggingObject, databaseMeta );
+      try {
+        database.connect();
+        return Const.sortStrings( database.getSchemas() );
+      } finally {
+        database.disconnect();
+      }
+    }
+    return new String[ 0 ];
+  }
+
+  @SuppressWarnings( "java:S1144" ) // Using reflection this method is being invoked
+  private JSONObject getTableFieldAction( Map<String, String> queryParams ) {
+    JSONObject response = new JSONObject();
+    String connectionName = queryParams.get( "connection" );
+    String schema = queryParams.get( "schema" );
+    String table = queryParams.get( "table" );
+    String[] columns = getTableFields( connectionName, schema, table );
+    JSONArray columnsList = new JSONArray();
+    for ( String column : columns ) {
+      columnsList.add( column );
+    }
+    response.put( "columns", columnsList );
+    response.put( "actionStatus", StepInterface.SUCCESS_RESPONSE );
+    return response;
+  }
+
+  private String[] getTableFields( String connection, String schema, String table ) {
+    DatabaseMeta databaseMeta = getTransMeta().findDatabase( connection );
+    LoggingObjectInterface loggingObject = new SimpleLoggingObject(
+      "DB Lookup Step", LoggingObjectType.STEP, null );
+    Database db = new Database( loggingObject, databaseMeta );
+    try {
+      db.connect();
+      RowMetaInterface r =
+        db.getTableFieldsMeta( schema, table );
+      if ( null != r ) {
+        String[] fieldNames = r.getFieldNames();
+        if ( null != fieldNames ) {
+          return fieldNames;
+        }
+      }
+    } catch ( Exception e ) {
+      // ignore any errors here. drop downs will not be
+      // filled, but no problem for the user
+    } finally {
+      try {
+        if ( db != null ) {
+          db.disconnect();
+        }
+      } catch ( Exception ignored ) {
+        // ignore any errors here. Nothing we can do if
+        // connection fails to close properly
+        db = null;
+      }
+    }
+    return null;
+  }
+
 }
