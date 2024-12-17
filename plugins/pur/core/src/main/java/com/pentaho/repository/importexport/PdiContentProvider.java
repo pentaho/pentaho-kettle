c5a017b6f39d7a3f1a740db31c5e78a9b458ca8f
srallapa <109674488+srallapa@users.noreply.github.com>
2024-12-17 09:26:26 -0600

    [BISERVER-15176] reverting reverted BISERVER-15051 (#9765)
    * Revert "[BISERVER-15176] reverting the changes from BISERVER-15051"
    
    This reverts commit 7daaf7f2454eb60668611f4a7fc144949092c6d7.
    
    * Revert "[BISERVER-15176] reverting the changes from BISERVER-15051"
    
    This reverts commit 964f618e4e7dac690a3106bf995d61573ffaf74b.

--- plugins/pur/core/src/main/java/com/pentaho/repository/importexport/PdiContentProvider.java
+++ plugins/pur/core/src/main/java/com/pentaho/repository/importexport/PdiContentProvider.java
@@ -21,6 +21,7 @@ import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.parameters.DuplicateParamException;
 import org.pentaho.di.core.parameters.NamedParams;
 import org.pentaho.di.core.parameters.NamedParamsDefault;
+import org.pentaho.di.core.row.value.ValueMetaString;
 import org.pentaho.di.job.JobExecutionConfiguration;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.repository.Repository;
@@ -113,6 +114,12 @@ public class PdiContentProvider implements IPdiContentProvider {
           log.error( e );
         }
       }
+      /*Updating userVariables to remove based on the HideInternalVariable setting. If no other variables other than
+      Internal variables, then it won't show the Internal variable screen.*/
+      if ( ValueMetaString.convertStringToBoolean( System.getProperty( Const.HIDE_INTERNAL_VARIABLES,
+              Const.HIDE_INTERNAL_VARIABLES_DEFAULT ) ) ) {
+        userVariables.keySet().removeIf(key -> key.contains("Internal."));
+      }
     }
     return userVariables;
   }
@@ -151,6 +158,11 @@ public class PdiContentProvider implements IPdiContentProvider {
     }
 
     return jobVarsMap;
+  }
+
+  @Override
+  public String getHideInternalVariable(){
+    return System.getProperty( Const.HIDE_INTERNAL_VARIABLES, Const.HIDE_INTERNAL_VARIABLES_DEFAULT );
   }
 
   private NamedParams filterUserParameters( NamedParams params ) {
