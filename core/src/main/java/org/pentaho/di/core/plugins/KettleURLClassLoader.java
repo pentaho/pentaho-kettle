e3e7fb1bf8be56662006b2e622cfd17119909f52
Ezequiel Cuellar <ezequiel.cuellar-ojeda@hitachivantara.com>
2025-04-02 10:39:34 -0400

    [BACKLOG-43495] Extract the Hadoop configuration boot strap code from 8.3.0.0 to feature/no-osgi and refactor it

--- core/src/main/java/org/pentaho/di/core/plugins/KettleURLClassLoader.java
+++ core/src/main/java/org/pentaho/di/core/plugins/KettleURLClassLoader.java
@@ -43,6 +43,7 @@ public class KettleURLClassLoader extends URLClassLoader {
   private static Class<?> PKG = KettleURLClassLoader.class; // for i18n purposes, needed by Translator2!!
 
   private String name;
+  private ClassLoader child;
 
   public KettleURLClassLoader( URL[] url, ClassLoader classLoader ) {
     super( url, classLoader );
@@ -91,11 +92,20 @@ public class KettleURLClassLoader extends URLClassLoader {
 
   protected Class<?> loadClassFromParent( String arg0, boolean arg1 ) throws ClassNotFoundException {
     Class<?> clz;
-    if ( ( clz = getParent().loadClass( arg0 ) ) != null ) {
-      if ( arg1 ) {
-        resolveClass( clz );
+    try {
+      if ( ( clz = getParent().loadClass( arg0 ) ) != null ) {
+        if ( arg1 ) {
+          resolveClass( clz );
+        }
+        return clz;
       }
-      return clz;
+    } catch (ClassNotFoundException e ) {
+      if ( this.child != null ) {
+        Class cl = this.child.loadClass( arg0 );
+        if ( cl != null ) {
+          return cl;
+        }
+      }
     }
     throw new ClassNotFoundException( "Could not find :" + arg0 );
   }
@@ -296,5 +306,9 @@ public class KettleURLClassLoader extends URLClassLoader {
       url = getParent().getResource( name );
     }
     return url;
+  }
+
+  public void setChild( ClassLoader child ) {
+    this.child = child;
   }
 }
