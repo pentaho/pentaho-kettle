e61d35b2566adccd2b818a87d0b4aaa17c0c1090
nmohsildar <noor.mohsildar@hitachivantara.com>
2025-09-05 21:50:44 +0530

    [BACKLOG-45480] Leftover AEL code in TextFileInputMeta.java#resolve calls AliasedFileObject#getAELSafeURIString which hard codes s3 -> s3a

--- engine/src/main/java/org/pentaho/di/trans/steps/fileinput/text/TextFileInputMeta.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/fileinput/text/TextFileInputMeta.java
@@ -44,7 +44,6 @@ import org.pentaho.di.core.row.value.ValueMetaString;
 import org.pentaho.di.core.util.EnvUtil;
 import org.pentaho.di.core.util.Utils;
 import org.pentaho.di.core.variables.VariableSpace;
-import org.pentaho.di.core.vfs.AliasedFileObject;
 import org.pentaho.di.core.vfs.KettleVFS;
 import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.i18n.BaseMessages;
@@ -63,15 +62,13 @@ import org.pentaho.di.trans.steps.file.BaseFileField;
 import org.pentaho.di.trans.steps.file.BaseFileInputAdditionalField;
 import org.pentaho.di.trans.steps.file.BaseFileInputFiles;
 import org.pentaho.di.trans.steps.file.BaseFileInputMeta;
-import org.pentaho.di.workarounds.ResolvableResource;
 import org.pentaho.metastore.api.IMetaStore;
-import org.pentaho.di.core.vfs.AliasedFileObject;
 import org.w3c.dom.Node;
 
 @SuppressWarnings( "deprecation" )
 @InjectionSupported( localizationPrefix = "TextFileInput.Injection.", groups = { "FILENAME_LINES", "FIELDS", "FILTERS" } )
 public class TextFileInputMeta extends BaseFileInputMeta<BaseFileInputAdditionalField, BaseFileInputFiles, BaseFileField>
-    implements StepMetaInterface, ResolvableResource, CsvInputAwareMeta {
+    implements StepMetaInterface, CsvInputAwareMeta {
   private static Class<?> PKG = TextFileInputMeta.class; // for i18n purposes, needed by Translator2!! TODO: check i18n
                                                          // for base
 
@@ -1378,21 +1375,7 @@ public class TextFileInputMeta extends BaseFileInputMeta<BaseFileInputAdditional
     return KettleVFS.getInstance( bowl )
       .getFileObject( variableSpace.environmentSubstitute( vfsFileName ), variableSpace );
   }
-  @Override
-  public void resolve( Bowl bowl ) {
-    for ( int i = 0; i < inputFiles.fileName.length; i++ ) {
-      if ( inputFiles.fileName[i] != null && !inputFiles.fileName[i].isEmpty() ) {
-        try {
-          FileObject fileObject = KettleVFS.getFileObject( getParentStepMeta().getParentTransMeta().environmentSubstitute( inputFiles.fileName[i] ) );
-          if ( AliasedFileObject.isAliasedFile( fileObject ) ) {
-            inputFiles.fileName[i] = ( (AliasedFileObject) fileObject ).getAELSafeURIString();
-          }
-        } catch ( KettleFileException e ) {
-          throw new RuntimeException( e );
-        }
-      }
-    }
-  }
+
   @Override
   public boolean hasHeader() {
     return content == null ? false : content.header;
