7603665dc75ddcc63c92885947281b6f0f48fa69
joana-fb <132680682+joana-fb@users.noreply.github.com>
2026-04-23 10:15:39 +0100

    fix[PDI-20828]: prevent duplication and fix completion of repository path in log filenames (#10484)
    * fix[PDI-20828]: prevent duplication of repository path in log filenames
    
    * fix[PDI-20828]: update internal directory variable handling in TransMeta and MetaFileLoaderImpl
    
    * fix[PDI-20828]: add tests to ensure repository path handling in job log filenames
    
    Backported for: SP-7225

--- core/src/test/java/org/pentaho/di/core/logging/Slf4jLoggingEventListenerTest.java
+++ core/src/test/java/org/pentaho/di/core/logging/Slf4jLoggingEventListenerTest.java
@@ -153,4 +153,72 @@ public class Slf4jLoggingEventListenerTest {
     verify( transLogger ).info( "[" + testPath + "/TestTrans"
       + RepositoryObjectType.TRANSFORMATION.getExtension() + "]  " + msgText );
   }
+
+  @Test
+  public void testTransDoesNotDuplicateRepositoryPathWhenFilenameAlreadyIncludesIt() {
+    when( logObjProvider.apply( logChannelId ) ).thenReturn( loggingObject );
+    when( loggingObject.getObjectType() ).thenReturn( LoggingObjectType.TRANS );
+    when( message.getLevel() ).thenReturn( LogLevel.BASIC );
+
+    when( loggingObject.getRepositoryDirectory() ).thenReturn( repositoryDirectory );
+    when( repositoryDirectory.getPath() ).thenReturn( "/public" );
+
+    // Filename coming from repository execution already includes the repository directory
+    when( loggingObject.getFilename() ).thenReturn( "/public/tr_writeToLog.ktr" );
+
+    listener.eventAdded( logEvent );
+
+    verify( transLogger ).info( "[/public/tr_writeToLog.ktr]  " + msgText );
+  }
+
+  @Test
+  public void testJobDoesNotDuplicateRepositoryPathWhenFilenameAlreadyIncludesIt() {
+    when( logObjProvider.apply( logChannelId ) ).thenReturn( loggingObject );
+    when( loggingObject.getObjectType() ).thenReturn( LoggingObjectType.JOB );
+    when( message.getLevel() ).thenReturn( LogLevel.BASIC );
+
+    when( loggingObject.getRepositoryDirectory() ).thenReturn( repositoryDirectory );
+    when( repositoryDirectory.getPath() ).thenReturn( "/public" );
+
+    // Filename coming from repository execution already includes the repository directory
+    when( loggingObject.getFilename() ).thenReturn( "/public/jb_internalPath.kjb" );
+
+    listener.eventAdded( logEvent );
+
+    verify( jobLogger ).info( "[/public/jb_internalPath.kjb]  " + msgText );
+  }
+
+  @Test
+  public void testTransPreservesRepositoryPathWhenFilenameIsNotAbsoluteRepoPath() {
+    when( logObjProvider.apply( logChannelId ) ).thenReturn( loggingObject );
+    when( loggingObject.getObjectType() ).thenReturn( LoggingObjectType.TRANS );
+    when( message.getLevel() ).thenReturn( LogLevel.BASIC );
+
+    when( loggingObject.getRepositoryDirectory() ).thenReturn( repositoryDirectory );
+    when( repositoryDirectory.getPath() ).thenReturn( "/public" );
+
+    // Filename without leading '/' should still get repository path prepended
+    when( loggingObject.getFilename() ).thenReturn( "jb_internalPath.ktr" );
+
+    listener.eventAdded( logEvent );
+
+    verify( transLogger ).info( "[/public/jb_internalPath.ktr]  " + msgText );
+  }
+
+  @Test
+  public void testJobPreservesRepositoryPathWhenFilenameIsNotAbsoluteRepoPath() {
+    when( logObjProvider.apply( logChannelId ) ).thenReturn( loggingObject );
+    when( loggingObject.getObjectType() ).thenReturn( LoggingObjectType.JOB );
+    when( message.getLevel() ).thenReturn( LogLevel.BASIC );
+
+    when( loggingObject.getRepositoryDirectory() ).thenReturn( repositoryDirectory );
+    when( repositoryDirectory.getPath() ).thenReturn( "/public" );
+
+    // Filename without leading '/' should still get repository path prepended
+    when( loggingObject.getFilename() ).thenReturn( "jb_internalPath.kjb" );
+
+    listener.eventAdded( logEvent );
+
+    verify( jobLogger ).info( "[/public/jb_internalPath.kjb]  " + msgText );
+  }
 }
