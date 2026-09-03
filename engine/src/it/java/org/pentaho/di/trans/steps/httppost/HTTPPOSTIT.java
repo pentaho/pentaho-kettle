4da02853c830119360127cf5861ae4758c2f11d4
Bruno Cavaleiro <befc@users.noreply.github.com>
2026-09-03 16:45:49 +0100

    test(engine): use the bound loopback address [BACKLOG-43728]

--- engine/src/it/java/org/pentaho/di/trans/steps/httppost/HTTPPOSTIT.java
+++ engine/src/it/java/org/pentaho/di/trans/steps/httppost/HTTPPOSTIT.java
@@ -164,7 +164,9 @@ public class HTTPPOSTIT {
   }
 
   private String getHttpLocalhostUrl() {
-    return "http://" + host + ":" + httpServer.getAddress().getPort() + "/";
+    String boundHost = httpServer.getAddress().getAddress().getHostAddress();
+    return "http://" + ( boundHost.contains( ":" ) ? "[" + boundHost + "]" : boundHost )
+      + ":" + httpServer.getAddress().getPort() + "/";
   }
 
   @Test
