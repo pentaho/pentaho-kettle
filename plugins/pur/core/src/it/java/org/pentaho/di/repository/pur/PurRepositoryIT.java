32f4d6ac7a870f33bb9740549eaead9672efd5eb
Bruno Cavaleiro <befc@users.noreply.github.com>
2026-09-03 16:45:37 +0100

    fix(pur): repair PUR repository integration tests [BACKLOG-43728] (#10688)

--- plugins/pur/core/src/it/java/org/pentaho/di/repository/pur/PurRepositoryIT.java
+++ plugins/pur/core/src/it/java/org/pentaho/di/repository/pur/PurRepositoryIT.java
@@ -46,6 +46,7 @@ import org.pentaho.di.cluster.SlaveServer;
 import org.pentaho.di.core.bowl.DefaultBowl;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.NotePadMeta;
+import org.pentaho.di.core.Props;
 import org.pentaho.di.core.ProgressMonitorListener;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleException;
@@ -55,6 +56,7 @@ import org.pentaho.di.core.logging.KettleLoggingEventListener;
 import org.pentaho.di.core.logging.LogLevel;
 import org.pentaho.di.core.plugins.JobEntryPluginType;
 import org.pentaho.di.core.plugins.StepPluginType;
+import org.pentaho.di.core.service.PluginServiceLoader;
 import org.pentaho.di.core.vfs.KettleVFS;
 import org.pentaho.di.imp.ImportRules;
 import org.pentaho.di.imp.rule.ImportRuleInterface;
@@ -74,6 +76,10 @@ import org.pentaho.di.shared.SharedObjectInterface;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.steps.tableinput.TableInputMeta;
+import org.pentaho.metastore.locator.api.MetastoreLocator;
+import org.pentaho.metastore.locator.api.MetastoreProvider;
+import org.pentaho.metastore.locator.api.impl.MetastoreLocatorImpl;
+import org.pentaho.metastore.locator.impl.repository.RepositoryMetastoreProvider;
 import org.pentaho.metastore.api.IMetaStore;
 import org.pentaho.metastore.api.IMetaStoreAttribute;
 import org.pentaho.metastore.api.IMetaStoreElement;
@@ -235,6 +241,14 @@ public class PurRepositoryIT extends RepositoryTestBase implements ApplicationCo
     super.setUp();
 
     KettleEnvironment.init();
+    if ( !Props.isInitialized() ) {
+      Props.init( Props.TYPE_PROPERTIES_EMPTY );
+    }
+
+    PluginServiceLoader.registerService( PurRepositoryIT.class, MetastoreLocator.class,
+      new MetastoreLocatorImpl(), 0 );
+    PluginServiceLoader.registerService( PurRepositoryIT.class, MetastoreProvider.class,
+      new RepositoryMetastoreProvider( () -> repository ), 0 );
 
     // programmatically register plugins, annotation based plugins do not get loaded unless
     // they are in kettle's plugins folder.
